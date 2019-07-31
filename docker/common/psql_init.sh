f599f1f3b80d48b20f4e64ebeb166ecc2892690c
kemar <marc@marcarea.com>
2019-07-31 10:15:18 +0200

    Activate postgis in template1

--- docker/common/psql_init.sh
+++ docker/common/psql_init.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 set -e
 
+# Initialization script.
+# https://hub.docker.com/_/postgres/#initialization-scripts
+
+# Creating a spatial database
 # https://docs.djangoproject.com/en/dev/ref/contrib/gis/install/postgis/#creating-a-spatial-database
+# We activate postgis in template1: when you create a new database you get an exact copy of template1.
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+  \c template1;
+  CREATE EXTENSION IF NOT EXISTS postgis;
+  \c postgres;
   CREATE USER $ITOU_POSTGRES_USER WITH ENCRYPTED PASSWORD '$ITOU_POSTGRES_PASSWORD';
   CREATE DATABASE $ITOU_POSTGRES_DATABASE_NAME OWNER $ITOU_POSTGRES_USER;
   ALTER USER $ITOU_POSTGRES_USER CREATEDB;
-  \c $ITOU_POSTGRES_DATABASE_NAME;
-  CREATE EXTENSION postgis;
 EOSQL
