f67fe9d11b9960d6efb6967fb1bb1d521c67ba81
kemar <marc@marcarea.com>
2019-08-15 15:36:23 +0200

    Display appellations inline in the admin

--- itou/jobs/admin.py
+++ itou/jobs/admin.py
@@ -3,11 +3,22 @@ from django.contrib import admin
 from itou.jobs import models
 
 
+class AppellationsInline(admin.TabularInline):
+    model = models.Appellation
+    readonly_fields = ('code', 'name', 'short_name',)
+    can_delete = False
+
+    def has_add_permission(self, request):
+        return False
+
+
 @admin.register(models.Job)
 class JobAdmin(admin.ModelAdmin):
     list_display = ('code_rome', 'name',)
     list_filter = ('riasec_major', 'riasec_minor',)
     search_fields = ('code_rome', 'name',)
+    inlines = (AppellationsInline,)
+
 
 @admin.register(models.Appellation)
 class AppellationAdmin(admin.ModelAdmin):
