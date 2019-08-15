725b154950ec3ac98492b26f8b44e7c0bf2bf4eb
kemar <marc@marcarea.com>
2019-08-15 20:09:45 +0200

    Automatically turn a click on the logout link into a POST

---
+++ itou/static/js/logout.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+
+  // The user is logged out only when the confirmation is received by means of a POST request.
+  // This automatically turns a click on the logout link into a POST.
+  // https://django-allauth.readthedocs.io/en/latest/views.html#logout-account-logout
+
+  let logoutLink = $('#js-logout')
+  let actionUrl = logoutLink.attr('href')
+  let csrfToken = $('meta[name=csrf-token]').attr('content')
+
+  logoutLink
+    .on('click', function (e) {
+      e.preventDefault()
+      let input = `<input type="hidden" name="csrfmiddlewaretoken" value="${csrfToken}">`
+      $(`<form action="${actionUrl}" method="post">${input}</form>`).submit()
+    })
+
+})
