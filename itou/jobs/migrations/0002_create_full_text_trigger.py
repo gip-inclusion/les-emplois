4d58346498b509de461a155986eaf649371acd63
kemar <marc@marcarea.com>
2019-08-21 16:56:15 +0200

    Reset migrations

--- itou/jobs/migrations/0002_create_full_text_trigger.py
+++ itou/jobs/migrations/0002_create_full_text_trigger.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('jobs', '0003_auto_20190821_1525'),
+        ('jobs', '0001_initial'),
     ]
 
     operations = [
