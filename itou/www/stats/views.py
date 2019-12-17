e669dbe686306478e5dc5c7065b06f7b5bcbb5a0
Vermeer GRANGE <vermeer.grange@gmail.com>
2019-12-17 12:40:46 +0200

    Add more KPIs to stats page (v2)

--- itou/www/stats/views.py
+++ itou/www/stats/views.py
@@ -11,58 +11,123 @@ from itou.users.models import User
 from itou.job_applications.models import JobApplication, JobApplicationWorkflow
 
 
+def inject_siaes_subset_total_and_by_kind(data, kpi_name, siaes_subset, visible=True):
+    if "siaes_by_kind" not in data:
+        data["siaes_by_kind"] = {}
+        data["siaes_by_kind"]["categories"] = Siae.KIND_CHOICES
+        data["siaes_by_kind"]["series"] = []
+
+    siaes_by_kind_as_list = (
+        siaes_subset.values("kind")
+        .annotate(total=Count("pk", distinct=True))
+        .order_by("kind")
+    )
+
+    siaes_by_kind_as_dict = {}
+    for item in siaes_by_kind_as_list:
+        siaes_by_kind_as_dict[item["kind"]] = item["total"]
+
+    serie_values = []
+    for kind_choice in data["siaes_by_kind"]["categories"]:
+        kind = kind_choice[0]
+        if kind in siaes_by_kind_as_dict:
+            serie_values.append(siaes_by_kind_as_dict[kind])
+        else:
+            serie_values.append(0)
+
+    total = siaes_subset.count()
+    if sum(serie_values) != total:
+        raise ValueError("Inconsistent results.")
+
+    data["siaes_by_kind"]["series"].append(
+        {"name": kpi_name, "values": serie_values, "total": total, "visible": visible}
+    )
+    return data
+
+
 def stats(request, template_name="stats/stats.html"):
     data = {}
 
     # --- Siae stats.
 
-    data["total_siaes"] = Siae.active_objects.count()
+    kpi_name = "Siaes à ce jour"
+    siaes_subset = Siae.active_objects
+    data = inject_siaes_subset_total_and_by_kind(data, kpi_name, siaes_subset)
 
-    data["total_siaes_with_user"] = (
+    kpi_name = "Siaes ayant au moins un utilisateur à ce jour"
+    siaes_subset = Siae.active_objects.filter(
+        siaemembership__user__is_active=True
+    ).distinct()
+    data = inject_siaes_subset_total_and_by_kind(
+        data, kpi_name, siaes_subset, visible=False
+    )
+
+    kpi_name = "Siaes ayant au moins une FDP à ce jour"
+    siaes_subset = Siae.active_objects.exclude(
+        job_description_through__isnull=True
+    ).distinct()
+    data = inject_siaes_subset_total_and_by_kind(
+        data, kpi_name, siaes_subset, visible=False
+    )
+
+    kpi_name = "Siaes ayant au moins une FDP active à ce jour"
+    siaes_subset = Siae.active_objects.filter(
+        job_description_through__is_active=True
+    ).distinct()
+    data = inject_siaes_subset_total_and_by_kind(
+        data, kpi_name, siaes_subset, visible=False
+    )
+
+    kpi_name = "Siaes ayant au moins un utilisateur et une FDP à ce jour"
+    siaes_subset = (
         Siae.active_objects.filter(siaemembership__user__is_active=True)
+        .exclude(job_description_through__isnull=True)
         .distinct()
-        .count()
+    )
+    data = inject_siaes_subset_total_and_by_kind(
+        data, kpi_name, siaes_subset, visible=False
     )
 
-    data["total_siaes_with_job_description"] = (
-        Siae.active_objects.exclude(job_description_through__isnull=True)
+    kpi_name = "Siaes ayant au moins un utilisateur et une FDP active à ce jour"
+    siaes_subset = (
+        Siae.active_objects.filter(siaemembership__user__is_active=True)
+        .filter(job_description_through__is_active=True)
         .distinct()
-        .count()
+    )
+    data = inject_siaes_subset_total_and_by_kind(
+        data, kpi_name, siaes_subset, visible=False
     )
 
-    data["total_siaes_with_active_job_description"] = (
-        Siae.active_objects.filter(job_description_through__is_active=True)
-        .distinct()
-        .count()
-    )
-
+    kpi_name = "Siaes actives à ce jour"
     two_weeks_ago = datetime.date.today() - datetime.timedelta(2 * 7)
-    data["total_active_siaes"] = (
-        Siae.active_objects.filter(
-            Q(updated_at__gte=two_weeks_ago)
-            # FIXME weird fact: most siae were created on Dec 5th o_O why??
-            # | Q(created_at__gte=two_weeks_ago)
-            | Q(siaemembership__user__date_joined__gte=two_weeks_ago)
-            | Q(job_description_through__created_at__gte=two_weeks_ago)
-            | Q(job_description_through__updated_at__gte=two_weeks_ago)
-            | Q(job_applications_received__created_at__gte=two_weeks_ago)
-            | Q(job_applications_received__updated_at__gte=two_weeks_ago)
-        )
-        .distinct()
-        .count()
-    )
+    siaes_subset = Siae.active_objects.filter(
+        Q(updated_at__gte=two_weeks_ago)
+        | Q(created_at__gte=two_weeks_ago)
+        | Q(siaemembership__user__date_joined__gte=two_weeks_ago)
+        | Q(job_description_through__created_at__gte=two_weeks_ago)
+        | Q(job_description_through__updated_at__gte=two_weeks_ago)
+        | Q(job_applications_received__created_at__gte=two_weeks_ago)
+        | Q(job_applications_received__updated_at__gte=two_weeks_ago)
+    ).distinct()
+    data = inject_siaes_subset_total_and_by_kind(data, kpi_name, siaes_subset)
+
+    kpi_name = "Siaes ayant au moins une embauche"
+    siaes_subset = Siae.active_objects.filter(
+        job_applications_received__state=JobApplicationWorkflow.STATE_ACCEPTED
+    ).distinct()
+    data = inject_siaes_subset_total_and_by_kind(data, kpi_name, siaes_subset)
 
     # --- Candidate stats.
 
     data["total_job_applications"] = JobApplication.objects.count()
 
-    data["job_applications_per_creation_week"] = get_total_per_week(
-        JobApplication.objects, date_field="created_at", total_expression=Count("pk")
-    )
-
     data["total_accepted_job_applications"] = JobApplication.objects.filter(
         state=JobApplicationWorkflow.STATE_ACCEPTED
     ).count()
+
+    data["job_applications_per_creation_week"] = get_total_per_week(
+        JobApplication.objects, date_field="created_at", total_expression=Count("pk")
+    )
 
     data["accepted_job_applications_per_creation_week"] = get_total_per_week(
         JobApplication.objects.filter(state=JobApplicationWorkflow.STATE_ACCEPTED),
