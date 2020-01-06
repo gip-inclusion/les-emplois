7101e2bc797c4ce60fbc83b7237d0c75bccb755c
Vermeer GRANGE <vermeer.grange@gmail.com>
2020-01-06 16:22:49 +0200

    Drop obsolete content app

--- itou/www/content/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.views.generic import TemplateView
-
-
-# https://docs.djangoproject.com/en/dev/topics/http/urls/#url-namespaces-and-included-urlconfs
-app_name = "content"
-
-urlpatterns = [
-]
