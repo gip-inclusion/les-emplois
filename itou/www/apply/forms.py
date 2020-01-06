70ac6b54b0696d440b4f45e707ce96440b7fa5e1
kemar <marc@marcarea.com>
2020-01-06 17:24:33 +0100

    Fix wording

--- itou/www/apply/forms.py
+++ itou/www/apply/forms.py
@@ -96,7 +96,7 @@ class RefusalForm(forms.Form):
     """
 
     ANSWER_INITIAL = _(
-        "Nous avons étudié votre dossier avec la plus grande attention mais "
+        "Nous avons étudié votre candidature avec la plus grande attention mais "
         "nous sommes au regret de vous informer que celle-ci n'a pas été retenue.\n\n"
         "Soyez assuré que cette décision ne met pas en cause vos qualités personnelles. "
         "Nous sommes très sensibles à l'intérêt que vous portez à notre entreprise, "
