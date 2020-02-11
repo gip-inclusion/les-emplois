f2dfd27747ae6b2316675b10d8cafd591a999929
kemar <marc@marcarea.com>
2020-02-11 12:00:30 +0100

    Prevent multiple submit client side

---
+++ itou/static/js/prevent_multiple_submit.js
@@ -0,0 +1,13 @@
+$(document).ready(() => {
+
+  // Prevent multiple submit client side.
+  // Never trust the client: validation must also happen server side.
+  // Opt-in by adding a `js-prevent-multiple-submit` CSS class to a <form>.
+
+  $('form.js-prevent-multiple-submit').on('submit', function () {
+      $(':submit', this).on('click', function () {
+          return false
+      })
+  })
+
+})
