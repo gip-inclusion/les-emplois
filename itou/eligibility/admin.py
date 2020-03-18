96ae0b85142ac888185f2f097d6e9a3f2a26852f
kemar <marc@marcarea.com>
2020-03-18 16:21:53 +0100

    Add `author` to raw_id_fields

--- itou/eligibility/admin.py
+++ itou/eligibility/admin.py
@@ -8,6 +8,6 @@ class EligibilityAdmin(admin.ModelAdmin):
     list_display = ("id", "job_seeker", "author", "author_kind", "created_at")
     list_display_links = ("id", "job_seeker")
     list_filter = ("author_kind",)
-    raw_id_fields = ("job_seeker", "author_siae", "author_prescriber_organization")
+    raw_id_fields = ("job_seeker", "author", "author_siae", "author_prescriber_organization")
     readonly_fields = ("created_at", "updated_at")
     search_fields = ("job_seeker__email", "author__email")
