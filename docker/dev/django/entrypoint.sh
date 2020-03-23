2dd995451a8bbae6c23575b64c889462738f5692
Céline MS <celine@hello-birds.com>
2020-03-23 11:31:24 +0100

    update Docker config

--- docker/dev/django/entrypoint.sh
+++ docker/dev/django/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -e
 
+# ------ Specific to Clever Cloud ------
+# If $POSTGRESQL_ADDON_* is set, replace POSTGRES_* by its value.
+# Otherwise, keep our default configuration.
+POSTGRES_HOST=${POSTGRESQL_ADDON_HOST:-$POSTGRES_HOST}
+POSTGRES_PORT=${POSTGRESQL_ADDON_PORT:-$POSTGRES_PORT}
+
 while ! pg_isready -h $POSTGRES_HOST -p $POSTGRES_PORT; do
     >&2 echo "Postgres is unavailable - sleeping"
     sleep 1
