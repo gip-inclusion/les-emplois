c7d5cbea6e697d313fd880d1c746a65f2317aa5b
Céline MS <celine@hello-birds.com>
2020-03-27 16:52:04 +0100

    Create review apps on Clever Cloud each time a PR has a  label.

---
+++ config/settings/clever_cloud.py
@@ -0,0 +1,51 @@
+from .base import *  # noqa
+
+DEBUG = os.environ.get("DJANGO_DEBUG", True)
+
+ALLOWED_HOSTS = ["127.0.0.1", ".cleverapps.io"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+# Django-extensions.
+# ------------------------------------------------------------------------------
+
+INSTALLED_APPS += ["django_extensions"]  # noqa F405
+
+# Django-debug-toolbar.
+# ------------------------------------------------------------------------------
+
+INSTALLED_APPS += ["debug_toolbar"]  # noqa F405
+
+INTERNAL_IPS = ["127.0.0.1"]
+
+# Enable django-debug-toolbar with Docker.
+import socket
+
+_, _, ips = socket.gethostbyname_ex(socket.gethostname())
+INTERNAL_IPS += [ip[:-1] + "1" for ip in ips]
+
+MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa F405
+
+DEBUG_TOOLBAR_CONFIG = {
+    # https://django-debug-toolbar.readthedocs.io/en/latest/panels.html#panels
+    "DISABLE_PANELS": [
+        "debug_toolbar.panels.redirects.RedirectsPanel",
+        # ProfilingPanel makes the django admin extremely slow...
+        "debug_toolbar.panels.profiling.ProfilingPanel",
+    ],
+    "SHOW_TEMPLATE_CONTEXT": True,
+}
+
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        "HOST": os.environ.get("POSTGRESQL_ADDON_HOST"),
+        "PORT": os.environ.get("POSTGRESQL_ADDON_PORT"),
+        "NAME": os.environ.get("POSTGRESQL_ADDON_DB"),
+        "USER": os.environ.get("POSTGRESQL_ADDON_USER"),
+        "PASSWORD": os.environ.get("POSTGRESQL_ADDON_PASSWORD"),
+    }
+}
