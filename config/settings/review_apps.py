959bcd43621fc1aaaa71de35c083c88f80831351
Céline MS <celine@hello-birds.com>
2020-04-01 11:03:47 +0200

    Rename settings/clever_cloud.py to settings/review_apps.py

---
+++ config/settings/review_apps.py
@@ -0,0 +1,24 @@
+from .base import *  # noqa
+
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+ALLOWED_HOSTS = [".cleverapps.io"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        "HOST": os.environ.get("POSTGRESQL_ADDON_HOST"),
+        "PORT": os.environ.get("POSTGRESQL_ADDON_PORT"),
+        "NAME": os.environ.get("POSTGRESQL_ADDON_DB"),
+        "USER": os.environ.get("POSTGRESQL_ADDON_USER"),
+        "PASSWORD": os.environ.get("POSTGRESQL_ADDON_PASSWORD"),
+    }
+}
+
+ITOU_PROTOCOL = "https"
+ITOU_FQDN = "staging.inclusion.beta.gouv.fr"
+ITOU_EMAIL_CONTACT = "contact+staging@inclusion.beta.gouv.fr"
+DEFAULT_FROM_EMAIL = "noreply+staging@inclusion.beta.gouv.fr"
+
+sentry_sdk.init(dsn=os.environ["SENTRY_DSN_STAGING"], integrations=[DjangoIntegration()])
