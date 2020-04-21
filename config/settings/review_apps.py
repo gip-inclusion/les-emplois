19518a6b8ad0cf83a9811a75d608a034e83fffb6
Céline MS <celine@hello-birds.com>
2020-04-21 17:11:32 +0200

    Try to avoid spamming Sentry with always the same known error 'Invalid HTTP_HOST header'.

--- config/settings/review_apps.py
+++ config/settings/review_apps.py
@@ -2,6 +2,7 @@ from .base import *  # noqa
 
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
+from sentry_sdk.integrations.logging import ignore_logger
 
 ALLOWED_HOSTS = ["127.0.0.1", ".cleverapps.io"]
 
@@ -22,3 +23,4 @@ ITOU_EMAIL_CONTACT = "contact+staging@inclusion.beta.gouv.fr"
 DEFAULT_FROM_EMAIL = "noreply+staging@inclusion.beta.gouv.fr"
 
 sentry_sdk.init(dsn=os.environ["SENTRY_DSN_STAGING"], integrations=[DjangoIntegration()])
+ignore_logger("django.security.DisallowedHost")
