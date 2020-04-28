3bc3bfa23bdb0b497026ea3510276276ace5a941
Vermeer GRANGE <vermeer.grange@gmail.com>
2020-04-28 17:09:11 +0300

    Add matomo custom variables for all account types

--- itou/utils/perms/context_processors.py
+++ itou/utils/perms/context_processors.py
@@ -37,10 +37,47 @@ def get_current_organization_and_perms(request):
             prescriber_organization = membership.organization
             user_is_prescriber_org_admin = membership.is_admin
 
-    return {
+    context = {
         "current_prescriber_organization": prescriber_organization,
         "current_siae": siae,
         "user_is_prescriber_org_admin": user_is_prescriber_org_admin,
         "user_is_siae_admin": user_is_siae_admin,
         "user_siae_set": user_siae_set,
+    }
+    context.update(
+        get_matomo_context(
+            user=request.user, prescriber_organization=prescriber_organization, user_is_siae_admin=user_is_siae_admin
+        )
+    )
+    return context
+
+
+def get_matomo_context(user, prescriber_organization, user_is_siae_admin):
+    matomo_custom_var_is_authenticated = "yes" if user.is_authenticated else "no"
+
+    if not user.is_authenticated:
+        matomo_custom_var_account_type = "anonymous"
+        matomo_custom_var_account_sub_type = "anonymous"
+    elif user.is_job_seeker:
+        matomo_custom_var_account_type = "job_seeker"
+        matomo_custom_var_account_sub_type = "peconnect" if user.is_peamu_user else "not_peconnect"
+    elif user.is_prescriber:
+        matomo_custom_var_account_type = "prescriber"
+        if prescriber_organization:
+            matomo_custom_var_account_sub_type = (
+                "authorized_org" if prescriber_organization.is_authorized else "unauthorized_org"
+            )
+        else:
+            matomo_custom_var_account_sub_type = "without_org"
+    elif user.is_siae_staff:
+        matomo_custom_var_account_type = "employer"
+        matomo_custom_var_account_sub_type = "admin" if user_is_siae_admin else "not_admin"
+    else:
+        matomo_custom_var_account_type = "unknown"
+        matomo_custom_var_account_sub_type = "unknown"
+
+    return {
+        "matomo_custom_var_is_authenticated": matomo_custom_var_is_authenticated,
+        "matomo_custom_var_account_type": matomo_custom_var_account_type,
+        "matomo_custom_var_account_sub_type": matomo_custom_var_account_sub_type,
     }
--- itou/utils/tests.py
+++ itou/utils/tests.py
@@ -59,6 +59,9 @@ class ContextProcessorsGetCurrentOrganizationAndPermsTest(TestCase):
                 "user_is_prescriber_org_admin": False,
                 "user_is_siae_admin": True,
                 "user_siae_set": [siae],
+                "matomo_custom_var_is_authenticated": "yes",
+                "matomo_custom_var_account_type": "employer",
+                "matomo_custom_var_account_sub_type": "admin",
             }
             self.assertDictEqual(expected, result)
 
@@ -92,6 +95,9 @@ class ContextProcessorsGetCurrentOrganizationAndPermsTest(TestCase):
                 "user_is_prescriber_org_admin": False,
                 "user_is_siae_admin": False,
                 "user_siae_set": [siae1, siae2, siae3],
+                "matomo_custom_var_is_authenticated": "yes",
+                "matomo_custom_var_account_type": "employer",
+                "matomo_custom_var_account_sub_type": "not_admin",
             }
             self.assertDictEqual(expected, result)
 
@@ -117,6 +123,9 @@ class ContextProcessorsGetCurrentOrganizationAndPermsTest(TestCase):
                 "user_is_prescriber_org_admin": True,
                 "user_is_siae_admin": False,
                 "user_siae_set": [],
+                "matomo_custom_var_is_authenticated": "yes",
+                "matomo_custom_var_account_type": "prescriber",
+                "matomo_custom_var_account_sub_type": "unauthorized_org",
             }
             self.assertDictEqual(expected, result)
 
