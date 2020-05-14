877f0046b5678e8b7e96733ea8708c9cb3f7ca12
Vermeer GRANGE <vermeer.grange@gmail.com>
2020-05-14 11:20:24 +0300

    Cut test duration by 33%

--- itou/www/stats/tests.py
+++ itou/www/stats/tests.py
@@ -1,8 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
 
-from itou.utils.address.departments import DEPARTMENTS
-
 
 class StatsViewTest(TestCase):
     def test_stats(self):
@@ -11,7 +9,7 @@ class StatsViewTest(TestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
-        for department in DEPARTMENTS.keys():
-            url = reverse("stats:index")
-            response = self.client.get(url, {"department": department})
-            self.assertEqual(response.status_code, 200)
+        department = "75"
+        url = reverse("stats:index")
+        response = self.client.get(url, {"department": department})
+        self.assertEqual(response.status_code, 200)
