7c80eac4d889d5e3efbf638ae0e68ee4925f9744
kemar <marc@marcarea.com>
2020-05-21 11:20:44 +0200

    Remove not needed set_logger

--- itou/approvals/management/commands/export_approvals.py
+++ itou/approvals/management/commands/export_approvals.py
@@ -16,27 +16,12 @@ class Command(BaseCommand):
     There is no optional argument at the moment.
     """
 
-    help = "Export the content of the approvals from the database into a xlsx file or an output stream."
-
-    def set_logger(self, verbosity):
-        """
-        Set logger level based on the verbosity option.
-        """
-        handler = logging.StreamHandler(self.stdout)
-
-        self.logger = logging.getLogger(__name__)
-        self.logger.propagate = False
-        self.logger.addHandler(handler)
-
-        self.logger.setLevel(logging.INFO)
-        if verbosity > 1:
-            self.logger.setLevel(logging.DEBUG)
+    help = "Export the content of the Approvals from the database into an xlsx file."
 
     def handle(self, **options):
-
-        self.set_logger(options.get("verbosity"))
-        self.logger.info(f"Exporting approvals")
+        self.stdout.write("Exporting approvals")
 
         result = export_approvals()
 
-        self.logger.info(f"Done!\nApprovals / PASS IAE export file written to: '{result}'")
+        self.stdout.write("Done!")
+        self.stdout.write(f"Approvals / PASS IAE export file written to: '{result}'")
