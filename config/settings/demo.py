21b1fac0c68d677bf718542fadde8517c9bc6f10
Frédéric Vergez <fred@ikarius.com>
2020-06-11 10:13:19 +0200

    WIP - Same as review apps

--- config/settings/demo.py
+++ config/settings/demo.py
@@ -4,17 +4,17 @@ import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 from sentry_sdk.integrations.logging import ignore_logger
 
-ALLOWED_HOSTS = ["127.0.0.1", "cleverapps.io", "demo.inclusion.beta.gouv.fr"]
+ALLOWED_HOSTS = ["127.0.0.1", ".cleverapps.io"]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
-ROOT_URLCONF = "config.urls"
+# ROOT_URLCONF = "config.urls"
 
 DATABASES = {
     "default": {
         "ENGINE": "django.contrib.gis.db.backends.postgis",
-        "HOST": os.environ.get("POSTGRESQL_ADDON_DIRECT_HOST"),
-        "PORT": os.environ.get("POSTGRESQL_ADDON_DIRECT_PORT"),
+        "HOST": os.environ.get("POSTGRESQL_ADDON_HOST"),
+        "PORT": os.environ.get("POSTGRESQL_ADDON_PORT"),
         "NAME": os.environ.get("DEMO_APP_DB_NAME"),
         "USER": os.environ.get("POSTGRESQL_ADDON_USER"),
         "PASSWORD": os.environ.get("POSTGRESQL_ADDON_PASSWORD"),
@@ -28,9 +28,9 @@ DATABASES = {
 
 
 ITOU_PROTOCOL = "https"
-#ITOU_FQDN = "demo.inclusion.beta.gouv.fr"
-#ITOU_EMAIL_CONTACT = "contact+demo@inclusion.beta.gouv.fr"
-#DEFAULT_FROM_EMAIL = "noreply+demo@inclusion.beta.gouv.fr"
+ITOU_FQDN = "staging.inclusion.beta.gouv.fr"
+ITOU_EMAIL_CONTACT = "contact+demo@inclusion.beta.gouv.fr"
+DEFAULT_FROM_EMAIL = "noreply+demo@inclusion.beta.gouv.fr"
 
-# sentry_sdk.init(dsn=os.environ["SENTRY_DSN_demo"], integrations=[DjangoIntegration()])
+ sentry_sdk.init(dsn=os.environ["SENTRY_DSN_STAGING"], integrations=[DjangoIntegration()])
 ignore_logger("django.security.DisallowedHost")
