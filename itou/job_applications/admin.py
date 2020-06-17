c47308a3a080e1e4e39ed7ce885aa1c429f90e46
kemar <marc@marcarea.com>
2020-06-17 14:32:32 +0200

    Add approval_number_delivered_by to readonly_fields

--- itou/job_applications/admin.py
+++ itou/job_applications/admin.py
@@ -52,7 +52,7 @@ class JobApplicationAdmin(admin.ModelAdmin):
         "sender_prescriber_organization__is_authorized",
         "to_siae__department",
     )
-    readonly_fields = ("created_at", "updated_at")
+    readonly_fields = ("created_at", "updated_at", "approval_number_delivered_by")
     inlines = (JobsInline, TransitionLogInline)
     search_fields = ("to_siae__siret", "job_seeker__email")
 
