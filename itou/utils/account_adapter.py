49eb666d5ad52b1e57a2c0d60082a02e8cc888b0
Frédéric Vergez <fred@ikarius.com>
2020-06-18 18:07:06 +0200

    Added comment for account adapter class

--- itou/utils/account_adapter.py
+++ itou/utils/account_adapter.py
@@ -2,10 +2,15 @@ from allauth.account.adapter import DefaultAccountAdapter
 from django.conf import settings
 
 
-print("Using demo adpater")
-
-
 class DemoAccountAdapter(DefaultAccountAdapter):
+    """
+    Overrides standard allauth adapter:
+        * provides additionnal context to some emails sent via allauth
+
+    Activation of this adapter is done in project settings with:
+        ACCOUNT_ADAPTER = "name_of_class"
+    """
+
     def send_mail(self, template_prefix, email, context):
         context["itou_environment"] = settings.ITOU_ENVIRONMENT
         super().send_mail(template_prefix, email, context)
