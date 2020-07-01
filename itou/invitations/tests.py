Chamane/itou

An employer can send an invitation to join its structure.

itou/invitations/tests.py
@@ -1,7 +1,14 @@
 from django.test import TestCase
 from django.utils import timezone
 
-from itou.invitations.factories import ExpiredInvitationFactory, InvitationFactory, SentInvitationFactory
+from itou.invitations.factories import (
+    ExpiredInvitationFactory,
+    InvitationFactory,
+    SentInvitationFactory,
+    SiaeSentInvitationFactory,
+)
+from itou.invitations.models import InvitationWrapper, SiaeStaffInvitation
+from itou.users.factories import UserFactory
 
 
 class InvitationModelTest(TestCase):
@@ -38,6 +45,16 @@ class InvitationModelTest(TestCase):
         invitation.extend_expiration_date()
         self.assertFalse(invitation.has_expired)
 
+    def test_get_model_from_string(self):
+        invitation_type = InvitationWrapper.get_model_from_string("siae_staff")
+        self.assertEqual(invitation_type, SiaeStaffInvitation)
+
+        with self.assertRaises(TypeError):
+            InvitationWrapper.get_model_from_string("wrong_type")
+
+        with self.assertRaises(TypeError):
+            InvitationWrapper.get_model_from_string(12)
+
 
 class InvitationEmailsTest(TestCase):
     def test_send_invitation(self):
@@ -45,7 +62,7 @@ class InvitationEmailsTest(TestCase):
         email = invitation.email_invitation
 
         # Subject
-        self.assertIn(invitation.sender.first_name, email.subject)
+        self.assertIn(invitation.sender.first_name.title(), email.subject)
         self.assertIn(invitation.sender.last_name, email.subject)
 
         # Body
@@ -58,8 +75,49 @@ class InvitationEmailsTest(TestCase):
         # To
         self.assertIn(invitation.email, email.to)
 
+
+class TestSiaeInvitation(TestCase):
+    def test_add_member_to_siae(self):
+        invitation = SiaeSentInvitationFactory(email="hey@you.com")
+        user = UserFactory(email=invitation.email)
+        siae_members = invitation.siae.members.count()
+        invitation.add_invited_user_to_siae()
+        siae_members_after = invitation.siae.members.count()
+        self.assertEqual(siae_members + 1, siae_members_after)
+
+        user.refresh_from_db()
+        self.assertTrue(user.is_siae_staff)
+
+
+class TestSiaeInvitationEmails(TestCase):
+    def test_accepted_notif_siae_members(self):
+        user = UserFactory()
+        invitation = SiaeSentInvitationFactory(email=user.email)
+        invitation.siae.members.add(user)
+        email = invitation.email_accepted_notif_siae_members
+
+        # Subject
+        self.assertIn(invitation.first_name, email.subject)
+        self.assertIn(invitation.last_name, email.subject)
+
+        # Body
+        self.assertIn(invitation.first_name, email.body)
+        self.assertIn(invitation.last_name, email.body)
+        self.assertIn(invitation.email, email.body)
+        self.assertIn(invitation.sender.first_name, email.body)
+        self.assertIn(invitation.sender.last_name, email.body)
+        self.assertIn(invitation.siae.display_name, email.body)
+
+        # To
+        members = invitation.siae.members.exclude(email__in=[invitation.sender.email, invitation.email])
+        for member in members:
+            self.assertIn(member.email, email.to)
+
+        self.assertNotIn(invitation.sender.email, email.to)
+        self.assertNotIn(invitation.email, email.to)
+
     def test_accepted_notif_sender(self):
-        invitation = SentInvitationFactory()
+        invitation = SiaeSentInvitationFactory()
         email = invitation.email_accepted_notif_sender
 
         # Subject
@@ -70,6 +128,23 @@ class InvitationEmailsTest(TestCase):
         self.assertIn(invitation.first_name, email.body)
         self.assertIn(invitation.last_name, email.body)
         self.assertIn(invitation.email, email.body)
+        self.assertIn(invitation.siae.display_name, email.body)
 
         # To
         self.assertIn(invitation.sender.email, email.to)
+
+    def test_email_invitation(self):
+        invitation = SiaeSentInvitationFactory()
+        email = invitation.email_invitation
+
+        # Subject
+        self.assertIn(invitation.siae.display_name, email.subject)
+
+        # Body
+        self.assertIn(invitation.first_name, email.body)
+        self.assertIn(invitation.last_name, email.body)
+        self.assertIn(invitation.acceptance_link, email.body)
+        self.assertIn(invitation.siae.display_name, email.body)
+
+        # To
+        self.assertIn(invitation.email, email.to)

itou/www/invitations_views/views.py
@@ -1,29 +1,29 @@
 from allauth.account.adapter import DefaultAccountAdapter
-from dateutil.relativedelta import relativedelta
 from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
-from django.http import Http404
-from django.shortcuts import redirect, render
-from django.utils import timezone
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import get_object_or_404, redirect, render, reverse
+from django.utils import formats, safestring
 from django.utils.translation import gettext as _, ngettext as __
 
-from itou.invitations.models import Invitation
-from itou.www.invitations_views.forms import InvitationFormSet, NewUserForm
+from itou.invitations.models import InvitationWrapper, SiaeStaffInvitation
+from itou.utils.perms.siae import get_current_siae_or_404
+from itou.utils.urls import get_safe_url
+from itou.www.invitations_views.forms import NewSiaeStaffInvitationFormSet, NewUserForm
 
 
-def accept(request, invitation_id, template_name="invitations_views/accept.html"):
-    if request.user.is_authenticated:
-        messages.error(request, _("Merci de vous déconnecter avant d'accepter cette invitation."))
-        return redirect("account_logout")
+def new_user(request, invitation_type, invitation_id, template_name="invitations_views/new_user.html"):
 
-    try:
-        invitation = Invitation.objects.get(pk=invitation_id)
-    except Invitation.DoesNotExist:
-        raise Http404(_("Aucune invitation n'a été trouvée."))
-
+    invitation_type = InvitationWrapper.get_model_from_string(invitation_type)
+    invitation = get_object_or_404(invitation_type, pk=invitation_id)
     context = {"invitation": invitation}
     next_step = None
+
+    if request.user.is_authenticated:
+        if not request.user.email == invitation.email:
+            messages.error(request, _("Merci de vous déconnecter avant d'accepter cette invitation."))
+            return redirect("account_logout")
 
     if invitation.can_be_accepted:
         user = get_user_model().objects.filter(email=invitation.email)
@@ -32,34 +32,73 @@ def accept(request, invitation_id, template_name="invitations_views/accept.html"
             context["form"] = form
             if form.is_valid():
                 user = form.save(request)
-                invitation.accept()
                 DefaultAccountAdapter().login(request, user)
-                next_step = redirect("dashboard:index")
+                next_step = redirect(get_safe_url(request, "redirect_to"))
         else:
-            messages.error(request, _("Vous comptez déjà parmi les membres de notre site."))
+            next_step = "{}?account_type={}&next={}".format(
+                reverse("account_login"), invitation.SIGNIN_ACCOUNT_TYPE, get_safe_url(request, "redirect_to")
+            )
+            next_step = redirect(next_step)
 
     return next_step or render(request, template_name, context=context)
 
 
 @login_required
-def create(request, template_name="invitations_views/create.html"):
-    form_kwargs = {"sender": request.user}
-    formset = InvitationFormSet(data=request.POST or None, form_kwargs=form_kwargs)
-    expiration_date = timezone.now() + relativedelta(days=Invitation.EXPIRATION_DAYS)
+def join_siae(request, invitation_id, template_name="invitations_views/join_siae.html"):
+    invitation = get_object_or_404(SiaeStaffInvitation, pk=invitation_id)
+    if not invitation.guest_can_join_organization(request):
+        raise PermissionDenied()
 
+    context = {"invitation": invitation, "show_join_button": True}
+
+    next_step = None
+    if request.method == "POST":
+        if invitation.can_be_accepted:
+            invitation.add_invited_user_to_siae()
+            invitation.accept()
+            messages.success(
+                request, _(f"Vous êtes désormais membre de l'organisation {invitation.siae.display_name}.")
+            )
+            next_step = redirect("dashboard:index")
+        else:
+            messages.error(request, _("Cette invitation n'est plus valide."))
+            context["show_join_button"] = False
+
+    return next_step or render(request, template_name, context=context)
+
+
+@login_required
+def invite_siae_staff(request, template_name="invitations_views/create.html"):
+    siae = get_current_siae_or_404(request)
+    form_kwargs = {"sender": request.user, "siae": siae}
+    formset = NewSiaeStaffInvitationFormSet(data=request.POST or None, form_kwargs=form_kwargs)
     if request.POST:
         if formset.is_valid():
-            formset.save()
+            invitations = formset.save()
             count = len(formset.forms)
 
-            message = __("Invitation envoyée avec succès.", "Invitations envoyées avec succès.", count) % {
-                "count": count
-            }
+            message_singular = (
+                "Votre invitation a été envoyée par e-mail.<br>"
+                "Pour rejoindre votre organisation, l'invité(e) peut désormais cliquer "
+                "sur le lien de validation reçu dans le courriel.<br>"
+            )
+
+            message_plural = (
+                "Vos invitations ont été envoyées par e-mail.<br>"
+                "Pour rejoindre votre organisation, vos invités peuvent désormais "
+                "cliquer sur le lien de validation reçu dans l'e-mail.<br>"
+            )
+
+            message = __(message_singular, message_plural, count) % {"count": count}
+            expiration_date = formats.date_format(invitations[0].expiration_date)
+            message += _(f"Le lien de validation est valable jusqu'au {expiration_date}.")
+            message = safestring.mark_safe(message)
 
             messages.success(request, message)
-            formset = InvitationFormSet(form_kwargs=form_kwargs)
+            formset = NewSiaeStaffInvitationFormSet(form_kwargs=form_kwargs)
 
-    pending_invitations = request.user.invitations.filter(accepted=False)
-    context = {"formset": formset, "expiration_date": expiration_date, "pending_invitations": pending_invitations}
+    form_post_url = reverse("invitations_views:invite_siae_staff")
+    back_url = reverse("siaes_views:members")
+    context = {"back_url": back_url, "form_post_url": form_post_url, "formset": formset}
 
     return render(request, template_name, context)

gip-inclusion/les-emplois

itou/invitations/tests.py
@@ -1,7 +1,14 @@
 from django.test import TestCase
 from django.utils import timezone
 
-from itou.invitations.factories import ExpiredInvitationFactory, InvitationFactory, SentInvitationFactory
+from itou.invitations.factories import (
+    ExpiredInvitationFactory,
+    InvitationFactory,
+    SentInvitationFactory,
+    SiaeSentInvitationFactory,
+)
+from itou.invitations.models import Invitation, SiaeStaffInvitation
+from itou.users.factories import UserFactory
 
 
 class InvitationModelTest(TestCase):
@@ -38,6 +45,16 @@ class InvitationModelTest(TestCase):
         invitation.extend_expiration_date()
         self.assertFalse(invitation.has_expired)
 
+    def test_get_model_from_string(self):
+        invitation_type = Invitation.get_model_from_string("siae_staff")
+        self.assertEqual(invitation_type, SiaeStaffInvitation)
+
+        with self.assertRaises(TypeError):
+            Invitation.get_model_from_string("wrong_type")
+
+        with self.assertRaises(TypeError):
+            Invitation.get_model_from_string(12)
+
 
 class InvitationEmailsTest(TestCase):
     def test_send_invitation(self):
@@ -45,7 +62,7 @@ class InvitationEmailsTest(TestCase):
         email = invitation.email_invitation
 
         # Subject
-        self.assertIn(invitation.sender.first_name, email.subject)
+        self.assertIn(invitation.sender.first_name.title(), email.subject)
         self.assertIn(invitation.sender.last_name, email.subject)
 
         # Body
@@ -58,8 +75,48 @@ class InvitationEmailsTest(TestCase):
         # To
         self.assertIn(invitation.email, email.to)
 
+
+class TestSiaeInvitation(TestCase):
+    def test_add_member_to_siae(self):
+        invitation = SiaeSentInvitationFactory(email="hey@you.com")
+        user = UserFactory(email=invitation.email)
+        siae_members = invitation.siae.members.count()
+        invitation.add_invited_user_to_siae()
+        siae_members_after = invitation.siae.members.count()
+        self.assertEqual(siae_members + 1, siae_members_after)
+
+        user.refresh_from_db()
+
+
+class TestSiaeInvitationEmails(TestCase):
+    def test_accepted_notif_siae_members(self):
+        user = UserFactory()
+        invitation = SiaeSentInvitationFactory(email=user.email)
+        invitation.siae.members.add(user)
+        email = invitation.email_accepted_notif_siae_members
+
+        # Subject
+        self.assertIn(invitation.first_name, email.subject)
+        self.assertIn(invitation.last_name, email.subject)
+
+        # Body
+        self.assertIn(invitation.first_name, email.body)
+        self.assertIn(invitation.last_name, email.body)
+        self.assertIn(invitation.email, email.body)
+        self.assertIn(invitation.sender.first_name, email.body)
+        self.assertIn(invitation.sender.last_name, email.body)
+        self.assertIn(invitation.siae.display_name, email.body)
+
+        # To
+        members = invitation.siae.members.exclude(email__in=[invitation.sender.email, invitation.email])
+        for member in members:
+            self.assertIn(member.email, email.to)
+
+        self.assertNotIn(invitation.sender.email, email.to)
+        self.assertNotIn(invitation.email, email.to)
+
     def test_accepted_notif_sender(self):
-        invitation = SentInvitationFactory()
+        invitation = SiaeSentInvitationFactory()
         email = invitation.email_accepted_notif_sender
 
         # Subject
@@ -70,6 +127,23 @@ class InvitationEmailsTest(TestCase):
         self.assertIn(invitation.first_name, email.body)
         self.assertIn(invitation.last_name, email.body)
         self.assertIn(invitation.email, email.body)
+        self.assertIn(invitation.siae.display_name, email.body)
 
         # To
         self.assertIn(invitation.sender.email, email.to)
+
+    def test_email_invitation(self):
+        invitation = SiaeSentInvitationFactory()
+        email = invitation.email_invitation
+
+        # Subject
+        self.assertIn(invitation.siae.display_name, email.subject)
+
+        # Body
+        self.assertIn(invitation.first_name, email.body)
+        self.assertIn(invitation.last_name, email.body)
+        self.assertIn(invitation.acceptance_link, email.body)
+        self.assertIn(invitation.siae.display_name, email.body)
+
+        # To
+        self.assertIn(invitation.email, email.to)

itou/www/invitations_views/views.py
@@ -1,29 +1,38 @@
 from allauth.account.adapter import DefaultAccountAdapter
-from dateutil.relativedelta import relativedelta
+from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
-from django.http import Http404
-from django.shortcuts import redirect, render
-from django.utils import timezone
+from django.core.exceptions import PermissionDenied
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, redirect, render, reverse
+from django.utils import formats, safestring
 from django.utils.translation import gettext as _, ngettext as __
 
-from itou.invitations.models import Invitation
-from itou.www.invitations_views.forms import InvitationFormSet, NewUserForm
+from itou.invitations.models import Invitation, SiaeStaffInvitation
+from itou.utils.perms.siae import get_current_siae_or_404
+from itou.utils.urls import get_safe_url
+from itou.www.invitations_views.forms import NewSiaeStaffInvitationFormSet, NewUserForm
 
 
-def accept(request, invitation_id, template_name="invitations_views/accept.html"):
-    if request.user.is_authenticated:
-        messages.error(request, _("Merci de vous déconnecter avant d'accepter cette invitation."))
-        return redirect("account_logout")
+def new_user(request, invitation_type, invitation_id, template_name="invitations_views/new_user.html"):
 
-    try:
-        invitation = Invitation.objects.get(pk=invitation_id)
-    except Invitation.DoesNotExist:
-        raise Http404(_("Aucune invitation n'a été trouvée."))
-
+    invitation_type = Invitation.get_model_from_string(invitation_type)
+    invitation = get_object_or_404(invitation_type, pk=invitation_id)
     context = {"invitation": invitation}
     next_step = None
+
+    if request.user.is_authenticated:
+        if not request.user.email == invitation.email:
+            message = (
+                "Un utilisateur précédent est déjà connecté.<br>"
+                "Veuillez déconnecter ce compte en cliquant sur le bouton ci-dessous "
+                "(ne tenez pas compte de la page d'accueil qui se chargera automatiquement) "
+                "puis cliquez de nouveau sur le lien envoyé par e-mail pour accepter votre invitation."
+            )
+            message = safestring.mark_safe(message)
+            messages.error(request, _(message))
+            return redirect("account_logout")
 
     if invitation.can_be_accepted:
         user = get_user_model().objects.filter(email=invitation.email)
@@ -32,34 +41,67 @@ def accept(request, invitation_id, template_name="invitations_views/accept.html"
             context["form"] = form
             if form.is_valid():
                 user = form.save(request)
-                invitation.accept()
                 DefaultAccountAdapter().login(request, user)
-                next_step = redirect("dashboard:index")
+                next_step = redirect(get_safe_url(request, "redirect_to"))
         else:
-            messages.error(request, _("Vous comptez déjà parmi les membres de notre site."))
+            next_step = "{}?account_type={}&next={}".format(
+                reverse("account_login"), invitation.SIGNIN_ACCOUNT_TYPE, get_safe_url(request, "redirect_to")
+            )
+            next_step = redirect(next_step)
 
     return next_step or render(request, template_name, context=context)
 
 
 @login_required
-def create(request, template_name="invitations_views/create.html"):
-    form_kwargs = {"sender": request.user}
-    formset = InvitationFormSet(data=request.POST or None, form_kwargs=form_kwargs)
-    expiration_date = timezone.now() + relativedelta(days=Invitation.EXPIRATION_DAYS)
+def join_siae(request, invitation_id, template_name="invitations_views/join_siae.html"):
+    invitation = get_object_or_404(SiaeStaffInvitation, pk=invitation_id)
+    if not invitation.guest_can_join_siae(request):
+        raise PermissionDenied()
 
+    if invitation.can_be_accepted:
+        invitation.add_invited_user_to_siae()
+        invitation.accept()
+        messages.success(request, _(f"Vous êtes désormais membre de l'organisation {invitation.siae.display_name}."))
+    else:
+        messages.error(request, _("Cette invitation n'est plus valide."))
+
+    request.session[settings.ITOU_SESSION_CURRENT_SIAE_KEY] = invitation.siae.pk
+    return redirect("dashboard:index")
+
+
+@login_required
+def invite_siae_staff(request, template_name="invitations_views/create.html"):
+    siae = get_current_siae_or_404(request)
+    form_kwargs = {"sender": request.user, "siae": siae}
+    formset = NewSiaeStaffInvitationFormSet(data=request.POST or None, form_kwargs=form_kwargs)
     if request.POST:
         if formset.is_valid():
-            formset.save()
+            invitations = formset.save()
             count = len(formset.forms)
 
-            message = __("Invitation envoyée avec succès.", "Invitations envoyées avec succès.", count) % {
-                "count": count
-            }
+            message_singular = (
+                "Votre invitation a été envoyée par e-mail.<br>"
+                "Pour rejoindre votre organisation, l'invité(e) peut désormais cliquer "
+                "sur le lien de validation reçu dans le courriel.<br>"
+            )
+
+            message_plural = (
+                "Vos invitations ont été envoyées par e-mail.<br>"
+                "Pour rejoindre votre organisation, vos invités peuvent désormais "
+                "cliquer sur le lien de validation reçu dans l'e-mail.<br>"
+            )
+
+            message = __(message_singular, message_plural, count) % {"count": count}
+            expiration_date = formats.date_format(invitations[0].expiration_date)
+            message += _(f"Le lien de validation est valable jusqu'au {expiration_date}.")
+            message = safestring.mark_safe(message)
 
             messages.success(request, message)
-            formset = InvitationFormSet(form_kwargs=form_kwargs)
+            formset = NewSiaeStaffInvitationFormSet(form_kwargs=form_kwargs)
+            return redirect(request.path)
 
-    pending_invitations = request.user.invitations.filter(accepted=False)
-    context = {"formset": formset, "expiration_date": expiration_date, "pending_invitations": pending_invitations}
+    form_post_url = reverse("invitations_views:invite_siae_staff")
+    back_url = reverse("siaes_views:members")
+    context = {"back_url": back_url, "form_post_url": form_post_url, "formset": formset}
 
     return render(request, template_name, context)
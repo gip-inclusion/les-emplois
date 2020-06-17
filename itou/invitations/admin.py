4862b8136b29afef42b4d6f097767ea341c9df7a
Céline MS <celine@hello-birds.com>
2020-06-17 19:07:02 +0200

    An employer can send an invitation to join its structure.

--- itou/invitations/admin.py
+++ itou/invitations/admin.py
@@ -4,11 +4,10 @@ from django.utils.html import format_html
 from django.utils.safestring import mark_safe
 from django.utils.translation import gettext as _
 
-from .models import Invitation
+from .models import SiaeStaffInvitation
 
 
-@admin.register(Invitation)
-class InvitationAdmin(admin.ModelAdmin):
+class BaseInvitationAdmin(admin.ModelAdmin):
     date_hierarchy = "sent_at"
     list_display = ("first_name", "last_name", "sender_name", "sent_at")
 
@@ -28,6 +27,7 @@ class InvitationAdmin(admin.ModelAdmin):
                     "sent",
                     "accepted",
                     "sent_at",
+                    "accepted_at",
                 ]
             },
         )
@@ -51,3 +51,41 @@ class InvitationAdmin(admin.ModelAdmin):
         return value
 
     has_expired.short_description = _("Expirée")
+
+
+@admin.register(SiaeStaffInvitation)
+class SiaeStaffInvitationAdmin(BaseInvitationAdmin):
+    readonly_fields = BaseInvitationAdmin.readonly_fields + ("org_link",)
+    list_filter = BaseInvitationAdmin.list_filter + (("siae", admin.RelatedOnlyFieldListFilter),)
+    list_display = BaseInvitationAdmin.list_display + ("org_name",)
+
+    fieldsets = [
+        (
+            None,
+            {
+                "fields": [
+                    "first_name",
+                    "last_name",
+                    "email",
+                    "sender_link",
+                    "org_link",
+                    "has_expired",
+                    "sent",
+                    "sent_at",
+                    "accepted",
+                    "accepted_at",
+                ]
+            },
+        )
+    ]
+
+    def org_link(self, obj):
+        link = reverse("admin:siaes_siae_change", kwargs={"object_id": obj.siae.pk})
+        return format_html('<a href="{}">{}</a>', mark_safe(link), obj.siae.display_name)
+
+    org_link.short_description = _("Structure")
+
+    def org_name(self, obj):
+        return obj.siae.display_name
+
+    org_name.short_description = _("Structure")
--- itou/invitations/tests.py
+++ itou/invitations/tests.py
@@ -1,7 +1,14 @@
 from django.test import TestCase
 from django.utils import timezone
 
-from itou.invitations.factories import ExpiredInvitationFactory, InvitationFactory, SentInvitationFactory
+from itou.invitations.factories import (
+    ExpiredInvitationFactory,
+    InvitationFactory,
+    SentInvitationFactory,
+    SiaeSentInvitationFactory,
+)
+from itou.invitations.models import Invitation, SiaeStaffInvitation
+from itou.users.factories import UserFactory
 
 
 class InvitationModelTest(TestCase):
@@ -38,6 +45,16 @@ class InvitationModelTest(TestCase):
         invitation.extend_expiration_date()
         self.assertFalse(invitation.has_expired)
 
+    def test_get_model_from_string(self):
+        invitation_type = Invitation.get_model_from_string("siae_staff")
+        self.assertEqual(invitation_type, SiaeStaffInvitation)
+
+        with self.assertRaises(TypeError):
+            Invitation.get_model_from_string("wrong_type")
+
+        with self.assertRaises(TypeError):
+            Invitation.get_model_from_string(12)
+
 
 class InvitationEmailsTest(TestCase):
     def test_send_invitation(self):
@@ -45,7 +62,7 @@ class InvitationEmailsTest(TestCase):
         email = invitation.email_invitation
 
         # Subject
-        self.assertIn(invitation.sender.first_name, email.subject)
+        self.assertIn(invitation.sender.first_name.title(), email.subject)
         self.assertIn(invitation.sender.last_name, email.subject)
 
         # Body
@@ -58,8 +75,48 @@ class InvitationEmailsTest(TestCase):
         # To
         self.assertIn(invitation.email, email.to)
 
+
+class TestSiaeInvitation(TestCase):
+    def test_add_member_to_siae(self):
+        invitation = SiaeSentInvitationFactory(email="hey@you.com")
+        user = UserFactory(email=invitation.email)
+        siae_members = invitation.siae.members.count()
+        invitation.add_invited_user_to_siae()
+        siae_members_after = invitation.siae.members.count()
+        self.assertEqual(siae_members + 1, siae_members_after)
+
+        user.refresh_from_db()
+
+
+class TestSiaeInvitationEmails(TestCase):
+    def test_accepted_notif_siae_members(self):
+        user = UserFactory()
+        invitation = SiaeSentInvitationFactory(email=user.email)
+        invitation.siae.members.add(user)
+        email = invitation.email_accepted_notif_siae_members
+
+        # Subject
+        self.assertIn(invitation.first_name, email.subject)
+        self.assertIn(invitation.last_name, email.subject)
+
+        # Body
+        self.assertIn(invitation.first_name, email.body)
+        self.assertIn(invitation.last_name, email.body)
+        self.assertIn(invitation.email, email.body)
+        self.assertIn(invitation.sender.first_name, email.body)
+        self.assertIn(invitation.sender.last_name, email.body)
+        self.assertIn(invitation.siae.display_name, email.body)
+
+        # To
+        members = invitation.siae.members.exclude(email__in=[invitation.sender.email, invitation.email])
+        for member in members:
+            self.assertIn(member.email, email.to)
+
+        self.assertNotIn(invitation.sender.email, email.to)
+        self.assertNotIn(invitation.email, email.to)
+
     def test_accepted_notif_sender(self):
-        invitation = SentInvitationFactory()
+        invitation = SiaeSentInvitationFactory()
         email = invitation.email_accepted_notif_sender
 
         # Subject
@@ -70,6 +127,23 @@ class InvitationEmailsTest(TestCase):
         self.assertIn(invitation.first_name, email.body)
         self.assertIn(invitation.last_name, email.body)
         self.assertIn(invitation.email, email.body)
+        self.assertIn(invitation.siae.display_name, email.body)
 
         # To
         self.assertIn(invitation.sender.email, email.to)
+
+    def test_email_invitation(self):
+        invitation = SiaeSentInvitationFactory()
+        email = invitation.email_invitation
+
+        # Subject
+        self.assertIn(invitation.siae.display_name, email.subject)
+
+        # Body
+        self.assertIn(invitation.first_name, email.body)
+        self.assertIn(invitation.last_name, email.body)
+        self.assertIn(invitation.acceptance_link, email.body)
+        self.assertIn(invitation.siae.display_name, email.body)
+
+        # To
+        self.assertIn(invitation.email, email.to)
