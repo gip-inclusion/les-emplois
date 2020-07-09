987ccb656326be84a12acfd1585591af9eca4a60
kemar <marc@marcarea.com>
2020-07-09 14:55:17 +0200

    Make search results more visible

--- itou/www/search/urls.py
+++ itou/www/search/urls.py
@@ -7,6 +7,8 @@ from itou.www.search import views
 app_name = "search"
 
 urlpatterns = [
-    path("employers", views.search_siaes, name="siaes"),
-    path("prescribers", views.search_prescribers, name="prescribers"),
+    path("employers", views.search_siaes_home, name="siaes_home"),
+    path("employers/results", views.search_siaes_results, name="siaes_results"),
+    path("prescribers", views.search_prescribers_home, name="prescribers_home"),
+    path("prescribers/results", views.search_prescribers_results, name="prescribers_results"),
 ]
