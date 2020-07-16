a3624ff4e7496e09a65725b17750b7598ffdaca7
Frédéric Vergez <fred@ikarius.com>
2020-07-16 17:36:29 +0200

    Updated model, added import metadata

--- itou/pe_data/models.py
+++ itou/pe_data/models.py
@@ -1,18 +1,27 @@
-from django.db import models
-from django.utils.translation import gettext_lazy as _
 from django.conf import settings
+from django.db import models
+from django.utils.timezone import now
+from django.utils.translation import gettext_lazy as _
+
 
 class ExtraUserData(models.Model):
 
-    created_at = models.DateTimeField(verbose_name=_("Date de création de l'import"))
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, 
-                             verbose_name=_("Candidat / Utilisateur API PE"), 
-                             on_delete=models.CASCADE,
-                             related_name="extrauserdata")
+    created_at = models.DateTimeField(verbose_name=_("Date de création de l'import"), default=now)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        verbose_name=_("Candidat / Utilisateur API PE"),
+        on_delete=models.CASCADE,
+        related_name="extrauserdata",
+    )
 
-    STATUS_CHOICES = (("OK", _("Import de données réalisé sans erreur")),
-                      ("PARTIAL", _("Import de données réalisé partiellement")),
-                      ("FAILED", _("Import de données en erreur")))
+    STATUS_OK = "OK"
+    STATUS_PARTIAL = "PARTIAL"
+    STATUS_FAILED = "FAILED"
+    STATUS_CHOICES = (
+        (STATUS_OK, _("Import de données réalisé sans erreur")),
+        (STATUS_PARTIAL, _("Import de données réalisé partiellement")),
+        (STATUS_FAILED, _("Import de données en erreur")),
+    )
 
     status = models.CharField(max_length=10, choices=STATUS_CHOICES)
 
