329499084d2bf36c13e4107916302aebbf9734d3
Céline MS <celine@hello-birds.com>
2020-08-06 16:22:47 +0200

    Replace Demo accounts by Test accounts

--- itou/static/js/test_accounts.js
+++ itou/static/js/test_accounts.js
@@ -8,17 +8,17 @@ function supports_local_storage() {
 
 $(document).ready(function(){
     if (supports_local_storage()) {
-        infoKey = localStorage.getItem("demoAccountsModal");
+        infoKey = localStorage.getItem("testAccountsModal");
         if (!infoKey) {
-            localStorage.setItem("demoAccountsModal", true);
-            $('#demoAccountsModal').modal();
+            localStorage.setItem("testAccountsModal", true);
+            $('#testAccountsModal').modal();
         }
     }
 
     $('.postLogin').on('click', function(){
         const account_type = $(this).data('type');
         const email = $(this).data('email');
-        const form = $("#demoForm");
+        const form = $("#testAccountsForm");
         form.find('input[type=email]').val(email);
         form.find('input[type=password]').val("password");
         form.find('input[name=account_type]').val(account_type);
