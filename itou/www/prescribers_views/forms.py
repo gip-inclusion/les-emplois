09f2fcadf38b1cd25f1745dfc0e7918147df5637
kemar <marc@marcarea.com>
2020-08-20 12:02:39 +0200

    Drop the ability to create a prescriber's organization a posteriori

--- itou/www/prescribers_views/forms.py
+++ itou/www/prescribers_views/forms.py
@@ -1,47 +1,7 @@
 from django import forms
 from django.utils.translation import gettext_lazy
 
-from itou.prescribers.models import PrescriberMembership, PrescriberOrganization
-from itou.utils.apis.siret import get_siret_data
-
-
-class CreatePrescriberOrganizationForm(forms.ModelForm):
-    """
-    Create a prescriber organization.
-    """
-
-    class Meta:
-        model = PrescriberOrganization
-        fields = ["name", "siret", "phone", "email", "website", "description"]
-        help_texts = {
-            "siret": gettext_lazy("Le numéro SIRET doit être composé de 14 chiffres."),
-            "phone": gettext_lazy("Par exemple 0610203040"),
-            "description": gettext_lazy("Texte de présentation de votre organisation."),
-            "website": gettext_lazy("Votre site web doit commencer par http:// ou https://"),
-        }
-
-    def save(self, user, commit=True):
-        organization = super().save(commit=False)
-
-        organization.created_by = user
-        organization.authorization_status = PrescriberOrganization.AuthorizationStatus.NOT_REQUIRED
-
-        siret = self.cleaned_data["siret"]
-        if siret:
-            siret_data = get_siret_data(self.cleaned_data["siret"])
-            # Try to automatically gather information for the given SIRET.
-            if siret_data:
-                organization.set_coords_and_address(siret_data["address"], post_code=siret_data["post_code"])
-                organization.siret = siret
-
-        if commit:
-            organization.save()
-            membership = PrescriberMembership()
-            membership.user = user
-            membership.organization = organization
-            membership.is_admin = True
-            membership.save()
-        return organization
+from itou.prescribers.models import PrescriberOrganization
 
 
 class EditPrescriberOrganizationForm(forms.ModelForm):
--- itou/www/prescribers_views/tests.py
+++ itou/www/prescribers_views/tests.py
@@ -1,13 +1,9 @@
-from unittest import mock
-
 from django.test import TestCase
 from django.urls import reverse
 
 from itou.prescribers.factories import PrescriberOrganizationFactory, PrescriberOrganizationWithMembershipFactory
 from itou.prescribers.models import PrescriberOrganization
-from itou.users.factories import DEFAULT_PASSWORD, PrescriberFactory
-from itou.utils.mocks.geocoding import BAN_GEOCODING_API_RESULT_MOCK
-from itou.utils.mocks.siret import API_INSEE_SIRET_RESULT_MOCK
+from itou.users.factories import DEFAULT_PASSWORD
 
 
 class CardViewTest(TestCase):
@@ -17,44 +13,6 @@ class CardViewTest(TestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.context["prescriber_org"], prescriber_org)
-
-
-class CreateOrganizationTest(TestCase):
-    @mock.patch("itou.utils.apis.siret.call_insee_api", return_value=API_INSEE_SIRET_RESULT_MOCK)
-    @mock.patch("itou.utils.apis.geocoding.call_ban_geocoding_api", return_value=BAN_GEOCODING_API_RESULT_MOCK)
-    def test_create(self, mock_call_ban_geocoding_api, mock_call_insee_api):
-        """Create a prescriber organization."""
-
-        user = PrescriberFactory()
-        self.assertTrue(self.client.login(username=user.email, password=DEFAULT_PASSWORD))
-
-        url = reverse("prescribers_views:create_organization")
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-
-        post_data = {
-            "siret": "12000015300011",
-            "phone": "",
-            "email": "",
-            "website": "",
-            "description": "",
-            "name": "foo",
-        }
-        response = self.client.post(url, data=post_data)
-        mock_call_insee_api.assert_called_once_with(post_data["siret"])
-        mock_call_ban_geocoding_api.assert_called_once()
-        self.assertEqual(response.status_code, 302)
-
-        organization = PrescriberOrganization.objects.get(siret=post_data["siret"])
-        self.assertEqual(user, organization.created_by)
-        self.assertIn(user, organization.members.all())
-        self.assertEqual(1, organization.members.count())
-
-        self.assertIn(organization, user.prescriberorganization_set.all())
-        self.assertEqual(1, user.prescriberorganization_set.count())
-
-        membership = user.prescribermembership_set.get(organization=organization)
-        self.assertTrue(membership.is_admin)
 
 
 class EditOrganizationTest(TestCase):
