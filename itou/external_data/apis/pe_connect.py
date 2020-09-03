7bfa2fdb62c0982b35b5bd7e6b55b529060fbbb1
Frédéric Vergez <fred@ikarius.com>
2020-09-03 18:52:39 +0200

    Added simpler parameters (serialization)

--- itou/external_data/apis/pe_connect.py
+++ itou/external_data/apis/pe_connect.py
@@ -7,6 +7,7 @@ from django.forms.models import model_to_dict
 from django.utils.dateparse import parse_datetime
 
 from itou.external_data.models import ExternalDataImport, JobSeekerExternalData
+from itou.users.models import User
 
 
 # PE Connect API data retrieval tools
@@ -252,78 +253,15 @@ def _store_user_data(user, status, data):
 #  Public
 
 
-def async_import_user_data(user, token):
-    """
-    Asynchronous update of user data:
-
-    This part of the app has to be processed asynchronously:
-        - many (blocking) HTTP requests are made sequentially in order to fetch data from PE,
-          rising up the total execution time to more than 5s in some cases,
-        - it is triggered from a synchronous signal receiver (see `signals.py`),
-        - it's mostly "fire & forget" and able to log any issue in its own context.
-
-    "What happens in a loop stays in the loop":
-
-    This part is designed to be non-blocking in case of failure:
-        - exceptions thrown in a distinct loop are confined into this loop,
-        - every status update is logged in DB (OK, FAILED, PARTIAL),
-        - data retrieved are not considered vital and can be pulled after a new user login.
-
-    User data updates:
-
-    Refresh strategies can be implemented using data import status and import datetime (`ExternalDataImport` object).
-    The actual data refresh is triggered at each user login and repeated until import status is OK.
-
-    Using loops and executors:
-
-    An event loop is an isolated computation context, where you can use processes or threads (via executors)
-    to do actual tasks, sequentially.
-
-    Processes are suited for long IO computations (mainly for files) without inter-process communications.
-    Threads are better for CPU based or short-lived IO tasks, and they can share context (but don't do that).
-
-    An executor is:
-        - what "does" the real async work within the context of the event loop,
-        - created with every new event loop, with default values, but can be customized / overriden,
-        - either using processes or threads (threads by default).
-
-    See: https://docs.python.org/3/library/concurrent.futures.html#concurrent.futures.Executor
-
-    Using loops and executor in this case, is the simplest option:
-        - we don't expect a return value (even if provided),
-        - using default executor (based on threads), to process async data import,
-        - ORM calls are all made in the same thread (single task).
-
-    When a return value exists and has to be processed, using Python `async` and `futures` is the way to go.
-    See: https://docs.python.org/3/library/asyncio-eventloop.html#creating-futures-and-tasks
-
-    ---
-
-    PROS:
-        - context isolation: can fail, we don't care
-        - effective and simple async processing
-        - non-blocking
-
-    CONS:
-        - (very) limited scalability: use scarcely (threads are not magic, there are some physical limitations),
-        - everything using threads / loops should be very cautious when using Django ORM
-          (all parts of a DB task have to be done in the same thread).
-
-    TIPS:
-        - most of the time, it's better to code sync functions first and wrap them with an async behavior,
-        - this can however be tricky (when dealing with ORM/DB, UI threads...)
-        - Django, since version 3.1, offers methods to round edges for sync/async concerns (not used here).
-    """
-    loop = asyncio.new_event_loop()
-    loop.run_in_executor(None, import_user_data, user, token)
-
-
-def import_user_data(user, token):
+def import_user_data(user_pk, token):
     """
     Import external user data via PE Connect
     Returns a valid ExternalDataImport object when result is PARTIAL or OK.
+
     This function is *synchronous*
+
     """
+    user = User.objects.get(pk=user_pk)
     data_import, _ = ExternalDataImport.objects.get_or_create(
         source=ExternalDataImport.DATA_SOURCE_PE_CONNECT, user=user
     )
