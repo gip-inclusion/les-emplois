6c0ebfcd223de0784aabecbf1b77017b982f0c63
Frédéric Vergez <fred@ikarius.com>
2020-09-21 15:46:16 +0200

    PR feedback 1/2

--- itou/utils/urls.py
+++ itou/utils/urls.py
@@ -1,3 +1,6 @@
+import time
+
+import jwt
 from django.conf import settings
 from django.utils.http import url_has_allowed_host_and_scheme
 
@@ -57,3 +60,17 @@ class SiretConverter:
 
     def to_url(self, value):
         return f"{value}"
+
+
+# Metabase private / signed URLs
+# See:
+# * https://www.metabase.com/docs/latest/enterprise-guide/full-app-embedding.html
+# * https://github.com/jpadilla/pyjwt
+# * https://github.com/metabase/embedding-reference-apps/blob/master/django/embedded_analytics/user_stats/views.py
+def _get_token(payload):
+    return jwt.encode(payload, settings.METABASE_SECRET_KEY, algorithm="HS256").decode("utf8")
+
+
+def metabase_embedded_url(dashboard_id):
+    payload = {"resource": {"dashboard": dashboard_id}, "params": {}, "exp": round(time.time()) + (60 * 10)}
+    return settings.METABASE_SITE_URL + "/embed/dashboard/" + _get_token(payload)
--- itou/www/stats/views.py
+++ itou/www/stats/views.py
@@ -1,60 +1,57 @@
-import time
-
-import jwt
 from django.conf import settings
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.core.exceptions import PermissionDenied
+from django.shortcuts import render
+
+from itou.utils.urls import metabase_embedded_url
+
 
 # Embedding Metabase dashboards:
 # Metabase dashboards can be included securely in the app via a signed URL
 # See an embedding sample at:
 # https://github.com/metabase/embedding-reference-apps/blob/master/django/embedded_analytics/user_stats/views.py
 
-
 # Each signed dashboard has the same look (at the moment)
 _STATS_HTML_TEMPLATE = "stats/stats.html"
 
+# Metabase dashboard IDs
+PUBLIC_STATS_DASHBOARD_ID = 34
+ADVANCED_STATS_DASHBOARD_ID = 43
+DIRECCTE_STATS_DASHBOARD_ID = 36
 
-def _get_token(payload):
-    return jwt.encode(payload, settings.METABASE_SECRET_KEY, algorithm="HS256").decode("utf8")
 
-
-def _signed_dashboard_embedded_url(dashboard_number):
-    payload = {"resource": {"dashboard": dashboard_number}, "params": {}, "exp": round(time.time()) + (60 * 10)}
-    return settings.METABASE_SITE_URL + "/embed/dashboard/" + _get_token(payload)
+def can_view_stats(user):
+    return user.is_reporting
 
 
 @login_required
 def public_stats(request, template_name=_STATS_HTML_TEMPLATE):
     """
-    Public stats are now signed and embedded (no more public links)
+    Public stats (signed and embedded version)
+    Public link:
+    https://stats.inclusion.beta.gouv.fr/public/dashboard/f1527a13-1508-498d-8014-b2fe487a3a70
     """
-    context = {"iframeurl": _signed_dashboard_embedded_url(34)}
+    context = {"iframeurl": metabase_embedded_url(PUBLIC_STATS_DASHBOARD_ID)}
     return render(request, template_name, context)
 
 
 @login_required
 def advanced_stats(request, template_name=_STATS_HTML_TEMPLATE):
     """
-    "Advanced" stats reporting data (signed)
-
+    "Advanced" stats reporting data (signed and embedded version)
+    Public link:
+    https://stats.inclusion.beta.gouv.fr/public/dashboard/c65faf79-3b89-4416-9faa-ff5182f41468
     """
-    if not request.user.is_reporting:
-        raise PermissionDenied
-
-    context = {"iframeurl": _signed_dashboard_embedded_url(43)}
+    context = {"iframeurl": metabase_embedded_url(ADVANCED_STATS_DASHBOARD_ID)}
     return render(request, template_name, context)
 
 
 @login_required
+@user_passes_test(can_view_stats, login_url="/dashboard")
 def reporting(request, template_name=_STATS_HTML_TEMPLATE):
     """
     If the user has the 'is_reporting' flag, this Metabase dashboard link
     is displayed on the dashboard page
     """
-    if not request.user.is_reporting:
-        raise PermissionDenied
-
-    context = {"iframeurl": _signed_dashboard_embedded_url(36)}
+    context = {"iframeurl": metabase_embedded_url(DIRECCTE_STATS_DASHBOARD_ID)}
     return render(request, template_name, context)
