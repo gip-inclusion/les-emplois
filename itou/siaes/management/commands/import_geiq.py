24a5ce9f5b24e8db28d405ebb04b68fc8a002952
Vermeer GRANGE <vermeer.grange@gmail.com>
2020-10-16 15:30:48 +0300

    Import second GEIQ export

--- itou/siaes/management/commands/import_geiq.py
+++ itou/siaes/management/commands/import_geiq.py
@@ -1,25 +1,101 @@
-import csv
 import logging
 import os
 
+import numpy as np
+import pandas as pd
 from django.core.management.base import BaseCommand
 
+from itou.siaes.management.commands._import_siae.siae import could_siae_be_deleted, geocode_siae
+from itou.siaes.management.commands._import_siae.utils import remap_columns, timeit
 from itou.siaes.models import Siae
-from itou.utils.address.departments import DEPARTMENTS, department_from_postcode
-from itou.utils.apis.geocoding import get_geocoding_data
+from itou.utils.address.departments import DEPARTMENTS_OPEN_FOR_NON_ETTI_SIAES, department_from_postcode
+from itou.utils.validators import validate_siret
 
 
 CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 
-CSV_FILE = f"{CURRENT_DIR}/data/2019_11_21_export_bbd_geiq.csv"
+GEIQ_DATASET_FILENAME = f"{CURRENT_DIR}/data/Geiq_-_liste_02-10-2020.xls"
 
-KINDS = dict(Siae.KIND_CHOICES).keys()
 
-# Below this score, results from `adresse.data.gouv.fr` are considered unreliable.
-# This score is arbitrarily set based on general observation.
-API_BAN_RELIABLE_MIN_SCORE = 0.6
+def clean_string(s):
+    """
+    Drop trailing whitespace and merge consecutive spaces.
+    """
+    if s is None:
+        return None
+    s = s.strip()
+    return " ".join(s.split())
 
-SEEN_SIRET = set()
+
+@timeit
+def get_geiq_df(filename=GEIQ_DATASET_FILENAME):
+    df = pd.read_excel(filename, converters={"siret": str, "zip": str})
+
+    column_mapping = {
+        "Geiq": "name",
+        "street": "address_line_1",
+        "street2": "address_line_2",
+        "zip": "post_code",
+        "city": "city",
+        "siret": "siret",
+        "email": "auth_email",
+    }
+    df = remap_columns(df, column_mapping=column_mapping)
+
+    # Replace NaN elements with None.
+    df = df.replace({np.nan: None})
+
+    # Clean string fields.
+    df["name"] = df.name.apply(clean_string)
+    df["address_line_1"] = df.address_line_1.apply(clean_string)
+    df["address_line_2"] = df.address_line_2.apply(clean_string)
+    df["post_code"] = df.post_code.apply(clean_string)
+    df["city"] = df.city.apply(clean_string)
+    df["siret"] = df.siret.apply(clean_string)
+    df["auth_email"] = df.auth_email.apply(clean_string)
+
+    # "GEIQ PROVENCE" becomes "Geiq Provence".
+    df["name"] = df.name.apply(str.title)
+
+    df["department"] = df.post_code.apply(department_from_postcode)
+
+    # Drop rows without siret.
+    df = df[~df.siret.isnull()]
+
+    # Drop rows without auth_email.
+    df = df[~df.auth_email.isnull()]
+
+    # Only keep GEIQ in opened departments.
+    df = df[df.department.isin(DEPARTMENTS_OPEN_FOR_NON_ETTI_SIAES)]
+
+    for _, row in df.iterrows():
+        validate_siret(row.siret)
+
+    assert df.siret.is_unique
+
+    return df
+
+
+def build_siae(row):
+    siae = Siae()
+    siae.siret = row.siret
+    siae.kind = Siae.KIND_GEIQ
+    siae.source = Siae.SOURCE_GEIQ
+    siae.name = row["name"]  # row.name returns row index.
+    assert not siae.name.isnumeric()
+    siae.email = ""  # Do not make the authentification email public!
+    siae.auth_email = row.auth_email
+    siae.address_line_1 = row.address_line_1
+    siae.address_line_2 = ""
+    if row.address_line_2:
+        siae.address_line_2 = row.address_line_2
+    siae.post_code = row.post_code
+    siae.city = row.city
+    siae.department = row.department
+
+    siae = geocode_siae(siae)
+
+    return siae
 
 
 class Command(BaseCommand):
@@ -56,130 +132,43 @@ class Command(BaseCommand):
         if verbosity > 1:
             self.logger.setLevel(logging.DEBUG)
 
+    def log(self, message):
+        self.stdout.write(message)
+
+    @timeit
     def handle(self, dry_run=False, **options):
 
         self.set_logger(options.get("verbosity"))
 
-        with open(CSV_FILE) as csvfile:
+        geiq_df = get_geiq_df()
+        self.log(f"Loaded {len(geiq_df)} GEIQ from export.")
 
-            reader = csv.reader(csvfile, delimiter=";")
+        db_sirets = set([siae.siret for siae in Siae.objects.filter(kind=Siae.KIND_GEIQ)])
+        df_sirets = set(geiq_df.siret.tolist())
 
-            for i, row in enumerate(reader):
+        # Create GEIQ which do not exist in database yet.
+        creatable_sirets = df_sirets - db_sirets
+        self.log(f"{len(creatable_sirets)} GEIQ will be created.")
+        siret_to_row = {row.siret: row for _, row in geiq_df.iterrows()}
+        for siret in creatable_sirets:
+            row = siret_to_row[siret]
+            siae = build_siae(row)
+            if not dry_run:
+                siae.save()
+                self.log(f"siae.id={siae.id} has been created.")
 
-                if i == 0:
-                    # Skip CSV header.
-                    continue
-
-                self.logger.debug("-" * 80)
-
-                name = row[0].strip()
-                name = " ".join(name.split())  # Replace multiple spaces by a single space.
-                self.logger.debug(name)
-
-                address_line_1 = row[1].strip()
-                address_line_1 = " ".join(address_line_1.split())
-                self.logger.debug(address_line_1)
-
-                address_line_2 = row[2].strip()
-                address_line_2 = " ".join(address_line_2.split())
-                self.logger.debug(address_line_2)
-
-                city = row[3].strip()
-                city = " ".join(city.split())
-                self.logger.debug(city)
-
-                post_code = row[4].strip()
-                post_code = " ".join(post_code.split())
-                self.logger.debug(post_code)
-
-                email = row[5].strip()
-                self.logger.debug(email)
-                assert " " not in email
-
-                phone = row[6].strip().replace(" ", "")
-                assert len(phone) == 10
-                self.logger.debug(phone)
-
-                naf = row[7]
-                self.logger.debug(naf)
-                if naf:
-                    assert len(naf) == 5
-
-                siret = row[8].strip()
-                self.logger.debug(siret)
-                assert len(siret) == 14
-
-                if siret in SEEN_SIRET:
-                    self.stderr.write("Siret already seen. Skipping.")
-                    continue
-                SEEN_SIRET.add(siret)
-
-                department = department_from_postcode(post_code)
-
-                self.logger.debug(department)
-                assert department in DEPARTMENTS
-
-                siae_info = f"{siret} {name} - {address_line_1} - {post_code} {city}."
-
-                self.logger.debug(siae_info)
-
+        # Delete GEIQ which no longer exist in the latest export.
+        deletable_sirets = db_sirets - df_sirets
+        self.log(f"{len(deletable_sirets)} GEIQ will be deleted.")
+        for siret in deletable_sirets:
+            siae = Siae.objects.get(siret=siret, kind=Siae.KIND_GEIQ)
+            if could_siae_be_deleted(siae):
+                self.log(f"siae.id={siae.id} will be deleted.")
                 if not dry_run:
+                    siae.delete()
+            else:
+                # As of 2020/10/16 five GEIQ are undeletable.
+                self.log(f"siae.id={siae.id} cannot be deleted as it has data.")
 
-                    siae = Siae()
-                    siae.siret = siret
-                    siae.naf = naf
-                    siae.kind = Siae.KIND_GEIQ
-                    siae.source = Siae.SOURCE_GEIQ
-                    siae.name = name
-                    siae.phone = phone
-                    siae.email = email
-                    siae.address_line_1 = address_line_1
-                    siae.address_line_2 = address_line_2
-                    siae.post_code = post_code
-                    siae.city = city
-                    siae.department = department
-
-                    if siae.address_on_one_line:
-
-                        geocoding_data = get_geocoding_data(siae.address_on_one_line, post_code=siae.post_code)
-
-                        if not geocoding_data or geocoding_data["score"] < API_BAN_RELIABLE_MIN_SCORE:
-                            geocoding_data = get_geocoding_data(
-                                siae.address_on_one_line, post_code=f"{siae.post_code[:2]}000"
-                            )
-
-                        if not geocoding_data or geocoding_data["score"] < API_BAN_RELIABLE_MIN_SCORE:
-                            geocoding_data = get_geocoding_data(siae.address_on_one_line)
-
-                        if not geocoding_data or geocoding_data["score"] < API_BAN_RELIABLE_MIN_SCORE:
-                            geocoding_data = get_geocoding_data(siae.address_line_1)
-
-                        if not geocoding_data or geocoding_data["score"] < API_BAN_RELIABLE_MIN_SCORE:
-                            geocoding_data = get_geocoding_data(siae.address_line_2)
-
-                        if not geocoding_data:
-                            self.stderr.write(f"No geocoding data found for {siae_info}")
-                            siae.save()
-                            continue
-
-                        siae.geocoding_score = geocoding_data["score"]
-                        # If the score is greater than API_BAN_RELIABLE_MIN_SCORE, coords are reliable:
-                        # use data returned by the BAN API because it's better written using accents etc.
-                        # while the source data is in all caps etc.
-                        # Otherwise keep the old address (which is probably wrong or incomplete).
-                        if siae.geocoding_score >= API_BAN_RELIABLE_MIN_SCORE:
-                            siae.address_line_1 = geocoding_data["address_line_1"]
-                            siae.city = geocoding_data["city"]
-                        else:
-                            self.stderr.write(f"Geocoding not reliable for {siae_info}\n{siae.address_on_one_line}")
-
-                        self.logger.debug("-" * 40)
-                        self.logger.debug(siae.address_line_1)
-                        self.logger.debug(siae.city)
-
-                        siae.coords = geocoding_data["coords"]
-
-                    siae.save()
-
-        self.stdout.write("-" * 80)
-        self.stdout.write("Done.")
+        self.log("-" * 80)
+        self.log("Done.")
