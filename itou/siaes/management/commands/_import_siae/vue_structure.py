0575212ee213564d38f5c9c0e3af68eecff050e0
Vermeer GRANGE <vermeer.grange@gmail.com>
2020-11-06 12:37:56 +0200

    Use latest ASP data

--- itou/siaes/management/commands/_import_siae/vue_structure.py
+++ itou/siaes/management/commands/_import_siae/vue_structure.py
@@ -22,7 +22,7 @@ from itou.utils.validators import validate_naf, validate_siret
 
 CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 
-VUE_STRUCTURE_FILENAME = f"{CURRENT_DIR}/../data/fluxIAE_Structure_12102020_074951.csv"
+VUE_STRUCTURE_FILENAME = f"{CURRENT_DIR}/../data/fluxIAE_Structure_02112020_075153.csv"
 
 
 @timeit
