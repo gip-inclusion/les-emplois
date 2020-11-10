350bb98e94d21068c237a41e553b3ef57b98fba1
Céline MS <celine@hello-birds.com>
2020-11-10 12:42:44 +0100

    Clean master notifications file.

--- itou/utils/notifications.py
+++ itou/utils/notifications.py
@@ -1,6 +1,5 @@
 from django.db.models import Q
-
-from itou.utils.emails import get_email_message
+from django.utils.module_loading import autodiscover_modules
 
 
 class NotificationBase:
@@ -12,7 +11,7 @@ class NotificationBase:
     def subscribed_lookup(self):
         """
         Return a Q object to be used in a queryset to get only subscribed members.
-        For example: 
+        For example:
           Cls.objects.filter(self.subscribed_lookup)
         """
         filters = {f"notifications__{self.class_name}__subscribed": True}
@@ -40,38 +39,51 @@ class NotificationBase:
         raise NotImplementedError
 
 
-##############################################################
-######################## Load notifications ##################
-##############################################################
+#############################################################
+################# DB DEFAULT JSON STRUCTURE #################
+#############################################################
 
-######################## PROPOSAL 1 ##########################
-
-from django.contrib.admin.sites import site
-from django.utils.module_loading import autodiscover_modules
-
+#####
+##### 1/ Magically import any notification.
+#####
 
 # Find and load modules called "notifications" in Django apps.
 autodiscover_modules("notifications")
-
+# Then get a list of configured notifications throughout the project.
 ALL_NOTIFICATIONS = NotificationBase.__subclasses__()
 
 
-def notifications_default():
-    """
-    Provide a default value to fill in the "notifications" field.
-    """
-    return {notification_class.__name__: {"subscribed": True} for notification_class in ALL_NOTIFICATIONS}
+#####
+##### 2/ Manually import class notifications and add
+#####    them to the correct Database table field.
+#####
 
-
-####################### PROPOSAL 2 ############################
-# I think it's the best one because it preserves our users repartition
-# (job seekers, prescribers and siae staff) as it lets us "assign" notifications
-# to the right user kind.
-
-from itou.job_applications.notifications import NewJobApplicationSiaeEmailNotification
+# Import classes after NotificationBase to avoid an import loop error.
+from itou.job_applications.notifications import NewJobApplicationSiaeEmailNotification  # noqa: ignore=E402
 
 
 SIAE_NOTIFICATIONS = [NewJobApplicationSiaeEmailNotification]
+PRESCRIBER_NOTIFICATIONS = []
+USER_NOTIFICATIONS = []
+
+
+#####
+##### 3/ Assert any notification existing in
+#####    the project is referenced here.
+#####
+
+IMPORTED_NOTIFICATIONS = SIAE_NOTIFICATIONS + PRESCRIBER_NOTIFICATIONS + USER_NOTIFICATIONS
+assert len(ALL_NOTIFICATIONS) == len(IMPORTED_NOTIFICATIONS)
+assert IMPORTED_NOTIFICATIONS.sort() == ALL_NOTIFICATIONS.sort()
+
+
+#####
+##### 4/ Declare methods called on migrations.
+#####
+
+
+def default_notifications_dict(notifications_list):
+    return {notification_class.__name__: {"subscribed": True} for notification_class in notifications_list}
 
 
 def siaemembership_notifications_default():
@@ -79,4 +91,20 @@ def siaemembership_notifications_default():
     Provide a default value to fill in the "notifications" field
     when a new SiaeMembership is created.
     """
-    return {notification_class.__name__: {"subscribed": True} for notification_class in SIAE_NOTIFICATIONS}
+    return default_notifications_dict(SIAE_NOTIFICATIONS)
+
+
+def prescribermembership_notifications_default():
+    """
+    Provide a default value to fill in the "notifications" field
+    when a new PrescriberMembership is created.
+    """
+    return default_notifications_dict(PRESCRIBER_NOTIFICATIONS)
+
+
+def user_notifications_default():
+    """
+    Provide a default value to fill in the "notifications" field
+    when a new User is created.
+    """
+    return default_notifications_dict(USER_NOTIFICATIONS)
