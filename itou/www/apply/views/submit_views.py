7729205d4ee7bfe27c30563f1b06698653223f51
Céline MS <celine@hello-birds.com>
2020-11-12 17:13:09 +0100

    Prevent PE prescribers from sharing a private resume.

--- itou/www/apply/views/submit_views.py
+++ itou/www/apply/views/submit_views.py
@@ -206,13 +206,13 @@ def step_send_resume(request, siae_pk, template_name="apply/submit_step_send_res
     form = ResumeFormMixin(data=request.POST or None)
 
     # Uploading a resume via Typeform without filling in the resume_link field
-    # will submit an empty form, which is an expected behavior.
+    # will submit an empty form which is an expected behavior.
     if request.method == "POST":
-        if form.is_valid():
+        if form.is_valid() or not form.has_error("resume_link"):
             job_seeker.resume_link = form.cleaned_data.get("resume_link")
             job_seeker.save()
-        next_url = reverse("apply:step_eligibility", kwargs={"siae_pk": siae.pk})
-        return HttpResponseRedirect(next_url)
+            next_url = reverse("apply:step_eligibility", kwargs={"siae_pk": siae.pk})
+            return HttpResponseRedirect(next_url)
 
     context = {"siae": siae, "job_seeker_signed_pk": job_seeker_signed_pk, "form": form}
     return render(request, template_name, context)
