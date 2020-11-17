ddffdb635ea88e2d0cb36c8397cc8e3946fb8718
Frédéric Vergez <fred@ikarius.com>
2020-11-17 15:46:17 +0100

    Added test for regression on prescriber org update

--- itou/www/prescribers_views/tests.py
+++ itou/www/prescribers_views/tests.py
@@ -67,6 +67,46 @@ class EditOrganizationTest(TestCase):
         self.assertEqual(organization.longitude, 2.316754)
         self.assertEqual(organization.geocoding_score, 0.587663373207207)
 
+    @mock.patch("itou.utils.apis.geocoding.call_ban_geocoding_api", return_value=BAN_GEOCODING_API_RESULT_MOCK)
+    def test_edit_with_multiple_memberships_and_same_siret(self, mock_call_ban_geocoding_api):
+        """
+        Updating information of the prescriber organization must be possible
+        when user is member of multiple orgs with the same SIRET (and different types)
+        (was a regression)
+        """
+        organization = PrescriberOrganizationWithMembershipFactory(kind="ML")
+        siret = organization.siret
+        user = organization.members.first()
+
+        org2 = PrescriberOrganizationWithMembershipFactory(kind="PLIE", siret=siret)
+        org2.members.add(user)
+        org2.save()
+
+        self.client.login(username=user.email, password=DEFAULT_PASSWORD)
+
+        url = reverse("prescribers_views:edit_organization")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+        post_data = {
+            "siret": siret,
+            "name": "foo",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            "address_line_1": "2 Rue de Soufflenheim",
+            "address_line_2": "",
+            "city": "Betschdorf",
+            "post_code": "67660",
+            "department": "67",
+            "email": "",
+            "phone": "0610203050",
+            "website": "https://famous-siae.com",
+        }
+        response = self.client.post(url, data=post_data)
+        self.assertEqual(response.status_code, 302)
+
+        url = reverse("dashboard:index")
+        self.assertEqual(url, response.url)
+
 
 class MembersTest(TestCase):
     def test_members(self):
