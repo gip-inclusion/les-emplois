b3346855e4ddde89e2cb1d8d888b5320bb6b548e
Vermeer GRANGE <vermeer.grange@gmail.com>
2020-11-20 11:51:47 +0200

    Use latest ASP data

--- itou/siaes/management/commands/_import_siae/vue_af.py
+++ itou/siaes/management/commands/_import_siae/vue_af.py
@@ -29,7 +29,7 @@ from itou.utils.validators import validate_af_number, validate_convention_number
 
 CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 
-VUE_AF_FILENAME = f"{CURRENT_DIR}/../data/fluxIAE_AnnexeFinanciere_09112020_063002.csv"
+VUE_AF_FILENAME = f"{CURRENT_DIR}/../data/fluxIAE_AnnexeFinanciere_16112020_063002.csv"
 
 
 @timeit
