27aace5fda027288a1e559aaf7af243a2f3bfcaa
Frédéric Vergez <fred@ikarius.com>
2020-12-08 17:54:38 +0100

    WIP - Work on reference files done except addresses

--- itou/employee_record/models.py
+++ itou/employee_record/models.py
@@ -1,12 +1,123 @@
 from django.db import models
+from django.forms import ValidationError
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 from itou.approvals.models import Approval
 from itou.eligibility.models import EligibilityDiagnosis
 from itou.siae.models import FinancialAnnex
+from itou.siaes.models import Siae
 from itou.users.models import User
 from itou.utils.validators import validate_siret
+
+
+# INSEE codes
+# Are needed for:
+# - the current living address of the employee
+# - the birth place of the employee
+
+
+class PeriodMixinQuerySet(models.QuerySet):
+    def current(self):
+        return self.filter(end_date=None)
+
+
+class PeriodMixin(models.Model):
+    start_date = models.DateField(verbose_name=_("Début de validité"))
+    end_date = models.DateField(verbose_name=_("Fin de validité"), null=True)
+
+    class Meta:
+        abstract = True
+
+
+class INSEECommune(PeriodMixin):
+    """
+    INSEE commune
+
+    Code and name of French communes.
+    Mainly used to get the commune code (different from postal code).
+
+    Imported from ASP reference file: ref_insee_com_v1.csv
+
+    Note:
+    reference file is currently not up-to-date (2018)
+    """
+
+    code = models.Charfield(max_length=5, verbose_name=_("Code commune INSEE"))
+    name = models.CharField(max_length=50, verbose_name=_("Nom de la commune"))
+
+    def __str__(self):
+        return f"{self.code} - {self.name}"
+
+    def __repr__(self):
+        return f"INSEE:code={self.code}, name={self.name}"
+
+
+class INSEEDepartment(PeriodMixin):
+    """
+    INSEE department code
+
+    Code and name of French departments
+
+    Imported from ASP reference file: ref_insee_dpt_v2.csv
+    """
+
+    code = models.CharField(max_length=3, verbose_name=_("Code département INSEE"))
+    name = models.CharField(max_length=50, verbose_name=_("Nom du département"))
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f"INSEEDepartementCode:code={self.code}, name={self.name}"
+
+
+class INSEECountry(models.Model):
+    """
+    INSEE country code
+
+    Code and name of world countries
+
+    Imported from ASP reference file: ref_insee_pays_v4.csv
+    """
+
+    COUNTRY_GROUP_FRANCE = "1"
+    # CEE = "Communauté Economique Européenne" is not used since 1993...
+    COUNTRY_GROUP_CEE = "2"
+    COUNTRY_GROUP_NOT_CEE = "3"
+
+    COUNTRY_GROUP_CHOICES = (
+        (COUNTRY_GROUP_FRANCE, _("France")),
+        (COUNTRY_GROUP_CEE, _("CEE"), (COUNTRY_GROUP_NOT_CEE, _("Hors CEE"))),
+    )
+
+    code = models.CharField(max_length=3, verbose_name=_("Code pays INSEE"))
+    name = models.CharField(max_length=50, verbose_name=_("Nom du pays"))
+    group = models.CharField(max_length=1, choices=COUNTRY_GROUP_CHOICES)
+    # TODO DPT field ?
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f"INSEECountry:code={self.code}, name={self.name}"
+
+
+class EducationalLevel(PeriodMixin):
+    """
+    Educational level of the employee
+
+    Imported from ASP reference file: ref_niveau_formation_v3.csv
+    """
+
+    code = models.CharField(max_length=2, verbose_name=_("Code niveau de formation"))
+    name = models.CharField(max_length=100, verbose_name=_("Nom du niveau de formation"))
+
+    # TODO rme_id field used ?
+
+
+class EmployeeRecordQuerySet(models.QuerySet):
+    pass
 
 
 class EmployeeRecord(models.Model):
@@ -16,7 +127,7 @@ class EmployeeRecord(models.Model):
     Holds information needed for JSON exports of "fiches salariés" to ASP
     """
 
-    # ER are only possible for these kind of SIAE
+    # EmployeeRecord is only available for these kind of SIAE
     KIND_EI = "EI"
     KIND_ACI = "ACI"
     KIND_ETTI = "ETTI"
@@ -27,7 +138,7 @@ class EmployeeRecord(models.Model):
         (KIND_ETTI, _("Entreprise de travail temporaire d'insertion")),
     )
 
-    # Current possible statuses for an EmployeeRecord
+    # Current possible statuses for an EmployeeRecord (WIP?)
     STATUS_NEW = "NEW"
     STATUS_COMPLETE = "COMPLETE"
     STATUS_SENT = "SENT"
@@ -41,6 +152,22 @@ class EmployeeRecord(models.Model):
         (STATUS_REFUSED, _("Rejet ASP")),
         (STATUS_PROCESSED, _("Traitée ASP")),
     )
+
+    # TODO move to eligibility diagnosis
+    ALLOCATION_DURATION_NONE = "NONE"
+    ALLOCATION_DURATION_LT_6_MONTHS = "NONE"
+    ALLOCATION_DURATION_6_TO_11_MONTHS = "NONE"
+    ALLOCATION_DURATION_12_TO_23_MONTHS = "NONE"
+    ALLOCATION_DURATION_GT_24_MONTHS = "NONE"
+
+    ALLOCATION_DURATION_CHOICES = (
+        (ALLOCATION_DURATION_NONE, _("Aucune")),
+        (ALLOCATION_DURATION_LT_6_MONTHS, _("Moins de 6 mois")),
+        (ALLOCATION_DURATION_6_TO_11_MONTHS, _("De 6 à 11 mois")),
+        (ALLOCATION_DURATION_12_TO_23_MONTHS, _("De 12 à 23 mois")),
+        (ALLOCATION_DURATION_GT_24_MONTHS, _("24 mois et plus")),
+    )
+
     # ..
 
     siret = models.CharField(verbose_name=_("Siret"), max_length=14, validators=[validate_siret])
@@ -72,8 +199,13 @@ class EmployeeRecord(models.Model):
     created_at = models.DateTimeField(verbose_name=_("Date de création"), default=timezone.now)
     updated_at = models.DateTimeField(verbose_name=_("Date de modification"), null=True)
 
+    # Link to financial annex, mainlu for its number
     financial_annex = models.ForeignKey(FinancialAnnex, on_delete=models.CASCADE)
+
+    # Some information in the eligibility diagnosis can be used in the ER
     eligibility_diagnosis = models.ForeignKey(EligibilityDiagnosis, on_delete=models.CASCADE)
+
+    # Link the approval, mainly for its number
     approval = models.ForeignKey(Approval, on_delete=models.CASCADE)
 
     # Employee
@@ -94,7 +226,12 @@ class EmployeeRecord(models.Model):
     # - personnePhysique.nomNaissance
     # - personnePhysique.codeComInsee.codeComInsee
     # - personnePhysique.codeComInsee.codeDpt
-    employee = models.ForeignKey(User, on_delete=models.CASCADE)
+    employee = models.ForeignKey(User, verbose_name=_("Employé"), on_delete=models.CASCADE)
+    educational_leval = models.ForeignKey(EducationalLevel, verbose_name=("Niveau de formation"))
+    birth_place = models.ForeignKey(INSEECommune, verbose_name=_("Commune de naissance"))
+    birth_country = models.ForeignKey(INSEECountry, verbose_name=_("Pays de naissance"))
+
+    # birth_place = models.ForeignKey(INSEECode, verbose_name=_("Lieu de naissance"))
 
     class Meta:
         verbose_name = _("Fiche salarié")
@@ -102,14 +239,18 @@ class EmployeeRecord(models.Model):
         unique_together = ("siret", "approval")
 
     def __str__(self):
-        return f"{self.siret}={self.approval.number}"
+        return f"EmployeeRecord:SIRET={self.siret},PASS-IAE={self.approval.number}"
+
+    def clean(self):
+        # Employee record can't be updated anymore if in "final" status
+        if not self.is_updatable:
+            raise ValidationError(_("Cette fiche salarié est historisée et non-modifiable"))
+        return super.clean()
 
     def save(self, *args, **kwargs):
-        # Employee record can't be updated anymore if in "final " status
-        # ToBeDiscussed: maybe exceptions are a bit to hard for model errors...
-        if not self.is_updatable:
-            raise RuntimeError(_("Cette fiche salarié est historisée et non-modifiable"))
-
+        # When not using a form for updating / creating EmployeeRecord objects
+        # performing a clean on the model will ensure some constraints are checked
+        self.clean()
         if self.pk:
             self.updated_at = timezone.now()
         return super.save(*args, **kwargs)
@@ -117,11 +258,34 @@ class EmployeeRecord(models.Model):
     @property
     def is_updatable(self):
         """
-        Once in final state an EmployeeRecord is not updatable anymore.
-        See save() method
+        Once in final state (PROCESSED), an EmployeeRecord is not updatable anymore.
+        See model save() and clean() method.
         """
         return not (self.status == self.kind.STATUS_PROCESSED and self.json is not None)
 
     @property
     def set_complete(self):
         pass
+
+    @staticmethod
+    def convert_kind_to_asp_id(kind):
+        """
+        Conversion of Siae.kind field value to ASP employer type  
+        i.e. field `rte_code_type_employeur` of ASP reference file: ref_type_employeur_v3.csv 
+
+        Employer code is coded in one char.
+
+        ASP has a code 5 for ESAT (not used)
+        """
+        if kind == Siae.KIND_EI:
+            return "1"
+        elif kind == Siae.KIND_ETTI:
+            return "2"
+        elif kind == Siae.KIND_AI:
+            return "3"
+        elif kind == Siae.KIND_ACI:
+            return "4"
+        elif kind == Siae.KIND_EA:
+            return "6"
+        else:
+            return "7"  # Other / "Autres"
