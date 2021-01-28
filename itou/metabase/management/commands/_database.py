842fbec44e88cc779d4a0dac3cfcd6b845b84079
Vermeer GRANGE <vermeer.grange@gmail.com>
2021-01-28 16:34:13 +0200

    Explain more why to_sql needs sqlalchemy

--- itou/metabase/management/commands/_database.py
+++ itou/metabase/management/commands/_database.py
@@ -3,7 +3,21 @@ from django.conf import settings
 from sqlalchemy import create_engine
 
 
-# Required for using pandas.to_sql method in `populate_metabase_fluxiae.py`.
+"""
+Required for using pandas.to_sql method in `populate_metabase_fluxiae.py`.
+
+As stated in https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.to_sql.html
+pd.to_sql expects a sqlalchemy engine, not our usual psycopg2 engine.
+So we have to use sqlalchemy just for this purpose ¬_¬
+
+See also https://stackoverflow.com/questions/23103962/how-to-write-dataframe-to-postgres-table
+
+More generally, pandas requires SQLAlchemy to be installed for SQL operations:
+
+Quoting: https://pandas.pydata.org/pandas-docs/stable/user_guide/io.html#sql-queries
+`Database abstraction is provided by SQLAlchemy if installed.
+If SQLAlchemy is not installed, a fallback is only provided for sqlite`
+"""
 PG_ENGINE = create_engine(
     f"postgresql://{settings.METABASE_USER}:{settings.METABASE_PASSWORD}"
     f"@{settings.METABASE_HOST}:{settings.METABASE_PORT}/{settings.METABASE_DATABASE}"
