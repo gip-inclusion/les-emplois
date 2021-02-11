d255cddf7853e61d15a5554420aed0af25801910
Vermeer GRANGE <vermeer.grange@gmail.com>
2021-02-11 17:49:09 +0200

    Drop tests which are redundant with DRF tests

--- itou/api/tests.py
+++ itou/api/tests.py
@@ -2,7 +2,7 @@ from django.urls import reverse
 from rest_framework.test import APIClient, APITestCase
 
 from itou.siaes.factories import SiaeFactory, SiaeWithMembershipFactory
-from itou.users.factories import DEFAULT_PASSWORD, SiaeStaffFactory
+from itou.users.factories import DEFAULT_PASSWORD
 
 
 class SiaeAPITest(APITestCase):
@@ -34,24 +34,3 @@ class SiaeAPITest(APITestCase):
         self.assertEqual(siae_json["kind"], siae1.kind)
         self.assertEqual(siae_json["siret"], siae1.siret)
         self.assertEqual(siae_json["source"], siae1.source)
-
-    def test_missing_token_breaks(self):
-        url = reverse("api:siaes")
-        response = self.client.get(url, format="json")
-        self.assertEqual(response.status_code, 401)
-
-    def test_incorrect_token_breaks(self):
-        url = reverse("api:siaes")
-        token = "you have to keep trying things in your life"
-        self.client.credentials(HTTP_AUTHORIZATION=f"Token {token}")
-        response = self.client.get(url, format="json")
-        self.assertEqual(response.status_code, 401)
-
-    def test_incorrect_password_breaks(self):
-        user = SiaeStaffFactory()
-
-        password = "not gonna work"
-        url = reverse("api:token-auth")
-        data = {"username": user.email, "password": password}
-        response = self.client.post(url, data, format="json")
-        self.assertEqual(response.status_code, 400)
