92361e757bc1e07fd75a4a640c60585c3d656b8f
Vermeer GRANGE <vermeer.grange@gmail.com>
2021-02-19 10:17:43 +0200

    Add DummyEmployeeRecord endpoint

--- itou/api/serializers.py
+++ itou/api/serializers.py
@@ -1,3 +1,7 @@
+from random import randint
+
+from django.conf import settings
+from django.utils.crypto import salted_hmac
 from rest_framework import serializers
 
 from itou.siaes.models import Siae
@@ -7,3 +11,84 @@ class SiaeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Siae
         fields = ["kind", "siret", "source"]
+
+
+class DummyEmployeeRecordSerializer(serializers.Serializer):
+    """
+    Fake serializer always returning the same preset json with some random variation.
+
+    See README-FS-SSII.md for a detailed documentation of all the json fields.
+
+    Field documentation is *not* duplicated here, to stay as DNRY as possible.
+    """
+
+    def to_representation(self, instance):
+        # Random integer always having exactly 5 digits.
+        rnd = randint(10000, 99999)
+        physical_person = {
+            "passIae": f"9999900{rnd}",
+            "sufPassIae": None,
+            "idItou": salted_hmac(
+                key_salt="job_seeker.id",
+                # rnd is supposed to be job_seeker.id
+                value=rnd,
+                secret=settings.SECRET_KEY,
+            ).hexdigest()[:30],
+            "civilite": "M",
+            "nomUsage": "DECAILLOUX",
+            "nomNaissance": None,
+            "prenom": "YVES",
+            "dateNaissance": "01/08/1954",
+            "codeComInsee": {
+                "codeComInsee": "34172",
+                "codeDpt": "034",
+            },
+            "codeInseePays": "100",
+            "codeGroupePays": "1",
+        }
+
+        address = {
+            "adrTelephone": f"01000{rnd}",
+            "adrMail": f"john.doe.{rnd}@gmail.com",
+            "adrNumeroVoie": None,
+            "codeextensionvoie": None,
+            "codetypevoie": "AV",
+            "adrLibelleVoie": "AVENUE ABBE PAUL PARGUEL",
+            "adrCpltDistribution": None,
+            "codeinseecom": "34172",
+            "codepostalcedex": "34000",
+        }
+
+        job_seeker_situation = {
+            "orienteur": "01",
+            "niveauFormation": "00",
+            "salarieEnEmploi": False,
+            "salarieTypeEmployeur": None,
+            "salarieSansEmploiDepuis": "04",
+            "salarieSansRessource": False,
+            "inscritPoleEmploi": True,
+            "inscritPoleEmploiDepuis": "04",
+            "numeroIDE": f"11{rnd}A",
+            "salarieRQTH": False,
+            "salarieOETH": False,
+            "salarieAideSociale": False,
+            "salarieBenefRSA": "NON",
+            "salarieBenefRSADepuis": None,
+            "salarieBenefASS": False,
+            "salarieBenefASSDepuis": None,
+            "salarieBenefAAH": False,
+            "salarieBenefAAHDepuis": None,
+            "salarieBenefATA": False,
+            "salarieBenefATADepuis": None,
+        }
+
+        fiche_salarie = {
+            "mesure": "ACI_DC",
+            "siret": f"330550393{rnd}",
+            "numeroAnnexe": f"ACI0232{rnd}A0M0",
+            "personnePhysique": physical_person,
+            "adresse": address,
+            "situationSalarie": job_seeker_situation,
+        }
+
+        return fiche_salarie
--- itou/api/views.py
+++ itou/api/views.py
@@ -2,17 +2,38 @@ from rest_framework import generics
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from itou.api.serializers import SiaeSerializer
+from itou.api.serializers import DummyEmployeeRecordSerializer, SiaeSerializer
+from itou.job_applications.models import JobApplication
 
 
 class SiaeList(generics.ListAPIView):
+    """
+    Return the list of all the siaes the currently authenticated user is a member of.
+    """
+
     serializer_class = SiaeSerializer
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        """
-        This view returns the list of all the siaes the currently authenticated user is a member of.
-        """
         # Order by pk to solve pagination warning.
         return self.request.user.siae_set.active().filter(members__is_active=True).order_by("pk")
+
+
+class DummyEmployeeRecordList(generics.ListAPIView):
+    """
+    Return dummy (fake) employee record objects for testing by external software editors.
+    """
+
+    serializer_class = DummyEmployeeRecordSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """
+        Return the same 25 job applications whatever the user.
+        The DummyEmployeeRecordSerializer will replace these objects with raw randomized jsons.
+        25 is slightly more than the page size (50) so that pagination can be tested.
+        Order by pk to solve pagination warning.
+        """
+        return JobApplication.objects.order_by("pk")[:25]
