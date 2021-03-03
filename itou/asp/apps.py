3ff31bac119319da628c6ed13b3bb157197afa95
Frédéric Vergez <fred@ikarius.com>
2021-03-03 15:02:54 +0100

    WIP EmployeeRecord admin: user data part

--- itou/asp/apps.py
+++ itou/asp/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class AspConfig(AppConfig):
-    name = "asp"
+    name = "itou.asp"
+    verbose_name = _("Référentiels de données ASP")
--- itou/employee_record/apps.py
+++ itou/employee_record/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class EmployeeRecordConfig(AppConfig):
-    name = "employee_record"
+    name = "itou.employee_record"
+    verbose_name = _("Fiches salarié")
