f559c49e239195f604015b0d0b59123109e670a0
kemar <marc@marcarea.com>
2021-03-10 14:18:49 +0100

    Use `OSMWidget` instead of `OpenLayersWidget` for the map in CityAdmin

--- itou/cities/admin.py
+++ itou/cities/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.gis import forms as gis_forms
+from django.contrib.gis.db import models as gis_models
 
 from itou.cities import models
 
@@ -8,3 +10,7 @@ class CityAdmin(admin.ModelAdmin):
     list_display = ("name", "department", "post_codes", "code_insee")
     list_filter = ("department",)
     search_fields = ("name", "department", "post_codes", "code_insee")
+    formfield_overrides = {
+        # https://docs.djangoproject.com/en/2.2/ref/contrib/gis/forms-api/#widget-classes
+        gis_models.PointField: {"widget": gis_forms.OSMWidget(attrs={"map_width": 800, "map_height": 500})}
+    }
