4dba39c71081573bcd7ce035f31efc1972ceac3c
kemar <marc@marcarea.com>
2021-03-10 17:38:22 +0100

    Use `BaseNotification` instead of `NotificationBase`

--- itou/approvals/notifications.py
+++ itou/approvals/notifications.py
@@ -1,11 +1,13 @@
 from itou.utils.emails import get_email_message
-from itou.utils.notifications.base_class import NotificationBase
+from itou.utils.notifications.base_class import BaseNotification
 
 
-class NewProlongationToAuthorizedPrescriberNotification(NotificationBase):
+class NewProlongationToAuthorizedPrescriberNotification(BaseNotification):
     """
     Notification sent to the authorized prescriber supposed to have validated the prolongation.
     """
+
+    NAME = "confirm_prolongation_email"
 
     def __init__(self, prolongation):
         self.prolongation = prolongation
@@ -17,7 +19,3 @@ class NewProlongationToAuthorizedPrescriberNotification(NotificationBase):
         subject = "approvals/email/new_prolongation_for_prescriber_subject.txt"
         body = "approvals/email/new_prolongation_for_prescriber_body.txt"
         return get_email_message(to, context, subject, body)
-
-    @property
-    def name(self):
-        return "confirm_prolongation_email"
