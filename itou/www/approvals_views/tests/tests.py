07e7cf1e4edb62c3d8d6ff1840f46683792e9c21
Céline MS <celine@hello-birds.com>
2021-03-10 10:39:53 +0100

    Redirect users trying to download an approval

--- itou/www/approvals_views/tests/tests.py
+++ itou/www/approvals_views/tests/tests.py
@@ -33,73 +33,78 @@ class TestDownloadApprovalAsPDF(TestCase):
             reverse("approvals:approval_as_pdf", kwargs={"job_application_id": job_application.pk})
         )
 
-        self.assertEqual(response.status_code, 200)
-        self.assertIn("pdf", response.get("Content-Type"))
+        # Temporarily disable approval download due to PDF Shift unavailability.
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, reverse("dashboard:index"))
 
-    def test_impossible_download_when_approval_is_missing(self, *args, **kwargs):
-        """
-        The button to download an approval is show only when
-        certain conditions are met.
-        Nevertheless, don't trust the client. Make sure we raise an error
-        if the same conditions are not met in this view.
-        """
-        # Create a job application without an approval.
-        job_application = JobApplicationFactory()
-        siae_member = job_application.to_siae.members.first()
-        job_seeker = job_application.job_seeker
-        EligibilityDiagnosisFactory(job_seeker=job_seeker)
+        # self.assertEqual(response.status_code, 200)
+        # self.assertEqual(response.status_code, 200)
+        # self.assertIn("pdf", response.get("Content-Type"))
 
-        self.client.login(username=siae_member.email, password=DEFAULT_PASSWORD)
-        response = self.client.get(
-            reverse("approvals:approval_as_pdf", kwargs={"job_application_id": job_application.pk})
-        )
-        self.assertEqual(response.status_code, 404)
+    # def test_impossible_download_when_approval_is_missing(self, *args, **kwargs):
+    #     """
+    #     The button to download an approval is show only when
+    #     certain conditions are met.
+    #     Nevertheless, don't trust the client. Make sure we raise an error
+    #     if the same conditions are not met in this view.
+    #     """
+    #     # Create a job application without an approval.
+    #     job_application = JobApplicationFactory()
+    #     siae_member = job_application.to_siae.members.first()
+    #     job_seeker = job_application.job_seeker
+    #     EligibilityDiagnosisFactory(job_seeker=job_seeker)
 
-    @patch("pdfshift.convert", side_effect=requests_exceptions.ConnectionError)
-    def test_pdfshift_api_is_down(self, *args, **kwargs):
-        job_application = JobApplicationWithApprovalFactory()
-        siae_member = job_application.to_siae.members.first()
-        job_seeker = job_application.job_seeker
-        EligibilityDiagnosisFactory(job_seeker=job_seeker)
+    #     self.client.login(username=siae_member.email, password=DEFAULT_PASSWORD)
+    #     response = self.client.get(
+    #         reverse("approvals:approval_as_pdf", kwargs={"job_application_id": job_application.pk})
+    #     )
+    #     self.assertEqual(response.status_code, 404)
 
-        self.client.login(username=siae_member.email, password=DEFAULT_PASSWORD)
+    # @patch("pdfshift.convert", side_effect=requests_exceptions.ConnectionError)
+    # def test_pdfshift_api_is_down(self, *args, **kwargs):
+    #     job_application = JobApplicationWithApprovalFactory()
+    #     siae_member = job_application.to_siae.members.first()
+    #     job_seeker = job_application.job_seeker
+    #     EligibilityDiagnosisFactory(job_seeker=job_seeker)
 
-        with self.assertRaises(ConnectionAbortedError):
-            self.client.get(reverse("approvals:approval_as_pdf", kwargs={"job_application_id": job_application.pk}))
+    #     self.client.login(username=siae_member.email, password=DEFAULT_PASSWORD)
 
-    @patch("pdfshift.convert", return_value=BITES_FILE)
-    @patch("itou.approvals.models.CommonApprovalMixin.originates_from_itou", False)
-    def test_download_approval_even_if_diagnosis_is_missing(self, *args, **kwargs):
-        job_application = JobApplicationWithApprovalFactory()
-        siae_member = job_application.to_siae.members.first()
+    #     with self.assertRaises(ConnectionAbortedError):
+    #         self.client.get(reverse("approvals:approval_as_pdf", kwargs={"job_application_id": job_application.pk}))
 
-        # An approval has been delivered but it does not come from Itou.
-        # Therefore, the linked diagnosis exists but is not in our database.
-        # Don't create a diagnosis.
-        # EligibilityDiagnosisFactory(job_seeker=job_seeker)
+    # @patch("pdfshift.convert", return_value=BITES_FILE)
+    # @patch("itou.approvals.models.CommonApprovalMixin.originates_from_itou", False)
+    # def test_download_approval_even_if_diagnosis_is_missing(self, *args, **kwargs):
+    #     job_application = JobApplicationWithApprovalFactory()
+    #     siae_member = job_application.to_siae.members.first()
 
-        self.client.login(username=siae_member.email, password=DEFAULT_PASSWORD)
+    #     # An approval has been delivered but it does not come from Itou.
+    #     # Therefore, the linked diagnosis exists but is not in our database.
+    #     # Don't create a diagnosis.
+    #     # EligibilityDiagnosisFactory(job_seeker=job_seeker)
 
-        response = self.client.get(
-            reverse("approvals:approval_as_pdf", kwargs={"job_application_id": job_application.pk})
-        )
+    #     self.client.login(username=siae_member.email, password=DEFAULT_PASSWORD)
 
-        self.assertEqual(response.status_code, 200)
-        self.assertIn("pdf", response.get("Content-Type"))
+    #     response = self.client.get(
+    #         reverse("approvals:approval_as_pdf", kwargs={"job_application_id": job_application.pk})
+    #     )
 
-    @patch("itou.approvals.models.CommonApprovalMixin.originates_from_itou", True)
-    def test_no_download_if_missing_diagnosis(self, *args, **kwargs):
-        job_application = JobApplicationWithApprovalFactory()
-        siae_member = job_application.to_siae.members.first()
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertIn("pdf", response.get("Content-Type"))
 
-        # An approval has been delivered by Itou but there is no diagnosis.
-        # It should raise an error.
-        # EligibilityDiagnosisFactory(job_seeker=job_seeker)
+    # @patch("itou.approvals.models.CommonApprovalMixin.originates_from_itou", True)
+    # def test_no_download_if_missing_diagnosis(self, *args, **kwargs):
+    #     job_application = JobApplicationWithApprovalFactory()
+    #     siae_member = job_application.to_siae.members.first()
 
-        self.client.login(username=siae_member.email, password=DEFAULT_PASSWORD)
+    #     # An approval has been delivered by Itou but there is no diagnosis.
+    #     # It should raise an error.
+    #     # EligibilityDiagnosisFactory(job_seeker=job_seeker)
 
-        with self.assertRaises(ObjectDoesNotExist):
-            self.client.get(reverse("approvals:approval_as_pdf", kwargs={"job_application_id": job_application.pk}))
+    #     self.client.login(username=siae_member.email, password=DEFAULT_PASSWORD)
+
+    #     with self.assertRaises(ObjectDoesNotExist):
+    #         self.client.get(reverse("approvals:approval_as_pdf", kwargs={"job_application_id": job_application.pk}))
 
 
 class ApprovalSuspendViewTest(TestCase):
