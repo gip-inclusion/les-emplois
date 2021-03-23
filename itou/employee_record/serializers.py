e87ca64a81ef75ebd0776c627cf4824b1a38a65a
Frédéric Vergez <fred@ikarius.com>
2021-03-23 11:12:49 +0100

    Added passIae field in the right position without recoding all sub-serializer

--- itou/employee_record/serializers.py
+++ itou/employee_record/serializers.py
@@ -4,6 +4,15 @@ from itou.employee_record.models import EmployeeRecord
 from itou.users.models import User
 
 
+class RemoveEmptyStringSerializer(serializers.ModelSerializer):
+    def to_representation(self, instance):
+
+        print(self.data)
+        print(self.__dict__)
+
+        return super().to_representation(instance)
+
+
 class _EmployeeSerializer(serializers.ModelSerializer):
 
     idItou = serializers.IntegerField(source="id")
@@ -69,18 +78,24 @@ class _EmployeeSituation(serializers.ModelSerializer):
 
     salarieSansEmploiDepuis = serializers.CharField(source="jobseeker_profile.unemployed_since")
     salarieSansRessource = serializers.CharField(source="jobseeker_profile.resourceless")
+
     inscritPoleEmploi = serializers.BooleanField(source="pole_emploi_id")
     inscritPoleEmploiDepuis = serializers.CharField(source="jobseeker_profile.pole_emploi_since")
     numeroIDE = serializers.CharField(source="pole_emploi_id")
+
     salarieRQTH = serializers.BooleanField(source="jobseeker_profile.rqth_employee")
     salarieOETH = serializers.BooleanField(source="jobseeker_profile.oeth_employee")
     salarieAideSociale = serializers.BooleanField(source="jobseeker_profile.has_social_allowance")
+
     salarieBenefRSA = serializers.BooleanField(source="jobseeker_profile.has_rsa_allocation")
     salarieBenefRSADepuis = serializers.CharField(source="jobseeker_profile.rsa_allocation_since")
+
     salarieBenefASS = serializers.BooleanField(source="jobseeker_profile.has_ass_allocation")
     salarieBenefASSDepuis = serializers.CharField(source="jobseeker_profile.ass_allocation_since")
+
     salarieBenefAAH = serializers.BooleanField(source="jobseeker_profile.has_aah_allocation")
     salarieBenefAAHDepuis = serializers.CharField(source="jobseeker_profile.aah_allocation_since")
+
     salarieBenefATA = serializers.BooleanField(source="jobseeker_profile.has_ata_allocation")
     salarieBenefATADepuis = serializers.CharField(source="jobseeker_profile.ata_allocation_since")
 
@@ -124,3 +139,17 @@ class EmployeeRecordSerializer(serializers.ModelSerializer):
             "situationSalarie",
         ]
         read_only_fields = fields
+
+    def to_representation(self, instance):
+        result = super().to_representation(instance)
+
+        print(result)
+
+        # Get passIae field out of root level
+        # and stick it into the personnePhysique JSON object
+        person = result["personnePhysique"]
+        person["passIae"] = result.pop("passIae")
+        # At first position (this is an OrderedDict)
+        person.move_to_end("passIae", last=False)
+
+        return result
