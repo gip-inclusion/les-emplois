60f3ecc57eaf460562a1f2af0e9efcd085ab2dd1
Frédéric Vergez <fred@ikarius.com>
2021-03-24 18:15:28 +0100

    Style applied

--- itou/employee_record/migrations/0004_employeerecord_asp_processing_label.py
+++ itou/employee_record/migrations/0004_employeerecord_asp_processing_label.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('employee_record', '0003_auto_20210318_1031'),
+        ("employee_record", "0003_auto_20210318_1031"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='employeerecord',
-            name='asp_processing_label',
-            field=models.CharField(blank=True, max_length=100, verbose_name='Libellé de traitement ASP'),
+            model_name="employeerecord",
+            name="asp_processing_label",
+            field=models.CharField(blank=True, max_length=100, verbose_name="Libellé de traitement ASP"),
         ),
     ]
