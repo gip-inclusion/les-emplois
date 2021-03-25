bbcf10e21d07e9dd1f2eca27cdab7bd0d8bd5d01
clemkeirua <clement@keiruaprod.fr>
2021-03-25 17:06:56 +0100

    update assistance url in emails

--- itou/utils/emails.py
+++ itou/utils/emails.py
@@ -29,6 +29,7 @@ def get_email_text_template(template, context):
     context.update(
         {
             "itou_doc_url": settings.ITOU_DOC_URL,
+            "itou_assistance_url": settings.ITOU_ASSISTANCE_URL,
             "itou_protocol": settings.ITOU_PROTOCOL,
             "itou_fqdn": settings.ITOU_FQDN,
             "itou_email_assistance": settings.ITOU_EMAIL_ASSISTANCE,
