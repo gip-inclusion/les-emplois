b69fe7ca77dbcecb666ec9f44ab56d2fc91f678b
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2021-04-13 09:34:40 +0200

    Merge two test cases and remove duplicated tests

--- itou/www/invitations_views/tests/tests_siae_accept.py
+++ itou/www/invitations_views/tests/tests_siae_accept.py
@@ -1,13 +1,8 @@
-from allauth.account.models import EmailAddress
 from django.core import mail
 from django.shortcuts import reverse
 from django.test import TestCase
 
-from itou.invitations.factories import (
-    ExpiredSiaeStaffInvitationFactory,
-    SentSiaeStaffInvitationFactory,
-    SiaeSentInvitationFactory,
-)
+from itou.invitations.factories import ExpiredSiaeStaffInvitationFactory, SentSiaeStaffInvitationFactory
 from itou.prescribers.factories import PrescriberOrganizationWithMembershipFactory
 from itou.siaes.factories import SiaeFactory, SiaeWith2MembershipsFactory
 from itou.users.factories import DEFAULT_PASSWORD, SiaeStaffFactory, UserFactory
@@ -16,6 +11,23 @@ from itou.utils.perms.siae import get_current_siae_or_404
 
 
 class TestAcceptInvitation(TestCase):
+    def assert_accepted_invitation(self, invitation, user, response):
+        user.refresh_from_db()
+        invitation.refresh_from_db()
+        self.assertTrue(user.is_siae_staff)
+        self.assertTrue(invitation.accepted)
+        self.assertTrue(invitation.accepted_at)
+
+        # Make sure there's a welcome message.
+        messages = list(response.context["messages"])
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(messages[0].level_tag, "success")
+
+        # A confirmation e-mail is sent to the invitation sender.
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(len(mail.outbox[0].to), 1)
+        self.assertEqual(invitation.sender.email, mail.outbox[0].to[0])
+
     def test_accept_invitation_signup(self):
         invitation = SentSiaeStaffInvitationFactory()
 
@@ -37,12 +49,13 @@ class TestAcceptInvitation(TestCase):
             follow=True,
         )
         self.assertRedirects(response, reverse("dashboard:index"))
-        total_users_after = User.objects.count()
 
+        total_users_after = User.objects.count()
         self.assertEqual((total_users_before + 1), total_users_after)
 
         user = User.objects.get(email=invitation.email)
         self.assertTrue(user.emailaddress_set.first().verified)
+        self.assert_accepted_invitation(invitation, user, response)
 
     def test_accept_invitation_logged_in_user(self):
         # A logged in user should log out before accepting an invitation.
@@ -71,6 +84,7 @@ class TestAcceptInvitation(TestCase):
 
         # Fill in the password and send
         response = self.client.post(invitation.acceptance_link, data={**form_data}, follow=True)
+        self.assertRedirects(response, reverse("dashboard:index"))
 
         user = User.objects.get(email=invitation.email)
         self.assertEqual(invitation.email, user.email)
@@ -126,133 +140,52 @@ class TestAcceptInvitation(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "acceptée")
 
-
-class TestAcceptSiaeInvitation(TestCase):
-    def setUp(self):
-        self.siae = SiaeWith2MembershipsFactory()
-        self.sender = self.siae.members.first()
-        self.invitation = SiaeSentInvitationFactory(sender=self.sender, siae=self.siae)
-        self.user = None
-        self.response = None
-
-    def assert_accepted_invitation(self):
-        self.assertEqual(self.response.status_code, 200)
-        self.user.refresh_from_db()
-        self.invitation.refresh_from_db()
-        self.assertTrue(self.user.is_siae_staff)
-        self.assertTrue(self.invitation.accepted)
-        self.assertTrue(self.invitation.accepted_at)
-        self.assertEqual(self.siae.members.count(), 3)
-
-        self.assertEqual(reverse("dashboard:index"), self.response.wsgi_request.path)
-        # Make sure there's a welcome message.
-        messages = list(self.response.context["messages"])
-        self.assertEqual(len(messages), 1)
-        self.assertEqual(messages[0].level_tag, "success")
-
-        # A confirmation e-mail is sent to the invitation sender.
-        self.assertEqual(len(mail.outbox), 1)
-        self.assertEqual(len(mail.outbox[0].to), 1)
-        self.assertEqual(self.invitation.sender.email, mail.outbox[0].to[0])
-
-    def test_accept_siae_invitation(self):
-        response = self.client.get(self.invitation.acceptance_link, follow=True)
-        self.assertIn(response.wsgi_request.path, self.invitation.acceptance_link)
-
-        form_data = {
-            "first_name": self.invitation.first_name,
-            "last_name": self.invitation.last_name,
-            "password1": "Erls92#32",
-            "password2": "Erls92#32",
-        }
-
-        self.response = self.client.post(self.invitation.acceptance_link, data=form_data, follow=True)
-
-        self.user = User.objects.get(email=self.invitation.email)
-        self.assert_accepted_invitation()
-
-    def test_accept_existing_user_is_employer(self):
-        self.user = SiaeWith2MembershipsFactory().members.first()
-        self.invitation = SentSiaeStaffInvitationFactory(
-            sender=self.sender,
-            siae=self.siae,
-            first_name=self.user.first_name,
-            last_name=self.user.last_name,
-            email=self.user.email,
-        )
-        self.client.login(email=self.user.email, password=DEFAULT_PASSWORD)
-        self.response = self.client.get(self.invitation.acceptance_link, follow=True)
-
-        current_siae = get_current_siae_or_404(self.response.wsgi_request)
-        self.assertEqual(self.invitation.siae.pk, current_siae.pk)
-        self.assert_accepted_invitation()
-
     def test_accept_existing_user_with_existing_inactive_siae(self):
         """
         An inactive siae user (i.e. attached to a single inactive siae)
         can only be ressucitated by being invited to a new siae.
         We test here that this is indeed possible.
         """
-        self.user = SiaeWith2MembershipsFactory(convention__is_active=False).members.first()
-        self.invitation = SentSiaeStaffInvitationFactory(
-            sender=self.sender,
-            siae=self.siae,
-            first_name=self.user.first_name,
-            last_name=self.user.last_name,
-            email=self.user.email,
+        siae = SiaeWith2MembershipsFactory()
+        sender = siae.members.first()
+        user = SiaeWith2MembershipsFactory(convention__is_active=False).members.first()
+        invitation = SentSiaeStaffInvitationFactory(
+            sender=sender,
+            siae=siae,
+            first_name=user.first_name,
+            last_name=user.last_name,
+            email=user.email,
         )
-        self.client.login(email=self.user.email, password=DEFAULT_PASSWORD)
-        self.response = self.client.get(self.invitation.acceptance_link, follow=True)
+        self.client.login(email=user.email, password=DEFAULT_PASSWORD)
+        response = self.client.get(invitation.acceptance_link, follow=True)
+        self.assertRedirects(response, reverse("dashboard:index"))
 
-        current_siae = get_current_siae_or_404(self.response.wsgi_request)
-        self.assertEqual(self.invitation.siae.pk, current_siae.pk)
-        self.assert_accepted_invitation()
-
-    def test_accept_existing_user_not_logged_in(self):
-        self.user = SiaeWith2MembershipsFactory().members.first()
-        # The user verified its email
-        EmailAddress(user_id=self.user.pk, email=self.user.email, verified=True, primary=True).save()
-        self.invitation = SentSiaeStaffInvitationFactory(
-            sender=self.sender,
-            siae=self.siae,
-            first_name=self.user.first_name,
-            last_name=self.user.last_name,
-            email=self.user.email,
-        )
-        response = self.client.get(self.invitation.acceptance_link, follow=True)
-
-        self.assertIn(reverse("account_login"), response.wsgi_request.get_full_path())
-        self.assertFalse(self.invitation.accepted)
-
-        self.response = self.client.post(
-            response.wsgi_request.get_full_path(),
-            data={"login": self.user.email, "password": DEFAULT_PASSWORD},
-            follow=True,
-        )
-        self.assertTrue(self.response.wsgi_request.user.is_authenticated)
-        self.assert_accepted_invitation()
+        current_siae = get_current_siae_or_404(response.wsgi_request)
+        self.assertEqual(siae.pk, current_siae.pk)
+        self.assert_accepted_invitation(invitation, user, response)
 
     def test_accept_existing_user_is_not_employer(self):
-        self.user = PrescriberOrganizationWithMembershipFactory().members.first()
-        self.invitation = SentSiaeStaffInvitationFactory(
-            sender=self.sender,
-            siae=self.siae,
-            first_name=self.user.first_name,
-            last_name=self.user.last_name,
-            email=self.user.email,
+        user = PrescriberOrganizationWithMembershipFactory().members.first()
+        invitation = SentSiaeStaffInvitationFactory(
+            first_name=user.first_name,
+            last_name=user.last_name,
+            email=user.email,
         )
 
-        self.client.login(email=self.user.email, password=DEFAULT_PASSWORD)
-        response = self.client.get(self.invitation.acceptance_link, follow=True)
+        self.client.login(email=user.email, password=DEFAULT_PASSWORD)
+        response = self.client.get(invitation.acceptance_link, follow=True)
 
         self.assertEqual(response.status_code, 403)
-        self.assertFalse(self.invitation.accepted)
+        self.assertFalse(invitation.accepted)
 
     def test_accept_connected_user_is_not_the_invited_user(self):
-        self.client.login(email=self.sender.email, password=DEFAULT_PASSWORD)
-        response = self.client.get(self.invitation.acceptance_link, follow=True)
+        invitation = SentSiaeStaffInvitationFactory()
+        self.client.login(email=invitation.sender.email, password=DEFAULT_PASSWORD)
+        response = self.client.get(invitation.acceptance_link, follow=True)
 
         self.assertEqual(reverse("account_logout"), response.wsgi_request.path)
-        self.assertFalse(self.invitation.accepted)
+        self.assertFalse(invitation.accepted)
         messages = list(response.context["messages"])
+
         self.assertEqual(len(messages), 1)
+        self.assertIn("Un utilisateur est déjà connecté.", str(messages[0]))
