7d3182defb625cb933a68baa10d4aa6cde1e024b
kemar <marc@marcarea.com>
2021-04-14 17:58:48 +0200

    Use `re_path`

--- itou/allauth_adapters/peamu/urls.py
+++ itou/allauth_adapters/peamu/urls.py
@@ -1,4 +1,4 @@
-from django.urls import include, path
+from django.urls import include, re_path
 
 from itou.allauth_adapters.peamu.provider import PEAMUProvider
 from itou.allauth_adapters.peamu.views import oauth2_callback as callback_view, oauth2_login as login_view
@@ -6,11 +6,11 @@ from itou.allauth_adapters.peamu.views import oauth2_callback as callback_view, 
 
 def default_urlpatterns(provider):
     urlpatterns = [
-        path("login", login_view, name=provider.id + "_login"),
-        path("login/callback", callback_view, name=provider.id + "_callback"),
+        re_path(r"^login/$", login_view, name=provider.id + "_login"),
+        re_path(r"^login/callback/$", callback_view, name=provider.id + "_callback"),
     ]
 
-    return [path(provider.get_slug(), include(urlpatterns))]
+    return [re_path(r"^" + provider.get_slug() + r"/", include(urlpatterns))]
 
 
 urlpatterns = default_urlpatterns(PEAMUProvider)
