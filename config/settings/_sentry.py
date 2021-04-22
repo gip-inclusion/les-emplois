00bb157df5e7cdf69e08e98b1f8fe4e87d0e4d27
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2021-04-22 18:22:16 +0200

    Add sample of trace for Sentry

--- config/settings/_sentry.py
+++ config/settings/_sentry.py
@@ -30,6 +30,10 @@ def sentry_init(dsn):
     sentry_sdk.init(
         dsn=dsn,
         integrations=[DjangoIntegration()],
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0,
         # Associate users (ID+email+username+IP) to errors.
         # https://docs.sentry.io/platforms/python/django/
         send_default_pii=True,
