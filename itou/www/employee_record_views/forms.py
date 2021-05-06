8df609677a74cbe5bfe07ee070d4f23bdb0c3ba3
Frédéric Vergez <fred@ikarius.com>
2021-05-06 15:35:45 +0200

    PR feedback

--- itou/www/employee_record_views/forms.py
+++ itou/www/employee_record_views/forms.py
@@ -3,22 +3,21 @@ from django import forms
 from itou.employee_record.models import EmployeeRecord
 
 
-# The user is only able to select a subset of the possible
-# employee record statuses.
-# The other ones are internal only.
-_statuses = [
-    EmployeeRecord.Status.NEW,
-    EmployeeRecord.Status.SENT,
-    EmployeeRecord.Status.REJECTED,
-    EmployeeRecord.Status.PROCESSED,
-]
-
-
 class SelectEmployeeRecordStatusForm(forms.Form):
 
-    _choices = [(choice.name, choice.label) for choice in _statuses]
+    # The user is only able to select a subset of the possible
+    # employee record statuses.
+    # The other ones are internal only.
+    STATUSES = [
+        EmployeeRecord.Status.NEW,
+        EmployeeRecord.Status.SENT,
+        EmployeeRecord.Status.REJECTED,
+        EmployeeRecord.Status.PROCESSED,
+    ]
+
+    STATUS_CHOICES = [(choice.name, choice.label) for choice in STATUSES]
     status = forms.ChoiceField(
         widget=forms.RadioSelect(),
-        choices=_choices,
+        choices=STATUS_CHOICES,
         initial=EmployeeRecord.Status.NEW,
     )
