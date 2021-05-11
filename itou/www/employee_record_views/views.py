90e84a36bb14a114047ff3b43444fc26b91aa676
Frédéric Vergez <fred@ikarius.com>
2021-05-11 15:29:41 +0200

    Updated query for badges, UI updates

--- itou/www/employee_record_views/views.py
+++ itou/www/employee_record_views/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
+from django.db.models import Count
 from django.shortcuts import render
 
 from itou.employee_record.models import EmployeeRecord
@@ -33,20 +34,30 @@ def list(request, template_name="employee_record/list.html"):
     navigation_pages = None
     data = None
 
-    # Fetch count of each status for badge display
+    # Construct badges
+
+    # Badges for "real" employee records
+    employee_record_statuses = (
+        EmployeeRecord.objects.values("status").annotate(cnt=Count("status")).order_by("-status")
+    )
+    employee_record_badges = {row["status"]: row["cnt"] for row in employee_record_statuses}
+
+    # Set count of each status for badge display
     status_badges = [
         (
             JobApplication.objects.eligible_as_employee_record(siae).count(),
             "info",
         ),
-        (EmployeeRecord.objects.sent_for_siae(siae).count(), "warning"),
-        (EmployeeRecord.objects.rejected_for_siae(siae).count(), "danger"),
-        (EmployeeRecord.objects.processed_for_siae(siae).count(), "success"),
+        (employee_record_badges.get(EmployeeRecord.Status.SENT, 0), "warning"),
+        (employee_record_badges.get(EmployeeRecord.Status.REJECTED, 0), "danger"),
+        (employee_record_badges.get(EmployeeRecord.Status.PROCESSED, 0), "success"),
     ]
 
     # Override defaut value (NEW status)
     if form.is_valid():
         status = form.cleaned_data["status"]
+
+    print(status)
 
     # See comment above on `employee_records_list` var
     if status == EmployeeRecord.Status.NEW:
@@ -54,6 +65,10 @@ def list(request, template_name="employee_record/list.html"):
         employee_records_list = False
     elif status == EmployeeRecord.Status.SENT:
         data = EmployeeRecord.objects.sent_for_siae(siae)
+    elif status == EmployeeRecord.Status.REJECTED:
+        data = EmployeeRecord.objects.rejected_for_siae(siae)
+    elif status == EmployeeRecord.Status.PROCESSED:
+        data = EmployeeRecord.objects.processed_for_siae(siae)
 
     if data:
         navigation_pages = pager(data, request.GET.get("page", 1), items_per_page=10)
