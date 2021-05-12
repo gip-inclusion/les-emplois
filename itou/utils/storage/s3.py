8fa17451054e32547cda7472d8fadc7d9a4b2f2a
Céline MS <celine@hello-birds.com>
2021-05-12 11:41:13 +0200

    Replace custom s3 signature methods by the one provided by boto3

--- itou/utils/storage/s3.py
+++ itou/utils/storage/s3.py
@@ -1,80 +1,45 @@
 """
 Useful functions to interact with an S3 bucket.
 """
-import base64
-import hashlib
-import hmac
-import json
-from datetime import timedelta
 
+import boto3
+from botocore.client import Config
 from django.conf import settings
-from django.utils import timezone
 
 
 class S3Upload:
-    def __init__(self, kind="default", date=None):
-        self.date = date or timezone.now()
+    def __init__(self, kind="default"):
         self.config = self.get_config(kind)
-
-    def generate_signature(self, string_to_sign):
-        """
-        Use credential's URL directories to sign a string.
-        """
-        date_str = self.format_date_short(self.date)
-        date_key = self.sign_to_bytes(("AWS4" + settings.S3_STORAGE_SECRET_ACCESS_KEY), date_str)
-        date_region_key = self.sign_to_bytes(date_key, settings.S3_STORAGE_BUCKET_REGION)
-        date_region_service_key = self.sign_to_bytes(date_region_key, "s3")
-        signing_key = self.sign_to_bytes(date_region_service_key, "aws4_request")
-        return self.sign_to_string(key=signing_key, msg=string_to_sign)
-
-    def policy_as_dict(self):
-        """
-        Set uploading file policy. Different from the bucket policy.
-        """
-        form_date = self.format_date_long(self.date)
-        expiration_date = self.format_expiration_date(self.form_expires_at)
-        bucket_name = settings.S3_STORAGE_BUCKET_NAME
-        key_path = self.config["key_path"]
-
-        policy = {
-            "expiration": expiration_date,
-            "conditions": [
-                ["starts-with", "$key", key_path],
-                {"bucket": bucket_name},
-                {"x-amz-algorithm": "AWS4-HMAC-SHA256"},
-                {"x-amz-credential": self.credential_url},
-                {"x-amz-date": form_date},
-            ],
-        }
-        return policy
-
-    @property
-    def credential_url(self):
-        """
-        Each directory is a key used when generating the signature.
-        """
-        date_str = self.format_date_short(self.date)
-        return f"{settings.S3_STORAGE_ACCESS_KEY_ID}/{date_str}/{settings.S3_STORAGE_BUCKET_REGION}/s3/aws4_request"
-
-    @property
-    def form_expires_at(self):
-        return self.date + timedelta(hours=self.config["upload_expiration"])
 
     @property
     def form_values(self):
-        form_date = self.format_date_long(self.date)
-        policy = self.policy_as_dict()
-        encoded_policy = self.encode_dict(policy)
-        credential_url = self.credential_url
-        signature = self.generate_signature(string_to_sign=encoded_policy)
-
-        return {
-            "credential_url": credential_url,
-            "date": form_date,
-            "encoded_policy": encoded_policy,
-            "signature": signature,
-            "endpoint": settings.S3_STORAGE_BUCKET_ENDPOINT_URL,
+        """
+        Returns a dict like this:
+        {
+            "url": "https://cellar-c2.services.clever-cloud.com/bucket_name",
+            "fields": {
+                'key': 'key_path',
+                'x-amz-algorithm': 'AWS4-HMAC-SHA256',
+                'x-amz-credential': '',
+                'x-amz-date': '',
+                'policy': '',
+                'x-amz-signature': '',
+            }
         }
+        """
+        connection_kwargs = {
+            "endpoint_url": settings.S3_STORAGE_BASE_ENDPOINT_URL,
+            "aws_access_key_id": settings.S3_STORAGE_ACCESS_KEY_ID,
+            "aws_secret_access_key": settings.S3_STORAGE_SECRET_ACCESS_KEY,
+            "region_name": settings.S3_STORAGE_BUCKET_REGION,
+            "config": Config(signature_version="s3v4"),
+        }
+
+        client = boto3.client("s3", **connection_kwargs)
+        key_path = self.config["key_path"] + "/${filename}"
+        values_dict = client.generate_presigned_post(settings.S3_STORAGE_BUCKET_NAME, key_path)
+        values_dict["fields"].pop("key")
+        return values_dict
 
     @staticmethod
     def get_config(kind):
@@ -89,41 +54,3 @@ class S3Upload:
         config["allowed_mime_types"] = ",".join(config["allowed_mime_types"])
 
         return config
-
-    @staticmethod
-    def sign_to_bytes(key, msg):
-        if isinstance(key, str):
-            key = key.encode("utf-8")
-        return hmac.new(key, msg.encode("utf-8"), hashlib.sha256).digest()
-
-    @staticmethod
-    def sign_to_string(key, msg):
-        if isinstance(key, str):
-            key = key.encode("utf-8")
-        return hmac.new(key, msg.encode("utf-8"), hashlib.sha256).hexdigest()
-
-    @staticmethod
-    def encode_dict(dict_to_encode):
-        json_dump = json.dumps(dict_to_encode).encode("utf-8")
-        return base64.b64encode(json_dump).decode("utf-8")
-
-    @staticmethod
-    def format_date_short(date):
-        """
-        Used in the credential URL and in the signature.
-        """
-        return date.strftime("%Y%m%d")
-
-    @staticmethod
-    def format_date_long(date):
-        """
-        Used in the policy and in the "x-amz-date" form input.
-        """
-        return date.strftime("%Y%m%dT%H%M%SZ")
-
-    @staticmethod
-    def format_expiration_date(date):
-        """
-        Used in the policy.
-        """
-        return date.strftime("%Y-%m-%dT%H:%M:%S.000Z")
--- itou/utils/storage/tests.py
+++ itou/utils/storage/tests.py
@@ -1,7 +1,3 @@
-import base64
-import datetime
-import json
-
 from django.conf import settings
 from django.test import SimpleTestCase
 
@@ -9,84 +5,13 @@ from itou.utils.storage.s3 import S3Upload
 
 
 class S3Tests(SimpleTestCase):
-    def test_sign_to_bytes(self):
-        # Amazon V4 signature.
-        # See https://docs.aws.amazon.com/AmazonS3/latest/API/sigv4-authentication-HTTPPOST.html
-        result = S3Upload.sign_to_bytes(key="key", msg="Les carottes sont cuites.")
-        expected = b"\xf7\x12\xcd\x1b\x91xN\xb5\x9e\xde\xf4\x9d\xf3G\x98)"
-        expected += b"^\xecq\x03\x99\x15\xc01>\x12\xa4\xeeTy\x07\x9b"
-        self.assertTrue(isinstance(result, bytes))
-        self.assertEqual(result, expected)
-
-    def test_sign_to_string(self):
-        result = S3Upload.sign_to_string(key="key", msg="Le loup est dans la charette.")
-        expected = "bdcbaf90073d958b4b8b98cdbd22ccbbecf72eebd5c7633855d0b058ffd4c1a8"
-        self.assertTrue(isinstance(result, str))
-        self.assertEqual(result, expected)
-
-    def test_encode_dict(self):
-        # Must be base64 encoded.
-        fruits = {"exotic": ["bananas"]}
-        encoded_result = S3Upload.encode_dict(fruits)
-        decoded_result = json.loads(base64.b64decode(encoded_result))
-        self.assertTrue(isinstance(encoded_result, str))
-        self.assertEqual(encoded_result, "eyJleG90aWMiOiBbImJhbmFuYXMiXX0=")
-        self.assertEqual(fruits, decoded_result)
-
-    def test_generate_credential_url(self):
-        date = datetime.datetime(2021, 4, 29)
-        access_key_id = "A1234567890"
-        region_name = "eu-west-4"
-        with self.settings(S3_STORAGE_ACCESS_KEY_ID=access_key_id, S3_STORAGE_BUCKET_REGION=region_name):
-            s3_upload = S3Upload(date=date)
-            self.assertEqual(s3_upload.credential_url, f"{access_key_id}/20210429/{region_name}/s3/aws4_request")
-
-    def test_generate_signature(self):
-        # Not sure how to test this...
-        # hmac.compare_digest(calculated_signature, signature_to_verify)
-        pass
-
-    def test_policy_as_dict(self):
-        """
-        Test required keys only.
-        """
-        date = datetime.datetime(2021, 4, 29, 12, 13, 14, 155)
-        test_settings = {
-            "S3_STORAGE_ACCESS_KEY_ID": "A1234567890",
-            "S3_STORAGE_BUCKET_NAME": "nemos-bucket",
-            "S3_STORAGE_BUCKET_REGION": "eu-west-4",
-        }
-        with self.settings(**test_settings):
-            s3_upload = S3Upload(date=date)
-            credential_url = s3_upload.credential_url
-            result = s3_upload.policy_as_dict()
-            expected = {
-                "expiration": "2021-04-29T13:13:14.000Z",
-                "conditions": [
-                    {"bucket": test_settings["S3_STORAGE_BUCKET_NAME"]},
-                    {"x-amz-algorithm": "AWS4-HMAC-SHA256"},
-                    {"x-amz-credential": credential_url},
-                    {"x-amz-date": "20210429T121314Z"},
-                ],
-            }
-
-            for item in expected["conditions"]:
-                self.assertIn(item, result["conditions"])
-
-            # test expiration formatting
-            self.assertEqual(expected["expiration"], result["expiration"])
-
-            # test key path condition
-            expected = {"conditions": ["starts-with", "$key", ""]}
-            self.assertEqual(result["conditions"][0], expected["conditions"])
-
     def test_generate_form_values(self):
-        date = datetime.datetime(2021, 4, 29, 12, 13, 14, 155)
-        s3_upload = S3Upload(date=date)
+        s3_upload = S3Upload()
         form_values = s3_upload.form_values
-        expected_keys = ["credential_url", "date", "encoded_policy", "signature", "endpoint"]
-        self.assertEqual(sorted(form_values.keys()), sorted(expected_keys))
-        self.assertEqual(form_values["date"], "20210429T121314Z")
+        self.assertTrue(form_values.get("url"))
+        self.assertTrue(form_values.get("fields"))
+        expected_keys = ["x-amz-algorithm", "x-amz-credential", "x-amz-date", "policy", "x-amz-signature"]
+        self.assertEqual(sorted(form_values["fields"].keys()), sorted(expected_keys))
 
     def test_get_upload_config(self):
         s3_upload = S3Upload()
