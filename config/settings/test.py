094ce6020d164f1bb3aacb979746818097b7ddda
Frédéric Vergez <fred@ikarius.com>
2021-05-17 19:14:32 +0200

    Fixed test config

--- config/settings/test.py
+++ config/settings/test.py
@@ -2,8 +2,6 @@ import logging
 
 from .base import *
 
-from django.urls import path
-
 # `ManifestStaticFilesStorage` (used in base settings) requires `collectstatic` to be run.
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
