07c134eace09b7fa437abae0b17c52889d585d8f
Frédéric Vergez <fred@ikarius.com>
2021-05-20 16:05:21 +0200

    Fix hidden field bug

--- itou/www/employee_record_views/urls.py
+++ itou/www/employee_record_views/urls.py
@@ -8,4 +8,5 @@ app_name = "employee_record_views"
 urlpatterns = [
     path("list", views.list, name="list"),
     path("create/<uuid:job_application_id>", views.create, name="create"),
+    path("create_step_2/<uuid:job_application_id>", views.create_step_2, name="create_step_2"),
 ]
