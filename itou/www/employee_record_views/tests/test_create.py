76248ebce04be0620c08c75e8e5d729627d5b8aa
Frédéric Vergez <fred@ikarius.com>
2021-05-31 09:37:10 +0200

    Temp test: need rework

--- itou/www/employee_record_views/tests/test_create.py
+++ itou/www/employee_record_views/tests/test_create.py
@@ -6,7 +6,7 @@ from itou.siaes.factories import SiaeWithMembershipAndJobsFactory
 from itou.users.factories import DEFAULT_PASSWORD
 
 
-class CreateEmployeeRecordsTest(TestCase):
+class CreateEmployeeRecordStep1Test(TestCase):
     def setUp(self):
         # User must be super user for UI first part (tmp)
         self.siae = SiaeWithMembershipAndJobsFactory(
@@ -21,12 +21,11 @@ class CreateEmployeeRecordsTest(TestCase):
             to_siae=self.siae,
         )
         self.job_seeker = self.job_application.job_seeker
-        self.url = reverse("employee_record_views:list")
+        self.url = reverse("employee_record_views:create", args=(self.job_application.id))
 
     def test_access_granted(self):
         # Must not have access
         self.client.login(username=self.user.username, password=DEFAULT_PASSWORD)
-
         response = self.client.get(self.url)
 
         self.assertEqual(response.status_code, 200)
@@ -34,7 +33,18 @@ class CreateEmployeeRecordsTest(TestCase):
     def test_access_denied(self):
         # Must have access
         self.client.login(username=self.user_without_perms.username, password=DEFAULT_PASSWORD)
-
         response = self.client.get(self.url)
 
         self.assertEqual(response.status_code, 403)
+
+    def test_birth_country(self):
+        # Validation is done by the model
+        self.client.login(username=self.user_without_perms.username, password=DEFAULT_PASSWORD)
+        response = self.client.get(self.url)
+
+        # Set a random commune
+        data = {}
+
+        # Set a country different from France
+
+        #
