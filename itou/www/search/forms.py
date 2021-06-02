a3956904c8bf76165a03b56fda3ccbf58896203f
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2021-06-02 17:29:38 +0200

    Human display of kind choices

--- itou/www/search/forms.py
+++ itou/www/search/forms.py
@@ -14,7 +14,7 @@ class SiaeSearchForm(forms.Form):
     CITY_AUTOCOMPLETE_SOURCE_URL = reverse_lazy("autocomplete:cities")
 
     # We temporarily hide ACIPHC kind in search filter until our staff has input enough of them, per their request.
-    KIND_CHOICES = [(k[0], k[0]) for k in Siae.KIND_CHOICES if k[0] != Siae.KIND_ACIPHC]
+    KIND_CHOICES = [(k, f"{v} ({k})") for k, v in Siae.KIND_CHOICES if k != Siae.KIND_ACIPHC]
 
     distance = forms.ChoiceField(
         label="Distance",
