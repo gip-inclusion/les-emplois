999f9065bad76095564e833e73ca880ede7142df
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2021-06-03 12:06:39 +0200

    Extract department and districts from search results

--- itou/www/search/views.py
+++ itou/www/search/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 
 from itou.prescribers.models import PrescriberOrganization
 from itou.siaes.models import Siae
+from itou.utils.address.departments import DEPARTMENTS_WITH_DISTRICT
 from itou.utils.pagination import pager
 from itou.www.search.forms import PrescriberSearchForm, SiaeSearchForm
 
@@ -63,6 +64,16 @@ def search_siaes_results(request, template_name="search/siaes_search_results.htm
         )
         if kind:
             siaes = siaes.filter(kind=kind)
+
+        # Extract departments from results to inject them as filters
+        # The DB contains around 4k SIAE (always fast in Python and no need of iterator())
+        departments = set()
+        districts = set()
+        for siae in siaes:
+            if siae.department in DEPARTMENTS_WITH_DISTRICT:
+                districts.add(siae.post_code)
+            departments.add(siae.department)
+
         siaes_page = pager(siaes, request.GET.get("page"), items_per_page=10)
 
     context = {
