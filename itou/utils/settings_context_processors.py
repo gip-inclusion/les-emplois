e7c80f9e1b967363c173ebaa54af75b72426fd7d
Céline MS <celine@hello-birds.com>
2021-06-10 18:01:35 +0200

    Clean up

--- itou/utils/settings_context_processors.py
+++ itou/utils/settings_context_processors.py
@@ -12,7 +12,6 @@ def expose_settings(request):
         "ITOU_ASSISTANCE_URL": settings.ITOU_ASSISTANCE_URL,
         "ITOU_COMMUNITY_URL": settings.ITOU_COMMUNITY_URL,
         "ITOU_DOC_URL": settings.ITOU_DOC_URL,
-        "ITOU_EMAIL_CONTACT": settings.ITOU_EMAIL_CONTACT,
         "ITOU_ENVIRONMENT": settings.ITOU_ENVIRONMENT,
         "ITOU_FQDN": settings.ITOU_FQDN,
         "ITOU_PROTOCOL": settings.ITOU_PROTOCOL,
