0e53c9ce30cde24432e0faa5d44164391ee8fb47
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2021-06-14 18:17:49 +0200

    Add tests

---
+++ itou/www/search/tests.py
@@ -0,0 +1,61 @@
+from django.contrib.gis.geos import Point
+from django.test import TestCase
+from django.urls import reverse
+
+from itou.cities.models import City
+from itou.siaes.factories import SiaeFactory
+from itou.siaes.models import Siae
+
+
+class SearchSiaeTest(TestCase):
+    def test_home_search(self):
+        url = reverse("search:siaes_home")
+        response = self.client.get(url)
+        # The title "Prenez contact avec un employeur solidaire" isn't direclty rendered...
+        self.assertContains(response, "Rechercher des employeurs")
+
+    def test_search_not_existing(self):
+        url = reverse("search:siaes_results")
+        response = self.client.get(url, {"city": "foo-44"})
+        self.assertContains(response, "Aucun résultat.")
+
+    def test_search_paris(self):
+        city_slug = "paris-75"
+        paris_city = City.objects.create(
+            name="Paris (75)", slug=city_slug, department="75", post_codes=["75001"], coords=Point(5, 23)
+        )
+        siae_1 = SiaeFactory(department="75", coords=paris_city.coords, post_code="75001")
+        SiaeFactory(department="75", coords=paris_city.coords, post_code="75002")
+
+        url = reverse("search:siaes_results")
+
+        # Filter on city
+        response = self.client.get(url, {"city": city_slug})
+
+        self.assertContains(response, "Employeurs solidaires à 25 km du centre de Paris (75)")
+        self.assertContains(response, "<b>2</b> résultats")
+        self.assertContains(response, "Arrondissements de Paris")
+
+        # Filter on district
+        response = self.client.get(url, {"city": city_slug, "districts_75": ["75001"]})
+        self.assertContains(response, "<b>1</b> résultat")
+        self.assertContains(response, siae_1.display_name)
+
+    def test_search_kind(self):
+        url = reverse("search:siaes_results")
+
+        city_slug = "saint-andre-des-eaux-44"
+        city = City.objects.create(
+            name="Saint-André-des-Eaux (75)",
+            slug=city_slug,
+            department="44",
+            post_codes=["44117"],
+            coords=Point(5, 23),
+        )
+        SiaeFactory(department="44", coords=city.coords, post_code="44117", kind=Siae.KIND_AI)
+
+        response = self.client.get(url, {"city": city_slug, "kinds": [Siae.KIND_AI]})
+        self.assertContains(response, "<b>1</b> résultat")
+
+        response = self.client.get(url, {"city": city_slug, "kinds": [Siae.KIND_EI]})
+        self.assertContains(response, "Aucun résultat")
