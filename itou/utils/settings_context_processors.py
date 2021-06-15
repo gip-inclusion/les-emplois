4f55f9b20a1301be7713e90d43899e3ff99bf54e
kemar <marc@marcarea.com>
2021-06-15 11:59:07 +0200

    Change text of the prolongation's notification sent to the authorized prescriber

--- itou/utils/settings_context_processors.py
+++ itou/utils/settings_context_processors.py
@@ -12,6 +12,7 @@ def expose_settings(request):
         "ITOU_ASSISTANCE_URL": settings.ITOU_ASSISTANCE_URL,
         "ITOU_COMMUNITY_URL": settings.ITOU_COMMUNITY_URL,
         "ITOU_DOC_URL": settings.ITOU_DOC_URL,
+        "ITOU_EMAIL_PROLONGATION": settings.ITOU_EMAIL_PROLONGATION,
         "ITOU_ENVIRONMENT": settings.ITOU_ENVIRONMENT,
         "ITOU_FQDN": settings.ITOU_FQDN,
         "ITOU_PROTOCOL": settings.ITOU_PROTOCOL,
