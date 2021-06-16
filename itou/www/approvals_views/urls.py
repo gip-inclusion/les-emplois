fcd524e145b02b2a7dd3d68c92451e11d4055253
clemkeirua <clement@keiruaprod.fr>
2021-06-16 12:19:33 +0200

    added 3 urls for importing a PE approval
     - searching for a PE approval by number
     - searching for a user by email
     - converting a PoleEmploiApproval into an Approval and JobApplication based on
    these data

--- itou/www/approvals_views/urls.py
+++ itou/www/approvals_views/urls.py
@@ -12,4 +12,11 @@ urlpatterns = [
     path("suspend/<int:approval_id>", views.suspend, name="suspend"),
     path("suspension/<int:suspension_id>/edit", views.suspension_update, name="suspension_update"),
     path("suspension/<int:suspension_id>/delete", views.suspension_delete, name="suspension_delete"),
+    path("search-pe-approval/", views.search_pe_approval, name="search_pe_approval"),
+    path("search-user/<int:pe_approval_id>", views.search_user, name="search_user"),
+    path(
+        "complete-pe-approval/<int:pe_approval_id>",
+        views.create_approval_from_pe_approval,
+        name="create_approval_from_pe_approval",
+    ),
 ]
