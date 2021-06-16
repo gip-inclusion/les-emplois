8ed6c987d4b0223172f2040352950b3e8c64c702
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2021-06-16 10:55:55 +0200

    Test filtering on distance and department

--- itou/www/search/tests.py
+++ itou/www/search/tests.py
@@ -11,11 +11,11 @@ class SearchSiaeTest(TestCase):
     def setUp(self):
         self.url = reverse("search:siaes_results")
 
-    def test_search_not_existing(self):
+    def test_not_existing(self):
         response = self.client.get(self.url, {"city": "foo-44"})
         self.assertContains(response, "Aucun résultat.")
 
-    def test_search_paris(self):
+    def test_district(self):
         city_slug = "paris-75"
         paris_city = City.objects.create(
             name="Paris (75)", slug=city_slug, department="75", post_codes=["75001"], coords=Point(5, 23)
@@ -35,10 +35,10 @@ class SearchSiaeTest(TestCase):
         self.assertContains(response, "<b>1</b> résultat")
         self.assertContains(response, siae_1.display_name)
 
-    def test_search_kind(self):
+    def test_kind(self):
         city_slug = "saint-andre-des-eaux-44"
         city = City.objects.create(
-            name="Saint-André-des-Eaux (75)",
+            name="Saint-André-des-Eaux (44)",
             slug=city_slug,
             department="44",
             post_codes=["44117"],
@@ -51,3 +51,65 @@ class SearchSiaeTest(TestCase):
 
         response = self.client.get(self.url, {"city": city_slug, "kinds": [Siae.KIND_EI]})
         self.assertContains(response, "Aucun résultat")
+
+    def test_distance(self):
+        # 3 SIAEs in two departments to test distance and department filtering
+        vannes = City.objects.create(
+            name="Vannes (56)",
+            slug="vannes-56",
+            department="56",
+            coords=Point(-2.8186843, 47.657641),
+            # Dummy
+            post_codes=["0001"],
+            code_insee="0001",
+        )
+        SIAE_VANNES = "SIAE Vannes"
+        SiaeFactory(name=SIAE_VANNES, department="56", coords=vannes.coords, post_code="56760", kind=Siae.KIND_AI)
+        guerande = City.objects.create(
+            name="Guérande (44)",
+            slug="guerande-44",
+            department="44",
+            coords=Point(-2.4747713, 47.3358576),
+            # Dummy
+            post_codes=["0002"],
+            code_insee="0002",
+        )
+        SIAE_GUERANDE = "SIAE Guérande"
+        SiaeFactory(name=SIAE_GUERANDE, department="44", coords=guerande.coords, post_code="44350", kind=Siae.KIND_AI)
+        saint_andre = City.objects.create(
+            name="Saint-André-des-Eaux (44)",
+            slug="saint-andre-des-eaux-44",
+            department="44",
+            coords=Point(-2.3140436, 47.3618584),
+            # Dummy
+            post_codes=["0003"],
+            code_insee="0003",
+        )
+        SIAE_SAINT_ANDRE = "SIAE Saint André des Eaux"
+        SiaeFactory(
+            name=SIAE_SAINT_ANDRE, department="44", coords=saint_andre.coords, post_code="44117", kind=Siae.KIND_AI
+        )
+
+        # 100 km
+        response = self.client.get(self.url, {"city": guerande.slug, "distance": 100})
+        self.assertContains(response, "<b>3</b> résultats")
+        self.assertContains(response, SIAE_VANNES.capitalize())
+        self.assertContains(response, SIAE_GUERANDE.capitalize())
+        self.assertContains(response, SIAE_SAINT_ANDRE.capitalize())
+
+        # 15 km
+        response = self.client.get(self.url, {"city": guerande.slug, "distance": 15})
+        self.assertContains(response, "<b>2</b> résultats")
+        self.assertContains(response, SIAE_GUERANDE.capitalize())
+        self.assertContains(response, SIAE_SAINT_ANDRE.capitalize())
+
+        # 100 km and 44
+        response = self.client.get(self.url, {"city": guerande.slug, "distance": 100, "departments": ["44"]})
+        self.assertContains(response, "<b>2</b> résultats")
+        self.assertContains(response, SIAE_GUERANDE.capitalize())
+        self.assertContains(response, SIAE_SAINT_ANDRE.capitalize())
+
+        # 100 km and 56
+        response = self.client.get(self.url, {"city": vannes.slug, "distance": 100, "departments": ["56"]})
+        self.assertContains(response, "<b>1</b> résultat")
+        self.assertContains(response, SIAE_VANNES.capitalize())
