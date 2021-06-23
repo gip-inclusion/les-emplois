2a3fb907179c35bb0382ad60449dbb32c5b0376d
kemar <marc@marcarea.com>
2021-06-23 15:59:12 +0200

    Use a custom data migration to link job applications and eligibility diagnoses

---
+++ itou/job_applications/migrations/0035_link_diagnoses.py
@@ -0,0 +1,83 @@
+from django.db import migrations
+
+# Use imports (application defined models) instead of `apps.get_model()`
+# (migration defined models). This can bite us later e.g. when running
+# tests in a fresh database that runs all migrations again and again).
+# However, we really need access to Managers, QuerySets and properties.
+from itou.eligibility.models import EligibilityDiagnosis
+from itou.job_applications.models import JobApplication
+
+
+def move_data_forward(apps, schema_editor):
+    """
+    Link eligibility diagnoses to job applications.
+    """
+
+    # I'm still asking myself if a data migration is really the best way.
+    print("-" * 80)
+    print("This data migration will take a bunch of minutes on a production database.")
+
+    job_applications_qs = (
+        JobApplication.objects.filter(state="accepted")
+        .select_related("approval", "job_seeker", "to_siae")
+        .iterator(chunk_size=500)
+    )
+
+    for job_application in job_applications_qs:
+
+        if not job_application.to_siae.is_subject_to_eligibility_rules:
+            continue
+
+        before = job_application.updated_at
+        job_seeker = job_application.job_seeker
+
+        qs = (
+            EligibilityDiagnosis.objects.for_job_seeker(job_seeker)
+            .filter(created_at__lte=before)
+            .order_by("created_at")
+        )
+
+        # A diagnosis made by a prescriber has priority.
+        eligibility_diagnosis = qs.by_author_kind_prescriber().last()
+
+        if not eligibility_diagnosis:
+            # Otherwise, use a diagnosis made by the SIAE ("auto-prescription").
+            eligibility_diagnosis = qs.authored_by_siae(job_application.to_siae).last()
+
+        if not eligibility_diagnosis:
+            # Deals with cases from the past (when there was no rules).
+            eligibility_diagnosis = qs.last()
+
+        if not eligibility_diagnosis:
+
+            if not job_application.approval and job_application.hiring_without_approval:
+                # There are many applications with `hiring_without_approval=True` for which
+                # the user has no diagnosis.
+                # This is strange because the diagnosis is always made before an application
+                # is accepted.
+                # Perhaps at some point the SIAEs that did not want a PASS IAE did not have
+                # to make a diagnosis…
+                # We live with it for now.
+                continue
+
+            if not job_application.approval:
+                print(f"{job_application.pk} - No diagnosis and no approval")
+                continue
+
+            if job_application.approval.originates_from_itou:
+                print(f"{job_application.pk} - No diagnosis - {job_application.approval.number}")
+                continue
+
+        job_application.eligibility_diagnosis = eligibility_diagnosis
+        job_application.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("eligibility", "0005_auto_20200913_0532"),
+        ("job_applications", "0034_jobapplication_eligibility_diagnosis"),
+        ("users", "0025_create_index_upper_email"),
+    ]
+
+    operations = [migrations.RunPython(move_data_forward, migrations.RunPython.noop)]
