f53d53c59c66c70558e8af0698289e49615df249
clemkeirua <clement@keiruaprod.fr>
2021-06-24 14:54:09 +0200

    the deploy_prod command goes back to its initial state when deployment is over

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+initial_branch=`git branch --show-current`
+
 # Ensure working directory is clean
 if ! git diff-index --quiet HEAD; then
   echo "Working directory not clean, please commit your changes first"
@@ -15,3 +17,16 @@ git checkout master_clever
 git pull origin master_clever 
 git merge master --no-edit
 git push origin master_clever 
+
+# When we are done, we want to restore the initial state
+# (in order to avoid writing things directly on master_clever by accident)
+if [ -z $initial_branch ]; then
+    # The initial_branch is empty when user is in detached state, so we simply go back to master
+    git checkout master
+    echo
+    echo "You were on detached state before deploying, you are back to master"
+else
+    git checkout $initial_branch
+    echo
+    echo "Back to $initial_branch"
+fi
