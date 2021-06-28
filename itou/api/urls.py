3e1e9c87c6e08929bf42f0929b1e59d17d4cf403
Frédéric Vergez <fred@ikarius.com>
2021-06-28 18:27:03 +0200

    Added authentication / SIAE memberships filter

--- itou/api/urls.py
+++ itou/api/urls.py
@@ -10,7 +10,7 @@ from .views import DummyEmployeeRecordList
 
 # Using DRF router means automatic definition for list or retrieve actions
 router = routers.DefaultRouter()
-router.register(r"employee-records", EmployeeRecordViewSet)
+router.register(r"employee-records", EmployeeRecordViewSet, basename="employee-records")
 
 # https://docs.djangoproject.com/en/dev/topics/http/urls/#url-namespaces-and-included-urlconfs
 app_name = "itou.api"
@@ -18,6 +18,7 @@ app_name = "itou.api"
 urlpatterns = [
     # TokenAuthentication endpoint to get token from login/password.
     path("token-auth/", auth_views.obtain_auth_token, name="token-auth"),
+    # Demo / dummy endpoint
     path("dummy-employee-records/", DummyEmployeeRecordList.as_view(), name="dummy-employee-records"),
     # OpenAPI / Swagger
     # See: https://www.django-rest-framework.org/topics/documenting-your-api/
