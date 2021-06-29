cec76c1e61ede788decd9131b9980da8bf8c5b4b
Frédéric Vergez <fred@ikarius.com>
2021-06-29 18:06:46 +0200

    Tuned default authentication and permissions for DRFi, added browseable interface

--- itou/api/employee_record_api/viewsets.py
+++ itou/api/employee_record_api/viewsets.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from rest_framework.authentication import TokenAuthentication
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 
 from itou.employee_record.models import EmployeeRecord
 from itou.employee_record.serializers import EmployeeRecordSerializer
@@ -8,24 +8,37 @@ from itou.siaes.models import SiaeMembership
 
 class EmployeeRecordViewSet(viewsets.ReadOnlyModelViewSet):
     """
-    Basic viewset for employee records:
-    - list
-    - detail
-    - read-only
+    # Fiches salarié
+
+    Cette API retourne la liste des fiches salarié :
+    * dans l'état `PROCESSED`
+    * pour toutes les embauches / candidatures des SIAE liées au token d'identification
+
+    Il est également possible d'obtenir le détail d'une fiche salarié par son
+    identifiant (dans les mêmes conditions d'autorisation que pour la liste complète)
+
+    L'utilisation externe de cette API nécessite l'utilisation d'un token d'autorisation
+    (voir le endpoint `auth-token`).
     """
 
-    # If no queryset class paramter is given (overiding f.i.)
-    # a `basename` parameter must be set on the router
+    # If no queryset class parameter is given (f.i. overidding)
+    # a `basename` parameter must be set on the router (see local `urls.py` file)
     # See: https://www.django-rest-framework.org/api-guide/routers/
 
     serializer_class = EmployeeRecordSerializer
-    # authentication_classes = [TokenAuthentication]
+    # Possible authentication frameworks:
+    # - token auth: for external access / real world use case
+    # - session auth: for dev context
+    authentication_classes = [TokenAuthentication, SessionAuthentication]
 
     def get_queryset(self):
         queryset = EmployeeRecord.objects.full_fetch().sent()
 
-        # employee record API will return objects related to
-        # all SIAE memberships of authenticated user
+        # Employee record API will return objects related to
+        # all SIAE memberships of authenticated user.
+        # There's something similar in context processors, but:
+        # - ctx processors are called AFTER this
+        # - and only when rendering a template
         memberships = SiaeMembership.objects.filter(user=self.request.user).values("siae")
 
-        return queryset.filter(job_application__to_siae__in=memberships)
+        return queryset.filter(job_application__to_siae__in=memberships).order_by("-created_at")
