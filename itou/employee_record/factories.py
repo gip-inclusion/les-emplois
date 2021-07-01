215d6cd3cdf33647b632a583848e5074d228fdab
Frédéric Vergez <fred@ikarius.com>
2021-07-01 13:04:06 +0200

    Split API permissions and fetch tests

--- itou/employee_record/factories.py
+++ itou/employee_record/factories.py
@@ -3,10 +3,19 @@ import string
 import factory
 
 from itou.employee_record.models import EmployeeRecord
-from itou.job_applications.factories import JobApplicationWithApprovalNotCancellableFactory
+from itou.job_applications.factories import (
+    JobApplicationWithApprovalNotCancellableFactory,
+    JobApplicationWithCompleteJobSeekerProfileFactory,
+)
 
 
 class EmployeeRecordFactory(factory.django.DjangoModelFactory):
+    """
+    "Basic" employee record factory:
+    At the first stage of its lifecycle (NEW)
+    (no job seeker profile linked => not updatable)
+    """
+
     class Meta:
         model = EmployeeRecord
 
@@ -14,3 +23,11 @@ class EmployeeRecordFactory(factory.django.DjangoModelFactory):
 
     asp_id = factory.fuzzy.FuzzyText(length=7, chars=string.digits)
     approval_number = factory.fuzzy.FuzzyText(length=7, chars=string.digits, prefix="99999")
+
+
+class EmployeeRecordWithProfileFactory(EmployeeRecordFactory):
+    """
+    Employee record with a complete job seeker profile
+    """
+
+    job_application = factory.SubFactory(JobApplicationWithCompleteJobSeekerProfileFactory)
