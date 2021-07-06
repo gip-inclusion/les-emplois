235db78d4bc1d232375ebb65b894d650ef265c27
Céline MS <celine@hello-birds.com>
2021-07-06 16:40:15 +0200

    Test Institution active and admin members

---
+++ itou/institutions/factories.py
@@ -0,0 +1,58 @@
+import string
+
+import factory
+import factory.fuzzy
+
+from itou.institutions import models
+
+# from itou.users.factories import ControlerFactory # TODO
+from itou.users.factories import UserFactory
+
+
+class InstitutionFactory(factory.django.DjangoModelFactory):
+    """Returns an Institution() object."""
+
+    class Meta:
+        model = models.Institution
+
+    name = factory.Faker("name", locale="fr_FR")
+    kind = models.Institution.Kind.DDEETS
+
+
+class InstitutionMembershipFactory(factory.django.DjangoModelFactory):
+    """
+    Returns a InstitutionMembership() object with:
+    - its related Institution()
+    - its related User()
+
+    https://factoryboy.readthedocs.io/en/latest/recipes.html#many-to-many-relation-with-a-through
+    """
+
+    class Meta:
+        model = models.InstitutionMembership
+
+    user = factory.SubFactory(UserFactory)
+    institution = factory.SubFactory(InstitutionFactory)
+    is_admin = True
+    is_active = True
+
+
+class InstitutionWithMembershipFactory(InstitutionFactory):
+    """
+    Returns a Institution() object with a related InstitutionMembership() object.
+
+    https://factoryboy.readthedocs.io/en/latest/recipes.html#many-to-many-relation-with-a-through
+    """
+
+    membership = factory.RelatedFactory(InstitutionMembershipFactory, "institution")
+
+
+class InstitutionWith2MembershipFactory(InstitutionFactory):
+    """
+    Returns a Institution() object with 2 InstitutionMembership() objects.
+
+    https://factoryboy.readthedocs.io/en/latest/recipes.html#many-to-many-relation-with-a-through
+    """
+
+    membership1 = factory.RelatedFactory(InstitutionMembershipFactory, "institution")
+    membership2 = factory.RelatedFactory(InstitutionMembershipFactory, "institution", is_admin=False)
--- itou/institutions/tests.py
+++ itou/institutions/tests.py
@@ -1,4 +1,36 @@
 from django.test import TestCase
 
+from itou.institutions.factories import InstitutionWith2MembershipFactory, InstitutionWithMembershipFactory
 
-# Create your tests here.
+
+class InstitutionModelTest(TestCase):
+    def test_active_admin_members(self):
+        """
+        Test that if a user is admin of org1 and regular user
+        of org2 it does not get considered as admin of org2.
+        # Same as itou.prescribers.tests.PrescriberOrganizationModelTest.test_active_admin_members
+        """
+        institution1 = InstitutionWithMembershipFactory()
+        institution1_admin_user = institution1.active_admin_members.get()
+        institution2 = InstitutionWithMembershipFactory()
+        institution2.members.add(institution1_admin_user)
+
+        self.assertEqual(institution1.members.count(), 1)
+        self.assertEqual(institution1.active_members.count(), 1)
+        self.assertEqual(institution1.active_admin_members.count(), 1)
+
+        self.assertEqual(institution2.members.count(), 2)
+        self.assertEqual(institution2.active_members.count(), 2)
+        self.assertEqual(institution2.active_admin_members.count(), 1)
+
+    def test_active_member_with_many_memberships(self):
+        institution1 = InstitutionWith2MembershipFactory(membership2__is_active=False)
+        user = institution1.members.filter(institutionmembership__is_admin=False).first()
+        institution2 = InstitutionWith2MembershipFactory()
+        institution2.members.add(user)
+
+        self.assertFalse(user in institution1.active_members)
+        self.assertEqual(institution1.members.count(), 2)
+        self.assertEqual(institution1.active_members.count(), 1)
+        self.assertEqual(institution2.members.count(), 3)
+        self.assertEqual(institution2.active_members.count(), 3)
