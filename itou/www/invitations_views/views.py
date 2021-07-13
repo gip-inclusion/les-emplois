f3dac166e7eece630fa478408987f92cf727371e
Céline MS <celine@hello-birds.com>
2021-07-13 11:56:43 +0200

    Fix forgotten session key in Prescriber invitations

--- itou/www/invitations_views/views.py
+++ itou/www/invitations_views/views.py
@@ -122,6 +122,7 @@ def join_prescriber_organization(request, invitation_id):
     else:
         messages.error(request, "Cette invitation n'est plus valide.")
 
+    request.session[settings.ITOU_SESSION_CURRENT_PRESCRIBER_ORG_KEY] = invitation.organization.pk
     return redirect("dashboard:index")
 
 
