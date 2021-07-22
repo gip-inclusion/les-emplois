162a0e1c29a88a832e6efa328e22d0402cea26f8
Céline MS <celine@hello-birds.com>
2021-07-22 15:57:49 +0200

    Create and delete review apps DB addons

--- clevercloud/demo-after-success.sh
+++ clevercloud/demo-after-success.sh
@@ -4,25 +4,6 @@
 ########################## Demo entrypoint ########################
 ###################################################################
 
-# ///////////////////////////// ! \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-# The demo uses the same CC instance as review apps but with
-# its own database.
-
-# -----------------------------------------------------------------
-# -------------------- A note on the Database ---------------------
-# -----------------------------------------------------------------
-# Review apps share the same Clever Cloud Postgresql add-on,
-# review_apps_databases, creating a new database for each one.
-# This is a workaround and may change in the future when
-# it will be possible to pay add-ons when we use it,(per usage),
-# and not in advance (per month).
-
-# The command CREATE DATABASE works by copying an existing database.
-# By default, it copies the standard system database named template1.
-# cf https://www.postgresql.org/docs/current/manage-ag-templatedbs.html
-
-# -----------------------------------------------------------------
-
 echo "Loading cities"
 PGPASSWORD=$POSTGRESQL_ADDON_PASSWORD pg_restore -d $DEMO_APP_DB_NAME -h $POSTGRESQL_ADDON_HOST -p $POSTGRESQL_ADDON_PORT -U $POSTGRESQL_ADDON_USER --if-exists --clean --no-owner --no-privileges $APP_HOME/itou/fixtures/postgres/cities.sql
 
