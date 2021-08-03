58639835cd5f1becff0df97f94b4c6fa0ca57739
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2021-08-03 12:41:07 +0200

    Don't define fields of instance outside __init__

--- itou/metabase/management/commands/_database_psycopg2.py
+++ itou/metabase/management/commands/_database_psycopg2.py
@@ -3,17 +3,21 @@ from django.conf import settings
 
 
 class MetabaseDatabaseCursor:
+    def __init__(self):
+        self.cursor = None
+        self.connection = None
+
     def __enter__(self):
-        self.conn = psycopg2.connect(
+        self.connection = psycopg2.connect(
             host=settings.METABASE_HOST,
             port=settings.METABASE_PORT,
             dbname=settings.METABASE_DATABASE,
             user=settings.METABASE_USER,
             password=settings.METABASE_PASSWORD,
         )
-        self.cur = self.conn.cursor()
-        return self.cur, self.conn
+        self.cursor = self.connection.cursor()
+        return self.cursor, self.connection
 
     def __exit__(self, exc_type, exc_value, exc_traceback):
-        self.cur.close()
-        self.conn.close()
+        self.cursor.close()
+        self.connection.close()
