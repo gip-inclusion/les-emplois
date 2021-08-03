8c033dc15545d4a6cfd3eb11f4255b2ba409ee81
Céline MS <celine@hello-birds.com>
2021-08-03 18:05:35 +0200

    Refactor member methods into a parent class

--- itou/institutions/models.py
+++ itou/institutions/models.py
@@ -10,16 +10,16 @@ They belong to a DDETS, a DREETS or a DGEFP.
 """
 
 from django.db import models
-from django.db.models import Prefetch
-from django.utils import timezone
 
 from itou.users.models import User
-from itou.utils.address.models import AddressMixin
-from itou.utils.emails import get_email_message
-from itou.utils.structures.models import MembershipAbstract, StructureQuerySet
+from itou.utils.structures.models import MembershipAbstract, Structure, StructureQuerySet
 
 
-class Institution(AddressMixin):
+class InstitutionQuerySet(StructureQuerySet):
+    pass
+
+
+class Institution(Structure):
     class Kind(models.TextChoices):
         DDETS = ("DDETS", "Direction départementale de l'emploi, du travail et des solidarités")
         DREETS = ("DREETS", "Direction régionale de l'économie, de l'emploi, du travail et des solidarités")
@@ -31,7 +31,6 @@ class Institution(AddressMixin):
         verbose_name_plural = "Institutions partenaires"
 
     kind = models.CharField(verbose_name="Type", max_length=20, choices=Kind.choices, default=Kind.OTHER)
-    name = models.CharField(verbose_name="Nom", max_length=255)
     members = models.ManyToManyField(
         User,
         verbose_name="Membres",
@@ -39,99 +38,8 @@ class Institution(AddressMixin):
         blank=True,
         through_fields=("institution", "user"),
     )
-    created_at = models.DateTimeField(verbose_name="Date de création", default=timezone.now)
-    updated_at = models.DateTimeField(verbose_name="Date de modification", blank=True, null=True)
 
-    objects = models.Manager.from_queryset(StructureQuerySet)()
-
-    def __str__(self):
-        return f"{self.name}"
-
-    def save(self, *args, **kwargs):
-        if self.pk:
-            self.updated_at = timezone.now()
-        return super().save(*args, **kwargs)
-
-    def has_admin(self, user):
-        return self.active_admin_members.filter(pk=user.pk).exists()
-
-    @property
-    def has_members(self):
-        return self.active_members.exists()
-
-    @property
-    def display_name(self):
-        # Keep same logic as SIAE and PrescriberOrganization.
-        return self.name
-
-    @property
-    def active_members(self):
-        """
-        In this context, active == has an active membership AND user is still active.
-
-        Query will be optimized later with Qs.
-        """
-        return self.members.filter(is_active=True, institutionmembership__is_active=True)
-
-    @property
-    def active_admin_members(self):
-        """
-        Active admin members:
-        active user/admin in this context means both:
-        * user.is_active: user is able to do something on the platform
-        * user.membership.is_active: is a member of this structure
-
-        Query will be optimized later with Qs.
-        """
-        return self.members.filter(
-            is_active=True, institutionmembership__is_admin=True, institutionmembership__is_active=True
-        )
-
-    @property
-    def deactivated_members(self):
-        """
-        List of previous members of the structure, still active as user (from the model POV)
-        but deactivated by an admin at some point in time.
-
-        Query will be optimized later with Qs.
-        """
-        return self.members.filter(is_active=True, institutionmembership__is_active=False)
-
-    # E-mails
-    def member_deactivation_email(self, user):
-        """
-        Send email when an admin of the structure disables the membership of a given user (deactivation).
-        """
-        to = [user.email]
-        context = {"structure": self}
-        subject = "common/emails/member_deactivation_email_subject.txt"
-        body = "common/emails/member_deactivation_email_body.txt"
-        return get_email_message(to, context, subject, body)
-
-    def add_admin_email(self, user):
-        """
-        Send info email to a new admin of the organization (added)
-        """
-        to = [user.email]
-        context = {"structure": self}
-        subject = "common/emails/add_admin_email_subject.txt"
-        body = "common/emails/add_admin_email_body.txt"
-        return get_email_message(to, context, subject, body)
-
-    def remove_admin_email(self, user):
-        """
-        Send info email to a former admin of the organization (removed)
-        """
-        to = [user.email]
-        context = {"structure": self}
-        subject = "common/emails/remove_admin_email_subject.txt"
-        body = "common/emails/remove_admin_email_body.txt"
-        return get_email_message(to, context, subject, body)
-
-
-class InstitutionMembershipQuerySet(models.QuerySet):
-    def active(self):
-        return self.filter(is_active=True, user__is_active=True)
+    objects = models.Manager.from_queryset(InstitutionQuerySet)()
 
 
 class InstitutionMembership(MembershipAbstract):
