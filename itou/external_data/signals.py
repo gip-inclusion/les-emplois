cfbb9d75d18d4f019218d43ad0f0d8a4cf1c77f7
clemkeirua <clement@keiruaprod.fr>
2021-08-04 16:30:26 +0200

    added logging of the rejected events + tests

--- itou/external_data/signals.py
+++ itou/external_data/signals.py
@@ -4,7 +4,7 @@ from django.dispatch import receiver
 
 from itou.allauth_adapters.peamu.provider import PEAMUProvider
 
-from .models import ExternalDataImport
+from .models import ExternalDataImport, RejectedEmailEventData
 from .tasks import huey_import_user_pe_data
 
 
@@ -26,25 +26,35 @@ def import_user_pe_data_on_peamu_login(sender, **kwargs):
             huey_import_user_pe_data(user, str(login.token), latest_pe_data_import)
 
 
+def store_rejected_email_event(event):
+    # Anymail understands Mailjet's webhook for us and translates its results
+    # https://anymail.readthedocs.io/en/stable/sending/tracking/#normalized-tracking-event
+    if event.event_type == "rejected":
+        # the email adress we attempted to send a message to
+        recipient = event.recipient
+        # The reason for rejection, can be one of:
+        # 'invalid': bad email address format.
+        # 'bounced': bounced recipient. (In a 'rejected' event, indicates the recipient
+        # is on your ESP’s prior-bounces suppression list.)
+        # 'timed_out': your ESP is giving up after repeated transient delivery failures
+        # (which may have shown up as 'deferred' events).
+        # 'blocked': your ESP’s policy prohibits this recipient.
+        # 'spam': the receiving MTA or recipient determined the message is spam.
+        # (In a 'rejected' event, indicates the recipient is on your ESP’s prior-spam-complaints suppression list.)
+        # 'unsubscribed': the recipient is in your ESP’s unsubscribed suppression list.
+        # 'other': some other reject reason; examine the raw esp_event.
+        reason = event.reject_reason
+        rejected_email = RejectedEmailEventData(recipient=recipient, reason=reason)
+        rejected_email.save()
+
+
 @receiver(user_logged_in)
 def user_logged_in_receiver(sender, **kwargs):
     # Wrapper required to mock the db_task of Huey in unit tests
     import_user_pe_data_on_peamu_login(sender, **kwargs)
 
 
-@receiver(tracking)  # add weak=False if inside some other function/class
-def handle_blocked_emails(sender, event, esp_name, **kwargs):
-    # Anymail understands Mailjet's webhook for us and translates its results
-    # https://anymail.readthedocs.io/en/stable/sending/tracking/#normalized-tracking-event
-    if event.event_type == 'rejected':
-        # the email adress we attempted to send a message to
-        recipient = event.recipient
-        # The reason for rejection, can be one of:
-        # 'invalid': bad email address format.
-        # 'bounced': bounced recipient. (In a 'rejected' event, indicates the recipient is on your ESP’s prior-bounces suppression list.)
-        # 'timed_out': your ESP is giving up after repeated transient delivery failures (which may have shown up as 'deferred' events).
-        # 'blocked': your ESP’s policy prohibits this recipient.
-        # 'spam': the receiving MTA or recipient determined the message is spam. (In a 'rejected' event, indicates the recipient is on your ESP’s prior-spam-complaints suppression list.)
-        # 'unsubscribed': the recipient is in your ESP’s unsubscribed suppression list.
-        # 'other': some other reject reason; examine the raw esp_event.
-        reason = event.reject_reason
+@receiver(tracking)
+def handle_email_webhooks(sender, event, esp_name, **kwargs):
+    # Wrapper in order to decouple the signal and its management
+    store_rejected_email_event(event)
