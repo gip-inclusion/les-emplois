63a0e7ea9de38e2b062eb74e8d84018225c1d5e9
Frédéric Vergez <fred@ikarius.com>
2021-08-05 15:26:15 +0200

    Add condition for running employee record CRON job

--- clevercloud/sync_employee_records.sh
+++ clevercloud/sync_employee_records.sh
@@ -2,10 +2,13 @@
 
 # Fetch and upload files to ASP SFTP server
 
-#
+# Do not run if this env var is not set:
+if [[ -z "$EMPLOYEE_RECORD_CRON_ENABLED" ]]; then
+    exit 0
+fi
+
 # About clever cloud cronjobs:
 # https://www.clever-cloud.com/doc/tools/crons/
-#
 
 if [[ "$INSTANCE_NUMBER" != "0" ]]; then
     echo "Instance number is ${INSTANCE_NUMBER}. Stop here."
