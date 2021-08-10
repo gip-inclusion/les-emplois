851c8fe50cc0c4a628d26e53b14e07d40b9ed08f
Céline MS <celine@hello-birds.com>
2021-08-10 17:19:50 +0200

    Skip fixtures when redeploying

--- clevercloud/review-app-after-success.sh
+++ clevercloud/review-app-after-success.sh
@@ -4,6 +4,12 @@
 ###################### Review apps entrypoint #####################
 ###################################################################
 
+# Skip this step when redeploying a review app.
+if [ "$SKIP_FIXTURES" = true ] ; then
+    echo "Skipping fixtures."
+    exit
+fi
+
 echo "Loading cities"
 PGPASSWORD=$POSTGRESQL_ADDON_PASSWORD pg_restore -d $POSTGRESQL_ADDON_DB -h $POSTGRESQL_ADDON_HOST -p $POSTGRESQL_ADDON_PORT -U $POSTGRESQL_ADDON_USER --if-exists --clean --no-owner --no-privileges $APP_HOME/itou/fixtures/postgres/cities.sql
 
