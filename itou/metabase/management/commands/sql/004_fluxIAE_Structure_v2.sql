f0669572b05e60dc4e43b89fc7bdc5a55c9c3551
SoumiaAHLOUCH <79250336+SoumiaAHLOUCH@users.noreply.github.com>
2021-08-20 10:57:15 +0200

    Update 004_fluxIAE_Structure_v2.sql

--- itou/metabase/management/commands/sql/004_fluxIAE_Structure_v2.sql
+++ itou/metabase/management/commands/sql/004_fluxIAE_Structure_v2.sql
@@ -4,25 +4,25 @@ L'objectif est de rajouter le nom du département et la région de la structure 
   
 */
 
-with structure_ASP as (
-     select 
-       /* l'ASP préconise l'utilisation de l'adresse administrative pour récupérer la commune de la structure */
-	   case
-           when  trim (substr (trim (structure.structure_adresse_admin_code_insee), 1,char_length (structure.structure_adresse_admin_code_insee)-2)) like  ('%97%') 
-           then  trim (substr (trim (structure.structure_adresse_admin_code_insee), 1,char_length (structure.structure_adresse_admin_code_insee)-2)) 
-           when  trim (substr (trim (structure.structure_adresse_admin_code_insee), 1,char_length (structure.structure_adresse_admin_code_insee)-2)) like  ('%98%') 
-           then  trim (substr (trim (structure.structure_adresse_admin_code_insee), 1,char_length (structure.structure_adresse_admin_code_insee)-2)) 
-           else  trim (substr (trim (structure.structure_adresse_admin_code_insee), 1,char_length (structure.structure_adresse_admin_code_insee)-3))
+with structure_asp as (
+    select 
+        /* l'ASP préconise l'utilisation de l'adresse administrative pour récupérer la commune de la structure */
+	case
+            when  trim (substr (trim (structure.structure_adresse_admin_code_insee), 1,char_length (structure.structure_adresse_admin_code_insee)-2)) like  ('%97%') 
+            then  trim (substr (trim (structure.structure_adresse_admin_code_insee), 1,char_length (structure.structure_adresse_admin_code_insee)-2)) 
+            when  trim (substr (trim (structure.structure_adresse_admin_code_insee), 1,char_length (structure.structure_adresse_admin_code_insee)-2)) like  ('%98%') 
+            then  trim (substr (trim (structure.structure_adresse_admin_code_insee), 1,char_length (structure.structure_adresse_admin_code_insee)-2)) 
+            else  trim (substr (trim (structure.structure_adresse_admin_code_insee), 1,char_length (structure.structure_adresse_admin_code_insee)-3))
         end code_departement,
         * 
-     from 
-         "fluxIAE_Structure" as structure
+    from 
+        "fluxIAE_Structure" as structure
 )
 select 
-      dept_structure.nom_departement as nom_departement_structure,
-      dept_structure.nom_region as nom_region_structure,
-      structure.*
+    dept_structure.nom_departement as nom_departement_structure,
+    dept_structure.nom_region as nom_region_structure,
+    structure.*
 from 
-     structure_ASP structure
-     left join "departements" dept_structure
-           on dept_structure.code_departement = structure.code_departement
+    structure_asp structure
+    left join "departements" dept_structure
+        on dept_structure.code_departement = structure.code_departement
