a6402b087a1ae7cbc50bb947f4c8ceff34dcd51f
clemkeirua <clement@keiruaprod.fr>
2021-08-24 10:58:35 +0200

    added tests

--- itou/api/siae_api/tests.py
+++ itou/api/siae_api/tests.py
@@ -1,7 +1,11 @@
+import json
+
 from django.urls import reverse
 from rest_framework.test import APIClient, APITestCase
 
-from itou.cities.factories import create_test_cities
+from itou.cities.factories import create_city_guerande, create_city_saint_andre
+from itou.siaes.factories import SiaeFactory, SiaeWithJobsFactory
+from itou.siaes.models import Siae
 from itou.users.factories import SiaeStaffFactory
 
 
@@ -12,9 +16,16 @@ class SiaeAPIFetchListTest(APITestCase):
     def setUp(self):
         self.client = APIClient()
         self.user = SiaeStaffFactory()
-        create_test_cities(["75"])
 
-    def test_fetch_employee_record_list_without_params(self):
+        # We create 2 cities and 2 siaes in Saint-Andre.
+        self.saint_andre = create_city_saint_andre()
+        self.guerande = create_city_guerande()
+        self.siae_a = SiaeFactory(kind=Siae.KIND_EI, department="44", coords=self.saint_andre.coords)
+        self.siae_b = SiaeWithJobsFactory(
+            romes=("N1101", "N1105", "N1103", "N4105"), department="44", coords=self.saint_andre.coords
+        )
+
+    def test_fetch_siae_list_without_params(self):
         """
         The query parameters for INSEE code and distance are mandatories
         """
@@ -22,25 +33,25 @@ class SiaeAPIFetchListTest(APITestCase):
 
         self.assertEquals(response.status_code, 400)
 
-    def test_fetch_employee_record_list_with_too_high_distance(self):
+    def test_fetch_siae_list_with_too_high_distance(self):
         """
         The query parameter distance must be <= 100
         """
-        query_params = {"code_insee": 75056, "distance_max_km": 200}
+        query_params = {"code_insee": 44056, "distance_max_km": 200}
         response = self.client.get(ENDPOINT_URL, query_params, format="json")
 
         self.assertContains(response, "distance_max_km doit être entre 0 et 100", status_code=400)
 
-    def test_fetch_employee_record_list_with_negative_distance(self):
+    def test_fetch_siae_list_with_negative_distance(self):
         """
         The query parameter distance must be positive
         """
-        query_params = {"code_insee": 75056, "distance_max_km": -10}
+        query_params = {"code_insee": 44056, "distance_max_km": -10}
         response = self.client.get(ENDPOINT_URL, query_params, format="json")
 
         self.assertContains(response, "distance_max_km doit être entre 0 et 100", status_code=400)
 
-    def test_fetch_employee_record_list_with_invalid_code_insee(self):
+    def test_fetch_siae_list_with_invalid_code_insee(self):
         """
         The query parameters for INSEE code and distance are mandatories
         """
@@ -50,11 +61,26 @@ class SiaeAPIFetchListTest(APITestCase):
         self.assertEquals(response.content, b'{"detail":"Pas de ville avec pour code_insee 12345"}')
         self.assertEquals(response.status_code, 404)
 
-    def test_fetch_employee_record_list(self):
+    def test_fetch_siae_list(self):
         """
-        The query parameters for INSEE code and distance are mandatories
+        Search for siaes in the city that has 2 SIAES
         """
-        query_params = {"code_insee": 75056, "distance_max_km": 10}
+
+        query_params = {"code_insee": self.saint_andre.code_insee, "distance_max_km": 100}
         response = self.client.get(ENDPOINT_URL, query_params, format="json")
 
+        body = json.loads(response.content)
+        self.assertEquals(body["count"], 2)
+        self.assertEquals(response.status_code, 200)
+
+    def test_fetch_siae_list_too_far(self):
+        """
+        Search for siaes in a city that has no SIAES
+        """
+
+        query_params = {"code_insee": self.guerande.code_insee, "distance_max_km": 10}
+        response = self.client.get(ENDPOINT_URL, query_params, format="json")
+
+        body = json.loads(response.content)
+        self.assertEquals(body["count"], 0)
         self.assertEquals(response.status_code, 200)
