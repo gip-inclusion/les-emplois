003c92d968330c2bf7479f3b391b2fde6acff8f0
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2021-08-28 17:07:01 +0200

    Add unit test for callback url

--- itou/france_connect/tests.py
+++ itou/france_connect/tests.py
@@ -1,3 +1,5 @@
+import httpx
+import respx
 from django.conf import settings
 from django.test import TestCase
 from django.urls import reverse
@@ -5,6 +7,26 @@ from django.utils import timezone
 
 from . import views as france_connect_views
 from .models import FranceConnectState
+
+
+FRANCE_CONNECT_USERINFO = {
+    "given_name": "Angela Claire Louise",
+    "family_name": "DUBOIS",
+    "birthdate": "1962-08-24",
+    "gender": "female",
+    "birthplace": "75107",
+    "birthcountry": "99100",
+    "email": "wossewodda-3728@yopmail.com",
+    "address": {
+        "country": "France",
+        "formatted": "France Paris 75107 20 avenue de Ségur",
+        "locality": "Paris",
+        "postal_code": "75107",
+        "street_address": "20 avenue de Ségur",
+    },
+    "phone_number": "123456789",
+    "sub": "b6048e95bb134ec5b1d1e1fa69f287172e91722b9354d637a1bcf2ebb0fd2ef5v1",
+}
 
 
 class FranceConnectTest(TestCase):
@@ -38,24 +60,7 @@ class FranceConnectTest(TestCase):
         )
 
     def test_create_user_from_user_data(self):
-        user_data = {
-            "given_name": "Angela Claire Louise",
-            "family_name": "DUBOIS",
-            "birthdate": "1962-08-24",
-            "gender": "female",
-            "birthplace": "75107",
-            "birthcountry": "99100",
-            "email": "wossewodda-3728@yopmail.com",
-            "address": {
-                "country": "France",
-                "formatted": "France Paris 75107 20 avenue de Ségur",
-                "locality": "Paris",
-                "postal_code": "75107",
-                "street_address": "20 avenue de Ségur",
-            },
-            "phone_number": "123456789",
-            "sub": "b6048e95bb134ec5b1d1e1fa69f287172e91722b9354d637a1bcf2ebb0fd2ef5v1",
-        }
+        user_data = FRANCE_CONNECT_USERINFO
         fc_user_data = france_connect_views.FranceConnectUserData(**france_connect_views.load_user_data(user_data))
         user, created = france_connect_views.create_or_update_user(fc_user_data)
         self.assertTrue(created)
@@ -68,3 +73,36 @@ class FranceConnectTest(TestCase):
         user, created = france_connect_views.create_or_update_user(fc_user_data)
         self.assertFalse(created)
         self.assertEqual(user.last_name, "DUPUIS")
+
+    def test_callback_no_code(self):
+        url = reverse("france_connect:callback")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json()["message"], "La requête ne contient pas le paramètre « code ».")
+
+    def test_callback_no_state(self):
+        url = reverse("france_connect:callback")
+        response = self.client.get(url, data={"code": "123"})
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json()["message"], "Le paramètre « state » n'est pas valide.")
+
+    def test_callback_invalid_state(self):
+        url = reverse("france_connect:callback")
+        response = self.client.get(url, data={"code": "123", "state": "000"})
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json()["message"], "Le paramètre « state » n'est pas valide.")
+
+    @respx.mock
+    def test_callback(self):
+        url_fc_token = settings.FRANCE_CONNECT_URL + settings.FRANCE_CONNECT_ENDPOINT_TOKEN
+        token_json = {"access_token": "7890123", "token_type": "Bearer", "expires_in": 60, "id_token": "123456"}
+        respx.post(url_fc_token).mock(return_value=httpx.Response(200, json=token_json))
+
+        url_fc_userinfo = settings.FRANCE_CONNECT_URL + settings.FRANCE_CONNECT_ENDPOINT_USERINFO
+        respx.get(url_fc_userinfo).mock(return_value=httpx.Response(200, json=FRANCE_CONNECT_USERINFO))
+
+        csrf_signed = france_connect_views.state_new()
+        url = reverse("france_connect:callback")
+        response = self.client.get(url, data={"code": "123", "state": csrf_signed})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()["family_name"], "DUBOIS")
