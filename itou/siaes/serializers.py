fcb08c533d129e145805f2a198c6c70060bf962f
clemkeirua <clement@keiruaprod.fr>
2021-09-02 16:21:21 +0200

    description of all the API fields

--- itou/siaes/serializers.py
+++ itou/siaes/serializers.py
@@ -1,15 +1,14 @@
 from rest_framework import serializers
 
-from itou.common_apps.address.models import AddressMixin
 from itou.siaes.models import Siae, SiaeJobDescription
 
 
 class _JobDescriptionSerializer(serializers.ModelSerializer):
-    rome = serializers.CharField(source="appellation.rome")
-    recrutement_ouvert = serializers.CharField(source="is_active")
-    appellation_modifiee = serializers.CharField(source="custom_name")
-    cree_le = serializers.DateTimeField(source="created_at")
-    mis_a_jour_le = serializers.DateTimeField(source="updated_at")
+    rome = serializers.CharField(source="appellation.rome", label="Code ROME du poste")
+    recrutement_ouvert = serializers.CharField(source="is_active", label="Ce poste est-il encore ouvert ?")
+    appellation_modifiee = serializers.CharField(source="custom_name", label="Nom personnalisé")
+    cree_le = serializers.DateTimeField(source="created_at", label="Date de création")
+    mis_a_jour_le = serializers.DateTimeField(source="updated_at", label="Date de mise à jour")
 
     class Meta:
         model = SiaeJobDescription
@@ -36,7 +35,9 @@ class SiaeSerializer(serializers.ModelSerializer):
     departement = serializers.CharField(source="department", label="Le département où se trouve la SIAE")
     raison_sociale = serializers.CharField(source="name", label="Raison sociale de la SIAE")
     enseigne = serializers.CharField(source="display_name", label="Nom de la SIAE utilisé pour affichage")
-    bloque_candidatures = serializers.BooleanField(source="block_job_applications", label="Est-ce que cette SIAE bloque toute les candidatures ?")
+    bloque_candidatures = serializers.BooleanField(
+        source="block_job_applications", label="Est-ce que cette SIAE bloque toute les candidatures ?"
+    )
     postes = _JobDescriptionSerializer(source="job_description_through", many=True, label="Les postes disponibles")
 
     class Meta:
