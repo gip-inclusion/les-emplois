b119e2de940087a62af755bf4ed2d15d0f8a78c0
clemkeirua <clement@keiruaprod.fr>
2021-09-02 15:48:46 +0200

    ajout de descriptions pour les champs de l’API SIAE

--- itou/siaes/serializers.py
+++ itou/siaes/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from itou.common_apps.address.models import AddressMixin
 from itou.siaes.models import Siae, SiaeJobDescription
 
 
@@ -25,18 +26,18 @@ class _JobDescriptionSerializer(serializers.ModelSerializer):
 
 
 class SiaeSerializer(serializers.ModelSerializer):
-    cree_le = serializers.DateTimeField(source="created_at")
-    mis_a_jour_le = serializers.DateTimeField(source="updated_at")
-    type = serializers.CharField(source="kind")
-    ville = serializers.CharField(source="city")
-    code_postal = serializers.CharField(source="post_code")
-    addresse_line_1 = serializers.CharField(source="address_line_1")
-    addresse_line_2 = serializers.CharField(source="address_line_2")
-    departement = serializers.CharField(source="department")
-    raison_sociale = serializers.CharField(source="name")
-    enseigne = serializers.SerializerMethodField()
-    bloque_candidatures = serializers.BooleanField(source="block_job_applications")
-    candidatures = _JobDescriptionSerializer(source="job_description_through", many=True)
+    cree_le = serializers.DateTimeField(source="created_at", label="Date de création")
+    mis_a_jour_le = serializers.DateTimeField(source="updated_at", label="Date de mise à jour")
+    type = serializers.ChoiceField(source="kind", label="Type de SIAE", choices=Siae.KIND_CHOICES)
+    ville = serializers.CharField(source="city", label="Ville où se trouve la SIAE")
+    code_postal = serializers.CharField(source="post_code", label="Code postal de la SIAE")
+    addresse_line_1 = serializers.CharField(source="address_line_1", label="1ère ligne d’adresse de la SIAE")
+    addresse_line_2 = serializers.CharField(source="address_line_2", label="2nde ligne d’adresse de la SIAE")
+    departement = serializers.CharField(source="department", label="Le département où se trouve la SIAE")
+    raison_sociale = serializers.CharField(source="name", label="Raison sociale de la SIAE")
+    enseigne = serializers.CharField(source="display_name", label="Nom de la SIAE utilisé pour affichage")
+    bloque_candidatures = serializers.BooleanField(source="block_job_applications", label="Est-ce que cette SIAE bloque toute les candidatures ?")
+    postes = _JobDescriptionSerializer(source="job_description_through", many=True, label="Les postes disponibles")
 
     class Meta:
         model = Siae
@@ -55,9 +56,6 @@ class SiaeSerializer(serializers.ModelSerializer):
             "code_postal",
             "ville",
             "departement",
-            "candidatures",
+            "postes",
         ]
         read_only_fields = fields
-
-    def get_enseigne(self, obj):
-        return obj.display_name
