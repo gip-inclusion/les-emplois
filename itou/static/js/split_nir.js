80ba9a875d96b7875a5f40ff49ac4e7ab7f4fea7
Céline MS <celine@hello-birds.com>
2021-09-30 15:52:00 +0200

    Add comments

--- itou/static/js/split_nir.js
+++ itou/static/js/split_nir.js
@@ -1,21 +1,21 @@
 "use strict";
 
-let $form = $( ".js-format-nir" );
-let $input = $form.find( "input[name='nir']" ).first();
+let form = $( ".js-format-nir" );
+let input = form.find( "input[name='nir']" ).first();
 
-$($input).keyup(function(e) {
+$(input).keyup(function(e) {
     if (e.keyCode == 8) {
-        // Backspace key
+        // Ignore backspace key to let users erase content.
         return;
     }
-    let elements = $(this).val().replace(/\s+/g, '').split("");
-    let breakpoints = [0, 2, 4, 6, 9, 12];
-    let counter = 0;
+    let elements = $(this).val().replace(/\s+/g, '').split(""); // Delete already existing white spaces.
+    let breakpoints = [0, 2, 4, 6, 9, 12]; // White spaces will be inserted after theses indexes + 1.
+    let counter = 0; // When a white space is added, the total number of items in list should be increased by 1.
     $.each(elements, function( index, value ) {
         if ($.inArray(index, breakpoints) != -1) {
-            elements.splice(index+1+counter, 0, " ");
+            elements.splice(index+1+counter, 0, " "); // Index + 1 to add a white space "in advance".
             counter +=1;
         }
     });
-    $($input).val(elements.join(""));
+    $(input).val(elements.join(""));
 });
