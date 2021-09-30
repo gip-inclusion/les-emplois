15799826e1f82a4e384651d9a6b657c99515e65e
Frédéric Vergez <fred@ikarius.com>
2021-09-30 14:19:13 +0200

    Updated opening date

--- itou/job_applications/models.py
+++ itou/job_applications/models.py
@@ -202,7 +202,7 @@ class JobApplicationQuerySet(models.QuerySet):
                 to_siae=siae,
                 hiring_start_at__lt=cancellation_date,
                 # Must be accepted after production date:
-                updated_at__gte=settings.EMPLOYEE_RECORD_FEATURE_AVAILABILITY_DATE,
+                created_at__gte=settings.EMPLOYEE_RECORD_FEATURE_AVAILABILITY_DATE,
             )
             .select_related("job_seeker", "approval")
         )
