10bb88c1d48153b38a77153e29c17febfb6b4b71
Céline MS <celine@hello-birds.com>
2021-10-06 15:49:09 +0200

    Ensure NIR is formatted without white spaces

--- itou/utils/templatetags/format_filters.py
+++ itou/utils/templatetags/format_filters.py
@@ -45,6 +45,7 @@ def format_siret(siret):
 @register.filter
 @stringfilter
 def format_nir(nir):
+    nir = nir.replace(" ", "")
     nir_regex = r"^([12])([0-9]{2})([0-1][0-9])(2[AB]|[0-9]{2})([0-9]{3})([0-9]{3})([0-9]{2})$"
     match = re.match(nir_regex, nir)
     return " ".join(match.groups())
