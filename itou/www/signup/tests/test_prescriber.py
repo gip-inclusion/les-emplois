bc9a6325e65e8fb0b523f25aaeaad6275fe40997
Sébastien Reuiller <sebastien@reuiller.email>
2021-10-08 16:58:58 +0200

    add organization without a member in list

--- itou/www/signup/tests/test_prescriber.py
+++ itou/www/signup/tests/test_prescriber.py
@@ -1,5 +1,5 @@
 import uuid
-from unittest import mock, skip
+from unittest import mock
 
 import httpx
 import respx
@@ -522,7 +522,6 @@ class PrescriberSignupTest(TestCase):
         subject = mail.outbox[0].subject
         self.assertIn("Confirmez votre adresse e-mail", subject)
 
-    @skip("I need to know what to do with organizations registered without a member ")
     @respx.mock
     @mock.patch("itou.utils.apis.geocoding.call_ban_geocoding_api", return_value=BAN_GEOCODING_API_RESULT_MOCK)
     def test_create_user_prescriber_with_same_siret_and_different_kind(self, mock_call_ban_geocoding_api):
@@ -577,7 +576,6 @@ class PrescriberSignupTest(TestCase):
         self.assertEqual(PrescriberOrganization.Kind.ML.value, org1.kind)
         self.assertEqual(PrescriberOrganization.Kind.PLIE.value, org2.kind)
 
-    @skip("I need to know what to do with organizations registered without a member ")
     @respx.mock
     @mock.patch("itou.utils.apis.geocoding.call_ban_geocoding_api", return_value=BAN_GEOCODING_API_RESULT_MOCK)
     def test_create_user_prescriber_with_same_siret_and_same_kind(self, mock_call_ban_geocoding_api):
