f5e5ea0c1aadc5ef24204d73f45d8d57370c01fb
Madjid.A <madjid@udez.fr>
2021-10-20 12:06:29 +0200

    SIAE Cards: refresh Siae cards

--- itou/www/siaes_views/views.py
+++ itou/www/siaes_views/views.py
@@ -29,7 +29,7 @@ def card(request, siae_id, template_name="siaes/card.html"):
     # COVID-19 "Operation ETTI".
     Public view (previously private, made public during COVID-19).
     """
-    queryset = Siae.objects.prefetch_job_description_through(is_active=True)
+    queryset = Siae.objects.prefetch_job_description_through(is_active=True).with_job_app_score()
     siae = get_object_or_404(queryset, pk=siae_id)
     back_url = get_safe_url(request, "back_url")
     context = {"siae": siae, "back_url": back_url}
