f49225ad3b099541d4873465e1b2553a2353edda
Madjid.A <madjid@udez.fr>
2021-10-25 11:36:09 +0200

    siae perms: add options to get Siae related tables

--- itou/utils/perms/siae.py
+++ itou/utils/perms/siae.py
@@ -1,11 +1,19 @@
 from django.conf import settings
 from django.shortcuts import get_object_or_404
+from django.utils import timezone
 
 from itou.siaes.models import Siae
 
 
-def get_current_siae_or_404(request):
+def get_current_siae_or_404(request, with_job_descriptions=False, with_job_app_score=False):
     pk = request.session.get(settings.ITOU_SESSION_CURRENT_SIAE_KEY)
     queryset = Siae.objects.member_required(request.user)
+
+    if with_job_descriptions:
+        queryset = queryset.prefetch_job_description_through()
+
+    if with_job_app_score:
+        queryset = queryset.with_job_app_score()
+
     siae = get_object_or_404(queryset, pk=pk)
     return siae
