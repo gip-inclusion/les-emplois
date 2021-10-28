4d0da64f9c495e0edbf3be85fcf9fabcfa059b87
SoumiaAhlouch <79250336+SoumiaAHLOUCH@users.noreply.github.com>
2021-10-28 15:50:02 +0200

    Create 010_sorties.sql

---
+++ itou/metabase/management/commands/sql/010_sorties.sql
@@ -0,0 +1,56 @@
+/*
+ 
+L'objectif est de créer une table agrégée avec l'indicateur nombre de sorties pas type de public cible:
+    - genre du salarié
+    - RSA
+    - niveau de formation du salarié
+    - commune de la structure
+    - établissement public territorial
+    - département et région de l'annexe financière
+  
+Un filtre est appliqué pour récupérer un historique de 2 ans en plus de l'année en cours
+
+*/    
+
+select 
+    count(distinct(identifiant_salarie)) as nombre_sorties, 
+    date_part('year', date_sortie) as annee_sortie,
+    etablissement_Public_Territorial, 
+    niveau_formation_salarie,
+    genre_salarie,
+    rsa, 
+    type_siae,
+    motif_sortie,
+    categorie_sortie,
+    id_structure_asp, 
+    structure_denomination,
+    commune_structure , 
+    code_insee_structure, 
+    nom_departement_af,
+    nom_region_af,
+    af_numero_convention,
+    af_numero_annexe_financiere
+from 
+    saisies_mensuelles_IAE
+where 
+    date_part('year', date_sortie) >= (date_part('year', current_date) - 2)
+    /* Prendre en compte les salariés qui ont travaillé au moins une heure dans la structure */
+    and nombre_heures_travaillees >= 1
+    and date_part('year', date_sortie) = date_part('year', date_saisie) 
+group by 
+    annee_sortie,  
+    etablissement_Public_Territorial, 
+    niveau_formation_salarie,
+    genre_salarie,
+    rsa, 
+    type_siae,
+    motif_sortie,
+    categorie_sortie,
+    id_structure_asp, 
+    structure_denomination,
+    commune_structure , 
+    code_insee_structure, 
+    nom_departement_af,
+    nom_region_af,
+    af_numero_convention,
+    af_numero_annexe_financiere
