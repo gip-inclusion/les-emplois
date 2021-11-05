e8b70dadb3c267d157901bfd9397cee27df831e2
Madjid.A <madjid@udez.fr>
2021-11-05 17:18:27 +0100

    fix ci quality

--- itou/www/siaes_views/tests.py
+++ itou/www/siaes_views/tests.py
@@ -176,23 +176,6 @@ class ConfigureJobsViewTest(TestCase):
             "code-create": ["10579", "16361"],
             # Exclude code `11999` from POST payload.
             "code-delete": ["11999"],
-            # Do nothing for "Agent / Agente cariste de livraison ferroviaire"
-            "is_active-10357": "on",  # "on" is set when the checkbox is checked.
-            # Update "Agent / Agente de quai manutentionnaire"
-            "custom-name-10579": "Agent de quai",
-            "description-10579": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-            "is_active-10579": "",
-            # Update "Agent magasinier / Agente magasinière gestionnaire de stocks"
-            "is_active-10750": "",
-            "custom-name-10750": "",
-            "description-10750": "",
-            # Delete for "Chauffeur-livreur / Chauffeuse-livreuse"
-            # Add "Aide-livreur / Aide-livreuse"
-            "is_active-10579": "on",
-            "custom-name-10579": "Aide-livreur hebdomadaire",
-            "description-10579": "Pellentesque ex ex, elementum sed sollicitudin sit amet, dictum vel elit.",
-            # Add "Manutentionnaire"
-            "is_active-16361": "",
         }
         response = self.client.post(self.url, data=post_data)
         self.assertEqual(response.status_code, 200)
