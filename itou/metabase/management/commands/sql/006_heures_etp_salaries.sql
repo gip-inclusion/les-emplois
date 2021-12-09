1bfd76d7771ce5022269f21dadc28120b3db18c5
SoumiaAhlouch <79250336+SoumiaAHLOUCH@users.noreply.github.com>
2021-12-09 17:43:51 +0100

    Create 006_heures_etp_salaries.sql

--- itou/metabase/management/commands/sql/006_heures_etp_salaries.sql
+++ itou/metabase/management/commands/sql/006_heures_etp_salaries.sql
@@ -10,10 +10,10 @@ Ces indicateurs sont déclinés par type de public cible:
     - niveau de formation du salarié
     - commune de la structure
     - établissement public territorial
+    - établissements publics de coopération intercommunale
     - département et région de l'annexe financière
   
 Un filtre est appliqué pour récupérer un historique de 2 ans en plus de l'année en cours
-
 */
     
 select 
@@ -21,7 +21,8 @@ select
     sum(nombre_etp_consommes)/12 as nombre_etp,
     sum(nombre_heures_travaillees) as nombre_heures_travaillees, 
     date_part('year', date_saisie) as annee_saisie,
-    etablissement_Public_Territorial, 
+    etablissement_Public_Territorial,
+    nom_epci,
     niveau_formation_salarie,
     genre_salarie,
     rsa, 
@@ -41,7 +42,8 @@ where nombre_heures_travaillees > 0
       and date_part('year', date_saisie) >= (date_part('year', current_date) - 2)
 group by 
     annee_saisie,
-    etablissement_Public_Territorial, 
+    etablissement_Public_Territorial,
+    nom_epci,
     niveau_formation_salarie,
     genre_salarie,
     rsa, 
