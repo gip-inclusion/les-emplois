e83c46754dcfe2a18ff5921931151e30175ad973
dejafait <dejafait@blackhole.com>
2022-01-27 12:09:45 +0200

    Fix 500 error for route /robots.txt

--- itou/utils/settings_context_processors.py
+++ itou/utils/settings_context_processors.py
@@ -7,13 +7,14 @@ def expose_settings(request):
     https://docs.djangoproject.com/en/2.1/ref/templates/api/#using-requestcontext
     """
 
-    full_view_name = request.resolver_match.view_name  # e.g. "home:hp" or "stats:stats_public"
+    assistance_url = settings.ITOU_ASSISTANCE_URL
 
-    if full_view_name.startswith("stats:"):
-        # On all stats pages the help button should redirect to the C2 help page instead of the C1 help page.
-        assistance_url = settings.PILOTAGE_ASSISTANCE_URL
-    else:
-        assistance_url = settings.ITOU_ASSISTANCE_URL
+    resolver_match = request.resolver_match
+    if resolver_match is not None:  # resolver_match is None for some routes e.g. /robots.txt
+        full_view_name = resolver_match.view_name  # e.g. "home:hp" or "stats:stats_public"
+        if full_view_name.startswith("stats:"):
+            # On all stats pages the help button should redirect to the C2 help page instead of the C1 help page.
+            assistance_url = settings.PILOTAGE_ASSISTANCE_URL
 
     return {
         "ALLOWED_HOSTS": settings.ALLOWED_HOSTS,
