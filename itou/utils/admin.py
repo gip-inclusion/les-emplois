ee3c7cfbdafb0b586202ea4feb7d0559052e06c7
Sébastien Reuiller <sebastien@reuiller.email>
2022-02-03 11:50:31 +0100

    hides LogEntry section

--- itou/utils/admin.py
+++ itou/utils/admin.py
@@ -1,13 +1,24 @@
+from django.contrib import admin
+from django.contrib.admin.models import LogEntry
 from django.contrib.contenttypes.admin import GenericStackedInline
+from django_admin_logs.admin import LogEntryAdmin
 
 from itou.utils.models import SupportRemark
 
 
 class SupportRemarkInline(GenericStackedInline):
     model = SupportRemark
-
     min_num = 0
     max_num = 1
     extra = 1
-
     can_delete = False
+
+
+# Hides the LogEntry section in the admin interface
+class LogEntryAdminHidden(LogEntryAdmin):
+    def has_view_permission(self, request, obj=None):
+        return False
+
+
+admin.site.unregister(LogEntry)
+admin.site.register(LogEntry, LogEntryAdminHidden)
