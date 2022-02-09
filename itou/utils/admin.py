f2b6368ca061fbc4c8a123db7c4b8c5adf9bd142
Sébastien Reuiller <sebastien@reuiller.email>
2022-02-09 10:46:57 +0100

    logadmin only displayed on dev

--- itou/utils/admin.py
+++ itou/utils/admin.py
@@ -1,7 +1,4 @@
-from django.contrib import admin
-from django.contrib.admin.models import LogEntry
 from django.contrib.contenttypes.admin import GenericStackedInline
-from django_admin_logs.admin import LogEntryAdmin
 
 from itou.utils.models import SupportRemark
 
@@ -12,13 +9,3 @@ class SupportRemarkInline(GenericStackedInline):
     max_num = 1
     extra = 1
     can_delete = False
-
-
-# Hides the LogEntry section in the admin interface
-class LogEntryAdminHidden(LogEntryAdmin):
-    def has_view_permission(self, request, obj=None):
-        return False
-
-
-admin.site.unregister(LogEntry)
-admin.site.register(LogEntry, LogEntryAdminHidden)
