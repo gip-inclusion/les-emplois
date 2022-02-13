d0ca036dbf2de28be5b8f6e8fd2b900dc27d5209
vincent porte <vincent@neuralia.co>
2022-02-13 06:36:16 +0100

    fetch & rebase

--- itou/job_applications/admin_forms.py
+++ itou/job_applications/admin_forms.py
@@ -32,7 +32,10 @@ class JobApplicationAdminForm(forms.ModelForm):
             raise ValidationError("SIAE émettrice inattendue.")
 
         if sender_kind == JobApplication.SENDER_KIND_PRESCRIBER:
-            if sender:
+            # request organization only if prescriber is linked to organization
+            if sender is not None and sender.is_prescriber_with_org and sender_prescriber_organization is None:
+                raise ValidationError("Organisation du prescripteur émettrice manquante.")
+            if sender is not None:
                 # Sender is optional, but if it exists, check its role.
                 if not sender.is_prescriber:
                     raise ValidationError("Emetteur du mauvais type.")
