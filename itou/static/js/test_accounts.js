c04a4371cef77001076b1e0415899440d1638182
Céline MS <celine@hello-birds.com>
2022-02-16 18:31:16 +0100

    Adapt test accounts logic to match new routes

--- itou/static/js/test_accounts.js
+++ itou/static/js/test_accounts.js
@@ -16,12 +16,12 @@ $(document).ready(function(){
     }
 
     $('.postLogin').on('click', function(){
-        const account_type = $(this).data('type');
+        const actionUrl = $(this).data('action-url');
         const email = $(this).data('email');
         const form = $("#testAccountsForm");
+        form.attr('action', actionUrl);
         form.find('input[type=email]').val(email);
         form.find('input[type=password]').val("password");
-        form.find('input[name=account_type]').val(account_type);
         form.submit();
     });
 });
--- itou/utils/templatetags/test_accounts.py
+++ itou/utils/templatetags/test_accounts.py
@@ -1,4 +1,5 @@
 from django import template
+from django.urls import reverse
 
 
 register = template.Library()
@@ -6,6 +7,7 @@ register = template.Library()
 
 @register.simple_tag
 def employers_accounts_tag():
+    action_url = reverse("login:siae_staff")
     return [
         {
             "email": "test+etti@inclusion.beta.gouv.fr",
@@ -13,6 +15,7 @@ def employers_accounts_tag():
             "image": "etti.svg",
             "location": "Beaucaire (30)",
             "title": "Entreprise de Travail Temporaire d'Insertion (E.T.T.I)",
+            "action_url": action_url,
         },
         {
             "email": "test+ei@inclusion.beta.gouv.fr",
@@ -20,6 +23,7 @@ def employers_accounts_tag():
             "image": "ei.svg",
             "location": "St-Etienne du Grès (13)",
             "title": "Entreprise d'Insertion (E.I.)",
+            "action_url": action_url,
         },
         {
             "email": "test+geiq@inclusion.beta.gouv.fr",
@@ -27,6 +31,7 @@ def employers_accounts_tag():
             "image": "geiq.svg",
             "location": "Tarascon (13)",
             "title": "Groupement d'Employeurs pour l'Insertion et la Qualification (G.E.I.Q.)",
+            "action_url": action_url,
         },
         {
             "email": "test+ea@inclusion.beta.gouv.fr",
@@ -34,6 +39,7 @@ def employers_accounts_tag():
             "image": "ea.svg",
             "location": "Fontvieille (13)",
             "title": "Entreprise Adaptée (E.A.)",
+            "action_url": action_url,
         },
         {
             "email": "test+ai@inclusion.beta.gouv.fr",
@@ -41,31 +47,37 @@ def employers_accounts_tag():
             "image": "ai.svg",
             "location": "Tours (37)",
             "title": "Association intermédiaire (A.I.)",
+            "action_url": action_url,
         },
     ]
 
 
 @register.simple_tag
 def prescribers_accounts_tag():
+    action_url = reverse("login:prescriber")
     return [
         {
             "title": "Prescripteur habilité",
             "email": "test+prescripteur@inclusion.beta.gouv.fr",
             "image": "prescripteur_habilite.svg",
+            "action_url": action_url,
         },
         {
             "title": "Orienteur <br>(prescripteur non habilité)",
             "email": "test+orienteur@inclusion.beta.gouv.fr",
             "image": "prescripteur_non_habilite.svg",
+            "action_url": action_url,
         },
         {
             "title": "Orienteur seul,<br> sans organisation",
             "email": "test+orienteur-solo@inclusion.beta.gouv.fr",
             "image": "prescripteur_solo.svg",
+            "action_url": action_url,
         },
     ]
 
 
 @register.simple_tag
 def job_seekers_accounts_tag():
-    return [{"email": "test+de@inclusion.beta.gouv.fr", "image": "de.svg"}]
+    action_url = reverse("login:job_seeker")
+    return [{"email": "test+de@inclusion.beta.gouv.fr", "image": "de.svg", "action_url": action_url}]
