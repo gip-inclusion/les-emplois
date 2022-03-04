21d62772d29f4476b892a5ccadbdb79858a3d242
clemkeirua <clement@keiruaprod.fr>
2022-03-04 10:56:39 +0100

    introduce MergedPoleEmploiApproval and fix code accordingly

--- itou/approvals/management/commands/merge_pe_approvals.py
+++ itou/approvals/management/commands/merge_pe_approvals.py
@@ -3,15 +3,13 @@ from django.db import connection
 from psycopg2 import sql  # noqa
 from tqdm import tqdm
 
-from itou.approvals.models import PoleEmploiApproval
+from itou.approvals.models import MergedPoleEmploiApproval, PoleEmploiApproval
 
 
 class Command(BaseCommand):
     """
     ./manage.py merge_pe_approvals --reset
     """
-
-    merge_table = "merged_approvals_poleemploiapproval"
 
     def add_arguments(self, parser):
         parser.add_argument("--reset", dest="reset", action="store_true", help="Resets the tables")
@@ -42,56 +40,25 @@ class Command(BaseCommand):
                 # we can bulk-update all the initial approvals
                 matching_approvals.update(merged=True)
                 # and insert a row in the merge table
-                query = f"""INSERT INTO {self.merge_table}
-                (
-                    number,
-                    pe_structure_code,
-                    pole_emploi_id,
-                    first_name,
-                    last_name,
-                    birth_name,
-                    birthdate,
-                    nir,
-                    ntt_nia,
-                    created_at,
-                    start_at,
-                    end_at,
-                    merged)
-                VALUES(
-                    %s,
-                    %s,
-                    %s,
-                    %s,
-                    %s,
-                    %s,
-                    %s,
-                    %s,
-                    %s,
-                    %s,
-                    %s,
-                    %s,
-                    true
-                )"""
-                values = [
-                    number,
-                    approval.pe_structure_code,
-                    approval.pole_emploi_id,
-                    approval.first_name,
-                    approval.last_name,
-                    approval.birth_name,
-                    approval.birthdate,
-                    approval.nir,
-                    approval.ntt_nia,
-                    pe_approval.created_at,
-                    pe_approval.start_at,
-                    pe_approval.end_at,
-                ]
-                self.cursor.execute(query, values)
-                connection.commit()
+                merged_approval = MergedPoleEmploiApproval(
+                    number=number,
+                    pe_structure_code=approval.pe_structure_code,
+                    pole_emploi_id=approval.pole_emploi_id,
+                    first_name=approval.first_name,
+                    last_name=approval.last_name,
+                    birth_name=approval.birth_name,
+                    birthdate=approval.birthdate,
+                    nir=approval.nir,
+                    ntt_nia=approval.ntt_nia,
+                    created_at=pe_approval.created_at,
+                    start_at=pe_approval.start_at,
+                    end_at=pe_approval.end_at,
+                )
+                merged_approval.save()
 
     def get_count_non_merged_approvals(self):
         nb_non_merged_peapproval_sql = (
-            "select count(distinct(left(number, 12))) from approvals_poleemploiapproval where merged=false"
+            f"select count(distinct(left(number, 12))) from {PoleEmploiApproval._meta.db_table} where merged=false"
         )
 
         self.cursor.execute(nb_non_merged_peapproval_sql)
@@ -103,38 +70,34 @@ class Command(BaseCommand):
         Returns the list of all the 12-digit PoleEmploiApproval number that have not yet been merged
         """
         nb_non_merged_peapproval_sql = (
-            "select distinct(left(number, 12)) from approvals_poleemploiapproval where merged=false"
+            f"select distinct(left(number, 12)) from {PoleEmploiApproval._meta.db_table} where merged=false"
         )
 
         self.cursor.execute(nb_non_merged_peapproval_sql)
         rows = self.cursor.fetchall()
         return rows
 
-    def reset_tables(self, reset):
-        create_query = (
-            f"CREATE TABLE IF NOT EXISTS {self.merge_table} (LIKE approvals_poleemploiapproval INCLUDING ALL);"  # noqa
-        )
-        self.cursor.execute(create_query)
+    def reset_tables(self):
         reset_queries = [
             f"TRUNCATE {self.merge_table};",  # noqa
-            "UPDATE approvals_poleemploiapproval set merged=false where merged=true;",
+            f"UPDATE {PoleEmploiApproval._meta.db_table} set merged=false where merged=true;",
         ]
-        if reset:
-            for query in reset_queries:
-                print(f"Running:\n{query}")
-                self.cursor.execute(query)
+        for query in reset_queries:
+            print(f"Running:\n{query}")
+            self.cursor.execute(query)
 
     def handle(self, dry_run=False, reset=False, **options):
         self.dry_run = dry_run
         self.stdout.write("Merging approvals / PASS IAE")
         self.cursor = connection.cursor()
 
-        self.reset_tables(reset)
+        if reset:
+            self.reset_tables(reset)
 
-        pbar = tqdm(total=self.get_count_non_merged_approvals())
+        progress_bar = tqdm(total=self.get_count_non_merged_approvals())
         print("Merge all the approvals \\o/")
         for number in self.get_non_merged_approvals_number12():
             matching_approvals = PoleEmploiApproval.objects.filter(number__startswith=number[0])  # noqa
             self.create_new_merged_approval(number[0], matching_approvals)
-            pbar.update(1)
-        pbar.close()
+            progress_bar.update(1)
+        progress_bar.close()
