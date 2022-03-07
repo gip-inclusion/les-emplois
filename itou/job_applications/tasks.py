6d0d5b6d807b8c02c1dfccaf4f5ca082bdde3311
clemkeirua <clement@keiruaprod.fr>
2022-03-07 14:26:07 +0100

    more logs to see if token/mode are empty somehow

--- itou/job_applications/tasks.py
+++ itou/job_applications/tasks.py
@@ -73,7 +73,7 @@ def notify_pole_emploi_pass(job_application, job_seeker, mode=POLE_EMPLOI_PASS_A
         sleep(1)
     except PoleEmploiMiseAJourPassIAEException as e:
         log.status = JobApplicationPoleEmploiNotificationLog.STATUS_FAIL_NOTIFY_POLE_EMPLOI
-        log.details = f"{e.http_code} {e.response_code} {token}  {settings.API_ESD_MISE_A_JOUR_PASS_MODE}"
+        log.details = f"{e.http_code} {e.response_code} Token: {token} Mode: {settings.API_ESD_MISE_A_JOUR_PASS_MODE}"
         log.save()
         return False
     log.details += f" {token} {settings.API_ESD_MISE_A_JOUR_PASS_MODE}"
