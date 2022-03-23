eb24b63e671bd51dc9843978fbcbd457403f5f8e
Yannick Passarelli <99667699+YannickPassa@users.noreply.github.com>
2022-03-23 11:29:41 +0100

    Update 015_taux_transformation_prescripteurs.sql
    Modification du script suite aux explications de Pierre.

--- itou/metabase/management/commands/sql/015_taux_transformation_prescripteurs.sql
+++ itou/metabase/management/commands/sql/015_taux_transformation_prescripteurs.sql
@@ -26,74 +26,56 @@ with candidats_p as ( /* Ici on sélectionne les colonnes pertinentes à partir 
     from
 	public.candidats as cdd /* cdd pour CanDiDats */
     where type_auteur_diagnostic = ('Prescripteur')
-),
-	
-candidatures_p as ( /* Ici on sélectionne les colonnes pertinentes à partir de la table candidatures */
-    select 
-	cddr.id_candidat_anonymisé  as id_candidat_anonymise, 
-	cddr.id_structure as id_structure,
-	cddr.date_candidature, 
-	cddr.date_embauche,
-	cddr.département_structure,
-	cddr.nom_département_structure,
-	cddr.état,
-	cddr.motif_de_refus,
-	cddr.nom_org_prescripteur,
-	cddr.nom_structure,
-	cddr.origine,
-	cddr.origine_détaillée,
-	cddr.région_structure,
-	cddr.type_structure,
-	cddr.injection_ai,
-	cddr.id_org_prescripteur	
-    from
-	public.candidatures as cddr /* cddr pour CanDiDatuRes */
-),
-	
-organisations_p as ( /* On ne prend que type et type complet, le reste n'étant pas intéressant pour le moment */
-    select 
-	orga.id as id_structure,
-	orga.type,
-	orga.type_complet
-    from 
-	public.organisations as orga
 )
+
 	
 select /* On selectionne les colonnes finales qui nous intéressent */
-    candidats_p.id_candidat_anonymise,
-    age,     
-    date_diagnostic, 
+    id_candidat_anonymise,
+    actif,
+    age,
+    date_diagnostic,
     departement_candidat,
     nom_departement_candidat,
     region_candidat,
     type_auteur_diagnostic,
-    sous_type_auteur_diagnostic, 
-    total_candidatures, 
+    case 
+	when candidats_p.sous_type_auteur_diagnostic = 'Prescripteur AFPA' then 'AFPA - Agence nationale pour la formation professionnelle des adultes'
+	when candidats_p.sous_type_auteur_diagnostic = 'Prescripteur ASE' then 'ASE - Aide sociale à l''enfance'
+	when candidats_p.sous_type_auteur_diagnostic = 'Prescripteur Autre' then 'Autre' 
+	when candidats_p.sous_type_auteur_diagnostic = 'Prescripteur CAARUD' then 'CAARUD - Centre d''accueil et d''accompagnement à la réduction de risques pour usagers de drogues'
+	when candidats_p.sous_type_auteur_diagnostic = 'Prescripteur CADA' then 'CADA - Centre d''accueil de demandeurs d''asile'
+	when candidats_p.sous_type_auteur_diagnostic = 'Prescripteur CAF' then 'CAF - Caisse d''allocations familiales'
+	when candidats_p.sous_type_auteur_diagnostic = 'Prescripteur CAP_EMPLOI' then 'CAP emploi'
+	when candidats_p.sous_type_auteur_diagnostic = 'Prescripteur CAVA' then 'ACAVA - Centre d''adaptation à la vie active'
+	when candidats_p.sous_type_auteur_diagnostic = 'Prescripteur CCAS' then 'CCAS - Centre communal d''action sociale ou centre intercommunal d''action sociale'
+	when candidats_p.sous_type_auteur_diagnostic = 'Prescripteur CHRS' then 'CHRS - Centre d''hébergement et de réinsertion sociale'
+	when candidats_p.sous_type_auteur_diagnostic = 'Prescripteur CHU' then 'CHU - Centre d''hébergement d''urgence'
+	when candidats_p.sous_type_auteur_diagnostic = 'Prescripteur CIDFF' then 'CIDFF - Centre d''information sur les droits des femmes et des familles'
+	when candidats_p.sous_type_auteur_diagnostic = 'Prescripteur CPH' then 'CPH - Centre provisoire d''hébergement'
+	when candidats_p.sous_type_auteur_diagnostic = 'Prescripteur CSAPA' then 'CSAPA - Centre de soins, d''accompagnement et de prévention en addictologie'
+	when candidats_p.sous_type_auteur_diagnostic = 'Prescripteur DEPT' then 'Service social du conseil départemental'
+	when candidats_p.sous_type_auteur_diagnostic = 'Prescripteur E2C' then 'E2C - École de la deuxième chance'
+	when candidats_p.sous_type_auteur_diagnostic = 'Prescripteur EPIDE' then 'EPIDE - Établissement pour l''insertion dans l''emploi'
+	when candidats_p.sous_type_auteur_diagnostic = 'Prescripteur HUDA' then 'HUDA - Hébergement d''urgence pour demandeurs d''asile'
+	when candidats_p.sous_type_auteur_diagnostic = 'Prescripteur ML' then 'Mission Locale'
+	when candidats_p.sous_type_auteur_diagnostic = 'Prescripteur MSA' then 'MSA - Mutualité Sociale Agricole'
+	when candidats_p.sous_type_auteur_diagnostic = 'Prescripteur None' then 'Autre'
+	when candidats_p.sous_type_auteur_diagnostic = 'Prescripteur OACAS' then 'OACAS - Structure porteuse d''un agrément national organisme d''accueil communautaire et d''activité solidaire'
+	when candidats_p.sous_type_auteur_diagnostic = 'Prescripteur ODC' then 'Organisation délégataire d''un CD'
+	when candidats_p.sous_type_auteur_diagnostic = 'Prescripteur OIL' then 'Opérateur d''intermédiation locative'
+	when candidats_p.sous_type_auteur_diagnostic = 'Prescripteur PE' then 'Pôle emploi'
+	when candidats_p.sous_type_auteur_diagnostic = 'Prescripteur PENSION' then 'Pension de famille / résidence accueil'
+	when candidats_p.sous_type_auteur_diagnostic = 'Prescripteur PIJ_BIJ' then 'PIJ-BIJ - Point/Bureau information jeunesse'
+	when candidats_p.sous_type_auteur_diagnostic = 'Prescripteur PJJ' then 'PJJ - Protection judiciaire de la jeunesse'
+	when candidats_p.sous_type_auteur_diagnostic = 'Prescripteur PLIE' then 'PLIE - Plan local pour l''insertion et l''emploi'
+	when candidats_p.sous_type_auteur_diagnostic = 'Prescripteur PREVENTION' then 'Service ou club de prévention'
+	when candidats_p.sous_type_auteur_diagnostic = 'Prescripteur RS_FJT' then 'Résidence sociale / FJT - Foyer de Jeunes Travailleurs'
+	when candidats_p.sous_type_auteur_diagnostic = 'Prescripteur SPIP' then 'SPIP - Service pénitentiaire d''insertion et de probation'
+    end type_auteur_diagnostic_detaille, 
+    total_candidatures,
     total_diagnostics,
     total_embauches,
     type_inscription,
-    pe_inscrit,
-    candidatures_p.id_structure,
-    date_candidature, 
-    date_embauche,
-    département_structure,
-    nom_département_structure,
-    état,
-    motif_de_refus,
-    nom_org_prescripteur,
-    nom_structure,
-    origine,
-    origine_détaillée,
-    région_structure,
-    type_structure,
-    id_org_prescripteur,
-    candidatures_p.injection_ai,
-    candidats_p.injection_ai,
-    type,
-    type_complet
+    pe_inscrit 
 from 
     candidats_p
-        left join candidatures_p
-		on candidats_p.id_candidat_anonymise = candidatures_p.id_candidat_anonymise 
-	left join organisations_p 
-		on candidatures_p.id_structure = organisations_p.id_structure
