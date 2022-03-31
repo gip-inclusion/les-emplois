b444ebb6ab1b226cbbdea2c87e0854efb8df29e9
vincent porte <vincent@neuralia.co>
2022-03-31 09:39:58 +0200

    Models (tables & methodes), admin, enums, factories, migration

--- itou/siae_evaluations/enums.py
+++ itou/siae_evaluations/enums.py
@@ -10,3 +10,17 @@ class EvaluationChosenPercent:
 class EvaluationSiaesKind:
     # Siae.KIND_AI will be eligible for Evaluation from 2022
     Evaluable = [Siae.KIND_EI, Siae.KIND_ACI, Siae.KIND_ETTI]
+
+
+class EvaluationJobApplicationsBoundariesNumber:
+    # one SIAE can be selected in evaluation if it made
+    # at least 10 job applications with self-approval
+    # during the evaluated period
+    MIN = 10
+
+    # whenever one SIAE made more than 20 job applications with self-approval
+    # during the evaluated period, only 20 job applications maximum will be
+    # added into the evaluation campaign
+    MAX = 100
+    SELECTION_PERCENTAGE = 20
+    SELECTED_MAX = int(MAX * SELECTION_PERCENTAGE / 100)
