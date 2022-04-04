19a149d5daf503e615a2934de4f96374258ab157
Victor Perron <victor@iso3103.net>
2022-04-04 17:54:00 +0200

    Import ASP: add tracing to the script
    Make the output more readable.

--- scripts/imports-asp.sh
+++ scripts/imports-asp.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+OUTPUT_PATH=shared_bucket/imports-asp
+
 # This shell script is meant to be run in production as part of a github workflow.
 #
 # It runs the frequent ASP data imports:
@@ -11,9 +13,13 @@
 
 echo "Running the ASP import script"
 
+# Activate trace of all the executed commands so that the output can be read more easily
+# Every command in this script will be written before being executed
+set +x
+
 cd $APP_HOME
 
-# creates the "data" directory inside of the app and clear it of any previously existing data
+# Create the "data" directory inside of the app and clear it of any previously existing data
 mkdir -p itou/siaes/management/commands/data/
 rm -rf itou/siaes/management/commands/data/*
 
@@ -22,10 +28,10 @@ unzip -P $ASP_UNZIP_PASSWORD asp_shared_bucket/fluxIAE_*.zip -d itou/siaes/manag
 unzip -P $ASP_UNZIP_PASSWORD asp_shared_bucket/Liste_Contact_EA*.zip -d itou/siaes/management/commands/data/
 
 # Perform the necessary data imports
-OUTPUT_PATH=shared_bucket/imports-asp
 mkdir -p $OUTPUT_PATH/populate_metabase_fluxiae
-mkdir $OUTPUT_PATH/import_siae
-mkdir $OUTPUT_PATH/import_ea_eatt
+mkdir -p $OUTPUT_PATH/import_siae
+mkdir -p $OUTPUT_PATH/import_ea_eatt
+
 time ./manage.py populate_metabase_fluxiae --verbosity 2 |& tee -a "$OUTPUT_PATH/populate_metabase_fluxiae/output_$(date '+%Y-%m-%d_%H-%M-%S').log"
 time ./manage.py import_siae --verbosity=2 |& tee -a "$OUTPUT_PATH/import_siae/output_$(date '+%Y-%m-%d_%H-%M-%S').log"
 time ./manage.py import_ea_eatt --verbosity=2 |& tee -a "$OUTPUT_PATH/import_ea_eatt/output_$(date '+%Y-%m-%d_%H-%M-%S').log"
