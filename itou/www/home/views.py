d9d6a4131f37d591d3825544a96c83c4e26dc4bb
hellodeloo <hello@hellodeloo.com>
2022-04-05 15:25:15 +0200

    feat: add siae jobs loop

--- itou/www/home/views.py
+++ itou/www/home/views.py
@@ -2,6 +2,7 @@ import logging
 
 from django.shortcuts import render
 
+from itou.siaes.models import SiaeJobDescription
 from itou.www.search.forms import PrescriberSearchForm, SiaeSearchForm
 
 
@@ -9,7 +10,14 @@ logger = logging.getLogger(__name__)
 
 
 def home(request, template_name="home/home.html"):
-    context = {"siae_search_form": SiaeSearchForm(), "prescribers_search_form": PrescriberSearchForm()}
+    siae_jobs = SiaeJobDescription.objects.filter(is_active=True).order_by("-updated_at").all()[:6]
+    siae_jobs_count = SiaeJobDescription.objects.filter(is_active=True).count()
+    context = {
+        "siae_jobs_count": siae_jobs_count,
+        "siae_jobs": siae_jobs,
+        "siae_search_form": SiaeSearchForm(),
+        "prescribers_search_form": PrescriberSearchForm(),
+    }
     return render(request, template_name, context)
 
 
