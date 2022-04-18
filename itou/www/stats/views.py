725d1aea1bb6fb897e125b34b7ac893ddbb6f51b
dejafait <dejafait@blackhole.com>
2022-04-18 11:24:51 +0200

    Refactor and simplify stats views

--- itou/www/stats/views.py
+++ itou/www/stats/views.py
@@ -39,27 +39,64 @@ from itou.utils.perms.prescriber import get_current_org_or_404
 from itou.utils.perms.siae import get_current_siae_or_404
 
 
-# Each signed dashboard has the same look (at the moment)
-_STATS_HTML_TEMPLATE = "stats/stats.html"
+def get_stats_siae_current_org(request):
+    current_org = get_current_siae_or_404(request)
+    if not request.user.can_view_stats_siae(current_org=current_org):
+        raise PermissionDenied
+    return current_org
 
 
-def stats_public(request, template_name=_STATS_HTML_TEMPLATE):
+def get_stats_ddets_department(request):
+    current_org = get_current_institution_or_404(request)
+    if not request.user.can_view_stats_ddets(current_org=current_org):
+        raise PermissionDenied
+    department = request.user.get_stats_ddets_department(current_org=current_org)
+    return department
+
+
+def ensure_stats_dgefp_permission(request):
+    current_org = get_current_institution_or_404(request)
+    if not request.user.can_view_stats_dgefp(current_org=current_org):
+        raise PermissionDenied
+
+
+def get_params_for_departement(department):
+    return {
+        DEPARTMENT_FILTER_KEY: DEPARTMENTS[department],
+        REGION_FILTER_KEY: DEPARTMENT_TO_REGION[department],
+    }
+
+
+def get_params_for_whole_country():
+    return {
+        DEPARTMENT_FILTER_KEY: list(DEPARTMENTS.values()),
+        REGION_FILTER_KEY: list(REGIONS.keys()),
+    }
+
+
+def render_stats(request, context, params={}, template_name="stats/stats.html"):
+    base_context = {
+        "iframeurl": metabase_embedded_url(request=request, params=params),
+        "stats_base_url": settings.METABASE_SITE_URL,
+    }
+    # Key value pairs in context override preexisting pairs in base_context.
+    base_context.update(context)
+    return render(request, template_name, base_context)
+
+
+def stats_public(request):
     """
     Public basic stats (signed and embedded version)
-    Public link:
-    https://stats.inclusion.beta.gouv.fr/public/dashboard/f1527a13-1508-498d-8014-b2fe487a3a70
     """
     context = {
-        "iframeurl": metabase_embedded_url(request=request),
         "page_title": "Statistiques",
-        "stats_base_url": settings.METABASE_SITE_URL,
         "is_stats_public": True,
     }
-    return render(request, template_name, context)
+    return render_stats(request=request, context=context)
 
 
 @xframe_options_exempt
-def stats_pilotage(request, dashboard_id, template_name="stats/stats_pilotage.html"):
+def stats_pilotage(request, dashboard_id):
     """
     All these dashboard are publicly available on `PILOTAGE_SITE_URL`.
     We do it because we want to allow users to download chart data which
@@ -70,53 +107,50 @@ def stats_pilotage(request, dashboard_id, template_name="stats/stats_pilotage.ht
 
     context = {
         "iframeurl": metabase_embedded_url(dashboard_id=dashboard_id, with_title=True),
-        "stats_base_url": settings.METABASE_SITE_URL,
     }
-    return render(request, template_name, context)
+    return render_stats(request=request, context=context, template_name="stats/stats_pilotage.html")
 
 
 @login_required
-def stats_siae_etp(request, template_name=_STATS_HTML_TEMPLATE):
+def stats_siae_etp(request):
     """
     SIAE stats shown to their own members.
     They can only view data for their own SIAE.
     These stats are about ETP data from the ASP.
     """
-    current_org = get_current_siae_or_404(request)
-    if not request.user.can_view_stats_siae(current_org=current_org):
-        raise PermissionDenied
-    params = {ASP_SIAE_FILTER_KEY: current_org.convention.asp_id}
+    current_org = get_stats_siae_current_org(request)
     context = {
-        "iframeurl": metabase_embedded_url(request=request, params=params),
         "page_title": "Données de ma structure (extranet ASP)",
-        "stats_base_url": settings.METABASE_SITE_URL,
         "matomo_custom_url": f"/stats/siae/etp/{format_region_and_department_for_matomo(current_org.department)}",
     }
-    return render(request, template_name, context)
+    return render_stats(
+        request=request,
+        context=context,
+        params={ASP_SIAE_FILTER_KEY: current_org.convention.asp_id},
+    )
 
 
 @login_required
-def stats_siae_hiring(request, template_name=_STATS_HTML_TEMPLATE):
+def stats_siae_hiring(request):
     """
     SIAE stats shown to their own members.
     They can only view data for their own SIAE.
     These stats are about hiring and are built directly from C1 data.
     """
-    current_org = get_current_siae_or_404(request)
-    if not request.user.can_view_stats_siae(current_org=current_org):
-        raise PermissionDenied
-    params = {C1_SIAE_FILTER_KEY: str(current_org.id)}
+    current_org = get_stats_siae_current_org(request)
     context = {
-        "iframeurl": metabase_embedded_url(request=request, params=params),
         "page_title": "Données de recrutement de ma structure (Plateforme de l'inclusion)",
-        "stats_base_url": settings.METABASE_SITE_URL,
         "matomo_custom_url": f"/stats/siae/hiring/{format_region_and_department_for_matomo(current_org.department)}",
     }
-    return render(request, template_name, context)
+    return render_stats(
+        request=request,
+        context=context,
+        params={C1_SIAE_FILTER_KEY: str(current_org.id)},
+    )
 
 
 @login_required
-def stats_cd(request, template_name=_STATS_HTML_TEMPLATE):
+def stats_cd(request):
     """
     CD ("Conseil Départemental") stats shown to relevant members.
     They can only view data for their own departement.
@@ -125,175 +159,129 @@ def stats_cd(request, template_name=_STATS_HTML_TEMPLATE):
     if not request.user.can_view_stats_cd(current_org=current_org):
         raise PermissionDenied
     department = request.user.get_stats_cd_department(current_org=current_org)
-    params = {
-        DEPARTMENT_FILTER_KEY: DEPARTMENTS[department],
-        REGION_FILTER_KEY: DEPARTMENT_TO_REGION[department],
-    }
+    params = get_params_for_departement(department)
     context = {
-        "iframeurl": metabase_embedded_url(request=request, params=params),
         "page_title": f"Données de mon département : {DEPARTMENTS[department]}",
-        "stats_base_url": settings.METABASE_SITE_URL,
         "matomo_custom_url": f"/stats/cd/{format_region_and_department_for_matomo(department)}",
     }
-    return render(request, template_name, context)
+    return render_stats(request=request, context=context, params=params)
 
 
 @login_required
-def stats_ddets_iae(request, template_name=_STATS_HTML_TEMPLATE):
+def stats_ddets_iae(request):
     """
     DDETS ("Directions départementales de l’emploi, du travail et des solidarités") stats shown to relevant members.
     They can only view data for their own departement.
     This dashboard shows data about IAE in general.
     """
-    current_institution = get_current_institution_or_404(request)
-    if not request.user.can_view_stats_ddets(current_org=current_institution):
-        raise PermissionDenied
-    department = request.user.get_stats_ddets_department(current_org=current_institution)
-    params = {
-        DEPARTMENT_FILTER_KEY: DEPARTMENTS[department],
-        REGION_FILTER_KEY: DEPARTMENT_TO_REGION[department],
-    }
+    department = get_stats_ddets_department(request)
+    params = get_params_for_departement(department)
     context = {
-        "iframeurl": metabase_embedded_url(request=request, params=params),
         "page_title": f"Données de mon département : {DEPARTMENTS[department]}",
-        "stats_base_url": settings.METABASE_SITE_URL,
         "matomo_custom_url": f"/stats/ddets/iae/{format_region_and_department_for_matomo(department)}",
     }
-    return render(request, template_name, context)
+    return render_stats(request=request, context=context, params=params)
 
 
 @login_required
-def stats_ddets_diagnosis_control(request, template_name=_STATS_HTML_TEMPLATE):
+def stats_ddets_diagnosis_control(request):
     """
     DDETS ("Directions départementales de l’emploi, du travail et des solidarités") stats shown to relevant members.
     They can only view data for their own departement.
     This dashboard shows data about diagnosis control ("Contrôle a posteriori").
     """
-    current_institution = get_current_institution_or_404(request)
-    if not request.user.can_view_stats_ddets(current_org=current_institution):
-        raise PermissionDenied
-    department = request.user.get_stats_ddets_department(current_org=current_institution)
-    params = {
-        DEPARTMENT_FILTER_KEY: DEPARTMENTS[department],
-        REGION_FILTER_KEY: DEPARTMENT_TO_REGION[department],
-    }
+    department = get_stats_ddets_department(request)
+    params = get_params_for_departement(department)
     context = {
-        "iframeurl": metabase_embedded_url(request=request, params=params),
         "page_title": "Données 2021 du contrôle a posteriori",
-        "stats_base_url": settings.METABASE_SITE_URL,
         "back_url": reverse("siae_evaluations_views:samples_selection"),
         "show_diagnosis_control_message": True,
         "matomo_custom_url": f"/stats/ddets/diagnosis_control/{format_region_and_department_for_matomo(department)}",
     }
-    return render(request, template_name, context)
+    return render_stats(request=request, context=context, params=params)
 
 
 @login_required
-def stats_ddets_hiring(request, template_name=_STATS_HTML_TEMPLATE):
+def stats_ddets_hiring(request):
     """
     DDETS ("Directions départementales de l’emploi, du travail et des solidarités") stats shown to relevant members.
     They can only view data for their own departement.
     This dashboard shows data about hiring ("Facilitation de l'embauche").
     """
-    current_institution = get_current_institution_or_404(request)
-    if not request.user.can_view_stats_ddets(current_org=current_institution):
-        raise PermissionDenied
-    department = request.user.get_stats_ddets_department(current_org=current_institution)
+    department = get_stats_ddets_department(request)
     params = {
         DEPARTMENT_FILTER_KEY: DEPARTMENTS[department],
     }
     context = {
-        "iframeurl": metabase_embedded_url(request=request, params=params),
         "page_title": f"Données facilitation de l'embauche de mon département : {DEPARTMENTS[department]}",
-        "stats_base_url": settings.METABASE_SITE_URL,
         "matomo_custom_url": f"/stats/ddets/hiring/{format_region_and_department_for_matomo(department)}",
     }
-    return render(request, template_name, context)
+    return render_stats(request=request, context=context, params=params)
 
 
 @login_required
-def stats_dreets_iae(request, template_name=_STATS_HTML_TEMPLATE):
+def stats_dreets_iae(request):
     """
     DREETS ("Directions régionales de l’économie, de l’emploi, du travail et des solidarités") stats shown to
     relevant members. They can only view data for their own region and can filter by department.
     This dashboard shows data about IAE in general.
     """
-    current_institution = get_current_institution_or_404(request)
-    if not request.user.can_view_stats_dreets(current_org=current_institution):
+    current_org = get_current_institution_or_404(request)
+    if not request.user.can_view_stats_dreets(current_org=current_org):
         raise PermissionDenied
-    region = request.user.get_stats_dreets_region(current_org=current_institution)
+    region = request.user.get_stats_dreets_region(current_org=current_org)
     departments = [DEPARTMENTS[dpt] for dpt in REGIONS[region]]
     params = {
         DEPARTMENT_FILTER_KEY: departments,
         REGION_FILTER_KEY: region,
     }
     context = {
-        "iframeurl": metabase_embedded_url(request=request, params=params),
         "page_title": f"Données de ma région : {region}",
-        "stats_base_url": settings.METABASE_SITE_URL,
         "matomo_custom_url": f"/stats/dreets/iae/{format_region_for_matomo(region)}",
     }
-    return render(request, template_name, context)
+    return render_stats(request=request, context=context, params=params)
 
 
 @login_required
-def stats_dgefp_iae(request, template_name=_STATS_HTML_TEMPLATE):
+def stats_dgefp_iae(request):
     """
     DGEFP ("délégation générale à l'Emploi et à la Formation professionnelle") stats shown to relevant members.
     They can view all data and filter by region and/or department.
     This dashboard shows data about IAE in general.
     """
-    current_institution = get_current_institution_or_404(request)
-    if not request.user.can_view_stats_dgefp(current_org=current_institution):
-        raise PermissionDenied
-    params = {
-        DEPARTMENT_FILTER_KEY: list(DEPARTMENTS.values()),
-        REGION_FILTER_KEY: list(REGIONS.keys()),
-    }
+    ensure_stats_dgefp_permission(request)
+    params = get_params_for_whole_country()
     context = {
-        "iframeurl": metabase_embedded_url(request=request, params=params),
         "page_title": "Données des régions",
-        "stats_base_url": settings.METABASE_SITE_URL,
     }
-    return render(request, template_name, context)
+    return render_stats(request=request, context=context, params=params)
 
 
 @login_required
-def stats_dgefp_diagnosis_control(request, template_name=_STATS_HTML_TEMPLATE):
+def stats_dgefp_diagnosis_control(request):
     """
     DGEFP ("délégation générale à l'Emploi et à la Formation professionnelle") stats shown to relevant members.
     They can view all data and filter by region and/or department.
     This dashboard shows data about diagnosis control ("Contrôle a posteriori").
     """
-    current_institution = get_current_institution_or_404(request)
-    if not request.user.can_view_stats_dgefp(current_org=current_institution):
-        raise PermissionDenied
-    params = {
-        DEPARTMENT_FILTER_KEY: list(DEPARTMENTS.values()),
-        REGION_FILTER_KEY: list(REGIONS.keys()),
-    }
+    ensure_stats_dgefp_permission(request)
+    params = get_params_for_whole_country()
     context = {
-        "iframeurl": metabase_embedded_url(request=request, params=params),
         "page_title": "Données 2021 (version bêta) du contrôle a posteriori",
-        "stats_base_url": settings.METABASE_SITE_URL,
         "show_diagnosis_control_message": True,
     }
-    return render(request, template_name, context)
+    return render_stats(request=request, context=context, params=params)
 
 
 @login_required
-def stats_dgefp_af(request, template_name=_STATS_HTML_TEMPLATE):
+def stats_dgefp_af(request):
     """
     DGEFP ("délégation générale à l'Emploi et à la Formation professionnelle") stats shown to relevant members.
     They can view all data and filter by region and/or department.
     This dashboard shows data about financial annexes ("af").
     """
-    current_institution = get_current_institution_or_404(request)
-    if not request.user.can_view_stats_dgefp(current_org=current_institution):
-        raise PermissionDenied
+    ensure_stats_dgefp_permission(request)
     context = {
-        "iframeurl": metabase_embedded_url(request=request),
         "page_title": "Annexes financières actives",
-        "stats_base_url": settings.METABASE_SITE_URL,
     }
-    return render(request, template_name, context)
+    return render_stats(request=request, context=context)
