224c75d7ae82ad8920a7adf1724491a0c8b6188b
vincent porte <vincent@neuralia.co>
2022-04-20 08:24:51 +0200

    add Siae Job Application List view

--- itou/www/siae_evaluations_views/tests.py
+++ itou/www/siae_evaluations_views/tests.py
@@ -1,11 +1,16 @@
 from django.test import TestCase
 from django.urls import reverse
-from django.utils import timezone
+from django.utils import dateformat, timezone
 
 from itou.institutions.factories import InstitutionMembershipFactory
 from itou.siae_evaluations import enums as evaluation_enums
-from itou.siae_evaluations.factories import EvaluationCampaignFactory
+from itou.siae_evaluations.factories import (
+    EvaluatedJobApplicationFactory,
+    EvaluatedSiaeFactory,
+    EvaluationCampaignFactory,
+)
 from itou.siae_evaluations.models import EvaluationCampaign
+from itou.siaes.factories import SiaeMembershipFactory
 from itou.users.factories import DEFAULT_PASSWORD
 from itou.www.siae_evaluations_views.forms import SetChosenPercentForm
 
@@ -101,3 +106,49 @@ class SamplesSelectionViewTest(TestCase):
         updated_evaluation_campaign = EvaluationCampaign.objects.get(pk=evaluation_campaign.pk)
         self.assertIsNotNone(updated_evaluation_campaign.percent_set_at)
         self.assertEqual(updated_evaluation_campaign.chosen_percent, post_data["chosen_percent"])
+
+
+class SiaeJobApplicationListViewTest(TestCase):
+    def setUp(self):
+        membership = SiaeMembershipFactory()
+        self.user = membership.user
+        self.siae = membership.siae
+        self.url = reverse("siae_evaluations_views:siae_job_applications_list")
+
+    def test_access(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 302)
+
+        # siae without active campaign
+        self.client.login(username=self.user.email, password=DEFAULT_PASSWORD)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(response.context["evaluations_asked_at"])
+        self.assertFalse(response.context["evaluated_job_applications"])
+        self.assertContains(response, "Vous n'avez aucun contrôle en cours.")
+
+        # siae with active campaign
+        evaluated_siae = EvaluatedSiaeFactory(evaluation_campaign__evaluations_asked_at=timezone.now(), siae=self.siae)
+        evaluated_job_application = EvaluatedJobApplicationFactory(evaluated_siae=evaluated_siae)
+
+        with self.assertNumQueries(10):
+            response = self.client.get(self.url)
+
+        self.assertEqual(
+            evaluated_siae.evaluation_campaign.evaluations_asked_at, response.context["evaluations_asked_at"]
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            evaluated_job_application,
+            response.context["evaluated_job_applications"][0],
+        )
+        self.assertEqual(
+            reverse("dashboard:index"),
+            response.context["back_url"],
+        )
+
+        self.assertContains(
+            response,
+            f"Contrôle initié le "
+            f"{dateformat.format(evaluated_siae.evaluation_campaign.evaluations_asked_at, 'd E Y').lower()}",
+        )
