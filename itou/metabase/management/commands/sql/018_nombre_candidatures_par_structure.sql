343e493fa5be2321d168eca136335d91c1c76cb5
Yannick Passarelli <99667699+YannickPassa@users.noreply.github.com>
2022-04-21 09:27:43 +0200

    Update 018_nombre_candidatures_par_structure.sql
    Ajout filtre par structures

--- itou/metabase/management/commands/sql/018_nombre_candidatures_par_structure.sql
+++ itou/metabase/management/commands/sql/018_nombre_candidatures_par_structure.sql
@@ -21,7 +21,7 @@ with z_candidatures as (
 	c.nom_org_prescripteur	
     from 
 	public.candidatures c 
-    where injection_ai = 0
+    where injection_ai = 0 and type_structure IN ('AI', 'ACI', 'EI', 'EITI', 'ETTI') 
     group by 
 	état,
 	c.date_candidature,
