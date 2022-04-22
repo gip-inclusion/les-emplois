bac75fb6bba6b4f34e4f2e93ad77f9174fef86a0
vincent porte <vincent@neuralia.co>
2022-04-22 10:52:06 +0200

    new feat : siae_select_criteria view & template

--- itou/www/siae_evaluations_views/urls.py
+++ itou/www/siae_evaluations_views/urls.py
@@ -9,4 +9,9 @@ app_name = "siae_evaluations_views"
 urlpatterns = [
     path("samples_selection", views.samples_selection, name="samples_selection"),
     path("siae_job_applications_list", views.siae_job_applications_list, name="siae_job_applications_list"),
+    path(
+        "siae_select_criteria/<int:evaluated_job_application_pk>/",
+        views.siae_select_criteria,
+        name="siae_select_criteria",
+    ),
 ]
--- itou/www/siae_evaluations_views/views.py
+++ itou/www/siae_evaluations_views/views.py
@@ -2,7 +2,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Min
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.utils import timezone
 
@@ -11,6 +11,7 @@ from itou.siae_evaluations.models import EvaluatedJobApplication, EvaluatedSiae,
 from itou.utils.perms.institution import get_current_institution_or_404
 from itou.utils.perms.siae import get_current_siae_or_404
 from itou.utils.urls import get_safe_url
+from itou.www.eligibility_views.forms import AdministrativeCriteriaOfJobApplicationForm
 from itou.www.siae_evaluations_views.forms import SetChosenPercentForm
 
 
@@ -69,6 +70,66 @@ def siae_job_applications_list(request, template_name="siae_evaluations/siae_job
     context = {
         "evaluated_job_applications": evaluated_job_applications,
         "evaluations_asked_at": evaluations_asked_at,
+        "back_url": back_url,
+    }
+    return render(request, template_name, context)
+
+
+@login_required
+def siae_select_criteria(
+    request, evaluated_job_application_pk, template_name="siae_evaluations/siae_select_criteria.html"
+):
+    siae = get_current_siae_or_404(request)
+    evaluated_job_application = get_object_or_404(
+        EvaluatedJobApplication, pk=evaluated_job_application_pk, evaluated_siae__siae=siae
+    )
+    initial_data = {
+        eval_criterion.administrative_criteria.key: True
+        for eval_criterion in evaluated_job_application.evaluated_eligibility_diagnoses.all()
+    }
+
+    form_administrative_criteria = AdministrativeCriteriaOfJobApplicationForm(
+        request.user,
+        siae=siae,
+        job_application=evaluated_job_application.job_application,
+        data=request.POST or None,
+        initial=initial_data,
+    )
+
+    if request.method == "POST" and form_administrative_criteria.is_valid():
+        evaluated_job_application.save_selected_criteria(
+            cleaned_keys=[
+                administrative_criteria.key for administrative_criteria in form_administrative_criteria.cleaned_data
+            ],
+            changed_keys=form_administrative_criteria.changed_data,
+        )
+
+        messages.success(request, "Votre sélection est prise en compte")
+
+        next_url = reverse("dashboard:index")
+        return HttpResponseRedirect(next_url)
+
+    level_1_fields = [
+        field
+        for field in form_administrative_criteria
+        if AdministrativeCriteriaOfJobApplicationForm.LEVEL_1_PREFIX in field.name
+    ]
+    level_2_fields = [
+        field
+        for field in form_administrative_criteria
+        if AdministrativeCriteriaOfJobApplicationForm.LEVEL_2_PREFIX in field.name
+    ]
+
+    back_url = get_safe_url(
+        request, "back_url", fallback_url=reverse("siae_evaluations_views:siae_job_applications_list")
+    )
+
+    context = {
+        "job_seeker": evaluated_job_application.job_application.job_seeker,
+        "approval": evaluated_job_application.job_application.approval,
+        "form_administrative_criteria": form_administrative_criteria,
+        "level_1_fields": level_1_fields,
+        "level_2_fields": level_2_fields,
         "back_url": back_url,
     }
     return render(request, template_name, context)
