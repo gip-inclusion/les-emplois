3d76bcca9dc596b5eba57fef3dbccd43e0175f0c
Victor Perron <victor@iso3103.net>
2022-04-26 17:44:46 +0200

    JobApplication: add logging to PE API calls

--- itou/job_applications/tasks.py
+++ itou/job_applications/tasks.py
@@ -1,9 +1,16 @@
+import logging
 from time import sleep
 
 from django.utils import timezone
 from huey.contrib.djhuey import db_task
 
 from itou.utils.apis.pole_emploi import PoleEmploiIndividu, PoleEmploiMiseAJourPassIAEException, mise_a_jour_pass_iae
+
+
+# 3 requests/second max. There were timeout issues so 1 second takes some margins
+SLEEP_DELAY = 1.0
+
+logger = logging.getLogger(__name__)
 
 
 def notify_pole_emploi_pass(job_application, job_seeker):
@@ -28,6 +35,7 @@ def notify_pole_emploi_pass(job_application, job_seeker):
     # can’t handle them. I’ll keep my opinion about this for talks that involve an unreasonnable amount of beer.
     # Another mechanism will be in charge of sending them on their start date
     if job_application.approval.start_at > timezone.now().date():
+        logger.info("! job_application starts after today, skipping.")
         return False
     from itou.job_applications.models import JobApplicationPoleEmploiNotificationLog
 
@@ -35,6 +43,7 @@ def notify_pole_emploi_pass(job_application, job_seeker):
     if individual is None or not individual.is_valid():
         # We may not have a valid user (missing NIR, for instance),
         # in which case we can bypass this process entirely
+        logger.info("! job_application had an invalid user, skipping.")
         return False
     log = JobApplicationPoleEmploiNotificationLog(
         job_application=job_application, status=JobApplicationPoleEmploiNotificationLog.STATUS_OK
@@ -42,18 +51,21 @@ def notify_pole_emploi_pass(job_application, job_seeker):
     # Step 1: we get the API token
     try:
         token = JobApplicationPoleEmploiNotificationLog.get_token()
-        sleep(1)
+        sleep(SLEEP_DELAY)
     except Exception as e:
+        logger.error("! fetching token raised exception=%s", e)
         log.status = JobApplicationPoleEmploiNotificationLog.STATUS_FAIL_AUTHENTICATION
         log.details = str(e)
         log.save()
         return False
+
     # Step 2 : we fetch the encrypted NIR
     try:
         encrypted_nir = JobApplicationPoleEmploiNotificationLog.get_encrypted_nir_from_individual(individual, token)
-        # 3 requests/second max. I had timeout issues so 1 second takes some margins
-        sleep(1)
+        logger.info("> got encrypted_nir=%s", encrypted_nir)
+        sleep(SLEEP_DELAY)
     except PoleEmploiMiseAJourPassIAEException as e:
+        logger.error("! fetching encrypted NIR raised http_code=%s message=%s", e.http_code, e.response_code)
         log = JobApplicationPoleEmploiNotificationLog(
             job_application=job_application,
             status=JobApplicationPoleEmploiNotificationLog.STATUS_FAIL_SEARCH_INDIVIDUAL,
@@ -74,8 +86,10 @@ def notify_pole_emploi_pass(job_application, job_seeker):
     # Step 3: we finally notify Pole Emploi that something happened for this user
     try:
         mise_a_jour_pass_iae(job_application, encrypted_nir, token)
-        sleep(1)
+        logger.info("> pass nir=%s updated successfully", job_application.job_seeker.nir)
+        sleep(SLEEP_DELAY)
     except PoleEmploiMiseAJourPassIAEException as e:
+        logger.error("! updating pass iae raised http_code=%s message=%s", e.http_code, e.response_code)
         log.status = JobApplicationPoleEmploiNotificationLog.STATUS_FAIL_NOTIFY_POLE_EMPLOI
         # We log the encrypted nir in case its not empty but
         # it leads to an E_ERR_EX042_PROBLEME_DECHIFFREMEMENT anyway
