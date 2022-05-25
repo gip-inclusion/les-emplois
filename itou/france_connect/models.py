866055a16cf7babe994961047da4cea80d1a3914
Céline MS <celine@hello-birds.com>
2022-05-25 17:54:18 +0200

    Code review updates

--- itou/france_connect/models.py
+++ itou/france_connect/models.py
@@ -12,6 +12,8 @@ class FranceConnectState(OIDConnectState):
 
 @dataclasses.dataclass
 class FranceConnectUserData(OIDConnectUserData):  # pylint: disable=too-many-instance-attributes
+    # Attributes are User model ones.
+    # Mapping is made in self.user_info_mapping_dict.
     phone: Optional[str] = None
     birthdate: Optional[datetime.date] = None
     address_line_1: Optional[str] = None
@@ -20,13 +22,17 @@ class FranceConnectUserData(OIDConnectUserData):  # pylint: disable=too-many-ins
     is_job_seeker: bool = True
     identity_provider: IdentityProvider = IdentityProvider.FRANCE_CONNECT
 
-    @classmethod
-    def from_user_info(cls, user_info: dict):
-        attrs = {
-            "username": user_info["sub"],
-            "email": user_info["email"],
-            "first_name": user_info.get("given_name"),
-            "last_name": user_info.get("family_name"),
+    @staticmethod
+    def user_info_mapping_dict(user_info: dict):
+        """
+        Map Django-User class attributes to the identity provider ones.
+        See https://openid.net/specs/openid-connect-core-1_0.html#StandardClaims
+        """
+        # Python 3's zero-argument super() only works in class or instance methods.
+        standard_attrs = super(FranceConnectUserData, FranceConnectUserData).user_info_mapping_dict(
+            user_info=user_info
+        )
+        attrs = standard_attrs | {
             "birthdate": datetime.date.fromisoformat(user_info["birthdate"]) if user_info.get("birthdate") else None,
             "phone": user_info.get("phone_number"),
         }
@@ -37,5 +43,4 @@ class FranceConnectUserData(OIDConnectUserData):  # pylint: disable=too-many-ins
                     "post_code": user_info["address"].get("postal_code"),
                     "city": user_info["address"].get("locality"),
                 }
-
-        return cls(**attrs)
+        return attrs
