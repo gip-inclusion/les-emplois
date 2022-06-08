c16a57a0a55fe250cc003da9bf2b367519574289
SoumiaAhl <79250336+SoumiaAHLOUCH@users.noreply.github.com>
2022-06-08 16:26:41 +0200

    Create 021_tx_candidats_acceptes_par_prescripteurs.sql

---
+++ itou/metabase/management/commands/sql/021_tx_candidats_acceptes_par_prescripteurs.sql
@@ -0,0 +1,18 @@
+select 
+    origine_détaillée, 
+    date_candidature, 
+    count(distinct(id_candidat_anonymisé)) filter (where(état = 'Candidature acceptée')) as nombre_candidats_acceptés,
+    count(distinct(id_candidat_anonymisé)) as nombre_candidats,
+    count(distinct(id_anonymisé)) filter (where(état = 'Candidature acceptée')) as nombre_candidatures_acceptées,
+    count(distinct(id_anonymisé)) as nombre_candidatures
+from 
+    candidatures
+where 
+    (origine <> 'Candidat' or origine is null)
+    and (origine <> 'Employeur' or origine is null) 
+    and (lower(origine_détaillée) like 'prescripteur%')
+    and injection_ai = 0
+group by 
+    origine_détaillée, 
+    date_candidature
+   
