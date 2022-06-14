d981b839a5fa09185cd98eea76c29f492c556170
vincent porte <vincent@neuralia.co>
2022-06-14 11:28:25 +0200

    emails content and its management command

---
+++ itou/siae_evaluations/management/commands/patch_siae_emails.py
@@ -0,0 +1,34 @@
+from django.core import mail
+from django.core.management.base import BaseCommand
+
+from itou.siae_evaluations.models import EvaluatedSiae
+from itou.utils.emails import get_email_message
+
+
+def get_email_patch_siae(to):
+    context = {}
+    subject = "siae_evaluations/email/patch_siaes_subject.txt"
+    body = "siae_evaluations/email/patch_siaes_body.txt"
+    return get_email_message(to, context, subject, body)
+
+
+class Command(BaseCommand):
+    """
+    To send emails:
+        django-admin patch_siae_emails
+    """
+
+    help = "Send emails to SIAE admin users, if their SIAE is in named evaluation_campaign and in selected state"
+
+    def handle(self, **options):
+
+        evaluation_campaign_name = "Campagne de janvier à décembre 2021 - partie 2"
+        evsiaes = (
+            evsiae
+            for evsiae in EvaluatedSiae.objects.filter(evaluation_campaign__name=evaluation_campaign_name)
+            if evsiae.state in ["PENDING", "SUBMITTABLE"]
+        )
+        emails = (get_email_patch_siae(evsiae.siae.active_admin_members) for evsiae in evsiaes)
+
+        connection = mail.get_connection()
+        connection.send_messages(emails)
