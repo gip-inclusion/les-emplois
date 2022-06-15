331233c6112ab23f31330fdd444f74cffa0dfc84
Céline MS <celine@hello-birds.com>
2022-06-15 18:55:25 +0200

    Patch Allauth magic
    Curiously, this allauth adapter does not take into account the User account
    adapter configured in settings.ACCOUNT_ADAPTER.
    Successful signups are redirected to settings.LOGIN_REDIRECT_URL
    (default Allauth behavior) even if we specified another URL in our
    own account adapter.
    I was not able to test a login locally because PE does not allow us to
    specify the callback domains in its interface.
    The most secure option was to simply redirect the default path to our own.

--- itou/allauth_adapters/peamu/urls.py
+++ itou/allauth_adapters/peamu/urls.py
@@ -1,16 +1,19 @@
 from django.urls import include, re_path
 
+from itou.allauth_adapters.peamu import views as peamu_views
 from itou.allauth_adapters.peamu.provider import PEAMUProvider
-from itou.allauth_adapters.peamu.views import oauth2_callback as callback_view, oauth2_login as login_view
 
 
 def default_urlpatterns(provider):
     urlpatterns = [
-        re_path(r"^login/$", login_view, name=provider.id + "_login"),
-        re_path(r"^login/callback/$", callback_view, name=provider.id + "_callback"),
+        re_path(r"^login/$", peamu_views.oauth2_login, name=provider.id + "_login"),
+        re_path(r"^login/callback/$", peamu_views.oauth2_callback, name=provider.id + "_callback"),
     ]
 
-    return [re_path(r"^" + provider.get_slug() + r"/", include(urlpatterns))]
+    return [
+        re_path(r"^profile/$", peamu_views.redirect_to_dashboard_view),
+        re_path(r"^" + provider.get_slug() + r"/", include(urlpatterns)),
+    ]
 
 
 urlpatterns = default_urlpatterns(PEAMUProvider)
