f37736aa9d425f73ec9f86318f92deb0f3572a2e
vincent porte <vincent@neuralia.co>
2022-06-27 17:20:08 +0200

    add `Inlines` and enhance navigation logics in admin
    fix missing `EvaluatedAdministrativeCriteriaInline`

--- itou/siae_evaluations/admin.py
+++ itou/siae_evaluations/admin.py
@@ -1,11 +1,74 @@
 from django.contrib import admin, messages
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
 from itou.siae_evaluations import models
 
 
+class EvaluatedSiaesInline(admin.TabularInline):
+    model = models.EvaluatedSiae
+    fields = ("id_link", "reviewed_at", "state")
+    readonly_fields = ("id_link", "reviewed_at", "state")
+    extra = 0
+
+    def state(self, obj):
+        return obj.state
+
+    def id_link(self, obj):
+        app_label = obj._meta.app_label
+        model_name = obj._meta.model_name
+        url = reverse(f"admin:{app_label}_{model_name}_change", args=[obj.id])
+        return mark_safe(f'<a href="{url}">Lien vers la Siae évaluée <strong>{obj}</strong></a>')
+
+    id_link.short_description = "Lien vers les Siaes évaluées"
+
+
+class EvaluatedJobApplicationsInline(admin.TabularInline):
+    model = models.EvaluatedJobApplication
+    fields = ("id_link", "approval", "job_seeker", "state")
+    readonly_fields = ("id_link", "approval", "job_seeker", "state")
+    extra = 0
+
+    def state(self, obj):
+        return obj.state
+
+    def id_link(self, obj):
+        app_label = obj._meta.app_label
+        model_name = obj._meta.model_name
+        url = reverse(f"admin:{app_label}_{model_name}_change", args=[obj.id])
+        return mark_safe(f'<a href="{url}">Lien vers la candidature évaluée <strong>{obj}</strong></a>')
+
+    id_link.short_description = "Lien vers les candidatures évaluées"
+
+    def approval(self, obj):
+        if obj.job_application.approval:
+            return obj.job_application.approval.number
+        return "-"
+
+    def job_seeker(self, obj):
+        if obj.job_application.job_seeker:
+            return obj.job_application.job_seeker
+        return "-"
+
+
+class EvaluatedAdministrativeCriteriaInline(admin.TabularInline):
+    model = models.EvaluatedAdministrativeCriteria
+    fields = ("id_link", "uploaded_at", "submitted_at", "review_state")
+    readonly_fields = ("id_link", "uploaded_at", "submitted_at", "review_state")
+    extra = 0
+
+    def id_link(self, obj):
+        app_label = obj._meta.app_label
+        model_name = obj._meta.model_name
+        url = reverse(f"admin:{app_label}_{model_name}_change", args=[obj.id])
+        return mark_safe(f'<a href="{url}">Lien vers le critère administratif <strong>{obj}</strong></a>')
+
+    id_link.short_description = "Lien vers les critères administratifs évalués"
+
+
 @admin.register(models.EvaluationCampaign)
 class EvaluationCampaignAdmin(admin.ModelAdmin):
-    @admin.action(description="Passer les campagnes en phase contradictiore")
+    @admin.action(description="Passer les campagnes en phase contradictoire")
     def transition_to_adversarial_phase(self, request, queryset):
         for campaign in queryset:
             campaign.transition_to_adversarial_phase()
@@ -19,7 +82,6 @@ class EvaluationCampaignAdmin(admin.ModelAdmin):
         )
 
     actions = [transition_to_adversarial_phase]
-
     list_display = (
         "name",
         "institution",
@@ -30,41 +92,74 @@ class EvaluationCampaignAdmin(admin.ModelAdmin):
         "evaluations_asked_at",
         "ended_at",
     )
-
     list_display_links = ("name",)
-    readonly_fields = ("created_at",)
-    raw_id_fields = ("institution",)
+    readonly_fields = (
+        "institution",
+        "evaluated_period_start_at",
+        "evaluated_period_end_at",
+        "chosen_percent",
+        "created_at",
+        "percent_set_at",
+        "evaluations_asked_at",
+        "ended_at",
+    )
+    list_filter = (
+        "evaluations_asked_at",
+        "ended_at",
+        "institution__department",
+    )
+    inlines = [
+        EvaluatedSiaesInline,
+    ]
 
 
 @admin.register(models.EvaluatedSiae)
 class EvaluatedSiae(admin.ModelAdmin):
     list_display = ("evaluation_campaign", "siae", "reviewed_at")
-    list_display_links = (
-        "evaluation_campaign",
-        "siae",
+    list_display_links = ("siae",)
+    readonly_fields = ("evaluation_campaign", "siae", "reviewed_at", "state")
+    list_filter = (
+        "reviewed_at",
+        "evaluation_campaign__institution__department",
     )
-    raw_id_fields = (
-        "siae",
-        "evaluation_campaign",
-    )
+    search_fields = ("siae__name",)
+    inlines = [
+        EvaluatedJobApplicationsInline,
+    ]
+
+    def state(self, obj):
+        return obj.state
 
 
 @admin.register(models.EvaluatedJobApplication)
 class EvaluatedJobApplication(admin.ModelAdmin):
-    list_display = ("evaluated_siae", "job_application")
-    list_display_links = ("evaluated_siae", "job_application")
-    raw_id_fields = (
-        "job_application",
-        "evaluated_siae",
-    )
+    list_display = ("evaluated_siae", "job_application", "approval", "job_seeker")
+    list_display_links = ("job_application",)
+    readonly_fields = ("evaluated_siae", "job_application", "approval", "job_seeker")
+    inlines = [
+        EvaluatedAdministrativeCriteriaInline,
+    ]
+
+    def approval(self, obj):
+        if obj.job_application.approval:
+            return obj.job_application.approval.number
+        return "-"
+
+    def job_seeker(self, obj):
+        if obj.job_application.job_seeker:
+            return obj.job_application.job_seeker
+        return "-"
 
 
 @admin.register(models.EvaluatedAdministrativeCriteria)
 class EvaluatedAdministrativeCriteria(admin.ModelAdmin):
     list_display = ("evaluated_job_application", "administrative_criteria", "submitted_at", "review_state")
-    list_display_links = ("evaluated_job_application", "administrative_criteria")
-    readonly_fields = ("uploaded_at", "proof_url", "submitted_at", "review_state")
-    raw_id_fields = (
+    list_display_links = ("administrative_criteria",)
+    readonly_fields = (
         "evaluated_job_application",
         "administrative_criteria",
+        "uploaded_at",
+        "proof_url",
+        "submitted_at",
+        "review_state",
     )
