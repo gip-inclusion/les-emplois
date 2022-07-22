8da12dfcbe4edfe855277bc7a7a30a5b31db74bf
Céline MS <celine@hello-birds.com>
2022-07-22 09:58:52 +0200

    Finding a user by NIR should be case insensitive

---
+++ itou/www/apply/tests/tests_forms.py
@@ -0,0 +1,40 @@
+from django.test import TestCase
+
+from itou.users.factories import JobSeekerFactory
+from itou.www.apply import forms as apply_forms
+
+
+class CheckJobSeekerNirFormTest(TestCase):
+    def test_form_job_seeker_not_found(self):
+        # This NIR is unique.
+        nir = JobSeekerFactory.build().nir
+        form_data = {"nir": nir}
+        form = apply_forms.CheckJobSeekerNirForm(data=form_data)
+        self.assertTrue(form.is_valid())
+        self.assertIsNone(form.job_seeker)
+
+    def test_form_job_seeker_found(self):
+        # A job seeker with this NIR already exists.
+        nir = "141062A78200555"
+        job_seeker = JobSeekerFactory(nir=nir)
+        form_data = {"nir": job_seeker.nir}
+        form = apply_forms.CheckJobSeekerNirForm(data=form_data)
+        # A job seeker has been found.
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.job_seeker, job_seeker)
+
+        # NIR should be case insensitive.
+        form_data = {"nir": job_seeker.nir.lower()}
+        form = apply_forms.CheckJobSeekerNirForm(data=form_data)
+        # A job seeker has been found.
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.job_seeker, job_seeker)
+
+    def test_form_not_valid(self):
+        # Application sent by a job seeker whose NIR is already used by another account.
+        existing_account = JobSeekerFactory()
+        user = JobSeekerFactory()
+        form_data = {"nir": existing_account.nir}
+        form = apply_forms.CheckJobSeekerNirForm(job_seeker=user, data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertIn("Ce numéro de sécurité sociale est déjà utilisé par un autre compte.", form.errors["nir"][0])
