1418b12916f249b520373948790f6f440fd9231f
dejafait <dejafait@blackhole.com>
2022-08-01 15:15:53 +0200

    Never use URL trailing slash anywhere

--- itou/www/stats/urls.py
+++ itou/www/stats/urls.py
@@ -8,12 +8,12 @@ app_name = "stats"
 urlpatterns = [
     # Public stats.
     path("", views.stats_public, name="stats_public"),
-    path("pilotage/<int:dashboard_id>/", views.stats_pilotage, name="stats_pilotage"),
+    path("pilotage/<int:dashboard_id>", views.stats_pilotage, name="stats_pilotage"),
     # Employer stats.
-    path("siae/etp/", views.stats_siae_etp, name="stats_siae_etp"),
-    path("siae/hiring/", views.stats_siae_hiring, name="stats_siae_hiring"),
+    path("siae/etp", views.stats_siae_etp, name="stats_siae_etp"),
+    path("siae/hiring", views.stats_siae_hiring, name="stats_siae_hiring"),
     # Prescriber stats.
-    path("cd/", views.stats_cd, name="stats_cd"),
+    path("cd", views.stats_cd, name="stats_cd"),
     path("pe/delay/main", views.stats_pe_delay_main, name="stats_pe_delay_main"),
     path("pe/delay/raw", views.stats_pe_delay_raw, name="stats_pe_delay_raw"),
     path("pe/conversion/main", views.stats_pe_conversion_main, name="stats_pe_conversion_main"),
@@ -22,14 +22,14 @@ urlpatterns = [
     path("pe/state/raw", views.stats_pe_state_raw, name="stats_pe_state_raw"),
     path("pe/tension", views.stats_pe_tension, name="stats_pe_tension"),
     # Institution stats - DDETS - department level.
-    path("ddets/iae/", views.stats_ddets_iae, name="stats_ddets_iae"),
-    path("ddets/diagnosis_control/", views.stats_ddets_diagnosis_control, name="stats_ddets_diagnosis_control"),
-    path("ddets/hiring/", views.stats_ddets_hiring, name="stats_ddets_hiring"),
+    path("ddets/iae", views.stats_ddets_iae, name="stats_ddets_iae"),
+    path("ddets/diagnosis_control", views.stats_ddets_diagnosis_control, name="stats_ddets_diagnosis_control"),
+    path("ddets/hiring", views.stats_ddets_hiring, name="stats_ddets_hiring"),
     # Institution stats - DREETS - region level.
-    path("dreets/iae/", views.stats_dreets_iae, name="stats_dreets_iae"),
-    path("dreets/hiring/", views.stats_dreets_hiring, name="stats_dreets_hiring"),
+    path("dreets/iae", views.stats_dreets_iae, name="stats_dreets_iae"),
+    path("dreets/hiring", views.stats_dreets_hiring, name="stats_dreets_hiring"),
     # Institution stats - DGEFP - nation level.
-    path("dgefp/iae/", views.stats_dgefp_iae, name="stats_dgefp_iae"),
-    path("dgefp/diagnosis_control/", views.stats_dgefp_diagnosis_control, name="stats_dgefp_diagnosis_control"),
-    path("dgefp/af/", views.stats_dgefp_af, name="stats_dgefp_af"),
+    path("dgefp/iae", views.stats_dgefp_iae, name="stats_dgefp_iae"),
+    path("dgefp/diagnosis_control", views.stats_dgefp_diagnosis_control, name="stats_dgefp_diagnosis_control"),
+    path("dgefp/af", views.stats_dgefp_af, name="stats_dgefp_af"),
 ]
