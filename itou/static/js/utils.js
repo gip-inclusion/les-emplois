87612be08afbfa9d4a369a8143cbf9fa683e713b
Romain Sébille <romain.sebille@beta.gouv.fr>
2022-08-08 15:48:00 +0200

    js.utils: Promote `js-enable-submit-when-valid` to global utilities

--- itou/static/js/utils.js
+++ itou/static/js/utils.js
@@ -4,4 +4,25 @@ $(document).ready(() => {
     });
 
     $(".js-display-if-javascript-enabled").css("display", "block");
+
+    /**
+     * JS to disable the submit button of the form when it's not valid
+     **/
+    function checkValidity(e) {
+        let submit = $(e.currentTarget).find('button[type="submit"]')
+        if (e.currentTarget.checkValidity()) {
+            submit.removeClass("disabled")
+        } else {
+            submit.addClass("disabled")
+        }
+    }
+
+    $(".js-enable-submit-when-valid").each(function () {
+        let form = $(this)
+        // Check everytime something change in the form
+        form.change(checkValidity)
+        form.on("duetChange", checkValidity)
+        // Check immediately
+        checkValidity({"currentTarget": this})
+    });
 });
