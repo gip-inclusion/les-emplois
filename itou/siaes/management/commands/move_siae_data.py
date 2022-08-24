9d246106e3346b08a1df0eb511253c4517c80037
Victor Perron <victor@iso3103.net>
2022-08-24 16:05:18 +0200

    move_siae_data: Refactor to make it use stdout/stderr
    And make it simpler as well

--- itou/siaes/management/commands/move_siae_data.py
+++ itou/siaes/management/commands/move_siae_data.py
@@ -1,5 +1,6 @@
+# pylint: disable=consider-using-f-string
+
 import argparse
-import logging
 
 from django.core.management.base import BaseCommand
 from django.db import transaction
@@ -13,8 +14,6 @@ from itou.job_applications import models as job_applications_models
 from itou.siaes import models as siaes_models
 from itou.users import models as users_models
 
-
-logger = logging.getLogger(__name__)
 
 HELP_TEXT = """
     Move all data from siae A to siae B (or only the job applications if `only-job-applications` option is set).
@@ -37,177 +36,7 @@ HELP_TEXT = """
 
     And in production:
     $ cd && cd app_* && django-admin move_siae_data --from 3243 --to 9612 --wet-run
-
 """
-
-
-def move_siae_data(from_id, to_id, wet_run=False, only_job_applications=False):
-    if from_id == to_id:
-        logger.error("Unable to use the same siae as source and destination (ID %s)", from_id)
-        return
-
-    from_siae_qs = siaes_models.Siae.objects.filter(pk=from_id)
-    try:
-        from_siae = from_siae_qs.get()
-    except siaes_models.Siae.DoesNotExist:
-        logger.error("Unable to find the siae ID %s", from_id)
-        return
-
-    to_siae_qs = siaes_models.Siae.objects.filter(pk=to_id)
-    try:
-        to_siae = to_siae_qs.get()
-    except siaes_models.Siae.DoesNotExist:
-        logger.error("Unable to find the siae ID %s", to_id)
-        return
-
-    # Intermediate variable for better readability
-    move_all_data = not only_job_applications
-
-    logger.info(
-        "MOVE %s OF siae.id=%s - %s %s - %s",
-        "DATA" if move_all_data else "JOB APPLICATIONS",
-        from_siae.pk,
-        from_siae.kind,
-        from_siae.siret,
-        from_siae.display_name,
-    )
-
-    job_applications_sent = job_applications_models.JobApplication.objects.filter(sender_siae_id=from_id)
-    logger.info("| Job applications sent: %s", job_applications_sent.count())
-
-    job_applications_received = job_applications_models.JobApplication.objects.filter(to_siae_id=from_id)
-    logger.info("| Job applications received: %s", job_applications_received.count())
-
-    if move_all_data:
-        # Move Job Description not already present in siae destination, Job Applications related will be attached to
-        # Job Description present in siae destination
-        appellation_subquery = Subquery(
-            siaes_models.SiaeJobDescription.objects.filter(siae_id=to_id, appellation_id=OuterRef("appellation_id"))
-        )
-        job_descriptions = siaes_models.SiaeJobDescription.objects.filter(siae_id=from_id).exclude(
-            Exists(appellation_subquery)
-        )
-        logger.info("| Job descriptions: %s", job_descriptions.count())
-
-        # Move users not already present in siae destination
-        members = siaes_models.SiaeMembership.objects.filter(siae_id=from_id).exclude(
-            user__in=users_models.User.objects.filter(siaemembership__siae_id=to_id)
-        )
-        logger.info("| Members: %s", members.count())
-
-        diagnoses = eligibility_models.EligibilityDiagnosis.objects.filter(author_siae_id=from_id)
-        logger.info("| Diagnoses: %s", diagnoses.count())
-
-        prolongations = approvals_models.Prolongation.objects.filter(declared_by_siae_id=from_id)
-        logger.info("| Prolongations: %s", prolongations.count())
-
-        suspensions = approvals_models.Suspension.objects.filter(siae_id=from_id)
-        logger.info("| Suspensions: %s", suspensions.count())
-
-        # Don't move invitations for existing members
-        # The goal is to keep information about the original information
-        invitations = invitations_models.SiaeStaffInvitation.objects.filter(siae_id=from_id).exclude(
-            email__in=users_models.User.objects.filter(siaemembership__siae_id=to_id).values_list("email", flat=True)
-        )
-        logger.info("| Invitations: %s", invitations.count())
-
-    logger.info(
-        "INTO siae.id=%s - %s %s - %s",
-        to_siae.pk,
-        to_siae.kind,
-        to_siae.siret,
-        to_siae.display_name,
-    )
-
-    dest_siae_job_applications_sent = job_applications_models.JobApplication.objects.filter(sender_siae_id=to_id)
-    logger.info("| Job applications sent: %s", dest_siae_job_applications_sent.count())
-
-    dest_siae_job_applications_received = job_applications_models.JobApplication.objects.filter(to_siae_id=to_id)
-    logger.info("| Job applications received: %s", dest_siae_job_applications_received.count())
-
-    if move_all_data:
-        logger.info(f"| Brand '{to_siae.brand}' will be updated with '{from_siae.display_name}'")
-        logger.info(f"| Description \n{to_siae.description}\nwill be updated with\n{from_siae.description}")
-        logger.info(f"| Phone '{to_siae.phone}' will be updated with '{from_siae.phone}'")
-        logger.info(f"| Coords '{to_siae.coords}' will be updated with '{from_siae.coords}'")
-        logger.info(f"| Geoscore '{to_siae.geocoding_score}' will be updated with '{from_siae.geocoding_score}'")
-
-    if not wet_run:
-        logger.info("Nothing to do in dry run mode.")
-        return
-
-    with transaction.atomic():
-
-        # If we move the job applications without moving the job descriptions as well, we need to unlink them,
-        # as job applications will be attached to siae B but job descriptions will stay attached to siae A.
-        if only_job_applications:
-            for job_application in job_applications_sent:
-                job_application.selected_jobs.clear()
-            for job_application in job_applications_received:
-                job_application.selected_jobs.clear()
-
-        # If we move job_description, we have to take care of existant job_description linked to siae B (destination),
-        # because we can't have 2 job_applications with the same Appellation for one siae. Job applications linked to
-        # these kind of job_description have to be unlinked to be transfered. Job_description can be different enough
-        # to be irrelevant.
-        if move_all_data:
-            # find Appellation linked to job_description siae B
-            to_siae_appellation_id = siaes_models.SiaeJobDescription.objects.filter(siae_id=to_id).values_list(
-                "appellation_id", flat=True
-            )
-
-            # find job_applications in siae A, linked with job_description which Appellation is found in siae B
-            job_applications_to_clear = job_applications_models.JobApplication.objects.filter(
-                to_siae_id=from_id,
-                selected_jobs__in=siaes_models.SiaeJobDescription.objects.filter(
-                    siae_id=from_id, appellation_id__in=to_siae_appellation_id
-                ),
-            )
-
-            # clean job_applications to let them be transfered in siae B
-            for job_application in job_applications_to_clear:
-                job_application.selected_jobs.clear()
-
-        job_applications_sent.update(sender_siae_id=to_id)
-        job_applications_received.update(to_siae_id=to_id)
-
-        if move_all_data:
-            # do not move duplicated job_descriptions
-            job_descriptions.exclude(appellation_id__in=to_siae_appellation_id).update(siae_id=to_id)
-            members.update(siae_id=to_id)
-            diagnoses.update(author_siae_id=to_id)
-            prolongations.update(declared_by_siae_id=to_id)
-            suspensions.update(siae_id=to_id)
-            invitations.update(siae_id=to_id)
-            to_siae_qs.update(
-                brand=from_siae.display_name,
-                description=from_siae.description,
-                phone=from_siae.phone,
-                coords=from_siae.coords,
-                geocoding_score=from_siae.geocoding_score,
-            )
-            from_siae_qs.update(
-                block_job_applications=True,
-                job_applications_blocked_at=timezone.now(),
-                # Make sure the old siae no longer appears in results
-                coords=None,
-                geocoding_score=None,
-            )
-
-    logger.info("MOVE %s OF siae.id=%s FINISHED", "DATA" if move_all_data else "JOB APPLICATIONS", from_siae.pk)
-    orig_job_applications_sent = job_applications_models.JobApplication.objects.filter(sender_siae_id=from_id)
-    logger.info("| Job applications sent: %s", orig_job_applications_sent.count())
-
-    orig_job_applications_received = job_applications_models.JobApplication.objects.filter(to_siae_id=from_id)
-    logger.info("| Job applications received: %s", orig_job_applications_received.count())
-
-    logger.info("INTO siae.id=%s", to_siae.pk)
-
-    dest_siae_job_applications_sent = job_applications_models.JobApplication.objects.filter(sender_siae_id=to_id)
-    logger.info("| Job applications sent: %s", dest_siae_job_applications_sent.count())
-
-    dest_siae_job_applications_received = job_applications_models.JobApplication.objects.filter(to_siae_id=to_id)
-    logger.info("| Job applications received: %s", dest_siae_job_applications_received.count())
 
 
 class Command(BaseCommand):
@@ -238,5 +67,178 @@ class Command(BaseCommand):
         )
         parser.add_argument("--wet-run", action=argparse.BooleanOptionalAction, default=False)
 
-    def handle(self, *args, **options):
-        move_siae_data(options["from_id"], options["to_id"], options["wet_run"], options["only_job_applications"])
+    def handle(self, from_id, to_id, wet_run=False, only_job_applications=False, **options):
+        if from_id == to_id:
+            self.stderr.write("Unable to use the same siae as source and destination (ID %s)\n" % from_id)
+            return
+
+        from_siae_qs = siaes_models.Siae.objects.filter(pk=from_id)
+        try:
+            from_siae = from_siae_qs.get()
+        except siaes_models.Siae.DoesNotExist:
+            self.stderr.write("Unable to find the siae ID %s\n" % from_id)
+            return
+
+        to_siae_qs = siaes_models.Siae.objects.filter(pk=to_id)
+        try:
+            to_siae = to_siae_qs.get()
+        except siaes_models.Siae.DoesNotExist:
+            self.stderr.write("Unable to find the siae ID %s\n" % to_id)
+            return
+
+        # Intermediate variable for better readability
+        move_all_data = not only_job_applications
+
+        self.stdout.write(
+            "MOVE %s OF siae.id=%s - %s %s - %s\n"
+            % (
+                "DATA" if move_all_data else "JOB APPLICATIONS",
+                from_siae.pk,
+                from_siae.kind,
+                from_siae.siret,
+                from_siae.display_name,
+            )
+        )
+
+        job_applications_sent = job_applications_models.JobApplication.objects.filter(sender_siae_id=from_id)
+        self.stdout.write("| Job applications sent: %s" % job_applications_sent.count())
+
+        job_applications_received = job_applications_models.JobApplication.objects.filter(to_siae_id=from_id)
+        self.stdout.write("| Job applications received: %s" % job_applications_received.count())
+
+        if move_all_data:
+            # Move Job Description not already present in siae destination, Job Applications
+            # related will be attached to Job Description present in siae destination
+            appellation_subquery = Subquery(
+                siaes_models.SiaeJobDescription.objects.filter(
+                    siae_id=to_id, appellation_id=OuterRef("appellation_id")
+                )
+            )
+            job_descriptions = siaes_models.SiaeJobDescription.objects.filter(siae_id=from_id).exclude(
+                Exists(appellation_subquery)
+            )
+            self.stdout.write("| Job descriptions: %s\n" % job_descriptions.count())
+
+            # Move users not already present in siae destination
+            members = siaes_models.SiaeMembership.objects.filter(siae_id=from_id).exclude(
+                user__in=users_models.User.objects.filter(siaemembership__siae_id=to_id)
+            )
+            self.stdout.write("| Members: %s\n" % members.count())
+
+            diagnoses = eligibility_models.EligibilityDiagnosis.objects.filter(author_siae_id=from_id)
+            self.stdout.write("| Diagnoses: %s\n" % diagnoses.count())
+
+            prolongations = approvals_models.Prolongation.objects.filter(declared_by_siae_id=from_id)
+            self.stdout.write("| Prolongations: %s\n" % prolongations.count())
+
+            suspensions = approvals_models.Suspension.objects.filter(siae_id=from_id)
+            self.stdout.write("| Suspensions: %s\n" % suspensions.count())
+
+            # Don't move invitations for existing members
+            # The goal is to keep information about the original information
+            invitations = invitations_models.SiaeStaffInvitation.objects.filter(siae_id=from_id).exclude(
+                email__in=users_models.User.objects.filter(siaemembership__siae_id=to_id).values_list(
+                    "email", flat=True
+                )
+            )
+            self.stdout.write("| Invitations: %s\n" % invitations.count())
+
+        self.stdout.write(
+            "INTO siae.id=%s - %s %s - %s\n" % (to_siae.pk, to_siae.kind, to_siae.siret, to_siae.display_name)
+        )
+
+        dest_siae_job_applications_sent = job_applications_models.JobApplication.objects.filter(sender_siae_id=to_id)
+        self.stdout.write("| Job applications sent: %s\n" % dest_siae_job_applications_sent.count())
+
+        dest_siae_job_applications_received = job_applications_models.JobApplication.objects.filter(to_siae_id=to_id)
+        self.stdout.write("| Job applications received: %s\n" % dest_siae_job_applications_received.count())
+
+        if move_all_data:
+            self.stdout.write(f"| Brand '{to_siae.brand}' will be updated with '{from_siae.display_name}'\n")
+            self.stdout.write(
+                f"| Description \n{to_siae.description}\nwill be updated with\n{from_siae.description}\n"
+            )
+            self.stdout.write(f"| Phone '{to_siae.phone}' will be updated with '{from_siae.phone}'\n")
+            self.stdout.write(f"| Coords '{to_siae.coords}' will be updated with '{from_siae.coords}'\n")
+            self.stdout.write(
+                f"| Geoscore '{to_siae.geocoding_score}' will be updated with '{from_siae.geocoding_score}'\n"
+            )
+
+        if not wet_run:
+            self.stdout.write("Nothing to do in dry run mode.\n")
+            return
+
+        with transaction.atomic():
+
+            # If we move the job applications without moving the job descriptions as well, we need to unlink them,
+            # as job applications will be attached to siae B but job descriptions will stay attached to siae A.
+            if only_job_applications:
+                for job_application in job_applications_sent:
+                    job_application.selected_jobs.clear()
+                for job_application in job_applications_received:
+                    job_application.selected_jobs.clear()
+
+            # If we move job_description, we have to take care of existant job_description linked
+            # to siae B (destination), because we can't have 2 job_applications with the same Appellation
+            # for one siae. Job applications linked to these kind of job_description have to be
+            # unlinked to be transfered. Job_description can be different enough to be irrelevant.
+            if move_all_data:
+                # find Appellation linked to job_description siae B
+                to_siae_appellation_id = siaes_models.SiaeJobDescription.objects.filter(siae_id=to_id).values_list(
+                    "appellation_id", flat=True
+                )
+
+                # find job_applications in siae A, linked with job_description which Appellation is found in siae B
+                job_applications_to_clear = job_applications_models.JobApplication.objects.filter(
+                    to_siae_id=from_id,
+                    selected_jobs__in=siaes_models.SiaeJobDescription.objects.filter(
+                        siae_id=from_id, appellation_id__in=to_siae_appellation_id
+                    ),
+                )
+
+                # clean job_applications to let them be transfered in siae B
+                for job_application in job_applications_to_clear:
+                    job_application.selected_jobs.clear()
+
+            job_applications_sent.update(sender_siae_id=to_id)
+            job_applications_received.update(to_siae_id=to_id)
+
+            if move_all_data:
+                # do not move duplicated job_descriptions
+                job_descriptions.exclude(appellation_id__in=to_siae_appellation_id).update(siae_id=to_id)
+                members.update(siae_id=to_id)
+                diagnoses.update(author_siae_id=to_id)
+                prolongations.update(declared_by_siae_id=to_id)
+                suspensions.update(siae_id=to_id)
+                invitations.update(siae_id=to_id)
+                to_siae_qs.update(
+                    brand=from_siae.display_name,
+                    description=from_siae.description,
+                    phone=from_siae.phone,
+                    coords=from_siae.coords,
+                    geocoding_score=from_siae.geocoding_score,
+                )
+                from_siae_qs.update(
+                    block_job_applications=True,
+                    job_applications_blocked_at=timezone.now(),
+                    # Make sure the old siae no longer appears in results
+                    coords=None,
+                    geocoding_score=None,
+                )
+
+        self.stdout.write(
+            "MOVE %s OF siae.id=%s FINISHED\n" % ("DATA" if move_all_data else "JOB APPLICATIONS", from_siae.pk)
+        )
+        orig_job_applications_sent = job_applications_models.JobApplication.objects.filter(sender_siae_id=from_id)
+        self.stdout.write("| Job applications sent: %s\n" % orig_job_applications_sent.count())
+
+        orig_job_applications_received = job_applications_models.JobApplication.objects.filter(to_siae_id=from_id)
+        self.stdout.write("| Job applications received: %s\n" % orig_job_applications_received.count())
+
+        self.stdout.write("INTO siae.id=%s\n" % to_siae.pk)
+
+        dest_siae_job_applications_sent = job_applications_models.JobApplication.objects.filter(sender_siae_id=to_id)
+        self.stdout.write("| Job applications sent: %s\n" % dest_siae_job_applications_sent.count())
+
+        dest_siae_job_applications_received = job_applications_models.JobApplication.objects.filter(to_siae_id=to_id)
+        self.stdout.write("| Job applications received: %s\n" % dest_siae_job_applications_received.count())
