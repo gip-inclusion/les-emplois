gip-inclusion/les-emplois

commit 57da62c8049df3dfb3ab2e14cf74a9ad2714efc2

Siaes: further customize the SiaeKind enums

itou/siaes/management/commands/_import_siae/convention.py
@@ -5,7 +5,7 @@ SiaeConvention object logic used by the import_siae.py script is gathered here.
 """
 from django.utils import timezone
 
-from itou.siaes.enums import SiaeKind
+from itou.siaes.enums import SIAE_WITH_CONVENTION_KINDS, SiaeKind
 from itou.siaes.management.commands._import_siae.siae import does_siae_have_an_active_convention
 from itou.siaes.management.commands._import_siae.vue_af import INACTIVE_SIAE_LIST
 from itou.siaes.management.commands._import_siae.vue_structure import ASP_ID_TO_SIRET_SIGNATURE, SIRET_TO_ASP_ID
@@ -181,12 +181,12 @@ def check_convention_data_consistency():
                 assert siae.kind == convention.kind
 
     asp_siaes_without_convention = Siae.objects.filter(
-        kind__in=Siae.ASP_MANAGED_KINDS, source=Siae.SOURCE_ASP, convention__isnull=True
+        kind__in=SIAE_WITH_CONVENTION_KINDS, source=Siae.SOURCE_ASP, convention__isnull=True
     ).count()
     assert asp_siaes_without_convention == 0
 
     user_created_siaes_without_convention = Siae.objects.filter(
-        kind__in=Siae.ASP_MANAGED_KINDS,
+        kind__in=SIAE_WITH_CONVENTION_KINDS,
         source=Siae.SOURCE_USER_CREATED,
         convention__isnull=True,
     ).count()

itou/approvals/tests/tests.py
@@ -987,9 +987,7 @@ class CustomApprovalAdminViewsTest(TestCase):
         )
 
         ## can_use_employee_record
-        for siae_kind in [
-            siae_kind for siae_kind, _ in SiaeKind.choices if siae_kind not in Siae.ASP_EMPLOYEE_RECORD_KINDS
-        ]:
+        for siae_kind in [siae_kind for siae_kind in SiaeKind if siae_kind not in Siae.ASP_EMPLOYEE_RECORD_KINDS]:
             not_eligible_siae = SiaeFactory(kind=siae_kind)
             job_application = JobApplicationWithApprovalFactory(to_siae=not_eligible_siae)
             self.assertIn(

itou/job_applications/tests/tests.py
@@ -113,9 +113,7 @@ class JobApplicationModelTest(TestCase):
         self.assertTrue(job_application.can_display_approval)
 
         # SIAE not subject to eligibility rules.
-        not_eligible_kinds = [
-            choice[0] for choice in SiaeKind.choices if choice[0] not in Siae.ELIGIBILITY_REQUIRED_KINDS
-        ]
+        not_eligible_kinds = [kind for kind in SiaeKind if kind not in Siae.ELIGIBILITY_REQUIRED_KINDS]
         not_eligible_siae = SiaeFactory(kind=not_eligible_kinds[0])
         job_application = JobApplicationWithApprovalFactory(to_siae=not_eligible_siae)
         self.assertFalse(job_application.can_display_approval)
@@ -311,9 +309,7 @@ class JobApplicationModelTest(TestCase):
 
         # test SIAE cannot use Employee_Record
         job_application.hiring_start_at = today
-        for siae_kind in [
-            siae_kind for siae_kind, _ in SiaeKind.choices if siae_kind not in Siae.ASP_EMPLOYEE_RECORD_KINDS
-        ]:
+        for siae_kind in [siae_kind for siae_kind in SiaeKind if siae_kind not in Siae.ASP_EMPLOYEE_RECORD_KINDS]:
             not_eligible_siae = SiaeFactory(kind=siae_kind)
             job_application.to_siae = not_eligible_siae
             self.assertFalse(job_application.is_waiting_for_employee_record_creation)

itou/siae_evaluations/tests/tests_models.py
@@ -243,7 +243,7 @@ class EvaluationCampaignManagerTest(TestCase):
         self.assertEqual(0, len(evaluation_campaign.eligible_job_applications()))
 
         # Job Application not eligible kind
-        for kind in [k for (k, _) in SiaeKind.choices if k not in evaluation_enums.EvaluationSiaesKind.Evaluable]:
+        for kind in [k for k in SiaeKind if k not in evaluation_enums.EvaluationSiaesKind.Evaluable]:
             with self.subTest(kind=kind):
                 siae_wrong_kind = SiaeFactory(department="14", kind=kind)
                 JobApplicationWithApprovalFactory(

itou/siaes/enums.py
@@ -2,7 +2,6 @@ from django.db import models
 
 
 class SiaeKind(models.TextChoices):
-
     EI = "EI", "Entreprise d'insertion"  # Regroupées au sein de la fédération des entreprises d'insertion.
     AI = "AI", "Association intermédiaire"
     ACI = "ACI", "Atelier chantier d'insertion"
@@ -18,6 +17,21 @@ class SiaeKind(models.TextChoices):
     EA = "EA", "Entreprise adaptée"
     EATT = "EATT", "Entreprise adaptée de travail temporaire"
     OPCS = "OPCS", "Organisation porteuse de la clause sociale"
+
+
+# This used to be the ASP_MANAGED_KINDS list in siaes.models; but it's clearer to talk about
+# SIAEs that have a convention.
+# Ported older comment: ASP data is used to keep the siae data of these kinds in sync.
+# These kinds and only these kinds thus have convention/AF logic.
+SIAE_WITH_CONVENTION_KINDS = [
+    SiaeKind.EI.value,
+    SiaeKind.AI.value,
+    SiaeKind.ACI.value,
+    SiaeKind.ETTI.value,
+    SiaeKind.EITI.value,
+]
+
+SIAE_WITH_CONVENTION_CHOICES = [(k, v) for k, v in SiaeKind.choices if k in SIAE_WITH_CONVENTION_KINDS]
 
 
 class ContractType(models.TextChoices):

itou/siaes/factories.py
@@ -41,6 +41,7 @@ class SiaeConventionFactory(factory.django.DjangoModelFactory):
 
     # Don't start a SIRET with 0.
     siret_signature = factory.fuzzy.FuzzyText(length=13, chars=string.digits, prefix="1")
+    # FIXME(vperron): this should be made random
     kind = SiaeKind.EI
     asp_id = factory.Sequence(int)
     is_active = True
@@ -78,7 +79,7 @@ class SiaeFactory(factory.django.DjangoModelFactory):
         )
         not_subject_to_eligibility = factory.Trait(
             kind=factory.fuzzy.FuzzyChoice(
-                [kind for kind, _ in SiaeKind.choices if kind not in models.Siae.ELIGIBILITY_REQUIRED_KINDS]
+                [kind for kind in SiaeKind if kind not in models.Siae.ELIGIBILITY_REQUIRED_KINDS]
             ),
         )
         with_membership = factory.Trait(
@@ -89,6 +90,7 @@ class SiaeFactory(factory.django.DjangoModelFactory):
     # Don't start a SIRET with 0.
     siret = factory.fuzzy.FuzzyText(length=13, chars=string.digits, prefix="1")
     naf = factory.fuzzy.FuzzyChoice(NAF_CODES)
+    # FIXME(vperron): this should be made random
     kind = SiaeKind.EI
     name = factory.Faker("company", locale="fr_FR")
     phone = factory.fuzzy.FuzzyText(length=10, chars=string.digits)

itou/siaes/management/commands/_import_siae/vue_af.py
@@ -19,8 +19,9 @@ an "siae_key" throughout the import_siae.py script code.
 """
 from django.utils import timezone
 
+from itou.siaes.enums import SIAE_WITH_CONVENTION_KINDS
 from itou.siaes.management.commands._import_siae.utils import get_fluxiae_df, remap_columns, timeit
-from itou.siaes.models import Siae, SiaeFinancialAnnex
+from itou.siaes.models import SiaeFinancialAnnex
 from itou.utils.validators import validate_af_number
 
 
@@ -76,7 +77,7 @@ def get_vue_af_df():
 
     # Ensure data quality.
     for _, row in df.iterrows():
-        assert row.kind in Siae.ASP_MANAGED_KINDS
+        assert row.kind in SIAE_WITH_CONVENTION_KINDS
         validate_af_number(row.number)
 
     df["ends_in_the_future"] = df.end_date.apply(timezone.make_aware) > timezone.now()

itou/siaes/management/commands/import_geiq.py
@@ -110,7 +110,11 @@ class Command(BaseCommand):
     def handle(self, dry_run=False, **options):
         geiq_df = get_geiq_df()
         sync_structures(
-            df=geiq_df, source=Siae.SOURCE_GEIQ, kinds=[SiaeKind.GEIQ], build_structure=build_geiq, dry_run=dry_run
+            df=geiq_df,
+            source=Siae.SOURCE_GEIQ,
+            kinds=[SiaeKind.GEIQ],
+            build_structure=build_geiq,
+            dry_run=dry_run,
         )
 
         self.stdout.write("-" * 80)

commit b06d2b9663578dd402e25fed2da798a8cd99b152

Fix issue in import_siae, introduced by 57da62c8049

itou/siaes/management/commands/_import_siae/convention.py
@@ -174,7 +174,7 @@ def check_convention_data_consistency():
         # Additional data consistency checks.
         for siae in convention.siaes.all():
             assert siae.siren == convention.siren_signature
-            if siae.kind == SiaeKind.ACIPHC and convention.kind == SiaeConvention.KIND_ACI:
+            if siae.kind == SiaeKind.ACIPHC and convention.kind == SiaeKind.KIND_ACI:
                 assert siae.source == Siae.SOURCE_USER_CREATED
                 # Sometimes our staff manually changes an existing ACI antenna's kind from ACI to ACIPHC and forgets
                 # to detach the ACI convention.