2d0910fee8c0318f235efb06e4a74d131c976117
dejafait <dejafait@blackhole.com>
2022-09-01 16:01:10 +0200

    Drop two C2 extracts 🔪

--- itou/users/management/commands/extract_c2_users.py
+++ itou/users/management/commands/extract_c2_users.py
@@ -4,19 +4,15 @@ import os
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
-from django.db.models import Q
 
 from itou.common_apps.address.departments import DEPARTMENTS
 from itou.institutions.enums import InstitutionKind
 from itou.institutions.models import InstitutionMembership
-from itou.prescribers.enums import PrescriberOrganizationKind
-from itou.prescribers.models import PrescriberMembership
-from itou.siaes.models import SiaeMembership
 
 
 class Command(BaseCommand):
     """
-    Extract all C2 users to CSV files, one file per category (DDETS, DREETS, CD, SIAE).
+    Extract all C2 users to CSV files, one file per category (DDETS, DREET).
 
     To see how many records would be extracted without actually extracting them:
         django-admin extract_c2_users --no-csv
@@ -55,7 +51,7 @@ class Command(BaseCommand):
             "Email": user.email,
             "Prénom": user.first_name,
             "Nom": user.last_name,
-            "Admin": membership.is_admin,
+            "Admin": "Oui" if membership.is_admin else "Non",
             "DateRattachement": membership.created_at.date(),
             "Département": DEPARTMENTS[org.department] if org.department else None,
             "Région": org.region,
@@ -69,10 +65,6 @@ class Command(BaseCommand):
 
         ddets_csv_rows = []
         dreets_csv_rows = []
-        cd_csv_rows = []
-        siae_csv_rows = []
-
-        # 1 - Institutions (DDETS and DREETS).
 
         institution_memberships = InstitutionMembership.objects.select_related("user", "institution").filter(
             is_active=True
@@ -90,56 +82,8 @@ class Command(BaseCommand):
                 del row["Département"]
                 dreets_csv_rows.append(row)
 
-        # 2 - CD.
-
-        cd_memberships = PrescriberMembership.objects.select_related("user", "organization").filter(
-            is_active=True,
-            organization__kind=PrescriberOrganizationKind.DEPT,
-        )
-
-        for membership in cd_memberships:
-            user = membership.user
-            org = membership.organization
-
-            if user.can_view_stats_cd(org):
-                # Only keep users actually able to see C2 stats for this extraction.
-                row = self.get_basic_row(membership=membership, org=org)
-                row["Nom du CD"] = org.display_name
-                cd_csv_rows.append(row)
-
-        # 3 - Employers.
-
-        if not 1 <= len(settings.STATS_SIAE_USER_PK_WHITELIST) <= 100:
-            raise ValueError(
-                "Whitelist size outside of normal range, are you sure you are running this script"
-                " in production with the proper STATS_SIAE_USER_PK_WHITELIST setting?"
-            )
-
-        siae_memberships = (
-            SiaeMembership.objects.select_related("user", "siae__convention")
-            .filter(is_active=True)
-            .filter(
-                Q(user_id__in=settings.STATS_SIAE_USER_PK_WHITELIST)
-                | Q(siae__department__in=settings.STATS_SIAE_DEPARTMENT_WHITELIST),
-            )
-        )
-
-        for membership in siae_memberships:
-            user = membership.user
-            org = membership.siae
-
-            if not org.is_active:
-                continue
-
-            row = self.get_basic_row(membership=membership, org=org)
-            row["Type de la SIAE"] = org.kind
-            row["Nom de la SIAE"] = org.display_name
-            siae_csv_rows.append(row)
-
         self.stdout.write("-" * 80)
         self.to_csv("ddets", ddets_csv_rows, "DDETS memberships")
         self.to_csv("dreets", dreets_csv_rows, "DREETS memberships")
-        self.to_csv("cd", cd_csv_rows, "CD memberships")
-        self.to_csv("siae", siae_csv_rows, "SIAE memberships")
         self.stdout.write("-" * 80)
         self.stdout.write("Done!")
