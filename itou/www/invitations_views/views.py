b853c297bec3560815ecddb7e455f8236ef14596
Antoine LAURENT <antoine.laurent@beta.gouv.fr>
2022-09-13 16:31:49 +0200

    invitation_views: Fix invited prescriber redirected url

--- itou/www/invitations_views/views.py
+++ itou/www/invitations_views/views.py
@@ -6,6 +6,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
 from django.db import transaction
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, redirect, render, reverse
 from django.utils import formats, safestring
 
@@ -170,7 +171,8 @@ def join_prescriber_organization(request, invitation_id):
         messages.error(request, "Cette invitation n'est plus valide.")
 
     request.session[settings.ITOU_SESSION_CURRENT_PRESCRIBER_ORG_KEY] = invitation.organization.pk
-    return redirect("dashboard:index")
+    url = get_adapter(request).get_login_redirect_url(request)
+    return HttpResponseRedirect(url)
 
 
 @login_required
