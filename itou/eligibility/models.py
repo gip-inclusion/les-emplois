gip-inclusion/les-emplois

commit ef6904332333aa9134f38a3b2e61675d51999654

eligibility.models: Make `expires_at` a DB field instead of a property

itou/eligibility/models.py
@@ -15,15 +15,11 @@ logger = logging.getLogger(__name__)
 
 
 class EligibilityDiagnosisQuerySet(models.QuerySet):
-    @property
-    def valid_lookup(self):
-        return models.Q(created_at__gt=self.model.get_expiration_dt())
-
     def valid(self):
-        return self.filter(self.valid_lookup)
+        return self.filter(expires_at__gt=timezone.now())
 
     def expired(self):
-        return self.exclude(self.valid_lookup)
+        return self.filter(expires_at__lte=timezone.now())
 
     def authored_by_siae(self, for_siae):
         return self.filter(author_siae=for_siae)
@@ -182,6 +178,7 @@ class EligibilityDiagnosis(models.Model):
 
     created_at = models.DateTimeField(verbose_name="Date de création", default=timezone.now, db_index=True)
     updated_at = models.DateTimeField(verbose_name="Date de modification", blank=True, null=True, db_index=True)
+    expires_at = models.DateTimeField(verbose_name="Date d'expiration", db_index=True)
 
     objects = EligibilityDiagnosisManager.from_queryset(EligibilityDiagnosisQuerySet)()
 
@@ -195,15 +192,13 @@ class EligibilityDiagnosis(models.Model):
 
     def save(self, *args, **kwargs):
         self.updated_at = timezone.now()
+        if not self.expires_at:
+            self.expires_at = self.created_at + relativedelta(months=self.EXPIRATION_DELAY_MONTHS)
         return super().save(*args, **kwargs)
 
     @property
     def is_valid(self):
-        return self.created_at > self.get_expiration_dt()
-
-    @property
-    def expires_at(self):
-        return self.created_at + relativedelta(months=self.EXPIRATION_DELAY_MONTHS)
+        return self.expires_at > timezone.now()
 
     # A diagnosis is considered valid for the whole duration of an approval.
     # Methods below (whose name contain `considered`) take into account
@@ -242,11 +237,6 @@ class EligibilityDiagnosis(models.Model):
                 diagnosis.administrative_criteria.add(criteria)
             diagnosis.save()
         return diagnosis
-
-    @staticmethod
-    def get_expiration_dt():
-        # Everything created after this date is valid.
-        return timezone.now() - relativedelta(months=EligibilityDiagnosis.EXPIRATION_DELAY_MONTHS)
 
 
 class AdministrativeCriteriaQuerySet(models.QuerySet):

itou/eligibility/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.template.defaultfilters import date as date_filter
 
 from itou.eligibility import models
 from itou.utils.admin import PkSupportRemarkInline
@@ -77,11 +76,6 @@ class EligibilityDiagnosisAdmin(admin.ModelAdmin):
 
     is_valid.boolean = True
     is_valid.short_description = "En cours de validité"
-
-    def expires_at(self, obj):
-        return date_filter(obj.expires_at, "d F Y H:i")
-
-    expires_at.short_description = "Date d'expiration"
 
     def is_considered_valid(self, obj):
         """

itou/eligibility/factories.py
@@ -13,6 +13,7 @@ class EligibilityDiagnosisFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = models.EligibilityDiagnosis
 
+    created_at = factory.LazyAttribute(lambda obj: timezone.now())
     author = factory.LazyAttribute(lambda obj: obj.author_prescriber_organization.members.first())
     author_kind = models.EligibilityDiagnosis.AUTHOR_KIND_PRESCRIBER
     author_prescriber_organization = factory.SubFactory(PrescriberOrganizationWithMembershipFactory, authorized=True)
@@ -25,6 +26,7 @@ class EligibilityDiagnosisMadeBySiaeFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = models.EligibilityDiagnosis
 
+    created_at = factory.LazyAttribute(lambda obj: timezone.now())
     author = factory.LazyAttribute(lambda obj: obj.author_siae.members.first())
     author_kind = models.EligibilityDiagnosis.AUTHOR_KIND_SIAE_STAFF
     author_siae = factory.SubFactory(SiaeFactory, with_membership=True)
@@ -33,16 +35,12 @@ class EligibilityDiagnosisMadeBySiaeFactory(factory.django.DjangoModelFactory):
 
 class ExpiredEligibilityDiagnosisFactory(EligibilityDiagnosisFactory):
 
-    created_at = factory.LazyAttribute(
-        lambda self: models.EligibilityDiagnosis.get_expiration_dt() - timezone.timedelta(days=1)
-    )
+    expires_at = factory.SelfAttribute("created_at")
 
 
 class ExpiredEligibilityDiagnosisMadeBySiaeFactory(EligibilityDiagnosisMadeBySiaeFactory):
 
-    created_at = factory.LazyAttribute(
-        lambda self: models.EligibilityDiagnosis.get_expiration_dt() - timezone.timedelta(days=1)
-    )
+    expires_at = factory.SelfAttribute("created_at")
 
 
 class AdministrativeCriteriaFactory(factory.django.DjangoModelFactory):

itou/eligibility/migrations/0010_eligibilitydiagnosis_expires_at.py
@@ -1,0 +1,33 @@
+from django.db import migrations, models
+from django.db.models import ExpressionWrapper, F, Func
+
+
+def fill_expires_at(apps, _schema_editor):
+    model = apps.get_model("eligibility", "EligibilityDiagnosis")
+    model.objects.update(
+        expires_at=ExpressionWrapper(
+            F("created_at") + Func(template="interval '6 months'"),
+            output_field=models.DateTimeField(),
+        )
+    )
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("eligibility", "0009_rename_refugee_administrative_criteria"),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name="eligibilitydiagnosis",
+            name="expires_at",
+            field=models.DateTimeField(db_index=True, blank=True, null=True, verbose_name="Date d'expiration"),
+        ),
+        migrations.RunPython(fill_expires_at, migrations.RunPython.noop, elidable=True),
+        migrations.AlterField(
+            model_name="eligibilitydiagnosis",
+            name="expires_at",
+            field=models.DateTimeField(db_index=True, verbose_name="Date d'expiration"),
+        ),
+    ]

commit 6db44423c856b54bbedcd18fa7eb6025414d6775

eligibility.models: Fix `is_valid` property for admin forms
We ask the Django admin to display `is_valid` as a boolean, but when
creating a new eligibility diagnosis `expires_at` doesn't exists yet
which make the comparison with `timezone.now()` raise a `TypeError`.

This bug was introduced in ef690433.

itou/eligibility/models.py
@@ -198,7 +198,7 @@ class EligibilityDiagnosis(models.Model):
 
     @property
     def is_valid(self):
-        return self.expires_at > timezone.now()
+        return self.expires_at and self.expires_at > timezone.now()
 
     @property
     def author_organization(self):