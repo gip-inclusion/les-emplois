2315a5a3a01d28bfd89e46bed8f2c7e0cdf69b75
Antoine LAURENT <antoine.laurent@beta.gouv.fr>
2022-09-23 12:13:12 +0200

    ADD TESTS - utils: Allow to use our custom paginator in ListView

--- itou/utils/pagination.py
+++ itou/utils/pagination.py
@@ -1,4 +1,35 @@
-from django.core.paginator import EmptyPage, InvalidPage, Paginator
+from django.core.paginator import EmptyPage, InvalidPage, Page, Paginator
+
+
+class ItouPaginator(Paginator):
+    def __init__(self, object_list, per_page, orphans=0, allow_empty_first_page=True, max_pages_num=10):
+        super().__init__(object_list, per_page, orphans=orphans, allow_empty_first_page=allow_empty_first_page)
+        self.max_pages_num = max_pages_num
+
+    def _get_page(self, *args, **kwargs):
+        return ItouPage(*args, **kwargs)
+
+
+class ItouPage(Page):
+    def __init__(self, object_list, number, paginator):
+        super().__init__(object_list, number, paginator)
+
+        total_pages = paginator.num_pages
+        if total_pages <= paginator.max_pages_num:
+            pages_to_display = paginator.page_range
+        else:
+            first = number - (paginator.max_pages_num // 2)
+            last = first + paginator.max_pages_num
+            if first <= 0:
+                first = 1
+                last = first + paginator.max_pages_num
+            elif last >= total_pages:
+                last = total_pages
+                first = last - paginator.max_pages_num
+            pages_to_display = range(first, last + 1)
+
+        self.pages_to_display = pages_to_display
+        self.display_pager = total_pages > 1
 
 
 def pager(queryset, page, items_per_page=10, pages_num=10):
@@ -17,7 +48,7 @@ def pager(queryset, page, items_per_page=10, pages_num=10):
             pages_to_display: list, allow to iterate and create a google-style pager
             display_pager: bool, True if there are more than one page to display
     """
-    paginator = Paginator(queryset, items_per_page)
+    paginator = ItouPaginator(queryset, items_per_page, max_pages_num=pages_num)
 
     try:
         page = int(page)
@@ -27,25 +58,7 @@ def pager(queryset, page, items_per_page=10, pages_num=10):
     total_pages = paginator.num_pages
 
     try:
-        custom_pager = paginator.page(page)
+        return paginator.page(page)
     except (EmptyPage, InvalidPage):
         # If page request is out of range, deliver last page of results.
-        custom_pager = paginator.page(total_pages)
-
-    if total_pages <= pages_num:
-        pages_to_display = paginator.page_range
-    else:
-        first = page - (pages_num // 2)
-        last = first + pages_num
-        if first <= 0:
-            first = 1
-            last = first + pages_num
-        elif last >= total_pages:
-            last = total_pages
-            first = last - pages_num
-        pages_to_display = range(first, last + 1)
-
-    setattr(custom_pager, "pages_to_display", pages_to_display)
-    setattr(custom_pager, "display_pager", total_pages > 1)
-
-    return custom_pager
+        return paginator.page(total_pages)
