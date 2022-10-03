d4ba46eeb974b969a3ae223eab2b066fc3e22388
Romain Sébille <romain.sebille@beta.gouv.fr>
2022-10-03 12:55:02 +0200

    docker: Use `postgres` user instead of the custom one `itou`
    The database is created solely for the current application so we can
    simplify things by using the user that already exists, especially when we
    were also giving the `SUPERUSER` right to it.

--- docker/dev/postgres/psql_init.sh
+++ docker/dev/postgres/psql_init.sh
@@ -4,16 +4,4 @@ set -e
 # Initialization script.
 # https://hub.docker.com/_/postgres/#initialization-scripts
 
-export BASE_DIR=$(dirname "$BASH_SOURCE")
-
-# The PostgreSQL user should be able to create extensions.
-# Only the PostgreSQL superuser role provides that permission.
-psql -v ON_ERROR_STOP=1 <<-EOSQL
-
-  \c postgres;
-
-  CREATE USER $ITOU_POSTGRES_USER WITH ENCRYPTED PASSWORD '$ITOU_POSTGRES_PASSWORD';
-  CREATE DATABASE $ITOU_POSTGRES_DATABASE_NAME OWNER $ITOU_POSTGRES_USER;
-  ALTER USER $ITOU_POSTGRES_USER CREATEDB SUPERUSER;
-
-EOSQL
+psql -v ON_ERROR_STOP=1 --echo-queries --command="CREATE DATABASE \"$ITOU_POSTGRES_DATABASE_NAME\";"
