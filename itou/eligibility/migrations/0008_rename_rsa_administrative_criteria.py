9a2e36a1cc163cb78dec20e0dfc7c2a64eca24d3
François Freitag <mail@franek.fr>
2022-10-05 18:22:23 +0200

    Install administrative criteria on post_migrate
    When running the test suite twice (e.g. pytest && pytest), the second
    execution always fails because the administrative criteria table is
    empty.
    
    That is caused by the presence of some TransactionTestCase subclasses in
    the test suite, which call the flush management command on
    `tearDownClass`.
    https://github.com/django/django/blob/e9a24a15f2cdd6b4768ea7b45cd6b1758fb4ed48/django/test/testcases.py#L1304-L1312
    
    Solved by installing the administrative criteria on the post_migrate
    signal. That removes the need for some data migrations in
    itou/eligibility/migrations.

--- itou/eligibility/migrations/0008_rename_rsa_administrative_criteria.py
+++ itou/eligibility/migrations/0008_rename_rsa_administrative_criteria.py
@@ -3,20 +3,10 @@
 from django.db import migrations
 
 
-def migrate_data_forward(apps, schema_editor):  # pylint: disable=unused-argument
-    AdministrativeCriteria = apps.get_model("eligibility", "AdministrativeCriteria")
-    AdministrativeCriteria.objects.filter(name="Bénéficiaire du RSA (socle)").update(name="Bénéficiaire du RSA")
-
-
-def migrate_data_backward(apps, schema_editor):  # pylint: disable=unused-argument
-    AdministrativeCriteria = apps.get_model("eligibility", "AdministrativeCriteria")
-    AdministrativeCriteria.objects.filter(name="Bénéficiaire du RSA").update(name="Bénéficiaire du RSA (socle)")
-
-
 class Migration(migrations.Migration):
 
     dependencies = [
         ("eligibility", "0007_administrativecriteria_written_proof_validity"),
     ]
 
-    operations = [migrations.RunPython(migrate_data_forward, migrate_data_backward)]
+    operations = []
--- itou/eligibility/migrations/0009_rename_refugee_administrative_criteria.py
+++ itou/eligibility/migrations/0009_rename_refugee_administrative_criteria.py
@@ -1,31 +1,10 @@
 from django.db import migrations
 
 
-def migrate_data_forward(apps, schema_editor):  # pylint: disable=unused-argument
-    AdministrativeCriteria = apps.get_model("eligibility", "AdministrativeCriteria")
-    AdministrativeCriteria.objects.filter(name="Réfugié statutaire, protégé subsidiaire ou demandeur d'asile").update(
-        name="Réfugié statutaire, bénéficiaire d'une protection temporaire, protégé subsidiaire ou demandeur d'asile",
-        written_proof="Titre de séjour valide ou demande de renouvellement du titre de séjour. "
-        "Pour les demandeurs d'asile : autorisation temporaire de travail. "
-        "Pour les bénéficiaires d'une protection temporaire : autorisation provisoire de séjour.",
-    )
-
-
-def migrate_data_backward(apps, schema_editor):  # pylint: disable=unused-argument
-    AdministrativeCriteria = apps.get_model("eligibility", "AdministrativeCriteria")
-    AdministrativeCriteria.objects.filter(
-        name="Réfugié statutaire, bénéficiaire d'une protection temporaire, protégé subsidiaire ou demandeur d'asile"
-    ).update(
-        name="Réfugié statutaire, protégé subsidiaire ou demandeur d'asile",
-        written_proof="Titre de séjour valide ou demande de renouvellement du titre de séjour. "
-        "Pour les demandeurs d'asile : autorisation temporaire de travail",
-    )
-
-
 class Migration(migrations.Migration):
 
     dependencies = [
         ("eligibility", "0008_rename_rsa_administrative_criteria"),
     ]
 
-    operations = [migrations.RunPython(migrate_data_forward, migrate_data_backward)]
+    operations = []
