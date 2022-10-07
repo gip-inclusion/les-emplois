0b194f004b9cf9c0b6509bc1ef7bfa9478ac6e90
Céline MS <celine@hello-birds.com>
2022-10-07 12:29:40 +0200

    Add an HTMX test client working both with Pytest and Unittest

--- itou/conftest.py
+++ itou/conftest.py
@@ -1,5 +1,7 @@
 import pytest
 
+from itou.utils.htmx.testing import HtmxClient
+
 
 @pytest.hookimpl(tryfirst=True)
 def pytest_collection_modifyitems(config, items):
@@ -8,3 +10,17 @@ def pytest_collection_modifyitems(config, items):
         markers = {marker.name for marker in item.iter_markers()}
         if "no_django_db" not in markers and "django_db" not in markers:
             item.add_marker(pytest.mark.django_db)
+
+
+@pytest.fixture()
+def htmx_client():
+    """
+    Mimic a response to an HTMX request.
+
+    Usage
+    ```
+    def my_test(htmx_client):
+        response = htmx_client.get("/)
+    ```
+    """
+    return HtmxClient()
