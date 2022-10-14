da5f66ab6f17276a310462edca97236a56cf421b
hellodeloo <hello@hellodeloo.com>
2022-10-14 16:37:56 +0200

    feat: update job description card ui

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -14,7 +14,7 @@ SECRET_KEY = "foobar"
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "192.168.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "192.168.0.1", "0.0.0.0"]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
