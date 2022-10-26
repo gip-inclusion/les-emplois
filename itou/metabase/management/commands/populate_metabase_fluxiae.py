d6f635703aab76c0a0bd49d0134220e01a960e08
Victor Perron <victor@iso3103.net>
2022-10-26 15:59:44 +0200

    populate_metabase_fluxiae: Add a FIXME about a future redesign.

--- itou/metabase/management/commands/populate_metabase_fluxiae.py
+++ itou/metabase/management/commands/populate_metabase_fluxiae.py
@@ -64,6 +64,11 @@ from django.core.management.base import BaseCommand
 from itou.metabase import constants
 from itou.metabase.management.commands._dataframes import store_df
 from itou.metabase.management.commands._utils import build_final_tables, enable_sql_logging
+
+# FIXME(vperron): Those helpers are shared between populate_metabase and import_siae.
+# It would make a lot more sense, to avoid eventual circular imports, to move everything
+# related to the fluxiae logic in its own application. Some architecture still needs to be thought of there.
+# Another way to do it would be to rationalize our import (to Itou) & export (to Metabase) logic.
 from itou.siaes.management.commands._import_siae.utils import get_fluxiae_df, get_fluxiae_referential_filenames
 from itou.utils.python import timeit
 from itou.utils.slack import send_slack_message
