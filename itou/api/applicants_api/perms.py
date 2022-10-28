9098a56bd6498817501cf5d0d954ba2cfa39478a
Frédéric Vergez <fred@ikarius.com>
2022-10-28 17:45:10 +0200

    api: added applicants API (candidats)

---
+++ itou/api/applicants_api/perms.py
@@ -0,0 +1,21 @@
+from rest_framework.permissions import IsAuthenticated
+
+from itou.siaes.models import SiaeMembership
+
+
+class ApplicantsAPIPermission(IsAuthenticated):
+    def has_permission(self, request, view) -> bool:
+        """
+        Check that user:
+            - belongs to one SIAE only (dedicated API account)
+            - is admin
+        """
+        if not super().has_permission(request, view):
+            return False
+
+        memberships = request.user.active_or_in_grace_period_siae_memberships()
+
+        try:
+            return memberships.get().is_admin
+        except (SiaeMembership.DoesNotExist, SiaeMembership.MultipleObjectsReturned):
+            return False
---
+++ itou/api/applicants_api/serializers.py
@@ -0,0 +1,43 @@
+from rest_framework import serializers
+
+from itou.users.models import User
+
+
+class ApplicantSerializer(serializers.ModelSerializer):
+    """Applicant serializer: job seeker field of a job application"""
+
+    civilite = serializers.SerializerMethodField()
+    nom = serializers.CharField(source="first_name")
+    prenom = serializers.CharField(source="last_name")
+    courriel = serializers.CharField(source="email")
+    telephone = serializers.CharField(source="phone")
+    adresse = serializers.CharField(source="address_line_1")
+    complement_adresse = serializers.CharField(source="address_line_2")
+    code_postal = serializers.CharField(source="post_code")
+    ville = serializers.CharField(source="city")
+    date_naissance = serializers.DateField(source="birthdate")
+    lieu_naissance = serializers.CharField(source="birth_place")
+    pays_naissance = serializers.CharField(source="birth_country")
+    lien_cv = serializers.CharField(source="resume_link")
+
+    class Meta:
+        model = User
+        fields = (
+            "civilite",
+            "nom",
+            "prenom",
+            "courriel",
+            "telephone",
+            "adresse",
+            "complement_adresse",
+            "code_postal",
+            "ville",
+            "date_naissance",
+            "lieu_naissance",
+            "pays_naissance",
+            "lien_cv",
+        )
+        read_only_fields = fields
+
+    def get_civilite(self, obj) -> str:
+        return obj.title or "Non fournie"
