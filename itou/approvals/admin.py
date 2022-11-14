gip-inclusion/les-emplois

commit 05d9167a853d82da8d30633af1fcf3bcc4c59177

approvals.admin: Fix a KeyError for users with the "view" permission

itou/approvals/admin.py
@@ -132,7 +132,6 @@ class StartDateFilter(admin.SimpleListFilter):
 
 @admin.register(models.Approval)
 class ApprovalAdmin(admin.ModelAdmin):
-    form = ApprovalAdminForm
     list_display = ("pk", "number", "user", "birthdate", "start_at", "end_at", "is_valid", "created_at")
     list_select_related = ("user",)
     search_fields = ("pk", "number", "user__first_name", "user__last_name", "user__email")
@@ -157,6 +156,14 @@ class ApprovalAdmin(admin.ModelAdmin):
         JobApplicationInline,
         PkSupportRemarkInline,
     )
+
+    def get_form(self, request, obj=None, **kwargs):
+        if self.has_change_permission(request, obj) or self.has_add_permission(request, obj):
+            # When a user has only the "view" permission it seems that ModelForm.fields is empty,
+            # ApprovalAdminForm wants to modify that attribute which result in a KeyError,
+            # so we return the custom form only for user with the "change" permission.
+            kwargs["form"] = ApprovalAdminForm
+        return super().get_form(request, obj, **kwargs)
 
     def save_model(self, request, obj, form, change):
         if not obj.pk:

commit 2bd29b8dd6bdb4a12a1a6ce79de5aa7c8f588ecd

approvals.admin: Fix an error introduced in 05d9167a8
`has_add_permission(request, obj)` is the signature for
`InlineModelAdmin()`, `ModelAdmin()` uses `has_add_permission(request)`

itou/approvals/admin.py
@@ -158,7 +158,7 @@ class ApprovalAdmin(admin.ModelAdmin):
     )
 
     def get_form(self, request, obj=None, **kwargs):
-        if self.has_change_permission(request, obj) or self.has_add_permission(request, obj):
+        if self.has_change_permission(request, obj) or self.has_add_permission(request):
             # When a user has only the "view" permission it seems that ModelForm.fields is empty,
             # ApprovalAdminForm wants to modify that attribute which result in a KeyError,
             # so we return the custom form only for user with the "change" permission.