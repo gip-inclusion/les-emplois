1b68906f0c73401be26ac6bb8fcdbea3abf5d9ae
Antoine LAURENT <antoine.c.laurent.pro@gmail.com>
2022-11-15 16:14:38 +0100

    approvals_views: Fix printable display
    Now that we rely on the approval id, we need to handle approvals with
    multiple accepted job_applications for the same user + siae

--- itou/www/approvals_views/tests/test_display.py
+++ itou/www/approvals_views/tests/test_display.py
@@ -1,10 +1,11 @@
 from unittest.mock import PropertyMock, patch
 
+from dateutil.relativedelta import relativedelta
 from django.conf import settings
 from django.test import TestCase
 from django.urls import reverse
 
-from itou.job_applications.factories import JobApplicationWithApprovalFactory
+from itou.job_applications.factories import JobApplicationFactory, JobApplicationWithApprovalFactory
 from itou.job_applications.models import JobApplication
 from itou.users.factories import UserFactory
 from itou.utils import constants as global_constants
@@ -26,6 +27,30 @@ class TestDisplayApproval(TestCase):
         self.assertEqual(response.context["approval"], job_application.approval)
         self.assertEqual(response.context["siae"], job_application.to_siae)
         self.assertEqual(response.context["job_seeker"], job_application.job_seeker)
+        self.assertContains(response, global_constants.ITOU_ASSISTANCE_URL)
+        self.assertContains(response, "Imprimer ce PASS IAE")
+        self.assertContains(response, "Astuce pour conserver cette attestation en format PDF")
+
+    def test_display_approval_multiple_job_applications(self, *args, **kwargs):
+        job_application = JobApplicationWithApprovalFactory()
+        JobApplicationFactory(
+            job_seeker=job_application.job_seeker,
+            approval=job_application.approval,
+            to_siae=job_application.to_siae,
+            state=job_application.state,
+            created_at=job_application.created_at - relativedelta(days=1),
+        )
+
+        siae_member = job_application.to_siae.members.first()
+        self.client.force_login(siae_member)
+
+        response = self.client.get(
+            reverse("approvals:display_printable_approval", kwargs={"approval_id": job_application.approval_id})
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["approval"], job_application.approval)
+        self.assertEqual(response.context["siae"], job_application.to_siae)
         self.assertContains(response, global_constants.ITOU_ASSISTANCE_URL)
         self.assertContains(response, "Imprimer ce PASS IAE")
         self.assertContains(response, "Astuce pour conserver cette attestation en format PDF")
