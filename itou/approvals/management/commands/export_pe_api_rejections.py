2f3f52a982bab993bbf19b92924a099932c612eb
Antoine LAURENT <antoine.c.laurent.pro@gmail.com>
2022-11-17 13:29:13 +0100

    approvals: Improve PE API rejections export command

--- itou/approvals/management/commands/export_pe_api_rejections.py
+++ itou/approvals/management/commands/export_pe_api_rejections.py
@@ -3,7 +3,9 @@ import datetime
 import os
 
 from dateutil.relativedelta import relativedelta
+from django.conf import settings
 from django.core.management.base import BaseCommand
+from django.utils import timezone
 
 from itou.approvals.models import Approval
 
@@ -11,7 +13,7 @@ from itou.approvals.models import Approval
 class Command(BaseCommand):
     def handle(self, **options):
 
-        first_day_of_month = datetime.date.today().replace(day=1)
+        first_day_of_month = timezone.now().replace(day=1, hour=0, minute=0, second=0, microsecond=0)
         rejected_approvals = (
             Approval.objects.filter(
                 pe_notification_status="notification_error",
@@ -24,9 +26,16 @@ class Command(BaseCommand):
             .order_by("pe_notification_time")
         )
 
-        writer = csv.writer(self.stdout, lineterminator=os.linesep)
+        if not rejected_approvals:
+            self.stdout.write("No rejected approvels")
+            return
 
-        if rejected_approvals:
+        log_datetime = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+        path = f"{settings.EXPORT_DIR}/{log_datetime}-export_pe_api_rejections.csv"
+        os.makedirs(settings.EXPORT_DIR, exist_ok=True)
+
+        with open(path, "w") as csvfile:
+            writer = csv.writer(csvfile)
             writer.writerow(
                 [
                     "numero",
@@ -38,22 +47,24 @@ class Command(BaseCommand):
                     "prenom",
                     "date_naissance",
                     "siae_departement",
-                ],
+                ]
             )
 
-        for approval in rejected_approvals:
-            # Use the same logic that Approval.notify_pole_emploi() to get the SIAE using the PASS.
-            siae = approval.jobapplication_set.accepted().order_by("-created_at").first().to_siae
-            writer.writerow(
-                [
-                    approval.number,
-                    approval.pe_notification_time,
-                    approval.pe_notification_exit_code,
-                    approval.user.nir,
-                    approval.user.pole_emploi_id,
-                    approval.user.last_name,
-                    approval.user.first_name,
-                    approval.user.birthdate,
-                    siae.department,
-                ],
-            )
+            for approval in rejected_approvals:
+                # Use the same logic that Approval.notify_pole_emploi() to get the SIAE using the PASS.
+                siae = approval.jobapplication_set.accepted().order_by("-created_at").first().to_siae
+                writer.writerow(
+                    [
+                        approval.number,
+                        approval.pe_notification_time,
+                        approval.pe_notification_exit_code,
+                        approval.user.nir,
+                        approval.user.pole_emploi_id,
+                        approval.user.last_name,
+                        approval.user.first_name,
+                        approval.user.birthdate,
+                        siae.department,
+                    ]
+                )
+
+        self.stdout.write(f"CSV file created `{path}`")
