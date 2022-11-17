d4fc5081250b48316273a60eeb3e1a9bae7bed6c
Antoine LAURENT <antoine.c.laurent.pro@gmail.com>
2022-11-17 15:18:09 +0100

    dashboard: Fix new features badges text color

--- itou/www/dashboard/tests.py
+++ itou/www/dashboard/tests.py
@@ -290,7 +290,7 @@ class DashboardViewTest(TestCase):
         self.assertContains(
             response,
             "Contrôle a posteriori "
-            '<span class="badge badge-pill badge-sm badge-important text-primary">Nouveau</span>',
+            '<span class="badge badge-pill badge-sm badge-important text-white">Nouveau</span>',
             count=1,
         )
         self.assertContains(
