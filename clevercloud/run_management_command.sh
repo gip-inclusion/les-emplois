f5eeaa9d7c98836346a0001a0f691b0dd5a2e559
François Freitag <mail@franek.fr>
2022-11-22 09:37:07 +0100

    Move antivirus scan to the C1 workers
    Create a script to run a management command.

---
+++ clevercloud/run_management_command.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -l
+
+set -ue
+
+if [[ "$CRON_ENABLED" != "1" ]]; then
+    echo "Crons not enabled."
+    exit 0
+fi
+
+cd $APP_HOME
+django-admin $@
--- scripts/antivirus-monthly.sh
+++
@@ -1,3 +0,0 @@
-set -ue
-
-django-admin s3_antivirus_scan c1-prod
