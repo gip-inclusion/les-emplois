44e647faef1c19ffecb5496304b9fcf41d82ad53
Xavier Fernandez <xavier.fernandez@beta.gouv.fr>
2022-11-25 18:17:35 +0100

    first escape issue with sed

--- clevercloud/pre_run.sh
+++ clevercloud/pre_run.sh
@@ -7,8 +7,9 @@ sops -d secrets-vault/c1/$ITOU_ENVIRONMENT.enc.env > .env
 
 pwd
 
+# Use @ instead of usual / in first sed to avoid the need to escape $APP_HOME
 cat clevercloud/uwsgi.ini \
-| sed "s/__APP_HOME__/$APP_HOME/g" \
+| sed "s@__APP_HOME__@$APP_HOME@g" \
 | sed "s/__HARAKIRI__/$HARAKIRI/g" \
 | sed "s/__CC_PYTHON_MODULE__/$CC_PYTHON_MODULE/g" \
 | sed "s/__WORKERS__/${WSGI_WORKERS:-2}/g" \
