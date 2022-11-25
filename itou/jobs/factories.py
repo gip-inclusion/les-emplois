f7dede1ce19b49b92c02f29811df7e8b25d9bc82
Victor Perron <victor@iso3103.net>
2022-11-25 11:52:04 +0100

    jobs: Remove old data files for ROMEs and Appellation
    We will sync via API now. If we need those, they'll be in git.
    
    Also, we renew the way those files were used during tests. There was a
    test routine (used with EVERY SiaeFactory) that would load the entirety
    of those gigantic JSON files, parse them and generate TENS OF THOUSANDS
    of DB requests, in the worst cases.
    
    Let's keep a single, short CSV file and load that one in TWO requests:
    one for the ROMEs, one for the Appellations.
    
    The repository grep will say thanks, and so will the speed of the tests.

--- itou/jobs/factories.py
+++ itou/jobs/factories.py
@@ -1,81 +1,42 @@
-import json
+import csv
 import os
+from collections import defaultdict
 
 from itou.jobs.models import Appellation, Rome
 
 
 CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 
-ROMES_JSON_FILE = f"{CURRENT_DIR}/management/commands/data/romes.json"
-
-APPELLATIONS_JSON_FILE = f"{CURRENT_DIR}/management/commands/data/appellations_for_romes.json"
+APPELLATIONS_CSV_FILE = f"{CURRENT_DIR}/data/appellations_test_fixture.csv"
 
 
 def create_test_romes_and_appellations(rome_codes, appellations_per_rome=30):
-    """
-    Not a factory strictly speaking, but it's sitting here for discoverability.
-
-    Create ROMEs and ROME's appellations based on the given ROME codes:
-        create_test_romes_and_appellations(['M1805'])
-
-    The number of appellations per ROME can be limited:
-        create_test_romes_and_appellations(['M1805', 'N1101'], appellations_per_rome=10)
-    """
-
-    done = 0
-
-    with open(ROMES_JSON_FILE, "r") as raw_json_data:
-
-        json_data = json.load(raw_json_data)
-
-        for item in json_data:
-
-            code = item["code"]
-            if code not in rome_codes:
+    with open(APPELLATIONS_CSV_FILE, "r", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        romes = set()
+        appellations = []
+        per_rome_code = defaultdict(int)
+        for line in reader:
+            rome_code = line["rome__code"]
+            if rome_codes and rome_code not in rome_codes:
                 continue
 
-            rome = Rome()
-            rome.code = code
-            rome.name = item["libelle"]
-            rome.riasec_major = item["riasecMajeur"]
-            rome.riasec_minor = item["riasecMineur"]
-            rome.code_isco = item["codeIsco"]
-            rome.save()
-
-            done += 1
-            if done == len(rome_codes):
-                break
-
-    if not appellations_per_rome:
-        return
-
-    appellations_counter = {code: 0 for code in rome_codes}
-    done = 0
-
-    with open(APPELLATIONS_JSON_FILE, "r") as raw_json_data:
-
-        json_data = json.load(raw_json_data)
-
-        for item in json_data:
-
-            code = item
-            if code not in rome_codes:
+            if per_rome_code[rome_code] >= appellations_per_rome:
                 continue
 
-            appellations_for_rome = json_data[code]
+            romes.add((rome_code, line["rome__name"]))
+            appellation = Appellation(code=line["code"], name=line["name"])
+            appellation._rome_code = rome_code
+            appellations.append(appellation)
+            per_rome_code[rome_code] += 1
 
-            for app in appellations_for_rome:
+        Rome.objects.bulk_create([Rome(code=d[0], name=d[1]) for d in romes], ignore_conflicts=True)
+        for appellation in appellations:
+            appellation.rome_id = Rome.objects.get(code=appellation._rome_code).pk
 
-                appellation = Appellation()
-                appellation.code = app["code"]
-                appellation.name = app["libelleCourt"]
-                appellation.rome_id = code
-                appellation.save()
-
-                appellations_counter[code] += 1
-                if appellations_counter[code] == appellations_per_rome:
-                    break
-
-            done += 1
-            if done == len(rome_codes):
-                break
+        Appellation.objects.bulk_create(
+            appellations,
+            update_conflicts=True,
+            update_fields=("name", "rome_id"),
+            unique_fields=("code",),
+        )
