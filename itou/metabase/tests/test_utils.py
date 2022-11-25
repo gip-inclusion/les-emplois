8b4c476d1b3422654eb57ede9c597824f375862a
dejafait <dejafait@blackhole.com>
2022-11-25 17:56:07 +0100

    [Metabase] Add test for get_qpv_job_seeker_pks()

---
+++ itou/metabase/tests/test_utils.py
@@ -0,0 +1,21 @@
+import pytest
+
+from itou.geo.factories import QPVFactory
+from itou.geo.utils import coords_to_geometry
+from itou.metabase.management.commands._utils import get_qpv_job_seeker_pks
+from itou.users.factories import JobSeekerFactory
+
+
+@pytest.mark.django_db
+def test_get_qpv_job_seeker_pks():
+    for code in ["QP093028"]:
+        QPVFactory(code=code)
+
+    # Somewhere in QPV QP093028 (Aubervilliers)
+    job_seeker_in_qpv = JobSeekerFactory(coords=coords_to_geometry("48.917735", "2.387311"))
+
+    # Somewhere not in a QPV near Aubervilliers
+    job_seeker_not_in_qpv = JobSeekerFactory(coords=coords_to_geometry("48.899", "2.412"))
+
+    assert job_seeker_in_qpv.pk in get_qpv_job_seeker_pks()
+    assert job_seeker_not_in_qpv.pk not in get_qpv_job_seeker_pks()
