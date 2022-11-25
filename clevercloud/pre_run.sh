a79d47d889828986f33acf7d1ebcf6439000b9a4
Xavier Fernandez <xavier.fernandez@beta.gouv.fr>
2022-11-25 17:46:20 +0100

    clevercloud: add uwsgi configuration in pre_run hook
    this allows us to easily tweak the uwsgi configuration
    
    In our case, simply add py-tracebacker option

--- clevercloud/pre_run.sh
+++ clevercloud/pre_run.sh
@@ -4,3 +4,10 @@ cd secrets-vault
 git pull
 cd -
 sops -d secrets-vault/c1/$ITOU_ENVIRONMENT.enc.env > .env
+
+cat clevercloud/uwsgi.ini \
+| sed "s/__APP_HOME__/$APP_HOME/g" \
+| sed "s/__HARAKIRI__/$HARAKIRI/g" \
+| sed "s/__CC_PYTHON_MODULE__/$CC_PYTHON_MODULE/g" \
+| sed "s/__WORKERS__/${WSGI_WORKERS:-2}/g" \
+> uwsgi.ini
