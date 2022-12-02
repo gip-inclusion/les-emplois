a2a190501af7376a864b262db730eb77969818a1
Victor Perron <victor@iso3103.net>
2022-12-02 15:38:24 +0100

    jobs: Add a command to synchronize ROMEs and Appellation
    It will now be handled monthly by a cron, the 15th of every month.

---
+++ itou/jobs/management/commands/sync_romes_and_appellations.py
@@ -0,0 +1,69 @@
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+
+from itou.jobs.models import Appellation, Rome
+from itou.utils.apis import pe_api_enums, pole_emploi_api_client
+from itou.utils.sync import yield_sync_diff
+
+
+# more than the number of Romes (~500) but less than the number of Appellations (~11000)
+BULK_CREATE_BATCH_SIZE = 1000
+
+
+def pe_data_to_appellation(data, at):
+    rome = Rome.objects.get(code=data["metier"]["code"])
+    return Appellation(code=data["code"], name=data["libelle"], updated_at=at, rome_id=rome.pk)
+
+
+def pe_data_to_rome(data, at):
+    return Rome(code=data["code"], name=data["libelle"], updated_at=at)
+
+
+class Command(BaseCommand):
+    """As explained at the time of writing this command, we simple update or create the new entries here,
+    still updating an `updated_at` column to at least keep track of the latest modification time.
+
+    No ROMEs or Appellation has ever been removed in the last 3 years, so we consider this case as rare
+    enough to not setup the whole process of keeping track of outdated objects in the sync, etc.
+    """
+
+    help = "Synchronizes ROMEs and Appellations from the PE API"
+
+    def add_arguments(self, parser):
+        parser.add_argument("--wet-run", dest="wet_run", action="store_true")
+
+    def handle(self, wet_run=False, **options):
+        now = timezone.now()
+        pe_client = pole_emploi_api_client()
+
+        romes_data = pe_client.referentiel(pe_api_enums.REFERENTIEL_ROME)
+        for line in yield_sync_diff(romes_data, "code", Rome.objects.all(), "code", [("libelle", "name")]):
+            self.stdout.write(line)
+
+        if wet_run:
+            romes = [pe_data_to_rome(item, now) for item in romes_data]
+            Rome.objects.bulk_create(
+                romes,
+                batch_size=BULK_CREATE_BATCH_SIZE,
+                update_conflicts=True,
+                update_fields=("name", "updated_at"),
+                unique_fields=("code",),
+            )
+            self.stdout.write(f"len={len(romes)} ROME entries have been created or updated.")
+
+        appellations_data = pe_client.appellations()
+        for line in yield_sync_diff(
+            appellations_data, "code", Appellation.objects.all(), "code", [("libelle", "name")]
+        ):
+            self.stdout.write(line)
+
+        if wet_run:
+            appellations = [pe_data_to_appellation(item, now) for item in appellations_data]
+            Appellation.objects.bulk_create(
+                appellations,
+                batch_size=BULK_CREATE_BATCH_SIZE,
+                update_conflicts=True,
+                update_fields=("name", "updated_at", "rome_id"),
+                unique_fields=("code",),
+            )
+            self.stdout.write(f"len={len(appellations)} Appellation entries have been created or updated.")
---
+++ itou/jobs/management/commands/test_sync_romes_and_appellations.py
@@ -0,0 +1,76 @@
+from django.core import management
+from django.test import override_settings
+
+from itou.jobs.models import Appellation, Rome
+
+
+@override_settings(
+    API_ESD={
+        "BASE_URL": "https://pe.fake",
+        "AUTH_BASE_URL": "https://auth.fr",
+        "KEY": "foobar",
+        "SECRET": "pe-secret",
+    }
+)
+def test_sync_rome_appellation(capsys, respx_mock):
+    respx_mock.post("https://auth.fr/connexion/oauth2/access_token?realm=%2Fpartenaire").respond(
+        200, json={"token_type": "foo", "access_token": "batman", "expires_in": 3600}
+    )
+    respx_mock.get("https://pe.fake/offresdemploi/v2/referentiel/metiers").respond(
+        200,
+        json=[
+            {"code": "F001", "libelle": "Pâtisserie avec accent"},
+            {"code": "MET01", "libelle": "Edition"},
+        ],
+    )
+    respx_mock.get("https://pe.fake/rome/v1/appellation?champs=code,libelle,metier(code)").respond(
+        200,
+        json=[
+            {"code": "JOB32", "libelle": "Ecriveur de bouquins", "metier": {"code": "MET01"}},
+            {"code": "papa01", "libelle": "Entraîneur sportif avéré", "metier": {"code": "B001"}},
+            {"code": "papa03", "libelle": "Chef cuistor d'élite", "metier": {"code": "F002"}},
+        ],
+    )
+
+    rome_0 = Rome(code="F001", name="Patisserie")
+    rome_1 = Rome(code="F002", name="Arts de la table")
+    rome_2 = Rome(code="B001", name="Métiers du corps")
+    rome_0.save()
+    rome_1.save()
+    rome_2.save()
+    appellation0 = Appellation(code="papa03", name="Chef cuistot d'élite", rome=rome_0)
+    appellation1 = Appellation(code="papa01", name="Entraîneur sportif", rome=rome_2)
+    appellation0.save()
+    appellation1.save()
+    management.call_command("sync_romes_and_appellations", wet_run=True)
+    stdout, stderr = capsys.readouterr()
+    assert stderr == ""
+    assert stdout.splitlines() == [
+        "count=1 label=Rome had the same key in collection and queryset",
+        "\tCHANGED name=Patisserie changes to libelle=Pâtisserie avec accent",
+        "count=1 label=Rome added by collection",
+        '\tADDED {"code": "MET01", "libelle": "Edition"}',
+        "count=2 label=Rome removed by collection",
+        "\tREMOVED Métiers du corps (B001)",
+        "\tREMOVED Arts de la table (F002)",  # not really removed though by our command, see docstring
+        "len=2 ROME entries have been created or updated.",
+        "count=2 label=Appellation had the same key in collection and queryset",
+        "\tCHANGED name=Entraîneur sportif changes to libelle=Entraîneur sportif " "avéré",
+        "\tCHANGED name=Chef cuistot d'élite changes to libelle=Chef cuistor d'élite",
+        "count=1 label=Appellation added by collection",
+        '\tADDED {"code": "JOB32", "libelle": "Ecriveur de bouquins", "metier": ' '{"code": "MET01"}}',
+        "count=0 label=Appellation removed by collection",
+        "len=3 Appellation entries have been created or updated.",
+    ]
+    rome_3 = Rome(code="MET01", name="Edition")
+    assert list(Rome.objects.all().order_by("code")) == [
+        Rome(code="B001", name="Métiers du corps"),
+        Rome(code="F001", name="Pâtisserie avec accent"),
+        Rome(code="F002", name="Arts de la table"),
+        rome_3,
+    ]
+    assert list(Appellation.objects.all().order_by("code")) == [
+        Appellation(code="JOB32", name="Ecriveur de bouquins", rome=rome_3),
+        Appellation(code="papa01", name="Entraîneur sportif avéré", rome=rome_2),
+        Appellation(code="papa03", name="Chef cuistot d'élite", rome=rome_1),  # it got switched to rome_1
+    ]
