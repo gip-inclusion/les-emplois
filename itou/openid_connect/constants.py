7fd242cc9232d4bda97b369d96cb327ae54dfa2e
Antoine LAURENT <antoine.c.laurent.pro@gmail.com>
2022-12-08 12:35:45 +0100

    openid_connect: Increase states lifetime

--- itou/openid_connect/constants.py
+++ itou/openid_connect/constants.py
@@ -1,5 +1,6 @@
 import datetime
 
 
-# This expiration time has been chosen arbitrarily.
-OIDC_STATE_EXPIRATION = datetime.timedelta(hours=1)
+# This expiration time has been set to 24 hours because Inclusion Connect's
+# email adress verification link is valid 24 hours.
+OIDC_STATE_EXPIRATION = datetime.timedelta(hours=24)
