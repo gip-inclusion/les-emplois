5ec1072efdaa4978b2dc01e3cb55f391e1877c87
dejafait <dejafait@blackhole.com>
2022-12-20 16:28:23 +0100

    metabase: Forward output to bucket

--- clevercloud/crons/populate_metabase_emplois.sh
+++ clevercloud/crons/populate_metabase_emplois.sh
@@ -11,25 +11,30 @@ set -o errexit
 # NOTE(vperron): this is not a proper getopt parsing but getopt is SO verbose for
 # just that simple use that I have rather use a very simple version. This script
 # is intended to be automated by a proper tool like Airflow anyway.
-if [[ "$1" == "--daily" ]]; then
-    django-admin send_slack_message ":rocket: lancement mise à jour de données C1 -> Metabase"
-    django-admin populate_metabase_emplois --mode=siaes
-    django-admin populate_metabase_emplois --mode=job_descriptions
-    django-admin populate_metabase_emplois --mode=organizations
-    django-admin populate_metabase_emplois --mode=job_seekers
-    django-admin populate_metabase_emplois --mode=job_applications
-    django-admin populate_metabase_emplois --mode=selected_jobs
-    django-admin populate_metabase_emplois --mode=approvals
-    django-admin populate_metabase_emplois --mode=final_tables
-    django-admin populate_metabase_emplois --mode=data_inconsistencies
-    django-admin send_slack_message ":white_check_mark: succès mise à jour de données C1 -> Metabase"
-elif [[ "$1" == "--monthly" ]]; then
-    django-admin send_slack_message ":rocket: lancement mise à jour de données peu fréquentes C1 -> Metabase"
-    django-admin populate_metabase_emplois --mode=rome_codes
-    django-admin populate_metabase_emplois --mode=insee_codes
-    django-admin populate_metabase_emplois --mode=departments
-    django-admin populate_metabase_emplois --mode=final_tables
-    django-admin send_slack_message ":white_check_mark: succès mise à jour de données peu fréquentes C1 -> Metabase"
-else
-    echo "populate_metabase_emplois shell script: unknown mode='$1' selected"
-fi
+function populate_metabase_emplois {
+    if [[ "$1" == "--daily" ]]; then
+        django-admin send_slack_message ":rocket: lancement mise à jour de données C1 -> Metabase"
+        django-admin populate_metabase_emplois --mode=siaes
+        django-admin populate_metabase_emplois --mode=job_descriptions
+        django-admin populate_metabase_emplois --mode=organizations
+        django-admin populate_metabase_emplois --mode=job_seekers
+        django-admin populate_metabase_emplois --mode=job_applications
+        django-admin populate_metabase_emplois --mode=selected_jobs
+        django-admin populate_metabase_emplois --mode=approvals
+        django-admin populate_metabase_emplois --mode=final_tables
+        django-admin populate_metabase_emplois --mode=data_inconsistencies
+        django-admin send_slack_message ":white_check_mark: succès mise à jour de données C1 -> Metabase"
+    elif [[ "$1" == "--monthly" ]]; then
+        django-admin send_slack_message ":rocket: lancement mise à jour de données peu fréquentes C1 -> Metabase"
+        django-admin populate_metabase_emplois --mode=rome_codes
+        django-admin populate_metabase_emplois --mode=insee_codes
+        django-admin populate_metabase_emplois --mode=departments
+        django-admin populate_metabase_emplois --mode=final_tables
+        django-admin send_slack_message ":white_check_mark: succès mise à jour de données peu fréquentes C1 -> Metabase"
+    else
+        echo "populate_metabase_emplois shell script: unknown mode='$1' selected"
+    fi
+}
+
+# `$1` is needed to forward the selected mode (e.g. `--daily`)
+populate_metabase_emplois $1 |& tee -a "shared_bucket/populate_metabase_emplois/output_$(date '+%Y-%m-%d_%H-%M-%S').log"
