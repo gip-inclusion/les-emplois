gip-inclusion/les-emplois

commit db56bc9c4d0d6f437a43efbe2c69c62d8991843b

siaes: move job_app_score to a database model
refreshed every hour
(avoiding to start at minute 0 since we already have a lot of cron
there)

On my local computer:

    $ ./manage.py update_siaes_job_app_score
    Updated 7201 Siaes in 0.203 seconds

itou/www/siaes_views/views.py
@@ -80,7 +80,7 @@ def job_description_card(request, job_description_id, template_name="siaes/job_d
 @login_required
 @require_safe
 def card_search_preview(request, template_name="siaes/includes/_card_siae.html"):
-    siae = get_current_siae_or_404(request, with_job_app_score=True, with_job_descriptions=True)
+    siae = get_current_siae_or_404(request, with_job_descriptions=True)
     job_descriptions = (
         SiaeJobDescription.objects.filter(siae__pk=siae.pk, is_active=True)
         .prefetch_related("appellation", "appellation__rome", "siae")
@@ -96,7 +96,7 @@ def card_search_preview(request, template_name="siaes/includes/_card_siae.html")
 @login_required
 @transaction.atomic
 def job_description_list(request, template_name="siaes/job_description_list.html"):
-    siae = get_current_siae_or_404(request, with_job_app_score=True, with_job_descriptions=True)
+    siae = get_current_siae_or_404(request, with_job_descriptions=True)
     job_descriptions = (
         SiaeJobDescription.objects.filter(siae__pk=siae.pk)
         .prefetch_related("appellation", "appellation__rome", "siae")
@@ -397,7 +397,7 @@ def select_financial_annex(request, template_name="siaes/select_financial_annex.
 
 
 def card(request, siae_id, template_name="siaes/card.html"):
-    queryset = Siae.objects.prefetch_job_description_through().with_job_app_score()
+    queryset = Siae.objects.prefetch_job_description_through()
 
     siae = get_object_or_404(queryset, pk=siae_id)
     jobs_descriptions = siae.job_description_through.all()

itou/www/search/views.py
@@ -160,7 +160,7 @@ class EmployerSearchView(EmployerSearchBaseView):
             # with 9 members, then siaes with 8 members etc...
             # This is clearly not what we want. We want to show siaes with members
             # (whatever the number of members is) then siaes without members.
-            .with_has_active_members().with_job_app_score()
+            .with_has_active_members()
             # Sort in 4 subgroups in the following order, each subgroup being sorted by job_app_score.
             # 1) has_active_members and not block_job_applications
             # These are the siaes which can currently hire, and should be on top.

itou/siaes/management/commands/update_siaes_job_app_score.py
@@ -1,0 +1,22 @@
+import time
+
+from django.core.management.base import BaseCommand
+from django.db.models import F, Q
+
+from itou.siaes import models
+
+
+class Command(BaseCommand):
+    help = """Update the SIAE job_app_score"""
+
+    def handle(self, **options):
+        start = time.perf_counter()
+        nb_updated = (
+            models.Siae.objects.with_computed_job_app_score()
+            # Do not update if nothing changes (NULL values have to be handled separately because NULL)
+            .exclude(
+                Q(job_app_score=F("computed_job_app_score"))
+                | Q(job_app_score__isnull=True) & Q(computed_job_app_score__isnull=True)
+            ).update(job_app_score=F("computed_job_app_score"))
+        )
+        self.stdout.write(f"Updated {nb_updated} Siaes in {time.perf_counter() - start:.3f} seconds")

itou/siaes/migrations/0063_siae_job_app_score.py
@@ -1,0 +1,21 @@
+# Generated by Django 4.1.4 on 2022-12-21 16:33
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("siaes", "0062_alter_siaejobdescription_unique_together"),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name="siae",
+            name="job_app_score",
+            field=models.FloatField(
+                null=True,
+                verbose_name="Score de recommandation (ratio de candidatures récentes vs nombre d'offres d'emploi)",
+            ),
+        ),
+    ]

itou/siaes/migrations/0064_fill_siae_job_app_score.py
@@ -1,0 +1,21 @@
+# Generated by Django 4.1.4 on 2022-12-21 16:33
+
+from django.db import migrations
+from django.db.models import F
+
+from itou.siaes import models
+
+
+def _fill_siae_job_app_score(apps, schema_editor):
+    # It's simpler to use Siae query set method (and not apps.get_model)
+    # if the queryset method disappears, this fill method can be replaced by a RunPython.noop
+    models.Siae.objects.with_computed_job_app_score().update(job_app_score=F("computed_job_app_score"))
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("siaes", "0063_siae_job_app_score"),
+    ]
+
+    operations = [migrations.RunPython(_fill_siae_job_app_score, reverse_code=migrations.RunPython.noop)]

itou/siaes/models.py
@@ -131,7 +131,7 @@ class SiaeQuerySet(OrganizationQuerySet):
         )
         return self.annotate(count_active_job_descriptions=Coalesce(sub_query, 0))
 
-    def with_job_app_score(self):
+    def with_computed_job_app_score(self):
         """
         Employers search results boost SIAE which did not receive enough job applications
         compared to their total job descriptions.
@@ -158,7 +158,7 @@ class SiaeQuerySet(OrganizationQuerySet):
             self.with_count_recent_received_job_apps()
             .with_count_active_job_descriptions()
             .annotate(
-                job_app_score=Case(
+                computed_job_app_score=Case(
                     When(has_active_job_desc, then=get_score),
                     default=None,
                 )
@@ -255,6 +255,10 @@ class Siae(AddressMixin, OrganizationAbstract):
         blank=True,
         null=True,
         related_name="siaes",
+    )
+
+    job_app_score = models.FloatField(
+        verbose_name="Score de recommandation (ratio de candidatures récentes vs nombre d'offres d'emploi)", null=True
     )
 
     objects = models.Manager.from_queryset(SiaeQuerySet)()

itou/siaes/tests/test_management_commands.py
@@ -2,6 +2,7 @@ import io
 
 from django.core import management
 
+from itou.job_applications.factories import JobApplicationFactory
 from itou.siae_evaluations.factories import EvaluatedSiaeFactory
 from itou.siaes import factories as siaes_factories
 from itou.siaes.enums import SiaeKind
@@ -43,3 +44,22 @@ class MoveSiaeDataTest(TestCase):
         )
         self.assertEqual(siae1.evaluated_siaes.count(), 0)
         self.assertEqual(siae2.evaluated_siaes.count(), 1)
+
+
+def test_update_siaes_job_app_score():
+    siae1 = siaes_factories.SiaeFactory()
+    siae2 = JobApplicationFactory(to_siae__with_jobs=True).to_siae
+
+    assert siae1.job_app_score is None
+    assert siae2.job_app_score is None
+
+    stdout = io.StringIO()
+    management.call_command("update_siaes_job_app_score", stdout=stdout)
+    # siae1 did not change (from None to None)
+    assert "Updated 1 Siaes" in stdout.getvalue()
+
+    siae1.refresh_from_db()
+    siae2.refresh_from_db()
+
+    assert siae1.job_app_score is None
+    assert siae2.job_app_score is not None

itou/siaes/tests/tests_models.py
@@ -279,14 +279,14 @@ class SiaeQuerySetTest(TestCase):
 
         self.assertEqual(expected, result.count_recent_received_job_apps)
 
-    def test_with_job_app_score(self):
+    def test_with_computed_job_app_score(self):
         siae = SiaeFactory(with_jobs=True, romes=("N1101", "N1105", "N1103", "N4105"))
         siae.job_description_through.first()
         JobApplicationFactory(to_siae=siae)
         JobApplicationFactory(to_siae=siae)
 
         expected_score = siae.job_applications_received.count() / siae.job_description_through.count()
-        result = Siae.objects.with_job_app_score().get(pk=siae.pk)
+        result = Siae.objects.with_computed_job_app_score().get(pk=siae.pk)
 
         active_job_descriptions = (
             Siae.objects.with_count_active_job_descriptions().get(pk=siae.pk).count_active_job_descriptions
@@ -296,17 +296,17 @@ class SiaeQuerySetTest(TestCase):
             Siae.objects.with_count_recent_received_job_apps().get(pk=siae.pk).count_recent_received_job_apps
         )
         self.assertEqual(recent_job_apps, 2)
-        self.assertEqual(expected_score, result.job_app_score)
+        self.assertEqual(expected_score, result.computed_job_app_score)
 
-    def test_with_job_app_score_no_job_description(self):
+    def test_with_computed_job_app_score_no_job_description(self):
         siae = SiaeFactory()
         JobApplicationFactory(to_siae=siae)
         JobApplicationFactory(to_siae=siae)
 
         expected_score = None
-        result = Siae.objects.with_job_app_score().get(pk=siae.pk)
+        result = Siae.objects.with_computed_job_app_score().get(pk=siae.pk)
 
-        self.assertEqual(expected_score, result.job_app_score)
+        self.assertEqual(expected_score, result.computed_job_app_score)
 
     def test_with_count_active_job_descriptions(self):
         siae = SiaeFactory(with_jobs=True, romes=("N1101", "N1105", "N1103", "N4105"))

commit c0e34c2ffdfb35fcf8e6af679dfc93b275f0edff

Restore active job descriptions on SIAE cards
Regression introduced in db56bc9c4d0d6f437a43efbe2c69c62d8991843b.

itou/www/siaes_views/views.py
@@ -397,7 +397,7 @@ def select_financial_annex(request, template_name="siaes/select_financial_annex.
 
 
 def card(request, siae_id, template_name="siaes/card.html"):
-    queryset = Siae.objects.prefetch_job_description_through()
+    queryset = Siae.objects.prefetch_job_description_through().with_count_active_job_descriptions()
 
     siae = get_object_or_404(queryset, pk=siae_id)
     jobs_descriptions = siae.job_description_through.all()

itou/www/siaes_views/tests/tests_views.py
@@ -7,10 +7,13 @@ from django.urls import reverse
 from django.urls.exceptions import NoReverseMatch
 from django.utils.html import escape
 
+from itou.cities.factories import create_city_vannes
+from itou.jobs.factories import create_test_romes_and_appellations
 from itou.siaes.enums import SiaeKind
 from itou.siaes.factories import (
     SiaeConventionFactory,
     SiaeFactory,
+    SiaeJobDescriptionFactory,
     SiaeWith2MembershipsFactory,
     SiaeWithMembershipAndJobsFactory,
 )
@@ -21,8 +24,14 @@ from itou.utils.test import TestCase
 
 
 class CardViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        create_test_romes_and_appellations(("N1101", "N1105", "N1103", "N4105"))
+        cls.vannes = create_city_vannes()
+
     def test_card(self):
         siae = SiaeFactory(with_membership=True)
+        job_description = SiaeJobDescriptionFactory(siae=siae, custom_name="Plaquiste")
         url = reverse("siaes_views:card", kwargs={"siae_id": siae.pk})
         response = self.client.get(url)
         assert response.status_code == 200
@@ -30,6 +39,35 @@ class CardViewTest(TestCase):
         self.assertContains(response, escape(siae.display_name))
         self.assertContains(response, siae.email)
         self.assertContains(response, siae.phone)
+        self.assertContains(
+            response,
+            f"""
+            <p class="fs-sm text-secondary">
+                <b>1 recrutement en cours</b>
+            </p>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item list-group-item-action px-0 py-2 ">
+                    <a class="d-flex flex-wrap align-items-center text-decoration-none matomo-event"
+                       href="/siae/job_description/{job_description.pk}/card?back_url=/siae/{siae.pk}/card"
+                       data-matomo-category="candidature"
+                       data-matomo-action="clic"
+                       data-matomo-option="clic-metiers">
+                        <div class="d-inline">
+                            <span class="font-weight-bold mr-1 mr-md-2">Plaquiste</span>
+                        </div>
+                        <div class="d-inline ml-lg-auto">
+                            <span class="fs-sm text-nowrap">
+                                <i class="ri-map-pin-2-line ri-sm"></i>
+                                Vannes (56)
+                            </span>
+                        </div>
+                    </a>
+                </li>
+            </ul>
+            """,
+            html=True,
+            count=1,
+        )
 
 
 class JobDescriptionCardViewTest(TestCase):