e076f279c741e1ec894b9b0b3e714991fea7e3f4
Antoine LAURENT <antoine.c.laurent.pro@gmail.com>
2023-01-16 08:34:41 +0100

    CSP: Enable for real
    We will now enforce CSP on all environments
    We will only send reports on review apps

--- config/settings/base.py
+++ config/settings/base.py
@@ -634,5 +634,4 @@ if S3_STORAGE_ENDPOINT_DOMAIN:
     ]
 CSP_INCLUDE_NONCE_IN = ["script-src", "script-src-elem"]
 CSP_REPORT_URI = os.getenv("CSP_REPORT_URI", None)
-CSP_REPORT_ONLY = True
 CSP_REPORT_PERCENTAGE = float(os.getenv("CSP_REPORT_PERCENTAGE", 1))
