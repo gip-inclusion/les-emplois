2e87e7e0527731fa3c2fc5d70c1cf774e00d63b2
Yannick Passarelli <yannick.passarelli@gmail.com>
2023-01-24 16:01:43 +0100

    metabase : indentation changes

--- itou/metabase/sql/021_tx_candidats_acceptes_par_prescripteurs.sql
+++ itou/metabase/sql/021_tx_candidats_acceptes_par_prescripteurs.sql
@@ -1,16 +1,26 @@
-select 
-    origine_détaillée, 
-    date_candidature, 
-    count(distinct(id_candidat)) filter (where(état = 'Candidature acceptée')) as nombre_candidats_acceptés,
+select
+    origine_détaillée,
+    date_candidature,
+    count(distinct(id_candidat)) filter (
+    where
+        (
+            état = 'Candidature acceptée'
+        )
+    ) as nombre_candidats_acceptés,
     count(distinct(id_candidat)) as nombre_candidats,
-    count(distinct(id)) filter (where(état = 'Candidature acceptée')) as nombre_candidatures_acceptées,
+    count(distinct(id)) filter (
+    where
+        (
+            état = 'Candidature acceptée'
+        )
+    ) as nombre_candidatures_acceptées,
     count(distinct(id)) as nombre_candidatures,
     injection_ai
-from 
+from
     candidatures
-where 
+where
     lower(origine_détaillée) like 'prescripteur%'
-group by 
-    origine_détaillée, 
+group by
+    origine_détaillée,
     date_candidature,
-    injection_ai 
+    injection_ai
--- itou/metabase/sql/022_delai_recrutement.sql
+++ itou/metabase/sql/022_delai_recrutement.sql
@@ -2,31 +2,32 @@
  L'objectif est de suivre le délai de recrutement des candidats en IAE.
  Statistiques d'impact des emplois de l'inclusion : maintenir un délai de recrutement inférieur à 30 jours 
 */
-
 with premiere_candidature as (
-    select 
+    select
         id_candidat,
-        min(date_candidature) as min_date_candidature 
-    from 
-        candidatures 
-    group by 
-        id_candidat  ) 
-select     
-    distinct (c.id_candidat) as identifiant_candidat, 
+        min(date_candidature) as min_date_candidature
+    from
+        candidatures
+    group by
+        id_candidat
+)
+select
+    distinct (c.id_candidat) as identifiant_candidat,
     pc.min_date_candidature,
     min(date_embauche) as min_date_embauche,
     min(date_embauche) - pc.min_date_candidature as delai_recrutement_jours,
     injection_ai
-from 
+from
     candidatures c
 left join 
     premiere_candidature pc
-    on c.id_candidat = pc.id_candidat
-where 
+    on
+    c.id_candidat = pc.id_candidat
+where
     date_embauche is not null
-    and état = 'Candidature acceptée' 
+    and état = 'Candidature acceptée'
     and origine != 'Employeur'
-group by 
-    c.id_candidat, 
+group by
+    c.id_candidat,
     pc.min_date_candidature,
-    injection_ai 
+    injection_ai
