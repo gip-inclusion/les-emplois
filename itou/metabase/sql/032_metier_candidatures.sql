0e1974b966cb33cad0728b77105829fe22211c59
laurinehu <laurinehu@gmail.com>
2023-01-27 16:47:34 +0100

    metabase: minor indentation and table name correction

--- itou/metabase/sql/032_metier_candidatures.sql
+++ itou/metabase/sql/032_metier_candidatures.sql
@@ -1,7 +1,8 @@
 select
     id_candidat,
     id_candidature,
-    id_fiche_de_poste id_structure,
+    id_fiche_de_poste,
+    id_structure,
     nom_structure,
     département_structure,
     nom_département_structure,
@@ -14,12 +15,13 @@ select
     date_embauche,
     état,
     département_employeur,
-    nom_département_employeur domaine_professionnel,
+    nom_département_employeur,
+    domaine_professionnel,
     grand_domaine,
     crdp.code_rome,
     nom_rome
 from
     candidatures_echelle_locale cel
-    inner join fiches_de_poste_par_candidature_v2 fdpc on fdpc.id_candidature = cel.id
+    inner join fiches_de_poste_par_candidature fdpc on fdpc.id_candidature = cel.id
     inner join fiches_de_poste fdp on fdpc.id_fiche_de_poste = fdp.id
     inner join code_rome_domaine_professionnel crdp on fdp.code_rome = crdp.code_rome
