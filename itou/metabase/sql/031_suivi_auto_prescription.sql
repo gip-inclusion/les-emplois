ca09dbfdcc3b02be99db11d3518323ec1710b809
Yannick Passarelli <yannick.passarelli@gmail.com>
2023-02-07 18:15:54 +0100

    metabase : not so dumb additions + join change

--- itou/metabase/sql/031_suivi_auto_prescription.sql
+++ itou/metabase/sql/031_suivi_auto_prescription.sql
@@ -23,10 +23,20 @@ select
     cd.département_structure,
     cd.nom_département_structure,
     cd.région_structure,
-    cd.injection_ai as injection_ai_candidatures,
-    c.injection_ai as injection_ai_candidats
-from candidats c
-    left join candidatures cd
-        on c.id = cd.id_candidat 
-/* on considère que l'on a de l'auto prescription lorsque l'employeur est l'auteur du diagnostic et effectue l'embauche */
-where c.type_auteur_diagnostic = 'Employeur' and cd.origine = 'Employeur' and c.id_auteur_diagnostic_employeur = cd.id_structure 
+    /* on considère que l'on a de l'auto prescription lorsque l'employeur est l'auteur du diagnostic et effectue l'embauche */
+    /* En créant une colonne on peut comparer les candidatures classiques à l'auto prescription */
+    case	
+    	when c.type_auteur_diagnostic = 'Employeur' and cd.origine = 'Employeur' and c.id_auteur_diagnostic_employeur = cd.id_structure then 'Autoprescription'
+    	else 'parcours classique'
+    end type_de_candidature,
+    case 
+    	when c.injection_ai = 0 then 'Non'
+    	else 'Oui'
+    end reprise_de_stock_ai_candidats,
+    case 
+    	when cd.injection_ai = 0 then 'Non'
+    	else 'Oui'
+    end reprise_de_stock_ai_candidatures
+from candidatures cd
+    left join candidats c
+        on cd.id_candidat = c.id
