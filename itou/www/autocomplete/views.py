a9be3a074b2703cd55029963258f977e492cd97a
Victor Perron <victor@iso3103.net>
2023-02-07 17:42:03 +0100

    autocomplete: Fix issue in looking for cities
    The alphabetical order was actually not properly done.

--- itou/www/autocomplete/views.py
+++ itou/www/autocomplete/views.py
@@ -1,7 +1,7 @@
 from datetime import datetime
-from os.path import commonprefix
 
-from django.db.models import Q
+from django.db.models import F, Q, Value
+from django.db.models.functions import Least, Lower, NullIf, StrIndex
 from django.http import JsonResponse
 from unidecode import unidecode
 
@@ -30,37 +30,34 @@ def cities_autocomplete(request):
 
     if term:
         if term.isdigit():
-            cities = City.objects.filter(post_codes__contains=[term]).order_by("name", "department")[
-                :MAX_CITIES_TO_RETURN
-            ]
+            cities = City.objects.filter(post_codes__contains=[term]).order_by("name", "department")
 
         else:
             term = unidecode(term.lower())
             term_spaced = term.replace("-", " ")
             term_hyphenated = term.replace(" ", "-")
-            index_term = commonprefix([term_spaced, term_hyphenated])
-            cities = sorted(
-                # The double search does not seem ideal, but we *did* start with a trigram similarity
-                # and word similarity approach. It is disappointing since it does return results that
-                # are not expected, for instance results containing letters not present in the search.
-                # It has been decided with the UX to use the simplest approach. Secondly, it seems
-                # that most people look for a city by "the start of the name", not by "any word within
-                # the name" so the icontains lookup, ordered by index of the matching string, feels more
-                # natural.
-                # The hyphenated/unhyphenated thing has been added considering the mess that hyphens
-                # represent in french city names. It is ugly, but simpler than the other approach which
-                # could have been to look for the slugs, but requires a perfect sync of th slug and name
-                # at all times, just for the search.
+            # We started with a trigram similarity and word similarity approach. It is disappointing
+            # since it does return results that are not expected, for instance results containing
+            # letters not present in the search.
+            # It has been decided with the UX to use the simplest approach. It seems that most
+            # people look for a city by "the start of the name", not by "any word within the name"
+            # so the icontains lookup, ordered by index of the matching string, feels more natural.
+            # The hyphenated/unhyphenated thing has been added considering the mess that hyphens
+            # represent in french city names. It should be improved in the future to handle cases
+            # such as search terms “La Chapelle du” not finding La Chapelle-du-Châtelard.
+            cities = (
                 City.objects.filter(
                     Q(name__unaccent__icontains=term_spaced) | Q(name__unaccent__icontains=term_hyphenated)
-                )[:MAX_CITIES_TO_RETURN],
-                # - the results __starting__ by the searched term are favoured (Paris over Cormeil-en-Parisis)
-                # - then if the length is the same, by alphabetic order
-                # - then if everything is the same (Sainte-Colombe...) by department.
-                key=lambda c: (unidecode(c.name.lower()).index(index_term), c.name, c.department),
+                )
+                .annotate(
+                    spaced_index=NullIf(StrIndex(Lower("name__unaccent"), Value(term_spaced)), 0),
+                    hyphenated_index=NullIf(StrIndex(Lower("name__unaccent"), Value(term_hyphenated)), 0),
+                    best_index=Least(F("spaced_index"), F("hyphenated_index")),
+                )
+                .order_by(F("best_index").asc(nulls_last=True), "name", "department")
             )
 
-        cities = [{"value": city.display_name, "slug": city.slug} for city in cities]
+        cities = [{"value": city.display_name, "slug": city.slug} for city in cities[:MAX_CITIES_TO_RETURN]]
 
     return JsonResponse(cities, safe=False)
 
