e3027cfb5131294a53378c0d039e814b29423cc2
Yannick Passarelli <yannick.passarelli@gmail.com>
2023-02-09 15:45:45 +0100

    metabase : addition of structure type

--- itou/metabase/sql/033_siae_pratiquant_autoprescription.sql
+++ itou/metabase/sql/033_siae_pratiquant_autoprescription.sql
@@ -3,26 +3,31 @@ with siae_autopr as (
         count (
             distinct (id_structure)
         ) as total_siae_autopr,
-        1 as id
+        type_structure
     from
         suivi_auto_prescription sap
     where
         type_de_candidature = 'Autoprescription'
+    group by 
+        type_structure
 ),
 siae_all as (
     select 
         count (
             distinct (id_structure)
         ) as total_siae_all,
-        1 as id
+        type_structure
     from
         suivi_auto_prescription sap
+    group by 
+        type_structure
 )
 select
     total_siae_autopr as "Nombre de structures utilisant l'autoprescription",
     total_siae_all as "Nombre total de structures",
-    cast (total_siae_autopr as numeric)/cast (total_siae_all as numeric) as "% structures utilisant l'autoprescription"
+    sau.type_structure
+    /*cast (total_siae_autopr as numeric)/cast (total_siae_all as numeric) as "% structures utilisant l'autoprescription"*/
 from
     siae_autopr sau
 left join siae_all sall 
-    on sau.id = sall.id
+    on sau.type_structure = sall.type_structure
