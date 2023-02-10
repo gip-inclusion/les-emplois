92f873ce83abb5e4baf29ddda2a85705d9c76fd1
Antoine LAURENT <antoine.c.laurent.pro@gmail.com>
2023-02-10 11:03:31 +0100

    scripts: Improve update_suspensions_end_at

--- itou/scripts/management/commands/update_suspensions_end_at.py
+++ itou/scripts/management/commands/update_suspensions_end_at.py
@@ -1,43 +1,43 @@
 import io
-from datetime import datetime as dt
+from datetime import date, datetime, timedelta
 
 import pandas
-from dateutil.relativedelta import relativedelta
 from django.core.exceptions import ValidationError
 from django.core.management.base import BaseCommand
+from django.db import transaction
+from django.db.models import F
 from django.utils import timezone
 
 from itou.approvals.models import Approval, Suspension
 
 
-FIRST_SCRIPT_RUNNING_DATE = timezone.make_aware(dt(2023, 2, 1, 17, 44))
+FIRST_SCRIPT_RUNNING_DATE = datetime(2023, 2, 1, 16, 44, tzinfo=timezone.utc)
 MONTHS_OF_PROLONGATION = 24
 
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument("--wet-run", action="store_true", dest="wet_run")
         parser.add_argument(
-            "--file-path",
             dest="file_path",
-            required=True,
             action="store",
             help="Path of the ASP CSV file to deduplicate",
         )
+        parser.add_argument("--wet-run", action="store_true", dest="wet_run")
 
     def clean_file(self, file_path):
-        with open(file_path, newline="", encoding="utf-8") as input_file:
+        with open(file_path) as input_file:
             output_file = io.StringIO()
             for line in input_file:
-                if not line.strip("\n").startswith("\t! skipping "):
+                if not line.startswith("\t! skipping "):
                     output_file.write(line)
         output_file.seek(0)
         return output_file
 
+    @transaction.atomic()
     def handle(self, file_path, wet_run=False, **options):
-        max_threshold = dt.today() + relativedelta(months=Suspension.MAX_DURATION_MONTHS)
-        suspensions = Suspension.objects.filter(end_at__gte=max_threshold)
-        self.stdout.write(f"Problematic suspensions found in database: {suspensions.count()}.")
+        max_threshold = timedelta(365 * Suspension.MAX_DURATION_MONTHS / 12)
+        suspensions = Suspension.objects.filter(end_at__gt=F("start_at") + max_threshold)
+        self.stdout.write(f"Problematic suspensions found in database: {suspensions.count()}")
 
         csv_file = self.clean_file(file_path)
         colnames = ["approval_number", "suspension_start_at", "suspension_end_at"]
@@ -53,11 +53,11 @@ class Command(BaseCommand):
 
         # Just for the records as we only have one.
         # Should be treated manually.
-        excluded_suspensions = suspensions.exclude(approval__number__in=unique_approval_numbers_from_file)
-        for suspension in excluded_suspensions:
+        suspensions_not_in_file = suspensions.exclude(approval__number__in=unique_approval_numbers_from_file)
+        for suspension in suspensions_not_in_file:
             self.stdout.write(
-                "Suspension not in log file. "
-                f"Pk: {suspension.pk}, start_at: {suspension.start_at}, end_at: {suspension.end_at}"
+                "Suspension not in log file : "
+                f"pk={suspension.pk} start_at={suspension.start_at} end_at={suspension.end_at}"
             )
 
         suspensions = suspensions.filter(approval__number__in=unique_approval_numbers_from_file)
@@ -67,55 +67,80 @@ class Command(BaseCommand):
         # Generated dataframe:
         #               approval_number  size
         #        35509  9999922919054    4
-        df = df.groupby(by="approval_number", as_index=False).size().sort_values("size")
+        df_gouped = df.groupby(by="approval_number", as_index=False).size().sort_values("size")
+        # Only keep lines with more the one value
+        df_gouped = df_gouped[df_gouped["size"] > 1]
+        self.stdout.write(f"Anomalies found in input file: {len(df_gouped)}")
 
+        self.stdout.write("")
+        self.stdout.write("Start processing")
+        self.stdout.write("========================================")
         updated_suspensions_counter = 0
-        skipped_suspensions_no_duplicate_counter = 0
         skipped_suspensions_other_reason_counter = 0
-        for _, row in df.iterrows():
-            # Only approvals with several suspensions have caused troubles.
-            if row["size"] == 1:
-                skipped_suspensions_no_duplicate_counter += 1
-                continue
-
+        for _, row in df_gouped.iterrows():
             try:
                 approval = Approval.objects.get(number=row["approval_number"])
             except Approval.DoesNotExist:
-                self.stdout.write(f"Skipping approval not found: {row['approval_number']}")
+                self.stdout.write(f"Skipping approval not found: approval={row['approval_number']}")
                 skipped_suspensions_other_reason_counter += 1
                 continue
 
             last_suspension = approval.suspension_set.order_by("end_at").last()
+            theorical_suspension_end = max(df[df["approval_number"] == approval.number]["suspension_end_at"])
             # Don't update suspensions updated by a user after the first script ran.
-            if last_suspension.updated_at and last_suspension.updated_at > FIRST_SCRIPT_RUNNING_DATE:
+            if str(last_suspension.end_at) != theorical_suspension_end:
+                updated_at = last_suspension.updated_at.date() if last_suspension.updated_at else "None"
                 self.stdout.write(
                     "Skipping suspension updated after script ran: "
-                    f"{last_suspension.pk}, start_at: {last_suspension.start_at}, end_at: {last_suspension.end_at}, "
-                    f"updated_at: {last_suspension.updated_at.date()}"
+                    f"pk={last_suspension.pk} start_at={last_suspension.start_at} end_at={last_suspension.end_at} "
+                    f"updated_at={updated_at}"
                 )
                 skipped_suspensions_other_reason_counter += 1
+                if last_suspension.end_at > Suspension.get_max_end_at(last_suspension.start_at):
+                    self.stdout.write(">  Still to long though !")
                 continue
 
-            last_suspension.end_at -= relativedelta(months=MONTHS_OF_PROLONGATION * (row["size"] - 1))
+            previous_end_at = last_suspension.end_at
+            last_suspension.end_at = date.fromisoformat(
+                min(df[df["approval_number"] == approval.number]["suspension_end_at"])
+            )
+
             try:
                 last_suspension.clean()
+                if last_suspension.end_at != Suspension.get_max_end_at(last_suspension.start_at):
+                    self.stdout.write(
+                        f"Updated suspension isn't 36 month long. Is it normal ? "
+                        f"pk={last_suspension.pk} start_at={last_suspension.start_at} end_at={last_suspension.end_at} "
+                    )
                 updated_suspensions_counter += 1
             except ValidationError as error:
                 self.stdout.write("; ".join(error.messages))
                 self.stdout.write(
                     (
-                        f">  Skipping because of validation error! Suspension: {last_suspension.pk}, "
-                        f"Approval number: {row['approval_number']}"
+                        f">  Skipping because of validation error! Suspension pk={last_suspension.pk} "
+                        f"approval={row['approval_number']} start_at={last_suspension.start_at} "
+                        f"end_at={last_suspension.end_at}"
                     )
                 )
-                self.stdout.write(f"start_at: {last_suspension.start_at}, end_at: {last_suspension.end_at}")
                 skipped_suspensions_other_reason_counter += 1
-                continue
+            else:
+                self.stdout.write(
+                    (
+                        f"Updating suspension pk={last_suspension.pk} approval={row['approval_number']} "
+                        f"start_at={last_suspension.start_at} "
+                        f"old_end_at={previous_end_at} new_end_at={last_suspension.end_at}"
+                    )
+                )
+                if wet_run:
+                    last_suspension.save(update_fields=["end_at", "updated_at"])
 
-            if wet_run:
-                last_suspension.save(update_fields=["end_at", "updated_at"])
+        self.stdout.write("")
+        self.stdout.write("Results")
+        self.stdout.write("========================================")
         self.stdout.write(f"Updated suspensions: {updated_suspensions_counter}")
-        self.stdout.write(
-            f"Skipped suspensions (one suspension per approval): {skipped_suspensions_no_duplicate_counter}"
-        )
-        self.stdout.write(f"Skipped suspensions (other reasons): {skipped_suspensions_other_reason_counter}")
+        self.stdout.write(f"Skipped suspensions (see reasons above): {skipped_suspensions_other_reason_counter}")
+
+        if updated_suspensions_counter + skipped_suspensions_other_reason_counter != len(df_gouped):
+            self.stdout.write("Some suspensions were not updated or skipped !!")
+        else:
+            self.stdout.write("Everything is good")
