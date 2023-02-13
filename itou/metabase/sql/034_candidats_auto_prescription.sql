937b06f8c82641a01e2b40761d53028761db736e
Yannick Passarelli <yannick.passarelli@gmail.com>
2023-02-13 15:54:40 +0100

    metabase : fixing a duplicated id problem

--- itou/metabase/sql/034_candidats_auto_prescription.sql
+++ itou/metabase/sql/034_candidats_auto_prescription.sql
@@ -73,7 +73,47 @@ all_candidates as (
     group by c2.id
 )
 select
-    *
+    autopr_c.id,
+    état,
+    age,
+    total_critères_niveau_1,
+    total_critères_niveau_2,
+    critère_n1_bénéficiaire_du_rsa,
+    critère_n1_allocataire_ass,
+    critère_n1_allocataire_aah,
+    critère_n1_detld_plus_de_24_mois,
+    critère_n2_niveau_d_étude_3_cap_bep_ou_infra,
+    critère_n2_senior_plus_de_50_ans,
+    critère_n2_jeune_moins_de_26_ans,
+    critère_n2_sortant_de_l_ase,
+    critère_n2_deld_12_à_24_mois,
+    critère_n2_travailleur_handicapé,
+    critère_n2_parent_isolé,
+    critère_n2_personne_sans_hébergement_ou_hébergée_ou_ayant_u,
+    critère_n2_réfugié_statutaire_bénéficiaire_d_une_protectio,
+    critère_n2_résident_zrr,
+    critère_n2_résident_qpv,
+    critère_n2_sortant_de_détention_ou_personne_placée_sous_main,
+    critère_n2_maîtrise_de_la_langue_française,
+    critère_n2_mobilité,
+    département,
+    nom_département,
+    région,
+    adresse_en_qpv,
+    total_candidatures,
+    total_embauches,
+    date_diagnostic,
+    id_auteur_diagnostic_employeur,
+    type_auteur_diagnostic,
+    sous_type_auteur_diagnostic,
+    nom_auteur_diagnostic,
+    nom_structure,
+    département_structure,
+    nom_département_structure,
+    région_structure,
+    total_candidats,
+    type_de_candidature,
+    reprise_de_stock_ai_candidats
 from
     autopr_candidates as autopr_c
 left join all_candidates ac
