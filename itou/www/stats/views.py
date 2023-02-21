e1845b346b4350f461c893e42e2c73cfa1d88631
dejafait <dejafait@blackhole.com>
2023-02-21 14:46:33 +0100

    [Stats] Tweak test views about tally popup issues

--- itou/www/stats/views.py
+++ itou/www/stats/views.py
@@ -141,7 +141,7 @@ def stats_public(request):
 def stats_test1(request):
     context = {
         "page_title": "Test 1 : stats_public clone + tally popup + with the usual iframeResizer",
-        "tally_form_id": "waQPkB",
+        "tally_form_id": "mBEr1K",
     }
     return render_stats(request=request, context=context, template_name="stats/stats_test1.html")
 
@@ -149,7 +149,7 @@ def stats_test1(request):
 def stats_test2(request):
     context = {
         "page_title": "Test 2 : stats_public clone + tally popup + *without* the usual iframeResizer",
-        "tally_form_id": "waQPkB",
+        "tally_form_id": "mBEr1K",
     }
     return render_stats(request=request, context=context, template_name="stats/stats_test2.html")
 
