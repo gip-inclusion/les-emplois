7566f76736ae281d13badc5fffcbc52b50ba6774
laurinehu <laurinehu@gmail.com>
2023-02-21 16:41:37 +0100

    metabase: clean some accents + add quotes for accentuated columns

--- itou/metabase/sql/035_suivi_cap.sql
+++ itou/metabase/sql/035_suivi_cap.sql
@@ -2,23 +2,23 @@ with nb_structures_par_dept as (
 -- nb de structures référencées par dept
     select
         département,
-        nom_département,
+        "nom_département",
         count(*) as nb_struct
     from
         structures
     group by
         département,
-        nom_département
+        "nom_département"
 )
 select
     cap_camp.nom,
-    struct.nom_département as nom_département,
-    struct.région,
-    -- récupération du pct de séléction attendu
-    max(cap_camp.pourcentage_sélection) as pct_selection,
+    struct."nom_département" as "nom_département",
+    struct."région",
+    -- récupération du pct de sélection attendu
+    max(cap_camp."pourcentage_sélection") as pct_sélection,
     --nb controlees : ie celles qui ont une date de contrôle non null
     sum(
-        case when cap_struct.date_contrôle is not null then
+        case when cap_struct."date_contrôle" is not null then
             1
         else
             0
@@ -27,7 +27,7 @@ select
     -- ie celles qui ont une date de contrôle non null
     -- sur toutes les SIAE référencées dans la table structure
     CAST(sum(
-            case when cap_struct.date_contrôle is not null then
+            case when cap_struct."date_contrôle" is not null then
                 1
             else
                 0
@@ -37,13 +37,13 @@ select
             1
         else
             0
-        end) as nb_acceptees,
+        end) as nb_acceptées,
     -- nb de structures refusées
     sum(case when cap_struct.état = 'REFUSED' then
             1
         else
             0
-        end) as nb_refusees,
+        end) as nb_refusées,
     -- nb de structures en attente notif
     sum(case when cap_struct.état = 'NOTIFICATION_PENDING' then
             1
@@ -54,9 +54,9 @@ from
     cap_structures cap_struct
     left join structures struct on cap_struct.id_structure = struct.id
     left join cap_campagnes cap_camp on cap_camp.id = cap_struct.id_cap_campagne
-    left join nb_structures_par_dept nb_tot_dep on nb_tot_dep.nom_département = struct.nom_département
+    left join nb_structures_par_dept nb_tot_dep on nb_tot_dep."nom_département" = struct."nom_département"
 group by
     cap_camp.nom,
-    struct.région,
-    struct.nom_département
-order by struct.nom_département;
+    struct."région",
+    struct."nom_département"
+order by struct."nom_département";
