2fc1b9c03621db56c9ac10c3b6ceebc08aae6378
Yannick Passarelli <yannick.passarelli@gmail.com>
2023-02-23 11:19:49 +0100

    metabase : year addition

--- itou/metabase/sql/033_siae_pratiquant_autoprescription.sql
+++ itou/metabase/sql/033_siae_pratiquant_autoprescription.sql
@@ -3,30 +3,38 @@ with siae_autopr as (
         count (
             distinct (id_structure)
         ) as total_siae_autopr,
-        type_structure
+        type_structure,
+        date_part('year',date_diagnostic) as annee
     from
         suivi_auto_prescription sap
     where
         type_de_candidature = 'Autoprescription'
     group by 
-        type_structure
+        type_structure,
+        annee
 ),
 siae_all as (
     select 
         count (
             distinct (id_structure)
         ) as total_siae_all,
-        type_structure
+        type_structure,
+        date_part('year',date_diagnostic) as annee
     from
         suivi_auto_prescription sap
     group by 
-        type_structure
+        type_structure,
+        annee
 )
 select
     total_siae_autopr as "Nombre de structures utilisant l'autoprescription",
     total_siae_all as "Nombre total de structures",
-    sau.type_structure
+    sau.type_structure,
+    sau.annee
 from
     siae_autopr sau
 left join siae_all sall 
-    on sau.type_structure = sall.type_structure
+    on 
+        sau.type_structure = sall.type_structure
+    and 
+        sau.annee = sall.annee
