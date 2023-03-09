9d4c78c5be5f938f4145b55cc1d8e2879f45353b
laurinehu <laurinehu@gmail.com>
2023-03-09 18:39:06 +0100

    metabase: add a new non aggregated table to follow structures by type

---
+++ itou/metabase/sql/039_suivi_cap_structures.sql
@@ -0,0 +1,22 @@
+select
+    id_cap_campagne,
+    cap_campagnes.nom as nom_campagne,
+    cap_structures.id_structure id_cap_structure,
+    structures.id id_structure,
+    structures.type,
+    structures.nom_département as nom_département,
+    structures."région" as région,
+    case when cap_structures. "date_contrôle" is not null then
+        1
+    else
+        0
+    end as "controlee",
+    cap_structures.état
+from
+    "public"."structures" structures
+    left join "public"."cap_structures" cap_structures on structures. "id" = cap_structures. "id_structure"
+    left join "cap_campagnes" cap_campagnes on cap_structures.id_cap_campagne = cap_campagnes.id
+where
+    structures. "active" = 1 
+    --and
+    --cap_structures. "date_contrôle" is not null;
