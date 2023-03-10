15f026528ae0c57d7c7c3f0f723bfbd1f8a3aa00
Yannick Passarelli <yannick.passarelli@gmail.com>
2023-03-10 10:40:45 +0100

    metabase : beuatifying the script

--- itou/metabase/sql/032_metier_candidatures.sql
+++ itou/metabase/sql/032_metier_candidatures.sql
@@ -5,6 +5,9 @@ select
     nom_rome
 from
     candidatures_echelle_locale cel
-    inner join fiches_de_poste_par_candidature fdpc on fdpc.id_candidature = cel.id
-    inner join fiches_de_poste fdp on fdpc.id_fiche_de_poste = fdp.id
-    inner join code_rome_domaine_professionnel crdp on fdp.code_rome = crdp.code_rome
+inner join fiches_de_poste_par_candidature fdpc on
+    fdpc.id_candidature = cel.id
+inner join fiches_de_poste fdp on
+    fdpc.id_fiche_de_poste = fdp.id
+inner join code_rome_domaine_professionnel crdp on
+    fdp.code_rome = crdp.code_rome
