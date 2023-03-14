67f6e4ecfd5127469a9b02027a955ee7bd57900f
laurinehu <laurinehu@gmail.com>
2023-03-14 16:44:32 +0100

    metabase: clean 038 -> brackets everywhere

--- itou/metabase/sql/039_suivi_cap_structures.sql
+++ itou/metabase/sql/039_suivi_cap_structures.sql
@@ -1,13 +1,13 @@
 select
-    id_cap_campagne,
-    cap_campagnes.nom as nom_campagne,
-    cap_structures.id_structure id_cap_structure,
-    structures.id id_structure,
-    structures.type,
-    structures."département" as "département",
-    structures."nom_département" as "nom_département",
-    structures."région" as région,
-    case when structures.active = 1 then
+    "id_cap_campagne",
+    "cap_campagnes"."nom" as nom_campagne,
+    "cap_structures"."id_structure" id_cap_structure,
+    "structures"."id" id_structure,
+    "structures"."type",
+    "structures"."département" as "département",
+    "structures"."nom_département" as "nom_département",
+    "structures"."région" as "région",
+    case when "structures"."active" = 1 then
         'Oui'
     else
         'Non' end as active,
@@ -16,8 +16,8 @@ select
     else
         'Non'
     end as "controlee",
-    cap_structures.état
+    "cap_structures"."état"
 from
     "public"."structures" structures
-    left join "public"."cap_structures" cap_structures on structures. "id" = cap_structures. "id_structure"
-    left join "cap_campagnes" cap_campagnes on cap_structures.id_cap_campagne = cap_campagnes.id;
+    left join "public"."cap_structures" cap_structures on "structures"."id" = "cap_structures"."id_structure"
+    left join "cap_campagnes" cap_campagnes on "cap_structures"."id_cap_campagne" = "cap_campagnes"."id";
