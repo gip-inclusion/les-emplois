7942e5d0d6d4f0a0df82f0ace9b6f70d317e1fc3
laurinehu <laurinehu@gmail.com>
2023-03-14 16:46:23 +0100

    metabase: clean 039 -> brackets everywhere

--- itou/metabase/sql/038_suivi_cap_criteres.sql
+++ itou/metabase/sql/038_suivi_cap_criteres.sql
@@ -3,7 +3,7 @@ select
     "criteres"."id" as "id_critère",
     "criteres"."niveau" as "niveau_critère",
     -- REFUSED et REFUSED_2 correspondent au même état (?) - à confirmer par Zo
-    case when "cap_criteres"."état" = 'REFUSED_2' then 'REFUSED' else "cap_criteres"."état" end as état,
+    case when "cap_criteres"."état" = 'REFUSED_2' then 'REFUSED' else "cap_criteres"."état" end as "état",
     "camp"."nom" as "nom_campagne",
     "structs"."département" as "département",
     "structs"."nom_département" as "nom_département",
@@ -11,8 +11,8 @@ select
     "structs"."type" as "type_structure"
 from
     "cap_critères_iae" cap_criteres
-    left join "critères_iae" criteres on cap_criteres."id_critère_iae" = criteres.id
-    left join "cap_candidatures" candidatures on cap_criteres.id_cap_candidature = candidatures.id
-    left join "cap_structures" cap_structs on candidatures.id_cap_structure = cap_structs.id
-    left join "structures" structs on cap_structs.id_structure = structs.id
-    left join "cap_campagnes" camp on cap_structs.id_cap_campagne = camp.id;
+    left join "critères_iae" criteres on "cap_criteres"."id_critère_iae" = "criteres"."id"
+    left join "cap_candidatures" candidatures on "cap_criteres"."id_cap_candidature" = "candidatures"."id"
+    left join "cap_structures" cap_structs on "candidatures"."id_cap_structure" = "cap_structs"."id"
+    left join "structures" structs on "cap_structs"."id_structure" = "structs"."id"
+    left join "cap_campagnes" camp on "cap_structs"."id_cap_campagne" = "camp"."id";
