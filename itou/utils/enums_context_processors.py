199437ae2c2ddb9c236400c93901419ea1ed57b0
Xavier Fernandez <xavier.fernandez@beta.gouv.fr>
2023-03-23 09:31:43 +0100

    www.apply: remove siae_is_geiq variable
    And use enums_context_processors to get the same result.

--- itou/utils/enums_context_processors.py
+++ itou/utils/enums_context_processors.py
@@ -1,5 +1,6 @@
 import itou.approvals.enums as approvals_enums
 import itou.job_applications.enums as job_applications_enums
+import itou.siaes.enums as siaes_enums
 
 
 def expose_enums(*args):
@@ -13,4 +14,5 @@ def expose_enums(*args):
         "JobApplicationOrigin": job_applications_enums.Origin,
         "SenderKind": job_applications_enums.SenderKind,
         "RefusalReason": job_applications_enums.RefusalReason,
+        "SiaeKind": siaes_enums.SiaeKind,
     }
