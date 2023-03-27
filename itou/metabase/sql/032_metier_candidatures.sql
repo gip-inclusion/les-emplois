9be96069a80cfd480cd9824d24ad1c7170f37d82
laurinehu <laurinehu@gmail.com>
2023-03-27 18:48:03 +0200

    metabase: add candidate genre to metiers_candidatures table

--- itou/metabase/sql/032_metier_candidatures.sql
+++ itou/metabase/sql/032_metier_candidatures.sql
@@ -1,10 +1,12 @@
 select
     cel.*,
+    cand.sexe_selon_nir,
     crdp."grand_domaine" as metier,
     fdp."nom_rome" as nom_rome,
     crdp."code_rome" as code_rome
 from
     candidatures_echelle_locale cel
+    left join candidats cand on candidats.id = cel.id_candidat
     inner join fiches_de_poste_par_candidature fdpc on fdpc.id_candidature = cel.id
     inner join fiches_de_poste fdp on fdpc.id_fiche_de_poste = fdp.id
     inner join code_rome_domaine_professionnel crdp on fdp.code_rome = crdp.code_rome
