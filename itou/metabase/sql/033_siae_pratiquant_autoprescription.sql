52329f21d3ae4f89df3f105adbe9608cc7e204c7
Yannick Passarelli <yannick.passarelli@gmail.com>
2023-04-02 21:21:58 +0200

    metabase : addition of the siae region

--- itou/metabase/sql/033_siae_pratiquant_autoprescription.sql
+++ itou/metabase/sql/033_siae_pratiquant_autoprescription.sql
@@ -4,6 +4,9 @@ with siae_autopr as (
             distinct (id_structure)
         ) as total_siae_autopr,
         type_structure,
+        département_structure,
+        nom_département_structure,
+        région_structure,
         date_part('year',date_diagnostic) as annee_diagnostic
     from
         suivi_auto_prescription sap
@@ -11,6 +14,9 @@ with siae_autopr as (
         type_de_candidature = 'Autoprescription' and active = 1
     group by 
         type_structure,
+        département_structure,
+        nom_département_structure,
+        région_structure,
         annee_diagnostic
 ),
 siae_all as (
@@ -19,6 +25,9 @@ siae_all as (
             distinct (id_structure)
         ) as total_siae_all,
         type_structure,
+        département_structure,
+        nom_département_structure,
+        région_structure,
         date_part('year',date_diagnostic) as annee_diagnostic
     from
         suivi_auto_prescription sap
@@ -26,17 +35,27 @@ siae_all as (
         active = 1
     group by 
         type_structure,
+        département_structure,
+        nom_département_structure,
+        région_structure,
         annee_diagnostic
 )
 select
     total_siae_autopr as "Nombre de structures utilisant l'autoprescription",
     total_siae_all as "Nombre total de structures",
     sau.type_structure,
+    sau.département_structure,
+    sau.nom_département_structure,
+    sau.région_structure,
     sau.annee_diagnostic
 from
-    siae_autopr sau
-left join siae_all sall 
+    siae_all sau
+left join siae_autopr sall 
     on 
         sau.type_structure = sall.type_structure
     and 
         sau.annee_diagnostic = sall.annee_diagnostic
+    and 
+        sau.nom_département_structure = sall.nom_département_structure 
+    and 
+        sau.région_structure = sall.région_structure
