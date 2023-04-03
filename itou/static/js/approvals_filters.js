2abf64cd99b2b1cb8077671bd2b1309bb601f819
hellodeloo <hello@hellodeloo.com>
2023-04-03 12:24:02 +0200

    feat: fix a11y errors

--- itou/static/js/approvals_filters.js
+++ itou/static/js/approvals_filters.js
@@ -1,23 +1,5 @@
-(function() {
-  const filtersToggleBtn = document.getElementById("js-approvals-filters-button");
-  const filtersForm = document.getElementById("js-approvals-filters-form");
-
-  function submitFiltersForm() {
-    filtersForm.submit();
-  }
-  $("#js-approvals-filters-form :input").change(submitFiltersForm);
-
-  function showFilters() {
-    // The filters toggle button is only displayed on small viewports.
-    return window.getComputedStyle(filtersToggleBtn).getPropertyValue("display") === "none";
-  }
-
-  function autoCollapseForm() {
-    filtersForm.classList.toggle("show", showFilters());
-  }
-
-  $(document).ready(function () {
-    autoCollapseForm()
-    window.addEventListener("resize", autoCollapseForm);
-  });
-})();
+function submitFiltersForm() {
+  $("#js-job-applications-filters-form").submit();
+}
+$("#js-job-applications-filters-form :input").change(submitFiltersForm);
+$("#js-job-applications-filters-apply-button").hide();
