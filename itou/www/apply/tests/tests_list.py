gip-inclusion/les-emplois

commit 97d9e7b0f9187a61837e90a9d19f855e9918ca90

apply: Add a commun job description template
Also move Approval.get_state_display to CommonApprovalMixin

itou/www/apply/tests/tests_list.py
@@ -190,6 +190,8 @@ class ProcessListSiaeTest(ProcessListTest):
             + 1  # get list of administrative criteria
             + 3  # get list of job application + prefetch of job descriptions + prefetch of approvals
             + 8  # get PE Approval per user. Can't use prefetch as there is no Foreign Key constraint
+            + 10  # get last valid eligibility diagnosis. Me may make more than one query per job application.
+            # We could try to prefetch, but we would have to skip models methods and it's not a good move
             + 1  # get list of sender org (distinct sender_prescriber_organization_id)
             + 3  # count, list & prefetch of job application
             + 1  # check user membership again

itou/approvals/models.py
@@ -107,6 +107,26 @@ class CommonApprovalMixin(models.Model):
             )
         return result
 
+    @property
+    def is_suspended(self):
+        # Only Approvals may be suspended, but it's required in state property
+        return False
+
+    @property
+    def state(self):
+        if not self.is_valid():
+            return enums.ApprovalStatus.EXPIRED
+        if self.is_suspended:
+            return enums.ApprovalStatus.SUSPENDED
+        if self.is_in_progress:
+            return enums.ApprovalStatus.VALID
+        # When creating an approval, it usually starts in the future.
+        # That's why the default "valid" state is future.
+        return enums.ApprovalStatus.FUTURE
+
+    def get_state_display(self):
+        return self.state.label
+
 
 class CommonApprovalQuerySet(models.QuerySet):
     """
@@ -287,19 +307,6 @@ class Approval(PENotificationMixin, CommonApprovalMixin):
                 f"pour {self.user.get_full_name()} ({self.user.email})."
             )
         super().clean()
-
-    @property
-    def state(self):
-        if not self.is_valid():
-            return enums.ApprovalStatus.EXPIRED
-        if self.is_suspended:
-            return enums.ApprovalStatus.SUSPENDED
-        if self.is_in_progress:
-            return enums.ApprovalStatus.VALID
-        return enums.ApprovalStatus.FUTURE
-
-    def get_state_display(self):
-        return self.state.label
 
     @property
     def number_with_spaces(self):

itou/templates/apply/includes/list_card_body_siae.html
@@ -1,94 +1,8 @@
 {% load str_filters %}
 
 <div class="card-body">
-    <div class="fs-sm d-flex align-items-center mb-3">
-        <span class="text-secondary flex-grow-1">
-            Émise le {{ job_application.created_at|date:"d F Y" }} par
- 
-            {% if request.user == job_application.sender %}
-                <strong>Vous</strong>
-            {% elif job_application.sender_kind == SenderKind.JOB_SEEKER %}
-                <strong>Le candidat lui-même</strong>
-            {% elif job_application.sender_kind == SenderKind.SIAE_STAFF %}
-                <strong class="mr-2">{{ job_application.sender_siae.display_name }}</strong>
-                {% if request.user.is_siae_staff %}
-                    <span class="badge badge-pill badge-sm badge-info">
-                        {% if job_application.origin == JobApplicationOrigin.PE_APPROVAL %}
-                            <i class="ri-hotel-line"></i>&nbsp;Employeur (Import agrément Pôle emploi)
-                        {% else %}
-                            <i class="ri-hotel-line"></i>&nbsp;Employeur (Auto-prescription)
-                        {% endif %}
-                    </span>
-                {% endif %}
-            {% elif job_application.sender_kind == SenderKind.PRESCRIBER %}
-                <strong class="mr-2">{{ job_application.sender_prescriber_organization.display_name }}</strong>
-                {% if job_application.sender_prescriber_organization %}
-                    {% if job_application.is_sent_by_authorized_prescriber %}
-                        <i class="ri-group-line"></i>&nbsp;Prescripteur habilité
-                    {% else %}
-                        <i class="ri-group-line"></i>&nbsp;Orienteur
-                    {% endif %}
-                {% endif %}
-            {% endif %}
 
-        </span>
-        {% include "apply/includes/state_badge.html" with job_application=job_application %}
-    </div>
-    {% with all_jobs=job_application.selected_jobs.all %}
-        <p>
-            {% if all_jobs|length > 1 %}
-                <a class="d-flex flex-wrap align-items-center text-decoration-none has-collapse-caret collapsed"
-                   data-toggle="collapse"
-                   href="#collapseJobs"
-                   role="button"
-                   aria-expanded="false"
-                   aria-controls="collapseExample">
-                {% endif %}
-
-                {% if not job_application.selected_jobs.exists %}
-                    <span class="font-weight-bold mr-1 mr-md-2">Candidature spontanée</span>
-                {% else %}
-                    {% with all_jobs|first as first_job %}
-                        <i class="ri-briefcase-line ri-xl mr-1"></i>
-                        <span class="font-weight-bold mr-1 mr-md-2">{{ first_job.display_name }}</span>
-
-                        {% if first_job.location %}
-                            <span class="fs-sm text-nowrap">
-                                <i class="ri-map-pin-2-line ri-sm mr-1"></i>
-                                {{ first_job.location }}
-                            </span>
-                        {% endif %}
-                    {% endwith %}
-                {% endif %}
-
-                {% if all_jobs|length > 1 %}
-                    {% with other_job_number=all_jobs|length|add:"-1" %}
-                        <span class="ml-auto fs-sm text-decoration-underline">+ {{ other_job_number }} autre{{ other_job_number|pluralize }} poste{{ other_job_number|pluralize }}</span>
-                    {% endwith %}
-                </a>
-            {% endif %}
-        </p>
-
-        {% if all_jobs|length > 1 %}
-            <ul class="list-group list-group-sm list-group-flush collapse" id="collapseJobs">
-                {% for job in all_jobs|slice:"1:" %}
-                    <li class="list-group-item list-group-item-action">
-                        <a class="d-flex flex-wrap align-items-center text-decoration-none" href="">
-                            <span class="font-weight-bold mr-1 mr-md-2">{{ job.display_name }}</span>
-
-                            {% if job.location %}
-                                <span class="ml-auto fs-sm text-nowrap">
-                                    <i class="ri-map-pin-2-line ri-sm mr-1"></i>
-                                    {{ job.location }}
-                                </span>
-
-                            {% endif %}
-                        </a>
-                    </li>
-                {% endfor %}
-            </ul>
-        {% endif %}
-    {% endwith %}
+    {% include "approvals/includes/job_description_list.html" with job_application=job_application %}
 
     <hr />
     <div class="row">
@@ -103,28 +17,24 @@
                 </span>
             {% endif %}
 
-            {% if siae_kind in SiaeWithConventionKinds %}
+            {% if siae.is_subject_to_eligibility_rules %}
                 {% with approval=job_application.job_seeker.latest_common_approval %}
                     {% if approval %}
                         <span class="badge badge-xs badge-pill{% if approval.state == 'EXPIRED' %} badge-emploi-light{% else %} badge-success-lighter{% endif %} text-primary">
                             <i class="{% if approval.state == 'EXPIRED' %}ri-close-circle-line{% elif approval.state == 'SUSPENDED' %}ri-pause-circle-line{% else %}ri-checkbox-circle-line{% endif %}"></i>
-                            PASS IAE {{ approval.get_state_display }}
+                            PASS IAE {{ approval.get_state_display|lower }}
                         </span>
                     {% else %}
-                        {% if job_application.to_siae.is_subject_to_eligibility_rules or request.user.is_prescriber %}
-                            {% if eligibility_diagnosis and eligibility_diagnosis.is_considered_valid %}
-                                {% if job_application.is_sent_by_authorized_prescriber %}
-                                    <span class="badge badge-xs badge-pill badge-success-light text-primary">
-                                        <i class="ri-check-line"></i>
-                                        Éligible à l’IAE
-                                    </span>
-                                {% endif %}
-                            {% else %}
-                                <span class="badge badge-xs badge-pill badge-accent-02-lighter text-primary">
-                                    <i class="ri-error-warning-line"></i>
-                                    Éligibilité à l’IAE à valider
-                                </span>
-                            {% endif %}
+                        {% if job_application.eligibility_diagnosis_by_siae_required %}
+                            <span class="badge badge-xs badge-pill badge-accent-02-lighter text-primary">
+                                <i class="ri-error-warning-line"></i>
+                                Éligibilité à l’IAE à valider
+                            </span>
+                        {% else %}
+                            <span class="badge badge-xs badge-pill badge-success-light text-primary">
+                                <i class="ri-check-line"></i>
+                                Éligible à l’IAE
+                            </span>
                         {% endif %}
                     {% endif %}
 

itou/templates/apply/list_for_job_seeker.html
@@ -67,6 +67,7 @@
             {% else %}
                 {% for job_application in job_applications_page %}
                     <div class="c-card card my-4 has-links-inside">
+                        {# TODO: when working on this template in the near future, try to refactor with list_card_body_siae.html #}
                         {% include "apply/includes/list_card_header.html" with job_application=job_application %}
                         {% include "apply/includes/list_card_body.html" with job_application=job_application %}
                         <div id="answer{{ forloop.counter0 }}" class="card-body collapse">

itou/templates/apply/list_for_prescriber.html
@@ -85,6 +85,7 @@
             {% else %}
                 {% for job_application in job_applications_page %}
                     <div class="c-card card my-4 has-links-inside">
+                        {# TODO: when working on this template in the near future, try to refactor with list_card_body_siae.html #}
                         {% include "apply/includes/list_card_header.html" with job_application=job_application %}
                         {% include "apply/includes/list_card_body.html" with job_application=job_application %}
                         <div class="card-footer text-right">

itou/templates/apply/list_for_siae.html
@@ -91,9 +91,9 @@
             {% else %}
                 {% for job_application in job_applications_page %}
                     <div class="c-card card my-4 has-links-inside">
-                        {% include "apply/includes/list_card_body_siae.html" with job_application=job_application siae_kind=siae.kind %}
-                        <div class="card-footer text-right">
-                            <a class="btn btn-sm btn-outline-primary"
+                        {% include "apply/includes/list_card_body_siae.html" %}
+                        <div class="card-footer text-right bg-light">
+                            <a class="btn btn-sm btn-outline-primary bg-white"
                                href="{% url 'apply:details_for_siae' job_application_id=job_application.id %}?back_url={{ request.get_full_path|urlencode }}"
                                aria-label="Gérer la candidature de {{ job_application.job_seeker.get_full_name|mask_unless:job_application.user_can_view_personal_information }}">
                                 Voir sa candidature

itou/templates/approvals/includes/job_applications.html
@@ -1,60 +1,6 @@
 <div class="card c-card has-links-inside my-4">
     <div class="card-body">
-        <div class="fs-sm d-flex align-items-center">
-            <span class="text-secondary flex-grow-1">
-                Émise le {{ job_application.created_at|date }} par <strong>{{ job_application.sender.get_full_name|title }}</strong>
-                <i class="ri-group-line ml-2" aria-hidden="true"></i> {{ job_application.get_sender_kind_display }}
-            </span>
-            {% include "apply/includes/state_badge.html" with job_application=job_application %}
-        </div>
-        <hr>
-        {% with selected_jobs_count=job_application.selected_jobs.all|length %}
-            {% if selected_jobs_count > 0 %}
-                {% for job in job_application.selected_jobs.all %}
-                    {% if forloop.first %}
-                        <p class="mx-2">
-                            {% if selected_jobs_count > 1 %}
-                                <a class="d-flex flex-wrap align-items-center text-decoration-none has-collapse-caret"
-                                   data-toggle="collapse"
-                                   href="#collapse-job-application-{{ job_application.id }}"
-                                   role="button"
-                                   aria-expanded="false"
-                                   aria-controls="collapse-job-application-{{ job_application.id }}">
-                                {% endif %}
-                                <i class="ri-briefcase-line ri-xl mr-1"></i>
-                                <span class="font-weight-bold mr-1 mr-md-2">{{ job }}</span>
-                                <span class="fs-sm text-nowrap">
-                                    <i class="ri-map-pin-2-line ri-sm mr-1"></i>
-                                    {{ job.display_location }}
-                                </span>
-                                {% if selected_jobs_count > 1 %}
-                                    <span class="ml-auto fs-sm text-decoration-underline">
-                                        + {{ selected_jobs_count|add:"-1" }} autre{{ selected_jobs_count|add:"-1"|pluralize }} poste{{ selected_jobs_count|add:"-1"|pluralize }}
-                                    </span>
-                                </a>
-                            {% endif %}
-                        </p>
-                    {% else %}
-                        <ul class="list-group list-group-sm list-group-flush collapse" id="collapse-job-application-{{ job_application.id }}">
-                            <li class="list-group-item list-group-item-action">
-                                <span class="d-flex flex-wrap align-items-center text-decoration-none">
-                                    <span class="font-weight-bold mr-1 mr-md-2">{{ job }}</span>
-                                    <span class="ml-auto fs-sm text-nowrap">
-                                        <i class="ri-map-pin-2-line ri-sm mr-1"></i>
-                                        {{ job.display_location }}
-                                    </span>
-                                </span>
-                            </li>
-                        </ul>
-                    {% endif %}
-                {% endfor %}
-            {% else %}
-                <p class="mx-2">
-                    <i class="ri-briefcase-line ri-xl mr-1"></i>
-                    <span class="font-weight-bold mr-1 mr-md-2">Candidature spontanée</span>
-                </p>
-            {% endif %}
-        {% endwith %}
+        {% include "approvals/includes/job_description_list.html" with job_application=job_application %}
     </div>
     <div class="card-footer text-right bg-light">
         <a href="{% url 'apply:details_for_siae' job_application_id=job_application.id %}"

itou/templates/approvals/includes/job_description_list.html
@@ -1,0 +1,84 @@
+<div class="fs-sm d-flex flex-column flex-md-row align-items-center mb-3">
+    <span class="text-secondary order-2 order-md-1 mr-auto">
+        Émise le {{ job_application.created_at|date:"d F Y" }} par
+
+        {% if request.user == job_application.sender %}
+            <strong>Vous</strong>
+        {% elif job_application.sender_kind == SenderKind.JOB_SEEKER %}
+            <i class="ri-user-line mr-2" aria-hidden="true"></i><strong>Le candidat lui-même</strong>
+        {% elif job_application.sender_kind == SenderKind.SIAE_STAFF %}
+            <strong class="mr-2">{{ job_application.sender_siae.display_name }}</strong>
+            {% if request.user.is_siae_staff %}
+                {% if job_application.origin == JobApplicationOrigin.PE_APPROVAL %}
+                    <i class="ri-hotel-line mr-2" aria-hidden="true"></i>Employeur (Import agrément Pôle emploi)
+                {% else %}
+                    <i class="ri-hotel-line mr-2" aria-hidden="true"></i>Employeur (Auto-prescription)
+                {% endif %}
+            {% endif %}
+        {% elif job_application.sender_kind == SenderKind.PRESCRIBER %}
+            {% if job_application.sender_prescriber_organization %}
+                <strong class="mr-2">{{ job_application.sender_prescriber_organization.display_name }}</strong>
+            {% else %}
+                <strong class="mr-2">{{ job_application.sender.get_full_name }}</strong>
+            {% endif %}
+            {% if job_application.is_sent_by_authorized_prescriber %}
+                <i class="ri-group-line mr-2" aria-hidden="true"></i>Prescripteur habilité
+            {% else %}
+                <i class="ri-group-line mr-2" aria-hidden="true"></i>Orienteur
+            {% endif %}
+        {% endif %}
+
+    </span>
+    {# élément à passer au dessus du précédent en vue mobile #}
+    <span class="order-1 order-md-2 ml-auto">
+        {% include "apply/includes/state_badge.html" with job_application=job_application %}
+    </span>
+</div>
+
+{% with all_jobs=job_application.selected_jobs.all %}
+    <p>
+        {% if all_jobs|length == 0 %}
+            <i class="ri-briefcase-line ri-xl mr-1"></i><span class="font-weight-bold mr-1 mr-md-2">Candidature spontanée</span>
+        {% elif all_jobs|length == 1 %}
+            {% with job=all_jobs.0 %}
+                <div class="text-primary">
+                    <i class="ri-briefcase-line ri-xl mr-1"></i>
+                    <span class="font-weight-bold mr-1 mr-md-2">{{ job.display_name }}</span>
+                    {% if job.location %}
+                        <span class="fs-sm text-nowrap">
+                            <i class="ri-map-pin-2-line ri-sm mr-1"></i>
+                            {{ first_job.location }}
+                        </span>
+                    {% endif %}
+                </div>
+            {% endwith %}
+        {% else %}
+            <a class="d-flex flex-wrap align-items-center text-decoration-none has-collapse-caret collapsed"
+               data-toggle="collapse"
+               href="#collapse-job-application-{{ job_application.id }}"
+               role="button"
+               aria-expanded="false"
+               aria-controls="collapse-job-application-{{ job_application.id }}">
+                <i class="ri-briefcase-line ri-xl mr-1"></i>
+                <span class="font-weight-bold mr-1 mr-md-2">Voir les {{ all_jobs|length }} postes</span>
+            </a>
+            <ul class="list-group list-group-sm list-group-flush collapse" id="collapse-job-application-{{ job_application.id }}">
+                {% for job in all_jobs %}
+                    <li class="list-group-item list-group-item-action">
+                        <span class="d-flex flex-wrap align-items-center text-decoration-none">
+                            <span class="font-weight-bold mr-1 mr-md-2">{{ job.display_name }}</span>
+
+                            {% if job.location %}
+                                <span class="ml-auto fs-sm text-nowrap">
+                                    <i class="ri-map-pin-2-line ri-sm mr-1"></i>
+                                    {{ job.location }}
+                                </span>
+
+                            {% endif %}
+                        </span>
+                    </li>
+                {% endfor %}
+            </ul>
+        {% endif %}
+    </p>
+{% endwith %}

commit bf1ca44e79a7b8152342c69b0dd65f7c15ba1d07

Fix 1+N queries for cities in job applications list view for SIAE
Regression introduced in 97d9e7b0f9187a61837e90a9d19f855e9918ca90.

itou/job_applications/models.py
@@ -242,6 +242,7 @@ class JobApplicationQuerySet(models.QuerySet):
             "to_siae__convention",
         ).prefetch_related(
             "selected_jobs__appellation",
+            "selected_jobs__location",
             Prefetch("job_seeker__approvals", queryset=Approval.objects.order_by("-start_at")),
         )
 

itou/www/apply/tests/tests_list.py
@@ -4,6 +4,7 @@ from django.utils.http import urlencode
 from pytest_django.asserts import assertContains, assertNumQueries
 
 from itou.approvals.factories import SuspensionFactory
+from itou.cities.factories import create_city_saint_andre
 from itou.eligibility.enums import AdministrativeCriteriaLevel
 from itou.eligibility.factories import EligibilityDiagnosisFactory
 from itou.eligibility.models import AdministrativeCriteria
@@ -13,11 +14,11 @@ from itou.job_applications.factories import (
     JobApplicationSentByJobSeekerFactory,
     JobApplicationSentByPrescriberFactory,
 )
-from itou.job_applications.models import JobApplicationWorkflow
+from itou.job_applications.models import JobApplication, JobApplicationWorkflow
 from itou.jobs.factories import create_test_romes_and_appellations
 from itou.jobs.models import Appellation
 from itou.prescribers.factories import PrescriberMembershipFactory, PrescriberOrganizationWithMembershipFactory
-from itou.siaes.factories import SiaeFactory
+from itou.siaes.factories import SiaeFactory, SiaeJobDescriptionFactory
 from itou.users.factories import PrescriberFactory
 from itou.utils.test import TestCase, format_html
 from itou.utils.widgets import DuetDatePickerWidget
@@ -180,21 +181,46 @@ class ProcessListSiaeTest(ProcessListTest):
         """
         Eddie wants to see a list of job applications sent to his SIAE.
         """
+        city = create_city_saint_andre()
+        create_test_romes_and_appellations(["N4105"], appellations_per_rome=2)
+        appellations = Appellation.objects.all()[:2]
+        job1 = SiaeJobDescriptionFactory(siae=self.hit_pit, appellation=appellations[0], location=city)
+        job2 = SiaeJobDescriptionFactory(siae=self.hit_pit, appellation=appellations[1], location=city)
+        for job_application in JobApplication.objects.all():
+            job_application.selected_jobs.set([job1, job2])
         self.client.force_login(self.eddie_hit_pit)
         with self.assertNumQueries(
             1  # fetch django session
             + 1  # fetch user
             + 3  # check for membership & infos
+            #
+            # SiaeFilterJobApplicationsForm:
             + 1  # get list of senders (distinct sender_id)
             + 1  # get list of job seekers (distinct job_seeker_id)
             + 1  # get list of administrative criteria
-            + 3  # get list of job application + prefetch of job descriptions + prefetch of approvals
-            + 8  # get PE Approval per user. Can't use prefetch as there is no Foreign Key constraint
-            + 16  # get last valid eligibility diagnosis. Me may make more than one query per job application.
-            # We could try to prefetch, but we would have to skip models methods and it's not a good move
-            + 1  # get list of sender org (distinct sender_prescriber_organization_id)
-            + 3  # count, list & prefetch of job application
-            + 1  # check user membership again
+            + 1  # get list of job application
+            + 1  # prefetch selected jobs
+            + 1  # prefetch jobs appellation
+            + 1  # select distinct sender_prescriber_organization
+            #
+            # Paginate the job applications queryset:
+            + 1  # has_suspended_approval subquery
+            + 1  # select job applications with annotations
+            + 1  # prefetch selected jobs
+            + 1  # prefetch jobs appellation
+            + 1  # prefetch jobs location
+            + 1  # prefetch approvals
+            #
+            # Render template:
+            + 1  # context processor: user siae membership
+            # 6 job applications
+            + 6
+            * (
+                1  # select poleemploiapproval (latest_pe_approval)
+                + 1  # select latest eligibility diagnosis (job_application.eligibility_diagnosis_by_siae_required)
+                + 1  # select last valid diagnosis made by prescriber (does not exist)
+                + 1  # select last valid diagnosis made by siae (exists)
+            )
             + 3  # update session
         ):
             response = self.client.get(self.siae_base_url)