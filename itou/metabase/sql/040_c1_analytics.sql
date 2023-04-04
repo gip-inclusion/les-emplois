6578f71738bc90579935b01084053f2c4a94cb68
Yannick Passarelli <yannick.passarelli@gmail.com>
2023-04-04 11:40:27 +0200

    metabase : forgot to remove some quotation marks

--- itou/metabase/sql/040_c1_analytics.sql
+++ itou/metabase/sql/040_c1_analytics.sql
@@ -5,12 +5,12 @@ select
     lag(cav.value) over (
         partition by cav.type_detail
         order by
-            cav."date"
+            cav.date
     ) as prev_value,
     cav.value - lag(cav.value) over (
         partition by cav.type_detail
         order by
-            cav."date"
+            cav.date
     ) as delta
 from
     c1_analytics_v0 as cav
