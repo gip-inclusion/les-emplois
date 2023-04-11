28406045978ee72a1f874bff6a3dbcf42eddbf4a
François Freitag <mail@franek.fr>
2023-04-11 12:11:36 +0200

    Fix ApprovalAdmin display of inline job apps
    A job application linked to a job seeker with an approval does not
    necessarily have a `hiring_start_date`.

---
+++ itou/approvals/tests/test_admin.py
@@ -0,0 +1,16 @@
+from django.urls import reverse
+
+from itou.approvals.factories import ApprovalFactory
+from itou.job_applications.factories import JobApplicationFactory
+from itou.users.factories import ItouStaffFactory
+
+
+class TestApprovalAdmin:
+    def test_change_approval_with_jobapp_no_hiring_dates(self, client):
+        approval = ApprovalFactory(with_jobapplication=True)
+        approval.jobapplication_set.add(
+            JobApplicationFactory(hiring_start_at=None, hiring_end_at=None, job_seeker=approval.user)
+        )
+        client.force_login(ItouStaffFactory(is_superuser=True))
+        response = client.get(reverse("admin:approvals_approval_change", kwargs={"object_id": approval.pk}))
+        assert response.status_code == 200
