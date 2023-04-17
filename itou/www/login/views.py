46095842c9ff9ff6c6add8c28b780464e009154f
Antoine LAURENT <antoine.c.laurent.pro@gmail.com>
2023-04-17 08:54:58 +0200

    login: Refactor inclusion connect url generation

--- itou/www/login/views.py
+++ itou/www/login/views.py
@@ -17,9 +17,26 @@ class ItouLoginView(LoginView):
     """
 
     form_class = ItouLoginForm
+    user_kind = None
 
     # Allow users to choose their account type
     template_name = "account/account_type_selection.html"
+
+    def _get_inclusion_connect_url(self, context):
+        if not settings.INCLUSION_CONNECT_BASE_URL:
+            return None
+
+        if self.user_kind in [UserKind.LABOR_INSPECTOR, UserKind.JOB_SEEKER]:
+            return None
+
+        params = {
+            "user_kind": self.user_kind,
+            "previous_url": self.request.get_full_path(),
+        }
+        if context["redirect_field_value"]:
+            params["next_url"] = context["redirect_field_value"]
+
+        return add_url_params(reverse("inclusion_connect:authorize"), params)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -31,28 +48,17 @@ class ItouLoginView(LoginView):
             "signup_allowed": True,
             "redirect_field_name": REDIRECT_FIELD_NAME,
             "redirect_field_value": get_safe_url(self.request, REDIRECT_FIELD_NAME),
+            "inclusion_connect_url": self._get_inclusion_connect_url(context),
         }
         return context | extra_context
 
 
 class PrescriberLoginView(ItouLoginView):
     template_name = "account/login_generic.html"
+    user_kind = UserKind.PRESCRIBER
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
-        params = {
-            "user_kind": UserKind.PRESCRIBER,
-            "previous_url": self.request.get_full_path(),
-        }
-        if context["redirect_field_value"]:
-            params["next_url"] = context["redirect_field_value"]
-
-        inclusion_connect_url = (
-            add_url_params(reverse("inclusion_connect:authorize"), params)
-            if settings.INCLUSION_CONNECT_BASE_URL
-            else None
-        )
 
         extra_context = {
             "account_type_display_name": "prescripteur",
@@ -61,29 +67,16 @@ class PrescriberLoginView(ItouLoginView):
             "signup_url": reverse("signup:prescriber_check_already_exists"),
             "signup_allowed": True,
             "uses_inclusion_connect": True,
-            "inclusion_connect_url": inclusion_connect_url,
         }
         return context | extra_context
 
 
 class SiaeStaffLoginView(ItouLoginView):
     template_name = "account/login_generic.html"
+    user_kind = UserKind.SIAE_STAFF
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
-        params = {
-            "user_kind": UserKind.SIAE_STAFF,
-            "previous_url": self.request.get_full_path(),
-        }
-        if context["redirect_field_value"]:
-            params["next_url"] = context["redirect_field_value"]
-
-        inclusion_connect_url = (
-            add_url_params(reverse("inclusion_connect:authorize"), params)
-            if settings.INCLUSION_CONNECT_BASE_URL
-            else None
-        )
 
         extra_context = {
             "account_type_display_name": "employeur solidaire",
@@ -92,13 +85,13 @@ class SiaeStaffLoginView(ItouLoginView):
             "signup_url": reverse("signup:siae_select"),
             "signup_allowed": True,
             "uses_inclusion_connect": True,
-            "inclusion_connect_url": inclusion_connect_url,
         }
         return context | extra_context
 
 
 class LaborInspectorLoginView(ItouLoginView):
     template_name = "account/login_generic.html"
+    user_kind = UserKind.LABOR_INSPECTOR
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -112,6 +105,7 @@ class LaborInspectorLoginView(ItouLoginView):
 
 class JobSeekerLoginView(ItouLoginView):
     template_name = "account/login_job_seeker.html"
+    user_kind = UserKind.JOB_SEEKER
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
