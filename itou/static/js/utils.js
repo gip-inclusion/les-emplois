f6fe92d432cac0c64600c7d61fb42c1ea3ae2926
François Freitag <mail@franek.fr>
2023-05-04 17:52:08 +0200

    Remove disabling of submit button based on form state
    This feature is mostly unnecessary (missing required fields get flagged
    by the browser). With the city select, there’s a missing change event
    causing the button to remain disabled until blur.

--- itou/static/js/utils.js
+++ itou/static/js/utils.js
@@ -5,30 +5,6 @@ htmx.onLoad((target) => {
 
     $(".js-display-if-javascript-enabled", target).css("display", "block");
 
-    /**
-     * JS to disable the submit button of the form when it's not valid
-     **/
-    function checkValidity(e) {
-        let submit = $(e.currentTarget).find('button[type="submit"]')
-        if (e.currentTarget.checkValidity()) {
-            submit.removeClass("disabled")
-        } else {
-            submit.addClass("disabled")
-        }
-    }
-
-    $(".js-enable-submit-when-valid", target).each(function () {
-        let form = $(this)
-        // Check the validity when something (possibly) change in the form.
-        form.on("change reset duetChange", (e) => {
-          // Use setTimeout() to execute checkValidity() in the next event cycle,
-          // when events *should* have done what they need to do.
-          window.setTimeout(checkValidity, 0, e)
-        })
-        // Check immediately
-        checkValidity({"currentTarget": this})
-    });
-
   /**
    * JS to swap elements based on CSS selectors
    */
