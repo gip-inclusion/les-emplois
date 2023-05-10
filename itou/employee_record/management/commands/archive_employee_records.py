e67d0a7f7f1a29f7ca623e9d99d7cf035241f3ab
Romain Sébille <romain.sebille@beta.gouv.fr>
2023-05-10 19:04:19 +0200

    employee_record.archive: Stop using `bulk_update()` to avoid blockage
    We don't expect a lot of objects to be archived with the recent changes
    and saving in bulk can lead to one employee record blocking the
    archiving of all the other ones.

--- itou/employee_record/management/commands/archive_employee_records.py
+++ itou/employee_record/management/commands/archive_employee_records.py
@@ -22,28 +22,14 @@ class Command(BaseCommand):
         self.stdout.write(f"Found {len(archivable)} archivable employee record(s)")
 
         archived_employee_records = []
-        # A bulk update will increase performance if there are a lot of employee records to update.
-        # However, if there is no performance issue, it is preferable to keep the archiving
-        # and validation logic in the model (update_as_archived).
-        # Update: let's bulk, with a batch size of 100 records
         for employee_record in archivable:
-            try:
-                # Do not trigger a save() call on the object
-                self.stdout.write(f"Archiving {employee_record.pk=}")
-                employee_record.update_as_archived(save=False)
-            except Exception as ex:
-                self.stdout.write(f"Can't archive {employee_record.pk=} {ex=}")
-            else:
-                archived_employee_records.append(employee_record)
+            self.stdout.write(f"Archiving {employee_record.pk=}")
+            if wet_run:
+                try:
+                    employee_record.update_as_archived()
+                except Exception as ex:
+                    self.stdout.write(f"Can't archive {employee_record.pk=} {ex=}")
+                else:
+                    archived_employee_records.append(employee_record)
 
-        self.stdout.write(f"{len(archived_employee_records)}/{len(archivable)} employee record(s) can be archived")
-
-        if wet_run:
-            updated = EmployeeRecord.objects.bulk_update(
-                archived_employee_records,
-                ["status", "updated_at", "archived_json"],
-                batch_size=100,
-            )
-            self.stdout.write(
-                f"{updated}/{len(archived_employee_records)}/{len(archivable)} employee record(s) were archived"
-            )
+        self.stdout.write(f"{len(archived_employee_records)}/{len(archivable)} employee record(s) were archived")
