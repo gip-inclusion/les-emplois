e23c3b165d180af09dfd0c2daf25a31e95ddc011
dejafait <dejafait@blackhole.com>
2023-05-12 17:48:59 +0200

    institutions: Add new kind `iae_network`

--- itou/institutions/enums.py
+++ itou/institutions/enums.py
@@ -6,4 +6,5 @@ class InstitutionKind(models.TextChoices):
     DREETS = ("DREETS", "Direction régionale de l'économie, de l'emploi, du travail et des solidarités")
     DGEFP = ("DGEFP", "Délégation générale à l'emploi et à la formation professionnelle")
     DIHAL = ("DIHAL", "Délégation interministérielle à l'hébergement et à l'accès au logement")
+    IAE_NETWORK = ("Réseau IAE", "Réseau IAE (Coorace, Emmaüs...)")
     OTHER = ("Autre", "Autre")
