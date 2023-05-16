a2fa8ffffbec6b3bb4ad613d003141a7f13d8cf6
Romain Sébille <romain.sebille@beta.gouv.fr>
2023-05-16 18:39:17 +0200

    itou: Use Django's messages to display toasts

--- itou/utils/templatetags/theme_inclusion.py
+++ itou/utils/templatetags/theme_inclusion.py
@@ -1,4 +1,5 @@
 from django import template
+from django.contrib.messages import constants as message_constants
 from django.templatetags.static import static
 
 
@@ -25,3 +26,26 @@ def static_theme_images(url_path):
         {% static_theme_images url_path %}
     """
     return static(f"{URL_THEME}images/{url_path}")
+
+
+TOAST_LEVEL_CLASSES = {
+    message_constants.INFO: "toast--info",
+    message_constants.SUCCESS: "toast--success",
+    message_constants.WARNING: "toast--warning",
+    message_constants.ERROR: "toast--danger",
+}
+
+
+@register.filter
+def itou_toast_classes(message):
+    return TOAST_LEVEL_CLASSES.get(message.level, "")
+
+
+@register.filter
+def itou_toast_title(message):
+    return message.message.split("||", maxsplit=1)[0]
+
+
+@register.filter
+def itou_toast_content(message):
+    return message.message.split("||", maxsplit=1)[1]
