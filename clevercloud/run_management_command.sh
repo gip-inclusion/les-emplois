5edc6d3db530550581eabcb8cc6d4f3082b5b787
Romain Sébille <romain.sebille@beta.gouv.fr>
2023-05-24 15:57:55 +0200

    crons: Deduplicate `run_management_command`

--- clevercloud/run_management_command.sh
+++ clevercloud/run_management_command.sh
@@ -7,5 +7,12 @@ if [[ "$CRON_ENABLED" != "1" ]]; then
     exit 0
 fi
 
+# About clever cloud cronjobs:
+# https://www.clever-cloud.com/doc/administrate/cron/#deduplicating-crons
+if [[ "$INSTANCE_NUMBER" != "0" ]]; then
+    echo "Instance number is ${INSTANCE_NUMBER}. Stop here."
+    exit 0
+fi
+
 cd "$APP_HOME"
 django-admin "$@"
