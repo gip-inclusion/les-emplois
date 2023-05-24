0b633b9100b89de3fb9210e9b6cbba285287e137
Romain Sébille <romain.sebille@beta.gouv.fr>
2023-05-24 15:55:27 +0200

    crons: Deduplicate `expire-elastic-indices`

--- clevercloud/expire-elastic-indices.sh
+++ clevercloud/expire-elastic-indices.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/bash -l
 
+if [[ "$CRON_ENABLED" != "1" ]]; then
+    echo "Crons not enabled."
+    exit 0
+fi
+
+# About clever cloud cronjobs:
+# https://www.clever-cloud.com/doc/administrate/cron/#deduplicating-crons
+if [[ "$INSTANCE_NUMBER" != "0" ]]; then
+    echo "Instance number is ${INSTANCE_NUMBER}. Stop here."
+    exit 0
+fi
+
 cd "${APP_HOME}" || exit
 scripts/expire-elastic-indices
