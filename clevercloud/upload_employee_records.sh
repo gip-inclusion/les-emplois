170f14b2dde6b3954125487547fdcac98fdfb060
Romain Sébille <romain.sebille@beta.gouv.fr>
2023-05-24 15:59:05 +0200

    crons: Update the link to clever cloud's CRON documentation

--- clevercloud/upload_employee_records.sh
+++ clevercloud/upload_employee_records.sh
@@ -8,8 +8,7 @@ if [[ "$EMPLOYEE_RECORD_CRON_ENABLED" != "1" ]]; then
 fi
 
 # About clever cloud cronjobs:
-# https://www.clever-cloud.com/doc/tools/crons/
-
+# https://www.clever-cloud.com/doc/administrate/cron/#deduplicating-crons
 if [[ "$INSTANCE_NUMBER" != "0" ]]; then
     echo "Instance number is ${INSTANCE_NUMBER}. Stop here."
     exit 0
