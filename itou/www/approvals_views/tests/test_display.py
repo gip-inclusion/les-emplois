8516b85ff548fd1d2e838f6d1374f476f5692ab9
Antoine LAURENT <antoine.c.laurent.pro@gmail.com>
2023-05-25 10:41:57 +0200

    approvals_views: Remove old checks in approval display
    All "admin" and "default" approvals have a diagnosis now.

--- itou/www/approvals_views/tests/test_display.py
+++ itou/www/approvals_views/tests/test_display.py
@@ -1,11 +1,9 @@
 from unittest.mock import PropertyMock, patch
 
-import pytest
 from dateutil.relativedelta import relativedelta
 from django.urls import reverse
 from freezegun import freeze_time
 
-from itou.approvals.enums import Origin
 from itou.approvals.factories import ApprovalFactory
 from itou.job_applications.factories import JobApplicationFactory
 from itou.job_applications.models import JobApplication
@@ -59,7 +57,7 @@ class TestDisplayApproval(TestCase):
         self.assertContains(response, "Imprimer ce PASS IAE")
         self.assertContains(response, "Astuce pour conserver cette attestation en format PDF")
 
-    def test_dont_display_approval_with_no_eligibility_diagnosis_and_no_job_applications(self, *args, **kwargs):
+    def test_display_approval_with_no_eligibility_diagnosis_and_no_job_applications(self, *args, **kwargs):
         approval = ApprovalFactory(eligibility_diagnosis=None)
         siae_member = SiaeMembershipFactory().user
         self.client.force_login(siae_member)
@@ -67,8 +65,7 @@ class TestDisplayApproval(TestCase):
         response = self.client.get(
             reverse("approvals:display_printable_approval", kwargs={"approval_id": approval.pk})
         )
-
-        assert response.status_code == 404
+        assert response.status_code == 200
 
     def test_display_approval_even_if_diagnosis_is_missing(self, *args, **kwargs):
         # An approval has been delivered but it does not come from Itou.
@@ -89,62 +86,3 @@ class TestDisplayApproval(TestCase):
         self.assertContains(response, global_constants.ITOU_ASSISTANCE_URL)
         self.assertContains(response, "Imprimer ce PASS IAE")
         self.assertContains(response, "Astuce pour conserver cette attestation en format PDF")
-
-    def test_display_approval_missing_diagnosis_ai_job_application(self, *args, **kwargs):
-        # TODO(alaurent) remove once last approvals were manually fixed
-        # On November 30th, 2021, AI were delivered approvals without a diagnosis.
-        job_application = JobApplicationFactory(
-            with_approval=True,
-            eligibility_diagnosis=None,
-            origin=Origin.AI_STOCK,
-        )
-
-        siae_member = job_application.to_siae.members.first()
-        self.client.force_login(siae_member)
-
-        response = self.client.get(
-            reverse("approvals:display_printable_approval", kwargs={"approval_id": job_application.approval_id})
-        )
-
-        assert response.context["approval"] == job_application.approval
-        assert response.context["siae"] == job_application.to_siae
-        self.assertContains(response, global_constants.ITOU_ASSISTANCE_URL)
-        self.assertContains(response, "Imprimer ce PASS IAE")
-        self.assertContains(response, "Astuce pour conserver cette attestation en format PDF")
-
-    def test_display_approval_missing_diagnosis_ai_approval(self, *args, **kwargs):
-        # TODO(alaurent) remove once last approvals were manually fixed
-        # On November 30th, 2021, AI were delivered approvals without a diagnosis.
-        job_application = JobApplicationFactory(
-            with_approval=True,
-            eligibility_diagnosis=None,
-            approval__origin=Origin.AI_STOCK,
-        )
-
-        siae_member = job_application.to_siae.members.first()
-        self.client.force_login(siae_member)
-
-        response = self.client.get(
-            reverse("approvals:display_printable_approval", kwargs={"approval_id": job_application.approval_id})
-        )
-
-        assert response.context["approval"] == job_application.approval
-        assert response.context["siae"] == job_application.to_siae
-        self.assertContains(response, global_constants.ITOU_ASSISTANCE_URL)
-        self.assertContains(response, "Imprimer ce PASS IAE")
-        self.assertContains(response, "Astuce pour conserver cette attestation en format PDF")
-
-    def test_no_display_if_missing_diagnosis(self, *args, **kwargs):
-        """
-        Given an existing job application with an approval delivered by Itou but no
-        diagnosis, when trying to display it as printable, then it raises an error.
-        """
-        job_application = JobApplicationFactory(with_approval=True, eligibility_diagnosis=None)
-
-        siae_member = job_application.to_siae.members.first()
-        self.client.force_login(siae_member)
-
-        with pytest.raises(Exception, match="had no eligibility diagnosis and also was not mass-imported"):
-            self.client.get(
-                reverse("approvals:display_printable_approval", kwargs={"approval_id": job_application.approval_id})
-            )
