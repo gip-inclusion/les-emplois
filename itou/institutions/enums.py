43d3eb1961a229306a67a87eaebe8b22f4943b07
dejafait <dejafait@blackhole.com>
2023-06-08 16:41:54 +0200

    institutions: Add and rename DDETS/DREETS kinds

--- itou/institutions/enums.py
+++ itou/institutions/enums.py
@@ -2,9 +2,22 @@ from django.db import models
 
 
 class InstitutionKind(models.TextChoices):
-    DDETS = ("DDETS", "Direction départementale de l'emploi, du travail et des solidarités")
-    DREETS = ("DREETS", "Direction régionale de l'économie, de l'emploi, du travail et des solidarités")
+    # DDETS_GEIQ coming soon.
+    DDETS_IAE = (
+        "DDETS IAE",
+        "Direction départementale de l'emploi, du travail et des solidarités, division IAE",
+    )
+    DDETS_LOG = (
+        "DDETS LOG",
+        "Direction départementale de l'emploi, du travail et des solidarités, division logement insertion",
+    )
+
+    # DREETS_GEIQ coming soon.
+    DREETS_IAE = (
+        "DREETS IAE",
+        "Direction régionale de l'économie, de l'emploi, du travail et des solidarités, division IAE",
+    )
     DGEFP = ("DGEFP", "Délégation générale à l'emploi et à la formation professionnelle")
     DIHAL = ("DIHAL", "Délégation interministérielle à l'hébergement et à l'accès au logement")
-    IAE_NETWORK = ("Réseau IAE", "Réseau IAE (Coorace, Emmaüs...)")
+    IAE_NETWORK = ("Réseau IAE", "Réseau employeur de l'insertion par l'activité économique")
     OTHER = ("Autre", "Autre")
