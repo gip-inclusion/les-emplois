ac6f383274196b3a7cc8ab2051a55274b573a3cc
Xavier Fernandez <xavier.fernandez@beta.gouv.fr>
2023-06-20 16:14:52 +0200

    metabase: remove useless workaround
    It was introduced in b9026508a599a7925c to make the change seamless
    for the Pilotage.
    It turns out they were already performing the reverse change to convert
    NOTIFICATION_PENDING to REFUSED state.
    
    Let's make it simpler for everyone.

--- itou/metabase/tables/evaluated_siaes.py
+++ itou/metabase/tables/evaluated_siaes.py
@@ -1,20 +1,9 @@
 from itou.metabase.tables.utils import MetabaseTable, get_column_from_field
-from itou.siae_evaluations.enums import EvaluatedSiaeState
 from itou.siae_evaluations.models import EvaluatedSiae
 
 
 def get_field(name):
     return EvaluatedSiae._meta.get_field(name)
-
-
-def get_state(evaluated_siae):
-    # For backward compatibility, continue to output NOTIFICATION_PENDING state
-    # until the dashboards could be adapted (we might want to also output the notified_at column)
-    # TODO(xfernandez): return to lambda o:o.state once it isn't necessary anymore
-    state = evaluated_siae.state
-    if state == EvaluatedSiaeState.REFUSED and evaluated_siae.evaluation_is_final and not evaluated_siae.notified_at:
-        return "NOTIFICATION_PENDING"
-    return state
 
 
 TABLE = MetabaseTable(name="cap_structures")
@@ -27,7 +16,7 @@ TABLE.add_columns(
             "name": "état",
             "type": "varchar",
             "comment": "Etat du contrôle de la structure",
-            "fn": get_state,
+            "fn": lambda o: o.state,
         },
         get_column_from_field(get_field("reviewed_at"), name="date_contrôle"),
         get_column_from_field(get_field("final_reviewed_at"), name="date_définitive_contrôle"),
