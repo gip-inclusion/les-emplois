1bd86f36a20ac00ef6b2023679883132d5621db8
François Freitag <mail@franek.fr>
2023-06-30 09:55:37 +0200

    Handle identifiers in admin search bar and change pages
    SIRET, NIR and PASS IAE number have special meaning, and the whitespace
    separating groups have no significance. The support team spends a
    significant amount of time editing the values to remove spaces from
    them (while searching for a candidate, copying from an external source
    such as Zammad).

---
+++ itou/static/js/admin.js
@@ -0,0 +1,27 @@
+(function () {
+  document.addEventListener("DOMContentLoaded", function (event) {
+    function handlePaste(event) {
+      const pasted = event.clipboardData.getData("text");
+      const cleaned = pasted.replaceAll(" ", "");
+      if (/[0-9]{9,}/.test(cleaned)) {
+        event.preventDefault();
+        const field = event.target
+        field.value = field.value.substring(0, field.selectionStart)
+            + cleaned
+            + field.value.substring(field.selectionEnd);
+      }
+    }
+
+    for (const id of [
+      "searchbar",
+      "id_nir",
+      "id_number", // PASS IAE
+      "id_siret",
+    ]) {
+      const element = document.getElementById(id);
+      if (element) {
+        element.addEventListener("paste", handlePaste);
+      }
+    }
+  });
+}());
