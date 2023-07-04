15f1f4da30914c3e1a39fce3d29d06edc388da68
Victor Perron <victor@iso3103.net>
2023-07-04 18:54:27 +0200

    populate_matomo: Do not fetch private dashboard visits
    It is VERY tedious to do so as it means requesting thousands of
    Matomo URLs (for each departments, regions, etc)
    
    Also it is superseded by our analytics table which is even more accurate
    (unfortunately without some matomo specific data, but the visits are
    clearer)

--- itou/metabase/management/commands/populate_metabase_matomo.py
+++ itou/metabase/management/commands/populate_metabase_matomo.py
@@ -13,13 +13,6 @@ from django.conf import settings
 from django.core.management.base import BaseCommand
 from psycopg2 import extras as psycopg2_extras, sql
 
-from itou.common_apps.address.departments import (
-    DEPARTMENT_TO_REGION,
-    DEPARTMENTS,
-    REGIONS,
-    format_region_and_department_for_matomo,
-    format_region_for_matomo,
-)
 from itou.metabase.db import MetabaseDatabaseCursor, create_table
 from itou.utils import constants
 
@@ -67,40 +60,6 @@ PUBLIC_DASHBOARDS = {
     # "employeurs": "tb 54 - Typologie des employeurs",
 }
 
-PRIVATE_STATIC_DASHBOARDS = {
-    "stats/dgefp/auto_prescription/": "TB DGEFP AP",
-    "stats/dgefp/follow_siae_evaluation/": "TB DGEFP CAP",
-    "stats/dgefp/iae/": "TB DGEFP IAE",
-    "stats/dihal/state/": "TB DIHAL",
-    "stats/siae/follow_siae_evaluation/": "TB Suivi CAP SIAE",
-    "stats/siae/auto_prescription/": "TB Suivi Auto Prescription SIAE",
-    "stats/iae_network/hiring/": "TB Suivi Recrutement par réseau",
-}
-
-PRIVATE_DEPARTMENT_DASHBOARDS = {
-    "stats/cd/{}": "tb 118 - Données IAE CD",
-    "stats/ddets/auto_prescription/{}": "tb 267 - Focus auto-prescription DREETS/DDETS IAE",
-    "stats/ddets/follow_siae_evaluation/{}": "tb 265 - Suivi CAP DREETS/DDETS IAE",
-    "stats/ddets/hiring/{}": "tb 160 - Facilitation de l'embauche DREETS/DDETS IAE",
-    "stats/ddets/iae/{}": "tb 117 - Données IAE DREETS/DDETS",
-    "stats/pe/conversion/main/{}": "tb 169 - Taux de transformation PE",
-    "stats/pe/delay/main/{}": "tb 168 - Délai d'entrée en IAE",
-    "stats/pe/state/main/{}": "tb 149 - Candidatures orientées PE",
-    "stats/pe/tension/{}": "tb 162 - Fiches de poste en tension PE",
-    "stats/siae/hiring/{}": "tb 165 - Recrutement SIAE",
-}
-
-PRIVATE_REGION_DASHBOARDS = {
-    "stats/dreets/auto_prescription/{}": "tb 267 - Focus auto-prescription DREETS/DDETS IAE",
-    "stats/dreets/follow_siae_evaluation/{}": "tb 265 - Suivi CAP DREETS/DDETS IAE",
-    "stats/dreets/hiring/{}": "tb 160 - Facilitation de l'embauche DREETS/DDETS IAE",
-    "stats/dreets/iae/{}": "tb 117 - Données IAE DREETS/DDETS",
-    "stats/pe/conversion/main/{}/drpe": "tb 169 - Taux de transformation PE",
-    "stats/pe/delay/main/{}/drpe": "tb 168 - Délai d'entrée en IAE",
-    "stats/pe/state/main/{}/drpe": "tb 149 - Candidatures orientées PE",
-    "stats/pe/tension/{}/drpe": "tb 162 - Fiches de poste en tension PE",
-}
-
 
 MATOMO_OPTIONS = {
     "expanded": 1,
@@ -117,7 +76,6 @@ MATOMO_OPTIONS = {
 MATOMO_TIMEOUT = 60  # in seconds. Matomo can be slow.
 
 METABASE_PUBLIC_DASHBOARDS_TABLE_NAME = "suivi_visiteurs_tb_publics_v1"
-METABASE_PRIVATE_DASHBOARDS_TABLE_NAME = "suivi_visiteurs_tb_prives_v1"
 
 
 def matomo_api_call(options):
@@ -212,111 +170,35 @@ def multiget_matomo_dashboards(at: datetime.datetime, dashboard_options: list[Ma
 class Command(BaseCommand):
     help = "Fetches dashboards from matomo and inserts them monday by monday in Metabase in its raw version"
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.MODE_TO_OPERATION = {
-            "public": self._fetch_matomo_public_dashboards,
-            "private": self._fetch_matomo_private_dashboards,
-        }
-
     def add_arguments(self, parser):
-        parser.add_argument("--mode", action="store", dest="mode", type=str, choices=self.MODE_TO_OPERATION.keys())
         parser.add_argument("--wet-run", dest="wet_run", action="store_true")
 
-    def handle(self, mode, *, wet_run, **options):
+    def handle(self, *, wet_run, **options):
         today = datetime.date.today()
         max_date = datetime.date.today() - datetime.timedelta(days=today.weekday() + 1)
         # NOTE(vperron): if you need to initiate this table, just run the following line with
         # dtstart=datetime.date(2022,1,1)
         for monday in rrule(WEEKLY, byweekday=MO, dtstart=max_date - datetime.timedelta(days=7), until=max_date):
-            self.MODE_TO_OPERATION[mode](monday.date(), wet_run=wet_run)
-
-    def _fetch_matomo_public_dashboards(self, at, wet_run=False):
-        api_call_options = []
-        for url_path, dashboard_name in PUBLIC_DASHBOARDS.items():
-            api_call_options.append(
-                MatomoFetchOptions(
-                    dashboard_name,
-                    {
-                        "idSite": constants.MATOMO_SITE_PILOTAGE_ID,
-                        "segment": f"pageUrl=={constants.PILOTAGE_SITE_URL}/tableaux-de-bord/{url_path}/",
-                    },
-                    {},
-                )
-            )
-
-        threadsafe_print(f"> about to fetch count={len(api_call_options)} public dashboards from Matomo.")
-        column_names, all_rows = multiget_matomo_dashboards(at, api_call_options)
-        if wet_run and column_names:
-            update_table_at_date(
-                METABASE_PUBLIC_DASHBOARDS_TABLE_NAME,
-                column_names,
-                at,
-                sorted(all_rows, key=lambda r: r[-1]),  # sort by dashboard name
-            )
-
-    def _fetch_matomo_private_dashboards(self, at, wet_run=False):
-        base_options = {"idSite": constants.MATOMO_SITE_EMPLOIS_ID}
-        base_extra_columns = {
-            "Nom Département": None,
-            "Département": None,
-            "Région": None,  # NOTE(vperron): Region is added even though it's not in the current table
-        }
-        api_call_options = [
-            MatomoFetchOptions(
-                value,
-                base_options
-                | {
-                    "segment": f"pageUrl=={constants.EMPLOIS_SITE_URL}/{key}",
-                },
-                base_extra_columns,
-            )
-            for key, value in PRIVATE_STATIC_DASHBOARDS.items()
-        ]
-
-        def _options_from_url_path(url_path):
-            return base_options | {
-                "segment": f"pageUrl=={constants.EMPLOIS_SITE_URL}/{url_path}",
-            }
-
-        for region, departments in REGIONS.items():
-            region_url_path = format_region_for_matomo(region)
-            for url_path_fmt, dashboard_name in PRIVATE_REGION_DASHBOARDS.items():
-                url_path = url_path_fmt.format(region_url_path)
-                extra_columns = base_extra_columns | {
-                    "Région": region,
-                }
+            monday_date = monday.date()
+            api_call_options = []
+            for url_path, dashboard_name in PUBLIC_DASHBOARDS.items():
                 api_call_options.append(
                     MatomoFetchOptions(
                         dashboard_name,
-                        _options_from_url_path(url_path),
-                        extra_columns,
+                        {
+                            "idSite": constants.MATOMO_SITE_PILOTAGE_ID,
+                            "segment": f"pageUrl=={constants.PILOTAGE_SITE_URL}/tableaux-de-bord/{url_path}/",
+                        },
+                        {},
                     )
                 )
 
-            for department in departments:
-                dpt_url_path = format_region_and_department_for_matomo(department)
-                for url_path_fmt, dashboard_name in PRIVATE_DEPARTMENT_DASHBOARDS.items():
-                    url_path = url_path_fmt.format(dpt_url_path)
-                    extra_columns = {
-                        "Nom Département": DEPARTMENTS[department],
-                        "Département": department,
-                        "Région": DEPARTMENT_TO_REGION[department],
-                    }
-                    api_call_options.append(
-                        MatomoFetchOptions(
-                            dashboard_name,
-                            _options_from_url_path(url_path),
-                            extra_columns,
-                        )
-                    )
-
-        threadsafe_print(f"> about to fetch count={len(api_call_options)} private dashboards from Matomo.")
-        column_names, all_rows = multiget_matomo_dashboards(at, api_call_options)
-        if wet_run and column_names:
-            update_table_at_date(
-                METABASE_PRIVATE_DASHBOARDS_TABLE_NAME,
-                column_names,
-                at,
-                sorted(all_rows, key=lambda r: (r[-3] or "", r[-4])),  # sort by department if any, then dashboard name
-            )
+            threadsafe_print(f"> about to fetch count={len(api_call_options)} public dashboards from Matomo.")
+            column_names, all_rows = multiget_matomo_dashboards(monday_date, api_call_options)
+            if wet_run and column_names:
+                update_table_at_date(
+                    METABASE_PUBLIC_DASHBOARDS_TABLE_NAME,
+                    column_names,
+                    monday_date,
+                    sorted(all_rows, key=lambda r: r[-1]),  # sort by dashboard name
+                )
