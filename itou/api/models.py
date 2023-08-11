6f8159cd6c4415f115077b141c8f74a60067762b
Victor Perron <victor@iso3103.net>
2023-08-11 12:54:26 +0200

    api: Add SiaeApiToken model

---
+++ itou/api/models.py
@@ -0,0 +1,17 @@
+import uuid
+
+from django.db import models
+from django.utils import timezone
+
+from itou.siaes.models import Siae
+
+
+class SiaeApiToken(models.Model):
+    key = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    label = models.CharField(verbose_name="mémo permettant d'identifier l'usage du jeton", max_length=60, unique=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    siaes = models.ManyToManyField(Siae, related_name="api_tokens")
+
+    class Meta:
+        verbose_name = "jeton d'API SIAE"
+        verbose_name_plural = "jetons d'API SIAE"
