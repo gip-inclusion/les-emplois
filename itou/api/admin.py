783073180ff6bcb86596fe0957db66a01e3e2f9f
Victor Perron <victor@iso3103.net>
2023-08-11 12:54:26 +0200

    api: Add SiaeApiToken model

--- itou/api/admin.py
+++ itou/api/admin.py
@@ -1,7 +1,18 @@
+from django.contrib import admin
 from rest_framework.authtoken.admin import TokenAdmin
+
+from .models import SiaeApiToken
 
 
 # Patching TokenAdmin for all sub-APIs
 # Avoids listing all users when updating auth token via admin
 # See: https://www.django-rest-framework.org/api-guide/authentication/#tokenauthentication
 TokenAdmin.raw_id_fields = ("user",)
+
+
+@admin.register(SiaeApiToken)
+class SiaeApiTokenAdmin(admin.ModelAdmin):
+    list_display = ["key", "label", "created_at"]
+    ordering = ["-created_at"]
+    read_only_fields = ["key", "created_at"]
+    autocomplete_fields = ["siaes"]
