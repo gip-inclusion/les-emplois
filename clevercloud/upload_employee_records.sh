c5919c05311dea8cc3d065abb1ac92077b7bbeae
Romain Sébille <romain.sebille@beta.gouv.fr>
2023-08-23 17:14:20 +0200

    cron: Run `sanitize_employee_records` like a standard management command
    - Can be run safely on every environment as it only use or modify our
      data in a custodial capacity.
    - Run it more often for consistency.
    - Management commands shouldn't rely that another management command has
      run before.

--- clevercloud/upload_employee_records.sh
+++ clevercloud/upload_employee_records.sh
@@ -17,9 +17,6 @@ fi
 # $APP_HOME is set by default by clever cloud.
 cd "$APP_HOME" || exit
 
-# Check for common blocking errors before uploads
-django-admin sanitize_employee_records
-
 # Upload employee records
 django-admin transfer_employee_records --upload --wet-run
 
