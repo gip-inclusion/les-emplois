8a26f90c49e48e62e2e7d759e9d3f4df89b0769a
Victor Perron <victor@iso3103.net>
2023-08-30 11:48:28 +0200

    Financial annexes: prevent any change within the admin

--- itou/siaes/admin.py
+++ itou/siaes/admin.py
@@ -388,3 +388,12 @@ class SiaeFinancialAnnexAdmin(admin.ModelAdmin):
         ),
     )
     search_fields = ("pk", "number")
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
