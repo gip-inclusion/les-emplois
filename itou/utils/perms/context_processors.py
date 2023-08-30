9ce7fac06b6f471a7ed8255ed263a74db190bc60
Xavier Fernandez <xavier.fernandez@beta.gouv.fr>
2023-08-30 17:54:23 +0200

    utils.perms: simplify get_current_organization_and_perms

--- itou/utils/perms/context_processors.py
+++ itou/utils/perms/context_processors.py
@@ -3,23 +3,15 @@ def sort_organizations(collection):
 
 
 def get_current_organization_and_perms(request):
-    if request.user.is_authenticated:
-        context, extra_context = {}, {}
-
-        if getattr(request, "current_organization", None) is not None:
-            if request.user.is_siae_staff:
-                extra_context = get_context_siae(request)
-
-            if request.user.is_prescriber:
-                extra_context = get_context_prescriber(request)
-
-            if request.user.is_labor_inspector:
-                extra_context = get_context_institution(request)
-
-        context.update(extra_context)
-        return context
-
-    return {}
+    context = {}
+    if request.user.is_authenticated and getattr(request, "current_organization", None) is not None:
+        if request.user.is_siae_staff:
+            context = get_context_siae(request)
+        elif request.user.is_prescriber:
+            context = get_context_prescriber(request)
+        elif request.user.is_labor_inspector:
+            context = get_context_institution(request)
+    return context
 
 
 def get_context_siae(request):
