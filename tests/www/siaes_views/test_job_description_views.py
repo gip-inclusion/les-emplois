gip-inclusion/les-emplois

commit 4c2cd64f21cd306c1d1cd4775e9081db60ef0289

utils: migrate to new unique ITOU_SESSION_CURRENT_ORGANIZATION_KEY
Since an user can only have one kind, no need to have separate keys

itou/www/siaes_views/views.py
@@ -423,7 +423,7 @@ def create_siae(request, template_name="siaes/create_siae.html"):
     if request.method == "POST" and form.is_valid():
         try:
             siae = form.save()
-            request.session[global_constants.ITOU_SESSION_CURRENT_SIAE_KEY] = siae.pk
+            request.session[global_constants.ITOU_SESSION_CURRENT_ORGANIZATION_KEY] = siae.pk
             return HttpResponseRedirect(reverse("dashboard:index"))
         except GeocodingDataError:
             messages.error(request, "L'adresse semble erronée. Veuillez la corriger avant de pouvoir « Enregistrer ».")

itou/www/dashboard/views.py
@@ -326,18 +326,15 @@ def switch_organization(request):
     match request.user.kind:
         case UserKind.SIAE_STAFF:
             queryset = Siae.objects.active_or_in_grace_period().member_required(request.user)
-            session_key = global_constants.ITOU_SESSION_CURRENT_SIAE_KEY
         case UserKind.PRESCRIBER:
             queryset = PrescriberOrganization.objects.member_required(request.user)
-            session_key = global_constants.ITOU_SESSION_CURRENT_PRESCRIBER_ORG_KEY
         case UserKind.LABOR_INSPECTOR:
             queryset = Institution.objects.member_required(request.user)
-            session_key = global_constants.ITOU_SESSION_CURRENT_INSTITUTION_KEY
         case _:
             raise Http404()
 
     organization = get_object_or_404(queryset, pk=pk)
-    request.session[session_key] = organization.pk
+    request.session[global_constants.ITOU_SESSION_CURRENT_ORGANIZATION_KEY] = organization.pk
     return HttpResponseRedirect(reverse("dashboard:index"))
 
 
@@ -354,7 +351,7 @@ def switch_siae(request):
     pk = request.POST["siae_id"]
     queryset = Siae.objects.active_or_in_grace_period().member_required(request.user)
     siae = get_object_or_404(queryset, pk=pk)
-    request.session[global_constants.ITOU_SESSION_CURRENT_SIAE_KEY] = siae.pk
+    request.session[global_constants.ITOU_SESSION_CURRENT_ORGANIZATION_KEY] = siae.pk
 
     return HttpResponseRedirect(dashboard_url)
 
@@ -370,7 +367,7 @@ def switch_prescriber_organization(request):
     pk = request.POST["prescriber_organization_id"]
     queryset = PrescriberOrganization.objects.member_required(request.user)
     prescriber_organization = get_object_or_404(queryset, pk=pk)
-    request.session[global_constants.ITOU_SESSION_CURRENT_PRESCRIBER_ORG_KEY] = prescriber_organization.pk
+    request.session[global_constants.ITOU_SESSION_CURRENT_ORGANIZATION_KEY] = prescriber_organization.pk
 
     return HttpResponseRedirect(dashboard_url)
 
@@ -386,7 +383,7 @@ def switch_institution(request):
     pk = request.POST["institution_id"]
     queryset = Institution.objects.member_required(request.user)
     institution = get_object_or_404(queryset, pk=pk)
-    request.session[global_constants.ITOU_SESSION_CURRENT_INSTITUTION_KEY] = institution.pk
+    request.session[global_constants.ITOU_SESSION_CURRENT_ORGANIZATION_KEY] = institution.pk
 
     return HttpResponseRedirect(dashboard_url)
 
@@ -396,7 +393,7 @@ def edit_user_notifications(request, template_name="dashboard/edit_user_notifica
     if not request.user.is_siae_staff:
         raise PermissionDenied
 
-    current_siae_pk = request.session.get(global_constants.ITOU_SESSION_CURRENT_SIAE_KEY)
+    current_siae_pk = request.session.get(global_constants.ITOU_SESSION_CURRENT_ORGANIZATION_KEY)
     siae = get_object_or_404(Siae, pk=current_siae_pk)
     membership = request.user.siaemembership_set.get(siae=siae)
     new_job_app_notification_form = EditNewJobAppEmployersNotificationForm(

itou/www/invitations_views/views.py
@@ -187,7 +187,7 @@ def join_prescriber_organization(request, invitation_id):
     else:
         messages.error(request, "Cette invitation n'est plus valide.")
 
-    request.session[global_constants.ITOU_SESSION_CURRENT_PRESCRIBER_ORG_KEY] = invitation.organization.pk
+    request.session[global_constants.ITOU_SESSION_CURRENT_ORGANIZATION_KEY] = invitation.organization.pk
     url = get_adapter(request).get_login_redirect_url(request)
     return HttpResponseRedirect(url)
 
@@ -248,7 +248,7 @@ def join_siae(request, invitation_id):
     else:
         messages.error(request, "Cette invitation n'est plus valide.")
 
-    request.session[global_constants.ITOU_SESSION_CURRENT_SIAE_KEY] = invitation.siae.pk
+    request.session[global_constants.ITOU_SESSION_CURRENT_ORGANIZATION_KEY] = invitation.siae.pk
     url = get_adapter(request).get_login_redirect_url(request)
     return HttpResponseRedirect(url)
 
@@ -309,5 +309,5 @@ def join_institution(request, invitation_id):
     else:
         messages.error(request, "Cette invitation n'est plus valide.")
 
-    request.session[global_constants.ITOU_SESSION_CURRENT_INSTITUTION_KEY] = invitation.institution.pk
+    request.session[global_constants.ITOU_SESSION_CURRENT_ORGANIZATION_KEY] = invitation.institution.pk
     return redirect("dashboard:index")

itou/utils/constants.py
@@ -13,6 +13,7 @@ POLE_EMPLOI_EMAIL_SUFFIX = "@pole-emploi.fr"
 ITOU_SESSION_CURRENT_PRESCRIBER_ORG_KEY = "current_prescriber_organization"
 ITOU_SESSION_CURRENT_SIAE_KEY = "current_siae"
 ITOU_SESSION_CURRENT_INSTITUTION_KEY = "current_institution"
+ITOU_SESSION_CURRENT_ORGANIZATION_KEY = "current_organization"
 ITOU_SESSION_PRESCRIBER_SIGNUP_KEY = "prescriber_signup"
 ITOU_SESSION_NIR_KEY = "job_seeker_nir"
 

itou/utils/perms/institution.py
@@ -5,7 +5,7 @@ from itou.utils import constants as global_constants
 
 
 def get_current_institution_or_404(request):
-    pk = request.session.get(global_constants.ITOU_SESSION_CURRENT_INSTITUTION_KEY)
+    pk = request.session.get(global_constants.ITOU_SESSION_CURRENT_ORGANIZATION_KEY)
     queryset = Institution.objects.member_required(request.user)
     institution = get_object_or_404(queryset, pk=pk)
     return institution

itou/utils/perms/middleware.py
@@ -10,8 +10,15 @@ from itou.utils import constants as global_constants
 from itou.www.login import urls as login_urls
 
 
-def extract_membership_infos_and_update_session(memberships, org_through_field, session, session_key):
-    current_org_pk = session.get(session_key)
+def extract_membership_infos_and_update_session(memberships, org_through_field, session, old_session_key):
+    # TODO(xfernandez): remove old_session_key management in a few weeks
+    current_org_pk = session.get(old_session_key)
+    if current_org_pk:
+        # If old session key is still used, migrate to the new one
+        session[global_constants.ITOU_SESSION_CURRENT_ORGANIZATION_KEY] = current_org_pk
+        del session[old_session_key]
+
+    current_org_pk = session.get(global_constants.ITOU_SESSION_CURRENT_ORGANIZATION_KEY)
     orgs = []
     current_org = None
     admin_status = {}
@@ -25,12 +32,12 @@ def extract_membership_infos_and_update_session(memberships, org_through_field, 
         if orgs:
             # If an org exists, choose the first one
             current_org = orgs[0]
-            session[session_key] = current_org.pk
+            session[global_constants.ITOU_SESSION_CURRENT_ORGANIZATION_KEY] = current_org.pk
         elif current_org_pk:
             # If the user has not active membership anymore
             # => No need to track the current org in session (none)
             # => Remove any old session entry if needed
-            del session[session_key]
+            del session[global_constants.ITOU_SESSION_CURRENT_ORGANIZATION_KEY]
     return (
         sorted(orgs, key=lambda o: (o.kind, o.display_name)),
         current_org,

itou/utils/perms/prescriber.py
@@ -6,7 +6,7 @@ from itou.utils import constants as global_constants
 
 
 def get_current_org_or_404(request):
-    pk = request.session.get(global_constants.ITOU_SESSION_CURRENT_PRESCRIBER_ORG_KEY)
+    pk = request.session.get(global_constants.ITOU_SESSION_CURRENT_ORGANIZATION_KEY)
     queryset = PrescriberOrganization.objects.member_required(request.user)
     organization = get_object_or_404(queryset, pk=pk)
     return organization

itou/utils/perms/siae.py
@@ -5,7 +5,7 @@ from itou.utils import constants as global_constants
 
 
 def get_current_siae_or_404(request, with_job_descriptions=False) -> Siae:
-    pk = request.session.get(global_constants.ITOU_SESSION_CURRENT_SIAE_KEY)
+    pk = request.session.get(global_constants.ITOU_SESSION_CURRENT_ORGANIZATION_KEY)
     queryset = Siae.objects.member_required(request.user)
 
     if with_job_descriptions:

commit eb247293ff019e9fa3e868eecaef5db4ae5fb7bf

www.siaes: Fix SIAE not able to edit their job descriptions
Probably broken since 4c2cd64f21cd306c1d1cd4775e9081db60ef0289.

itou/www/siaes_views/views.py
@@ -34,7 +34,7 @@ def job_description_card(request, job_description_id, template_name="siaes/job_d
     job_description = get_object_or_404(SiaeJobDescription, pk=job_description_id)
     back_url = get_safe_url(request, "back_url")
     siae = job_description.siae
-    can_update_job_description = request.session.get("current_siae") == siae.pk
+    can_update_job_description = request.user.is_authenticated and request.current_organization.pk == siae.pk
 
     # select_related on siae, location useful for _list_siae_actives_jobs_row.html template
     others_active_jobs = (

tests/www/siaes_views/test_job_description_views.py
@@ -481,18 +481,21 @@ class JobDescriptionCardTest(JobDescriptionAbstractTest):
         # Checks if SIAE can update their job descriptions
         response = self._login(self.user)
 
-        assert response.status_code == 200
-
-        response = self.client.get(self.url)
-
-        self.assertContains(response, "Modifier")
+        self.assertContains(response, "Modifier la fiche de poste")
+        self.assertContains(
+            response,
+            reverse(
+                "siaes_views:update_job_description",
+                kwargs={"job_description_id": self.job_description.pk},
+            ),
+        )
+        self.assertContains(response, "Retour vers la liste des postes")
+        self.assertContains(response, reverse("siaes_views:job_description_list"))
+        self.assertNotContains(response, reverse("apply:start", kwargs={"siae_pk": self.siae.pk}))
 
     def test_non_siae_card_actions(self):
         # Checks if non-SIAE can apply to opened job descriptions
-        user = PrescriberOrganizationWithMembershipFactory().members.first()
-        response = self._login(user)
-
-        assert response.status_code == 200
+        self.client.force_login(PrescriberOrganizationWithMembershipFactory().members.first())
 
         with self.assertNumQueries(
             BASE_NUM_QUERIES
@@ -503,14 +506,22 @@ class JobDescriptionCardTest(JobDescriptionAbstractTest):
             + 1  # fetch siaes infos
             + 1  # fetch jobappelation
             + 1  # fetch other job infos
+            + 3  # update session
         ):
             response = self.client.get(self.url)
 
-        self.assertContains(response, "Postuler")
+        self.assertContains(response, "Postuler auprès de l'employeur solidaire")
+        self.assertContains(response, reverse("apply:start", kwargs={"siae_pk": self.siae.pk}))
+        self.assertNotContains(
+            response,
+            reverse(
+                "siaes_views:update_job_description",
+                kwargs={"job_description_id": self.job_description.pk},
+            ),
+        )
 
     def test_display_placeholder_for_empty_fields(self):
         response = self._login(self.user)
-        response = self.client.get(self.url)
 
         # Job description created in setup has empty description fields
         self.assertContains(response, "La structure n'a pas encore renseigné cette rubrique", count=2)