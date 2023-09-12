gip-inclusion/les-emplois

commit 528a1841c1258909207aa72f7010e3c572c0fc2e

www.siaes: Fix SIAE not able to edit their job descriptions
Probably broken since 4c2cd64f21cd306c1d1cd4775e9081db60ef0289.

itou/www/siaes_views/views.py
@@ -34,7 +34,7 @@ def job_description_card(request, job_description_id, template_name="siaes/job_d
     job_description = get_object_or_404(SiaeJobDescription, pk=job_description_id)
     back_url = get_safe_url(request, "back_url")
     siae = job_description.siae
-    can_update_job_description = request.session.get("current_siae") == siae.pk
+    can_update_job_description = request.user.is_authenticated and request.current_organization.pk == siae.pk
 
     # select_related on siae, location useful for _list_siae_actives_jobs_row.html template
     others_active_jobs = (

tests/www/siaes_views/test_job_description_views.py
@@ -481,18 +481,21 @@ class JobDescriptionCardTest(JobDescriptionAbstractTest):
         # Checks if SIAE can update their job descriptions
         response = self._login(self.user)
 
-        assert response.status_code == 200
-
-        response = self.client.get(self.url)
-
-        self.assertContains(response, "Modifier")
+        self.assertContains(response, "Modifier la fiche de poste")
+        self.assertContains(
+            response,
+            reverse(
+                "siaes_views:update_job_description",
+                kwargs={"job_description_id": self.job_description.pk},
+            ),
+        )
+        self.assertContains(response, "Retour vers la liste des postes")
+        self.assertContains(response, reverse("siaes_views:job_description_list"))
+        self.assertNotContains(response, reverse("apply:start", kwargs={"siae_pk": self.siae.pk}))
 
     def test_non_siae_card_actions(self):
         # Checks if non-SIAE can apply to opened job descriptions
-        user = PrescriberOrganizationWithMembershipFactory().members.first()
-        response = self._login(user)
-
-        assert response.status_code == 200
+        self.client.force_login(PrescriberOrganizationWithMembershipFactory().members.first())
 
         with self.assertNumQueries(
             BASE_NUM_QUERIES
@@ -503,14 +506,22 @@ class JobDescriptionCardTest(JobDescriptionAbstractTest):
             + 1  # fetch siaes infos
             + 1  # fetch jobappelation
             + 1  # fetch other job infos
+            + 3  # update session
         ):
             response = self.client.get(self.url)
 
-        self.assertContains(response, "Postuler")
+        self.assertContains(response, "Postuler auprès de l'employeur solidaire")
+        self.assertContains(response, reverse("apply:start", kwargs={"siae_pk": self.siae.pk}))
+        self.assertNotContains(
+            response,
+            reverse(
+                "siaes_views:update_job_description",
+                kwargs={"job_description_id": self.job_description.pk},
+            ),
+        )
 
     def test_display_placeholder_for_empty_fields(self):
         response = self._login(self.user)
-        response = self.client.get(self.url)
 
         # Job description created in setup has empty description fields
         self.assertContains(response, "La structure n'a pas encore renseigné cette rubrique", count=2)

commit 4c8ae24a1d284e587f96e8398732813935ef4bc6

www.siaes: Fix job seeker not able to see the job description
Bug introduced by 528a1841c.

itou/www/siaes_views/views.py
@@ -34,7 +34,9 @@ def job_description_card(request, job_description_id, template_name="siaes/job_d
     job_description = get_object_or_404(SiaeJobDescription, pk=job_description_id)
     back_url = get_safe_url(request, "back_url")
     siae = job_description.siae
-    can_update_job_description = request.user.is_authenticated and request.current_organization.pk == siae.pk
+    can_update_job_description = (
+        request.user.is_authenticated and request.user.is_siae_staff and request.current_organization.pk == siae.pk
+    )
 
     # select_related on siae, location useful for _list_siae_actives_jobs_row.html template
     others_active_jobs = (

tests/www/siaes_views/test_job_description_views.py
@@ -13,6 +13,7 @@ from itou.www.siaes_views.views import ITOU_SESSION_JOB_DESCRIPTION_KEY
 from tests.jobs.factories import create_test_romes_and_appellations
 from tests.prescribers.factories import PrescriberOrganizationWithMembershipFactory
 from tests.siaes.factories import SiaeFactory, SiaeJobDescriptionFactory
+from tests.users.factories import JobSeekerFactory
 from tests.utils.test import BASE_NUM_QUERIES, TestCase, assertMessages
 
 
@@ -477,7 +478,7 @@ class JobDescriptionCardTest(JobDescriptionAbstractTest):
             },
         )
 
-    def test_siae_card_actions(self):
+    def test_siae_staff_card_actions(self):
         # Checks if SIAE can update their job descriptions
         response = self._login(self.user)
 
@@ -493,7 +494,7 @@ class JobDescriptionCardTest(JobDescriptionAbstractTest):
         self.assertContains(response, reverse("siaes_views:job_description_list"))
         self.assertNotContains(response, reverse("apply:start", kwargs={"siae_pk": self.siae.pk}))
 
-    def test_non_siae_card_actions(self):
+    def test_prescriber_card_actions(self):
         # Checks if non-SIAE can apply to opened job descriptions
         self.client.force_login(PrescriberOrganizationWithMembershipFactory().members.first())
 
@@ -509,6 +510,44 @@ class JobDescriptionCardTest(JobDescriptionAbstractTest):
             + 3  # update session
         ):
             response = self.client.get(self.url)
+
+        self.assertContains(response, "Postuler auprès de l'employeur solidaire")
+        self.assertContains(response, reverse("apply:start", kwargs={"siae_pk": self.siae.pk}))
+        self.assertNotContains(
+            response,
+            reverse(
+                "siaes_views:update_job_description",
+                kwargs={"job_description_id": self.job_description.pk},
+            ),
+        )
+
+    def test_job_seeker_card_actions(self):
+        self.client.force_login(JobSeekerFactory())
+
+        with self.assertNumQueries(
+            BASE_NUM_QUERIES
+            + 1  # fetch django session
+            + 1  # fetch user
+            + 1  # fetch siaes_siaejobdescription
+            + 1  # fetch siaes infos
+            + 1  # fetch jobappelation
+            + 1  # fetch other job infos
+            + 3  # update session
+        ):
+            response = self.client.get(self.url)
+
+        self.assertContains(response, "Postuler auprès de l'employeur solidaire")
+        self.assertContains(response, reverse("apply:start", kwargs={"siae_pk": self.siae.pk}))
+        self.assertNotContains(
+            response,
+            reverse(
+                "siaes_views:update_job_description",
+                kwargs={"job_description_id": self.job_description.pk},
+            ),
+        )
+
+    def test_anonymous_card_actions(self):
+        response = self.client.get(self.url)
 
         self.assertContains(response, "Postuler auprès de l'employeur solidaire")
         self.assertContains(response, reverse("apply:start", kwargs={"siae_pk": self.siae.pk}))