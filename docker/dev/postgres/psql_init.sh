3deb605ff0b34ba5292ea2a84b6af2e5495aa40b
François Freitag <mail@franek.fr>
2023-09-12 11:07:07 +0200

    dev: Use POSTGRES_DB instead of a custom init
    My system has umask=077, so the psql_init.sh file was created with
    permissions 0700. When mounting on the docker, user postgres sees it
    does not have the executable bit set, tries to source it, but it also
    cannot read it.

--- docker/dev/postgres/psql_init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-# Initialization script.
-# https://hub.docker.com/_/postgres/#initialization-scripts
-
-psql -v ON_ERROR_STOP=1 --echo-queries --command="CREATE DATABASE \"$ITOU_POSTGRES_DATABASE_NAME\";"
