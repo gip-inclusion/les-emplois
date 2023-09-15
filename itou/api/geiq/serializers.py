36d68c2c0556b2dcc597fa9d7031ead5a30eeb9e
Victor Perron <victor@iso3103.net>
2023-09-15 19:50:59 +0200

    more mappings

--- itou/api/geiq/serializers.py
+++ itou/api/geiq/serializers.py
@@ -10,7 +10,7 @@ from itou.eligibility.enums import AuthorKind
 from itou.eligibility.models.geiq import GEIQAdministrativeCriteria
 from itou.job_applications.models import JobApplication, PriorAction
 from itou.prescribers.enums import PrescriberOrganizationKind
-from itou.siaes.enums import ContractType, SiaeKind
+from itou.siaes.enums import ContractType
 from itou.users.enums import Title
 
 
@@ -39,23 +39,22 @@ EMPLOIS_TO_LABEL_SENDER_KIND = {
 
 
 class LabelEducationLevel(models.TextChoices):
-    LEVEL_3 = "LEVEL_3", "Niveau 3 (CAP, BEP)"
-    LEVEL_4 = "LEVEL_4", "Niveau 4 (BP, Bac Général, Techno ou Pro, BT)"
-    LEVEL_5 = "LEVEL_5", "Niveau 5 ou + (Bac+2 ou +)"
-    NO_QUALIFICATION = "NO_QUALIFICATION", "Sans qualification"
-    OTHER = "OTHER", "Autre"
+    N3 = "N3", "Niveau 3 (CAP, BEP)"
+    N4 = "N4", "Niveau 4 (BP, Bac Général, Techno ou Pro, BT)"
+    N5 = "N5", "Niveau 5 ou + (Bac+2 ou +)"
+    SQ = "SQ", "Sans qualification"
 
 
 LABEL_TO_ASP_EDUCATION_LEVELS = {
-    LabelEducationLevel.NO_QUALIFICATION: [
+    LabelEducationLevel.SQ: [
         EducationLevel.NON_CERTIFYING_QUALICATIONS,
         EducationLevel.NO_SCHOOLING,
         EducationLevel.NO_SCHOOLING_BEYOND_MANDATORY,
         EducationLevel.TRAINING_1_YEAR,
     ],
-    LabelEducationLevel.LEVEL_3: [EducationLevel.BEP_OR_CAP_DIPLOMA, EducationLevel.BEP_OR_CAP_LEVEL],
-    LabelEducationLevel.LEVEL_4: [EducationLevel.BAC_LEVEL, EducationLevel.BT_OR_BACPRO_LEVEL],
-    LabelEducationLevel.LEVEL_5: [
+    LabelEducationLevel.N3: [EducationLevel.BEP_OR_CAP_DIPLOMA, EducationLevel.BEP_OR_CAP_LEVEL],
+    LabelEducationLevel.N4: [EducationLevel.BAC_LEVEL, EducationLevel.BT_OR_BACPRO_LEVEL],
+    LabelEducationLevel.N5: [
         EducationLevel.BTS_OR_DUT_LEVEL,
         EducationLevel.LICENCE_LEVEL,
         EducationLevel.THIRD_CYCLE_OR_ENGINEERING_SCHOOL,
@@ -64,6 +63,71 @@ LABEL_TO_ASP_EDUCATION_LEVELS = {
 
 ASP_TO_LABEL_EDUCATION_LEVELS = {
     asp_level: label_level for label_level, values in LABEL_TO_ASP_EDUCATION_LEVELS.items() for asp_level in values
+}
+
+
+class LabelDiagAuthorKind(models.TextChoices):
+    PRESCRIPTEUR = "PRESCRIPTEUR", "Prescripteur"
+    EMPLOYEUR = "EMPLOYEUR", "Employeur"
+    GEIQ = "GEIQ", "GEIQ"
+
+
+EMPLOIS_TO_LABEL_DIAG_AUTHOR_KIND = {
+    AuthorKind.PRESCRIBER: LabelDiagAuthorKind.PRESCRIPTEUR,
+    AuthorKind.SIAE_STAFF: LabelDiagAuthorKind.EMPLOYEUR,
+    AuthorKind.GEIQ: LabelDiagAuthorKind.GEIQ,
+}
+
+
+class LabelProfessionalSituationExperience(models.TextChoices):
+    MRS = "MRS", "Mise en relation sociale"
+    AUTRE = "AUTRE", "Autre"
+    PMSMP = "PMSMP", "Période de mise en situation en milieu professionnel"
+    STAGE = "STAGE", "Stage"
+
+
+EMPLOIS_TO_LABEL_PRO_SITU_EXP = {
+    enums.ProfessionalSituationExperience.MRS: LabelProfessionalSituationExperience.MRS,
+    enums.ProfessionalSituationExperience.OTHER: LabelProfessionalSituationExperience.AUTRE,
+    enums.ProfessionalSituationExperience.PMSMP: LabelProfessionalSituationExperience.PMSMP,
+    enums.ProfessionalSituationExperience.STAGE: LabelProfessionalSituationExperience.STAGE,
+}
+
+
+class LabelPrequalification(models.TextChoices):
+    AFPR = "AFPR", "AFPR"
+    LOCAL_PLAN = "LOCAL_PLAN", "Dispositif régional ou sectoriel"
+    POE = "POE", "POE"
+    OTHER = "AUTRE", "Autre"
+
+
+EMPLOIS_TO_LABEL_PREQUALIFICATION = {
+    enums.Prequalification.AFPR: LabelPrequalification.AFPR,
+    enums.Prequalification.LOCAL_PLAN: LabelPrequalification.LOCAL_PLAN,
+    enums.Prequalification.POE: LabelPrequalification.POE,
+    enums.Prequalification.OTHER: LabelPrequalification.OTHER,
+}
+
+
+class LabelContractType(models.TextChoices):
+    CPRO = "CPRO", "Contrat de professionnalisation"
+    CAPP = "CAPP", "Contrat d'apprentissage"
+    CUI_F = "CUI+F", "CUI (toute catégorie)"
+    CUI = "CUI", "CUI (catégorie 1)"
+    CDD = "CDD", "CDD"
+    CDI = "CDI", "CDI"
+    AUTRE_F = "AUTRE F", "Autre F"
+    CDD_CPF = "CDD+CPF", "CDD CPF"
+    CDD_autre = "CDD+autre", "CDD autre"
+    AUTRE_SF = "Autre SF", "Autre SF"
+
+
+EMPLOIS_TO_LABEL_CONTRACT_TYPE = {
+    ContractType.FIXED_TERM: LabelContractType.CDD,
+    ContractType.PERMANENT: LabelContractType.CDI,
+    ContractType.PROFESSIONAL_TRAINING: LabelContractType.CPRO,
+    ContractType.APPRENTICESHIP: LabelContractType.CAPP,
+    ContractType.OTHER: LabelContractType.AUTRE_SF,
 }
 
 
@@ -83,10 +147,8 @@ class LazyChoiceField(serializers.ChoiceField):
     choices = property(fget=get_choices, fset=set_choices)
 
 
-class PriorActionSerializer(serializers.ModelSerializer):
-    code = serializers.ChoiceField(
-        source="action", choices=sorted(enums.ProfessionalSituationExperience.choices + enums.Prequalification.choices)
-    )
+class BasePriorActionSerializer(serializers.ModelSerializer):
+    code = serializers.SerializerMethodField()
     date_debut = serializers.DateField(source="dates.lower")
     date_fin = serializers.DateField(source="dates.upper")
 
@@ -97,6 +159,18 @@ class PriorActionSerializer(serializers.ModelSerializer):
             "date_debut",
             "date_fin",
         )
+
+
+class MSPPriorActionSerializer(BasePriorActionSerializer):
+    @extend_schema_field(serializers.ChoiceField(choices=sorted(LabelProfessionalSituationExperience.choices)))
+    def get_code(self, obj) -> str:
+        return EMPLOIS_TO_LABEL_PRO_SITU_EXP[obj.action]
+
+
+class PrequalPriorActionSerializer(BasePriorActionSerializer):
+    @extend_schema_field(serializers.ChoiceField(choices=sorted(LabelPrequalification.choices)))
+    def get_code(self, obj) -> str:
+        return EMPLOIS_TO_LABEL_PREQUALIFICATION[obj.action]
 
 
 class GeiqJobApplicationSerializer(serializers.ModelSerializer):
@@ -119,19 +193,12 @@ class GeiqJobApplicationSerializer(serializers.ModelSerializer):
         choices=sorted(PrescriberOrganizationKind.choices),
     )
     criteres_eligibilite = serializers.SerializerMethodField()
-    auteur_diagnostic = serializers.ChoiceField(
-        source="geiq_eligibility_diagnosis.author_kind",
-        choices=sorted(AuthorKind.choices),
-        allow_null=True,
-    )
+    auteur_diagnostic = serializers.SerializerMethodField()
     niveau_formation = serializers.SerializerMethodField()
-    mises_en_situation_pro = PriorActionSerializer(many=True)
-    prequalifications = PriorActionSerializer(many=True)
+    mises_en_situation_pro = MSPPriorActionSerializer(many=True)
+    prequalifications = PrequalPriorActionSerializer(many=True)
     jours_accompagnement = serializers.IntegerField(source="prehiring_guidance_days", min_value=0)
-    type_contrat = serializers.ChoiceField(
-        source="contract_type",
-        choices=ContractType.choices_for_siae_kind(SiaeKind.GEIQ),
-    )
+    type_contrat = serializers.SerializerMethodField()
     poste_occupe = serializers.SerializerMethodField()
     duree_hebdo = serializers.IntegerField(
         source="nb_hours_per_week",
@@ -202,12 +269,22 @@ class GeiqJobApplicationSerializer(serializers.ModelSerializer):
     def get_source_orientation(self, obj) -> str | None:
         return EMPLOIS_TO_LABEL_SENDER_KIND.get(obj.sender_kind, None)
 
+    @extend_schema_field(serializers.ChoiceField(choices=sorted(LabelDiagAuthorKind.choices)))
+    def get_auteur_diagnostic(self, obj) -> str | None:
+        if diag := obj.geiq_eligibility_diagnosis:
+            return EMPLOIS_TO_LABEL_DIAG_AUTHOR_KIND[diag.author_kind]
+        return None
+
     @extend_schema_field(serializers.ChoiceField(choices=sorted(LabelEducationLevel.choices)))
     def get_niveau_formation(self, obj) -> str | None:
         asp_level = obj.job_seeker.jobseeker_profile.education_level
         if asp_level:
-            return ASP_TO_LABEL_EDUCATION_LEVELS.get(asp_level, LabelEducationLevel.OTHER)
+            return ASP_TO_LABEL_EDUCATION_LEVELS[asp_level]
         return None
+
+    @extend_schema_field(serializers.ChoiceField(choices=sorted(LabelContractType.choices)))
+    def get_type_contrat(self, obj) -> str | None:
+        return EMPLOIS_TO_LABEL_CONTRACT_TYPE.get(obj.contract_type, None)
 
     def get_poste_occupe(self, obj) -> str | None:
         """
