865e92088e496e866f32249e1a47fa712b6e8266
Xavier Fernandez <xavier.fernandez@beta.gouv.fr>
2023-09-19 10:42:37 +0200

    static: remove version from destination directories
    It removes a place to change when upgrading the versions

--- itou/api/redoc_views.py
+++ itou/api/redoc_views.py
@@ -5,4 +5,4 @@ from drf_spectacular.views import SpectacularRedocView
 class ItouSpectacularRedocView(SpectacularRedocView):
     @staticmethod
     def _redoc_standalone():
-        return static("vendor/redoc-2.0.0/redoc.standalone.js")
+        return static("vendor/redoc/redoc.standalone.js")
