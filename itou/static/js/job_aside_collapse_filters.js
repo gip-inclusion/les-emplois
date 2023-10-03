971240f78bc06a8e80ba7fa071aadeeeb2d7be8d
hellodeloo <hello@hellodeloo.com>
2023-10-03 16:00:54 +0200

    feat: remove collapse_filters.js to use autoCollapseAsideFilters() from theme

--- itou/static/js/job_aside_collapse_filters.js
+++
@@ -1,9 +0,0 @@
-// Show or Hide the search-filter-form sidebar based on responsive breakpoint
-var breakpointMD = getComputedStyle(document.documentElement).getPropertyValue("--breakpoint-md");
-$(window).on("load resize", function() {
-    if (window.matchMedia("(min-width: "+breakpointMD+")").matches) {
-        $("#job-aside-filters-form").collapse("show");
-    } else {
-        $("#job-aside-filters-form").collapse("hide");
-    }
-});
