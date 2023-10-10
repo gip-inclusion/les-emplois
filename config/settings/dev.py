a9d9e322cac3eafb5ef41ed1a2163e9ef2c23bbf
François Freitag <mail@franek.fr>
2023-10-10 17:29:42 +0200

    Move DEV settings for S3 access to config.settings.dev
    Mistakenly remained in test settings, but tests are configured by
    fixture `test_settings`.
    
    That revealed the wrong settings were overridden in test_settings, they
    have been fixed.

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -74,4 +74,7 @@ METABASE_DATABASE = os.getenv("PGDATABASE", "metabase")  # noqa: F405
 
 FORCE_IC_LOGIN = os.getenv("FORCE_IC_LOGIN", "True") == "True"
 
+AWS_S3_ENDPOINT_URL = f"http://{os.getenv('CELLAR_ADDON_HOST', 'localhost:9000')}/"
+AWS_S3_ACCESS_KEY_ID = "minioadmin"
+AWS_S3_SECRET_ACCESS_KEY = "minioadmin"
 AWS_STORAGE_BUCKET_NAME = "dev"
