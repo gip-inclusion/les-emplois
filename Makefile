gip-inclusion/les-emplois

commit 2c77b028cf36ec6ad467ad1641b40f8bc90ddd7a

Makefile: add small message to help understant failures

Makefile
@@ -50,7 +50,7 @@ quality: $(VENV_REQUIREMENT)
 	ruff check $(LINTER_CHECKED_DIRS)
 	djlint --lint --check itou
 	find * -type f -name '*.sh' -exec shellcheck --external-sources {} +
-	python manage.py makemigrations --check --dry-run --noinput
+	python manage.py makemigrations --check --dry-run --noinput || echo "⚠ Missing migration ⚠"
 
 fix: $(VENV_REQUIREMENT)
 	black $(LINTER_CHECKED_DIRS)

commit 1bd1bfaf2df43f7833d1d79baeb1424b2a98d6b6

Makefile: fix migration check
broken in 2c77b028cf36ec6ad467ad1641b40f8bc90ddd7a

Otherwise the exit code is 0 (echo exit code) when a migration is missing

Makefile
@@ -50,7 +50,7 @@ quality: $(VENV_REQUIREMENT)
 	ruff check $(LINTER_CHECKED_DIRS)
 	djlint --lint --check itou
 	find * -type f -name '*.sh' -exec shellcheck --external-sources {} +
-	python manage.py makemigrations --check --dry-run --noinput || echo "⚠ Missing migration ⚠"
+	python manage.py makemigrations --check --dry-run --noinput || (echo "⚠ Missing migration ⚠"; exit 1)
 
 fix: $(VENV_REQUIREMENT)
 	black $(LINTER_CHECKED_DIRS)