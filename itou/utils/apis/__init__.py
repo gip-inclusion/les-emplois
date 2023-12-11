2bfed899068a4efb1bba71aafeed7dd24bc3a8f4
Céline MS <celine@hello-birds.com>
2023-12-11 14:51:00 +0100

    DO NOT SEND ME TO PROD

--- itou/utils/apis/__init__.py
+++ itou/utils/apis/__init__.py
@@ -3,6 +3,8 @@ from django.conf import settings
 from .pole_emploi import PoleEmploiApiClient
 
 
+# Values updated in review app's environment to communicate with
+# PE's pre-production servers.
 def pole_emploi_api_client():
     return PoleEmploiApiClient(
         settings.API_ESD["BASE_URL"],
