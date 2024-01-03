479c3880bf8a6172b5dbe1114dda94d62e8b57bb
Xavier Fernandez <xavier.fernandez@beta.gouv.fr>
2024-01-03 10:31:38 +0100

    utils: adapt DeprecatedLoggerMixin for LoggedCommandMixin

--- itou/utils/management_commands.py
+++ itou/utils/management_commands.py
@@ -17,12 +17,6 @@ class DeprecatedLoggerMixin:
         """
         Set logger level based on the verbosity option.
         """
-        handler = logging.StreamHandler(self.stdout)
-
-        self.logger = logging.getLogger(__name__)
-        self.logger.propagate = False
-        self.logger.addHandler(handler)
-
         self.logger.setLevel(logging.INFO)
         if verbosity is not None and verbosity >= 1:
             self.logger.setLevel(logging.DEBUG)
