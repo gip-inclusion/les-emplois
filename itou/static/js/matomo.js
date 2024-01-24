4a0e92ae77359302ad3ae2eb1a523203a369c919
vincent porte <vincent@neuralia.co>
2024-01-24 15:51:59 +0100

    (matomo) cleanup matomo.js since legacy usage becomes obsolete

--- itou/static/js/matomo.js
+++ itou/static/js/matomo.js
@@ -2,17 +2,14 @@ htmx.onLoad((target) => {
 
     /********************************************************************
        Send an event to Matomo on click.
-       Legacy usage:
-       <a href="#" class="matomo-event" data-matomo-category="MyCategory"
-       data-matomo-action="MyAction" data-matomo-option="MyOption" >
-       New usage:
-         <a href="#" data-matomo-event="true" data-matomo-category="MyCategory"
-            data-matomo-action="MyAction" data-matomo-option="MyOption" >
-        With matomo-attributes tag:
-         {% load matomo %}
+       Usage with matomo-attributes tag:
+            {% load matomo %}
             <a href="#" {% matomo_event "MyCategory" "MyAction" "MyOption" %} >
+        Converted by simpletag to:
+            <a href="#" data-matomo-event="true" data-matomo-category="MyCategory"
+            data-matomo-action="MyAction" data-matomo-option="MyOption" >
     ********************************************************************/
-    $('[data-matomo-event="true"], .matomo-event', target).on("click", function() {
+    $('data-matomo-event="true"', target).on("click", function() {
         var category = $(this).data("matomo-category");
         var action = $(this).data("matomo-action");
         var option = $(this).data("matomo-option");
