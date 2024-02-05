e85d8b9719ab8e8df1f17f03c6cb002437435c34
Romain Sébille <romain.sebille@beta.gouv.fr>
2024-02-05 14:03:05 +0100

    itou.tests: Add the `geopoint` formater to our custom Faker provider

--- itou/utils/faker_providers.py
+++ itou/utils/faker_providers.py
@@ -1,8 +1,15 @@
 import random
 
+from django.contrib.gis.geos import Point
 from faker.providers import BaseProvider
 
 
 class ItouProvider(BaseProvider):
+
     def asp_batch_filename(self) -> str:
         return f"RIAE_FS_{random.randint(0, 99999999999999)}.json"
+
+    def geopoint(self) -> Point:
+        return Point(
+            [float(coord) for coord in self.generator.format("local_latlng", country_code="FR", coords_only=True)]
+        )
