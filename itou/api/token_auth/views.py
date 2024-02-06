f780d20a64e29bc7d7628441a555125c9c072b3b
Xavier Fernandez <xavier.fernandez@beta.gouv.fr>
2024-02-06 10:10:42 +0100

    api: add log for invalid token case
    To help diagnose connection issues with API users

--- itou/api/token_auth/views.py
+++ itou/api/token_auth/views.py
@@ -1,7 +1,12 @@
+import logging
+
 from rest_framework import serializers
 from rest_framework.authtoken import views as drf_authtoken_views
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
+
+
+logger = logging.getLogger(__name__)
 
 
 TOKEN_ID_STR = "__token__"
@@ -10,11 +15,18 @@ TOKEN_ID_STR = "__token__"
 class ObtainAuthToken(drf_authtoken_views.ObtainAuthToken):
     def post(self, request, *args, **kwargs):
         if request.data.get("username") == TOKEN_ID_STR:
+            password = request.data.get("password")
             try:
-                token = Token.objects.get(key=request.data.get("password"))
+                token = Token.objects.get(key=password)
                 return Response({"token": token.key})
             except Token.DoesNotExist:
-                msg = "Impossible de se connecter avec le token fourni."
-                raise serializers.ValidationError(msg, code="authorization")
+                logger.info(
+                    "Auth with special user '%s' failed: unknown token received (len=%s)",
+                    TOKEN_ID_STR,
+                    len(password),
+                )
+                raise serializers.ValidationError(
+                    "Impossible de se connecter avec le token fourni.", code="authorization"
+                )
 
         return super().post(request, *args, **kwargs)
