dfc2f45a8255df05799f244a476adf288f9c3773
Xavier Fernandez <xavier.fernandez@beta.gouv.fr>
2024-02-13 12:20:37 +0100

    tests: always close export_pe_api_rejections workbook

--- tests/approvals/test_management_commands.py
+++ tests/approvals/test_management_commands.py
@@ -31,32 +31,35 @@ class ExportPEApiRejectionsTestCase(TestCase):
         )
         stdout = io.StringIO()
         management.call_command("export_pe_api_rejections", stdout=stdout, stderr=io.StringIO())
-        workbook = openpyxl.load_workbook("exports/2022-09-13-00-00-00-export_pe_api_rejections.xlsx")
-        assert [[cell.value or "" for cell in row] for row in workbook.active.rows] == [
-            [
-                "numero",
-                "date_notification",
-                "code_echec",
-                "nir",
-                "pole_emploi_id",
-                "nom_naissance",
-                "prenom",
-                "date_naissance",
-                "siae_type",
-                "siae_raison_sociale",
-                "siae_departement",
-            ],
-            [
-                approval.number,
-                "2022-08-31 00:00:00+00:00",
-                "FOOBAR",
-                approval.user.nir,
-                approval.user.jobseeker_profile.pole_emploi_id,
-                "Pers,e",
-                'Jul"ie',
-                str(approval.user.birthdate),
-                "EI",
-                "Ma petite entreprise",
-                "42",
-            ],
-        ]
+        workbook = openpyxl.load_workbook("exports/2022-09-13-00-00-00-export_pe_api_rejections.xlsx", read_only=True)
+        try:
+            assert [[cell.value or "" for cell in row] for row in workbook.active.rows] == [
+                [
+                    "numero",
+                    "date_notification",
+                    "code_echec",
+                    "nir",
+                    "pole_emploi_id",
+                    "nom_naissance",
+                    "prenom",
+                    "date_naissance",
+                    "siae_type",
+                    "siae_raison_sociale",
+                    "siae_departement",
+                ],
+                [
+                    approval.number,
+                    "2022-08-31 00:00:00+00:00",
+                    "FOOBAR",
+                    approval.user.nir,
+                    approval.user.jobseeker_profile.pole_emploi_id,
+                    "Pers,e",
+                    'Jul"ie',
+                    str(approval.user.birthdate),
+                    "EI",
+                    "Ma petite entreprise",
+                    "42",
+                ],
+            ]
+        finally:
+            workbook.close()
