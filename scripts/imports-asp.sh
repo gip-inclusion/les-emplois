fc4cfb6a4afade7f722523e9b3f4b23dca89c9bd
Romain Sébille <romain.sebille@beta.gouv.fr>
2024-02-27 14:35:07 +0100

    scripts: Fix the missing environment variable for `imports-asp.sh`
    Follow-up of d1ce32902 and f7b6f4400.

--- scripts/imports-asp.sh
+++ scripts/imports-asp.sh
@@ -46,8 +46,9 @@ mkdir -p $OUTPUT_PATH/populate_metabase_fluxiae
 mkdir -p $OUTPUT_PATH/import_siae
 mkdir -p $OUTPUT_PATH/import_ea_eatt
 
+export ASP_FLUX_IAE_DIR="$FLUX_IAE_DIR"
 time ./manage.py populate_metabase_fluxiae --verbosity 2 |& tee -a "$OUTPUT_PATH/populate_metabase_fluxiae/output_$(date '+%Y-%m-%d_%H-%M-%S').log"
-time ASP_FLUX_IAE_DIR="$FLUX_IAE_DIR" ./manage.py import_siae --wet-run --verbosity=2 |& tee -a "$OUTPUT_PATH/import_siae/output_$(date '+%Y-%m-%d_%H-%M-%S').log"
+time ./manage.py import_siae --wet-run --verbosity=2 |& tee -a "$OUTPUT_PATH/import_siae/output_$(date '+%Y-%m-%d_%H-%M-%S').log"
 time ./manage.py import_ea_eatt --wet-run --verbosity=2 |& tee -a "$OUTPUT_PATH/import_ea_eatt/output_$(date '+%Y-%m-%d_%H-%M-%S').log"
 
 # Destroy the cleartext ASP data
