7654dc169692b9eaddc089ec7a8a354e282a47cc
Céline MS <celine@hello-birds.com>
2024-03-04 14:05:18 +0100

    Remove onboarding from demo

--- itou/users/adapter.py
+++ itou/users/adapter.py
@@ -26,9 +26,7 @@ class UserAdapter(DefaultAccountAdapter):
 
     def get_login_redirect_url(self, request):
         url = reverse("dashboard:index")
-        # In demo, false accounts are used by many different persons but never recreated.
-        # The welcoming tour should show up anyway.
-        if not request.user.has_completed_welcoming_tour or settings.ITOU_ENVIRONMENT == "DEMO":
+        if not request.user.has_completed_welcoming_tour:
             url = reverse("welcoming_tour:index")
         return url
 
