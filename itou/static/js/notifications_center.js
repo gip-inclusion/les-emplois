7dda2fbb9d58a28e72442b5e725ff9b5225f1fb3
François Freitag <mail@franek.fr>
2024-03-12 14:33:56 +0100

    Simplify the JS a bit and handle a couple odd behaviors
    Quelques comportements bizarres :
    
    STR 1:
    1. Sur une page avec plusieurs groupes, toutes les notifications sont actives, tous les groupes sont collapsed
    1. expand le premier groupe
    2. Cliquer sur “Activer toutes les notifications”
    
    Le premier groupe se referme, les autres s’ouvrent.
    
    STR 2 :
    
    1. Sur une page avec plusieurs groupes, toutes les notifications sont actives, tous les groupes sont collapsed
    2. Expand le premier groupe
    3. Cliquer sur la première notification du premier groupe
    4. Tous les groupes s’ouvrent

--- itou/static/js/notifications_center.js
+++ itou/static/js/notifications_center.js
@@ -1,134 +1,60 @@
 htmx.onLoad((target) => {
-  /**
-   * JS to handle checkboxes
-   **/
+  const allNotifications = document.getElementById("id_notifications-all");
+  const form = allNotifications.closest("form");
+  updateAllNotifications();
 
-  let notificationGroups = [];
-  let notificationsContainer = document.querySelector("#id_notifications");
-  let mainNotificationsInputElement = document.querySelector("#id_notifications-all");
+  function childrenNotificationsSelected(container) {
+    return !container.querySelectorAll("input.notification-checkbox:not(:checked)").length;
+  }
 
-  handleMainNotificationsInputChange = function () {
-    notificationsContainer.querySelectorAll("input.wrapping-checkbox").forEach((input) => {
-      input.checked = mainNotificationsInputElement.checked;
-    });
+  function updateAllNotifications() {
+    allNotifications.checked = childrenNotificationsSelected(form);
+  }
 
-    notificationsContainer.querySelectorAll("input.notification-checkbox").forEach((input) => {
-      input.checked = mainNotificationsInputElement.checked;
-    });
-
-    notificationGroups.forEach(function (group) {
-      group.hide() ? mainNotificationsInputElement.checked : group.show();
-    });
-  };
-
-  checkAllChecked = function () {
-    mainNotificationsInputElement.checked = Boolean(
-      !notificationsContainer.querySelectorAll("input.notification-checkbox:not(:checked)").length
-    );
-
-    if (!mainNotificationsInputElement.checked) {
-      notificationGroups.forEach((group) => {
-        group.show();
-      });
-    }
-  };
-
-  class NotificationGroup {
-    constructor(container) {
-      this.container = container;
-      this.titleElement = this.container.firstElementChild;
-      this.collapsibleElement = this.titleElement.nextElementSibling;
-      this.allNotificationsInputElement = this.collapsibleElement.querySelector("input.wrapping-checkbox");
-      this.notificationsInputElements = this.collapsibleElement.querySelectorAll(
-        ".category-notifications input.notification-checkbox"
-      );
-
-      this.allNotificationsInputElement.checked = this.isAllChecked();
-
-      this.collapseInstance = bootstrap.Collapse.getOrCreateInstance(this.collapsibleElement, {
-        toggle: false,
-      });
-
-      this.titleElement.addEventListener("click", () => {
-        this.toggle();
-      });
-
-      this.allNotificationsInputElement.addEventListener("click", () => {
-        this.onAllClick();
-      });
-
-      this.notificationsInputElements.forEach((input) => {
-        input.addEventListener("click", () => {
-          this.onIndividualClick(input);
-        });
-      });
-
-      this.collapsibleElement.addEventListener("hide.bs.collapse", () => {
-        this.onHide();
-      });
-
-      this.collapsibleElement.addEventListener("show.bs.collapse", () => {
-        this.onShow();
-      });
-    }
-
-    show() {
-      this.collapseInstance.show();
-    }
-
-    hide() {
-      this.collapseInstance.hide();
-    }
-
-    toggle() {
-      this.collapseInstance.toggle();
-    }
-
-    isAllChecked() {
-      for (let input of this.notificationsInputElements) {
-        if (!input.checked) {
-          return false;
-        }
-      }
-      return true;
-    }
-
-    checkAllChecked() {
-      this.allNotificationsInputElement.checked = this.isAllChecked();
-      checkAllChecked();
-    }
-
-    onShow() {
-      this.titleElement.setAttribute("aria-expanded", true);
-    }
-
-    onHide() {
-      this.titleElement.setAttribute("aria-expanded", false);
-    }
-
-    onAllClick() {
-      this.notificationsInputElements.forEach((input) => {
-        input.checked = this.allNotificationsInputElement.checked;
-      });
-      if (this.allNotificationsInputElement.checked) {
-        checkAllChecked();
-      } else {
-        mainNotificationsInputElement.checked = false;
-      }
-    }
-
-    onIndividualClick(input) {
-      this.checkAllChecked();
+  function toggleCollapse(collapse, force) {
+    const collapseController = bootstrap.Collapse.getOrCreateInstance(collapse, {toggle: false});
+    if (force === undefined) {
+      collapseController.toggle();
+    } else if (force === true) {
+      collapseController.show();
+    } else {
+      throw new Error("Not implemented");
     }
   }
 
-  target.querySelectorAll(".notification-collapse").forEach((element) => {
-    notificationGroups.push(new NotificationGroup(element));
+  form.addEventListener("change", (event) => {
+    if (event.target === allNotifications) {
+      form.querySelectorAll("input[type=checkbox]").forEach((input) => {
+        input.checked = event.target.checked;
+      });
+      if (!event.target.checked) {
+        form.querySelectorAll(".collapse").forEach(collapse => toggleCollapse(collapse, true));
+      }
+    } else if (event.target.classList.contains("category-grouper")) {
+      const categoryGroup = event.target.closest(".notification-collapse");
+      categoryGroup.querySelectorAll("input.notification-checkbox").forEach(notification => {
+        notification.checked = event.target.checked;
+      });
+    }
+
+    updateAllNotifications();
+    const collapseGroup = event.target.closest(".notification-collapse");
+    if (collapseGroup) {
+      collapseGroup.querySelector(".category-grouper").checked = childrenNotificationsSelected(collapseGroup);
+    }
   });
 
-  mainNotificationsInputElement.addEventListener("change", () => {
-    handleMainNotificationsInputChange();
+  document.getElementsByClassName("notification-collapse").forEach((collapseGroup) => {
+    const collapseToggle = collapseGroup.querySelector("a[aria-controls]");
+    collapseToggle.addEventListener("click", (event) => {
+      toggleCollapse(collapseGroup.querySelector(".collapse"));
+    });
+    const collapsibleElement = collapseGroup.querySelector(".collapse");
+    collapsibleElement.addEventListener("hide.bs.collapse", () => {
+      collapseToggle.ariaExpanded = "false";
+    });
+    collapsibleElement.addEventListener("show.bs.collapse", () => {
+      collapseToggle.ariaExpanded = "true";
+    });
   });
-
-  checkAllChecked();
 });
