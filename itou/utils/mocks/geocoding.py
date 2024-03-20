08ba17a73cd7a0f96d1d32c13ffc826d6debdde8
Romain Sébille <romain.sebille@beta.gouv.fr>
2024-03-20 11:37:41 +0100

    tests.apis.geocoding: Use `respx` instead of `mock`
    - Move to pytest
    - Update results to the current API response
    - Harden tests to avoid passing because of undefined `API_BAN_BASE_URL`

--- itou/utils/mocks/geocoding.py
+++ itou/utils/mocks/geocoding.py
@@ -1,37 +1,40 @@
-"""
-Result for a call to:
-https://api-adresse.data.gouv.fr/search/?q=10+PL+5+MARTYRS+LYCEE+BUFFON&limit=1&postcode=75015
-"""
-
+# https://api-adresse.data.gouv.fr/search/?q=10+PL+5+MARTYRS+LYCEE+BUFFON&limit=1&postcode=75015
 BAN_GEOCODING_API_RESULT_MOCK = {
     "type": "Feature",
     "geometry": {"type": "Point", "coordinates": [2.316754, 48.838411]},
     "properties": {
-        "label": "10 Pl des Cinq Martyrs du Lycee Buffon 75015 Paris",
-        "score": 0.587663373207207,
+        "label": "10 Place des Cinq Martyrs du Lycée Buffon 75015 Paris",
+        "score": 0.5197687103594081,
         "housenumber": "10",
         "id": "75115_2048_00010",
-        "type": "housenumber",
-        "name": "10 Pl des Cinq Martyrs du Lycee Buffon",
+        "name": "10 Place des Cinq Martyrs du Lycée Buffon",
         "postcode": "75015",
         "citycode": "75115",
         "x": 649850.81,
-        "y": 6860034.74,
+        "y": 6860034.75,
         "city": "Paris",
         "district": "Paris 15e Arrondissement",
         "context": "75, Paris, Île-de-France",
-        "importance": 0.6950663360485076,
-        "street": "Pl des Cinq Martyrs du Lycee Buffon",
+        "type": "housenumber",
+        "importance": 0.6942,
+        "street": "Place des Cinq Martyrs du Lycée Buffon",
     },
 }
+BAN_GEOCODING_API_WITH_RESULT_RESPONSE = {
+    "type": "FeatureCollection",
+    "version": "draft",
+    "features": [BAN_GEOCODING_API_RESULT_MOCK],
+    "attribution": "BAN",
+    "licence": "ETALAB-2.0",
+    "query": "10 PL 5 MARTYRS LYCEE BUFFON",
+    "filters": {"postcode": "75015"},
+    "limit": 1,
+}
 
-"""
-Result for a call to:
-https://api-adresse.data.gouv.fr/search/?q=10+PL+5+ANATOLE&limit=1&postcode=75010
-"""
-
+# https://api-adresse.data.gouv.fr/search/?q=10+PL+5+ANATOLE&limit=1&postcode=75010
 BAN_GEOCODING_API_NO_RESULT_MOCK = {
     "type": "FeatureCollection",
+    "version": "draft",
     "features": [],
     "attribution": "BAN",
     "licence": "ETALAB-2.0",
--- tests/utils/apis/test_geocoding_api.py
+++ tests/utils/apis/test_geocoding_api.py
@@ -1,38 +1,37 @@
-from unittest import mock
-
 import pytest
 from django.contrib.gis.geos import GEOSGeometry
 
+from itou.utils.apis import geocoding
 from itou.utils.apis.exceptions import GeocodingDataError
-from itou.utils.apis.geocoding import get_geocoding_data
-from itou.utils.mocks.geocoding import BAN_GEOCODING_API_NO_RESULT_MOCK, BAN_GEOCODING_API_RESULT_MOCK
-from tests.utils.test import TestCase
+from itou.utils.mocks.geocoding import BAN_GEOCODING_API_NO_RESULT_MOCK, BAN_GEOCODING_API_WITH_RESULT_RESPONSE
 
 
-class UtilsGeocodingTest(TestCase):
-    @mock.patch("itou.utils.apis.geocoding.call_ban_geocoding_api", return_value=BAN_GEOCODING_API_RESULT_MOCK)
-    def test_get_geocoding_data(self, mock_call_ban_geocoding_api):
-        geocoding_data = mock_call_ban_geocoding_api()
-        result = get_geocoding_data(geocoding_data)
-        # Expected data comes from BAN_GEOCODING_API_RESULT_MOCK.
-        expected = {
-            "score": 0.587663373207207,
-            "address_line_1": "10 Pl des Cinq Martyrs du Lycee Buffon",
-            "number": "10",
-            "lane": "Pl des Cinq Martyrs du Lycee Buffon",
-            "address": "10 Pl des Cinq Martyrs du Lycee Buffon",
-            "post_code": "75015",
-            "insee_code": "75115",
-            "city": "Paris",
-            "longitude": 2.316754,
-            "latitude": 48.838411,
-            "coords": GEOSGeometry("POINT(2.316754 48.838411)"),
-        }
-        assert result == expected
+def test_get_geocoding_data(caplog, snapshot, respx_mock, settings):
+    settings.API_BAN_BASE_URL = "https://geo.foo"
+    respx_mock.get(f"{settings.API_BAN_BASE_URL}/search/").respond(200, json=BAN_GEOCODING_API_WITH_RESULT_RESPONSE)
 
-    @mock.patch("itou.utils.apis.geocoding.call_ban_geocoding_api", return_value=BAN_GEOCODING_API_NO_RESULT_MOCK)
-    def test_get_geocoding_data_error(self, mock_call_ban_geocoding_api):
-        geocoding_data = mock_call_ban_geocoding_api()
+    result = geocoding.get_geocoding_data("")
+    # Expected data comes from BAN_GEOCODING_API_RESULT_MOCK.
+    assert result == {
+        "score": 0.5197687103594081,
+        "address_line_1": "10 Place des Cinq Martyrs du Lycée Buffon",
+        "number": "10",
+        "lane": "Place des Cinq Martyrs du Lycée Buffon",
+        "address": "10 Place des Cinq Martyrs du Lycée Buffon",
+        "post_code": "75015",
+        "insee_code": "75115",
+        "city": "Paris",
+        "longitude": 2.316754,
+        "latitude": 48.838411,
+        "coords": GEOSGeometry("POINT(2.316754 48.838411)"),
+    }
+    assert [record for record in caplog.record_tuples if record[0] == geocoding.__name__] == snapshot
 
-        with pytest.raises(GeocodingDataError):
-            get_geocoding_data(geocoding_data)
+
+def test_get_geocoding_data_error(caplog, snapshot, respx_mock, settings):
+    settings.API_BAN_BASE_URL = "https://geo.foo"
+    respx_mock.get(f"{settings.API_BAN_BASE_URL}/search/").respond(200, json=BAN_GEOCODING_API_NO_RESULT_MOCK)
+
+    with pytest.raises(GeocodingDataError):
+        geocoding.get_geocoding_data("")
+    assert [record for record in caplog.record_tuples if record[0] == geocoding.__name__] == snapshot
