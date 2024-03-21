6142dbe225ec396e063abe70a6394254bf81129b
François Freitag <mail@franek.fr>
2024-03-21 11:49:04 +0100

    Increase rate limits for employee_records API
    Rate limits started to be enforced correctly with
    9a6c142ccb6834fad7ef7f70d5e5e8246d31f7c4.
    
    Users complain that the limit of 12 requests per minute is too low.
    Historically, some clients were consuming the API very quickly, leading
    to significant slowdowns. Tentatively set a higher limit, but stay below
    the site-wide limits for APIs.
    
    https://itou-inclusion.slack.com/archives/C0412CTV63D/p1711018102580339

--- tests/api/employee_record_api/test_throttling.py
+++ tests/api/employee_record_api/test_throttling.py
@@ -1,43 +1,19 @@
 from django.urls import reverse
 from rest_framework.test import APIClient, APITestCase
 
-from itou.api.employee_record_api.viewsets import EmployeeRecordRateThrottle
 from tests.companies.factories import CompanyFactory
-from tests.users.factories import DEFAULT_PASSWORD
-
-
-ENDPOINT_URL = reverse("v1:employee-records-list")
 
 
 class EmployeeRecordThrottleTest(APITestCase):
-    # This a simple smoke test, goal is *not* testing DRF throttling
-
-    def test_basic_ko_throttling(self):
+    def test_throttling(self):
         client = APIClient()
         user = CompanyFactory(with_membership=True).members.first()
-
-        client.login(username=user.username, password=DEFAULT_PASSWORD)
-
-        response = client.get(ENDPOINT_URL, format="json")
-        assert response.status_code == 200
-
+        client.force_authenticate(user)
+        url = reverse("v1:employee-records-list")
+        # EmployeeRecordRateThrottle.rate
+        for _ in range(60):
+            response = client.get(url, format="json")
+            assert response.status_code == 200
         # Too fast, Jacky, one too many
-        for _ in range(EmployeeRecordRateThrottle.EMPLOYEE_RECORD_API_REQUESTS_NUMBER):
-            response = client.get(ENDPOINT_URL, format="json")
-
+        response = client.get(url, format="json")
         assert response.status_code == 429
-
-    def test_basic_ok_throttling(self):
-        client = APIClient()
-        user = CompanyFactory(with_membership=True).members.first()
-
-        client.login(username=user.username, password=DEFAULT_PASSWORD)
-
-        response = client.get(ENDPOINT_URL, format="json")
-        assert response.status_code == 200
-
-        # Should hold it : a total of EMPLOYEE_RECORD_API_REQUESTS_NUMBER is sent
-        for _ in range(EmployeeRecordRateThrottle.EMPLOYEE_RECORD_API_REQUESTS_NUMBER - 1):
-            response = client.get(ENDPOINT_URL, format="json")
-
-        assert response.status_code == 200
