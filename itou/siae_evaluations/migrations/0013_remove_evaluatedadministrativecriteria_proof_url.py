387e3e1fdd3f6ba109a9e3bc2555dd2e2268ed21
Antoine LAURENT <antoine.c.laurent.pro@gmail.com>
2024-03-22 11:19:56 +0100

    siae_evaluations: Squash migrations

--- itou/siae_evaluations/migrations/0013_remove_evaluatedadministrativecriteria_proof_url.py
+++
@@ -1,9 +0,0 @@
-from django.db import migrations
-
-
-class Migration(migrations.Migration):
-    dependencies = [
-        ("siae_evaluations", "0012_evaluatedadministrativecriteria_proof_url_to_proof"),
-    ]
-
-    operations = []
