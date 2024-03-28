f88970bb08c94c68d5dab9ea6303ad42eb889de5
vincent porte <vincent@neuralia.co>
2024-03-28 18:26:32 +0100

    let modify object in debug mode for dev purpose

--- itou/premium/admin.py
+++ itou/premium/admin.py
@@ -1,18 +1,49 @@
+from django.conf import settings
 from django.contrib import admin
 
-from itou.premium.models import Note
+from itou.premium.models import Customer, Note, SyncedJobApplication
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ("company", "end_subscription_date", "last_synced_at")
+    raw_id_fields = ("company",)
+
+    def has_add_permission(self, request):
+        return settings.DEBUG
+
+    def has_change_permission(self, request, obj=None):
+        return settings.DEBUG
+
+    def has_delete_permission(self, request, obj=None):
+        return settings.DEBUG
+
+
+@admin.register(SyncedJobApplication)
+class SyncedJobApplicationAdmin(admin.ModelAdmin):
+    list_display = ("customer", "job_application")
+    raw_id_fields = ("customer", "job_application")
+
+    def has_add_permission(self, request):
+        return settings.DEBUG
+
+    def has_change_permission(self, request, obj=None):
+        return settings.DEBUG
+
+    def has_delete_permission(self, request, obj=None):
+        return settings.DEBUG
 
 
 @admin.register(Note)
 class NoteAdmin(admin.ModelAdmin):
-    list_display = ("job_application", "created_by")
-    raw_id_fields = ("job_application", "created_by", "updated_by")
+    list_display = ("synced_job_application", "created_by")
+    raw_id_fields = ("synced_job_application", "created_by", "updated_by")
 
     def has_add_permission(self, request):
-        return False
+        return settings.DEBUG
 
     def has_change_permission(self, request, obj=None):
-        return False
+        return settings.DEBUG
 
     def has_delete_permission(self, request, obj=None):
-        return False
+        return settings.DEBUG
