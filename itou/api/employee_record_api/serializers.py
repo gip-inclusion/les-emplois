519bf60e4896ba77092e24150d13cb548262496f
Romain Sébille <romain.sebille@beta.gouv.fr>
2024-03-28 10:59:38 +0100

    api.employee_record: Copy serializers from `employee_record`
    API serializers are mostly the same as the ones used for transfers, only
    some fields are added or modified to enrich the API.
    I choose to duplicate everything because:
    - We want to make some changes when serializing for transfers but not
      break the API.
    - We already don't return the same things in the API so we could remove
      more transfers rules and hacks
    - We rarely ever touch those files, so it's probably not too PITA to maintain and
      we can always refactor later

--- itou/api/employee_record_api/serializers.py
+++ itou/api/employee_record_api/serializers.py
@@ -1,15 +1,25 @@
+import re
+
 from rest_framework import serializers
+from unidecode import unidecode
 
-from itou.employee_record.serializers import EmployeeRecordSerializer, _AddressSerializer, _PersonSerializer
+from itou.employee_record.models import EmployeeRecord
+from itou.employee_record.typing import CodeComInsee
+from itou.users.enums import Title
+from itou.users.models import User
+from itou.utils.serializers import NullField, NullIfEmptyCharField
 
 
-# Employee record serializer is mostly the same as the one used
-# for serialization transfers.
-# Except some fields are "unobfuscated" and added for third-party
-# software connecting to the API.
+# Employee record serializers are mostly the same as the ones used
+# for serialization transfers, except some fields are "unobfuscated"
+# and added for third-party software connecting to the API.
+# For now, we are going to duplicate everything:
+#   - We want to make some changes when serializing for transfer but not break the API
+#   - We could then remove some transfert specific formatting rules
+#   - We rarely ever touch those, so probably not too PITA to maintain, and we can refactor later
 
 
-class _API_AddressSerializer(_AddressSerializer):
+class _API_AddressSerializer(serializers.Serializer):
     """
     This class in only useful for compatibility.
     We decided not to send phone and email (business concerns and bad ASP address filters).
@@ -20,16 +30,128 @@ class _API_AddressSerializer(_AddressSerializer):
     adrTelephone = serializers.CharField(source="phone")
     adrMail = serializers.CharField(source="email")
 
+    adrNumeroVoie = serializers.CharField(source="jobseeker_profile.hexa_lane_number")
+    codeextensionvoie = NullIfEmptyCharField(source="jobseeker_profile.hexa_std_extension", allow_blank=True)
+    codetypevoie = serializers.CharField(source="jobseeker_profile.hexa_lane_type")
 
-class _API_PersonSerializer(_PersonSerializer):
-    """
-    Specific fields added to the API (not used in ASP transfers)
-    """
+    adrLibelleVoie = serializers.SerializerMethodField()
+    adrCpltDistribution = serializers.SerializerMethodField()
 
+    codeinseecom = serializers.CharField(source="jobseeker_profile.hexa_commune.code", allow_null=True)
+    codepostalcedex = serializers.CharField(source="jobseeker_profile.hexa_post_code")
+
+    def get_adrCpltDistribution(self, obj: User) -> str | None:
+        # Don't send extended address if it must be truncated:
+        # Do not lower quality of data on 'itou' side
+        # Check ASP rule : T030_c026_rg002
+        # This rule is badly written, and innacurate (regarding special characters).
+        # Follows the acceptable format / RE for this field (now validated by ASP).
+        additional_address = obj.jobseeker_profile.hexa_additional_address
+        if not additional_address:
+            # Force empty string to be rendered as `null`
+            return None
+        if additional_address and not re.match("^[a-zA-Z0-9@ ]{,32}$", additional_address):
+            return None
+        return additional_address
+
+    def get_adrLibelleVoie(self, obj: User) -> str | None:
+        # Remove diacritics and parenthesis from adrLibelleVoie field fixes ASP error 3330
+        # (parenthesis are not described as invalid characters in specification document).
+        lane = obj.jobseeker_profile.hexa_lane_name
+        if lane:
+            return unidecode(lane.translate({ord(ch): "" for ch in "()"}))
+        return None
+
+
+class _API_PersonSerializer(serializers.Serializer):
+    # Specific field added to the API (not used in ASP transfers)
     NIR = serializers.CharField(source="job_seeker.jobseeker_profile.nir")
 
+    passIae = serializers.CharField(source="approval_number")
+    sufPassIae = NullField()
+    idItou = serializers.CharField(source="job_seeker.jobseeker_profile.asp_uid")
 
-class EmployeeRecordAPISerializer(EmployeeRecordSerializer):
+    civilite = serializers.ChoiceField(choices=Title.choices, source="job_seeker.title")
+    nomUsage = serializers.SerializerMethodField()
+    nomNaissance = NullField()
+    prenom = serializers.SerializerMethodField()
+    dateNaissance = serializers.DateField(format="%d/%m/%Y", source="job_seeker.birthdate")
+
+    codeDpt = serializers.CharField(source="job_seeker.birth_place.department_code", required=False)
+    codeInseePays = serializers.CharField(source="job_seeker.jobseeker_profile.birth_country.code", allow_null=True)
+    codeGroupePays = serializers.CharField(source="job_seeker.jobseeker_profile.birth_country.group", allow_null=True)
+
+    # codeComInsee is only mandatory if birth country is France
+    codeComInsee = serializers.SerializerMethodField(required=False)
+
+    passDateDeb = serializers.DateField(format="%d/%m/%Y", source="approval.start_at")
+    passDateFin = serializers.DateField(format="%d/%m/%Y", source="approval.end_at")
+
+    def get_nomUsage(self, obj: EmployeeRecord) -> str:
+        return unidecode(obj.job_seeker.last_name).upper()
+
+    def get_prenom(self, obj: EmployeeRecord) -> str:
+        return unidecode(obj.job_seeker.first_name).upper()
+
+    def get_codeComInsee(self, obj: EmployeeRecord) -> CodeComInsee:
+        # Another ASP subtlety, making top-level and children with the same name
+        # The commune can be empty if the job seeker is not born in France
+        if birth_place := obj.job_seeker.jobseeker_profile.birth_place:
+            return {
+                "codeComInsee": birth_place.code,
+                "codeDpt": birth_place.department_code,
+            }
+
+        # However, if the employee is not born in France
+        # the department code must be '099' (error 3411)
+        return {
+            "codeComInsee": None,
+            "codeDpt": "099",
+        }
+
+
+class _API_SituationSerializer(serializers.Serializer):
+    niveauFormation = serializers.CharField(source="job_seeker.jobseeker_profile.education_level")
+    salarieEnEmploi = serializers.BooleanField(source="job_seeker.jobseeker_profile.is_employed")
+
+    salarieSansEmploiDepuis = NullIfEmptyCharField(source="job_seeker.jobseeker_profile.unemployed_since")
+    salarieSansRessource = serializers.BooleanField(source="job_seeker.jobseeker_profile.resourceless")
+
+    inscritPoleEmploi = serializers.BooleanField(source="job_seeker.jobseeker_profile.pole_emploi_id")
+    inscritPoleEmploiDepuis = NullIfEmptyCharField(source="job_seeker.jobseeker_profile.pole_emploi_since")
+    numeroIDE = serializers.CharField(source="job_seeker.jobseeker_profile.pole_emploi_id")
+
+    salarieRQTH = serializers.BooleanField(source="job_seeker.jobseeker_profile.rqth_employee")
+    salarieOETH = serializers.BooleanField(source="asp_oeth_employee")
+    salarieAideSociale = serializers.BooleanField(source="job_seeker.jobseeker_profile.has_social_allowance")
+
+    salarieBenefRSA = serializers.CharField(source="job_seeker.jobseeker_profile.has_rsa_allocation")
+    salarieBenefRSADepuis = NullIfEmptyCharField(
+        source="job_seeker.jobseeker_profile.rsa_allocation_since", allow_blank=True
+    )
+
+    salarieBenefASS = serializers.BooleanField(source="job_seeker.jobseeker_profile.has_ass_allocation")
+    salarieBenefASSDepuis = NullIfEmptyCharField(
+        source="job_seeker.jobseeker_profile.ass_allocation_since", allow_blank=True
+    )
+
+    salarieBenefAAH = serializers.BooleanField(source="job_seeker.jobseeker_profile.has_aah_allocation")
+    salarieBenefAAHDepuis = NullIfEmptyCharField(
+        source="job_seeker.jobseeker_profile.aah_allocation_since", allow_blank=True
+    )
+
+    salarieBenefATA = serializers.BooleanField(source="job_seeker.jobseeker_profile.has_ata_allocation")
+    salarieBenefATADepuis = NullIfEmptyCharField(
+        source="job_seeker.jobseeker_profile.ata_allocation_since", allow_blank=True
+    )
+
+    # There is a clear lack of knowledge of ASP business rules on this point.
+    # Without any satisfactory answer, it has been decided to obfuscate / mock these fields.
+    salarieTypeEmployeur = serializers.CharField(source="asp_employer_type", required=False)
+    orienteur = serializers.CharField(source="asp_prescriber_type", required=False)
+
+
+class EmployeeRecordAPISerializer(serializers.Serializer):
     """
     This serializer is a version with the `numeroAnnexe` field added (financial annex number).
 
@@ -37,6 +159,33 @@ class EmployeeRecordAPISerializer(EmployeeRecordSerializer):
     main SFTP serializer but was removed for RGPD concerns.
     """
 
-    numeroAnnexe = serializers.CharField(source="financial_annex_number")
-    adresse = _API_AddressSerializer(source="job_seeker")
+    numLigne = serializers.IntegerField(source="asp_batch_line_number")
+    typeMouvement = serializers.CharField(source="ASP_MOVEMENT_TYPE")
+    siret = serializers.CharField()
+    mesure = serializers.CharField(source="asp_siae_type")
+
+    # See : http://www.tomchristie.com/rest-framework-2-docs/api-guide/fields
     personnePhysique = _API_PersonSerializer(source="*")
+    adresse = _API_AddressSerializer(source="job_seeker")
+    situationSalarie = _API_SituationSerializer(source="*")
+
+    # These fields are null at the beginning of the ASP processing
+    codeTraitement = serializers.CharField(source="asp_processing_code", allow_blank=True)
+    libelleTraitement = serializers.CharField(source="asp_processing_label", allow_blank=True)
+
+    numeroAnnexe = serializers.CharField(source="financial_annex_number")
+
+
+class EmployeeRecordUpdateNotificationAPISerializer(serializers.Serializer):
+    numLigne = serializers.IntegerField(source="asp_batch_line_number")
+    typeMouvement = serializers.CharField(source="ASP_MOVEMENT_TYPE")
+    siret = serializers.CharField(source="employee_record.siret")
+    mesure = serializers.CharField(source="employee_record.asp_siae_type")
+
+    personnePhysique = _API_PersonSerializer(source="employee_record")
+    adresse = _API_AddressSerializer(source="employee_record.job_seeker")
+    situationSalarie = _API_SituationSerializer(source="employee_record")
+
+    # These fields are null at the beginning of the ASP processing
+    codeTraitement = serializers.CharField(source="asp_processing_code", allow_blank=True)
+    libelleTraitement = serializers.CharField(source="asp_processing_label", allow_blank=True)
---
+++ tests/api/employee_record_api/test_serializers.py
@@ -0,0 +1,148 @@
+from datetime import timedelta
+
+import pytest
+from django.utils import timezone
+
+from itou.api.employee_record_api.serializers import (
+    EmployeeRecordAPISerializer,
+    EmployeeRecordUpdateNotificationAPISerializer,
+    _API_AddressSerializer,
+    _API_PersonSerializer,
+)
+from itou.asp.models import SiaeMeasure
+from itou.companies.enums import CompanyKind
+from itou.companies.models import Company
+from itou.employee_record.enums import Status
+from itou.employee_record.models import EmployeeRecordUpdateNotification
+from tests.employee_record.factories import EmployeeRecordFactory, EmployeeRecordWithProfileFactory
+from tests.users.factories import JobSeekerFactory
+
+
+def test_address_serializer_hexa_additional_address():
+    # If additional address contains special characters or is more than 32 characters long
+    # then resulting additional address becomes `None`
+    employee_record = EmployeeRecordWithProfileFactory(status=Status.PROCESSED)
+    job_seeker = employee_record.job_seeker
+    profile = job_seeker.jobseeker_profile
+    profile.hexa_additional_address = "Bad additional address with %$£"
+
+    serializer = _API_AddressSerializer(job_seeker)
+    data = serializer.data
+
+    assert data is not None
+    assert data["adrCpltDistribution"] is None
+
+    profile.hexa_additional_address = "Bad additional address because it is really over 32 characters"
+    serializer = _API_AddressSerializer(job_seeker)
+    data = serializer.data
+
+    assert data is not None
+    assert data["adrCpltDistribution"] is None
+
+    good_address = "Good additional address"
+    profile.hexa_additional_address = good_address
+
+    serializer = _API_AddressSerializer(job_seeker)
+    data = serializer.data
+
+    assert data is not None
+    assert good_address == data["adrCpltDistribution"]
+
+
+def test_address_serializer_hexa_lane_name():
+    # If lane name contains parens,
+    # then remove them from resulting lane name
+    # (better geolocation)
+    employee_record = EmployeeRecordWithProfileFactory(status=Status.PROCESSED)
+    job_seeker = employee_record.job_seeker
+    profile = job_seeker.jobseeker_profile
+    profile.hexa_lane_name = "Lane name (with parens)"
+
+    serializer = _API_AddressSerializer(job_seeker)
+    data = serializer.data
+
+    assert data is not None
+    assert "Lane name with parens" == data["adrLibelleVoie"]
+
+    good_lane_name = "Lane name without parens"
+    profile.hexa_lane_name = good_lane_name
+    serializer = _API_AddressSerializer(job_seeker)
+    data = serializer.data
+
+    assert data is not None
+    assert good_lane_name == data["adrLibelleVoie"]
+
+
+def test_address_serializer_with_empty_fields():
+    serializer = _API_AddressSerializer(JobSeekerFactory(phone="", email=None))
+
+    assert serializer.data == {
+        "adrTelephone": "",
+        "adrMail": None,
+        "adrNumeroVoie": "",
+        "codeextensionvoie": None,
+        "codetypevoie": "",
+        "adrLibelleVoie": None,
+        "adrCpltDistribution": None,
+        "codeinseecom": None,
+        "codepostalcedex": "",
+    }
+
+
+def test_person_serializer_with_empty_birth_country():
+    serializer = _API_PersonSerializer(
+        EmployeeRecordFactory(job_application__job_seeker__jobseeker_profile__birth_country=None)
+    )
+
+    assert serializer.data["codeInseePays"] is None
+    assert serializer.data["codeGroupePays"] is None
+
+
+def test_oeth_employee_for_eiti():
+    employee_record = EmployeeRecordWithProfileFactory(
+        status=Status.PROCESSED,
+        job_application__to_company__kind=CompanyKind.EITI,
+    )
+    employee_record.job_application.job_seeker.jobseeker_profile.oeth_employee = True
+    data = EmployeeRecordAPISerializer(employee_record).data
+
+    assert data["mesure"] == "EITI_DC"
+    assert data["situationSalarie"]["salarieOETH"] is False
+
+
+@pytest.mark.parametrize("kind", set(Company.ASP_EMPLOYEE_RECORD_KINDS) - {CompanyKind.EITI})
+def test_oeth_employee_for_non_eiti(kind):
+    employee_record = EmployeeRecordWithProfileFactory(
+        status=Status.PROCESSED,
+        job_application__to_company__kind=kind,
+    )
+    employee_record.job_application.job_seeker.jobseeker_profile.oeth_employee = True
+    data = EmployeeRecordAPISerializer(employee_record).data
+
+    assert data["mesure"] == SiaeMeasure.from_siae_kind(kind)
+    assert data["situationSalarie"]["salarieOETH"] is True
+
+
+def test_notification_serializer():
+    # High-level: check basic information
+    start_at = timezone.localdate()
+    end_at = timezone.localdate() + timedelta(weeks=52)
+    employee_record = EmployeeRecordWithProfileFactory(status=Status.PROCESSED)
+    approval = employee_record.approval
+    approval.start_at = start_at
+    approval.end_at = end_at
+    employee_record.save()
+
+    data = EmployeeRecordUpdateNotificationAPISerializer(
+        EmployeeRecordUpdateNotification(employee_record=employee_record)
+    ).data
+    assert data is not None
+    assert data.get("siret") == employee_record.siret
+    assert data.get("mesure") == employee_record.asp_siae_type
+    assert data.get("typeMouvement") == EmployeeRecordUpdateNotification.ASP_MOVEMENT_TYPE
+
+    personnal_data = data.get("personnePhysique")
+    assert personnal_data is not None
+    assert personnal_data.get("passIae") == employee_record.approval_number
+    assert personnal_data.get("passDateDeb") == start_at.strftime("%d/%m/%Y")
+    assert personnal_data.get("passDateFin") == end_at.strftime("%d/%m/%Y")
