1ea82e39f35d8541b54cf80ac1c73d26217a14ee
vincent porte <vincent@neuralia.co>
2024-03-29 10:52:30 +0100

    (premium) add www premium views and templates

--- itou/www/premium_views/urls.py
+++ itou/www/premium_views/urls.py
@@ -1,4 +1,13 @@
+from django.urls import path
+
+from itou.www.premium_views.views import RefreshSyncJobApplicationView, SaveNoteView, SyncJobApplicationListView
+
+
 # https://docs.djangoproject.com/en/dev/topics/http/urls/#url-namespaces-and-included-urlconfs
 app_name = "premium_views"
 
-urlpatterns = []
+urlpatterns = [
+    path("note/<int:synced_job_application_id>/", SaveNoteView.as_view(), name="save_note"),
+    path("job_applications/", SyncJobApplicationListView.as_view(), name="job_applications"),
+    path("job_applications/refresh/", RefreshSyncJobApplicationView.as_view(), name="refresh"),
+]
