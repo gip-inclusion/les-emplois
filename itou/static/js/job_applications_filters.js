e4e6297ccbdd898ec8c131a7e258f283ad0f3119
Xavier Fernandez <xavier.fernandez@beta.gouv.fr>
2024-03-29 17:31:29 +0100

    apply: fix js for non-employer job application lists

--- itou/static/js/job_applications_filters.js
+++ itou/static/js/job_applications_filters.js
@@ -1,3 +1,9 @@
+function submitFiltersForm() {
+    $("#js-job-applications-filters-form").submit();
+}
+$("#js-job-applications-filters-form :input").change(submitFiltersForm);
+$("#js-job-applications-filters-form duet-date-picker").on("duetChange", submitFiltersForm);
+
 $("#js-job-applications-filters-apply-button").hide();
 
 // If the Filtres button is present, the associated card should be collapsed on display.
