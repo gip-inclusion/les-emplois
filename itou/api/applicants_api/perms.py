832ff95b036f70a9b4e6e1604aa1b310d7222ecf
Antoine LAURENT <antoine.c.laurent.pro@gmail.com>
2024-04-02 11:22:17 +0200

    api: Fix permissions

--- itou/api/applicants_api/perms.py
+++ itou/api/applicants_api/perms.py
@@ -11,9 +11,8 @@ class ApplicantsAPIPermission(IsAuthenticated):
 
         if not request.user.is_employer:
             return False
-        # If filter with company id: check permission so that it's not to be done later
-        # (and it should save a query too).
 
-        memberships_qs = request.user.active_or_in_grace_period_company_memberships()
-
-        return all(memberships_qs.values_list("is_admin", flat=True))
+        memberships_status_is_admin = request.user.active_or_in_grace_period_company_memberships().values_list(
+            "is_admin", flat=True
+        )
+        return memberships_status_is_admin and all(memberships_status_is_admin)
