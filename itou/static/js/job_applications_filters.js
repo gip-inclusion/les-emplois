2a0039787d1842e49a433bccba8e98e336da72cc
François Freitag <mail@franek.fr>
2024-04-03 18:42:56 +0200

    Filter job seeker job applications with HTMX
    Instead of reloading the page, use HTMX to reload the content.

--- itou/static/js/job_applications_filters.js
+++ itou/static/js/job_applications_filters.js
@@ -1,7 +1,1 @@
-function submitFiltersForm() {
-    $("#js-job-applications-filters-form").submit();
-}
-$("#js-job-applications-filters-form :input").change(submitFiltersForm);
-$("#js-job-applications-filters-form duet-date-picker").on("duetChange", submitFiltersForm);
-
 $("#js-job-applications-filters-apply-button").hide();
