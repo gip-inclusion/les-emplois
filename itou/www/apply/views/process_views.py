db124fbe2c55988c73614c753c83df0a0b470705
François Freitag <mail@franek.fr>
2024-04-08 16:38:35 +0200

    Fix an open-redirect in application transfer view
    The URL provided by the POST parameter cannot be trusted.
    
    https://github.com/gip-inclusion/les-emplois/security/code-scanning/58
    https://cwe.mitre.org/data/definitions/601.html

--- itou/www/apply/views/process_views.py
+++ itou/www/apply/views/process_views.py
@@ -451,7 +451,7 @@ def transfer(request, job_application_id):
     queryset = JobApplication.objects.is_active_company_member(request.user)
     job_application = get_object_or_404(queryset, pk=job_application_id)
     target_company = get_object_or_404(Company.objects, pk=request.POST.get("target_company_id"))
-    back_url = request.POST.get("back_url", reverse("apply:list_for_siae"))
+    back_url = get_safe_url(request, "back_url", reverse("apply:list_for_siae"))
 
     try:
         job_application.transfer_to(request.user, target_company)
