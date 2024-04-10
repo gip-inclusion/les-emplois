90e7929f7766b9545b96dbcdfd92704b4cd55689
François Freitag <mail@franek.fr>
2024-04-10 11:07:37 +0200

    Squash JobSeekerProfile.previous_employer_kind migrations

--- itou/users/migrations/0003_remove_jobseekerprofile_previous_employer_kind_from_db.py
+++ itou/users/migrations/0003_remove_jobseekerprofile_previous_employer_kind_from_db.py
@@ -7,6 +7,4 @@ class Migration(migrations.Migration):
         ("users", "0002_remove_jobseekerprofile_previous_employer_kind"),
     ]
 
-    operations = [
-        migrations.RunSQL("ALTER TABLE users_jobseekerprofile DROP COLUMN previous_employer_kind"),
-    ]
+    operations = []
