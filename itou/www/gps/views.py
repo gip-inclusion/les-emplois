5a9fefd223af2e5d0c5be86c1f6aade668306ce7
Vincent Jousse <vincent@jousse.org>
2024-04-11 13:42:53 +0200

    Create FollowUpGroup entity

--- itou/www/gps/views.py
+++ itou/www/gps/views.py
@@ -1,9 +1,15 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from itou.gps.models import FollowUpGroup
+
 
 @login_required
 def my_groups(request, template_name="gps/my_groups.html"):
-    context = {}
+
+    current_user = request.user
+    groups = FollowUpGroup.objects.filter(members=current_user).all()
+
+    context = {"groups": groups}
 
     return render(request, template_name, context)
