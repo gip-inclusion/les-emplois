560b35f7e1b542788943cb686501affb88e10688
Vincent Jousse <vincent@jousse.org>
2024-04-29 11:25:45 +0200

    test: basic navigation

--- tests/gps/tests.py
+++ tests/gps/tests.py
@@ -1,6 +1,10 @@
+from django.urls import reverse
+from pytest_django.asserts import assertContains
+
 from itou.users.models import User
 from tests.gps.factories import FollowUpGroupFactory
 from tests.users.factories import JobSeekerFactory, PrescriberFactory
+from tests.utils.test import parse_response_to_soup
 
 
 def test_user_autocomplete():
@@ -19,3 +23,31 @@ def test_user_autocomplete():
     users = User.objects.autocomplete("gps", current_user=member)
     assert users.count() == 1
     assert users[0].id == another_beneficiary.id
+
+
+def test_navigation(snapshot, client):
+
+    member = PrescriberFactory(first_name="gps member first_name")
+    member_first_beneficiary = JobSeekerFactory(first_name="gps first beneficiary first_name")
+    member_second_beneficiary = JobSeekerFactory(first_name="gps second beneficiary first_name")
+
+    FollowUpGroupFactory(beneficiary=member_first_beneficiary, memberships=4, memberships__member=member)
+    FollowUpGroupFactory(beneficiary=member_second_beneficiary, memberships=2, memberships__member=member)
+
+    client.force_login(member)
+
+    response = client.get(reverse("dashboard:index"))
+
+    assert str(parse_response_to_soup(response, ".c-box__header--gps")) == snapshot
+
+    response = client.get(reverse("gps:my_groups"))
+
+    assertContains(response, member_first_beneficiary.get_full_name())
+    assertContains(response, member_second_beneficiary.get_full_name())
+
+    assertContains(response, member_first_beneficiary.email)
+    assertContains(response, member_second_beneficiary.email)
+
+    assertContains(response, "référent")
+
+    assertContains(response, "2 bénéficiaires suivis")
