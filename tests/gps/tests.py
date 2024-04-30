b3e62b52c6f280f82fd14433dc94cd287305eff6
Vincent Jousse <vincent@jousse.org>
2024-04-30 15:41:04 +0200

    test: join_group

--- tests/gps/tests.py
+++ tests/gps/tests.py
@@ -1,6 +1,8 @@
+import pytest
 from django.urls import reverse
 from pytest_django.asserts import assertContains
 
+from itou.gps.models import FollowUpGroup, FollowUpGroupMembership
 from itou.users.enums import UserKind
 from itou.users.models import User
 from tests.gps.factories import FollowUpGroupFactory
@@ -26,6 +28,85 @@ def test_user_autocomplete():
     users = User.objects.autocomplete("gps", current_user=member)
     assert users.count() == 1
     assert users[0].id == another_beneficiary.id
+
+
+@pytest.mark.parametrize(
+    "is_referent",
+    [
+        True,
+        False,
+    ],
+)
+def test_join_group_as_job_seeker(is_referent, client, snapshot):
+    prescriber = PrescriberFactory()
+    job_seeker = JobSeekerFactory()
+
+    client.force_login(prescriber)
+
+    url = reverse("gps:join_group")
+
+    response = client.get(url)
+
+    assert str(parse_response_to_soup(response, "#join_group")) == snapshot
+
+    post_data = {
+        "user": job_seeker.id,
+        "is_referent": is_referent,
+    }
+
+    response = client.post(url, data=post_data)
+    assert response.status_code == 302
+
+    # A follow up group and a membership to this group should have been created
+    assert FollowUpGroup.objects.count() == 1
+    follow_up_group = FollowUpGroup.objects.get(beneficiary=job_seeker)
+    assert FollowUpGroupMembership.objects.count() == 1
+    membership = (
+        FollowUpGroupMembership.objects.filter(member=prescriber).filter(follow_up_group=follow_up_group).first()
+    )
+
+    assert membership.is_referent == is_referent
+
+    # Login with another prescriber and join the same follow_up_group
+    other_prescriber = PrescriberFactory()
+
+    client.force_login(other_prescriber)
+
+    post_data = {
+        "user": job_seeker.id,
+        "is_referent": not is_referent,
+    }
+
+    response = client.post(url, data=post_data)
+    assert response.status_code == 302
+
+    # We should not have created another FollowUpGroup
+    assert FollowUpGroup.objects.count() == 1
+    follow_up_group = FollowUpGroup.objects.get(beneficiary=job_seeker)
+
+    # Just a new membership should have been created
+    assert FollowUpGroupMembership.objects.count() == 2
+
+
+def test_join_group_as_prescriber(client):
+    prescriber = PrescriberFactory()
+    another_prescriber = PrescriberFactory()
+
+    client.force_login(prescriber)
+
+    url = reverse("gps:join_group")
+
+    response = client.get(url)
+
+    post_data = {
+        "user": another_prescriber.id,
+        "is_referent": True,
+    }
+
+    response = client.post(url, data=post_data)
+    assert response.status_code == 200
+
+    assertContains(response, "Seul un candidat peut être ajouté à un groupe de suivi")
 
 
 def test_navigation(snapshot, client):
