2553fb723287c96da2aaf5d8b506d6c1d91db394
Vincent Jousse <vincent@jousse.org>
2024-04-30 09:59:42 +0200

    fix: referent should not be mandatory

--- itou/www/users_views/views.py
+++ itou/www/users_views/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse
 from django.views.generic import DetailView
 
 from itou.gps.models import FollowUpGroupMembership
@@ -16,66 +17,22 @@ class UserDetailsView(LoginRequiredMixin, DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["gps_memberships"] = (
-            FollowUpGroupMembership.objects.filter(follow_up_group=context["object"].follow_up_group)
+            FollowUpGroupMembership.objects.filter(follow_up_group=self.object.follow_up_group)
             .filter(is_active=True)
+            .order_by("-is_referent")
             .select_related("follow_up_group", "member")
         )
+
+        context["can_view_personal_information"] = self.request.user.can_view_personal_information(self.object)
+
+        context["breadcrumbs"] = {
+            "Mes groupes de suivi": reverse("gps:my_groups"),
+            f"Fiche de {self.object.get_full_name()}": reverse(
+                "users:details", kwargs={"public_id": self.object.public_id}
+            ),
+        }
+
         return context
-        # context = super().get_context_data(**kwargs)
-        # approval = self.object
-        # job_application = self.get_job_application(self.object)
-
-        # context["can_view_personal_information"] = True  # SIAE members have access to personal info
-        # context["can_edit_personal_information"] = self.request.user.can_edit_personal_information(approval.user)
-        # context["approval_can_be_suspended_by_siae"] = approval.can_be_suspended_by_siae(self.siae)
-        # context["hire_by_other_siae"] = not approval.user.last_hire_was_made_by_company(self.siae)
-        # context["approval_can_be_prolonged"] = approval.can_be_prolonged
-        # context["job_application"] = job_application
-        # context["hiring_pending"] = job_application and job_application.is_pending
-        # context["matomo_custom_title"] = "Profil salarié"
-        # context["eligibility_diagnosis"] = job_application and job_application.get_eligibility_diagnosis()
-
-        # if approval.is_in_progress:
-        #     for suspension in approval.suspensions_by_start_date_asc:
-        #         if suspension.is_in_progress:
-        #             suspension_duration = date.today() - suspension.start_at
-        #             has_hirings_after_suspension = False
-        #         else:
-        #             suspension_duration = suspension.duration
-        #             has_hirings_after_suspension = (
-        #                 approval.jobapplication_set.accepted()
-
-    # .filter(hiring_start_at__gte=suspension.end_at).exists()
-    #             )
-
-    #         if (
-    #             suspension_duration > SUSPENSION_DURATION_BEFORE_APPROVAL_DELETABLE
-    #             and not has_hirings_after_suspension
-    #         ):
-    #             context["approval_deletion_form_url"] = "https://tally.so/r/3je84Q?" + urllib.parse.urlencode(
-    #                 {
-    #                     "siaeID": self.siae.pk,
-    #                     "nomSIAE": self.siae.display_name,
-    #                     "prenomemployeur": self.request.user.first_name,
-    #                     "nomemployeur": self.request.user.last_name,
-    #                     "emailemployeur": self.request.user.email,
-    #                     "userID": self.request.user.pk,
-    #                     "numPASS": approval.number_with_spaces,
-    #                     "prenomsalarie": approval.user.first_name,
-    #                     "nomsalarie": approval.user.last_name,
-    #                 }
-    #             )
-    #             break
-
-    # context["all_job_applications"] = (
-    #     JobApplication.objects.filter(
-    #         job_seeker=approval.user,
-    #         to_company=self.siae,
-    #     )
-    #     .select_related("sender")
-    #     .prefetch_related("selected_jobs")
-    # )
-    # return context
 
 
 class UserListView(ApprovalListView):
