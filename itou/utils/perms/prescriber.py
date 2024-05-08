3f2aa246da378b84f880e0dee1dc4912a4246cab
Romain Sébille <romain.sebille@beta.gouv.fr>
2024-05-08 12:21:40 +0200

    utils.perms.prescriber: Remove unnecessary function call
    The `if` and `get_current_org_or_404()` check the exact same conditions.

--- itou/utils/perms/prescriber.py
+++ itou/utils/perms/prescriber.py
@@ -17,13 +17,12 @@ def get_all_available_job_applications_as_prescriber(request):
     from itou.job_applications.models import JobApplication
 
     if request.current_organization and request.user.is_prescriber:  # Set by middleware for prescriber users
-        prescriber_organization = get_current_org_or_404(request)
         # Show all applications organization-wide + applications sent by the
         # current user for backward compatibility (in the past, a user could
         # create his prescriber's organization later on).
         return JobApplication.objects.filter(
             (Q(sender=request.user) & Q(sender_prescriber_organization__isnull=True))
-            | Q(sender_prescriber_organization=prescriber_organization)
+            | Q(sender_prescriber_organization=request.current_organization)
         )
     else:
         return request.user.job_applications_sent
