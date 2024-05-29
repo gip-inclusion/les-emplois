b2c2408eda985e2d069ae35e060c5e94b214d11f
laurinehu <laurinehu@gmail.com>
2024-05-29 16:21:55 +0200

    metabase - change utilisateurs table name to utilisateurs_v0 and add name and surname columns

--- itou/metabase/tables/users.py
+++ itou/metabase/tables/users.py
@@ -6,11 +6,13 @@ def get_field(name):
     return User._meta.get_field(name)
 
 
-TABLE = MetabaseTable(name="utilisateurs")
+TABLE = MetabaseTable(name="utilisateurs_v0")
 TABLE.add_columns(
     [
         get_column_from_field(get_field("id"), name="id"),
         get_column_from_field(get_field("email"), name="email"),
         get_column_from_field(get_field("kind"), name="type"),
+        get_column_from_field(get_field("first_name"), name="prenom"),
+        get_column_from_field(get_field("last_name"), name="nom"),
     ]
 )
