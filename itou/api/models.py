16b3525d394849c19dea27cd4f8488910d4d2324
Xavier Fernandez <xavier.fernandez@beta.gouv.fr>
2024-06-19 10:17:29 +0200

    api: generate secure CompanyApiToken
    uuid4() entropy isn't ideal for token generation while secrets.token_bytes was designed for it

--- itou/api/models.py
+++ itou/api/models.py
@@ -1,3 +1,4 @@
+import secrets
 import uuid
 
 from django.db import models
@@ -6,8 +7,12 @@ from django.utils import timezone
 from itou.companies.models import Company
 
 
+def _generate_random_token_uuid():
+    return uuid.UUID(bytes=secrets.token_bytes(16))
+
+
 class CompanyApiToken(models.Model):
-    key = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    key = models.UUIDField(primary_key=True, default=_generate_random_token_uuid, editable=False)
     label = models.CharField(verbose_name="mémo permettant d'identifier l'usage du jeton", max_length=60, unique=True)
     created_at = models.DateTimeField(default=timezone.now)
     companies = models.ManyToManyField(Company, related_name="api_tokens")
