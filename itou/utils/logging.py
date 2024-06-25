94636255c762c63176c91f2b24b36ab4545569eb
Xavier Fernandez <xavier.fernandez@beta.gouv.fr>
2024-06-25 14:04:25 +0200

    utils: add token in our Datadog logs
    A new `datadog_info` method is used to make this mechanism opt-in and
    ensure that no secret is accidentally injected in our logs

--- itou/utils/logging.py
+++ itou/utils/logging.py
@@ -1,4 +1,12 @@
+import logging
+
 from django_datadog_logger.formatters import datadog
+
+
+logger = logging.getLogger(__name__)
+
+
+NO_RECURSIVE_LOG_FLAG = "_no_recursive_log_flag"
 
 
 class ItouDataDogJSONFormatter(datadog.DataDogJSONFormatter):
@@ -11,6 +19,21 @@ class ItouDataDogJSONFormatter(datadog.DataDogJSONFormatter):
             if log_key in log_entry_dict:
                 del log_entry_dict[log_key]
         wsgi_request = self.get_wsgi_request()
-        if wsgi_request is not None and (current_org := getattr(wsgi_request, "current_organization", None)):
-            log_entry_dict["usr.organization_id"] = current_org.pk
+        if wsgi_request is not None:
+            if current_org := getattr(wsgi_request, "current_organization", None):
+                log_entry_dict["usr.organization_id"] = current_org.pk
+            if token := getattr(wsgi_request, "auth", None):
+                if hasattr(token, "datadog_info"):
+                    log_entry_dict["token"] = token.datadog_info()
+                else:
+                    try:
+                        user_pk = wsgi_request.user.pk
+                    except AttributeError:
+                        user_pk = None
+                    if user_pk is None and not getattr(wsgi_request, NO_RECURSIVE_LOG_FLAG, False):
+                        # Avoid coming right back here with the following logger.warning
+                        setattr(wsgi_request, NO_RECURSIVE_LOG_FLAG, True)
+                        logger.warning(
+                            "Request using token (%r) without datadog_info() method: please define one", token
+                        )
         return log_entry_dict
