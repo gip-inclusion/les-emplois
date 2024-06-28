523704e71acd8114da7d0d25ffc7fc89c21e9546
Céline MS <celine@hello-birds.com>
2024-06-28 12:13:07 +0200

    Escape user name

--- itou/users/models.py
+++ itou/users/models.py
@@ -748,9 +748,6 @@ class User(AbstractUser, AddressMixin):
     def get_kind_display(self):
         return UserKind(self.kind).label
 
-    def autocomplete_display(self):
-        return self.get_full_name()
-
 
 def get_allauth_account_user_display(user):
     return user.email
