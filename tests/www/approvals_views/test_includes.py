0b12e5580cc1b0ff8086b46c17ccc39d4aa67965
Xavier Fernandez <xavier.fernandez@beta.gouv.fr>
2024-07-08 10:38:51 +0200

    approval: always use request to access the user

--- tests/www/approvals_views/test_includes.py
+++ tests/www/approvals_views/test_includes.py
@@ -2,6 +2,7 @@ import datetime
 
 import pytest  # noqa
 from django.template import Context, Template
+from django.test import RequestFactory
 from freezegun import freeze_time
 
 import itou.approvals.enums as approvals_enums
@@ -14,10 +15,12 @@ from tests.utils.test import remove_static_hash
 @freeze_time("2022-01-10")
 class TestStatusInclude:
     def test_valid_approval_for_employer(self, snapshot, db):
+        request = RequestFactory()
+        request.user = EmployerFactory()
         context = Context(
             {
                 "common_approval": ApprovalFactory(for_snapshot=True),
-                "user": EmployerFactory(),
+                "request": request,
                 "hiring_pending": False,
                 "job_application": None,
                 "ApprovalOrigin": approvals_enums.Origin,
@@ -27,10 +30,12 @@ class TestStatusInclude:
         assert remove_static_hash(rendered_template) == snapshot(name="valid_approval_for_employer")
 
     def test_valid_approval_for_prescriber(self, snapshot, db):
+        request = RequestFactory()
+        request.user = PrescriberFactory()
         context = Context(
             {
                 "common_approval": ApprovalFactory(for_snapshot=True),
-                "user": PrescriberFactory(),
+                "request": request,
                 "hiring_pending": False,
                 "job_application": None,
                 "ApprovalOrigin": approvals_enums.Origin,
@@ -40,10 +45,12 @@ class TestStatusInclude:
         assert remove_static_hash(rendered_template) == snapshot(name="valid_approval_for_prescriber")
 
     def test_expired_approval_without_eligibility_diagnosis_for_employer(self, snapshot, db):
+        request = RequestFactory()
+        request.user = EmployerFactory()
         context = Context(
             {
                 "common_approval": ApprovalFactory(for_snapshot=True, end_at=datetime.date(2022, 1, 1)),
-                "user": EmployerFactory(),
+                "request": request,
                 "hiring_pending": False,
                 "job_application": None,
                 "ApprovalOrigin": approvals_enums.Origin,
@@ -55,12 +62,14 @@ class TestStatusInclude:
         )
 
     def test_expired_approval_with_eligibility_diagnosis_for_employer(self, snapshot, db):
+        request = RequestFactory()
+        request.user = EmployerFactory()
         approval = ApprovalFactory(for_snapshot=True, end_at=datetime.date(2022, 1, 1))
         approval.eligibility_diagnosis = IAEEligibilityDiagnosisFactory(from_prescriber=True, job_seeker=approval.user)
         context = Context(
             {
                 "common_approval": approval,
-                "user": EmployerFactory(),
+                "request": request,
                 "hiring_pending": False,
                 "job_application": None,
                 "ApprovalOrigin": approvals_enums.Origin,
@@ -72,12 +81,14 @@ class TestStatusInclude:
         )
 
     def test_expired_approval_with_eligibility_diagnosis_for_prescriber(self, snapshot, db):
+        request = RequestFactory()
+        request.user = PrescriberFactory()
         approval = ApprovalFactory(for_snapshot=True, end_at=datetime.date(2022, 1, 1))
         approval.eligibility_diagnosis = IAEEligibilityDiagnosisFactory(from_prescriber=True, job_seeker=approval.user)
         context = Context(
             {
                 "common_approval": approval,
-                "user": PrescriberFactory(),
+                "request": request,
                 "hiring_pending": False,
                 "job_application": None,
                 "ApprovalOrigin": approvals_enums.Origin,
@@ -91,10 +102,12 @@ class TestStatusInclude:
     def test_expired_approval_with_eligibility_diagnosis_for_jobseeker(self, snapshot, db):
         approval = ApprovalFactory(for_snapshot=True, end_at=datetime.date(2022, 1, 1))
         approval.eligibility_diagnosis = IAEEligibilityDiagnosisFactory(from_prescriber=True, job_seeker=approval.user)
+        request = RequestFactory()
+        request.user = approval.user
         context = Context(
             {
                 "common_approval": approval,
-                "user": approval.user,
+                "request": request,
                 "hiring_pending": False,
                 "job_application": None,
                 "ApprovalOrigin": approvals_enums.Origin,
@@ -106,6 +119,8 @@ class TestStatusInclude:
         )
 
     def test_valid_approval_for_employer_not_from_pe_approval(self, snapshot, db):
+        request = RequestFactory()
+        request.user = EmployerFactory()
         context = Context(
             {
                 "common_approval": ApprovalFactory(
@@ -115,7 +130,7 @@ class TestStatusInclude:
                     end_at=datetime.date(3000, 1, 1),
                     origin_ai_stock=True,
                 ),
-                "user": EmployerFactory(),
+                "request": request,
                 "hiring_pending": False,
                 "job_application": None,
                 "ApprovalOrigin": approvals_enums.Origin,
