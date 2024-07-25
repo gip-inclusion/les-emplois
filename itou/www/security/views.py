8528c68ffc05c3f3731739fd5482077bbe4c5a12
François Freitag <mail@franek.fr>
2024-07-25 14:42:44 +0200

    Delegate security.txt to gip-inclusion
    Instead of having each team manage their security.txt, use the new,
    shared document managed by the security team.

--- itou/www/security/views.py
+++ itou/www/security/views.py
@@ -1,13 +1,5 @@
-from django.shortcuts import render
-from django.views.decorators.http import require_safe
+from django.http import HttpResponseRedirect
 
 
-@require_safe
 def security_txt(request):
-    # https://www.rfc-editor.org/rfc/rfc9116
-    # https://securitytxt.org/ can be helpful in generating the document.
-    return render(
-        request,
-        template_name="static/security/security.txt",
-        content_type="text/plain; charset=utf-8",
-    )
+    return HttpResponseRedirect("https://inclusion.beta.gouv.fr/.well-known/security.txt")
