1be8b74f674019858811740c761a8355322a1a10
Romain Sébille <romain.sebille@beta.gouv.fr>
2024-08-07 14:35:49 +0200

    Extract employee records' `get_sftp_connection()` into `itou.utils.asp`
    Also move SFTP related fixtures to the top level conftest to match that.

---
+++ itou/utils/asp.py
@@ -0,0 +1,22 @@
+import paramiko
+from django.conf import settings
+
+
+def get_sftp_connection() -> paramiko.SFTPClient:
+    client = paramiko.SSHClient()
+    if settings.ASP_FS_KNOWN_HOSTS:
+        client.load_host_keys(settings.ASP_FS_KNOWN_HOSTS)
+
+    client.connect(
+        hostname=settings.ASP_FS_SFTP_HOST,
+        port=settings.ASP_FS_SFTP_PORT,
+        username=settings.ASP_FS_SFTP_USER,
+        key_filename=settings.ASP_FS_SFTP_PRIVATE_KEY_PATH,
+        disabled_algorithms={
+            "pubkeys": ["rsa-sha2-512", "rsa-sha2-256"],  # We want ssh-rsa
+        },
+        allow_agent=False,  # No need to try other keys if the one we have failed
+        look_for_keys=False,  # No need to try other keys if the one we have failed
+        timeout=10,
+    )
+    return client.open_sftp()
