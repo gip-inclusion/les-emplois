c6e6607e3b94df35df879dd9da95d4db04a1cadc
François Freitag <mail@franek.fr>
2024-09-18 11:45:13 +0200

    Squash job_applications archived_at migrations

--- itou/job_applications/migrations/0011_drop_hidden_for_company.py
+++ itou/job_applications/migrations/0011_drop_hidden_for_company.py
@@ -6,22 +6,4 @@ class Migration(migrations.Migration):
         ("job_applications", "0010_set_jobapplication_archived_at"),
     ]
 
-    operations = [
-        # TODO: Squash with 0010_set_jobapplication_archived_at when applied.
-        migrations.SeparateDatabaseAndState(
-            state_operations=[],
-            database_operations=[
-                migrations.RunSQL(
-                    """
-                    UPDATE job_applications_jobapplication
-                       SET archived_at=NOW()
-                     WHERE hidden_for_company
-                        AND archived_at IS NULL
-                        AND state NOT IN ('accepted', 'prior_to_hire');
-
-                    ALTER TABLE job_applications_jobapplication DROP COLUMN hidden_for_company;
-                    """
-                )
-            ],
-        ),
-    ]
+    operations = []
