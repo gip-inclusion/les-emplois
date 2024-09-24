e38b5d434bd091bad767de0c8694018e6eff7c29
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2024-09-24 15:59:14 +0200

    apply: Improve matomo tracking when refusing an application

--- itou/www/apply/views/process_views.py
+++ itou/www/apply/views/process_views.py
@@ -403,6 +403,7 @@ class JobApplicationRefuseView(LoginRequiredMixin, NamedUrlSessionWizardView):
             "job_application": self.job_application,
             "can_view_personal_information": True,  # SIAE members have access to personal info
             "matomo_custom_title": "Candidature refusée",
+            "matomo_event_name": f"refuse-application-{self.steps.current}-submit",
             "primary_button_label": "Suivant" if context["wizard"]["steps"].next else "Confirmer le refus",
             "secondary_button_label": "Précédent" if context["wizard"]["steps"].prev else "Annuler",
         }
