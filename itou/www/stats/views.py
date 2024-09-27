4c8dd6ce0820a22971fda9e5cc06c621811996da
dejafait <dejafait@blackhole.com>
2024-09-27 11:30:46 +0200

    stats: Rename constant C1_ORG_FILTER_KEY

--- itou/www/stats/views.py
+++ itou/www/stats/views.py
@@ -524,7 +524,7 @@ def stats_ph_state_main(request):
         page_title="Etat des candidatures orientées",
         extra_params={
             mb.PRESCRIBER_FILTER_KEY: PrescriberOrganizationKind(request.current_organization.kind).label,
-            mb.C1_ORG_FILTER_KEY: allowed_org_pks,
+            mb.C1_PRESCRIBER_ORG_FILTER_KEY: allowed_org_pks,
         },
     )
 
