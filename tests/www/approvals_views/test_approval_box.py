5245c8ec53c061443fcbdd0ef36d88f80c09d025
Xavier Fernandez <xavier.fernandez@beta.gouv.fr>
2024-10-18 00:01:31 +0200

    approvals: adapt box to handle PE Approval

--- tests/www/approvals_views/test_approval_box.py
+++ tests/www/approvals_views/test_approval_box.py
@@ -3,7 +3,12 @@ import datetime
 from django.template import Context
 from freezegun import freeze_time
 
-from tests.approvals.factories import ApprovalFactory, ProlongationRequestFactory, SuspensionFactory
+from tests.approvals.factories import (
+    ApprovalFactory,
+    PoleEmploiApprovalFactory,
+    ProlongationRequestFactory,
+    SuspensionFactory,
+)
 from tests.utils.test import load_template
 
 
@@ -51,3 +56,20 @@ def test_suspended_approval(snapshot):
 
     template = load_template("approvals/includes/box.html")
     assert template.render(Context({"approval": approval, "link_from_current_url": "/"})) == snapshot
+
+
+@freeze_time("2024-08-06")
+def test_expired_pe_approval(snapshot):
+    pe_approval = PoleEmploiApprovalFactory(pk=1, start_at=datetime.date(2022, 1, 1), number="123456789012")
+
+    template = load_template("approvals/includes/box.html")
+    assert template.render(Context({"approval": pe_approval})) == snapshot
+
+
+@freeze_time("2024-08-06")
+def test_valid_pe_approval(snapshot):
+    # One of the last 8 valid Pole Emploi
+    pe_approval = PoleEmploiApprovalFactory(pk=1, start_at=datetime.date(2024, 1, 1), number="123456789012")
+
+    template = load_template("approvals/includes/box.html")
+    assert template.render(Context({"approval": pe_approval})) == snapshot
