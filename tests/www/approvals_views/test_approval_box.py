08e70a56512ad06e51af1aece9314d6d95787522
Xavier Fernandez <xavier.fernandez@beta.gouv.fr>
2024-10-17 22:39:34 +0200

    dashboard: adapt job seeker dashboard

--- tests/www/approvals_views/test_approval_box.py
+++ tests/www/approvals_views/test_approval_box.py
@@ -9,6 +9,9 @@ from tests.approvals.factories import (
     ProlongationRequestFactory,
     SuspensionFactory,
 )
+from tests.asp.factories import CommuneFactory
+from tests.cities.factories import create_city_geispolsheim
+from tests.eligibility.factories import IAEEligibilityDiagnosisFactory
 from tests.utils.test import load_template
 
 
@@ -73,3 +76,63 @@ def test_valid_pe_approval(snapshot):
 
     template = load_template("approvals/includes/box.html")
     assert template.render(Context({"approval": pe_approval})) == snapshot
+
+
+@freeze_time("2024-08-06")
+def test_expired_approval_in_waiting_period_with_valid_diagnosis(snapshot):
+    approval = ApprovalFactory(pk=1, start_at=datetime.date(2022, 1, 1), number="XXXXX1234567")
+    IAEEligibilityDiagnosisFactory(job_seeker=approval.user, from_prescriber=True)
+
+    template = load_template("approvals/includes/box.html")
+    assert template.render(
+        Context(
+            {
+                "approval": approval,
+                "link_from_current_url": "/",
+                "job_seeker_dashboard_version": True,
+            }
+        )
+    ) == snapshot(name="without city")
+    approval.user.jobseeker_profile.hexa_commune = CommuneFactory(code="67152")
+    approval.user.jobseeker_profile.save(update_fields=("hexa_commune",))
+    approval.user.jobseeker_profile.hexa_commune.city = create_city_geispolsheim()
+    approval.user.jobseeker_profile.hexa_commune.save(update_fields=("city",))
+
+    assert template.render(
+        Context(
+            {
+                "approval": approval,
+                "link_from_current_url": "/",
+                "job_seeker_dashboard_version": True,
+            }
+        )
+    ) == snapshot(name="with city")
+
+
+@freeze_time("2024-08-06")
+def test_expired_approval_in_waiting_period_without_diagnosis(snapshot):
+    approval = ApprovalFactory(pk=1, start_at=datetime.date(2022, 1, 1), number="XXXXX1234567")
+
+    template = load_template("approvals/includes/box.html")
+    assert template.render(
+        Context(
+            {
+                "approval": approval,
+                "link_from_current_url": "/",
+                "job_seeker_dashboard_version": True,
+            }
+        )
+    ) == snapshot(name="without_city")
+    approval.user.jobseeker_profile.hexa_commune = CommuneFactory(code="67152")
+    approval.user.jobseeker_profile.save(update_fields=("hexa_commune",))
+    approval.user.jobseeker_profile.hexa_commune.city = create_city_geispolsheim()
+    approval.user.jobseeker_profile.hexa_commune.save(update_fields=("city",))
+    assert template.render(
+        Context(
+            {
+                "approval": approval,
+                "link_from_current_url": "/",
+                "job_seeker_dashboard_version": True,
+            }
+        )
+    ) == snapshot(name="with_city")
