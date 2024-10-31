e238da37a1b113487d0c387823c0335447f0bb6f
hellodeloo <hello@hellodeloo.com>
2024-10-31 15:36:18 +0100

    fix: sliding tabs on htmx reload

--- itou/static/js/sliding_tabs.js
+++ itou/static/js/sliding_tabs.js
@@ -1,24 +1,20 @@
-// Only for <ul.s-tabs-01__nav.nav.nav-tabs> how has data-it-sliding-tabs="true" attribute
-let slidingTabs = document.querySelector("[data-it-sliding-tabs=true]");
+"use strict";
 
-if (slidingTabs) {
-  let slidingTabsStartIndex = 0;
-
-  if (slidingTabs.hasAttribute("data-it-sliding-tabs-startindex")) {
-    slidingTabsStartIndex = slidingTabs.getAttribute("data-it-sliding-tabs-startindex")
-  }
-
-  tns({
-    container: slidingTabs,
-    slideBy: "page",
-    autoWidth: true,
-    arrowKeys: true,
-    loop: false,
-    mouseDrag: true,
-    swipeAngle: false,
-    speed: 300,
-    nav: false,
-    controls: true,
-    startIndex: slidingTabsStartIndex,
-  });
-}
+htmx.onLoad(function(target) {
+  target.querySelectorAll("[data-it-sliding-tabs=true]").forEach(function(slidingTabs) {
+    const slidingTabsStartIndex = Number.parseInt(slidingTabs.getAttribute("data-it-sliding-tabs-startindex")) || 0;
+    tns({
+      container: slidingTabs,
+      slideBy: "page",
+      autoWidth: true,
+      arrowKeys: true,
+      loop: false,
+      mouseDrag: true,
+      swipeAngle: false,
+      speed: 300,
+      nav: false,
+      controls: true,
+      startIndex: slidingTabsStartIndex,
+    });
+  })
+});
