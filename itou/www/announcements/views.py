1b8d9c55491741b3f14e5c2b95545c8b82988940
Calum Mackervoy <calummackervoy@protonmail.com>
2024-11-07 09:58:06 +0100

    Remove fallback redirect on news page

--- itou/www/announcements/views.py
+++ itou/www/announcements/views.py
@@ -1,12 +1,10 @@
 from django.db.models import Count, Prefetch, Q
-from django.shortcuts import redirect
 from django.urls import reverse
 from django.utils import timezone
 from django.views.generic import TemplateView
 
 from itou.communications.models import AnnouncementCampaign, AnnouncementItem
 from itou.users.enums import UserKind
-from itou.utils.constants import ITOU_HELP_CENTER_URL
 from itou.utils.pagination import pager
 from itou.utils.urls import get_safe_url
 
@@ -27,16 +25,5 @@ class NewsView(TemplateView):
             .order_by("-start_date")
         )
 
-        if not campaigns.count():
-            raise AnnouncementCampaign.DoesNotExist()
-
         news_page = pager(campaigns, self.request.GET.get("page"), items_per_page=12)
         return {"back_url": get_safe_url(self.request, "back_url", reverse("home:hp")), "news_page": news_page}
-
-    def get(self, request, *args, **kwargs):
-        try:
-            return super().get(request, *args, **kwargs)
-        except AnnouncementCampaign.DoesNotExist:
-            # TODO: this redirect can be removed once there is guaranteed news for all users
-            # the purpose is to avoid serving a blank page, on production
-            return redirect(f"{ ITOU_HELP_CENTER_URL }/categories/25225629682321--Nouveaut%C3%A9s")
