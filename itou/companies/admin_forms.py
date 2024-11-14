59776b3afbff27009560f8b22d36672d3d102d58
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2024-11-14 11:52:20 +0100

    organizations: Add automatic_geocoding_update boolean field to models
    We want to always update except for specific organizations where the
    geocoding is known to give bad results

--- itou/companies/admin_forms.py
+++ itou/companies/admin_forms.py
@@ -8,22 +8,6 @@ from itou.companies.models import Company
 from itou.utils.admin import ChooseFieldsToTransfer
 
 
-class CompanyAdminForm(forms.ModelForm):
-    # Add a custom form field that is not part of the model in the admin.
-    extra_field_refresh_geocoding = forms.BooleanField(
-        label="Recalculer le geocoding",
-        help_text=(
-            "Si cette case est cochée, les coordonnées géographiques seront mises à "
-            "jour si l'adresse est correctement renseignée."
-        ),
-        required=False,
-    )
-
-    class Meta:
-        model = Company
-        fields = "__all__"
-
-
 FakeField = namedtuple("FakeField", ("name",))
 
 
