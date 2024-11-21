39700b96b3a559abe2bf6fe87513b0c0d7a7dcbd
Léo S. <leo@naeka.fr>
2024-11-21 08:47:46 +0100

    feat(rdvi): add readonly RDV-I objects to admin

--- itou/rdv_insertion/apps.py
+++ itou/rdv_insertion/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class RdvInsertionConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "itou.rdv_insertion"
+    verbose_name = "RDV-Insertion"
