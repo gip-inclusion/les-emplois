c5dc6550757826d7f5121cbf27dd3f012e79c5d7
Xavier Fernandez <xavier.fernandez@beta.gouv.fr>
2024-11-29 16:28:24 +0100

    www.api: simplify index view

--- itou/www/api/urls.py
+++ itou/www/api/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-
-from itou.www.api import views
+from django.views.generic import TemplateView
 
 
 app_name = "api"
 
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", TemplateView.as_view(template_name="api/index.html"), name="index"),
 ]
--- itou/www/api/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def index(request, template_name="api/index.html"):
-    """
-    Render the home page for the API
-    """
-
-    return render(request, template_name)
