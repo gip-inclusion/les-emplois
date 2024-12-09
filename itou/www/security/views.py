72df9d443011c13d66f6b90325d61e9d2214033f
François Freitag <mail@franek.fr>
2024-12-09 15:08:26 +0100

    Update security.txt location

--- itou/www/security/views.py
+++ itou/www/security/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponseRedirect
 
 
 def security_txt(request):
-    return HttpResponseRedirect("https://inclusion.beta.gouv.fr/.well-known/security.txt")
+    return HttpResponseRedirect("https://inclusion.gouv.fr/.well-known/security.txt")
