d281681da2e4db8be5d3933a05390f63d1545396
dejafait <10533583+dejafait@users.noreply.github.com>
2024-12-19 16:07:42 +0100

    companies: Fix update_companies_job_app_score

--- itou/companies/management/commands/update_companies_job_app_score.py
+++ itou/companies/management/commands/update_companies_job_app_score.py
@@ -14,10 +14,8 @@ class Command(BaseCommand):
         nb_updated = (
             models.Company.objects.with_computed_job_app_score()
             # Do not update if nothing changes (NULL values have to be handled separately because NULL)
-            .exclude(
-                Q(job_app_score=F("computed_job_app_score"))
-                | Q(job_app_score__isnull=True) & Q(computed_job_app_score__isnull=True)
-            )
+            .exclude(Q(job_app_score=F("computed_job_app_score")) & Q(computed_job_app_score__isnull=False))
+            .exclude(Q(job_app_score__isnull=True) & Q(computed_job_app_score__isnull=True))
             .update(job_app_score=F("computed_job_app_score"))
         )
         self.stdout.write(f"Updated {nb_updated} companies in {time.perf_counter() - start:.3f} seconds")
