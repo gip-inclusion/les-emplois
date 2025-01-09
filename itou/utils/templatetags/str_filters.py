d04a4df2fd5b0556b6fe2959f762f1b1a8e0d740
Xavier Fernandez <xavier.fernandez@beta.gouv.fr>
2025-01-09 12:10:22 +0100

    str_fitlers: adapt pluralizefr to behave like Django's pluralize

--- itou/utils/templatetags/str_filters.py
+++ itou/utils/templatetags/str_filters.py
@@ -15,16 +15,23 @@ register = template.Library()
 @register.filter(is_safe=False)
 def pluralizefr(value, arg="s"):
     """
-    Return a plural suffix if the value is greater than 1
-    NB : the basic django pluralize filter returns the plural suffix for value==0
+    This is the Django's pluralize filter code, adapted to match French rules.
+    (NB : the basic django pluralize filter returns the plural suffix for value==0)
     """
+    if "," not in arg:
+        arg = "," + arg
+    bits = arg.split(",")
+    if len(bits) > 2:
+        return ""
+    singular_suffix, plural_suffix = bits[:2]
+
     try:
-        return arg if float(value) > 1 else ""
+        return singular_suffix if float(value) <= 1 else plural_suffix
     except ValueError:  # Invalid string that's not a number.
         pass
     except TypeError:  # Value isn't a string or a number; maybe it's a list?
         try:
-            return arg if len(value) > 1 else ""
+            return singular_suffix if len(value) <= 1 else plural_suffix
         except TypeError:  # len() of unsized object.
             pass
     return ""
