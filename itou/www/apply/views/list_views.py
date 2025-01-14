63ef012eb40bd76b6c4a69ae1c8bb063104ae835
Xavier Fernandez <xavier.fernandez@beta.gouv.fr>
2025-01-14 17:33:34 +0100

    add batch transfer

--- itou/www/apply/views/list_views.py
+++ itou/www/apply/views/list_views.py
@@ -400,11 +400,13 @@ def list_for_siae_actions(request):
         response["HX-Refresh"] = "true"
         return response
     can_archive = all(job_application.can_be_archived for job_application in selected_job_applications)
+    can_transfer = all(job_application.transfer.is_available() for job_application in selected_job_applications)
     context = {
         "batch_mode": bool(selected_job_applications),
         "selected_nb": len(selected_job_applications),
         "selected_application_ids": [job_app.pk for job_app in selected_job_applications],
         "can_archive": can_archive,
+        "can_transfer": can_transfer,
         "list_url": get_safe_url(request, "list_url", fallback_url=reverse("apply:list_for_siae")),
     }
     response = render(
--- tests/www/apply/test_batch_views.py
+++ tests/www/apply/test_batch_views.py
@@ -7,7 +7,7 @@ from pytest_django.asserts import assertMessages, assertRedirects
 
 from itou.job_applications.enums import JobApplicationState
 from itou.utils.urls import add_url_params
-from tests.companies.factories import CompanyFactory
+from tests.companies.factories import CompanyFactory, CompanyMembershipFactory
 from tests.job_applications.factories import (
     JobApplicationFactory,
 )
@@ -247,5 +247,269 @@ class TestBatchArchive:
                     extra_tags="toast",
                 ),
                 messages.Message(messages.SUCCESS, "2 candidatures ont bien été archivées.", extra_tags="toast"),
+            ],
+        )
+
+
+class TestBatchTransfer:
+    def test_invalid_access(self, client):
+        transferable_app = JobApplicationFactory(state=JobApplicationState.NEW)
+        company = CompanyFactory(with_membership=True)
+        assert transferable_app.transfer.is_available()
+        for user in [transferable_app.job_seeker, transferable_app.sender, LaborInspectorFactory(membership=True)]:
+            client.force_login(user)
+            response = client.post(
+                reverse("apply:batch_transfer"),
+                data={"target_company_id": company.pk, "application_ids": [transferable_app.pk]},
+            )
+            assert response.status_code == 403
+
+    def test_no_next_url(self, client):
+        company = CompanyFactory(with_membership=True)
+        employer = company.members.first()
+        other_company = CompanyMembershipFactory(user=employer).company
+        client.force_login(employer)
+
+        transferable_app = JobApplicationFactory(to_company=company, state=JobApplicationState.NEW)
+
+        response = client.post(
+            reverse("apply:batch_transfer"),
+            data={"target_company_id": other_company.pk, "application_ids": [transferable_app.pk]},
+        )
+        assert response.status_code == 404
+
+    def test_single_app(self, client):
+        company = CompanyFactory(with_membership=True)
+        employer = company.members.first()
+        other_company = CompanyMembershipFactory(user=employer).company
+        next_url = add_url_params(reverse("apply:list_for_siae"), {"state": "NEW"})
+        client.force_login(employer)
+
+        transferable_app = JobApplicationFactory(to_company=company, state=JobApplicationState.NEW)
+
+        response = client.post(
+            add_url_params(reverse("apply:batch_transfer"), {"next_url": next_url}),
+            data={"target_company_id": other_company.pk, "application_ids": [transferable_app.pk]},
+        )
+        assertRedirects(response, next_url)
+        transferable_app.refresh_from_db()
+        assert transferable_app.to_company == other_company
+        assertMessages(
+            response,
+            [messages.Message(messages.SUCCESS, "1 candidature a bien été transférée.", extra_tags="toast")],
+        )
+
+    def test_multiple_apps(self, client):
+        company = CompanyFactory(with_membership=True)
+        employer = company.members.first()
+        other_company = CompanyMembershipFactory(user=employer).company
+        client.force_login(employer)
+
+        transferable_apps = JobApplicationFactory.create_batch(2, to_company=company, state=JobApplicationState.NEW)
+
+        next_url = add_url_params(reverse("apply:list_for_siae"), {"state": "REFUSED"})
+
+        response = client.post(
+            add_url_params(reverse("apply:batch_transfer"), {"next_url": next_url}),
+            data={
+                "target_company_id": other_company.pk,
+                "application_ids": [transferable_app.pk for transferable_app in transferable_apps],
+            },
+        )
+        assertRedirects(response, next_url)
+        for transferable_app in transferable_apps:
+            transferable_app.refresh_from_db()
+            assert transferable_app.to_company == other_company
+        assertMessages(
+            response,
+            [messages.Message(messages.SUCCESS, "2 candidatures ont bien été transférées.", extra_tags="toast")],
+        )
+
+    def test_sent_application(self, client):
+        transferable_app = JobApplicationFactory(sent_by_another_employer=True, state=JobApplicationState.NEW)
+        to_company = transferable_app.to_company
+        next_url = add_url_params(reverse("apply:list_for_siae"), {"state": "NEW"})
+        assert transferable_app.transfer.is_available()
+
+        client.force_login(transferable_app.sender)
+        response = client.post(
+            add_url_params(reverse("apply:batch_transfer"), {"next_url": next_url}),
+            data={
+                "target_company_id": transferable_app.sender_company.pk,
+                "application_ids": [transferable_app.pk],
+            },
+        )
+        assertRedirects(response, next_url)
+        transferable_app.refresh_from_db()
+        assert transferable_app.to_company == to_company
+        assertMessages(
+            response,
+            [
+                messages.Message(
+                    messages.ERROR,
+                    "Une candidature sélectionnée n’existe plus ou a été transférée.",
+                ),
+            ],
+        )
+
+    def test_unexisting_app(self, client):
+        company = CompanyFactory(with_membership=True)
+        employer = company.members.first()
+        next_url = add_url_params(reverse("apply:list_for_siae"), {"state": "NEW"})
+        client.force_login(employer)
+
+        response = client.post(
+            add_url_params(reverse("apply:batch_transfer"), {"next_url": next_url}),
+            data={
+                "target_company_id": company.pk,
+                "application_ids": [uuid.uuid4()],
+            },
+        )
+        assertRedirects(response, next_url)
+        assertMessages(
+            response,
+            [
+                messages.Message(
+                    messages.ERROR,
+                    "Une candidature sélectionnée n’existe plus ou a été transférée.",
+                ),
+            ],
+        )
+
+    def test_untransferable(self, client):
+        company = CompanyFactory(with_membership=True)
+        employer = company.members.first()
+        other_company = CompanyMembershipFactory(user=employer).company
+        next_url = add_url_params(reverse("apply:list_for_siae"), {"state": "ACCEPTED"})
+        client.force_login(employer)
+
+        untransferable_app = JobApplicationFactory(
+            job_seeker__first_name="John",
+            job_seeker__last_name="Rambo",
+            to_company=company,
+            state=JobApplicationState.ACCEPTED,
+        )
+        assert not untransferable_app.transfer.is_available()
+
+        response = client.post(
+            add_url_params(reverse("apply:batch_transfer"), {"next_url": next_url}),
+            data={"target_company_id": other_company.pk, "application_ids": [untransferable_app.pk]},
+        )
+        assertRedirects(response, next_url)
+        untransferable_app.refresh_from_db()
+        assert untransferable_app.to_company == company
+        assertMessages(
+            response,
+            [
+                messages.Message(
+                    messages.ERROR,
+                    (
+                        "La candidature de John RAMBO n’a pas pu être transférée car elle est au statut "
+                        "« Candidature acceptée »."
+                    ),
+                    extra_tags="toast",
+                ),
+            ],
+        )
+
+    def test_already_transfered(self, client):
+        company = CompanyFactory(with_membership=True)
+        employer = company.members.first()
+        other_company = CompanyMembershipFactory(user=employer).company
+        next_url = add_url_params(reverse("apply:list_for_siae"), {"state": "NEW"})
+        client.force_login(employer)
+
+        transferable_app = JobApplicationFactory(to_company=other_company, state=JobApplicationState.NEW)
+
+        response = client.post(
+            add_url_params(reverse("apply:batch_transfer"), {"next_url": next_url}),
+            data={"target_company_id": other_company.pk, "application_ids": [transferable_app.pk]},
+        )
+        assertRedirects(response, next_url)
+        transferable_app.refresh_from_db()
+        assert transferable_app.to_company == other_company
+        assertMessages(
+            response,
+            [
+                messages.Message(
+                    messages.ERROR,
+                    "Une candidature sélectionnée n’existe plus ou a été transférée.",
+                ),
+            ],
+        )
+
+    def test_wrong_company(self, client):
+        company = CompanyFactory(with_membership=True)
+        employer = company.members.first()
+        other_company = CompanyFactory(with_membership=True)
+        next_url = add_url_params(reverse("apply:list_for_siae"), {"state": "NEW"})
+        client.force_login(employer)
+
+        transferable_app = JobApplicationFactory(to_company=company, state=JobApplicationState.NEW)
+
+        response = client.post(
+            add_url_params(reverse("apply:batch_transfer"), {"next_url": next_url}),
+            data={"target_company_id": other_company.pk, "application_ids": [transferable_app.pk]},
+        )
+        assert response.status_code == 404
+
+    def test_mishmash(self, client):
+        company = CompanyFactory(with_membership=True)
+        employer = company.members.first()
+        other_company = CompanyMembershipFactory(user=employer).company
+        client.force_login(employer)
+
+        apps = [
+            # 2 transferable applications:
+            JobApplicationFactory(to_company=company, state=JobApplicationState.NEW),
+            JobApplicationFactory(to_company=company, state=JobApplicationState.POSTPONED),
+            # 1 untransferable application:
+            JobApplicationFactory(
+                job_seeker__first_name="John",
+                job_seeker__last_name="Rambo",
+                to_company=company,
+                state=JobApplicationState.ACCEPTED,
+            ),
+            # 1 already transfered application:
+            JobApplicationFactory(
+                job_seeker__first_name="Jean",
+                job_seeker__last_name="Bond",
+                to_company=other_company,
+                state=JobApplicationState.NEW,
+                archived_at=timezone.now(),
+            ),
+        ]
+
+        next_url = add_url_params(reverse("apply:list_for_siae"), {"start_date": "1970-01-01"})
+
+        response = client.post(
+            add_url_params(reverse("apply:batch_transfer"), {"next_url": next_url}),
+            data={
+                "target_company_id": other_company.pk,
+                "application_ids": ([transferable_app.pk for transferable_app in apps] + [uuid.uuid4(), uuid.uuid4()]),
+            },
+        )
+        assertRedirects(response, next_url)
+        # 2 transferable apps have been successfully transfered despite all the error messages
+        apps[0].refresh_from_db()
+        assert apps[0].to_company == other_company
+        apps[1].refresh_from_db()
+        assert apps[1].to_company == other_company
+        assertMessages(
+            response,
+            [
+                messages.Message(
+                    messages.ERROR,
+                    "3 candidatures sélectionnées n’existent plus ou ont été transférées.",
+                ),
+                messages.Message(
+                    messages.ERROR,
+                    (
+                        "La candidature de John RAMBO n’a pas pu être transférée car elle est au statut "
+                        "« Candidature acceptée »."
+                    ),
+                    extra_tags="toast",
+                ),
+                messages.Message(messages.SUCCESS, "2 candidatures ont bien été transférées.", extra_tags="toast"),
             ],
         )
