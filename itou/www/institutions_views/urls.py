82eb8097cc9ef7acecc335c976f6298c4cc75f3f
vincent porte <vincent@neuralia.co>
2025-01-20 10:52:28 +0100

    replace user.id with user.public_id in institutions_views

--- itou/www/institutions_views/urls.py
+++ itou/www/institutions_views/urls.py
@@ -8,9 +8,11 @@ app_name = "institutions_views"
 
 urlpatterns = [
     path("colleagues", views.member_list, name="members"),
-    path("deactivate_member/<int:user_id>", views.deactivate_member, name="deactivate_member"),
+    path("deactivate_member/<uuid:public_id>", views.deactivate_member, name="deactivate_member"),
     # # Can't mix capture var syntaxes in `re_path`: all path vars expressed as RE
     re_path(
-        "admin_role/(?P<action>add|remove)/(?P<user_id>[0-9]+)", views.update_admin_role, name="update_admin_role"
+        "admin_role/(?P<action>add|remove)/(?P<public_id>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})",
+        views.update_admin_role,
+        name="update_admin_role",
     ),
 ]
