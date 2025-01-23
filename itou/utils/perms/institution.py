ba2203ad446cd49f5b675c06431bacfa07340a00
Xavier Fernandez <xavier.fernandez@beta.gouv.fr>
2025-01-23 22:00:24 +0100

    perms: improve typing for get_current_xxx functions

--- itou/utils/perms/institution.py
+++ itou/utils/perms/institution.py
@@ -1,7 +1,9 @@
 from django.http import Http404
 
+from itou.institutions.models import Institution
 
-def get_current_institution_or_404(request):
+
+def get_current_institution_or_404(request) -> Institution:
     if request.user.is_labor_inspector and request.current_organization:  # Set by middleware for labor_inspector
         return request.current_organization
     raise Http404("L'utilisateur n'est pas membre d'une organisation")
