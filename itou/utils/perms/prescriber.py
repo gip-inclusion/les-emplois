2a44f4ccd984d3e862fb171554abd561c23c947f
Xavier Fernandez <xavier.fernandez@beta.gouv.fr>
2025-01-23 22:00:24 +0100

    perms: improve typing for get_current_xxx functions

--- itou/utils/perms/prescriber.py
+++ itou/utils/perms/prescriber.py
@@ -1,7 +1,9 @@
 from django.http import Http404
 
+from itou.prescribers.models import PrescriberOrganization
 
-def get_current_org_or_404(request):
+
+def get_current_org_or_404(request) -> PrescriberOrganization:
     if request.user.is_prescriber and request.current_organization:  # Set by middleware for prescriber users
         return request.current_organization
     raise Http404("L'utilisateur n'est pas membre d'une organisation")
