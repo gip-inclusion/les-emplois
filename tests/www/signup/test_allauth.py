04afe69070c4243170b6281b32dc116c67b6e8e2
Calum Mackervoy <calummackervoy@protonmail.com>
2025-01-24 13:27:19 +0100

    Remove allauth url redirection test

--- tests/www/signup/test_allauth.py
+++
@@ -1,9 +0,0 @@
-from django.urls import reverse
-from pytest_django.asserts import assertRedirects
-
-
-def test_allauth_signup_url_redirect(client):
-    ALLAUTH_SIGNUP_URL = reverse("account_signup")
-    assert ALLAUTH_SIGNUP_URL == "/accounts/signup/"
-    response = client.get(ALLAUTH_SIGNUP_URL)
-    assertRedirects(response, reverse("signup:choose_user_kind"))
