ccb6a92b211689e023b9854fc37d634a314fe69d
Xavier Fernandez <xavier.fernandez@beta.gouv.fr>
2025-01-28 12:09:15 +0100

    analytics: migrate collect_analytics_data to logging

--- itou/analytics/management/commands/collect_analytics_data.py
+++ itou/analytics/management/commands/collect_analytics_data.py
@@ -1,6 +1,5 @@
 import datetime
 
-import sentry_sdk
 from django import db
 from django.db import transaction
 from django.forms import models
@@ -22,10 +21,10 @@ class Command(BaseCommand):
         before = timezone.now().replace(hour=0, minute=0, second=0, microsecond=0) - datetime.timedelta(
             days=options["offset"]
         )
-        self.stderr.write(f"Collecting analytics data before '{before!s}'.")
+        self.logger.info(f"Collecting analytics data before '{before!s}'.")
 
         data = self._get_data(before)
-        self.stderr.write("Analytics data computed.")
+        self.logger.info("Analytics data computed.")
 
         self.show_data(data)
         if options["save"]:
@@ -43,11 +42,11 @@ class Command(BaseCommand):
 
     def show_data(self, data):
         for code, value in data.items():
-            self.stdout.write(f"{code.label} ({code.value}): {value}")
+            self.logger.info(f"{code.label} ({code.value}): {value}")
 
     def save_data(self, data, before):
         bucket = (before.date() - datetime.timedelta(days=1)).isoformat()
-        self.stderr.write(f"Saving analytics data in bucket '{bucket}'.")
+        self.logger.info(f"Saving analytics data in bucket '{bucket}'.")
         for code, value in data.items():
             datum = Datum(
                 code=code.value,
@@ -58,13 +57,12 @@ class Command(BaseCommand):
                 with transaction.atomic():
                     datum.save()
             except db.IntegrityError:
-                self.stderr.write(f"Failed to save code={code.value} for bucket={bucket} because it already exists.")
+                self.logger.warning(f"Failed to save code={code.value} for bucket={bucket} because it already exists.")
             except models.ValidationError as error:
                 message = (
                     f"Failed to save code={code.value} for bucket={bucket} because of a ValidationError: "
                     f"{error.message}"
                 )
-                sentry_sdk.capture_message(message, "error")
-                self.stderr.write(message)
+                self.logger.error(message)
             else:
-                self.stdout.write(f"Successfully saved code={code.value} bucket={bucket} value={value}.")
+                self.logger.info(f"Successfully saved code={code.value} bucket={bucket} value={value}.")
--- tests/analytics/test_collect_analytics_data_management_command.py
+++ tests/analytics/test_collect_analytics_data_management_command.py
@@ -1,5 +1,4 @@
 import datetime
-import io
 import unittest.mock
 
 import pytest
@@ -20,7 +19,7 @@ class CodeTest(models.TextChoices):
 
 @pytest.fixture(name="command")
 def command_fixture(mocker):
-    command = collect_analytics_data.Command(io.StringIO(), io.StringIO())
+    command = collect_analytics_data.Command()
     mocker.patch.object(command, "_get_data", return_value={})
     mocker.spy(command, "show_data")
     mocker.spy(command, "save_data")
@@ -29,7 +28,7 @@ def command_fixture(mocker):
 
 
 @freeze_time("2022-01-01")
-def test_handle_default_options(command):
+def test_handle_default_options(command, caplog):
     command.handle(save=False, offset=0)
 
     assert command._get_data.call_args_list == [
@@ -37,10 +36,9 @@ def test_handle_default_options(command):
     ]
     assert command.show_data.call_args_list == [unittest.mock.call({})]
     assert command.save_data.call_args_list == []
-    assert command.stderr.getvalue().split("\n") == [
+    assert caplog.messages == [
         "Collecting analytics data before '2022-01-01 00:00:00+00:00'.",
         "Analytics data computed.",
-        "",
     ]
 
 
@@ -54,27 +52,25 @@ def test_handle_save_option(command):
 
 
 @freeze_time("2022-01-01")
-def test_handle_offset_option(command):
+def test_handle_offset_option(command, caplog):
     command.handle(save=False, offset=7)
 
-    assert command.stderr.getvalue().split("\n") == [
+    assert caplog.messages == [
         "Collecting analytics data before '2021-12-25 00:00:00+00:00'.",
         "Analytics data computed.",
-        "",
     ]
 
 
-def test_show_data(command):
+def test_show_data(command, caplog):
     command.show_data({CodeTest.CODE_001: 42, CodeTest.CODE_002: 21})
 
-    assert command.stdout.getvalue().split("\n") == [
+    assert caplog.messages == [
         "Premier code de test (CODE-001): 42",
         "Second code de test (CODE-002): 21",
-        "",
     ]
 
 
-def test_save_data(command):
+def test_save_data(command, caplog):
     command.save_data(
         {CodeTest.CODE_001: 42, CodeTest.CODE_002: 21},
         datetime.datetime(2022, 1, 1, tzinfo=datetime.UTC),
@@ -86,15 +82,14 @@ def test_save_data(command):
     assert objects[0].value == 42
     assert objects[1].code == CodeTest.CODE_002
     assert objects[1].value == 21
-    assert command.stderr.getvalue() == "Saving analytics data in bucket '2021-12-31'.\n"
-    assert command.stdout.getvalue().split("\n") == [
+    assert caplog.messages == [
+        "Saving analytics data in bucket '2021-12-31'.",
         "Successfully saved code=CODE-001 bucket=2021-12-31 value=42.",
         "Successfully saved code=CODE-002 bucket=2021-12-31 value=21.",
-        "",
     ]
 
 
-def test_save_data_with_an_integrity_error(command):
+def test_save_data_with_an_integrity_error(command, caplog):
     factories.DatumFactory(code=CodeTest.CODE_001, bucket="2021-12-31")
 
     command.save_data(
@@ -106,15 +101,14 @@ def test_save_data_with_an_integrity_error(command):
     assert len(objects) == 2
     assert objects[1].code == CodeTest.CODE_002
     assert objects[1].value == 21
-    assert command.stderr.getvalue().split("\n") == [
+    assert caplog.messages == [
         "Saving analytics data in bucket '2021-12-31'.",
         "Failed to save code=CODE-001 for bucket=2021-12-31 because it already exists.",
-        "",
+        "Successfully saved code=CODE-002 bucket=2021-12-31 value=21.",
     ]
-    assert command.stdout.getvalue() == "Successfully saved code=CODE-002 bucket=2021-12-31 value=21.\n"
 
 
-def test_save_data_with_a_float(command):
+def test_save_data_with_a_float(command, caplog):
     command.save_data(
         {CodeTest.CODE_001: 42.5, CodeTest.CODE_002: 21},
         datetime.datetime(2022, 1, 1, tzinfo=datetime.UTC),
@@ -123,12 +117,11 @@ def test_save_data_with_a_float(command):
     assert len(objects) == 1
     assert objects[0].code == CodeTest.CODE_002
     assert objects[0].value == 21
-    assert command.stderr.getvalue().split("\n") == [
+    assert caplog.messages == [
         "Saving analytics data in bucket '2021-12-31'.",
         "Failed to save code=CODE-001 for bucket=2021-12-31 because of a ValidationError: 42.5 is not an integer.",
-        "",
+        "Successfully saved code=CODE-002 bucket=2021-12-31 value=21.",
     ]
-    assert command.stdout.getvalue() == "Successfully saved code=CODE-002 bucket=2021-12-31 value=21.\n"
 
 
 @freeze_time("2024-12-03")
