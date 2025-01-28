c579dab07830dfa618faf41a227568f00e28c60d
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2025-01-28 11:13:22 +0100

    eligibility: Add message when linking a diagnosis to a non authorized org

--- itou/eligibility/admin.py
+++ itou/eligibility/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.contrib.admin.templatetags import admin_list
 from django.forms import ValidationError
 from django.forms.models import BaseInlineFormSet
@@ -181,6 +181,9 @@ class AbstractEligibilityDiagnosisAdmin(ItouModelAdmin):
     def save_model(self, request, obj, form, change):
         if not obj.pk:
             obj.expires_at = self.model._expiration_date(obj.author)
+        # We cannot make this message in the admin form
+        if obj.author_prescriber_organization_id and not obj.author_prescriber_organization.is_authorized:
+            messages.warning(request, "L'organisation prescriptrice n'est actuellement pas habilitée.")
         return super().save_model(request, obj, form, change)
 
 
