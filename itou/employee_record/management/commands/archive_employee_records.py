407129492f5e6bf9ce24d17ea3ba7b811b1bce46
Xavier Fernandez <xavier.fernandez@beta.gouv.fr>
2025-01-28 17:59:18 +0100

    employee_record: migrate archive_employee_records to logging

--- itou/employee_record/management/commands/archive_employee_records.py
+++ itou/employee_record/management/commands/archive_employee_records.py
@@ -12,24 +12,24 @@ class Command(BaseCommand):
 
     @transaction.atomic()
     def handle(self, *, wet_run, **options):
-        self.stdout.write("Start archiving employee records")
+        self.logger.info("Start archiving employee records")
 
         archivable = (
             EmployeeRecord.objects.archivable()
             .order_by("job_application__approval__end_at")
             .select_related("job_application__approval")
         )
-        self.stdout.write(f"Found {len(archivable)} archivable employee record(s)")
+        self.logger.info(f"Found {len(archivable)} archivable employee record(s)")
 
         archived_employee_records = []
         for employee_record in archivable:
-            self.stdout.write(f"Archiving {employee_record.pk=}")
+            self.logger.info(f"Archiving {employee_record.pk=}")
             if wet_run:
                 try:
                     employee_record.update_as_archived()
                 except Exception as ex:
-                    self.stdout.write(f"Can't archive {employee_record.pk=} {ex=}")
+                    self.logger.warning("Can't archive employee_record=%d ex=%s", employee_record.pk, ex)
                 else:
                     archived_employee_records.append(employee_record)
 
-        self.stdout.write(f"{len(archived_employee_records)}/{len(archivable)} employee record(s) were archived")
+        self.logger.info("%d/%d employee record(s) were archived", len(archived_employee_records), len(archivable))
