a3acb62ed2e73bfe642e9c5c114c053a9d1b6dcf
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2025-02-01 20:00:36 +0100

    gps: Allow to update membership created_at in the admin

--- tests/gps/test_admin.py
+++ tests/gps/test_admin.py
@@ -1,3 +1,5 @@
+import datetime
+
 import pytest
 from django.contrib.auth import get_user
 from django.urls import reverse
@@ -55,6 +57,8 @@ def test_create_follow_up_membership(admin_client):
         "is_active": "on",
         "follow_up_group": group.pk,
         "member": prescriber.pk,
+        "created_at_0": "01/01/2025",
+        "created_at_1": "12:34:56",
     }
     response = admin_client.post(url, data=post_data)
     assert response.status_code == 302
@@ -62,9 +66,19 @@ def test_create_follow_up_membership(admin_client):
     membership = group.memberships.get()
     assert membership.creator == get_user(admin_client)
     assert membership.ended_at is None
+    # the admin is in UTC+1
+    assert membership.created_at == datetime.datetime(2025, 1, 1, 11, 34, 56, tzinfo=datetime.UTC)
 
     url = reverse("admin:gps_followupgroupmembership_change", args=(membership.pk,))
-    response = admin_client.post(url, data={})  # no is_active to set it to False
+    response = admin_client.post(
+        url,
+        data={
+            "created_at_0": "01/01/2025",
+            "created_at_1": "12:34:56",
+            # no is_active to set it to False
+        },
+    )
+    assert response.status_code == 302
 
     membership.refresh_from_db()
     assert membership.ended_at is not None
