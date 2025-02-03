gip-inclusion/les-emplois

commit a853758052c6397e4170e52bf0faf4783ef82a99

Simplify deactivate_org_member

itou/common_apps/organizations/views.py
@@ -3,27 +3,27 @@ Functions used in organization views.
 """
 
 from django.contrib import messages
-from django.core.exceptions import PermissionDenied
+from django.core.exceptions import ObjectDoesNotExist, PermissionDenied
 
 
 def deactivate_org_member(request, target_member):
-    if not request.is_current_organization_admin or target_member not in request.current_organization.active_members:
+    if not request.is_current_organization_admin or request.user == target_member:
         raise PermissionDenied
 
-    membership = request.current_organization.memberships.get(user=target_member)
+    try:
+        membership = request.current_organization.memberships.get(user=target_member)
+    except ObjectDoesNotExist:
+        raise PermissionDenied
 
     if request.method == "POST":
-        if request.user != target_member and request.is_current_organization_admin:
-            if membership.is_active:
-                # Deactivate the membership without deleting it.
-                membership.deactivate_membership_by_user(request.user)
-                membership.save()
-                messages.success(
-                    request, f"{target_member.get_full_name()} a été retiré(e) des membres actifs de cette structure."
-                )
-                request.current_organization.member_deactivation_email(membership.user).send()
-        else:
-            raise PermissionDenied
+        if membership.is_active:
+            # Deactivate the membership without deleting it.
+            membership.deactivate_membership_by_user(request.user)
+            membership.save()
+            messages.success(
+                request, f"{target_member.get_full_name()} a été retiré(e) des membres actifs de cette structure."
+            )
+            request.current_organization.member_deactivation_email(membership.user).send()
         return True
 
     return False

commit 11adf31183e3000dd10dcfa87827e3548ef08de2

Restore is_active check in deactivate_member views
The membership check should verifies the member is active in order to
deactivate them. Regression introduced in
a853758052c6397e4170e52bf0faf4783ef82a99.

itou/common_apps/organizations/views.py
@@ -11,7 +11,7 @@ def deactivate_org_member(request, target_member):
         raise PermissionDenied
 
     try:
-        membership = request.current_organization.memberships.get(user=target_member)
+        membership = request.current_organization.memberships.get(user=target_member, is_active=True)
     except ObjectDoesNotExist:
         raise PermissionDenied
 

tests/www/companies_views/test_members_views.py
@@ -8,6 +8,7 @@ from tests.companies.factories import (
     CompanyFactory,
     CompanyMembershipFactory,
     CompanyWith2MembershipsFactory,
+    EmployerFactory,
 )
 
 
@@ -155,6 +156,30 @@ class TestUserMembershipDeactivation:
         assert response.status_code == 403
         other_membership.refresh_from_db()
         assert other_membership.is_active is True
+
+    @pytest.mark.parametrize("method", ["get", "post"])
+    def test_deactivate_inactive_member(self, client, method, mailoutbox):
+        company = CompanyFactory()
+        admin_membership = CompanyMembershipFactory(company=company, is_admin=True)
+        guest_membership = CompanyMembershipFactory(company=company, is_active=False)
+
+        client.force_login(admin_membership.user)
+        request = getattr(client, method)
+        response = request(reverse("companies_views:deactivate_member", kwargs={"user_id": guest_membership.user_id}))
+        assert response.status_code == 403
+        guest_membership.refresh_from_db()
+        assert guest_membership.is_active is False
+        assert mailoutbox == []
+
+    @pytest.mark.parametrize("method", ["get", "post"])
+    def test_deactivate_non_member(self, client, method, mailoutbox):
+        company = CompanyFactory()
+        admin_membership = CompanyMembershipFactory(company=company, is_admin=True)
+        other_user = EmployerFactory()
+        client.force_login(admin_membership.user)
+        request = getattr(client, method)
+        response = request(reverse("companies_views:deactivate_member", kwargs={"user_id": other_user.pk}))
+        assert response.status_code == 403
         assert mailoutbox == []
 
     def test_user_with_no_company_left(self, client):

tests/www/institutions_views/test_views.py
@@ -1,3 +1,4 @@
+import pytest
 from django.urls import reverse
 from pytest_django.asserts import assertContains, assertNotContains
 
@@ -7,6 +8,7 @@ from tests.institutions.factories import (
     InstitutionMembershipFactory,
     InstitutionWith2MembershipFactory,
     InstitutionWithMembershipFactory,
+    LaborInspectorFactory,
 )
 
 
@@ -134,6 +136,32 @@ class TestMembers:
         assert response.status_code == 403
         other_membership.refresh_from_db()
         assert other_membership.is_active is True
+
+    @pytest.mark.parametrize("method", ["get", "post"])
+    def test_deactivate_inactive_member(self, client, method, mailoutbox):
+        institution = InstitutionFactory()
+        admin_membership = InstitutionMembershipFactory(institution=institution, is_admin=True)
+        guest_membership = InstitutionMembershipFactory(institution=institution, is_active=False)
+
+        client.force_login(admin_membership.user)
+        request = getattr(client, method)
+        response = request(
+            reverse("institutions_views:deactivate_member", kwargs={"user_id": guest_membership.user_id})
+        )
+        assert response.status_code == 403
+        guest_membership.refresh_from_db()
+        assert guest_membership.is_active is False
+        assert mailoutbox == []
+
+    @pytest.mark.parametrize("method", ["get", "post"])
+    def test_deactivate_non_member(self, client, method, mailoutbox):
+        institution = InstitutionFactory()
+        admin_membership = InstitutionMembershipFactory(institution=institution, is_admin=True)
+        other_user = LaborInspectorFactory()
+        client.force_login(admin_membership.user)
+        request = getattr(client, method)
+        response = request(reverse("institutions_views:deactivate_member", kwargs={"user_id": other_user.pk}))
+        assert response.status_code == 403
         assert mailoutbox == []
 
     def test_remove_admin(self, client, mailoutbox):

tests/www/prescribers_views/test_members.py
@@ -1,3 +1,4 @@
+import pytest
 from django.urls import reverse
 from pytest_django.asserts import assertContains, assertNotContains
 
@@ -156,6 +157,33 @@ class TestUserMembershipDeactivation:
         url = reverse("prescribers_views:deactivate_member", kwargs={"user_id": guest.id})
         response = client.post(url)
         assert response.status_code == 403
+
+    @pytest.mark.parametrize("method", ["get", "post"])
+    def test_deactivate_inactive_member(self, client, method, mailoutbox):
+        organization = PrescriberOrganizationFactory()
+        admin_membership = PrescriberMembershipFactory(organization=organization, is_admin=True)
+        guest_membership = PrescriberMembershipFactory(organization=organization, is_active=False)
+
+        client.force_login(admin_membership.user)
+        request = getattr(client, method)
+        response = request(
+            reverse("prescribers_views:deactivate_member", kwargs={"user_id": guest_membership.user_id})
+        )
+        assert response.status_code == 403
+        guest_membership.refresh_from_db()
+        assert guest_membership.is_active is False
+        assert mailoutbox == []
+
+    @pytest.mark.parametrize("method", ["get", "post"])
+    def test_deactivate_non_member(self, client, method, mailoutbox):
+        organization = PrescriberOrganizationFactory()
+        admin_membership = PrescriberMembershipFactory(organization=organization, is_admin=True)
+        other_user = PrescriberFactory()
+        client.force_login(admin_membership.user)
+        request = getattr(client, method)
+        response = request(reverse("prescribers_views:deactivate_member", kwargs={"user_id": other_user.pk}))
+        assert response.status_code == 403
+        assert mailoutbox == []
 
     def test_deactivated_prescriber_is_orienter(self, client):
         """