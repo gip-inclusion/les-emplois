9ffee160d890e9899b73607c62a4aa3331337e20
François Freitag <mail@franek.fr>
2025-02-06 14:42:05 +0100

    Improve admin organization membership handling
    An email is now always sent when an administrator joins a structure.

--- itou/common_apps/organizations/admin.py
+++ itou/common_apps/organizations/admin.py
@@ -1,46 +1,44 @@
-import logging
-
 from django.contrib import admin, messages
 from django.db.models import Count
-from django.forms import BaseInlineFormSet, ModelForm
+from django.forms import BaseInlineFormSet
 
 from itou.utils.admin import ItouModelAdmin, ItouTabularInline
 
 
-logger = logging.getLogger(__name__)
-
-
-def get_membership_structure(membership):
-    if hasattr(membership, "institution"):
-        return membership.institution
-    elif hasattr(membership, "organization"):
-        return membership.organization
-    elif hasattr(membership, "company"):
-        return membership.company
-    else:
-        logger.error("Invalid membership kind : %s", membership)
-
-
-class MembersInlineForm(ModelForm):
-    def save(self, commit=True):
-        instance = super().save(commit=commit)
-        if "is_admin" in self.changed_data:
-            structure = get_membership_structure(instance)
-            if structure is not None:
-                if instance.is_admin:
-                    structure.add_admin_email(instance.user).send()
-                else:
-                    structure.remove_admin_email(instance.user).send()
-        return instance
-
-
 class MembersInlineFormSet(BaseInlineFormSet):
+    def __init__(self, *args, acting_user, **kwargs):
+        self.acting_user = acting_user
+        return super().__init__(*args, **kwargs)
+
+    def _handle_save(self, form):
+        if form.instance.pk:
+            membership = form.instance
+            if "is_admin" in form.changed_data:
+                self.instance.set_admin_role(
+                    membership,
+                    form.cleaned_data["is_admin"],
+                    updated_by=self.acting_user,
+                )
+        else:
+            membership = self.instance.add_or_activate_membership(
+                form.cleaned_data["user"],
+                force_admin=form.cleaned_data["is_admin"],
+            )
+        return membership
+
+    def save_new(self, form, commit=True):
+        if commit:
+            return self._handle_save(form)
+        return super().save_new(form, commit=commit)
+
+    def save_existing(self, form, obj, commit=True):
+        if commit:
+            return self._handle_save(form)
+        return super().save_existing(form, obj, commit=commit)
+
     def delete_existing(self, obj, commit=True):
-        if obj.is_admin is True:
-            structure = get_membership_structure(obj)
-            if structure is not None:
-                structure.remove_admin_email(obj.user).send()
-        super().delete_existing(obj, commit=commit)
+        if commit:
+            self.instance.deactivate_membership(obj, updated_by=self.acting_user)
 
 
 class MembersInline(ItouTabularInline):
@@ -49,7 +47,6 @@ class MembersInline(ItouTabularInline):
     extra = 1
     raw_id_fields = ("user",)
     readonly_fields = ("is_active", "created_at", "updated_at", "updated_by", "joined_at")
-    form = MembersInlineForm
     formset = MembersInlineFormSet
 
 
@@ -76,6 +73,12 @@ class OrganizationAdmin(ItouModelAdmin):
 
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(_member_count=Count("members", distinct=True))
+
+    def get_formset_kwargs(self, request, obj, inline, prefix):
+        kwargs = super().get_formset_kwargs(request, obj, inline, prefix)
+        if isinstance(inline, MembersInline):
+            kwargs["acting_user"] = request.user
+        return kwargs
 
     def save_related(self, request, form, formsets, change):
         had_admin = change and form.instance.active_admin_members.exists()
