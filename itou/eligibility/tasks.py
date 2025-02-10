fc5f2fe23930a32292675ae7ec652375ee15e892
Céline MS <celine@hello-birds.com>
2025-02-10 19:29:46 +0100

    API particulier: Remove always true condition
    The database query for SelectedAdministrativeCriteria filters on
    administrative_criteria__kind__in=CERTIFIABLE_ADMINISTRATIVE_CRITERIA_KINDS,
    which is currently only RSA.

--- itou/eligibility/tasks.py
+++ itou/eligibility/tasks.py
@@ -9,7 +9,7 @@ from django.utils import timezone
 from huey.contrib.djhuey import task
 from huey.exceptions import RetryTask
 
-from itou.eligibility.enums import CERTIFIABLE_ADMINISTRATIVE_CRITERIA_KINDS, AdministrativeCriteriaKind
+from itou.eligibility.enums import CERTIFIABLE_ADMINISTRATIVE_CRITERIA_KINDS
 from itou.utils.apis import api_particulier
 from itou.utils.types import InclusiveDateRange
 
@@ -33,55 +33,55 @@ def certify_criteria(eligibility_diagnosis):
     )
     with api_particulier.client() as client:
         for criterion in criteria:
-            if criterion.administrative_criteria.kind == AdministrativeCriteriaKind.RSA:
-                try:
-                    data = api_particulier.certify_criteria(criterion.administrative_criteria.kind, client, job_seeker)
-                except httpx.HTTPStatusError as exc:
-                    criterion.data_returned_by_api = exc.response.json()
-                    logger.error(
-                        "Error certifying criterion %r: code=%d json=%s",
-                        criterion,
-                        exc.response.status_code,
-                        criterion.data_returned_by_api,
+            try:
+                data = api_particulier.certify_criteria(criterion.administrative_criteria.kind, client, job_seeker)
+            except httpx.HTTPStatusError as exc:
+                criterion.data_returned_by_api = exc.response.json()
+                logger.error(
+                    "Error certifying criterion %r: code=%d json=%s",
+                    criterion,
+                    exc.response.status_code,
+                    criterion.data_returned_by_api,
+                )
+                match exc.response.status_code:
+                    case 400 | 404:
+                        # Job seeker not found or missing profile information.
+                        criterion.certified_at = timezone.now()
+                    case 429:
+                        # https://particulier.api.gouv.fr/developpeurs#respecter-la-volumétrie
+                        raise RetryTask(delay=int(exc.response.headers["Retry-After"])) from exc
+                    case 503:
+                        # TODO: Use the error code instead the message when switching to API v3.
+                        if criterion.data_returned_by_api["message"] == (
+                            "Erreur de fournisseur de donnée : "
+                            "Trop de requêtes effectuées, veuillez réessayer plus tard."
+                        ):
+                            # The data provider for API particulier returned a 429.
+                            # Let’s hope the data provider rate limit has been reset by then.
+                            raise RetryTask(delay=3600) from exc
+                        else:
+                            # The data provider for API particulier likely returned a 500.
+                            # According to the API particulier team, it often means integrity
+                            # errors on the beneficiary case. The data provider itself aggregates data
+                            # from other providers (e.g. regional information systems). When the data
+                            # sources aren’t consistent with each other, the data provider cannot
+                            # answer.
+                            # Retrying won’t fix the issue, and the error has been logged already.
+                            pass
+                    case _:
+                        raise
+            else:
+                criterion.certified = data["is_certified"]
+                criterion.certified_at = timezone.now()
+                criterion.data_returned_by_api = data["raw_response"]
+                criterion.certification_period = None
+                if criterion.certified:
+                    start_at = data["start_at"]
+                    end_at = timezone.localdate(criterion.certified_at) + datetime.timedelta(
+                        days=criterion.CERTIFICATION_GRACE_PERIOD_DAYS
                     )
-                    match exc.response.status_code:
-                        case 400 | 404:
-                            # Job seeker not found or missing profile information.
-                            criterion.certified_at = timezone.now()
-                        case 429:
-                            # https://particulier.api.gouv.fr/developpeurs#respecter-la-volumétrie
-                            raise RetryTask(delay=int(exc.response.headers["Retry-After"])) from exc
-                        case 503:
-                            # TODO: Use the error code instead the message when switching to API v3.
-                            if criterion.data_returned_by_api["message"] == (
-                                "Erreur de fournisseur de donnée : "
-                                "Trop de requêtes effectuées, veuillez réessayer plus tard."
-                            ):
-                                # The data provider for API particulier returned a 429.
-                                # Let’s hope the data provider rate limit has been reset by then.
-                                raise RetryTask(delay=3600) from exc
-                            else:
-                                # The data provider for API particulier likely returned a 500.
-                                # According to the API particulier team, it often means integrity
-                                # errors on the beneficiary case. The data provider itself aggregates data
-                                # from other providers (e.g. regional information systems). When the data
-                                # sources aren’t consistent with each other, the data provider cannot
-                                # answer.
-                                # Retrying won’t fix the issue, and the error has been logged already.
-                                pass
-                        case _:
-                            raise
-                else:
-                    criterion.certified = data["is_certified"]
-                    criterion.certified_at = timezone.now()
-                    criterion.data_returned_by_api = data["raw_response"]
-                    criterion.certification_period = None
-                    if criterion.certified:
-                        start_at = data["start_at"]
-                        end_at = timezone.localdate(criterion.certified_at) + datetime.timedelta(
-                            days=criterion.CERTIFICATION_GRACE_PERIOD_DAYS
-                        )
-                        criterion.certification_period = InclusiveDateRange(start_at, end_at)
+                    criterion.certification_period = InclusiveDateRange(start_at, end_at)
+
     SelectedAdministrativeCriteria.objects.bulk_update(
         criteria,
         fields=[
