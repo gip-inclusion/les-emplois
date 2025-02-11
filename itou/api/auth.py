e3a062664d603485911a7a72dc19b92a95c22ad9
Léo S. <leo@naeka.fr>
2025-02-11 08:20:44 +0100

    api: implement service account class used as anonymous user for DepartmentTokenAuthentication

--- itou/api/auth.py
+++ itou/api/auth.py
@@ -4,13 +4,19 @@ from rest_framework import authentication
 from itou.api import models
 
 
+class ServiceAccount(AnonymousUser):
+    @property
+    def is_authenticated(self):
+        return True
+
+
 class DepartmentTokenAuthentication(authentication.TokenAuthentication):
     model = models.DepartmentToken
 
     def authenticate_credentials(self, key):
         try:
             api_token = self.model.objects.get(key=key)
-            return (AnonymousUser(), api_token)
+            return (ServiceAccount(), api_token)
         except self.model.DoesNotExist:
             # Do not raise AuthenticationFailed to allow other authentication to succeed
             return None
