0b9d8fb46e1b3c977539e6bc1e52b5f70f5a1a62
Romain Sébille <romain.sebille@beta.gouv.fr>
2025-02-13 18:36:07 +0100

    gps: Use a setting instead of a hardcoded value for nav entry display

--- tests/prescribers/factories.py
+++ tests/prescribers/factories.py
@@ -3,6 +3,7 @@ import string
 
 import factory
 import factory.fuzzy
+from django.conf import settings
 
 from itou.common_apps.address.departments import department_from_postcode
 from itou.prescribers.enums import PrescriberAuthorizationStatus, PrescriberOrganizationKind
@@ -26,7 +27,9 @@ class PrescriberOrganizationFactory(factory.django.DjangoModelFactory):
             authorization_status=PrescriberAuthorizationStatus.NOT_SET,
         )
         not_in_territorial_experimentation = factory.Trait(
-            post_code=factory.LazyFunction(functools.partial(create_fake_postcode, ignore=["30"]))
+            post_code=factory.LazyFunction(
+                functools.partial(create_fake_postcode, ignore=settings.GPS_NAV_ENTRY_DEPARTMENTS)
+            )
         )
         for_snapshot = factory.Trait(
             uid="0260ad4f-2008-48bd-88cc-b41c0211e219",
