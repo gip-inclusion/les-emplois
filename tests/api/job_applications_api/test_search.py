61c0b58a81f5fcdb1da26314cf68a5d270315f52
Léo S. <leo@naeka.fr>
2025-02-14 19:44:36 +0100

    fixup: improve serializer test

--- tests/api/job_applications_api/test_search.py
+++ tests/api/job_applications_api/test_search.py
@@ -1,13 +1,14 @@
+import json
 from datetime import date
 
 import pytest
 from django.urls import reverse_lazy
+from freezegun import freeze_time
 
 from itou.api.auth import ServiceAccount
 from itou.api.job_application_api.serializers import JobApplicationSearchResponseSerializer
 from itou.api.models import DepartmentToken
 from itou.companies.models import Company
-from tests.api.utils import _str_with_tz
 from tests.job_applications.factories import JobApplicationFactory
 from tests.users.factories import (
     EmployerFactory,
@@ -164,59 +165,32 @@ class TestJobApplicationSearchApi:
         response = api_client.post(self.ENDPOINT_URL, VALID_SEARCH_DATA, format="json")
         assert response.status_code == 429
 
-    def test_serialized_data(self, api_client):
+    def test_serialized_data(self, api_client, snapshot):
         api_client.force_authenticate(ServiceAccount(), self.token)
+        with freeze_time("2025-02-14"):
+            job_application = JobApplicationFactory(
+                sent_by_authorized_prescriber_organisation=True,
+                with_approval=True,
+                was_hired=True,
+                for_snapshot=True,
+                sender_prescriber_organization__for_snapshot=True,
+                sender_prescriber_organization__membership__user__for_snapshot=True,
+                hired_job__for_snapshot=True,
+            )
+            job_application.selected_jobs.set({job_application.hired_job})
 
-        response = api_client.post(self.ENDPOINT_URL, VALID_SEARCH_DATA, format="json")
+        response = api_client.post(
+            self.ENDPOINT_URL,
+            {
+                "nir": "290010101010125",
+                "nom": "Doe",
+                "prenom": "Jane",
+                "date_naissance": "1990-01-01",
+            },
+            format="json",
+        )
         assert response.status_code == 200
-
-        results = response.json()["results"]
-        assert len(results) == 3
-
-        first_application = results[-1]  # Reversed sorting
-        assert first_application["cree_le"] == _str_with_tz(self.job_application.created_at)
-        assert first_application["statut"] == self.job_application.state
-        assert first_application["candidat_nom"] == self.job_seeker_1.last_name
-        assert first_application["candidat_prenom"] == self.job_seeker_1.first_name
-        assert first_application["candidat_nir"] == self.job_seeker_1.jobseeker_profile.nir
-        assert first_application["candidat_email"] == self.job_seeker_1.email
-        assert first_application["candidat_telephone"] == self.job_seeker_1.phone
-        assert first_application["candidat_pass_iae_statut"] == self.job_application.approval.state
-        assert first_application["candidat_pass_iae_numero"] == self.job_application.approval.number
-        assert first_application["candidat_pass_iae_date_debut"] == self.job_application.approval.start_at.isoformat()
-        assert first_application["candidat_pass_iae_date_fin"] == self.job_application.approval.end_at.isoformat()
-        assert first_application["entreprise_type"] == self.job_application.to_company.kind
-        assert first_application["entreprise_nom"] == self.job_application.to_company.display_name
-        assert first_application["entreprise_siret"] == self.job_application.to_company.siret
-        assert first_application["entreprise_adresse"] == self.job_application.to_company.address_on_one_line
-        assert first_application["entreprise_employeur_email"] == self.job_application.to_company.email
-        assert first_application["orientation_emetteur_type"] == self.job_application.sender_kind
-        assert (
-            first_application["orientation_emetteur_sous_type"]
-            == self.job_application.sender_prescriber_organization.kind
-        )
-        assert first_application["orientation_emetteur_nom"] == self.job_application.sender.last_name
-        assert first_application["orientation_emetteur_prenom"] == self.job_application.sender.first_name
-        assert first_application["orientation_emetteur_email"] == self.job_application.sender.email
-        assert (
-            first_application["orientation_emetteur_organisme"]
-            == self.job_application.sender_prescriber_organization.name
-        )
-        assert (
-            first_application["orientation_emetteur_organisme_telephone"]
-            == self.job_application.sender_prescriber_organization.phone
-        )
-        # FIXME(leo): add orientation_postes_recherches. Use snapshot?
-        assert first_application["orientation_candidat_message"] == self.job_application.message
-        assert first_application["orientation_candidat_cv"] == self.job_application.resume_link
-        assert first_application["contrat_date_debut"] == self.job_application.hiring_start_at.isoformat()
-        assert first_application["contrat_date_fin"] == self.job_application.hiring_end_at.isoformat()
-        hired_job = self.job_application.hired_job
-        assert first_application["contrat_poste_retenu"] == {
-            "rome": hired_job.appellation.rome.code,
-            "titre": hired_job.appellation.rome.name,
-            "ville": hired_job.location.name if hired_job.location else hired_job.company.city,
-        }
+        assert json.dumps(response.json(), indent=4) == snapshot
 
     @pytest.mark.parametrize(
         "company_source,expected_len",
