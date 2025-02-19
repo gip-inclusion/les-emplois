34495ad63cf746d233dee6ff7657fc85773e474c
hellodeloo <hello@hellodeloo.com>
2025-02-19 10:01:03 +0100

    feat: remove introJsPostuler

--- tests/www/apply/test_submit_from_job_seekers_list.py
+++ tests/www/apply/test_submit_from_job_seekers_list.py
@@ -52,7 +52,6 @@ class TestApplyAsPrescriber:
                 data-bs-title="Postuler pour ce candidat"
                 data-matomo-event="true" data-matomo-category="candidature" data-matomo-action="clic"
                 data-matomo-option="postuler-pour-ce-candidat"
-                id="introJsBtnPostuler"
                 href="{next_url}">
                 <i class="ri-draft-line" aria-label="Postuler pour ce candidat">
                 </i>
@@ -214,7 +213,6 @@ class TestApplyAsPrescriber:
                 data-bs-title="Postuler pour ce candidat"
                 data-matomo-event="true" data-matomo-category="candidature" data-matomo-action="clic"
                 data-matomo-option="postuler-pour-ce-candidat"
-                id="introJsBtnPostuler"
                 href="{next_url}">
                 <i class="ri-draft-line" aria-label="Postuler pour ce candidat">
                 </i>
