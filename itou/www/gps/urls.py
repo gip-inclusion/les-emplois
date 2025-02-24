d23671da8ad4b9baf3a8e688d949eb205d3c64e1
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2025-02-24 06:06:13 +0100

    gps: Remove old details view
    And the actions that were available on the page

--- itou/www/gps/urls.py
+++ itou/www/gps/urls.py
@@ -9,13 +9,12 @@ app_name = "gps"
 urlpatterns = [
     path("groups/list", views.group_list, name="group_list", kwargs={"current": True}),
     path("groups/old/list", views.group_list, name="old_group_list", kwargs={"current": False}),
-    path("groups/<int:group_id>/leave", views.leave_group, name="leave_group"),
-    path("groups/<int:group_id>/toggle_referent", views.toggle_referent, name="toggle_referent"),
     path("groups/<int:group_id>/memberships", views.GroupMembershipsView.as_view(), name="group_memberships"),
     path("groups/<int:group_id>/beneficiary", views.GroupBeneficiaryView.as_view(), name="group_beneficiary"),
     path("groups/<int:group_id>/contribution", views.GroupContributionView.as_view(), name="group_contribution"),
     path("groups/<int:group_id>/edition", views.GroupEditionView.as_view(), name="group_edition"),
-    path("details/<uuid:public_id>", views.UserDetailsView.as_view(), name="user_details"),
+    # FIXME Old view to delete in a few days
+    path("details/<uuid:public_id>", views.user_details, name="user_details"),
     path(
         "display/<int:group_id>/<uuid:target_participant_public_id>/<str:mode>",
         views.display_contact_info,
