ba0d51a6609ded4343c8673554bbe787c5c8502e
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2025-02-25 11:21:22 +0100

    gps: Add new group details view - contribution edition

--- itou/www/gps/urls.py
+++ itou/www/gps/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path("groups/<int:group_id>/memberships", views.GroupMembershipsView.as_view(), name="group_memberships"),
     path("groups/<int:group_id>/beneficiary", views.GroupBeneficiaryView.as_view(), name="group_beneficiary"),
     path("groups/<int:group_id>/contribution", views.GroupContributionView.as_view(), name="group_contribution"),
+    path("groups/<int:group_id>/edition", views.GroupEditionView.as_view(), name="group_edition"),
     path("details/<uuid:public_id>", views.UserDetailsView.as_view(), name="user_details"),
     path(
         "display/<int:group_id>/<uuid:target_participant_public_id>/<str:mode>",
