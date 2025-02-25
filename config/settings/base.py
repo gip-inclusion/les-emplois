98fdecc8b63272d4c21669110d379cabe3c0eaba
Romain Sébille <romain.sebille@beta.gouv.fr>
2025-02-25 14:36:26 +0100

    www.apply: Use a setting for optional refusal reason

--- config/settings/base.py
+++ config/settings/base.py
@@ -757,6 +757,9 @@ PILOTAGE_SHOW_STATS_WEBINAR = os.getenv("PILOTAGE_SHOW_STATS_WEBINAR", True) in 
 # Github
 API_GITHUB_BASE_URL = "https://api.github.com"
 
+# Territorial experimentation
+# ------------------------------------------------------------------------------
+JOB_APPLICATION_OPTIONAL_REFUSAL_REASON_DEPARTMENTS = ["69"]
 
 SERIALIZATION_MODULES = {
     "json-no-auto-fields": "itou.utils.json_no_auto_fields_serializer",
