54451cdf70df469f8941c4a3f7a8299d78585b5a
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2025-03-09 08:04:05 +0100

    employee_record_views: Use our own wizard for AddView

--- tests/www/employee_record_views/test_add.py
+++ tests/www/employee_record_views/test_add.py
@@ -1,14 +1,14 @@
-import pytest
 from dateutil.relativedelta import relativedelta
 from django.urls import reverse
-from pytest_django.asserts import assertRedirects
+from pytest_django.asserts import assertContains, assertRedirects
 
 from itou.employee_record.enums import Status
+from itou.utils.urls import add_url_params
 from tests.approvals.factories import ApprovalFactory
 from tests.companies.factories import CompanyFactory
 from tests.employee_record.factories import EmployeeRecordFactory
 from tests.job_applications.factories import JobApplicationFactory
-from tests.utils.test import assertSnapshotQueries, parse_response_to_soup
+from tests.utils.test import KNOWN_SESSION_KEYS, assertSnapshotQueries, parse_response_to_soup
 
 
 def test_wizard(snapshot, client):
@@ -21,20 +21,33 @@ def test_wizard(snapshot, client):
         approval=approval,
     )
     client.force_login(company.members.first())
+    reset_url = reverse("employee_record_views:list")
 
-    start_url = reverse("employee_record_views:add")
-    choose_employee_url = reverse("employee_record_views:add", kwargs={"step": "choose-employee"})
-    choose_approval_url = reverse("employee_record_views:add", kwargs={"step": "choose-approval"})
-    end_url = reverse("employee_record_views:add", kwargs={"step": "done"})
+    # Start view
+    # ----------------------------------------------------------------
+    with assertSnapshotQueries(snapshot(name="start-queries")):
+        response = client.get(add_url_params(reverse("employee_record_views:add"), {"reset_url": reset_url}))
 
-    # Starting the tunnel should redirect to the first step
-    with assertSnapshotQueries(snapshot):
-        assertRedirects(client.get(start_url), choose_employee_url)
+    [wizard_session_name] = [k for k in client.session.keys() if k not in KNOWN_SESSION_KEYS]
+    expected_session = {
+        "config": {
+            "session_kind": "add-employee-record",
+            "reset_url": reset_url,
+        },
+    }
+    assert client.session[wizard_session_name] == expected_session
+    choose_employee_url = reverse(
+        "employee_record_views:add", kwargs={"session_uuid": wizard_session_name, "step": "choose-employee"}
+    )
+    assertRedirects(response, choose_employee_url)
 
-    # Submit data for the "choose-employee" step
+    # Choose employee step
+    # ----------------------------------------------------------------
+    with assertSnapshotQueries(snapshot(name="choose-employee-queries")):
+        response = client.get(choose_employee_url)
     assert str(
         parse_response_to_soup(
-            client.get(choose_employee_url),
+            response,
             selector="#main .s-section",
             replace_in_attr=[
                 (
@@ -51,53 +64,55 @@ def test_wizard(snapshot, client):
             ],
         )
     ) == snapshot(name="choose-employee")
-    response = client.post(
-        choose_employee_url,
-        {
-            "choose-employee-employee": job_application.job_seeker.pk,
-            f"company_{company.pk}_add_employee_record-current_step": "choose-employee",
-        },
+    post_data = {
+        "employee": str(job_application.job_seeker.pk),
+    }
+    response = client.post(choose_employee_url, post_data)
+    expected_session["choose-employee"] = post_data
+    assert client.session[wizard_session_name] == expected_session
+    choose_approval_url = reverse(
+        "employee_record_views:add", kwargs={"session_uuid": wizard_session_name, "step": "choose-approval"}
     )
     assertRedirects(response, choose_approval_url)
 
-    # Force step 2 even if the user tries to skip it
-    response = client.get(end_url)
-    assertRedirects(response, choose_approval_url)
-
-    # Check "choose-approval" step
-    soup = parse_response_to_soup(client.get(choose_approval_url), selector="#main .s-section")
-    assert soup.find(id="id_choose-approval-approval").option["value"] == str(approval.pk)
-    soup.find(id="id_choose-approval-approval").option["value"] = "[PK of Approval]"
-    [input] = soup.select(selector=f"#id_company_{company.pk}_add_employee_record-current_step")
-    input["name"] = input["name"].replace(str(company.pk), "[PK of Company]")
-    input["id"] = input["id"].replace(str(company.pk), "[PK of Company]")
+    # Choose approval step
+    # ----------------------------------------------------------------
+    with assertSnapshotQueries(snapshot(name="choose-approval-queries")):
+        response = client.get(choose_approval_url)
+    soup = parse_response_to_soup(
+        response,
+        selector="#main .s-section",
+        replace_in_attr=[
+            ("value", str(approval.pk), "[PK of Approval]"),
+            ("value", wizard_session_name, "[UUID of session]"),
+        ],
+    )
     assert str(soup) == snapshot(name="choose-approval")
 
     # ERROR : Submit data for the "choose-approval" step when the employee already has an employee record
     # typically if two members of the company want to create the record at the same time
     employee_record = EmployeeRecordFactory(job_application=job_application, status=Status.NEW)
     post_data = {
-        "choose-approval-approval": job_application.approval.pk,
-        f"company_{company.pk}_add_employee_record-current_step": "choose-approval",
+        "approval": job_application.approval.pk,
     }
-    # FIXME: improve this when we stop using formtools.Wizard
-    with pytest.raises(TypeError):
-        response = client.post(choose_approval_url, post_data)
+    response = client.post(choose_approval_url, post_data, follow=True)
+    # We are redirected to choose employee step
+    assertRedirects(response, choose_employee_url)
+    assertContains(response, " Certaines informations sont absentes ou invalides")
     employee_record.delete()
 
     # Submit data for the "choose-approval" step
     response = client.post(choose_approval_url, post_data)
-    assertRedirects(response, end_url, fetch_redirect_response=False)
+    next_url = reverse("employee_record_views:create", kwargs={"job_application_id": job_application.pk})
+    assertRedirects(response, next_url, fetch_redirect_response=False)
 
-    # get end_url to clear the wizard data
-    client.get(end_url, follow=True)
+    # The wizard is cleared
+    assert wizard_session_name not in client.session
 
-    # Don't crash when going back to last step
+    # We cannot go back to last step
     response = client.get(choose_approval_url)
-    assertRedirects(response, choose_employee_url)
-
-    response = client.get(end_url)
-    assertRedirects(response, choose_employee_url)
+    # FIXME: redirect to reset url with a message ?
+    assert response.status_code == 404
 
 
 def test_employee_list(client):
@@ -122,44 +137,21 @@ def test_employee_list(client):
 
     client.force_login(company.members.first())
 
-    choose_employee_url = reverse("employee_record_views:add", kwargs={"step": "choose-employee"})
+    reset_url = reverse("employee_record_views:list")
+    response = client.get(add_url_params(reverse("employee_record_views:add"), {"reset_url": reset_url}))
+
+    [wizard_session_name] = [k for k in client.session.keys() if k not in KNOWN_SESSION_KEYS]
+    choose_employee_url = reverse(
+        "employee_record_views:add", kwargs={"session_uuid": wizard_session_name, "step": "choose-employee"}
+    )
+    assertRedirects(response, choose_employee_url)
+
     response = client.get(choose_employee_url)
     assert response.context["form"].fields["employee"].choices == [
         (None, "Sélectionnez le salarié"),
         (job_application_1.job_seeker.pk, job_application_1.job_seeker.get_full_name()),
         (job_application_2.job_seeker.pk, job_application_2.job_seeker.get_full_name()),
     ]
-
-
-def test_done_step_when_the_employee_record_need_to_be_created(client):
-    company = CompanyFactory(with_membership=True)
-    job_application = JobApplicationFactory(
-        to_company=company,
-        with_approval=True,
-    )
-    client.force_login(company.members.first())
-
-    choose_employee_url = reverse("employee_record_views:add", kwargs={"step": "choose-employee"})
-    choose_approval_url = reverse("employee_record_views:add", kwargs={"step": "choose-approval"})
-    end_url = reverse("employee_record_views:create", kwargs={"job_application_id": job_application.pk})
-
-    client.post(
-        choose_employee_url,
-        {
-            "choose-employee-employee": job_application.job_seeker.pk,
-            f"company_{company.pk}_add_employee_record-current_step": "choose-employee",
-        },
-    )
-    response = client.post(
-        choose_approval_url,
-        {
-            "choose-approval-approval": job_application.approval.pk,
-            f"company_{company.pk}_add_employee_record-current_step": "choose-approval",
-        },
-        follow=True,  # Don't stop to the `done` step
-    )
-
-    assertRedirects(response, end_url)
 
 
 def test_choose_employee_step_with_a_bad_choice(client):
@@ -173,15 +165,16 @@ def test_choose_employee_step_with_a_bad_choice(client):
 
     client.force_login(company.members.first())
 
-    choose_employee_url = reverse("employee_record_views:add", kwargs={"step": "choose-employee"})
+    reset_url = reverse("employee_record_views:list")
+    response = client.get(add_url_params(reverse("employee_record_views:add"), {"reset_url": reset_url}))
 
-    response = client.post(
-        choose_employee_url,
-        {
-            "choose-employee-employee": job_application.job_seeker.pk,
-            f"company_{company.pk}_add_employee_record-current_step": "choose-employee",
-        },
+    [wizard_session_name] = [k for k in client.session.keys() if k not in KNOWN_SESSION_KEYS]
+    choose_employee_url = reverse(
+        "employee_record_views:add", kwargs={"session_uuid": wizard_session_name, "step": "choose-employee"}
     )
+    assertRedirects(response, choose_employee_url)
+
+    response = client.post(choose_employee_url, {"employee": job_application.job_seeker.pk})
     assert response.status_code == 200
     assert response.context["form"].errors == {
         "employee": [f"Sélectionnez un choix valide. {job_application.job_seeker.pk} n’en fait pas partie."]
