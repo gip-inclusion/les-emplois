11cc6251482d9875dee56932ebf69795d0bd723a
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2025-03-10 11:04:45 +0100

    gps: mask in group details

--- tests/gps/test_views.py
+++ tests/gps/test_views.py
@@ -428,7 +428,22 @@ class TestGroupDetailsBeneficiaryTab:
 
         client.force_login(prescriber)
 
+        # When the prescriber can't view personal info
         url = reverse("gps:group_beneficiary", kwargs={"group_id": group.pk})
+        response = client.get(url)
+        html_details = parse_response_to_soup(
+            response,
+            selector="#main",
+            replace_in_attr=[
+                ("href", f"/gps/groups/{group.pk}", "/gps/groups/[PK of FollowUpGroup]"),
+                ("href", f"%2Fgps%2Fgroups%2F{group.pk}", "%2Fgps%2Fgroups%2F[PK of FollowUpGroup]"),
+            ],
+        )
+        assert str(html_details) == snapshot(name="masked_info")
+
+        # When he can but is not from an authorized organization
+        beneficiary.created_by = prescriber
+        beneficiary.save()
         response = client.get(url)
         html_details = parse_response_to_soup(
             response,
@@ -440,6 +455,7 @@ class TestGroupDetailsBeneficiaryTab:
         )
         assert str(html_details) == snapshot(name="no_diagnostic")
 
+        # When he is
         PrescriberOrganization.objects.update(is_authorized=True)
         response = client.get(url)
         html_details = parse_response_to_soup(
