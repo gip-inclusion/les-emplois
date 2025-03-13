ac083bbede9e352e8372a474123655e7198c340a
Romain Sébille <romain.sebille@beta.gouv.fr>
2025-03-13 16:45:15 +0100

    tests.metabase: Make `test_populate_job_seekers` for lisible

--- itou/utils/db.py
+++ itou/utils/db.py
@@ -5,3 +5,14 @@ def or_queries(queries, required=True):
     if required and not queries:
         raise ValueError("Filter queries must not be empty.")
     return Q.create(queries, connector=Q.OR)
+
+
+def dictfetchall(cursor):
+    """
+    Return all rows from a cursor as a dict.
+    Assume the column names are unique.
+
+    Source: https://docs.djangoproject.com/en/dev/topics/db/sql/#executing-custom-sql-directly
+    """
+    columns = [col[0] for col in cursor.description]
+    return [dict(zip(columns, row)) for row in cursor.fetchall()]
