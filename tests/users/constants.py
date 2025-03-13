9d9d306be4442317ed1ad869fa0d4e385b58ce34
François Freitag <mail@franek.fr>
2025-03-13 18:13:21 +0100

    Explain why job seekers PII are readonly when identity is certified

---
+++ tests/users/constants.py
@@ -0,0 +1,14 @@
+CERTIFIED_FORM_READONLY_HTML = """\
+<p class="mb-4">
+    Certains champs ne sont plus modifiables car ils sont certifiés
+    par un système d’information de l’État. Si toutefois vous
+    observez une erreur, merci de
+    <a href="https://aide.emplois.inclusion.beta.gouv.fr/hc/fr/requests/new"
+       target="_blank"
+       rel="noopener"
+       class="has-external-link"
+       aria-label="contactez l’assistance (ouverture dans un nouvel onglet)">
+        contacter l’assistance
+    </a>.
+</p>
+"""
