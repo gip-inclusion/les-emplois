35ab91eadaca67f7f4f15d7af6ad6cf3fa229472
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2025-03-13 11:35:06 +0100

    eligibility: Add missing model in __init__.py

--- itou/eligibility/models/__init__.py
+++ itou/eligibility/models/__init__.py
@@ -1,4 +1,8 @@
-from itou.eligibility.models.geiq import GEIQAdministrativeCriteria, GEIQEligibilityDiagnosis  # noqa: F401
+from itou.eligibility.models.geiq import (  # noqa: F401
+    GEIQAdministrativeCriteria,
+    GEIQEligibilityDiagnosis,
+    GEIQSelectedAdministrativeCriteria,
+)
 from itou.eligibility.models.iae import (  # noqa: F401
     AdministrativeCriteria,
     EligibilityDiagnosis,
