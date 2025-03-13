0baf6833bda6c4d46ecf868c2763a02b8b731736
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2025-03-13 16:12:49 +0100

    hijack: Improve constant name

--- itou/utils/hijack/views.py
+++ itou/utils/hijack/views.py
@@ -1,7 +1,7 @@
 from hijack import views
 
 
-HIJACK_KEY = "hijack_previous_url"
+HIJACK_PREVIOUS_URL_SESSION_KEY = "hijack_previous_url"
 
 
 class AcquireUserView(views.AcquireUserView):
@@ -9,13 +9,13 @@ class AcquireUserView(views.AcquireUserView):
         previous_url = self.request.META.get("HTTP_REFERER")
         res = super().post(request, *args, **kwargs)
         if previous_url:
-            self.request.session[HIJACK_KEY] = previous_url
+            self.request.session[HIJACK_PREVIOUS_URL_SESSION_KEY] = previous_url
         return res
 
 
 class ReleaseUserView(views.ReleaseUserView):
     def post(self, request, *args, **kwargs):
-        self.previous_url = self.request.session.get(HIJACK_KEY)
+        self.previous_url = self.request.session.get(HIJACK_PREVIOUS_URL_SESSION_KEY)
         return super().post(request, *args, **kwargs)
 
     def get_success_url(self):
