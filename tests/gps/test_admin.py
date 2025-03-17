4f3c4b892e97728753cd6a499d8ec86f07ce6675
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2025-03-17 21:35:09 +0100

    users: Change gps links in user admin page

--- tests/gps/test_admin.py
+++ tests/gps/test_admin.py
@@ -14,15 +14,12 @@ def test_job_seeker_admin_link(admin_client):
     job_seeker = JobSeekerFactory()
 
     response = admin_client.get(reverse("admin:users_user_change", args=(job_seeker.pk,)))
-    expected_url = add_url_params(
-        reverse("admin:gps_followupgroupmembership_changelist"), {"follow_up_group__beneficiary": job_seeker.pk}
-    )
-    assertContains(response, expected_url)
-    assertContains(response, "Liste des professionnels suivant ce bénéficiaire (0)")
+    assertContains(response, "Pas de groupe de suivi")
 
-    FollowUpGroupFactory(beneficiary=job_seeker, memberships=2)
+    group = FollowUpGroupFactory(beneficiary=job_seeker, memberships=2)
     response = admin_client.get(reverse("admin:users_user_change", args=(job_seeker.pk,)))
-    assertContains(response, "Liste des professionnels suivant ce bénéficiaire (2)")
+    expected_url = reverse("admin:gps_followupgroup_change", args=(group.pk,))
+    assertContains(response, "Groupe de suivi de ce bénéficiaire")
 
     # Assert that the lookup works
     admin_client.get(expected_url)
@@ -33,15 +30,13 @@ def test_participant_admin_link(admin_client, user_factory):
     participant = user_factory()
 
     response = admin_client.get(reverse("admin:users_user_change", args=(participant.pk,)))
-    expected_url = add_url_params(
-        reverse("admin:gps_followupgroup_changelist"), {"memberships__member": participant.pk}
-    )
+    expected_url = add_url_params(reverse("admin:gps_followupgroupmembership_changelist"), {"member": participant.pk})
     assertContains(response, expected_url)
-    assertContains(response, "Liste des groupes de suivi de cet utilisateur (0)")
+    assertContains(response, "Liste des relations de cet utilisateur (0)")
 
     FollowUpGroupMembershipFactory.create_batch(2, member=participant)
     response = admin_client.get(reverse("admin:users_user_change", args=(participant.pk,)))
-    assertContains(response, "Liste des groupes de suivi de cet utilisateur (2)")
+    assertContains(response, "Liste des relations de cet utilisateur (2)")
 
     # Assert that the lookup works
     admin_client.get(expected_url)
