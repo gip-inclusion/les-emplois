7bd41e25c337ba85bd0846722be3caa3a9c0fdec
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2025-03-23 09:38:52 +0100

    WIP -- apply: Use new eligibility view in apply process

--- itou/www/eligibility_views/views.py
+++ itou/www/eligibility_views/views.py
@@ -2,8 +2,10 @@ from django.contrib import messages
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import get_object_or_404
+from django.urls import reverse
 from django.views.generic import FormView
 
+from itou.companies.models import Company
 from itou.eligibility.models.iae import EligibilityDiagnosis
 from itou.users.enums import UserKind
 from itou.users.models import User
@@ -15,23 +17,31 @@ class UpdateEligibilityView(UserPassesTestMixin, FormView):
     template_name = "eligibility/update.html"
     form_class = AdministrativeCriteriaForm
     standalone = None
+    prescription_process = None
 
     def setup(self, request, *args, public_id, **kwargs):
         super().setup(request, *args, **kwargs)
 
-        # FIXME: Add more tunnels
-        self.standalone = True
-
         self.job_seeker = get_object_or_404(User.objects.filter(kind=UserKind.JOB_SEEKER), public_id=public_id)
 
-        # FIXME: in other tunnels we need the company
         self.company = None
+        if company_pk := request.GET.get("company_pk"):
+            self.company = get_object_or_404(Company.objects, pk=company_pk)
+
+        # Set tunnel
+        if self.company:
+            self.prescription_process = True
+        else:
+            self.standalone = True
 
         self.eligibility_diagnosis = EligibilityDiagnosis.objects.last_considered_valid(self.job_seeker, self.company)
 
     def test_func(self):
         if self.standalone:
             return self.request.user.is_prescriber_with_authorized_org
+        if self.prescription_process:
+            # Don't perform an eligibility diagnosis is the SIAE doesn't need it,
+            return self.company.is_subject_to_eligibility_rules and self.request.user.is_prescriber_with_authorized_org
         return False
 
     def dispatch(self, request, *args, **kwargs):
@@ -80,15 +90,24 @@ class UpdateEligibilityView(UserPassesTestMixin, FormView):
         back_url = None
         if self.standalone:
             back_url = get_safe_url(self.request, "back_url")
+        if self.prescription_process:
+            back_url = reverse(
+                "apply:application_jobs",
+                kwargs={"company_pk": self.company.pk, "job_seeker_public_id": self.job_seeker.public_id},
+            )
         # Force developpers to always provide a proper back_url
         if back_url:
             return back_url
         raise Http404
 
     def get_success_url(self):
-        # FIXME: it depends on the tunnel
         if self.standalone:
             return self.get_back_url()
+        if self.prescription_process:
+            return reverse(
+                "apply:application_resume",
+                kwargs={"company_pk": self.company.pk, "job_seeker_public_id": self.job_seeker.public_id},
+            )
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -97,9 +116,18 @@ class UpdateEligibilityView(UserPassesTestMixin, FormView):
                 "back_url": self.get_back_url(),
                 "job_seeker": self.job_seeker,
                 "eligibility_diagnosis": self.eligibility_diagnosis,
+                "standalone": self.standalone,
+                "prescription_process": self.prescription_process,
+                "company": self.company,
+                "can_view_personal_information": self.request.user.can_view_personal_information(self.job_seeker),
             }
         )
-
         if self.eligibility_diagnosis:
             context["new_expires_at_if_updated"] = self.eligibility_diagnosis._expiration_date(self.request.user)
+        if self.prescription_process:
+            context["progress"] = 50
+            context["full_content_width"] = True
+            # FIXME : raise Http404 if not there, here or in setup() ?
+            context["reset_url"] = get_safe_url(self.request, "reset_url")
+            # Do we need new_check_needed alert ?
         return context
