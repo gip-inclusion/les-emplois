cf565436f226986576cf3c4308b94eb569654ed8
Romain Sébille <romain.sebille@beta.gouv.fr>
2025-03-24 12:37:30 +0100

    users: Set `is_stalled` flag using the pilotage's data

--- tests/metabase/management/test_metabase_data.py
+++ tests/metabase/management/test_metabase_data.py
@@ -1,10 +1,14 @@
 import freezegun
 import pytest
+from django.core import management
 from django.core.cache import caches
 from django.utils import timezone
+from pytest_django.asserts import assertQuerySetEqual
 
 from itou.metabase.management.commands import metabase_data
 from itou.metabase.models import DatumKey
+from itou.users.models import User
+from tests.users.factories import JobSeekerFactory
 
 
 @pytest.fixture(name="command")
@@ -53,3 +57,43 @@ def test_show(capsys, snapshot, command):
 
     command.show_kpi(wet_run=None)
     assert capsys.readouterr() == snapshot(name="stdout and stderr")
+
+
+@pytest.mark.parametrize("wet_run", [True, False])
+def test_fetch_stalled_job_seekers(caplog, mocker, snapshot, settings, wet_run):
+    settings.METABASE_API_KEY = "metabase-api-key"
+
+    entering_job_seeker = JobSeekerFactory(jobseeker_profile__is_stalled=False)
+    exiting_job_seeker = JobSeekerFactory(jobseeker_profile__is_stalled=True)
+    noop_job_seeker = JobSeekerFactory(jobseeker_profile__is_stalled=True)
+    mocker.patch(
+        "itou.utils.apis.metabase.Client.fetch_card_results",
+        return_value=[
+            {"ID": entering_job_seeker.pk},
+            {"ID": noop_job_seeker.pk},
+        ],
+    )
+
+    management.call_command("metabase_data", "stalled-job-seekers", wet_run=wet_run)
+    if wet_run:
+        assertQuerySetEqual(
+            User.objects.filter(jobseeker_profile__is_stalled=True),
+            {entering_job_seeker, noop_job_seeker},
+            ordered=False,
+        )
+        assertQuerySetEqual(
+            User.objects.filter(jobseeker_profile__is_stalled=False), {exiting_job_seeker}, ordered=False
+        )
+    else:
+        assertQuerySetEqual(
+            User.objects.filter(jobseeker_profile__is_stalled=True),
+            {exiting_job_seeker, noop_job_seeker},
+            ordered=False,
+        )
+        assertQuerySetEqual(
+            User.objects.filter(jobseeker_profile__is_stalled=False), {entering_job_seeker}, ordered=False
+        )
+    assert caplog.messages[-1].startswith(
+        "Management command itou.metabase.management.commands.metabase_data succeeded in "
+    )
+    assert caplog.messages[:-1] == snapshot(name="logs")
