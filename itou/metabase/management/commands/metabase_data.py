8d9d7d7615b49ad3a4af8558b71fba50849b6b99
Romain Sébille <romain.sebille@beta.gouv.fr>
2025-03-24 12:37:30 +0100

    users: Set `is_stalled` flag using the pilotage's data

--- itou/metabase/management/commands/metabase_data.py
+++ itou/metabase/management/commands/metabase_data.py
@@ -3,10 +3,12 @@ from urllib.parse import unquote
 
 from django.conf import settings
 from django.core.cache import caches
+from django.db import transaction
 from django.utils import timezone
 from django.utils.dateparse import parse_datetime
 
 from itou.metabase.models import DatumKey
+from itou.users.models import JobSeekerProfile
 from itou.utils.apis.metabase import DEPARTMENT_FILTER_KEY, REGION_FILTER_KEY, Client
 from itou.utils.command import BaseCommand
 
@@ -58,6 +60,9 @@ class Command(BaseCommand):
         kpi.add_argument("action", choices=["fetch", "show"])
         kpi.add_argument("--wet-run", dest="wet_run", action="store_true")
 
+        stalled_job_seekers = subparsers.add_parser("stalled-job-seekers")
+        stalled_job_seekers.add_argument("--wet-run", dest="wet_run", action="store_true")
+
     def fetch_kpi(self, *, wet_run):
         cache = caches[self.CACHE_NAME]
         client = Client(settings.METABASE_SITE_URL)
@@ -97,6 +102,34 @@ class Command(BaseCommand):
             print(repr(value))
             print()
 
+    @transaction.atomic()
+    def fetch_stalled_job_seekers(self, *, wet_run):
+        client = Client(settings.METABASE_SITE_URL)
+
+        currently_stalled_job_seeker_ids = {row["ID"] for row in client.fetch_card_results(4412, fields=[54509])}
+        self.logger.info("Number of stalled job seekers: %d", len(currently_stalled_job_seeker_ids))
+
+        db_stalled_job_seeker_ids = set(
+            JobSeekerProfile.objects.filter(is_stalled=True).values_list("user_id", flat=True)
+        )
+        self.logger.info("Number of stalled job seekers in database: %d", len(db_stalled_job_seeker_ids))
+
+        exiting_stalled_status_ids = db_stalled_job_seeker_ids - currently_stalled_job_seeker_ids
+        self.logger.info("Number of job seekers exiting stalled status: %d", len(exiting_stalled_status_ids))
+        entering_stalled_status_ids = currently_stalled_job_seeker_ids - db_stalled_job_seeker_ids
+        self.logger.info("Number of job seekers entering stalled status: %d", len(entering_stalled_status_ids))
+
+        if wet_run:
+            exiting_update_count = JobSeekerProfile.objects.filter(
+                is_stalled=True, pk__in=exiting_stalled_status_ids
+            ).update(is_stalled=False)
+            entering_update_count = JobSeekerProfile.objects.filter(
+                is_stalled=False, pk__in=entering_stalled_status_ids
+            ).update(is_stalled=True)
+            self.logger.info(
+                "Number of job seekers updated: exiting=%d entering=%d", exiting_update_count, entering_update_count
+            )
+
     def handle(self, *, data, **options):
         match data:
             case "kpi":
@@ -105,3 +138,5 @@ class Command(BaseCommand):
                     "show": self.show_kpi,
                 }[options["action"]]
                 action_function(wet_run=options["wet_run"])
+            case "stalled-job-seekers":
+                self.fetch_stalled_job_seekers(wet_run=options["wet_run"])
