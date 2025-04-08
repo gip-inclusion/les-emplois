29bdef4da53b41a700fef4f6f578faad9f2a9c18
Xavier Fernandez <xavier.fernandez@inclusion.gouv.fr>
2025-04-08 11:40:31 +0200

    cleanup URL

--- itou/www/geiq_assessments_views/urls.py
+++ itou/www/geiq_assessments_views/urls.py
@@ -6,74 +6,99 @@ from itou.www.geiq_assessments_views import views
 app_name = "geiq_assessments_views"
 
 urlpatterns = [
-    path("list", views.list_for_geiq, name="list_for_geiq"),
+    # GEIQ urls
+    path("", views.list_for_geiq, name="list_for_geiq"),
     path("create", views.create_assessment, name="create"),
-    path("details/<uuid:pk>", views.assessment_details, name="details"),
-    path("details/<uuid:pk>/kpi", views.assessment_kpi, name="assessment_kpi"),
-    path("details/<uuid:pk>/result", views.assessment_kpi, name="assessment_kpi"),
+    path("<uuid:pk>", views.assessment_details, name="details"),
+    path("<uuid:pk>/kpi", views.assessment_kpi, name="assessment_kpi"),
+    # path("details/<uuid:pk>/result", views.assessment_result, name="assessment_result"),
     path(
-        "details/<uuid:pk>/summary-document",
+        "<uuid:pk>/summary-document",
         views.assessment_get_file,
         name="summary_document",
         kwargs={"file_field": "summary_document_file"},
     ),
     path(
-        "details/<uuid:pk>/summary-document/sync",
+        "<uuid:pk>/summary-document/sync",
         views.assessment_sync_file,
         name="sync_summary_document",
         kwargs={"file_field": "summary_document_file"},
     ),
     path(
-        "details/<uuid:pk>/structure-financial-asssessment",
+        "<uuid:pk>/structure-financial-asssessment",
         views.assessment_get_file,
         name="structure_financial_assessment",
         kwargs={"file_field": "structure_financial_assessment_file"},
     ),
     path(
-        "details/<uuid:pk>/structure-financial-asssessment/sync",
+        "<uuid:pk>/structure-financial-asssessment/sync",
         views.assessment_sync_file,
         name="sync_structure_financial_assessment",
         kwargs={"file_field": "structure_financial_assessment_file"},
     ),
     path(
-        "details/<uuid:pk>/action-financial-asssessment",
+        "<uuid:pk>/action-financial-asssessment",
         views.assessment_get_file,
         name="action_financial_assessment",
         kwargs={"file_field": "action_financial_assessment_file"},
     ),
     path(
-        "details/<uuid:pk>/action-financial-asssessment/upload",
+        "<uuid:pk>/action-financial-asssessment/upload",
         views.upload_action_financial_assessment,
         name="upload_action_financial_assessment",
     ),
     path(
-        "details/<uuid:pk>/comment",
+        "<uuid:pk>/comment",
         views.assessment_comment,
         name="assessment_comment",
     ),
     path(
-        "details/<uuid:pk>/contracts/sync",
+        "<uuid:pk>/contracts/sync",
         views.assessment_contracts_sync,
         name="assessment_contracts_sync",
     ),
     path(
-        "details/<uuid:pk>/contracts",
+        "<uuid:pk>/contracts",
         views.assessment_contracts_list,
         name="assessment_contracts_list",
     ),
     path(
-        "details/<uuid:pk>/contracts/<uuid:contract_pk>/include",
+        "contracts/<uuid:contract_pk>/include",
         views.assessment_contracts_include,
         name="assessment_contracts_include",
     ),
     path(
-        "details/<uuid:pk>/contracts/<uuid:contract_pk>/exclude",
+        "contracts/<uuid:contract_pk>/exclude",
         views.assessment_contracts_exclude,
         name="assessment_contracts_exclude",
     ),
     path(
-        "details/<uuid:pk>/contracts/<uuid:contract_pk>/<str:tab>",
+        "contracts/<uuid:contract_pk>/<str:tab>",
         views.assessment_contracts_details,
         name="assessment_contracts_detail",
     ),
+    # institution urls
+    # path("institution/", views.list_for_institution, name="list_for_institution"),
+    # path("institution/<uuid:pk>", views.details_for_institution, name="details_for_institution"),
+    # path(
+    #    "institution/<uuid:pk>/contracts",
+    #    views.contracts_list_for_institution,
+    #    name="contracts_list_for_institution",
+    # ),
+    # path("institution/<uuid:pk>/review", views.review_assessment, name="contracts_list_for_institution"),
+    # path(
+    #    "institution/contracts/<uuid:contract_pk>/include",
+    #    views.contracts_include_for_institution,
+    #    name="contracts_include",
+    # ),
+    # path(
+    #    "institution/contracts/<uuid:contract_pk>/exclude",
+    #    views.contracts_exclude_for_institution,
+    #    name="contracts_exclude",
+    # ),
+    # path(
+    #    "institution/contracts/<uuid:contract_pk>/<str:tab>",
+    #    views.contracts_details_for_institution,
+    #    name="contracts_details_for_institution",
+    # ),
 ]
