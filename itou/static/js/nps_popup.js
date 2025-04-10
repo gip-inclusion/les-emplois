f18aca903ec8f777ddca67d9bed65bae602293a1
dejafait <10533583+dejafait@users.noreply.github.com>
2025-04-10 15:52:40 +0200

    nps: Add tally popup to prompt for nps

---
+++ itou/static/js/nps_popup.js
@@ -0,0 +1,39 @@
+/********************************************************************
+  Show tally form as a popup to prompt user for NPS.
+
+  Example use:
+  {% load static %}
+  <script async src="https://tally.so/widgets/embed.js"></script>
+  <script src='{% static "js/nps_popup.js" %}' data-delaypopup="true" data-userkind="employeur" data-page="liste-candidatures"></script>
+
+********************************************************************/
+
+const data = document.currentScript.dataset;
+
+tallyConfig = {
+  "formId": "3qzqeO",
+  "popup": {
+    "width": 300,
+    "emoji": {
+        "text": "👋",
+        "animation": "wave"
+    },
+    "hiddenFields": {
+        "Userkind": data.userkind,
+        "Page": data.page
+    },
+    "hideTitle": true,
+    "autoClose": 4000,
+    "showOnce": true,
+    "doNotShowAfterSubmit": true
+  }
+};
+
+if (data.delaypopup == "true") {
+  tallyConfig.popup.open = {
+    "trigger": "time",
+    "ms": 10000
+  };
+};
+
+window.TallyConfig = tallyConfig;
