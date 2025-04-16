b6514a5b8f82a2f1954567a1695973537536c8d8
vincent porte <vincent@neuralia.co>
2025-04-16 23:44:21 +0200

    notify and archive inactive jobseekers management command

---
+++ itou/archive/management/commands/notify_archive_jobseekers.py
@@ -0,0 +1,170 @@
+import datetime
+import logging
+
+from django.db import transaction
+from django.db.models import Exists, F, OuterRef
+from django.utils import timezone
+from sentry_sdk.crons import monitor
+
+from itou.approvals.models import Approval
+from itou.archive.models import ArchivedJobSeeker
+from itou.eligibility.models import EligibilityDiagnosis, GEIQEligibilityDiagnosis
+from itou.gps.models import FollowUpGroup
+from itou.job_applications.models import JobApplication
+from itou.users.models import User, UserKind
+from itou.users.notifications import ArchiveJobSeeker, InactiveJobSeeker
+from itou.utils.command import BaseCommand
+
+
+logger = logging.getLogger(__name__)
+
+INACTIVITY_PERIOD = datetime.timedelta(days=365)
+GRACE_PERIOD = datetime.timedelta(days=30)
+BATCH_SIZE = 100
+
+
+def inactive_jobseekers_without_related_objects(inactive_since, batch_size):
+    job_applications = JobApplication.objects.filter(job_seeker=OuterRef("pk"))
+    approval = Approval.objects.filter(user_id=OuterRef("pk"))
+    eligibility_diagnosis = EligibilityDiagnosis.objects.filter(
+        job_seeker=OuterRef("pk"),
+    )
+    geiq_eligibility_diagnosis = GEIQEligibilityDiagnosis.objects.filter(
+        job_seeker=OuterRef("pk"),
+    )
+    follow_up_group = FollowUpGroup.objects.filter(
+        beneficiary=OuterRef("pk"),
+    )
+
+    return (
+        User.objects.filter(
+            kind=UserKind.JOB_SEEKER,
+            upcoming_deletion_notified_at__isnull=True,
+        )
+        .filter(
+            ~Exists(job_applications),
+            ~Exists(approval),
+            ~Exists(eligibility_diagnosis),
+            ~Exists(geiq_eligibility_diagnosis),
+            ~Exists(follow_up_group),
+        )
+        .job_seekers_with_last_activity()
+        .filter(last_activity__lt=inactive_since)[:batch_size]
+    )
+
+
+def anonymized_jobseeker(user):
+    kwargs = {
+        "date_joined": timezone.localdate(user.date_joined).replace(day=1),
+        "first_login": timezone.localdate(user.first_login).replace(day=1) if user.first_login else None,
+        "last_login": timezone.localdate(user.last_login).replace(day=1) if user.last_login else None,
+        "user_signup_kind": getattr(user.created_by, "kind", None),
+        "department": user.department,
+        "title": user.title,
+        "identity_provider": user.identity_provider,
+        "had_pole_emploi_id": bool(user.jobseeker_profile.pole_emploi_id),
+        "had_nir": bool(user.jobseeker_profile.nir),
+        "lack_of_nir_reason": user.jobseeker_profile.lack_of_nir_reason,
+        "nir_sex": user.jobseeker_profile.nir[0] if user.jobseeker_profile.nir else None,
+        "nir_year": user.jobseeker_profile.nir[1:3] if user.jobseeker_profile.nir else None,
+        "birth_year": user.jobseeker_profile.birthdate.year if user.jobseeker_profile.birthdate else None,
+    }
+
+    return ArchivedJobSeeker(**kwargs)
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        super().add_arguments(parser)
+        parser.add_argument(
+            "--wet-run",
+            action="store_true",
+            help="Perform the actual archiving of jobseekers",
+        )
+
+        parser.add_argument(
+            "--batch-size",
+            action="store",
+            type=int,
+            default=BATCH_SIZE,
+            help="Number of jobseekers to process in a batch",
+        )
+
+    @transaction.atomic
+    def notify_inactive_jobseekers(self):
+        now = timezone.now()
+        inactive_since = now - INACTIVITY_PERIOD
+        self.logger.info("Notifying inactive job seekers without activity before: %s", inactive_since)
+        users = list(
+            inactive_jobseekers_without_related_objects(inactive_since=inactive_since, batch_size=self.batch_size)
+        )
+
+        if self.wet_run:
+            for user in users:
+                InactiveJobSeeker(
+                    user,
+                    job_seeker=user,
+                    end_of_grace_period=now + GRACE_PERIOD,
+                ).send()
+            User.objects.filter(id__in=[user.id for user in users]).update(upcoming_deletion_notified_at=now)
+
+        logger.info("Notified inactive job seekers without recent activity: %s", len(users))
+
+    def reset_notified_jobseekers_with_recent_activity(self):
+        self.logger.info("Reseting inactive job seekers with recent activity")
+
+        users_to_reset_qs = (
+            User.objects.filter(kind=UserKind.JOB_SEEKER, upcoming_deletion_notified_at__isnull=False)
+            .job_seekers_with_last_activity()
+            .filter(last_activity__gte=F("upcoming_deletion_notified_at"))
+        )
+
+        if self.wet_run:
+            reset_nb = users_to_reset_qs.update(upcoming_deletion_notified_at=None)
+        else:
+            reset_nb = users_to_reset_qs.count()
+        self.logger.info("Reset notified job seekers with recent activity: %s", reset_nb)
+
+    @transaction.atomic
+    def archive_jobseekers_after_grace_period(self):
+        now = timezone.now()
+        grace_period_since = now - GRACE_PERIOD
+        self.logger.info("Archiving job seekers after grace period, notified before: %s", grace_period_since)
+
+        users_to_archive = list(
+            User.objects.filter(kind=UserKind.JOB_SEEKER, upcoming_deletion_notified_at__lte=grace_period_since)[
+                : self.batch_size
+            ]
+        )
+        archived_jobseekers = [anonymized_jobseeker(user) for user in users_to_archive]
+
+        if self.wet_run:
+            for user in users_to_archive:
+                ArchiveJobSeeker(
+                    user,
+                    job_seeker=user,
+                ).send()
+            ArchivedJobSeeker.objects.bulk_create(archived_jobseekers)
+            User.objects.filter(id__in=[user.id for user in users_to_archive]).delete()
+
+        self.logger.info("Archived jobseekers after grace period, count: %d", len(archived_jobseekers))
+
+    @monitor(
+        monitor_slug="notify_archive_jobseekers",
+        monitor_config={
+            "schedule": {"type": "crontab", "value": "*/5 7-20 * * MON-FRI"},
+            "checkin_margin": 5,
+            "max_runtime": 10,
+            "failure_issue_threshold": 2,
+            "recovery_threshold": 1,
+            "timezone": "UTC",
+        },
+    )
+    def handle(self, *args, wet_run, batch_size, **options):
+        self.wet_run = wet_run
+        self.batch_size = batch_size
+        self.logger.info("Start notifying and archiving jobseekers in %s mode", "wet_run" if wet_run else "dry_run")
+
+        self.reset_notified_jobseekers_with_recent_activity()
+        self.notify_inactive_jobseekers()
+        self.archive_jobseekers_after_grace_period()
---
+++ tests/archive/tests_management_command.py
@@ -0,0 +1,448 @@
+import datetime
+
+import pytest
+from django.core.management import call_command
+from django.utils import timezone
+
+from itou.archive.management.commands.notify_archive_jobseekers import GRACE_PERIOD
+from itou.archive.models import ArchivedJobSeeker
+from itou.users.models import User
+from tests.approvals.factories import ApprovalFactory
+from tests.eligibility.factories import (
+    GEIQEligibilityDiagnosisFactory,
+    IAEEligibilityDiagnosisFactory,
+)
+from tests.gps.factories import FollowUpGroupFactory
+from tests.job_applications.factories import JobApplicationFactory
+from tests.users.factories import (
+    EmployerFactory,
+    ItouStaffFactory,
+    JobSeekerFactory,
+    LaborInspectorFactory,
+    PrescriberFactory,
+)
+
+
+class TestNotifyArchiveJobSeekersManagementCommand:
+    @pytest.mark.parametrize("wet_run", [True, False])
+    @pytest.mark.parametrize(
+        "kwargs, model",
+        [
+            pytest.param(
+                {"joined_days_ago": 365},
+                "user",
+                id="jobseeker_to_notify",
+            ),
+            pytest.param(
+                {"joined_days_ago": 365, "notified_days_ago": 1, "last_login": timezone.now()},
+                "user",
+                id="notified_jobseeker_to_reset",
+            ),
+            pytest.param(
+                {"joined_days_ago": 365, "notified_days_ago": 30},
+                "archived_jobseeker",
+                id="jobseeker_to_archive",
+            ),
+        ],
+    )
+    def test_dry_run(self, kwargs, model, wet_run):
+        jobseeker = JobSeekerFactory(**kwargs)
+        call_command("notify_archive_jobseekers", wet_run=wet_run)
+
+        if not wet_run or model == "user":
+            assert jobseeker == User.objects.get()
+            assert not ArchivedJobSeeker.objects.exists()
+        elif model == "archived_jobseeker":
+            assert ArchivedJobSeeker.objects.count() == 1
+            assert not User.objects.exists()
+
+    @pytest.mark.parametrize(
+        "kwargs, model",
+        [
+            pytest.param(
+                {"joined_days_ago": 365},
+                "user",
+                id="jobseeker_to_notify",
+            ),
+            pytest.param(
+                {"joined_days_ago": 365, "notified_days_ago": 30},
+                "archived_jobseeker",
+                id="jobseeker_to_archive",
+            ),
+        ],
+    )
+    def test_batch_size(self, kwargs, model):
+        JobSeekerFactory.create_batch(3, **kwargs)
+        call_command("notify_archive_jobseekers", batch_size=2, wet_run=True)
+
+        if model == "user":
+            assert User.objects.filter(upcoming_deletion_notified_at__isnull=True).count() == 1
+            assert User.objects.exclude(upcoming_deletion_notified_at__isnull=True).count() == 2
+        else:
+            assert ArchivedJobSeeker.objects.count() == 2
+            assert User.objects.count() == 1
+
+    @pytest.mark.parametrize(
+        "factory, related_object_factory, updated_notification_date",
+        [
+            pytest.param(
+                lambda: JobSeekerFactory(joined_days_ago=365, for_snapshot=True),
+                None,
+                True,
+                id="jobseeker_without_recent_activity",
+            ),
+            pytest.param(
+                lambda: JobSeekerFactory(joined_days_ago=365, is_active=False),
+                None,
+                True,
+                id="deactivated_jobseeker_without_recent_activity",
+            ),
+            pytest.param(
+                lambda: JobSeekerFactory(joined_days_ago=365, last_login=timezone.now()),
+                None,
+                False,
+                id="jobseeker_with_recent_activity",
+            ),
+            pytest.param(
+                lambda: JobSeekerFactory(joined_days_ago=365),
+                lambda jobseeker: JobApplicationFactory(job_seeker=jobseeker),
+                False,
+                id="jobseeker_with_recent_job_application",
+            ),
+            pytest.param(
+                lambda: JobSeekerFactory(joined_days_ago=365),
+                lambda jobseeker: ApprovalFactory(user=jobseeker),
+                False,
+                id="jobseeker_with_recent_approval",
+            ),
+            pytest.param(
+                lambda: JobSeekerFactory(joined_days_ago=365),
+                lambda jobseeker: IAEEligibilityDiagnosisFactory(job_seeker=jobseeker, from_prescriber=True),
+                False,
+                id="jobseeker_with_recent_eligibility_diagnosis",
+            ),
+            pytest.param(
+                lambda: JobSeekerFactory(joined_days_ago=365),
+                lambda jobseeker: GEIQEligibilityDiagnosisFactory(job_seeker=jobseeker, from_prescriber=True),
+                False,
+                id="jobseeker_with_recent_geiq_eligibility_diagnosis",
+            ),
+            pytest.param(
+                lambda: JobSeekerFactory(joined_days_ago=365),
+                lambda jobseeker: FollowUpGroupFactory(beneficiary=jobseeker),
+                False,
+                id="jobseeker_with_recent_follow_up_group",
+            ),
+            pytest.param(
+                lambda: PrescriberFactory(joined_days_ago=365),
+                None,
+                False,
+                id="prescriber_without_recent_activity",
+            ),
+            pytest.param(
+                lambda: EmployerFactory(joined_days_ago=365),
+                None,
+                False,
+                id="employer_without_recent_activity",
+            ),
+            pytest.param(
+                lambda: LaborInspectorFactory(joined_days_ago=365),
+                None,
+                False,
+                id="labor_inspector_without_recent_activity",
+            ),
+            pytest.param(
+                lambda: ItouStaffFactory(joined_days_ago=365),
+                None,
+                False,
+                id="itou_staff_without_recent_activity",
+            ),
+        ],
+    )
+    def test_notify_inactive_jobseekers(
+        self,
+        factory,
+        related_object_factory,
+        updated_notification_date,
+        django_capture_on_commit_callbacks,
+        caplog,
+        mailoutbox,
+        snapshot,
+    ):
+        user = factory()
+        if related_object_factory:
+            related_object_factory(user)
+
+        with django_capture_on_commit_callbacks(execute=True):
+            call_command("notify_archive_jobseekers", wet_run=True)
+
+        user.refresh_from_db()
+        assert (user.upcoming_deletion_notified_at is not None) == updated_notification_date
+
+        if updated_notification_date:
+            assert "Notified inactive job seekers without recent activity: 1" in caplog.messages
+
+            if user.is_active:
+                [mail] = mailoutbox
+                assert [user.email] == mail.to
+                assert mail.subject == snapshot(name="inactive_jobseeker_email_subject")
+                fmt_date_joined = timezone.localdate(user.date_joined).strftime("%d/%m/%Y")
+                fmt_end_of_grace = (timezone.localdate(user.upcoming_deletion_notified_at) + GRACE_PERIOD).strftime(
+                    "%d/%m/%Y"
+                )
+                body = mail.body.replace(fmt_date_joined, "XX/XX/XXXX").replace(fmt_end_of_grace, "YY/YY/YYYY")
+                assert body == snapshot(name="inactive_jobseeker_email_body")
+            else:
+                assert not mailoutbox
+
+        else:
+            assert "Notified inactive job seekers without recent activity: 0" in caplog.messages
+            assert not mailoutbox
+
+    @pytest.mark.parametrize(
+        "factory, related_object_factory, notification_reset",
+        [
+            pytest.param(
+                lambda: JobSeekerFactory(
+                    joined_days_ago=365,
+                    notified_days_ago=29,
+                ),
+                None,
+                False,
+                id="notified_jobseeker",
+            ),
+            pytest.param(
+                lambda: JobSeekerFactory(joined_days_ago=365, notified_days_ago=1, last_login=timezone.now()),
+                None,
+                True,
+                id="notified_jobseeker_with_recent_login",
+            ),
+            pytest.param(
+                lambda: JobSeekerFactory(
+                    date_joined=timezone.now(),
+                    notified_days_ago=1,
+                ),
+                None,
+                True,
+                id="notified_jobseeker_with_recent_date_joined",
+            ),
+            pytest.param(
+                lambda: JobSeekerFactory(joined_days_ago=365, notified_days_ago=1, is_active=False),
+                lambda jobseeker: JobApplicationFactory(job_seeker=jobseeker),
+                True,
+                id="inactive_jobseeker_with_recent_job_application",
+            ),
+            pytest.param(
+                lambda: JobSeekerFactory(joined_days_ago=365, notified_days_ago=1),
+                lambda jobseeker: JobApplicationFactory(job_seeker=jobseeker),
+                True,
+                id="notified_jobseeker_with_recent_job_application",
+            ),
+            pytest.param(
+                lambda: JobSeekerFactory(joined_days_ago=365, notified_days_ago=1),
+                lambda jobseeker: ApprovalFactory(user=jobseeker),
+                True,
+                id="notified_jobseeker_with_recent_approval",
+            ),
+            pytest.param(
+                lambda: JobSeekerFactory(joined_days_ago=365, notified_days_ago=1),
+                lambda jobseeker: IAEEligibilityDiagnosisFactory(job_seeker=jobseeker, from_prescriber=True),
+                True,
+                id="notified_jobseeker_with_recent_eligibility_diagnosis",
+            ),
+            pytest.param(
+                lambda: JobSeekerFactory(joined_days_ago=365, notified_days_ago=1),
+                lambda jobseeker: GEIQEligibilityDiagnosisFactory(job_seeker=jobseeker, from_prescriber=True),
+                True,
+                id="notified_jobseeker_with_recent_geiq_eligibility_diagnosis",
+            ),
+            pytest.param(
+                lambda: JobSeekerFactory(joined_days_ago=365, notified_days_ago=1),
+                lambda jobseeker: FollowUpGroupFactory(beneficiary=jobseeker),
+                True,
+                id="notified_jobseeker_with_recent_follow_up_group",
+            ),
+            pytest.param(
+                lambda: ItouStaffFactory(joined_days_ago=365, notified_days_ago=1, last_login=timezone.now()),
+                None,
+                False,
+                id="itoustaff_with_recent_login",
+            ),
+            pytest.param(
+                lambda: LaborInspectorFactory(joined_days_ago=365, notified_days_ago=1, last_login=timezone.now()),
+                None,
+                False,
+                id="labor_inspector_with_recent_login",
+            ),
+            pytest.param(
+                lambda: EmployerFactory(joined_days_ago=365, notified_days_ago=1, last_login=timezone.now()),
+                None,
+                False,
+                id="employer_with_recent_login",
+            ),
+            pytest.param(
+                lambda: PrescriberFactory(joined_days_ago=365, notified_days_ago=1, last_login=timezone.now()),
+                None,
+                False,
+                id="prescriber_with_recent_login",
+            ),
+        ],
+    )
+    def test_reset_notified_jobseekers_with_recent_activity(self, factory, related_object_factory, notification_reset):
+        user = factory()
+        if related_object_factory:
+            related_object_factory(user)
+
+        call_command("notify_archive_jobseekers", wet_run=True)
+
+        user.refresh_from_db()
+        assert (user.upcoming_deletion_notified_at is None) == notification_reset
+
+    @pytest.mark.parametrize(
+        "user_factory",
+        [
+            pytest.param(
+                lambda: JobSeekerFactory(
+                    notified_days_ago=29,
+                ),
+                id="jobseeker_notified_still_in_grace_period",
+            ),
+            pytest.param(
+                lambda: JobSeekerFactory(
+                    upcoming_deletion_notified_at=None,
+                ),
+                id="jobseeker_never_notified",
+            ),
+            pytest.param(
+                lambda: EmployerFactory(is_active=False, notified_days_ago=30),
+                id="employer",
+            ),
+            pytest.param(
+                lambda: PrescriberFactory(notified_days_ago=30),
+                id="prescriber",
+            ),
+            pytest.param(
+                lambda: ItouStaffFactory(notified_days_ago=30),
+                id="itou_staff",
+            ),
+            pytest.param(
+                lambda: LaborInspectorFactory(notified_days_ago=30),
+                id="laborinspector",
+            ),
+        ],
+    )
+    def test_exclude_users_when_archiving(self, user_factory):
+        user = user_factory()
+        call_command("notify_archive_jobseekers", wet_run=True)
+
+        expected_user = User.objects.get()
+        assert user == expected_user
+        assert not ArchivedJobSeeker.objects.exists()
+
+    @pytest.mark.parametrize(
+        "kwargs",
+        [
+            pytest.param(
+                {
+                    "title": "MME",
+                    "first_name": "Johanna",
+                    "last_name": "Andrews",
+                    "date_joined": timezone.make_aware(datetime.datetime(2020, 4, 18, 0, 0)),
+                    "first_login": timezone.make_aware(datetime.datetime(2020, 4, 18, 0, 0)),
+                    "last_login": timezone.make_aware(datetime.datetime(2020, 4, 18, 0, 0)),
+                    "created_by": PrescriberFactory,
+                    "jobseeker_profile__pole_emploi_id": "12345678",
+                    "jobseeker_profile__nir": "855456789012345",
+                    "jobseeker_profile__lack_of_nir_reason": "",
+                    "jobseeker_profile__birthdate": datetime.date(1985, 6, 8),
+                },
+                id="jobseeker_with_all_datas_created_by_prescriber",
+            ),
+            pytest.param(
+                {
+                    "title": "MME",
+                    "first_name": "Johan",
+                    "last_name": "Anderson",
+                    "date_joined": timezone.make_aware(datetime.datetime(2021, 12, 12, 0, 0)),
+                    "first_login": timezone.make_aware(datetime.datetime(2021, 12, 13, 0, 0)),
+                    "last_login": timezone.make_aware(datetime.datetime(2021, 12, 14, 0, 0)),
+                    "created_by": EmployerFactory,
+                    "jobseeker_profile__pole_emploi_id": "45678123",
+                    "jobseeker_profile__nir": "655456789012345",
+                    "jobseeker_profile__lack_of_nir_reason": "",
+                    "jobseeker_profile__birthdate": datetime.date(1990, 12, 15),
+                },
+                id="jobseeker_with_all_datas_created_by_employer",
+            ),
+            pytest.param(
+                {
+                    "title": "M",
+                    "first_name": "Martin",
+                    "last_name": "Jacobson",
+                    "date_joined": timezone.make_aware(datetime.datetime(2022, 11, 3, 0, 0)),
+                    "first_login": None,
+                    "last_login": None,
+                    "created_by": None,
+                    "jobseeker_profile__pole_emploi_id": "",
+                    "jobseeker_profile__nir": "",
+                    "jobseeker_profile__lack_of_nir_reason": "reason",
+                    "jobseeker_profile__birthdate": None,
+                },
+                id="jobseeker_with_very_few_datas",
+            ),
+            pytest.param(
+                {
+                    "title": "M",
+                    "first_name": "Stephan",
+                    "last_name": "Xiao",
+                    "date_joined": timezone.make_aware(datetime.datetime(2023, 10, 30, 0, 0)),
+                    "jobseeker_profile__nir": "74185296365487",
+                    "jobseeker_profile__birthdate": datetime.date(1964, 5, 30),
+                    "is_active": False,
+                },
+                id="jobseeker_not_is_active",
+            ),
+        ],
+    )
+    def test_archive_inactive_jobseekers_after_grace_period(
+        self, kwargs, django_capture_on_commit_callbacks, caplog, mailoutbox, snapshot
+    ):
+        if kwargs.get("created_by"):
+            kwargs["created_by"] = kwargs["created_by"]()
+
+        jobseeker = JobSeekerFactory(notified_days_ago=31, **kwargs)
+
+        with django_capture_on_commit_callbacks(execute=True):
+            call_command("notify_archive_jobseekers", wet_run=True)
+
+        assert not User.objects.filter(id=jobseeker.id).exists()
+
+        archived_jobseeker = ArchivedJobSeeker.objects.all().values(
+            "date_joined",
+            "first_login",
+            "last_login",
+            "user_signup_kind",
+            "department",
+            "title",
+            "identity_provider",
+            "had_pole_emploi_id",
+            "had_nir",
+            "lack_of_nir_reason",
+            "nir_sex",
+            "nir_year",
+            "birth_year",
+        )
+        assert list(archived_jobseeker) == snapshot(name="archived_jobseeker")
+
+        assert "Archived jobseekers after grace period, count: 1" in caplog.messages
+        if jobseeker.is_active:
+            [mail] = mailoutbox
+            assert jobseeker.email == mail.to[0]
+            assert mail.subject == snapshot(name="archived_jobseeker_email_subject")
+
+            body = mail.body.replace(
+                timezone.localdate(jobseeker.upcoming_deletion_notified_at).strftime("%d/%m/%Y"), "XX/XX/XXXX"
+            )
+            assert body == snapshot(name="archived_jobseeker_email_body")
+        else:
+            assert not mailoutbox
