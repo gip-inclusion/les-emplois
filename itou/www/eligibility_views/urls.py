3c05f26f12f6ee43e5b606aa60593be0514b4ef1
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2025-04-18 08:08:55 +0200

    eligibility_views: Add tunnel as kwarg in path

--- itou/www/eligibility_views/urls.py
+++ itou/www/eligibility_views/urls.py
@@ -6,5 +6,10 @@ from itou.www.eligibility_views import views
 app_name = "eligibility_views"
 
 urlpatterns = [
-    path("update/<uuid:job_seeker_public_id>", views.UpdateEligibilityView.as_view(), name="update"),
+    path(
+        "update/<uuid:job_seeker_public_id>",
+        views.UpdateEligibilityView.as_view(),
+        name="update",
+        kwargs={"tunnel": "standalone_process"},
+    ),
 ]
