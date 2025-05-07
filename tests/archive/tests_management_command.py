1fab7b05fc948a3db1cc72559d52bc51ee9ff0b4
vincent porte <vincent@neuralia.co>
2025-05-07 15:36:34 +0200

    refactor dry_run test

--- tests/archive/tests_management_command.py
+++ tests/archive/tests_management_command.py
@@ -6,7 +6,7 @@ from django.core.management import call_command
 from django.utils import timezone
 from freezegun import freeze_time
 
-from itou.archive.models import ArchivedApplication, ArchivedJobSeeker
+from itou.archive.models import ArchivedApplication, ArchivedJobSeeker, ArchivedProfessional
 from itou.companies.enums import CompanyKind, ContractNature, ContractType
 from itou.gps.models import FollowUpGroup, FollowUpGroupMembership
 from itou.job_applications.enums import JobApplicationState
@@ -33,37 +33,53 @@ from tests.users.factories import (
 
 
 class TestNotifyArchiveUsersManagementCommand:
-    @pytest.mark.parametrize("wet_run", [True, False])
     @pytest.mark.parametrize(
-        "kwargs, model",
+        "factory,kwargs",
         [
             pytest.param(
+                JobSeekerFactory,
                 {"joined_days_ago": DAYS_OF_INACTIVITY},
-                "user",
                 id="jobseeker_to_notify",
             ),
             pytest.param(
+                JobSeekerFactory,
                 {"joined_days_ago": DAYS_OF_INACTIVITY, "notified_days_ago": 1, "last_login": timezone.now()},
-                "user",
                 id="notified_jobseeker_to_reset",
             ),
             pytest.param(
+                JobSeekerFactory,
                 {"joined_days_ago": DAYS_OF_INACTIVITY, "notified_days_ago": 30},
-                "archived_jobseeker",
                 id="jobseeker_to_archive",
+            ),
+            pytest.param(
+                PrescriberFactory,
+                {"last_login_days_ago": DAYS_OF_INACTIVITY},
+                id="professional_to_notify",
+            ),
+            pytest.param(
+                PrescriberFactory,
+                {"last_login_days_ago": DAYS_OF_INACTIVITY, "notified_days_ago": 1, "last_login": timezone.now()},
+                id="notified_professional_to_reset",
+            ),
+            pytest.param(
+                PrescriberFactory,
+                {"last_login_days_ago": DAYS_OF_INACTIVITY, "notified_days_ago": 30},
+                id="professional_to_archive",
             ),
         ],
     )
-    def test_dry_run(self, kwargs, model, wet_run):
-        jobseeker = JobSeekerFactory(**kwargs)
-        call_command("notify_archive_users", wet_run=wet_run)
+    def test_dry_run(self, factory, kwargs, django_capture_on_commit_callbacks, mailoutbox):
+        user = factory(**kwargs)
 
-        if not wet_run or model == "user":
-            assert jobseeker == User.objects.get()
-            assert not ArchivedJobSeeker.objects.exists()
-        elif model == "archived_jobseeker":
-            assert ArchivedJobSeeker.objects.count() == 1
-            assert not User.objects.exists()
+        with django_capture_on_commit_callbacks(execute=True):
+            call_command("notify_archive_users")
+
+        unmodified_user = User.objects.get()
+        assert user == unmodified_user
+        assert not mailoutbox
+        assert not ArchivedJobSeeker.objects.exists()
+        assert not ArchivedApplication.objects.exists()
+        assert not ArchivedProfessional.objects.exists()
 
     @pytest.mark.parametrize(
         "kwargs, model",
