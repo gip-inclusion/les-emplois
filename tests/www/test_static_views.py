f292126158fb43dfbfd438fd0a8114d14b2b6d01
hellodeloo <hello@hellodeloo.com>
2025-05-07 11:20:41 +0200

    fix: update content of the accessibility page

--- tests/www/test_static_views.py
+++ tests/www/test_static_views.py
@@ -6,7 +6,7 @@ from pytest_django.asserts import assertContains
 @pytest.mark.parametrize(
     "view_name,expected_title",
     [
-        ("accessibility", "<h1>Déclaration d'accessibilité</h1>"),
+        ("accessibility", "<h1>Accessibilité</h1>"),
         ("legal-notice", "<h1>Mentions légales</h1>"),
         ("legal-privacy", "<h1>Politique de confidentialité</h1>"),
         ("legal-terms", '<h1 id="conditions-générales-dutilisation">Conditions Générales d&#39;Utilisation</h1>'),
