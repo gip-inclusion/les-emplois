1e0411bddb7e06f5b9ece3d0cd4eb24faf780ba8
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2025-05-12 12:24:25 +0200

    files: Add download link in admin

--- itou/files/admin.py
+++ itou/files/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.core.files.storage import default_storage
+from django.utils.html import format_html
 
 from itou.files.models import File
 from itou.utils.admin import ItouModelAdmin
@@ -7,4 +9,13 @@ from itou.utils.admin import ItouModelAdmin
 @admin.register(File)
 class FileAdmin(ItouModelAdmin):
     list_display = ["key", "last_modified"]
-    readonly_fields = ["key", "last_modified"]
+    readonly_fields = ["key", "link", "last_modified"]
+
+    fields = ["key", "link", "last_modified", "deleted_at"]
+
+    @admin.display(description="lien")
+    def link(self, obj):
+        return format_html(
+            "<a href='{}' target='_blank'>Télécharger</a>",
+            default_storage.url(obj.key),
+        )
