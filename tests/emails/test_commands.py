e0c21f81dddfd92c524f30be91a5a7f0b94a98a2
François Freitag <mail@franek.fr>
2025-05-12 16:12:03 +0200

    Delete emails from the database sooner
    We’re sending approximately 400K emails per month, which ends up around
    1.2 GB compressed data in dump, and 8 GB uncompressed. That’s pretty
    large, and audit usually don’t require going back a year. Even the new
    limit of 6 months is pretty conservative.

--- tests/emails/test_commands.py
+++ tests/emails/test_commands.py
@@ -11,9 +11,9 @@ from itou.emails.models import Email
 class TestExpireOldEmails:
     def test_dry_run(self, caplog):
         now = timezone.now()
-        with freeze_time(now - timedelta(days=365)):
+        with freeze_time(now - timedelta(days=182)):
             old = Email.objects.create(to=["old@test.local"], subject="Old stuff", body_text="Old")
-        with freeze_time(now - timedelta(days=364)):
+        with freeze_time(now - timedelta(days=181)):
             after_cutoff = Email.objects.create(to=["recent@test.local"], subject="Recent stuff", body_text="Recent")
         call_command("delete_old_emails")
         assert caplog.messages[0] == "Would delete 1 email"
@@ -21,9 +21,9 @@ class TestExpireOldEmails:
 
     def test_wet_run(self, caplog):
         now = timezone.now()
-        with freeze_time(now - timedelta(days=365)):
+        with freeze_time(now - timedelta(days=182)):
             Email.objects.create(to=["old@test.local"], subject="Old stuff", body_text="Old")
-        with freeze_time(now - timedelta(days=364)):
+        with freeze_time(now - timedelta(days=181)):
             after_cutoff = Email.objects.create(to=["recent@test.local"], subject="Recent stuff", body_text="Recent")
         call_command("delete_old_emails", wet_run=True)
         assert caplog.messages[0] == "Deleted 1 email"
