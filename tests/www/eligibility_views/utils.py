fc2360c6bfd981675c1c77a4ca489a9008cc037d
François Freitag <mail@franek.fr>
2025-05-14 21:48:30 +0200

    Verify certified badge isn’t shown after diagnosis expiration
    The computation of certification expiry was incorrect, a criteria was
    considered valid much longer than intended. This bug hasn’t surfaced to
    employers though, because the eligibility diagnosis validity period has
    been adjusted to match the certification period, so the diagnosis
    expired even though the certification was still valid.
    
    This commit adds a test to confirm employers did not see the certified
    badge.
    
    Refs https://github.com/gip-inclusion/les-emplois/pull/6126

---
+++ tests/www/eligibility_views/utils.py
@@ -0,0 +1,8 @@
+CERTIFIED_BADGE_HTML = """\
+<span class="badge badge-xs rounded-pill bg-info-lighter text-info ms-3">
+    <i class="ri-verified-badge-fill" aria-hidden="true"></i>
+    Certifié</span>"""
+NOT_CERTIFIED_BADGE_HTML = """\
+<span class="badge badge-xs rounded-pill bg-warning-lighter text-warning ms-3">
+    <i class="ri-error-warning-fill" aria-hidden="true"></i>
+    Non certifié</span>"""
