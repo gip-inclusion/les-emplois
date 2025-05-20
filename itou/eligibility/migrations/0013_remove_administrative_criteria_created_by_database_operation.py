988b1c3d837b4875dd5fb6afc600a86112f84773
vincent porte <vincent@neuralia.co>
2025-05-20 15:46:39 +0200

    cleanup double separate_state_and_database migrations

--- itou/eligibility/migrations/0013_remove_administrative_criteria_created_by_database_operation.py
+++ itou/eligibility/migrations/0013_remove_administrative_criteria_created_by_database_operation.py
@@ -6,17 +6,4 @@ class Migration(migrations.Migration):
         ("eligibility", "0012_remove_administrativecriteria_created_by_state_operation"),
     ]
 
-    operations = [
-        migrations.RunSQL(
-            sql="ALTER TABLE eligibility_administrativecriteria DROP COLUMN IF EXISTS created_by_id",
-            reverse_sql=(
-                "ALTER TABLE eligibility_administrativecriteria ADD COLUMN created_by_id timestamp DEFAULT NULL"
-            ),
-        ),
-        migrations.RunSQL(
-            sql="ALTER TABLE eligibility_geiqadministrativecriteria DROP COLUMN IF EXISTS created_by_id",
-            reverse_sql=(
-                "ALTER TABLE eligibility_geiqadministrativecriteria ADD COLUMN created_by_id timestamp DEFAULT NULL"
-            ),
-        ),
-    ]
+    operations = []
