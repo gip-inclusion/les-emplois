7400c93d6391f478aba2733ed5815ebefa4503b6
Ewen Corre <ewen.corre@inclusion.gouv.fr>
2025-05-30 11:16:07 +0200

    dashboard: display geiq eligibility diagnosis on job seeker dashboard

--- itou/www/dashboard/views.py
+++ itou/www/dashboard/views.py
@@ -20,8 +20,9 @@ from rest_framework.authtoken.models import Token
 from itou.api.token_auth.views import TOKEN_ID_STR
 from itou.approvals.enums import ProlongationRequestStatus
 from itou.approvals.models import ProlongationRequest
+from itou.eligibility.models.geiq import GEIQEligibilityDiagnosis
 from itou.eligibility.models.iae import EligibilityDiagnosis
-from itou.eligibility.utils import iae_criteria_for_display
+from itou.eligibility.utils import geiq_criteria_for_display, iae_criteria_for_display
 from itou.employee_record.enums import Status
 from itou.employee_record.models import EmployeeRecord
 from itou.institutions.enums import InstitutionKind
@@ -171,11 +172,23 @@ def dashboard(request, template_name="dashboard/dashboard.html"):
                 return HttpResponseRedirect(reverse("dashboard:edit_user_info"))
 
         iae_eligibility_diagnosis = EligibilityDiagnosis.objects.last_for_job_seeker(request.user)
+        geiq_eligibility_diagnosis = (
+            GEIQEligibilityDiagnosis.objects.diagnoses_for(
+                job_seeker=request.user,
+                for_geiq=None,
+                for_job_seeker=True,
+            )
+            .prefetch_related("selected_administrative_criteria__administrative_criteria")
+            .first()
+        )
         if iae_eligibility_diagnosis:
             iae_eligibility_diagnosis.criteria_display = iae_criteria_for_display(iae_eligibility_diagnosis)
+        if geiq_eligibility_diagnosis:
+            geiq_eligibility_diagnosis.criteria_display = geiq_criteria_for_display(geiq_eligibility_diagnosis)
 
         context |= {
             "iae_eligibility_diagnosis": iae_eligibility_diagnosis,
+            "geiq_eligibility_diagnosis": geiq_eligibility_diagnosis,
         }
     return render(request, template_name, context)
 
