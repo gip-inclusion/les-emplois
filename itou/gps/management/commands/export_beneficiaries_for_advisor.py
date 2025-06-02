650c0fcc5363faf1a315d7f27c31c4fa9690212b
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2025-06-02 15:21:39 +0200

    gps: export all beneficiaries
    No need to filter with the department anymore

--- itou/gps/management/commands/export_beneficiaries_for_advisor.py
+++ itou/gps/management/commands/export_beneficiaries_for_advisor.py
@@ -1,3 +1,7 @@
+import csv
+import os
+
+from django.conf import settings
 from django.utils import timezone
 
 from itou.users.enums import UserKind
@@ -13,39 +17,37 @@ class Command(BaseCommand, XlsxExportMixin):
 
     help = "Export job seekers to give to FT in order to retrieve advisor contact information."
 
-    def add_arguments(self, parser):
-        parser.add_argument(
-            "department",
-            type=str,
-            help="The department of the beneficiaries",
-        )
-
-    def handle(self, department, **options):
-        job_seekers = (
-            User.objects.filter(department=department, kind=UserKind.JOB_SEEKER)
-            .select_related("jobseeker_profile")
-            .order_by("pk")
-        )
+    def handle(self, **options):
+        job_seekers = User.objects.filter(kind=UserKind.JOB_SEEKER).select_related("jobseeker_profile").order_by("pk")
 
         headers = [
             "ID",
             "prénom",
             "nom",
             "nir",
+            "identifiant_ft",
             "date_de_naissance",
         ]
 
-        data = [
-            [
+        def serialize_job_seeker(job_seeker):
+            return [
                 str(job_seeker.pk),
                 job_seeker.first_name.capitalize(),
                 job_seeker.last_name.upper(),
                 job_seeker.jobseeker_profile.nir,
+                job_seeker.jobseeker_profile.pole_emploi_id,
                 job_seeker.jobseeker_profile.birthdate.strftime("%d/%m/%Y")
                 if job_seeker.jobseeker_profile.birthdate
                 else "",
             ]
-            for job_seeker in job_seekers
-        ]
-        filename = f"gps_dpt_{department}_{timezone.localtime().strftime('%Y-%m-%d_%H:%M')}.xlsx"
-        self.export_to_xlsx(filename, headers, data)
+
+        filename = f"gps_export_beneficiaires_{timezone.localtime().strftime('%Y-%m-%d_%H:%M')}.csv"
+
+        path = f"{settings.EXPORT_DIR}/{filename}"
+        os.makedirs(settings.EXPORT_DIR, exist_ok=True)
+
+        with open(path, "w") as file:
+            writer = csv.writer(file, delimiter=";")
+            writer.writerow(headers)
+            for job_seeker in job_seekers.iterator():
+                writer.writerow(serialize_job_seeker(job_seeker))
