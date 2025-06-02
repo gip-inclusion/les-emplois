8e10fc57673d09217bc050535f41ce8db280e0ff
François Freitag <mail@franek.fr>
2025-06-02 15:42:28 +0200

    siae_evaluations: Validate criteria selection rules
    Regression introduced in 9acbe5f32d0bf4d808a5798f6a86746a8fa7fd5e. The
    user attribute has been replaced with a `is_authorized_prescriber`.
    Passing a user (Python object), which `__bool__()` is always True,
    resulted in the form validation acting as-if an authorized prescriber
    was submitting the data, thus not validating eligibility. However, an
    employer is acting.

--- tests/www/eligibility_views/test_forms.py
+++ tests/www/eligibility_views/test_forms.py
@@ -198,7 +198,6 @@ class TestAdministrativeCriteriaEvaluationForm:
         )
 
         form = AdministrativeCriteriaEvaluationForm(
-            user,
             company,
             job_application.eligibility_diagnosis.selected_administrative_criteria.select_related(
                 "administrative_criteria"
