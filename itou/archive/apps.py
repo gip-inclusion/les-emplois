5220a60e3c545c09ae8fb25371c5e19b5e30aaaf
vincent porte <vincent@neuralia.co>
2025-06-03 16:34:12 +0200

    Add explicit verbose names in archive app
    Reduce ambiguity between the Archive app, its archive_jobseeker and archive_job_applications (etc ...)
    and job_applications with an archived state. The former are anonymous, not accessible to users.
    The latter are hidden in dedicated views.

--- itou/archive/apps.py
+++ itou/archive/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ArchiveConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "itou.archive"
+    verbose_name = "Archives anonymisées"
