b98422f67add9f1348ee91c3dc7b8a63ab32d78a
François Freitag <mail@franek.fr>
2025-06-05 14:49:35 +0200

    Use a template tag for the structure switcher
    The component is about to evolve to offer creating an antenna when user
    has permission. Using a template tag allows to control permissions for
    creating an antenna in Python.

---
+++ itou/utils/templatetags/structure_switcher.py
@@ -0,0 +1,66 @@
+from django import template
+from django.template.loader import get_template
+
+from itou.prescribers.enums import PrescriberOrganizationKind
+from itou.users.enums import UserKind
+from itou.utils.errors import silently_report_exception
+
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def structure_switcher(context, mobile):
+    try:
+        request = context["request"]
+        current_organization = getattr(request, "current_organization", None)
+        organizations = getattr(request, "organizations", [])
+        template_context = {
+            "csrf_token": context["csrf_token"],
+            "current_organization": current_organization,
+            "organizations": organizations,
+            "user": request.user,
+        }
+        userkind_context = {
+            UserKind.JOB_SEEKER: {
+                "icon": "ri-user-line",
+                "kind_display": "Candidat",
+                "no_org_display": request.user.get_full_name(),
+            },
+            UserKind.EMPLOYER: {
+                "icon": "ri-community-line",
+                "kind_display": "Employeur",
+                "no_org_display": "Compte inactif",
+            },
+            UserKind.PRESCRIBER: {
+                "icon": (
+                    "ri-user-line"
+                    if current_organization is None
+                    else (
+                        "ri-home-smile-line"
+                        if current_organization.kind != PrescriberOrganizationKind.OTHER
+                        else "ri-group-line"
+                    )
+                ),
+                "kind_display": "Orienteur seul" if current_organization is None else current_organization.kind,
+                "no_org_display": request.user.get_full_name(),
+            },
+            UserKind.LABOR_INSPECTOR: {
+                "icon": "ri-government-line",
+                "kind_display": "Inspecteur du travail",
+                "no_org_display": "Compte inactif",
+            },
+            UserKind.ITOU_STAFF: {
+                "icon": "ri-admin-line",
+                "kind_display": "Itou",
+                "no_org_display": "Staff",
+            },
+        }
+        template_context.update(userkind_context[request.user.kind])
+        template_name = (
+            "layout/_structure_switcher_offcanvas.html" if mobile else "layout/_structure_switcher_nav.html"
+        )
+        template = get_template(template_name)
+        return template.render(template_context)
+    except Exception as e:
+        silently_report_exception(e)
