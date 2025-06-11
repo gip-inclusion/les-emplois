d9efd6f6b57a6ccf2c1509fd176139932356bf2b
Romain Sébille <romain.sebille@beta.gouv.fr>
2025-06-11 10:29:32 +0200

    asp.sync_communes: Make some improvements

--- itou/asp/management/commands/sync_communes.py
+++ itou/asp/management/commands/sync_communes.py
@@ -22,12 +22,16 @@ def csv_import_communes(filename):
     with open(filename, encoding="utf-8") as f:
         reader = csv.reader(f, delimiter=";")
         next(reader)
-        for line in reader:
-            code = line[0]
-            name = line[1]
-            start_date = datetime.strptime(line[2], ASP_DATE_FORMAT).date() if line[2] else None
-            end_date = datetime.strptime(line[3], ASP_DATE_FORMAT).date() if line[3] else None
-            yield dict(code=code, name=name, start_date=start_date, end_date=end_date)
+        for line_number, line in enumerate(reader, start=2):
+            try:
+                code = f"{line[0]:0>5}"
+                name = line[1]
+                start_date = datetime.strptime(line[2], ASP_DATE_FORMAT).date() if line[2] else None
+                end_date = datetime.strptime(line[3], ASP_DATE_FORMAT).date() if line[3] else None
+                yield dict(code=code, name=name, start_date=start_date, end_date=end_date)
+            except Exception as e:
+                print(f"Error on line {line_number}: {e}")
+                raise
 
 
 def guess_commune(code, name, at=None):
@@ -63,6 +67,7 @@ class Command(BaseCommand):
             help="Path of the ASP CSV file to import",
         )
 
+    @transaction.atomic
     def handle(self, *, file_path, wet_run, **options):
         communes_from_csv = list(csv_import_communes(file_path))
 
@@ -93,102 +98,102 @@ class Command(BaseCommand):
                 if item.kind == DiffItemKind.EDITION:
                     commune.pk = item.db_obj.pk
                     communes_updated_by_csv.append(commune)
+                    self.stdout.write(f"{item.label} for {item.db_obj.name=} {item.db_obj.code=} {item.db_obj.pk=}\n")
                 elif item.kind == DiffItemKind.ADDITION:
                     communes_added_by_csv.append(commune)
-                self.stdout.write(f"{item.label}\n")
+                    self.stdout.write(f"{item.label}\n")
             elif item.kind == DiffItemKind.DELETION:
                 communes_removed_by_csv.add(item.db_obj.pk)
                 data = {key: getattr(item.db_obj, key) for key in ("code", "name", "start_date", "end_date")}
                 self.stdout.write(f"\tREMOVED {json.dumps(data, ensure_ascii=False, cls=DjangoJSONEncoder)}")
 
         if wet_run:
-            with transaction.atomic():
-                profiles_to_remap = []
-                for pk in communes_removed_by_csv:
-                    commune = Commune.objects.get(pk=pk)
-                    try:
-                        commune.delete()
-                    except RestrictedError as exc:
-                        for obj in exc.restricted_objects:
-                            if obj.birth_place == commune:
-                                obj.birth_place = None
-                                setattr(obj, "_birth_place", (commune.code, commune.name))
-                            if obj.hexa_commune == commune:
-                                obj.hexa_commune = None
-                                setattr(obj, "_hexa_commune", (commune.code, commune.name))
-                            profiles_to_remap.append(obj)
-                        JobSeekerProfile.objects.bulk_update(
-                            exc.restricted_objects,
-                            fields=["birth_place", "hexa_commune"],
-                        )
-                        commune.delete()
-                self.stdout.write(f"> successfully deleted count={len(communes_removed_by_csv)} communes")
-
-                n_objs = Commune.objects.bulk_update(
-                    communes_updated_by_csv,
-                    fields=[
-                        "code",
-                        "name",
-                        "start_date",
-                        "end_date",
-                        "city",
-                    ],
-                    batch_size=1000,
-                )
-                self.stdout.write(f"> successfully updated count={n_objs} communes")
-
-                objs = Commune.objects.bulk_create(communes_added_by_csv)
-                self.stdout.write(f"> successfully created count={len(objs)} new communes")
-
-                if profiles_to_remap:
-                    has_raised = False
-                    for profile in profiles_to_remap:
-                        if old_commune_info := getattr(profile, "_birth_place", None):
-                            try:
-                                new_commune = guess_commune(
-                                    *old_commune_info,
-                                    profile.birthdate,
-                                )
-                                self.stdout.write(
-                                    f"> REALIGN birth_place for code={old_commune_info[0]} "
-                                    f"name={old_commune_info[1]} "
-                                    f"at={profile.birthdate} "
-                                    f"found with new_name={new_commune.name} "
-                                    f"and start_date={new_commune.start_date}"
-                                )
-                            except Commune.DoesNotExist:
-                                self.stdout.write(
-                                    f"! new commune for code={old_commune_info[0]} "
-                                    f"at={profile.birthdate} "
-                                    f"name={old_commune_info[1]} not found ! Resolve manually."
-                                )
-                                has_raised = True
-                            else:
-                                profile.birth_place = new_commune
-
-                        if old_commune_info := getattr(profile, "_hexa_commune", None):
-                            try:
-                                new_commune = guess_commune(*old_commune_info)
-                                self.stdout.write(
-                                    f"> REALIGN hexa_commune for code={old_commune_info[0]} "
-                                    f"name={old_commune_info[1]} "
-                                    f"found with new_name={new_commune.name} "
-                                    f"and start_date={new_commune.start_date}"
-                                )
-                            except Commune.DoesNotExist:
-                                self.stdout.write(
-                                    f"! new commune for code={old_commune_info[0]} "
-                                    f"name={old_commune_info[1]} not found ! Resolve manually."
-                                )
-                                has_raised = True
-                            else:
-                                profile.hexa_commune = new_commune
-
-                    if has_raised:
-                        raise Exception("Some communes could not be found. Please resolve manually.")
-
-                    n_objs = JobSeekerProfile.objects.bulk_update(
-                        profiles_to_remap,
+            profiles_to_remap = []
+            for pk in communes_removed_by_csv:
+                commune = Commune.objects.get(pk=pk)
+                try:
+                    commune.delete()
+                except RestrictedError as exc:
+                    for obj in exc.restricted_objects:
+                        if obj.birth_place == commune:
+                            obj.birth_place = None
+                            setattr(obj, "_birth_place", (commune.code, commune.name))
+                        if obj.hexa_commune == commune:
+                            obj.hexa_commune = None
+                            setattr(obj, "_hexa_commune", (commune.code, commune.name))
+                        profiles_to_remap.append(obj)
+                    JobSeekerProfile.objects.bulk_update(
+                        exc.restricted_objects,
                         fields=["birth_place", "hexa_commune"],
                     )
-                    self.stdout.write(f"> realigned count={n_objs} JobSeeker profiles with updated communes.")
+                    commune.delete()
+            self.stdout.write(f"> successfully deleted count={len(communes_removed_by_csv)} communes")
+
+            n_objs = Commune.objects.bulk_update(
+                communes_updated_by_csv,
+                fields=[
+                    "code",
+                    "name",
+                    "start_date",
+                    "end_date",
+                    "city",
+                ],
+                batch_size=1000,
+            )
+            self.stdout.write(f"> successfully updated count={n_objs} communes")
+
+            objs = Commune.objects.bulk_create(communes_added_by_csv)
+            self.stdout.write(f"> successfully created count={len(objs)} new communes")
+
+            if profiles_to_remap:
+                has_raised = False
+                for profile in profiles_to_remap:
+                    if old_commune_info := getattr(profile, "_birth_place", None):
+                        try:
+                            new_commune = guess_commune(
+                                *old_commune_info,
+                                profile.birthdate,
+                            )
+                            self.stdout.write(
+                                f"> REALIGN birth_place for code={old_commune_info[0]} "
+                                f"name={old_commune_info[1]} "
+                                f"at={profile.birthdate} "
+                                f"found with new_name={new_commune.name} "
+                                f"and start_date={new_commune.start_date}"
+                            )
+                        except Commune.DoesNotExist:
+                            self.stdout.write(
+                                f"! new commune for code={old_commune_info[0]} "
+                                f"at={profile.birthdate} "
+                                f"name={old_commune_info[1]} not found ! Resolve manually."
+                            )
+                            has_raised = True
+                        else:
+                            profile.birth_place = new_commune
+
+                    if old_commune_info := getattr(profile, "_hexa_commune", None):
+                        try:
+                            new_commune = guess_commune(*old_commune_info)
+                            self.stdout.write(
+                                f"> REALIGN hexa_commune for code={old_commune_info[0]} "
+                                f"name={old_commune_info[1]} "
+                                f"found with new_name={new_commune.name} "
+                                f"and start_date={new_commune.start_date}"
+                            )
+                        except Commune.DoesNotExist:
+                            self.stdout.write(
+                                f"! new commune for code={old_commune_info[0]} "
+                                f"name={old_commune_info[1]} not found ! Resolve manually."
+                            )
+                            has_raised = True
+                        else:
+                            profile.hexa_commune = new_commune
+
+                if has_raised:
+                    raise Exception("Some communes could not be found. Please resolve manually.")
+
+                n_objs = JobSeekerProfile.objects.bulk_update(
+                    profiles_to_remap,
+                    fields=["birth_place", "hexa_commune"],
+                )
+                self.stdout.write(f"> realigned count={n_objs} JobSeeker profiles with updated communes.")
