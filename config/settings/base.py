9bff2aa1496d3584b1af90d7688574d8bc57cdda
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2025-06-13 14:22:33 +0200

    archive: Split notifying and archiving users in 2 commands
    Run archive_users every hour (7000 users per week)
    Run notify_inactive_users every working day (1000 users per week)
    
    We archive more users because we may have to pause archive_users to
    handle a strange behaviour.

--- config/settings/base.py
+++ config/settings/base.py
@@ -828,9 +828,9 @@ OTP_TOTP_ISSUER = f"Les Emplois de l'inclusion ({ITOU_ENVIRONMENT})"
 OTP_ADMIN_HIDE_SENSITIVE_DATA = True
 REQUIRE_OTP_FOR_STAFF = os.getenv("REQUIRE_OTP_FOR_STAFF", "True") == "True"
 
-# Notify_archive_users
+# archive_users
 # ------------------------------------------------------------------------------
-SUSPEND_NOTIFY_ARCHIVE_USERS = os.getenv("SUSPEND_NOTIFY_ARCHIVE_USERS", "False") == "True"
+SUSPEND_ARCHIVE_USERS = os.getenv("SUSPEND_ARCHIVE_USERS", "False") == "True"
 
 
 # Mainenance mode
