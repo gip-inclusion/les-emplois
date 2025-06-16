f2ad97c85e199d0ef298533f8f80bc7dacfb95f4
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2025-06-16 06:34:01 +0200

    files: Fix sync_s3_files
    Don't create an already present File

--- itou/files/management/commands/sync_s3_files.py
+++ itou/files/management/commands/sync_s3_files.py
@@ -19,16 +19,19 @@ class Command(BaseCommand):
         permanent_files_nb = 0
         temporary_files_nb = 0
         known_permanent_files_nb = 0
-        known_keys = set(File.objects.values_list("id", flat=True))
+        known_keys = set(File.objects.values_list("key", flat=True))
         self.logger.info("Checking existing files: %d files in database before sync", len(known_keys))
         for page in page_iterator:
             obj_summaries = page["Contents"]
             for obj_summary in obj_summaries:
                 key = obj_summary["Key"]
                 if not key.startswith(f"{TEMPORARY_STORAGE_PREFIX}/"):
-                    batch.append(File(key=key, last_modified=obj_summary["LastModified"], deleted_at=timezone.now()))
-                    permanent_files_nb += 1
-                    if key in known_keys:
+                    if key not in known_keys:
+                        batch.append(
+                            File(key=key, last_modified=obj_summary["LastModified"], deleted_at=timezone.now())
+                        )
+                        permanent_files_nb += 1
+                    else:
                         known_permanent_files_nb += 1
                         known_keys.remove(key)
                 else:
