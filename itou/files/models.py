c61712ad59ca08411b9dc8163fbb86659c671b06
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2025-06-16 11:19:03 +0200

    files: Add db uniqueness on File.key

--- itou/files/models.py
+++ itou/files/models.py
@@ -13,7 +13,7 @@ class File(models.Model):
     # https://docs.aws.amazon.com/AmazonS3/latest/userguide/object-keys.html
     # The name for a key is a sequence of Unicode characters whose UTF-8
     # encoding is at most 1024 bytes long.
-    key = models.CharField(max_length=1024)
+    key = models.CharField(max_length=1024, unique=True)
 
     last_modified = models.DateTimeField("dernière modification sur Cellar", default=timezone.now)
     deleted_at = models.DateTimeField(
