1896256d325eba7b3b0c20a8b2abcda35f2ff0c8
vincent porte <vincent@neuralia.co>
2025-06-17 11:10:24 +0200

    archive app: add AnonymizedApproval and AnonymizedEligibilityDiagnosis models
    Store non-personnal datas on Approval and non-personnal datas on Eligibility Diagnoses (IAE & GEIQ)
    for long time stats purposes

--- itou/archive/admin.py
+++ itou/archive/admin.py
@@ -44,6 +44,10 @@ class AnonymizedJobSeekerAdmin(ItouModelAdmin):
         "count_accepted_applications",
         "count_IAE_applications",
         "count_total_applications",
+        "count_approvals",
+        "first_approval_start_at",
+        "last_approval_end_at",
+        "count_eligibility_diagnoses",
     )
 
     readonly_fields = fields
@@ -80,3 +84,64 @@ class AnonymizedApplicationAdmin(ItouModelAdmin):
     )
 
     readonly_fields = fields
+
+
+@admin.register(models.AnonymizedApproval)
+class AnonymizedApprovalAdmin(ItouModelAdmin):
+    fields = (
+        "anonymized_at",
+        "origin",
+        "origin_company_kind",
+        "origin_sender_kind",
+        "origin_prescriber_organization_kind",
+        "start_at",
+        "end_at",
+        "had_eligibility_diagnosis",
+        "number_of_prolongations",
+        "duration_of_prolongations",
+        "number_of_suspensions",
+        "duration_of_suspensions",
+        "number_of_job_applications",
+        "number_of_accepted_job_applications",
+    )
+
+    readonly_fields = fields
+
+
+class BaseAnonymizedEligibilityDiagnosisAdmin(ItouModelAdmin):
+    fields = (
+        "anonymized_at",
+        "created_at",
+        "expired_at",
+        "job_seeker_birth_year",
+        "job_seeker_department",
+        "job_seeker_had_pole_emploi_id",
+        "job_seeker_had_nir",
+        "author_kind",
+        "author_prescriber_organization_kind",
+        "number_of_administrative_criteria",
+        "number_of_administrative_criteria_level_1",
+        "number_of_administrative_criteria_level_2",
+        "number_of_certified_administrative_criteria",
+        "selected_administrative_criteria",
+        "number_of_job_applications",
+        "number_of_accepted_job_applications",
+    )
+
+    readonly_fields = fields
+
+
+@admin.register(models.AnonymizedSIAEEligibilityDiagnosis)
+class AnonymizedSIAEEligibilityDiagnosisAdmin(BaseAnonymizedEligibilityDiagnosisAdmin):
+    fields = BaseAnonymizedEligibilityDiagnosisAdmin.fields + (
+        "author_siae_kind",
+        "number_of_approvals",
+        "first_approval_start_at",
+        "last_approval_end_at",
+    )
+    readonly_fields = fields
+
+
+@admin.register(models.AnonymizedGEIQEligibilityDiagnosis)
+class AnonymizedGEIQEligibilityDiagnosisAdmin(BaseAnonymizedEligibilityDiagnosisAdmin):
+    pass
