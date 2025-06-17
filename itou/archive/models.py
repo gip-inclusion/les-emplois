e695d51ca3112ef507316599071c7b332fc23a17
vincent porte <vincent@neuralia.co>
2025-06-17 11:10:24 +0200

    archive: add AnonymizedApproval and AnonymizedEligibilityDiagnosis models
    Store non-personnal datas on Approval and non-personnal datas on Eligibility Diagnoses (IAE & GEIQ)
    for long time stats purposes

--- itou/archive/models.py
+++ itou/archive/models.py
@@ -67,6 +67,16 @@ class AnonymizedJobSeeker(models.Model):
     )
     count_IAE_applications = models.PositiveIntegerField(verbose_name="nombre de candidatures dans l'IAE", default=0)
     count_total_applications = models.PositiveIntegerField(verbose_name="nombre de candidatures totales", default=0)
+    count_approvals = models.PositiveIntegerField(verbose_name="nombre de PASS IAE accordés", default=0)
+    first_approval_start_at = models.DateField(
+        verbose_name="année et mois de début du premier PASS IAE accordé", blank=True, null=True
+    )
+    last_approval_end_at = models.DateField(
+        verbose_name="année et mois de fin du dernier PASS IAE accordé", blank=True, null=True
+    )
+    count_eligibility_diagnoses = models.PositiveIntegerField(
+        verbose_name="nombre de diagnostics d'éligibilité", default=0
+    )
 
     class Meta:
         verbose_name = "candidat anonymisé"
@@ -122,6 +132,7 @@ class AnonymizedApplication(models.Model):
     had_diagoriente_invitation = models.BooleanField(
         verbose_name="avait reçu une invitation à un diagnostic d'orientation", default=False
     )
+    had_approval = models.BooleanField(verbose_name="avait un PASS IAE", default=False)
 
     # hiring
     hiring_rome = models.CharField(verbose_name="code ROME de l'embauche", blank=True, null=True)
@@ -136,3 +147,126 @@ class AnonymizedApplication(models.Model):
 
     def __str__(self):
         return f"candidature {self.id} anonymisée le {self.anonymized_at.strftime('%Y-%m-%d %H:%M:%S')}"
+
+
+class AnonymizedApproval(models.Model):
+    anonymized_at = models.DateTimeField(auto_now_add=True, verbose_name="anonymisé le")
+    origin = models.CharField(verbose_name="origine du PASS")
+    origin_company_kind = models.CharField(verbose_name="type d'entreprise à l'origine du PASS", blank=True, null=True)
+    origin_sender_kind = models.CharField(
+        verbose_name="type d'emetteur de la candidature à l'origine du PASS", blank=True, null=True
+    )
+    origin_prescriber_organization_kind = models.CharField(
+        verbose_name="typologie du prescripteur à l'origine du PASS", blank=True, null=True
+    )
+    start_at = models.DateField(verbose_name="année et mois de début du PASS", blank=True, null=True)
+    end_at = models.DateField(verbose_name="année et mois de fin du PASS", blank=True, null=True)
+    had_eligibility_diagnosis = models.BooleanField(verbose_name="a eu un diagnostic d'éligibilité", default=False)
+    number_of_prolongations = models.PositiveIntegerField(verbose_name="nombre de prolongations", default=0)
+    duration_of_prolongations = models.PositiveIntegerField(
+        verbose_name="durée totale des prolongations en jours", default=0
+    )
+    number_of_suspensions = models.PositiveIntegerField(verbose_name="nombre de suspensions", default=0)
+    duration_of_suspensions = models.PositiveIntegerField(
+        verbose_name="durée totale des suspensions en jours", default=0
+    )
+    number_of_job_applications = models.PositiveIntegerField(
+        verbose_name="nombre de candidatures pour lesquelles le PASS a été utilisé", default=0
+    )
+    number_of_accepted_job_applications = models.PositiveIntegerField(
+        verbose_name="nombre de candidatures acceptées pour lesquelles le PASS a été utilisé", default=0
+    )
+
+    class Meta:
+        verbose_name = "PASS IAE anonymisé"
+        verbose_name_plural = "PASS IAE anonymisés"
+        ordering = ["-anonymized_at"]
+
+    def __str__(self):
+        return f"PASS IAE {self.id} anonymisé le {self.anonymized_at.strftime('%Y-%m-%d %H:%M:%S')}"
+
+
+class AbstractAnonymizedEligibilityDiagnosis(models.Model):
+    anonymized_at = models.DateField(auto_now_add=True, verbose_name="anonymisé le")
+    created_at = models.DateField(verbose_name="année et mois de création du diagnostic")
+    expired_at = models.DateField(verbose_name="année et mois d'expiration du diagnostic", blank=True, null=True)
+    # job seeker
+    job_seeker_birth_year = models.PositiveSmallIntegerField(
+        verbose_name="année de naissance du candidat", blank=True, null=True
+    )
+    job_seeker_department = models.CharField(
+        verbose_name="département du candidat", max_length=3, blank=True, null=True
+    )
+    job_seeker_had_pole_emploi_id = models.BooleanField(
+        verbose_name="le candidat avait un identifiant Pôle emploi", default=False
+    )
+    job_seeker_had_nir = models.BooleanField(verbose_name="le candidat avait un NIR", default=False)
+    # author
+    author_kind = models.CharField(verbose_name="type de l'auteur du diagnostic")
+    author_prescriber_organization_kind = models.CharField(
+        verbose_name="type de l'organisation prescriptrice de l'auteur du diagnostic", blank=True, null=True
+    )
+    # administrative criteria
+    # some GEIQ adminsistrative criteria have no level, so we need to keep track of them
+    number_of_administrative_criteria = models.PositiveIntegerField(
+        verbose_name="nombre de critères administratifs selectionnés", default=0
+    )
+
+    number_of_administrative_criteria_level_1 = models.PositiveIntegerField(
+        verbose_name="nombre de critères administratifs de niveau 1", default=0
+    )
+    number_of_administrative_criteria_level_2 = models.PositiveIntegerField(
+        verbose_name="nombre de critères administratifs de niveau 2", default=0
+    )
+    number_of_certified_administrative_criteria = models.PositiveIntegerField(
+        verbose_name="nombre de critères administratifs certifiés", default=0
+    )
+    selected_administrative_criteria = models.JSONField(
+        verbose_name="critères administratifs sélectionnés", default=list
+    )
+    # job applications
+    number_of_job_applications = models.PositiveIntegerField(
+        verbose_name="nombre de candidatures pour lesquelles le diagnostic a été utilisé", default=0
+    )
+    number_of_accepted_job_applications = models.PositiveIntegerField(
+        verbose_name="nombre de candidatures acceptées pour lesquelles le diagnostic a été utilisé", default=0
+    )
+
+    class Meta:
+        abstract = True
+
+
+class AnonymizedSIAEEligibilityDiagnosis(AbstractAnonymizedEligibilityDiagnosis):
+    author_siae_kind = models.CharField(verbose_name="type de SIAE de l'auteur du diagnostic", blank=True, null=True)
+    # approvals
+    number_of_approvals = models.PositiveIntegerField(
+        verbose_name="nombre de PASS IAE accordés suite au diagnostic", default=0
+    )
+    first_approval_start_at = models.DateField(
+        verbose_name="année et mois de début du premier PASS IAE accordé suite au diagnostic", blank=True, null=True
+    )
+    last_approval_end_at = models.DateField(
+        verbose_name="année et mois de fin du dernier PASS IAE accordé suite au diagnostic", blank=True, null=True
+    )
+
+    class Meta:
+        verbose_name = "diagnostic d'éligibilité SIAE anonymisé"
+        verbose_name_plural = "diagnostics d'éligibilité SIAE anonymisés"
+        ordering = ["-anonymized_at"]
+
+    def __str__(self):
+        return (
+            f"diagnostic d'éligibilité SIAE {self.id} anonymisé le {self.anonymized_at.strftime('%Y-%m-%d %H:%M:%S')}"
+        )
+
+
+class AnonymizedGEIQEligibilityDiagnosis(AbstractAnonymizedEligibilityDiagnosis):
+    class Meta:
+        verbose_name = "diagnostic d'éligibilité GEIQ anonymisé"
+        verbose_name_plural = "diagnostics d'éligibilité GEIQ anonymisés"
+        ordering = ["-anonymized_at"]
+
+    def __str__(self):
+        return (
+            f"diagnostic d'éligibilité GEIQ {self.id} anonymisé le {self.anonymized_at.strftime('%Y-%m-%d %H:%M:%S')}"
+        )
