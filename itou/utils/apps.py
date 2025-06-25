6b0a12d4e3f84fdc1fd63a3469e2af8436ab0e35
Xavier Fernandez <xavier.fernandez@inclusion.gouv.fr>
2025-06-25 16:17:56 +0200

    admin: fix User/JobSeekerProfile display in admin forms

--- itou/utils/apps.py
+++ itou/utils/apps.py
@@ -1,6 +1,21 @@
 from django.apps import AppConfig
+from django.utils.text import Truncator
+
+
+class PIIAwareTruncator(Truncator):
+    def __init__(self, text):
+        if hasattr(text, "display_with_pii"):
+            text = text.display_with_pii
+        super().__init__(text)
 
 
 class UtilsAppConfig(AppConfig):
     name = "itou.utils"
     verbose_name = "utils"
+
+    def ready(self):
+        from django.contrib.admin import widgets
+
+        # This allows models to be displayed in the admin forms via their display_with_pii property
+        # instead of the default __str__ method when such property exists.
+        widgets.Truncator = PIIAwareTruncator
