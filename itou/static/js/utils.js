afbcf0cba20e4b91ac03328ba71402563a5c4007
François Freitag <mail@franek.fr>
2025-06-30 11:33:01 +0200

    Merge prevent_multiple_submit into utils.js
    No need for this bit of functionality to be split out into a different
    file.

--- itou/static/js/utils.js
+++ itou/static/js/utils.js
@@ -102,6 +102,19 @@ htmx.onLoad((target) => {
     form.addEventListener("change", form.submit);
   });
 
+  $('form.js-prevent-multiple-submit', target).on('submit', function () {
+    // Prevent multiple submit client side.
+    // Never trust the client: validation must also happen server side.
+    // Opt-in by adding a `js-prevent-multiple-submit` CSS class to a <form>.
+
+    // Pay attention: in multi-step forms, the browser will remember the disabled
+    // state of the button when clicking "Previous" in the browser, eventually
+    // disabling moving forward afterwards !
+    $(':submit', this).on('click', function () {
+      return false
+    })
+  });
+
   /*
    * File selector.
    */
