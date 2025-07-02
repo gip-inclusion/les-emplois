e0246dc953a0c799920da26f1afc96e0c8fedbb7
hellodeloo <hello@hellodeloo.com>
2025-07-02 16:06:22 +0200

    feat: update theme-inclusion  to v3.0.0 and remove itou useless css rules

--- itou/utils/staticfiles.py
+++ itou/utils/staticfiles.py
@@ -220,11 +220,11 @@ ASSET_INFOS = {
     },
     "theme-inclusion": {
         "download": {
-            "url": "https://github.com/gip-inclusion/itou-theme/archive/refs/tags/v2.9.9.zip",
-            "sha256": "5e12066815511dac0da9104bd3a53b354ddc87923310117e0f71f07ed2e2dd07",
+            "url": "https://github.com/gip-inclusion/itou-theme/archive/refs/tags/v3.0.0.zip",
+            "sha256": "3fd5dce9c6359719d734e7c441d622d3bb47dae19f4dc8a7c3fa29f342533bf2",
         },
         "extract": {
-            "origin": "itou-theme-2.9.9/dist",
+            "origin": "itou-theme-3.0.0/dist",
             "destination": "vendor/theme-inclusion/",
             "files": [
                 "javascripts/app.js",
