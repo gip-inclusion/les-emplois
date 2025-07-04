7029b96979b723ccf16c9ea988397a7c6133a87f
François Freitag <mail@franek.fr>
2025-07-04 11:13:03 +0200

    Prevent migrations from running concurrently
    When multiple instances are serving the site, CleverCloud builds a new
    machine for each instance during deploy. Part of the build process is to
    run CC_MANAGE_PYTHON_TASKS, which includes `migrate`. We don’t want two
    concurrent runs of migrations, mostly because data migrations are often
    set with `atomic=False`, which means the workers would run them
    concurrently.
    
    A session-level advisory lock is acquired for the migrate command to
    proceed. It cannot be a transaction-level lock, because some data
    migrations run with `atomic=False` and use multiple transactions.
    Since the connection will be released at the end of the command, a
    session-level advisory lock is guaranteed to be released fairly quickly
    after the migrate command completes.
    
    Since the migrations duration cannot be known in advance, the statement
    timeout is explicitly unbound. To help troubleshoot a stuck deployment
    process, the “Acquiring advisory lock for migrations” message was added.

--- itou/utils/db.py
+++ itou/utils/db.py
@@ -1,4 +1,12 @@
+import binascii
+import contextlib
+import logging
+
+from django.db import connection
 from django.db.models import Q
+
+
+logger = logging.getLogger(__name__)
 
 
 def or_queries(queries, required=True):
@@ -16,3 +24,26 @@ def dictfetchall(cursor):
     """
     columns = [col[0] for col in cursor.description]
     return [dict(zip(columns, row)) for row in cursor.fetchall()]
+
+
+@contextlib.contextmanager
+def statement_timeout(timeout):
+    with connection.cursor() as cursor:
+        cursor.execute("SHOW statement_timeout")
+        [original_statement_timeout_row] = cursor.fetchall()
+        [original_statement_timeout] = original_statement_timeout_row
+
+        cursor.execute("SET SESSION statement_timeout TO %s", (timeout,))
+        yield
+        cursor.execute("SET SESSION statement_timeout TO %s", (original_statement_timeout,))
+
+
+@contextlib.contextmanager
+def pg_advisory_lock(name):
+    lock_id = binascii.crc32(name.encode())
+    logger.info("Acquiring advisory lock for %s (lock id %s).", name, lock_id)
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT pg_advisory_lock(%s)", (lock_id,))
+        yield
+        cursor.execute("SELECT pg_advisory_unlock(%s)", (lock_id,))
+    logger.info("Releasing advisory lock for %s (lock id %s).", name, lock_id)
