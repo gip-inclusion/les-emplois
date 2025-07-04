41768331a777494537ffd5f3a137d27255676bc8
François Freitag <mail@franek.fr>
2025-07-04 09:19:03 +0200

    Rate limit anonymous user
    Since this morning, we’re receiving about 80K requests per hour, whereas
    usual traffic in peak hours is around 5K requests per hour.
    
    It looks like robots crawling the public companies and job description
    pages. Try rate limiting them per IP, even though they might just change
    IP as soon as they get a 429…

--- itou/utils/throttling.py
+++ itou/utils/throttling.py
@@ -2,6 +2,15 @@ from django.core.cache import caches
 from rest_framework import throttling
 
 
+class FailSafeAnonRateThrottle(throttling.AnonRateThrottle):
+    rate = "30/minute"
+
+    @property
+    def cache(self):
+        # The property allows swapping cache configuration in tests.
+        return caches["failsafe"]
+
+
 class FailSafeUserRateThrottle(throttling.UserRateThrottle):
     rate = "60/minute"
 
