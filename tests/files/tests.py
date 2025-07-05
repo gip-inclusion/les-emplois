3070f769b32cb8bd4f0379b16c18dc4c3a0e430a
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2025-07-05 07:34:41 +0200

    files Remove purge_files command
    There's no need for it.
    We delete files when they aren't used anymore, and the s3 is cleaned
    seperately

--- tests/files/tests.py
+++ tests/files/tests.py
@@ -170,26 +170,3 @@ def test_delete_orphans(caplog):
     assert caplog.messages[-1].startswith(
         "Management command itou.files.management.commands.delete_orphan_files succeeded in"
     )
-
-
-@pytest.mark.usefixtures("temporary_bucket")
-def test_purge_files(caplog):
-    in_the_past = timezone.now() - datetime.timedelta(days=1)
-    to_purge = FileFactory(deleted_at=in_the_past)
-    not_an_orphan = JobApplicationFactory(resume__deleted_at=in_the_past).resume
-    FileFactory(deleted_at=in_the_past)  # File without S3 object (remove anyway)
-    too_recently_marked = FileFactory(deleted_at=timezone.now())
-
-    for file in [to_purge, not_an_orphan]:
-        with io.BytesIO() as content:
-            default_storage.save(file.key, content)
-
-    call_command("purge_files")
-
-    assert set(File.objects.values_list("pk", flat=True)) == {not_an_orphan.pk, too_recently_marked.pk}
-
-    assert caplog.messages[:-1] == [
-        f"Could not delete protected file {not_an_orphan.key}",
-        "Purged 2 files",
-    ]
-    assert caplog.messages[-1].startswith("Management command itou.files.management.commands.purge_files succeeded in")
