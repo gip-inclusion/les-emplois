9791baf43376ca059f7c6a5db3c757e26d3f72d6
Céline MS <celine@hello-birds.com>
2025-07-11 17:25:24 +0200

    BasePoleEmploiAPIClient to prepare for the upcoming realm Agent

--- itou/utils/apis/pole_emploi.py
+++ itou/utils/apis/pole_emploi.py
@@ -47,19 +47,6 @@ API_TIMEOUT_SECONDS = 60  # this API is pretty slow, let's give it a chance
 
 CACHE_API_TOKEN_KEY = "pole_emploi_api_client_token"
 
-# Pole Emploi also sent us a "sandbox" scope value: "api_testmaj-pass-iaev1" instead of "api_maj-pass-iaev1"
-AUTHORIZED_SCOPES = [
-    "api_maj-pass-iaev1",
-    "api_offresdemploiv2",
-    "api_rechercheindividucertifiev1",
-    "api_rome-metiersv1",
-    "nomenclatureRome",
-    "o2dsoffre",
-    "passIAE",
-    "rechercherIndividuCertifie",
-    "api_referentielagencesv1",
-    "organisationpe",
-]
 API_MAJ_PASS_SUCCESS = "S000"
 API_RECH_INDIVIDU_SUCCESS = "S001"
 DATE_FORMAT = "%Y-%m-%d"
@@ -82,19 +69,22 @@ def _pole_emploi_name(name: str, hyphenate=False, max_len=25) -> str:
     return replaced[:max_len]
 
 
-class PoleEmploiApiClient:
-    def __init__(self, base_url, auth_base_url, key, secret):
+class BasePoleEmploiApiClient:
+    AUTHORIZED_SCOPES = []
+
+    def __init__(self, base_url, auth_base_url, key, secret, realm):
         self.base_url = base_url
         self.auth_base_url = auth_base_url
         self.key = key
         self.secret = secret
+        self.realm = realm
 
     def _refresh_token(self):
-        scopes = " ".join(AUTHORIZED_SCOPES)
+        scopes = " ".join(self.AUTHORIZED_SCOPES)
         auth_data = (
             httpx.post(
                 f"{self.auth_base_url}/connexion/oauth2/access_token",
-                params={"realm": "/partenaire"},
+                params={"realm": self.realm},
                 data={
                     "client_id": self.key,
                     "client_secret": self.secret,
@@ -148,6 +138,26 @@ class PoleEmploiApiClient:
             return data
         except httpx.RequestError as exc:
             raise PoleEmploiAPIException(API_CLIENT_HTTP_ERROR_CODE) from exc
+
+
+# TODO(cms): rename me to PoleEmploiRoyaumePartenaireApiClient
+class PoleEmploiApiClient(BasePoleEmploiApiClient):
+    # Pole Emploi also sent us a "sandbox" scope value: "api_testmaj-pass-iaev1" instead of "api_maj-pass-iaev1"
+    AUTHORIZED_SCOPES = [
+        "api_maj-pass-iaev1",
+        "api_offresdemploiv2",
+        "api_rechercheindividucertifiev1",
+        "api_rome-metiersv1",
+        "nomenclatureRome",
+        "o2dsoffre",
+        "passIAE",
+        "rechercherIndividuCertifie",
+        "api_referentielagencesv1",
+        "organisationpe",
+    ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(realm="/partenaire", *args, **kwargs)
 
     def recherche_individu_certifie(self, first_name, last_name, birthdate, nir):
         """Example data:
