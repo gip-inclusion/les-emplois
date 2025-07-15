7823310afa526ede111ba9d2caf934268112f3e5
Céline MS <celine@hello-birds.com>
2025-07-15 11:51:17 +0200

    [Consumed API] Add a new realm to FranceTravail client
    The RQTH API is in the Agent realm and uses the
    RechercherUsager endpoint. Implement a new realm.

--- itou/utils/apis/pole_emploi.py
+++ itou/utils/apis/pole_emploi.py
@@ -273,3 +273,15 @@ class PoleEmploiRoyaumePartenaireApiClient(BasePoleEmploiApiClient):
         if safir:
             return next((agence for agence in agences if agence["codeSafir"] == str(safir)), None)
         return agences
+
+
+class PoleEmploiRoyaumeAgentAPIClient(BasePoleEmploiApiClient):
+    AUTHORIZED_SCOPES = [
+        "api_rechercher-usagerv2",
+        "rechercheusager",
+        "profil_accedant",
+        "api_donnees-rqthv1",
+        "h2a",
+    ]
+    REALM = "/agent"
+    CACHE_API_TOKEN_KEY = "pole_emploi_api_agent_client_token"
