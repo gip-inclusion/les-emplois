1edfa541b6c5a650c6b1e85066cf013c381cab24
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2025-07-16 16:56:16 +0200

    job_applications: Squash migrations

--- itou/job_applications/migrations/0011_drop_hidden_for_company.py
+++
@@ -1,9 +0,0 @@
-from django.db import migrations
-
-
-class Migration(migrations.Migration):
-    dependencies = [
-        ("job_applications", "0010_set_jobapplication_archived_at"),
-    ]
-
-    operations = []
