98df85ae506dc54fbb1d979d91245f1283e41b68
dejafait <10533583+dejafait@users.noreply.github.com>
2025-07-17 13:55:01 +0300

    search: Simplify update_companies_job_app_score

--- itou/companies/management/commands/update_companies_job_app_score.py
+++ itou/companies/management/commands/update_companies_job_app_score.py
@@ -10,9 +10,7 @@ class Command(BaseCommand):
     def handle(self, **options):
         nb_updated = (
             models.Company.objects.with_computed_job_app_score()
-            # Do not update if nothing changes (NULL values have to be handled separately because NULL)
-            .exclude(Q(job_app_score=F("computed_job_app_score")) & Q(computed_job_app_score__isnull=False))
-            .exclude(Q(job_app_score__isnull=True) & Q(computed_job_app_score__isnull=True))
+            .exclude(Q(job_app_score=F("computed_job_app_score")))
             .update(job_app_score=F("computed_job_app_score"))
         )
         self.logger.info("Updated %d companies", nb_updated)
