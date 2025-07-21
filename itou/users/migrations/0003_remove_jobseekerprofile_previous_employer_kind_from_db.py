8d7ebb99342afe73e83dfb49707a6c120924a630
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2025-07-21 06:36:37 +0200

    users: Squash migrations

--- itou/users/migrations/0003_remove_jobseekerprofile_previous_employer_kind_from_db.py
+++
@@ -1,9 +0,0 @@
-from django.db import migrations
-
-
-class Migration(migrations.Migration):
-    dependencies = [
-        ("users", "0002_remove_jobseekerprofile_previous_employer_kind"),
-    ]
-
-    operations = []
