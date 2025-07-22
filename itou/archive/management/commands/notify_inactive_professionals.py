55b67a5f55793db15da3d498568aaee3263285a8
vincent porte <vincent@neuralia.co>
2025-07-22 09:16:35 +0200

    archive: notify more professionals per day
    Adjusting the frequency of professionals to be notified

--- itou/archive/management/commands/notify_inactive_professionals.py
+++ itou/archive/management/commands/notify_inactive_professionals.py
@@ -55,7 +55,7 @@ class Command(BaseCommand):
     @monitor(
         monitor_slug="notify_inactive_professionals",
         monitor_config={
-            "schedule": {"type": "crontab", "value": "0 7 * * MON-FRI"},
+            "schedule": {"type": "crontab", "value": "0 7,10,13 * * MON-FRI"},
             "checkin_margin": 5,
             "max_runtime": 10,
             "failure_issue_threshold": 2,
