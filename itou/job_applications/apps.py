bcee912699bfa2c6d232499b347bbd6a9e489789
François Freitag <mail@franek.fr>
2025-07-22 11:19:20 +0200

    admin: Set the job_applications app name
    Everybody gets tricked once in a while looking for the french name,
    since all other apps are using french names.

---
+++ itou/job_applications/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class JobApplicationsConfig(AppConfig):
+    default_auto_field = "django.db.models.AutoField"
+    name = "itou.job_applications"
+    verbose_name = "Candidatures"
