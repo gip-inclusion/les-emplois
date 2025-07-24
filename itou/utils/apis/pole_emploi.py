81ccfec1a33db76c5e45a0c74c2ff859f0f26eb2
Céline MS <celine@hello-birds.com>
2025-07-24 13:48:17 +0200

    [API Pole emploi] Move function definition to its own module
    This client was the only one to be exposed via the __init__.py
    file, making its import different from the others.

--- itou/utils/apis/pole_emploi.py
+++ itou/utils/apis/pole_emploi.py
@@ -5,6 +5,7 @@ import re
 import time
 
 import httpx
+from django.conf import settings
 from django.core.cache import caches
 from unidecode import unidecode
 
@@ -428,3 +429,12 @@ class PoleEmploiRoyaumeAgentAPIClient(BasePoleEmploiApiClient):
             "end_at": end_at,
             "raw_response": data,
         }
+
+
+def pole_emploi_partenaire_api_client():
+    return PoleEmploiRoyaumePartenaireApiClient(
+        settings.API_ESD["BASE_URL"],
+        settings.API_ESD["AUTH_BASE_URL"],
+        settings.API_ESD["KEY"],
+        settings.API_ESD["SECRET"],
+    )
