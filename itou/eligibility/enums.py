ebddc891078bc365ba0b6983d6cbcb7a24118195
Céline MS <celine@hello-birds.com>
2025-07-24 12:45:56 +0200

    Split certified criteria for API particulier
    With the upcoming RQTH certification, we need to split
    criteria certifiable by the API Particulier from those
    certifiable by the France Travail API.

--- itou/eligibility/enums.py
+++ itou/eligibility/enums.py
@@ -114,7 +114,15 @@ class AdministrativeCriteriaKind(models.TextChoices):
             cls.SIAE_CUI,
         }
 
+    @classmethod
+    def certifiable_by_api_particulier(cls):
+        return frozenset(
+            [
+                cls.RSA,
+                cls.AAH,
+                cls.PI,
+            ]
+        )
 
-CERTIFIABLE_ADMINISTRATIVE_CRITERIA_KINDS = frozenset(
-    [AdministrativeCriteriaKind.RSA, AdministrativeCriteriaKind.AAH, AdministrativeCriteriaKind.PI]
-)
+
+CERTIFIABLE_ADMINISTRATIVE_CRITERIA_KINDS = AdministrativeCriteriaKind.certifiable_by_api_particulier()
