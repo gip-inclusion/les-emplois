a2a4e6fe82d406c511d9082faad9eface3366e87
Romain Sébille <romain.sebille@beta.gouv.fr>
2025-07-29 11:27:23 +0200

    populate_metabase_emplois: `dbt_daily` should always be at the end

--- clevercloud/crons/populate_metabase_emplois.sh
+++ clevercloud/crons/populate_metabase_emplois.sh
@@ -39,9 +39,9 @@ if [[ "$1" == "--daily" ]]; then
     django-admin populate_metabase_emplois --mode=evaluated_criteria |& tee -a "$OUTPUT_LOG"
     django-admin populate_metabase_emplois --mode=users |& tee -a "$OUTPUT_LOG"
     django-admin populate_metabase_emplois --mode=memberships |& tee -a "$OUTPUT_LOG"
-    django-admin populate_metabase_emplois --mode=dbt_daily |& tee -a "$OUTPUT_LOG"
     django-admin populate_metabase_emplois --mode=gps_groups |& tee -a "$OUTPUT_LOG"
     django-admin populate_metabase_emplois --mode=gps_memberships |& tee -a "$OUTPUT_LOG"
+    django-admin populate_metabase_emplois --mode=dbt_daily |& tee -a "$OUTPUT_LOG"
     django-admin send_slack_message ":white_check_mark: succès mise à jour de données C1 -> Metabase"
 elif [[ "$1" == "--monthly" ]]; then
     django-admin send_slack_message ":rocket: lancement mise à jour de données peu fréquentes C1 -> Metabase"
