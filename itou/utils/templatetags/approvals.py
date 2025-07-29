342889e9c4fd96b9184ab70c4d23007ff210290c
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2025-07-29 14:42:57 +0200

    approvals: Refactor templatetag

--- itou/utils/templatetags/approvals.py
+++ itou/utils/templatetags/approvals.py
@@ -7,16 +7,17 @@ register = template.Library()
 @register.inclusion_tag("utils/templatetags/approval_box.html")
 def approval_details_box(
     approval,
+    request,  # only used in template when hide_details_link is False
     *,
-    detail_view_version=False,
-    job_seeker_dashboard_version=False,
-    link_from_current_url=None,
+    hide_details_link=False,
+    version=None,
     extra_classes="",
 ):
+    assert version in [None, "detail_view", "job_seeker_dashboard"]
     return {
         "approval": approval,
-        "detail_view_version": detail_view_version,
-        "link_from_current_url": link_from_current_url,
-        "job_seeker_dashboard_version": job_seeker_dashboard_version,
+        "request": request,
+        "version": version,
+        "with_details_link": hide_details_link is False,
         "extra_classes": extra_classes,
     }
