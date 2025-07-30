4a41e7c25b23e75967f8a643af84c983430e599b
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2025-07-30 08:17:56 +0200

    SQUASH_ME: refactor version parameter

--- itou/utils/templatetags/approvals.py
+++ itou/utils/templatetags/approvals.py
@@ -7,17 +7,25 @@ register = template.Library()
 @register.inclusion_tag("utils/templatetags/approval_box.html")
 def approval_details_box(
     approval,
-    request,  # only used in template when hide_details_link is False
+    version,
     *,
-    hide_details_link=False,
-    version=None,
+    request=None,  # only used in template when details link is displayed
     extra_classes="",
 ):
-    assert version in [None, "detail_view", "job_seeker_dashboard"]
+    assert version in [
+        "box",  # default version
+        "box_without_link",
+        "details_view",
+        "job_seeker_dashboard",
+    ]
+
+    with_link_versions = ["box", "job_seeker_dashboard"]
+    assert request or version not in with_link_versions, "request is needed for version='box' or version='details'"
+
     return {
         "approval": approval,
         "request": request,
         "version": version,
-        "with_details_link": hide_details_link is False,
+        "with_details_link": version in with_link_versions and approval.is_pass_iae,
         "extra_classes": extra_classes,
     }
--- tests/www/approvals_views/test_approval_box.py
+++ tests/www/approvals_views/test_approval_box.py
@@ -1,9 +1,11 @@
 import datetime
 
+import pytest
 from django.template import Context, Template
 from django.test import RequestFactory
 from freezegun import freeze_time
 
+from itou.approvals.enums import Origin
 from itou.asp.models import Commune
 from tests.approvals.factories import (
     ApprovalFactory,
@@ -24,7 +26,9 @@ approval_number = "XXXXX1234567"
 def test_expired_approval(snapshot):
     approval = ApprovalFactory(start_at=datetime.date(2022, 1, 1), number=approval_number, public_id=public_id)
     request = RequestFactory().get("/")
-    template = Template("{% load approvals %}{% approval_details_box approval=approval request=request %}")
+    template = Template(
+        "{% load approvals %}{% approval_details_box approval=approval version='box' request=request %}"
+    )
     assert pretty_indented(template.render(Context({"approval": approval, "request": request}))) == snapshot
 
 
@@ -32,7 +36,9 @@ def test_expired_approval(snapshot):
 def test_future_approval(snapshot):
     approval = ApprovalFactory(start_at=datetime.date(2025, 1, 1), number=approval_number, public_id=public_id)
     request = RequestFactory().get("/")
-    template = Template("{% load approvals %}{% approval_details_box approval=approval request=request %}")
+    template = Template(
+        "{% load approvals %}{% approval_details_box approval=approval version='box' request=request %}"
+    )
     assert pretty_indented(template.render(Context({"approval": approval, "request": request}))) == snapshot
 
 
@@ -40,16 +46,8 @@ def test_future_approval(snapshot):
 def test_valid_approval(snapshot):
     approval = ApprovalFactory(start_at=datetime.date(2024, 1, 1), number=approval_number, public_id=public_id)
     request = RequestFactory().get("/")
-    template = Template("{% load approvals %}{% approval_details_box approval=approval request=request %}")
-    assert pretty_indented(template.render(Context({"approval": approval, "request": request}))) == snapshot
-
-
-@freeze_time("2024-08-06")
-def test_approval_no_details_link(snapshot):
-    approval = ApprovalFactory(start_at=datetime.date(2024, 1, 1), number=approval_number, public_id=public_id)
-    request = RequestFactory().get("/")
     template = Template(
-        "{% load approvals %}{% approval_details_box approval=approval request=request hide_details_link=True %}"
+        "{% load approvals %}{% approval_details_box approval=approval version='box' request=request %}"
     )
     assert pretty_indented(template.render(Context({"approval": approval, "request": request}))) == snapshot
 
@@ -59,7 +57,9 @@ def test_valid_approval_with_pending_prolongation_request(snapshot):
     approval = ApprovalFactory(start_at=datetime.date(2024, 1, 1), number=approval_number, public_id=public_id)
     ProlongationRequestFactory(approval=approval, start_at=approval.end_at)
     request = RequestFactory().get("/")
-    template = Template("{% load approvals %}{% approval_details_box approval=approval request=request %}")
+    template = Template(
+        "{% load approvals %}{% approval_details_box approval=approval version='box' request=request %}"
+    )
     assert pretty_indented(template.render(Context({"approval": approval, "request": request}))) == snapshot
 
 
@@ -72,7 +72,9 @@ def test_suspended_approval(snapshot):
         end_at=datetime.date(2024, 8, 31),
     )
     request = RequestFactory().get("/")
-    template = Template("{% load approvals %}{% approval_details_box approval=approval request=request %}")
+    template = Template(
+        "{% load approvals %}{% approval_details_box approval=approval version='box' request=request %}"
+    )
     assert pretty_indented(template.render(Context({"approval": approval, "request": request}))) == snapshot
 
 
@@ -81,7 +83,7 @@ def test_expired_pe_approval(snapshot):
     pe_approval = PoleEmploiApprovalFactory(start_at=datetime.date(2022, 1, 1), number="123456789012")
     request = RequestFactory().get("/")
     template = Template(
-        "{% load approvals %}{% approval_details_box approval=approval request=request hide_details_link=True %}"
+        "{% load approvals %}{% approval_details_box approval=approval version='box' request=request %}"
     )
     assert pretty_indented(template.render(Context({"approval": pe_approval, "request": request}))) == snapshot
 
@@ -92,80 +94,69 @@ def test_valid_pe_approval(snapshot):
     pe_approval = PoleEmploiApprovalFactory(start_at=datetime.date(2024, 1, 1), number="123456789012")
     request = RequestFactory().get("/")
     template = Template(
-        "{% load approvals %}{% approval_details_box approval=approval request=request hide_details_link=True %}"
+        "{% load approvals %}{% approval_details_box approval=approval version='box' request=request %}"
     )
     assert pretty_indented(template.render(Context({"approval": pe_approval, "request": request}))) == snapshot
 
 
 @freeze_time("2024-08-06")
-def test_expired_approval_in_waiting_period_with_valid_diagnosis(snapshot):
-    approval = ApprovalFactory(start_at=datetime.date(2022, 1, 1), number=approval_number, public_id=public_id)
-    IAEEligibilityDiagnosisFactory(job_seeker=approval.user, from_prescriber=True)
-    request = RequestFactory().get("/")
-
-    template = Template(
-        "{% load approvals %}{% approval_details_box approval=approval request=request version=version %}"
-    )
-    assert pretty_indented(
-        template.render(
-            Context(
-                {
-                    "approval": approval,
-                    "request": request,
-                    "version": "job_seeker_dashboard",
-                }
-            )
-        )
-    ) == snapshot(name="without city")
-    approval.user.jobseeker_profile.hexa_commune = Commune.objects.by_insee_code("67152")
-    approval.user.jobseeker_profile.save(update_fields=("hexa_commune",))
-    approval.user.jobseeker_profile.hexa_commune.city = create_city_geispolsheim()
-    approval.user.jobseeker_profile.hexa_commune.save(update_fields=("city",))
-
-    assert pretty_indented(
-        template.render(
-            Context(
-                {
-                    "approval": approval,
-                    "request": request,
-                    "version": "job_seeker_dashboard",
-                }
-            )
-        )
-    ) == snapshot(name="with city")
+def test_box_without_link(snapshot):
+    approval = ApprovalFactory(start_at=datetime.date(2024, 1, 1), number=approval_number, public_id=public_id)
+    template = Template("{% load approvals %}{% approval_details_box approval=approval version='box_without_link' %}")
+    assert pretty_indented(template.render(Context({"approval": approval}))) == snapshot
 
 
 @freeze_time("2024-08-06")
-def test_expired_approval_in_waiting_period_without_diagnosis(snapshot):
-    approval = ApprovalFactory(start_at=datetime.date(2022, 1, 1), number=approval_number, public_id=public_id)
-    request = RequestFactory().get("/")
-
-    template = Template(
-        "{% load approvals %}{% approval_details_box approval=approval request=request version=version %}"
+@pytest.mark.parametrize("origin", [Origin.DEFAULT, Origin.PE_APPROVAL])
+def test_details_view_version(snapshot, origin):
+    approval = ApprovalFactory(
+        start_at=datetime.date(2024, 1, 1),
+        number=approval_number,
+        public_id=public_id,
+        origin_pe_approval=origin == Origin.PE_APPROVAL,
     )
-    assert pretty_indented(
-        template.render(
-            Context(
-                {
-                    "approval": approval,
-                    "request": request,
-                    "version": "job_seeker_dashboard",
-                }
-            )
-        )
-    ) == snapshot(name="without_city")
+    template = Template("{% load approvals %}{% approval_details_box approval=approval version='details_view' %}")
+    assert pretty_indented(template.render(Context({"approval": approval}))) == snapshot
+
+
+@freeze_time("2024-08-06")
+def test_job_seeker_dashboard_expired_approval_in_waiting_period_with_valid_diagnosis(snapshot):
+    approval = ApprovalFactory(start_at=datetime.date(2022, 1, 1), number=approval_number, public_id=public_id)
+    IAEEligibilityDiagnosisFactory(job_seeker=approval.user, from_prescriber=True)
+    request = RequestFactory().get("/")
+    template = Template(
+        "{% load approvals %}"
+        "{% approval_details_box approval=approval version='job_seeker_dashboard' request=request %}"
+    )
+    assert pretty_indented(template.render(Context({"approval": approval, "request": request}))) == snapshot(
+        name="without city"
+    )
     approval.user.jobseeker_profile.hexa_commune = Commune.objects.by_insee_code("67152")
     approval.user.jobseeker_profile.save(update_fields=("hexa_commune",))
     approval.user.jobseeker_profile.hexa_commune.city = create_city_geispolsheim()
     approval.user.jobseeker_profile.hexa_commune.save(update_fields=("city",))
-    assert pretty_indented(
-        template.render(
-            Context(
-                {
-                    "approval": approval,
-                    "request": request,
-                    "version": "job_seeker_dashboard",
-                }
-            )
-        )
-    ) == snapshot(name="with_city")
+
+    assert pretty_indented(template.render(Context({"approval": approval, "request": request}))) == snapshot(
+        name="with city"
+    )
+
+
+@freeze_time("2024-08-06")
+def test_job_seeker_dashboard_expired_approval_in_waiting_period_without_diagnosis(snapshot):
+    approval = ApprovalFactory(start_at=datetime.date(2022, 1, 1), number=approval_number, public_id=public_id)
+    request = RequestFactory().get("/")
+
+    template = Template(
+        "{% load approvals %}"
+        "{% approval_details_box approval=approval version='job_seeker_dashboard' request=request %}"
+    )
+    assert pretty_indented(template.render(Context({"approval": approval, "request": request}))) == snapshot(
+        name="without_city"
+    )
+    approval.user.jobseeker_profile.hexa_commune = Commune.objects.by_insee_code("67152")
+    approval.user.jobseeker_profile.save(update_fields=("hexa_commune",))
+    approval.user.jobseeker_profile.hexa_commune.city = create_city_geispolsheim()
+    approval.user.jobseeker_profile.hexa_commune.save(update_fields=("city",))
+    assert pretty_indented(template.render(Context({"approval": approval, "request": request}))) == snapshot(
+        name="with_city"
+    )
