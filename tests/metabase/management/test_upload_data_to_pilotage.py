e1a17cef1a4230d7f3e59a54df2fd6407cff0b6f
Romain Sébille <romain.sebille@beta.gouv.fr>
2025-07-31 15:22:32 +0200

    metabase: Harden `upload_data_to_pilotage`
    Checking the file exists in the bucket is not enough as the upload can
    failed or be interrupted, the content length should do it.
    Also launched the management command more often to limit the need for
    human intervention, and be more future proof in case we add other files.

--- tests/metabase/management/test_upload_data_to_pilotage.py
+++ tests/metabase/management/test_upload_data_to_pilotage.py
@@ -1,3 +1,5 @@
+import io
+
 import pytest
 from django.core.management import call_command
 from django.utils import timezone
@@ -35,3 +37,33 @@ def test_command_idempotence(tmp_path, settings):
     assert response["KeyCount"] == 1
     assert response["Contents"][0]["Key"] == f"flux-iae/{filename}"
     assert response["Contents"][0]["LastModified"] == modified_at
+
+
+@pytest.mark.usefixtures("temporary_bucket")
+def test_command_check_content_length(caplog, snapshot, tmp_path, settings):
+    client = pilotage_s3_client()
+    filename = "fluxIAE_ITOU_.tar.gz"
+    tmp_path.joinpath(filename).write_bytes(b"Hello World")
+
+    client.upload_fileobj(io.BytesIO(), Bucket=settings.PILOTAGE_DATASTORE_S3_BUCKET_NAME, Key=f"flux-iae/{filename}")
+    call_command("upload_data_to_pilotage", tmp_path.as_posix(), "--wet-run")
+    assert (
+        client.get_object(Bucket=settings.PILOTAGE_DATASTORE_S3_BUCKET_NAME, Key=f"flux-iae/{filename}")["Body"].read()
+        == b"Hello World"
+    )
+    assert caplog.messages[:-1] == snapshot
+    assert caplog.messages[-1].startswith(
+        "Management command itou.metabase.management.commands.upload_data_to_pilotage succeeded in"
+    )
+
+
+def test_command_retries(caplog, snapshot, tmp_path, settings, mocker):
+    filename = "fluxIAE_ITOU_.tar.gz"
+    tmp_path.joinpath(filename).touch()
+    mocker.patch("itou.metabase.management.commands.upload_data_to_pilotage.Command._upload_file", return_value=None)
+
+    call_command("upload_data_to_pilotage", tmp_path.as_posix(), "--wet-run")
+    assert caplog.messages[:-1] == snapshot
+    assert caplog.messages[-1].startswith(
+        "Management command itou.metabase.management.commands.upload_data_to_pilotage succeeded in"
+    )
