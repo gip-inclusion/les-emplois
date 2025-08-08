8feb771ff60d052a15cbe4cfeedcca4c34e75259
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2025-08-08 14:31:12 +0200

    approvals: Improve permissions

--- tests/approvals/test_perms.py
+++ tests/approvals/test_perms.py
@@ -1,4 +1,5 @@
-from itou.approvals.perms import can_view_approval_details
+from itou.approvals.perms import PERMS_READ, PERMS_READ_AND_WRITE, can_view_approval_details
+from itou.job_applications.enums import JobApplicationState
 from tests.approvals.factories import ApprovalFactory
 from tests.job_applications.factories import JobApplicationFactory
 from tests.prescribers.factories import PrescriberMembershipFactory
@@ -9,6 +10,12 @@ from tests.utils.testing import get_request
 def test_can_view_approval_details():
     approval = ApprovalFactory()
 
+    employer_with_write_permissions = JobApplicationFactory(
+        job_seeker=approval.user, state=JobApplicationState.ACCEPTED
+    ).to_company.members.first()
+    request = get_request(employer_with_write_permissions)
+    assert can_view_approval_details(request, approval) == PERMS_READ_AND_WRITE
+
     for user in [
         approval.user,
         JobApplicationFactory(
@@ -18,7 +25,7 @@ def test_can_view_approval_details():
         JobApplicationFactory(job_seeker=approval.user, sent_by_company=True).sender,  # employer who sent a job app
     ]:
         request = get_request(user)
-        assert can_view_approval_details(request, approval) is True
+        assert can_view_approval_details(request, approval) == PERMS_READ
 
     for bad_user in [
         JobSeekerFactory(),  # another job seeker
@@ -27,4 +34,4 @@ def test_can_view_approval_details():
         EmployerFactory(with_company=True),
     ]:
         request = get_request(bad_user)
-        assert can_view_approval_details(request, approval) is False
+        assert can_view_approval_details(request, approval) is None
