290aa938359d46e9eee7ef204f01c98c2d5ba967
Romain Sébille <romain.sebille@beta.gouv.fr>
2025-08-21 09:55:48 +0200

    populate_metabase_emplois: Replace prefetches for `job_seekers`
    Expected gain: 45%

--- itou/metabase/tables/job_seekers.py
+++ itou/metabase/tables/job_seekers.py
@@ -11,7 +11,6 @@ from itou.metabase.tables.utils import (
     get_ai_stock_job_seeker_pks,
     get_choice,
     get_department_and_region_columns,
-    get_hiring_company,
     get_post_code_column,
     get_qpv_job_seeker_pks,
     hash_content,
@@ -374,7 +373,7 @@ def get_table():
                 "name": "type_structure_dernière_embauche",
                 "type": "varchar",
                 "comment": "Type de la structure destinataire de la dernière embauche du candidat",
-                "fn": lambda o: get_hiring_company(o).kind if get_hiring_company(o) else None,
+                "fn": lambda o: o.last_hiring_company_kind,
             },
             {
                 "name": "total_critères_niveau_1",
