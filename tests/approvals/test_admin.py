fdc647ef0333d15976e7e1c5398b37a7bd7ae588
François Freitag <mail@franek.fr>
2025-08-28 17:38:29 +0200

    tests: Prefer get_user() over _auth_user_id

--- tests/approvals/test_admin.py
+++ tests/approvals/test_admin.py
@@ -4,6 +4,7 @@ import pytest
 from dateutil.relativedelta import relativedelta
 from django.contrib import messages
 from django.contrib.admin import helpers
+from django.contrib.auth import get_user
 from django.contrib.contenttypes.models import ContentType
 from django.urls import reverse
 from django.utils import timezone
@@ -302,7 +303,7 @@ class TestApprovalAdmin:
             content_type=approval_content_type,
             object_id=approval.pk,
         ).get()
-        user = User.objects.get(pk=admin_client.session["_auth_user_id"])
+        user = User.objects.get(pk=get_user(admin_client).pk)
         assert support_remark.remark == f"2025-08-21 : PASS IAE clôturé par {user.get_full_name()}."
 
     @freeze_time()
