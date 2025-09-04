815c50140d1b33fa1c7b0af72e9549589eb601e0
Ewen Corre <ewen.corre@inclusion.gouv.fr>
2025-09-04 10:24:21 +0200

    search_views: add a new saved search

--- itou/www/search_views/urls.py
+++ itou/www/search_views/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path("job-descriptions/results", views.JobDescriptionSearchView.as_view(), name="job_descriptions_results"),
     path("prescribers", views.search_prescribers_home, name="prescribers_home"),
     path("prescribers/results", views.search_prescribers_results, name="prescribers_results"),
+    path("saved-searches/add", views.add_saved_search, name="add_saved_search"),
 ]
