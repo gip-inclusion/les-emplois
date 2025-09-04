e3612a36e17a60d784718fb1d1b38ed43204f424
vincent porte <vincent@neuralia.co>
2025-09-04 12:06:20 +0200

    siae_evaluations: add final_state in EvaluatedSiae model
    Do not recompute the value of evaluated_siae.state after EvaluationCampaign is closed

--- itou/siae_evaluations/models.py
+++ itou/siae_evaluations/models.py
@@ -544,6 +544,14 @@ class EvaluatedSiae(models.Model):
 
     reminder_sent_at = models.DateTimeField(verbose_name="rappel envoyé le", null=True, blank=True)
 
+    final_state = models.CharField(
+        verbose_name="état final après la cloture de la campagne d'évaluation",
+        choices=evaluation_enums.EvaluatedSiaeFinalState.choices,
+        blank=True,
+        null=True,
+        editable=False,
+    )
+
     objects = EvaluatedSiaeQuerySet.as_manager()
 
     class Meta:
