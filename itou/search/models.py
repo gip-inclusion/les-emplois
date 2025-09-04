260f0b78c985ee540b9b1e014421de96889ba5b5
Ewen Corre <ewen.corre@inclusion.gouv.fr>
2025-09-04 10:30:37 +0200

    delete saved searches

--- itou/search/models.py
+++ itou/search/models.py
@@ -4,9 +4,10 @@ from django.db import models
 from django.urls import reverse
 
 from itou.cities.models import City
-from itou.common_apps.address.departments import DEPARTMENTS
+from itou.common_apps.address.departments import DEPARTMENTS, format_district
 from itou.companies.enums import CompanyKind
 from itou.jobs.models import ROME_DOMAINS
+from itou.utils.templatetags.str_filters import pluralizefr
 
 
 class SavedSearch(models.Model):
@@ -62,4 +63,48 @@ class SavedSearch(models.Model):
                 "domains": self.domains,
             }
             | self.districts,
+        )
+
+    @property
+    def display_details(self):
+        departments_str = (
+            f"département{pluralizefr(self.departments)} {', '.join(self.departments)}" if self.departments else None
+        )
+
+        districts = [
+            format_district(post_code=d, department=self.city.department)
+            for d in self.districts.get(f"districts_{self.city.department}", [])
+        ]
+        districts_str = f"{', '.join(districts)} arrondissement{pluralizefr(districts)}" if districts else None
+
+        kinds_str = ", ".join(self.kinds) if self.kinds else None
+
+        domains_str = (
+            ", ".join([ROME_DOMAINS.get(domain) for domain in self.domains])
+            if self.domains and len(self.domains) <= 2
+            else None
+        )
+
+        from itou.www.search_views.forms import JobDescriptionSearchForm
+
+        CONTRACT_TYPE_DICT = dict(JobDescriptionSearchForm.CONTRACT_TYPE_CHOICES)
+        contract_types_str = (
+            ", ".join([CONTRACT_TYPE_DICT.get(contract) for contract in self.contract_types])
+            if self.contract_types
+            else None
+        )
+
+        return ", ".join(
+            filter(
+                lambda x: x is not None,
+                [
+                    self.city.name,
+                    f"{self.distance} km",
+                    districts_str,
+                    kinds_str,
+                    departments_str,
+                    contract_types_str,
+                    domains_str,
+                ],
+            )
         )
