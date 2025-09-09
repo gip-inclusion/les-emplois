a9804712393841a853c939d1934550d14bc98c88
François Freitag <mail@franek.fr>
2025-09-09 16:25:31 +0200

    eligibility: Certify the RQTH criteria
    Update the criteria_certification_badge to use an actual certification
    period. Previously, the certification was mostly binary (certified/not
    certified), because the API Particulier certifies from the current day,
    and has no end date.
    
    The RQTH validity follows calendar spans. To simplify the
    implementation, the certification_period does not store the history of
    validity periods, but only the active period (when the criteria was
    selected in an eligibility diagnosis).
    A retry mechanism retries certificating the criteria for job
    applications where the certification would be beneficial (not hired
    yet).
    
    TODO:
    - early PR to create the task before calling it
    - maybe reorganize tests/utils/template_tags (to collect all template
      tags and make submodules tests.utils.templatetags.test_xxx)

--- tests/www/eligibility_views/utils.py
+++ tests/www/eligibility_views/utils.py
@@ -14,3 +14,21 @@ NOT_CERTIFIED_BADGE_HTML = """\
 <span class="badge badge-xs rounded-pill bg-warning-lighter text-warning ms-1">
     <i class="ri-error-warning-fill" aria-hidden="true"></i>
     Non certifié</span>"""
+EXPIRED_CERTIFICATION_HTML = """\
+    <span class="badge badge-xs bg-warning-lighter ms-1 rounded-pill text-warning">
+        <i aria-hidden="true" class="ri-error-warning-fill"></i>
+        Certification expirée</span>"""
+
+
+def certified_temporarily_html(until_date):
+    return f"""\
+    <span class="badge badge-xs bg-warning-lighter ms-1 rounded-pill text-warning">
+        <i aria-hidden="true" class="ri-timer-line"></i>Certifié jusqu’au {until_date:%d/%m/%Y}
+    </span>"""
+
+
+def certified_in_the_future_html(from_date):
+    return f"""\
+    <span class="badge badge-xs bg-warning-lighter ms-1 rounded-pill text-warning">
+        <i aria-hidden="true" class="ri-timer-line"></i>Certifié à partir du {from_date:%d/%m/%Y}
+    </span>"""
