969a26ca96ff9c791e2860acb14fdcf74c148941
Xavier Fernandez <xavier.fernandez@inclusion.gouv.fr>
2025-09-11 13:43:07 +0200

    check_inconsistencies: check limit_choices_to consistency

--- tests/utils/test_check_inconsistencies.py
+++ tests/utils/test_check_inconsistencies.py
@@ -1,5 +1,7 @@
 from django.core.management import call_command
 
+from tests.companies.factories import CompanyFactory
+from tests.eligibility.factories import GEIQEligibilityDiagnosisFactory
 from tests.users.factories import EmployerFactory, JobSeekerProfileFactory
 
 
@@ -36,6 +38,26 @@ def test_inconsistencies_with_slack_webhook(caplog, settings, mocker):
                 "1 incohérence trouvée:\n"
                 f" - http://localhost:8000/admin/users/jobseekerprofile/{inconsistent_profile.pk}/change/ :"
                 " ['Profil lié à un utilisateur non-candidat']"
+            ),
+            url=settings.SLACK_INCONSISTENCIES_WEBHOOK_URL,
+        )
+    ]
+
+
+def test_limit_choices_to_inconsistency_detection(caplog, settings, mocker):
+    diag = GEIQEligibilityDiagnosisFactory(from_employer=True)
+    diag._meta.model.objects.filter(pk=diag.pk).update(author_geiq=CompanyFactory())
+    slack_mock = mocker.patch("itou.utils.management.commands.check_inconsistencies.send_slack_message")
+    settings.SLACK_INCONSISTENCIES_WEBHOOK_URL = "http://slack.fake"
+    call_command("check_inconsistencies")
+
+    assert "GEIQEligibilityDiagnosis - found 1 limit_choices_to inconsistencies" in caplog.text
+    assert slack_mock.mock_calls == [
+        mocker.call(
+            text=(
+                "1 incohérence trouvée:\n"
+                f" - http://localhost:8000/admin/eligibility/geiqeligibilitydiagnosis/{diag.pk}/change/ :"
+                " [\"La valeur du champ 'author_geiq' (GEIQ de l'auteur) ne respecte pas {'kind': CompanyKind.GEIQ}\"]"
             ),
             url=settings.SLACK_INCONSISTENCIES_WEBHOOK_URL,
         )
