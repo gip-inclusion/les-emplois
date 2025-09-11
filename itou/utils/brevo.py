fdc021b1ed1de63a990b0e5ad47dd39fbb2fae74
vincent porte <vincent@neuralia.co>
2025-09-11 16:30:38 +0200

    archive: do not send HTTP Delete request to Brevo when email ends with _old
    Brevo answers with a 400 status code.
    These addresses do not exist in Brevo, they are generated by support team, in admin views, when they deactivate a user to free his/her email address.

--- itou/utils/brevo.py
+++ itou/utils/brevo.py
@@ -73,7 +73,10 @@ class BrevoClient:
                 self._import_contacts(batch, list_id, serializer)
 
     def delete_contact(self, email):
-        if not email:
+        # Brevo rejects with 400 status code, HTTP DELETE calls for addresses ending by "_old"
+        # These address are made when Support Team disable a user in admin
+        # We do not want to call Brevo in that case
+        if not email or email.endswith("_old"):
             return
         try:
             response = self.client.delete(
