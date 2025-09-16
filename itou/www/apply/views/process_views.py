679929ae23979f8d7490ef74d807be3791154ec4
vincent porte <vincent@neuralia.co>
2025-09-16 15:27:49 +0200

    apply: add simplified view for AcceptView
    Dispatch to apply:details_for_company when certain personal data required from a job applicant is missing
    Remove form_user_address, form_personal_data and form_birth_place
    Compliant with legacy display for backward compatibility

--- itou/www/apply/views/process_views.py
+++ itou/www/apply/views/process_views.py
@@ -504,6 +504,16 @@ class AcceptView(common_views.BaseAcceptView):
                 extra_tags="toast",
             )
             return HttpResponseRedirect(self.next_url)
+        if self.only_accept_form:
+            missing_fields = self.job_seeker.jobseeker_profile.get_missing_personal_info_for_hire()
+            if missing_fields:
+                fmt_missing_fields = ", ".join(sorted(missing_fields))
+                messages.error(
+                    request,
+                    f"Les données suivantes sont manquantes pour accepter la candidature : {fmt_missing_fields}.",
+                    extra_tags="toast",
+                )
+                return HttpResponseRedirect(self.next_url)
         return super().dispatch(request, *args, **kwargs)
 
     def get_back_url(self):
@@ -901,7 +911,7 @@ class IAEEligibilityView(BaseIAEEligibilityViewForEmployer):
 
     def get_success_url(self):
         return reverse(
-            "apply:accept",
+            "apply:accept_contract",
             kwargs={"job_application_id": self.job_application.id},
             query={"next_url": self.next_url} if self.next_url else None,
         )
