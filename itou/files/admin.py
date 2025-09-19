0d67f8fa0baff33767655278abc3b856383d3ac7
Xavier Fernandez <xavier.fernandez@inclusion.gouv.fr>
2025-09-19 10:18:06 +0200

    files: make the admin readonly
    No reason to add/edit/delete a file directly.

--- itou/files/admin.py
+++ itou/files/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from django.utils.html import format_html
 
 from itou.files.models import File
-from itou.utils.admin import ItouModelAdmin
+from itou.utils.admin import ItouModelAdmin, ReadonlyMixin
 
 
 @admin.register(File)
-class FileAdmin(ItouModelAdmin):
+class FileAdmin(ReadonlyMixin, ItouModelAdmin):
     list_display = ["key", "last_modified"]
     readonly_fields = ["key", "link", "last_modified"]
 
