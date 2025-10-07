6ed09b58ac1bc47f95d997574575f43d07975c11
Xavier Fernandez <xavier.fernandez@inclusion.gouv.fr>
2025-10-07 14:14:17 +0200

    siae_evaluations: drop data migration
    It has been run in production and its use of the live model prevents the
    addition of new columns.

--- itou/siae_evaluations/migrations/0003_evaluatedsiae_final_state.py
+++ itou/siae_evaluations/migrations/0003_evaluatedsiae_final_state.py
@@ -1,21 +1,5 @@
 from django.db import migrations, models
 
-from itou.siae_evaluations.models import EvaluatedSiae
-
-
-def populate_final_state(apps, schema_editor):
-    evaluated_siaes = list(
-        EvaluatedSiae.objects.filter(evaluation_campaign__ended_at__isnull=False)
-        .prefetch_related("evaluated_job_applications__evaluated_administrative_criteria")
-        .select_for_update(of=("self",), no_key=True)
-    )
-
-    for evaluated_siae in evaluated_siaes:
-        evaluated_siae.final_state = evaluated_siae.state
-
-    updated = EvaluatedSiae.objects.bulk_update(evaluated_siaes, ["final_state"])
-    print(f"updated {updated} EvaluatedSiae")
-
 
 class Migration(migrations.Migration):
     dependencies = [
@@ -34,5 +18,4 @@ class Migration(migrations.Migration):
                 verbose_name="état final après la cloture de la campagne d'évaluation",
             ),
         ),
-        migrations.RunPython(code=populate_final_state, reverse_code=migrations.RunPython.noop, elidable=True),
     ]
