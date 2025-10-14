7a415752d0ef50f405b4db664a2eda93909afb37
François Freitag <mail@franek.fr>
2025-10-14 17:29:22 +0200

    eligibility: Store SelectedAdministrativeCriteria last certification attempt
    Storing the latest attempt is the first step towards a more robust
    re-certification policy.

---
+++ itou/eligibility/migrations/0004_geiqselectedadministrativecriteria_last_certification_attempt_at.py
@@ -0,0 +1,17 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("eligibility", "0003_selectedadministrativecriteria_last_certification_attempt_at"),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name="geiqselectedadministrativecriteria",
+            name="last_certification_attempt_at",
+            field=models.DateTimeField(
+                blank=True, editable=False, null=True, verbose_name="dernière tentative de certification"
+            ),
+        ),
+    ]
