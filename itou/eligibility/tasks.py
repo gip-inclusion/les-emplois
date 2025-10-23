634daf8aa42714ee1faf950b8c8f82381cad03f9
François Freitag <mail@franek.fr>
2025-10-23 17:49:44 +0200

    eligibility: Introduce async_certify_criterion_with_api_particulier
    The eligibility diagnosis isn’t the correct level to handle
    certification. If multiple criteria can be certified, a failure for one
    criteria would prevent other criteria from being certified.
    
    Instead, deal with each criteria independently.
    
    The task is not called yet, to let it be deployed on the worker first.
    It will be called in a future commit.

--- itou/eligibility/tasks.py
+++ itou/eligibility/tasks.py
@@ -21,103 +21,126 @@ from itou.utils.types import InclusiveDateRange
 logger = logging.getLogger("APIParticulierClient")
 
 
-def certify_eligibility_diagnosis_by_api_particulier(eligibility_diagnosis):
+def certify_criterion_with_api_particulier(criterion):
     if settings.ITOU_ENVIRONMENT == ItouEnvironment.DEV:
         logging.info(
             "API particulier is not configured in %s, certification was skipped.",
             settings.ITOU_ENVIRONMENT,
         )
         return
-    job_seeker = eligibility_diagnosis.job_seeker
+    job_seeker = criterion.eligibility_diagnosis.job_seeker
     if not api_particulier.has_required_info(job_seeker):
         logger.info("Skipping job seeker %s, missing required information.", job_seeker.pk)
         return
-    SelectedAdministrativeCriteria = eligibility_diagnosis.administrative_criteria.through
-    criteria = (
-        SelectedAdministrativeCriteria.objects.filter(
-            administrative_criteria__kind__in=AdministrativeCriteriaKind.certifiable_by_api_particulier(),
-            eligibility_diagnosis=eligibility_diagnosis,
-        )
-        .select_for_update(of=("self",), no_key=True)
-        .select_related("administrative_criteria")
-    )
     with api_particulier.client() as client:
-        for criterion in criteria:
-            try:
-                data = api_particulier.certify_criteria(criterion.administrative_criteria.kind, client, job_seeker)
-            except httpx.HTTPStatusError as exc:
-                criterion.data_returned_by_api = exc.response.json()
-                match exc.response.status_code:
-                    case (
-                        # Identity found, but not attached to a data provider
-                        # (no “caisse de rattachement”, or no entry for that
-                        # person in the “caisse de rattachement”).
-                        404
-                        # Identity not found, change the query parameters.
-                        | 422
-                    ):
-                        if "errors" not in criterion.data_returned_by_api:
-                            raise
-                        criterion.certified_at = timezone.now()
-                    case 429:
-                        # https://particulier.api.gouv.fr/developpeurs#respecter-la-volumétrie
-                        raise RetryTask(delay=int(exc.response.headers["Retry-After"])) from exc
-                    case 502:
-                        if len(criterion.data_returned_by_api["errors"]) == 1 and criterion.data_returned_by_api[
-                            "errors"
-                        ][0]["code"] in {
-                            api_particulier.UNKNOWN_RESPONSE_FROM_PROVIDER_CNAV_ERROR_CODE,
-                            api_particulier.UNKNOWN_RESPONSE_FROM_PROVIDER_SECURITE_SOCIALE_ERROR_CODE,
-                        }:
-                            # Retrying won’t fix the issue.
-                            logger.info(
-                                "Error certifying criterion %r, API Particulier got an unknown response "
-                                "from the data provider.",
-                                criterion,
-                            )
-                        else:
-                            raise
-                    case _:
+        try:
+            data = api_particulier.certify_criteria(criterion.administrative_criteria.kind, client, job_seeker)
+        except httpx.HTTPStatusError as exc:
+            criterion.data_returned_by_api = exc.response.json()
+            match exc.response.status_code:
+                case (
+                    # Identity found, but not attached to a data provider
+                    # (no “caisse de rattachement”, or no entry for that
+                    # person in the “caisse de rattachement”).
+                    404
+                    # Identity not found, change the query parameters.
+                    | 422
+                ):
+                    if "errors" not in criterion.data_returned_by_api:
                         raise
-            else:
-                criterion.certified = data["is_certified"]
-                criterion.certified_at = timezone.now()
-                criterion.data_returned_by_api = data["raw_response"]
-                criterion.certification_period = None
-                if criterion.certified:
-                    start_at = data["start_at"]
-                    end_at = timezone.localdate(criterion.certified_at) + datetime.timedelta(
-                        days=criterion.CERTIFICATION_GRACE_PERIOD_DAYS
-                    )
-                    criterion.certification_period = InclusiveDateRange(start_at, end_at)
+                    criterion.certified_at = timezone.now()
+                case 429:
+                    # https://particulier.api.gouv.fr/developpeurs#respecter-la-volumétrie
+                    raise RetryTask(delay=int(exc.response.headers["Retry-After"])) from exc
+                case 502:
+                    if len(criterion.data_returned_by_api["errors"]) == 1 and criterion.data_returned_by_api["errors"][
+                        0
+                    ]["code"] in {
+                        api_particulier.UNKNOWN_RESPONSE_FROM_PROVIDER_CNAV_ERROR_CODE,
+                        api_particulier.UNKNOWN_RESPONSE_FROM_PROVIDER_SECURITE_SOCIALE_ERROR_CODE,
+                    }:
+                        # Retrying won’t fix the issue.
+                        logger.info(
+                            "Error certifying criterion %r, API Particulier got an unknown response "
+                            "from the data provider.",
+                            criterion,
+                        )
+                    else:
+                        raise
+                case _:
+                    raise
+        else:
+            criterion.certified = data["is_certified"]
+            criterion.certified_at = timezone.now()
+            criterion.data_returned_by_api = data["raw_response"]
+            criterion.certification_period = None
+            if criterion.certified:
+                start_at = data["start_at"]
+                end_at = timezone.localdate(criterion.certified_at) + datetime.timedelta(
+                    days=criterion.CERTIFICATION_GRACE_PERIOD_DAYS
+                )
+                criterion.certification_period = InclusiveDateRange(start_at, end_at)
+    with transaction.atomic():
+        criterion.save()
+        if criterion.certified is not None:
+            IdentityCertification.objects.upsert_certifications(
+                [
+                    IdentityCertification(
+                        certifier=IdentityCertificationAuthorities.API_PARTICULIER,
+                        jobseeker_profile=job_seeker.jobseeker_profile,
+                        certified_at=criterion.certified_at,
+                    ),
+                ]
+            )
 
-    SelectedAdministrativeCriteria.objects.bulk_update(
-        criteria,
-        fields=[
-            "certification_period",
-            "certified",
-            "certified_at",
-            "data_returned_by_api",
-        ],
-    )
-    if any(c.certified is not None for c in criteria):
-        IdentityCertification.objects.upsert_certifications(
-            [
-                IdentityCertification(
-                    certifier=IdentityCertificationAuthorities.API_PARTICULIER,
-                    jobseeker_profile=job_seeker.jobseeker_profile,
-                    certified_at=max(c.certified_at for c in criteria),
-                ),
-            ]
-        )
+
+# Retry every 10 minutes for 24h.
+@on_commit_task(retries=24 * 6, retry_delay=10 * 60)
+def async_certify_criterion_with_api_particulier(model_name, selected_administrative_criteria_id):
+    model = apps.get_model("eligibility", model_name)
+    with transaction.atomic():
+        try:
+            criterion = (
+                model.objects.select_related(
+                    "eligibility_diagnosis__job_seeker__jobseeker_profile",
+                    "administrative_criteria",
+                )
+                .select_for_update(of=("self",), no_key=True)
+                .get(pk=selected_administrative_criteria_id)
+            )
+        except model.DoesNotExist:
+            logger.info(
+                "%s with pk %d does not exist, it cannot be certified.",
+                model_name,
+                selected_administrative_criteria_id,
+            )
+            return
+
+        captured_exc = None
+        retry = False
+        try:
+            certify_criterion_with_api_particulier(criterion)
+        except (
+            httpx.HTTPError,  # Could not connect, unexpected status code, …
+            JSONDecodeError,  # Response was not JSON (text, HTML, …).
+            RetryTask,  # Rate limiting.
+        ) as e:
+            retry = True
+            captured_exc = e
+        except Exception as e:
+            retry = False
+            captured_exc = e
+    # Outer transaction committed, we can raise.
+    if captured_exc:
+        if retry:
+            raise captured_exc
+        logger.exception(captured_exc)
 
 
 def _async_certify_eligibility_diagnosis_by_api_particulier(model_name, eligibility_diagnosis_pk):
     model = apps.get_model("eligibility", model_name)
     try:
-        eligibility_diagnosis = model.objects.select_related("job_seeker__jobseeker_profile").get(
-            pk=eligibility_diagnosis_pk
-        )
+        eligibility_diagnosis = model.objects.get(pk=eligibility_diagnosis_pk)
     except model.DoesNotExist:
         logger.info(
             "%s with pk %d does not exist, it cannot be certified.",
@@ -125,20 +148,13 @@ def _async_certify_eligibility_diagnosis_by_api_particulier(model_name, eligibil
             eligibility_diagnosis_pk,
         )
         return
-    try:
-        with transaction.atomic():
-            certify_eligibility_diagnosis_by_api_particulier(eligibility_diagnosis)
-    except (
-        httpx.HTTPError,  # Could not connect, unexpected status code, …
-        JSONDecodeError,  # Response was not JSON (text, HTML, …).
-        RetryTask,  # Rate limiting.
+    for criterion in eligibility_diagnosis.selected_administrative_criteria.filter(
+        administrative_criteria__kind__in=AdministrativeCriteriaKind.certifiable_by_api_particulier()
     ):
-        # Worth retrying.
-        raise
-    except Exception as e:
-        logger.exception(e)
+        async_certify_criterion_with_api_particulier(criterion._meta.model_name, criterion.pk)
 
 
+# Deprecated, preserved for backward-compatibility. Drop 24h after the deploy.
 # Retry every 10 minutes for 24h.
 async_certify_eligibility_diagnosis_by_api_particulier = on_commit_task(retries=24 * 6, retry_delay=10 * 60)(
     _async_certify_eligibility_diagnosis_by_api_particulier,
--- tests/eligibility/test_tasks.py
+++ tests/eligibility/test_tasks.py
@@ -9,10 +9,7 @@ from huey.exceptions import RetryTask
 from pytest_django.asserts import assertQuerySetEqual
 
 from itou.eligibility.enums import CERTIFIABLE_ADMINISTRATIVE_CRITERIA_KINDS, AdministrativeCriteriaKind
-from itou.eligibility.tasks import (
-    async_certify_eligibility_diagnosis_by_api_particulier,
-    certify_eligibility_diagnosis_by_api_particulier,
-)
+from itou.eligibility.tasks import async_certify_criterion_with_api_particulier, certify_criterion_with_api_particulier
 from itou.users.enums import IdentityCertificationAuthorities
 from itou.users.models import JobSeekerProfile
 from itou.utils.apis import api_particulier
@@ -49,21 +46,16 @@ class TestCertifyCriteriaApiParticulier:
     @freeze_time("2025-01-06")
     def test_queue_task(self, criteria_kind, factory, respx_mock):
         eligibility_diagnosis = factory(certifiable=True, criteria_kinds=[criteria_kind])
+        criterion = eligibility_diagnosis.selected_administrative_criteria.get()
         response = RESPONSES[criteria_kind][ResponseKind.CERTIFIED]
         respx_mock.get(settings.API_PARTICULIER_BASE_URL + api_particulier.ENDPOINTS[criteria_kind]).respond(
             status_code=response["status_code"], json=response["json"]
         )
 
-        async_certify_eligibility_diagnosis_by_api_particulier.call_local(
-            eligibility_diagnosis._meta.model_name, eligibility_diagnosis.pk
-        )
+        async_certify_criterion_with_api_particulier.call_local(criterion._meta.model_name, criterion.pk)
 
+        criterion.refresh_from_db()
         assert len(respx_mock.calls) == 1
-        SelectedAdministrativeCriteria = eligibility_diagnosis.administrative_criteria.through
-        criterion = SelectedAdministrativeCriteria.objects.filter(
-            administrative_criteria__kind=criteria_kind,
-            eligibility_diagnosis=eligibility_diagnosis,
-        ).get()
         assert criterion.certified is True
         assert criterion.certified_at is not None
         assert criterion.data_returned_by_api == response["json"]
@@ -82,22 +74,17 @@ class TestCertifyCriteriaApiParticulier:
     def test_retry_task_rate_limits(self, factory, respx_mock):
         with freeze_time("2024-09-12T00:00:00Z"):
             eligibility_diagnosis = factory(certifiable=True, criteria_kinds=[AdministrativeCriteriaKind.RSA])
+            criterion = eligibility_diagnosis.selected_administrative_criteria.get()
             respx_mock.get("https://fake-api-particulier.com/v3/dss/revenu_solidarite_active/identite").mock(
                 return_value=httpx.Response(429, headers={"Retry-After": "1"}, json={}),
             )
 
             with pytest.raises(RetryTask) as exc_info:
-                async_certify_eligibility_diagnosis_by_api_particulier.call_local(
-                    eligibility_diagnosis._meta.model_name, eligibility_diagnosis.pk
-                )
+                async_certify_criterion_with_api_particulier.call_local(criterion._meta.model_name, criterion.pk)
 
+            criterion.refresh_from_db()
             assert exc_info.value.delay == 1
             assert len(respx_mock.calls) == 1
-            SelectedAdministrativeCriteria = eligibility_diagnosis.administrative_criteria.through
-            criterion = SelectedAdministrativeCriteria.objects.filter(
-                administrative_criteria__kind=AdministrativeCriteriaKind.RSA,
-                eligibility_diagnosis=eligibility_diagnosis,
-            ).get()
             assert criterion.certified is None
             assert criterion.certified_at is None
             assert criterion.data_returned_by_api is None
@@ -127,11 +114,12 @@ class TestCertifyCriteriaApiParticulier:
         self, status_code, json_data, headers, retry_task_exception, factory, respx_mock
     ):
         eligibility_diagnosis = factory(certifiable=True, criteria_kinds=[AdministrativeCriteriaKind.RSA])
+        criterion = eligibility_diagnosis.selected_administrative_criteria.get()
         respx_mock.get("https://fake-api-particulier.com/v3/dss/revenu_solidarite_active/identite").respond(
             status_code, json=json_data, headers=headers
         )
         try:
-            certify_eligibility_diagnosis_by_api_particulier(eligibility_diagnosis)
+            certify_criterion_with_api_particulier(criterion)
         except RetryTask:
             retry_task = True
         except Exception:
@@ -146,18 +134,15 @@ class TestCertifyCriteriaApiParticulier:
 
     def test_ignores_api_particulier_internal_error(self, factory, respx_mock):
         eligibility_diagnosis = factory(certifiable=True, criteria_kinds=[AdministrativeCriteriaKind.RSA])
+        criterion = eligibility_diagnosis.selected_administrative_criteria.get()
         response = RESPONSES[AdministrativeCriteriaKind.RSA][ResponseKind.PROVIDER_UNKNOWN_ERROR]
         respx_mock.get("https://fake-api-particulier.com/v3/dss/revenu_solidarite_active/identite").respond(
             status_code=response["status_code"], json=response["json"]
         )
         # Does not raise a RetryTask, this specific error is ignored.
-        async_certify_eligibility_diagnosis_by_api_particulier.call_local(
-            eligibility_diagnosis._meta.model_name, eligibility_diagnosis.pk
-        )
+        async_certify_criterion_with_api_particulier.call_local(criterion._meta.model_name, criterion.pk)
         assert len(respx_mock.calls) == 1
-        criterion = eligibility_diagnosis.selected_administrative_criteria.get(
-            administrative_criteria__kind=AdministrativeCriteriaKind.RSA,
-        )
+        criterion.refresh_from_db()
         assert criterion.certified is None
         assert criterion.certified_at is None
         assert criterion.data_returned_by_api == response["json"]
@@ -170,18 +155,15 @@ class TestCertifyCriteriaApiParticulier:
         self, factory, mutate_response, respx_mock
     ):
         eligibility_diagnosis = factory(certifiable=True, criteria_kinds=[AdministrativeCriteriaKind.RSA])
+        criterion = eligibility_diagnosis.selected_administrative_criteria.get()
         response = mutate_response(RESPONSES[AdministrativeCriteriaKind.RSA][ResponseKind.PROVIDER_UNKNOWN_ERROR])
         respx_mock.get("https://fake-api-particulier.com/v3/dss/revenu_solidarite_active/identite").respond(
             status_code=response["status_code"], json=response["json"]
         )
         with pytest.raises(httpx.HTTPStatusError):
-            async_certify_eligibility_diagnosis_by_api_particulier.call_local(
-                eligibility_diagnosis._meta.model_name, eligibility_diagnosis.pk
-            )
+            async_certify_criterion_with_api_particulier.call_local(criterion._meta.model_name, criterion.pk)
         assert len(respx_mock.calls) == 1
-        criterion = eligibility_diagnosis.selected_administrative_criteria.get(
-            administrative_criteria__kind=AdministrativeCriteriaKind.RSA,
-        )
+        criterion.refresh_from_db()
         assert criterion.certified is None
         assert criterion.certified_at is None
         assert criterion.data_returned_by_api is None
@@ -189,8 +171,9 @@ class TestCertifyCriteriaApiParticulier:
         jobseeker_profile = JobSeekerProfile.objects.get(pk=eligibility_diagnosis.job_seeker.jobseeker_profile)
         assertQuerySetEqual(jobseeker_profile.identity_certifications.all(), [])
 
-    def test_no_retries_when_diag_does_not_exist(self, caplog, factory):
-        eligibility_diagnosis_model = factory._meta.model
-        modelname = eligibility_diagnosis_model._meta.model_name
-        async_certify_eligibility_diagnosis_by_api_particulier.call_local(modelname, 0)
+    def test_no_retries_when_criterion_does_not_exist(self, caplog, factory):
+        eligibility_diagnosis = factory(certifiable=True, criteria_kinds=[AdministrativeCriteriaKind.RSA])
+        criterion = eligibility_diagnosis.selected_administrative_criteria.get()
+        modelname = criterion._meta.model_name
+        async_certify_criterion_with_api_particulier.call_local(modelname, 0)
         assert caplog.messages == [f"{modelname} with pk 0 does not exist, it cannot be certified."]
