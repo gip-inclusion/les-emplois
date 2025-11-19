5d76781df725f18c29f740533a3b3b707f5a3515
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2025-11-19 20:53:39 +0100

    nexus: Add utils to store activated services
    Since Pilotage is already available for all emplois users, we want to be
    able to track which users use it
    For mon recap, we might be able to sync users that used the tally, but
    in the MVP, we'll track the users clicking on the "activate" button for
    this service.

--- tests/nexus/test_utils.py
+++ tests/nexus/test_utils.py
@@ -1,10 +1,13 @@
 import datetime
 
 import pytest
+from django.utils import timezone
 from freezegun import freeze_time
 from jwcrypto import jwt
 
-from itou.nexus.utils import EXPIRY_DELAY, decode_jwt, generate_jwt
+from itou.nexus.enums import Service
+from itou.nexus.models import User
+from itou.nexus.utils import EXPIRY_DELAY, activate_service, decode_jwt, generate_jwt
 from tests.users.factories import PrescriberFactory
 
 
@@ -25,3 +28,53 @@ def test_generate_and_decode_jwt():
         frozen_now.tick(datetime.timedelta(seconds=EXPIRY_DELAY + leeway + 1))
         with pytest.raises(ValueError):
             decode_jwt(token)
+
+
+def test_activate_service_bad_service():
+    user = PrescriberFactory.build()
+    for service in Service:
+        if service not in [Service.PILOTAGE, Service.MON_RECAP]:
+            with pytest.raises(AssertionError):
+                activate_service(user, service)
+
+
+def test_activate_service_mon_recap():
+    user = PrescriberFactory(last_login=timezone.now())
+    activate_service(user, Service.MON_RECAP)
+    nexus_user = User.objects.get()
+    assert nexus_user.source_id != user.pk
+    assert nexus_user.source == "mon-recap"
+    assert nexus_user.id == f"mon-recap--{nexus_user.source_id}"
+    assert nexus_user.email == user.email
+    assert nexus_user.last_name == user.last_name
+    assert nexus_user.first_name == user.first_name
+    assert nexus_user.phone == user.phone
+    assert nexus_user.auth == ""
+    assert nexus_user.kind == ""
+    updated_at = nexus_user.updated_at
+
+    # Allow a second call with same email / service to update the user data
+    activate_service(user, Service.MON_RECAP)
+    nexus_user = User.objects.get()
+    assert nexus_user.updated_at > updated_at
+
+
+def test_activate_service_pilotage():
+    user = PrescriberFactory(last_login=timezone.now())
+    activate_service(user, Service.PILOTAGE)
+    nexus_user = User.objects.get()
+    assert nexus_user.source_id == str(user.pk)
+    assert nexus_user.source == "pilotage"
+    assert nexus_user.id == f"pilotage--{user.pk}"
+    assert nexus_user.email == user.email
+    assert nexus_user.last_name == user.last_name
+    assert nexus_user.first_name == user.first_name
+    assert nexus_user.phone == user.phone
+    assert nexus_user.auth == ""
+    assert nexus_user.kind == ""
+    updated_at = nexus_user.updated_at
+
+    # Allow a second call with same email / service to update the user data
+    activate_service(user, Service.PILOTAGE)
+    nexus_user = User.objects.get()
+    assert nexus_user.updated_at > updated_at
