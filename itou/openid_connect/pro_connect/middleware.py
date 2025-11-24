7f85b4863b328198e56bc3575d958df9da434ece
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2025-11-24 22:40:49 +0100

    pro_connect: Change proconnect middleware param
    This is a breaking change. We will sync release here and in
    la-communauté

--- itou/openid_connect/pro_connect/middleware.py
+++ itou/openid_connect/pro_connect/middleware.py
@@ -1,31 +1,58 @@
+import logging
+
+from django.contrib.auth import logout
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.utils.deprecation import MiddlewareMixin
-from django.utils.http import urlencode
 
-from itou.users.enums import IdentityProvider
+from itou.nexus.utils import decode_jwt
+from itou.users.enums import UserKind
 from itou.users.models import User
+
+
+logger = logging.getLogger(__name__)
 
 
 class ProConnectLoginMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        if "proconnect_login" not in request.GET:
+        if "auto_login" not in request.GET:
             return
 
         query_params = request.GET.copy()
-        query_params.pop("proconnect_login")
-        username = query_params.pop("username", [None])
-        new_url = (
-            f"{request.path}?{urlencode({k: v for k, v in query_params.items() if v})}"
-            if query_params
-            else request.path
-        )
+        auto_login = query_params.pop("auto_login")
+        email = None
 
-        if request.user.is_authenticated:
+        new_url = f"{request.path}?{query_params.urlencode()}" if query_params else request.path
+
+        if len(auto_login) != 1:
+            logger.info("Nexus auto login: Multiple tokens found -> ignored")
             return HttpResponseRedirect(new_url)
+        else:
+            [token] = auto_login
 
         try:
-            user = User.objects.get(username=username[0], identity_provider=IdentityProvider.PRO_CONNECT.value)
+            decoded_data = decode_jwt(token)
+            email = decoded_data.get("email")
+        except ValueError:
+            logger.info("Invalid auto login token")
+
+        if email is None:
+            logger.info("ProConnect auto login: Missing email in token -> ignored")
+            return HttpResponseRedirect(new_url)
+
+        if request.user.is_authenticated:
+            if request.user.email == email:
+                logger.info("ProConnect auto login: user is already logged in")
+                return HttpResponseRedirect(new_url)
+            else:
+                logger.info("ProConnect auto login: wrong user is logged in -> logging them out")
+                # We should probably also logout the user from ProConnect but it requires to redirect to ProConnect
+                # and the flow becomes a lotmore complicated
+                logout(request)
+
+        try:
+            user = User.objects.get(email=email, kind__in=[UserKind.EMPLOYER, UserKind.PRESCRIBER])
+            logger.info("ProConnect auto login: %s was found and forwarded to ProConnect", user)
             return HttpResponseRedirect(
                 reverse(
                     "pro_connect:authorize",
@@ -33,4 +60,7 @@ class ProConnectLoginMiddleware(MiddlewareMixin):
                 )
             )
         except User.DoesNotExist:
+            # There's no user with this email, we have to create them an account but we need to know
+            # if they are a prescriber or an employer.
+            logger.info("ProConnect auto login: no user found for jwt=%s", token)
             return HttpResponseRedirect(reverse("signup:choose_user_kind", query={"next_url": new_url}))
