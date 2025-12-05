917a52a92188264e23e10c6645127d360e8a7774
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2025-12-05 10:19:15 +0100

    nexus: Add utils to store activated services
    Since Pilotage is already available for all emplois users, we want to be
    able to track which users use it
    For mon recap, we might be able to sync users that used the tally, but
    in the MVP, we'll track the users clicking on the "activate" button for
    this service.

--- itou/nexus/utils.py
+++ itou/nexus/utils.py
@@ -1,11 +1,13 @@
 import json
 import logging
 import time
+import uuid
 
 from django.conf import settings
 from jwcrypto import jwk, jwt
 
 from itou.nexus.enums import Service
+from itou.nexus.models import NexusUser
 
 
 logger = logging.getLogger(__name__)
@@ -46,3 +48,29 @@ SERVICE_MAPPING = {
 
 def unique_id(id, service):
     return f"{SERVICE_MAPPING[service]}--{id}"
+
+
+def activate_service(user, service):
+    # Allow to track services without user sync mecanism
+    assert service in [Service.PILOTAGE, Service.MON_RECAP]
+    # pilotage uses (and is available to ) users from les emplois
+    # mon recap doesn't have user ids, just fake one
+    id = uuid.uuid4() if service == Service.MON_RECAP else user.pk
+    defaults = {
+        "first_name": user.first_name,
+        "last_name": user.last_name,
+        "phone": user.phone,
+        "last_login": user.last_login,
+        "auth": "",
+        "kind": "",
+    }
+    NexusUser.objects.update_or_create(
+        source=service,
+        email=user.email,
+        defaults=defaults,
+        create_defaults=defaults
+        | {
+            "id": unique_id(id, service),
+            "source_id": id,
+        },
+    )
