6024d26eb80026161178974711c14eba1a4db3a7
Romain Sébille <romain.sebille@beta.gouv.fr>
2025-12-11 12:49:58 +0100

    utils.templatetags: Add `shorten` filter

--- tests/utils/tests.py
+++ tests/utils/tests.py
@@ -831,6 +831,20 @@ class TestUtilsTemplateTags:
             == "F… M… L…"
         )
 
+    def test_shorten(self):
+        text = (
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, "
+            "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        )
+        template = Template("""{% load str_filters %}{{ value|shorten:length }}""")
+
+        assert template.render(Context({"value": text, "length": 25})) == "Lorem ipsum dolor sit …"
+        assert (
+            template.render(Context({"value": text, "length": 60}))
+            == "Lorem ipsum dolor sit amet, consectetur adipiscing elit, …"
+        )
+        assert template.render(Context({"value": text, "length": len(text)})) == text
+
     def test_tally_url_custom_template_tag(self, settings):
         settings.TALLY_URL = "https://foobar"
         test_id = 1234
