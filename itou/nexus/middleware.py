515f8c573e76b0cff2fb69d672edcbe11f2ccd4b
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2025-12-15 14:26:54 +0100

    nexus: Use itoutils BaseAutoLoginMiddleware

--- itou/nexus/middleware.py
+++ itou/nexus/middleware.py
@@ -1,66 +1,19 @@
-import logging
-
-from django.contrib.auth import logout
-from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.utils.deprecation import MiddlewareMixin
-from itoutils.django.nexus.token import decode_token
+from itoutils.django.nexus.middleware import BaseAutoLoginMiddleware
 
 from itou.users.enums import UserKind
 from itou.users.models import User
 
 
-logger = logging.getLogger(__name__)
+class AutoLoginMiddleware(BaseAutoLoginMiddleware):
+    def get_queryset(self):
+        return User.objects.filter(kind__in=[UserKind.EMPLOYER, UserKind.PRESCRIBER])
 
+    def get_proconnect_authorize_url(self, user, next_url):
+        return reverse(
+            "pro_connect:authorize",
+            query={"user_kind": user.kind, "next_url": next_url, "user_email": user.email},
+        )
 
-class AutoLoginMiddleware(MiddlewareMixin):
-    def process_request(self, request):
-        if "auto_login" not in request.GET:
-            return
-
-        query_params = request.GET.copy()
-        auto_login = query_params.pop("auto_login")
-        email = None
-
-        new_url = f"{request.path}?{query_params.urlencode()}" if query_params else request.path
-
-        if len(auto_login) != 1:
-            logger.info("Nexus auto login: Multiple tokens found -> ignored")
-            return HttpResponseRedirect(new_url)
-        else:
-            [token] = auto_login
-
-        try:
-            decoded_data = decode_token(token)
-            email = decoded_data.get("email")
-        except ValueError:
-            logger.info("Invalid auto login token")
-
-        if email is None:
-            logger.info("Nexus auto login: Missing email in token -> ignored")
-            return HttpResponseRedirect(new_url)
-
-        if request.user.is_authenticated:
-            if request.user.email == email:
-                logger.info("Nexus auto login: user is already logged in")
-                return HttpResponseRedirect(new_url)
-            else:
-                logger.info("Nexus auto login: wrong user is logged in -> logging them out")
-                # We should probably also logout the user from ProConnect but it requires to redirect to ProConnect
-                # and the flow becomes a lotmore complicated
-                logout(request)
-
-        try:
-            user = User.objects.get(email=email, kind__in=[UserKind.EMPLOYER, UserKind.PRESCRIBER])
-            logger.info("Nexus auto login: %s was found and forwarded to ProConnect", user)
-            return HttpResponseRedirect(
-                reverse(
-                    "pro_connect:authorize",
-                    query={"user_kind": user.kind, "next_url": new_url, "user_email": user.email},
-                )
-            )
-        except User.DoesNotExist:
-            # There's no user with this email, we have to create them an account but we need to know
-            # if they are a prescriber or an employer.
-            logger.info("Nexus auto login: no user found for jwt=%s", token)
-            return HttpResponseRedirect(reverse("signup:choose_user_kind", query={"next_url": new_url}))
+    def get_no_user_url(self, email, next_url):
+        return reverse("signup:choose_user_kind", query={"next_url": next_url})
