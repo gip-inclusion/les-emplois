4783fa5123db5061f4be601c4d6155c297f444b9
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2025-12-15 14:26:54 +0100

    nexus: Use itoutils BaseAutoLoginMiddleware

--- tests/nexus/test_middleware.py
+++ tests/nexus/test_middleware.py
@@ -1,4 +1,3 @@
-import pytest
 from django.urls import reverse
 from itoutils.django.nexus.token import generate_token
 from pytest_django.asserts import assertRedirects
@@ -7,27 +6,18 @@ from itou.users.enums import IdentityProvider
 from tests.users.factories import EmployerFactory
 
 
-params_tuples = [
-    ({}, ""),
-    ({"filter": "76", "username": "123"}, "?filter=76&username=123"),
-]
-
-
-@pytest.mark.parametrize("params,expected_params", params_tuples)
-def test_middleware_for_authenticated_user(client, params, expected_params, caplog):
+def test_middleware_for_authenticated_user(client, caplog):
     user = EmployerFactory(membership=True)
     client.force_login(user)
-    params["auto_login"] = generate_token(user)
-
+    params = {"auto_login": generate_token(user)}
     response = client.get(reverse("home:hp", query=params))
-    assertRedirects(response, f"/{expected_params}", fetch_redirect_response=False)
+    assertRedirects(response, "/", fetch_redirect_response=False)
     assert caplog.messages == ["Nexus auto login: user is already logged in"]
 
 
-@pytest.mark.parametrize("params,expected_params", params_tuples)
-def test_middleware_for_wrong_authenticated_user(client, params, expected_params, caplog):
+def test_middleware_for_wrong_authenticated_user(client, caplog):
     user = EmployerFactory(membership=True)
-    params["auto_login"] = generate_token(user)
+    params = {"auto_login": generate_token(user)}
     # Another user is logged in
     client.force_login(EmployerFactory(membership=True))
 
@@ -36,7 +26,7 @@ def test_middleware_for_wrong_authenticated_user(client, params, expected_params
         response,
         reverse(
             "pro_connect:authorize",
-            query={"user_kind": "employer", "next_url": f"/{expected_params}", "user_email": user.email},
+            query={"user_kind": "employer", "next_url": "/", "user_email": user.email},
         ),
         fetch_redirect_response=False,
     )
@@ -46,51 +36,27 @@ def test_middleware_for_wrong_authenticated_user(client, params, expected_params
     ]
 
 
-def test_middleware_multiple_tokens(client, caplog):
-    user = EmployerFactory(membership=True)
-    params = [("auto_login", generate_token(user)), ("auto_login", generate_token(user))]
-    response = client.get(reverse("home:hp", query=params))
-    assertRedirects(response, reverse("home:hp"), fetch_redirect_response=False)
-    assert caplog.messages == [
-        "Nexus auto login: Multiple tokens found -> ignored",
-    ]
-
-
-def test_middleware_invalid_token(client, caplog):
-    params = {"auto_login": "bad jwt"}
-    response = client.get(reverse("home:hp", query=params))
-    assertRedirects(response, reverse("home:hp"), fetch_redirect_response=False)
-    assert caplog.messages == [
-        "Could not decrypt jwt",
-        "Invalid auto login token",
-        "Nexus auto login: Missing email in token -> ignored",
-    ]
-
-
-@pytest.mark.parametrize("params,expected_params", params_tuples)
-def test_middleware_with_no_existing_user(client, params, expected_params, caplog):
+def test_middleware_with_no_existing_user(client, caplog):
     jwt = generate_token(EmployerFactory.build())
-    params["auto_login"] = jwt
-    response = client.get(reverse("home:hp", query=params))
+    response = client.get(reverse("home:hp", query={"auto_login": jwt}))
     assertRedirects(
         response,
-        reverse("signup:choose_user_kind", query={"next_url": f"/{expected_params}"}),
+        reverse("signup:choose_user_kind", query={"next_url": "/"}),
         fetch_redirect_response=False,
     )
     assert caplog.messages == [f"Nexus auto login: no user found for jwt={jwt}"]
 
 
-@pytest.mark.parametrize("params,expected_params", params_tuples)
-def test_middleware_for_unlogged_user(client, params, expected_params, caplog):
+def test_middleware_for_unlogged_user(client, caplog):
     user = EmployerFactory(membership=True)
-    params["auto_login"] = generate_token(user)
+    params = {"auto_login": generate_token(user)}
 
     response = client.get(reverse("home:hp", query=params))
     assertRedirects(
         response,
         reverse(
             "pro_connect:authorize",
-            query={"user_kind": "employer", "next_url": f"/{expected_params}", "user_email": user.email},
+            query={"user_kind": "employer", "next_url": "/", "user_email": user.email},
         ),
         fetch_redirect_response=False,
     )
@@ -106,7 +72,7 @@ def test_middleware_for_unlogged_user(client, params, expected_params, caplog):
         response,
         reverse(
             "pro_connect:authorize",
-            query={"user_kind": "employer", "next_url": f"/{expected_params}", "user_email": user.email},
+            query={"user_kind": "employer", "next_url": "/", "user_email": user.email},
         ),
         fetch_redirect_response=False,
     )
