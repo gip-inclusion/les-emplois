76fe1c12b98e9287e3cc8a4db8167edfce334c52
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2026-01-02 15:09:16 +0100

    nexus: Move sync data utils
    Make the code available to calls without going through the API
    his will allow us to use the same code to sync emplois data

--- itou/api/nexus/views.py
+++ itou/api/nexus/views.py
@@ -12,9 +12,8 @@ from itou.api.nexus.serializers import (
     SyncCompletedSerializer,
     UserSerializer,
 )
-from itou.nexus.enums import USER_KIND_MAPPING
-from itou.nexus.models import NexusMembership, NexusRessourceSyncStatus, NexusStructure, NexusUser
-from itou.nexus.utils import service_id
+from itou.nexus import utils as nexus_utils
+from itou.nexus.models import NexusMembership, NexusStructure, NexusUser
 
 
 logger = logging.getLogger(__name__)
@@ -43,24 +42,12 @@ class UsersView(NexusApiMixin, generics.GenericAPIView):
         memberships = []
 
         for user_data in validated_data:
-            user_id = service_id(self.source, user_data["source_id"])
             memberships_data = user_data.pop("memberships")
-            for membership_data in memberships_data:
-                structure_id = service_id(self.source, membership_data["structure_id"])
-                memberships.append(
-                    NexusMembership(
-                        source=self.source, user_id=user_id, structure_id=structure_id, role=membership_data["role"]
-                    )
-                )
+            users.append(nexus_utils.build_user(user_data, self.source))
 
-            users.append(
-                NexusUser(
-                    source=self.source,
-                    id=user_id,
-                    kind=USER_KIND_MAPPING[self.source][user_data["source_kind"]],
-                    **user_data,
-                )
-            )
+            for membership_data in memberships_data:
+                membership_data["user_id"] = user_data["source_id"]
+                memberships.append(nexus_utils.build_membership(membership_data, self.source))
 
         # Filter out memberships on unknown structures
         # NB: no need to filter with source since ids are always prefixed with source
@@ -76,29 +63,8 @@ class UsersView(NexusApiMixin, generics.GenericAPIView):
 
         # Write in database
         updated_at = timezone.now()
-        users = NexusUser.objects.bulk_create(
-            users,
-            update_conflicts=True,
-            update_fields=[
-                "kind",
-                "source_kind",
-                "source_id",
-                "first_name",
-                "last_name",
-                "email",
-                "phone",
-                "last_login",
-                "auth",
-                "updated_at",
-            ],
-            unique_fields=["id"],
-        )
-        NexusMembership.objects.bulk_create(
-            filtered_memberships,
-            update_conflicts=True,
-            update_fields=["role", "updated_at"],
-            unique_fields=["user", "structure"],
-        )
+        nexus_utils.sync_users(users)
+        nexus_utils.sync_memberships(filtered_memberships)
         # Remove old memberships (they don't exist anymore)
         NexusMembership.objects.filter(user__in=users, updated_at__lt=updated_at).delete()
 
@@ -124,43 +90,8 @@ class StructuresView(NexusApiMixin, generics.GenericAPIView):
 
     def perform_create(self, serializer):
         validated_data = serializer.validated_data
-
-        structures = []
-
-        for structure_data in validated_data:
-            structures.append(
-                NexusStructure(
-                    source=self.source,
-                    id=service_id(self.source, structure_data["source_id"]),
-                    kind=structure_data["source_kind"],  # TODO: Add mapping
-                    **structure_data,
-                )
-            )
-        NexusStructure.objects.bulk_create(
-            structures,
-            update_conflicts=True,
-            update_fields=[
-                "kind",
-                "source_kind",
-                "source_id",
-                "siret",
-                "name",
-                "phone",
-                "email",
-                "address_line_1",
-                "address_line_2",
-                "post_code",
-                "city",
-                "department",
-                "accessibility",
-                "description",
-                "opening_hours",
-                "source_link",
-                "website",
-                "updated_at",
-            ],
-            unique_fields=["id"],
-        )
+        structures = [nexus_utils.build_structure(structure_data, self.source) for structure_data in validated_data]
+        nexus_utils.sync_structures(structures)
 
     def delete(self, request, *args, **kwargs):
         serializer = DeleteObjectSerializer(data=request.data)
@@ -177,16 +108,10 @@ class StructuresView(NexusApiMixin, generics.GenericAPIView):
 @extend_schema(exclude=True)
 class SyncStartView(NexusApiMixin, generics.GenericAPIView):
     def post(self, request, *args, **kwargs):
-        return Response({"started_at": self.init_sync().isoformat()}, status=status.HTTP_200_OK)
-
-    def init_sync(self):
-        now = timezone.now()
-        NexusRessourceSyncStatus.objects.update_or_create(
-            service=self.source,
-            defaults={"new_start_at": now},
-            create_defaults={"service": self.source, "new_start_at": now},
+        return Response(
+            {"started_at": nexus_utils.init_full_sync(self.source).isoformat()},
+            status=status.HTTP_200_OK,
         )
-        return now
 
 
 @extend_schema(exclude=True)
@@ -196,9 +121,7 @@ class SyncCompletedView(NexusApiMixin, generics.GenericAPIView):
         serializer.is_valid(raise_exception=True)
 
         started_at = serializer.validated_data["started_at"]
-        if NexusRessourceSyncStatus.objects.filter(service=self.source, new_start_at=started_at).update(
-            new_start_at=None, valid_since=started_at
-        ):
+        if nexus_utils.complete_full_sync(self.source, started_at):
             return Response({}, status=status.HTTP_200_OK)
 
         logger.warning("Got invalid start_at for source=%s", self.source, exc_info=True)
