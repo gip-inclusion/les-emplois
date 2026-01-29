54234e34a334b229619702788657d9798f25d03b
Xavier Fernandez <xavier.fernandez@inclusion.gouv.fr>
2026-01-29 22:12:56 +0100

    geo: display geometry for QPV admin

--- itou/utils/admin.py
+++ itou/utils/admin.py
@@ -61,7 +61,7 @@ class UUIDSupportRemarkInline(AbstractSupportRemarkInline):
 class ItouGISMixin:
     def formfield_for_dbfield(self, db_field, request, **kwargs):
         field = super().formfield_for_dbfield(db_field, request, **kwargs)
-        if isinstance(field, gis_fields.PointField):
+        if isinstance(field, gis_fields.GeometryField):
             field.widget = OSMWidget(attrs={"map_width": 800, "map_height": 500})
         return field
 
