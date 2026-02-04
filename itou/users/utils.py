9f78d244b9f00fa3b6379ce26a209abadc69b4f7
Ewen Corre <ewen.corre@inclusion.gouv.fr>
2026-02-04 11:07:12 +0100

    admin: handle job seeker assignments when transferring users

--- itou/users/utils.py
+++ itou/users/utils.py
@@ -1,5 +1,7 @@
 import re
 
+from itou.users.models import JobSeekerAssignment
+
 
 # https://fr.wikipedia.org/wiki/Num%C3%A9ro_de_s%C3%A9curit%C3%A9_sociale_en_France#Signification_des_chiffres_du_NIR
 NIR_RE = re.compile(
@@ -16,3 +18,14 @@ NIR_RE = re.compile(
     $""",
     re.IGNORECASE | re.VERBOSE,
 )
+
+
+def merge_job_seeker_assignments(*, assignment_to_delete, assignment_to_keep):
+    last_assignment = max([assignment_to_delete, assignment_to_keep], key=lambda a: a.updated_at)
+    JobSeekerAssignment.objects.filter(pk=assignment_to_keep.pk).update(
+        created_at=min(assignment_to_delete.created_at, assignment_to_keep.created_at),
+        updated_at=last_assignment.updated_at,
+        last_action_kind=last_assignment.last_action_kind,
+        job_seeker=assignment_to_keep.job_seeker,
+    )
+    assignment_to_delete.delete()
