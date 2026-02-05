219d0c1492a7e03eedde8858b9ad1c0198fa45f8
Louis LALBA <louis.lalba@inclusion.gouv.fr>
2026-02-05 13:09:35 +0100

    feat: add NIR modification request context

--- itou/users/models.py
+++ itou/users/models.py
@@ -1509,6 +1509,9 @@ class JobSeekerProfile(models.Model):
 
 
 class NirModificationRequest(models.Model):
+    MIN_RATIONALE_LENGTH = 10
+    MAX_RATIONALE_LENGTH = 2_000
+
     jobseeker_profile = models.ForeignKey(
         JobSeekerProfile,
         verbose_name="profil demandeur d'emploi",
@@ -1526,6 +1529,12 @@ class NirModificationRequest(models.Model):
         verbose_name="auteur de la demande",
         related_name="+",
         on_delete=models.RESTRICT,
+    )
+    rationale = models.TextField(
+        verbose_name="contexte de la demande",
+        default="",
+        blank=True,
+        validators=[MaxLengthValidator(MAX_RATIONALE_LENGTH)],
     )
     created_at = models.DateTimeField("créée le", auto_now_add=True)
     processed_at = models.DateTimeField(verbose_name="traitée le", null=True)
