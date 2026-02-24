5ab8a82b568f25baf250c3faf28b2ebdc98628cf
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2026-02-24 15:25:28 +0100

    tests: Move test in a class
    We will soon add more tests on another middleware

--- tests/nexus/test_middleware.py
+++ tests/nexus/test_middleware.py
@@ -6,74 +6,72 @@ from itou.users.enums import IdentityProvider
 from tests.users.factories import EmployerFactory
 
 
-def test_middleware_for_authenticated_user(client, caplog):
-    user = EmployerFactory(membership=True)
-    client.force_login(user)
-    params = {"auto_login": generate_auto_login_token(user)}
-    response = client.get(reverse("home:hp", query=params))
-    assertRedirects(response, "/", fetch_redirect_response=False)
-    assert caplog.messages == ["Nexus auto login: user is already logged in"]
+class TestAutoLoginMiddleware:
+    def test_middleware_for_authenticated_user(self, client, caplog):
+        user = EmployerFactory(membership=True)
+        client.force_login(user)
+        params = {"auto_login": generate_auto_login_token(user)}
+        response = client.get(reverse("home:hp", query=params))
+        assertRedirects(response, "/", fetch_redirect_response=False)
+        assert caplog.messages == ["Nexus auto login: user is already logged in"]
 
+    def test_middleware_for_wrong_authenticated_user(self, client, caplog):
+        user = EmployerFactory(membership=True)
+        params = {"auto_login": generate_auto_login_token(user)}
+        # Another user is logged in
+        client.force_login(EmployerFactory(membership=True))
 
-def test_middleware_for_wrong_authenticated_user(client, caplog):
-    user = EmployerFactory(membership=True)
-    params = {"auto_login": generate_auto_login_token(user)}
-    # Another user is logged in
-    client.force_login(EmployerFactory(membership=True))
+        response = client.get(reverse("home:hp", query=params))
+        assertRedirects(
+            response,
+            reverse(
+                "pro_connect:authorize",
+                query={"user_kind": "employer", "next_url": "/", "user_email": user.email},
+            ),
+            fetch_redirect_response=False,
+        )
+        assert caplog.messages == [
+            "Nexus auto login: wrong user is logged in -> logging them out",
+            f"Nexus auto login: {user} was found and forwarded to ProConnect",
+        ]
 
-    response = client.get(reverse("home:hp", query=params))
-    assertRedirects(
-        response,
-        reverse(
-            "pro_connect:authorize",
-            query={"user_kind": "employer", "next_url": "/", "user_email": user.email},
-        ),
-        fetch_redirect_response=False,
-    )
-    assert caplog.messages == [
-        "Nexus auto login: wrong user is logged in -> logging them out",
-        f"Nexus auto login: {user} was found and forwarded to ProConnect",
-    ]
+    def test_middleware_with_no_existing_user(self, client, caplog):
+        jwt = generate_auto_login_token(EmployerFactory.build())
+        response = client.get(reverse("home:hp", query={"auto_login": jwt}))
+        assertRedirects(
+            response,
+            reverse("signup:choose_user_kind", query={"next_url": "/"}),
+            fetch_redirect_response=False,
+        )
+        assert caplog.messages == [f"Nexus auto login: no user found for jwt={jwt}"]
 
+    def test_middleware_for_unlogged_user(self, client, caplog):
+        user = EmployerFactory(membership=True)
+        params = {"auto_login": generate_auto_login_token(user)}
 
-def test_middleware_with_no_existing_user(client, caplog):
-    jwt = generate_auto_login_token(EmployerFactory.build())
-    response = client.get(reverse("home:hp", query={"auto_login": jwt}))
-    assertRedirects(
-        response,
-        reverse("signup:choose_user_kind", query={"next_url": "/"}),
-        fetch_redirect_response=False,
-    )
-    assert caplog.messages == [f"Nexus auto login: no user found for jwt={jwt}"]
+        response = client.get(reverse("home:hp", query=params))
+        assertRedirects(
+            response,
+            reverse(
+                "pro_connect:authorize",
+                query={"user_kind": "employer", "next_url": "/", "user_email": user.email},
+            ),
+            fetch_redirect_response=False,
+        )
+        assert caplog.messages == [f"Nexus auto login: {user} was found and forwarded to ProConnect"]
 
+        # It also works if it's not a ProConnect user
+        user.identity_provider = IdentityProvider.INCLUSION_CONNECT
+        user.save()
+        caplog.clear()
 
-def test_middleware_for_unlogged_user(client, caplog):
-    user = EmployerFactory(membership=True)
-    params = {"auto_login": generate_auto_login_token(user)}
-
-    response = client.get(reverse("home:hp", query=params))
-    assertRedirects(
-        response,
-        reverse(
-            "pro_connect:authorize",
-            query={"user_kind": "employer", "next_url": "/", "user_email": user.email},
-        ),
-        fetch_redirect_response=False,
-    )
-    assert caplog.messages == [f"Nexus auto login: {user} was found and forwarded to ProConnect"]
-
-    # It also works if it's not a ProConnect user
-    user.identity_provider = IdentityProvider.INCLUSION_CONNECT
-    user.save()
-    caplog.clear()
-
-    response = client.get(reverse("home:hp", query=params))
-    assertRedirects(
-        response,
-        reverse(
-            "pro_connect:authorize",
-            query={"user_kind": "employer", "next_url": "/", "user_email": user.email},
-        ),
-        fetch_redirect_response=False,
-    )
-    assert caplog.messages == [f"Nexus auto login: {user} was found and forwarded to ProConnect"]
+        response = client.get(reverse("home:hp", query=params))
+        assertRedirects(
+            response,
+            reverse(
+                "pro_connect:authorize",
+                query={"user_kind": "employer", "next_url": "/", "user_email": user.email},
+            ),
+            fetch_redirect_response=False,
+        )
+        assert caplog.messages == [f"Nexus auto login: {user} was found and forwarded to ProConnect"]
