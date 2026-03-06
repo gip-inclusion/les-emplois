664f3d51ed909de11e3c89d6ce0c548910024c6a
Ewen Corre <ewen.corre@inclusion.gouv.fr>
2026-03-06 08:54:03 +0100

    dashboard: accept organization_key (with org kind) instead of pk only
    In structure switcher view.
    This is an intermediate commit for compatibility.

--- itou/www/dashboard/views.py
+++ itou/www/dashboard/views.py
@@ -20,6 +20,7 @@ from rest_framework.authtoken.models import Token
 from itou.api.token_auth.views import TOKEN_ID_STR
 from itou.approvals.enums import ProlongationRequestStatus
 from itou.approvals.models import ProlongationRequest
+from itou.common_apps.organizations.models import OrganizationKind
 from itou.eligibility.models.geiq import GEIQEligibilityDiagnosis
 from itou.eligibility.models.iae import EligibilityDiagnosis
 from itou.employee_record.enums import Status
@@ -368,9 +369,14 @@ def switch_organization(request):
     next_url = get_safe_url(request, "next_url")
 
     try:
-        pk = int(request.POST["organization_id"])
-    except (KeyError, ValueError):
-        return HttpResponseBadRequest(b"organization_id key is missing")
+        # FIXME: remove condition and support for organization_id
+        if key := request.POST.get("organization_key"):
+            assert key.split("-")[0] in [o.name for o in OrganizationKind]
+            pk = int(key.split("-")[1])
+        else:
+            pk = int(request.POST["organization_id"])
+    except (KeyError, ValueError, IndexError, AssertionError):
+        return HttpResponseBadRequest(b"organization_key is missing or badly formed")
 
     if not request.user.is_professional or pk not in {organization.pk for organization in request.organizations}:
         raise Http404()
