9a7606746cf40ed09a1f5b5d75328b1822c16f23
Romain Sébille <romain.sebille@beta.gouv.fr>
2026-03-12 15:22:19 +0100

    FIXME: utils.templatetags.dora: Rewrite DORA's services URL scheme and host

--- itou/utils/templatetags/dora.py
+++ itou/utils/templatetags/dora.py
@@ -1,8 +1,9 @@
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlsplit, urlunsplit
 
 from django import template
 from django.conf import settings
 from django.urls import reverse
+from django.utils.http import url_has_allowed_host_and_scheme
 
 from itou.utils.urls import add_url_params
 
@@ -14,6 +15,12 @@ register = template.Library()
 def dora_service_url(service, *, orientation_jwt, source):
     if service["source"] == "dora" and service["lien_source"]:
         service_url = service["lien_source"]
+        # FIXME: Choose if we want to keep it or not, but for now this will be used to
+        #  not have prod URLs in our review app when testing `op` parameter and auto_login.
+        if not url_has_allowed_host_and_scheme(service_url, settings.DORA_BASE_URL):
+            service_url_parts = urlsplit(service_url)
+            dora_url_parts = urlsplit(settings.DORA_BASE_URL)
+            service_url = urlunsplit(dora_url_parts[:2] + service_url_parts[2:])
     else:
         service_url = urljoin(settings.DORA_BASE_URL, f"/services/di--{service['id']}")
     params = {"mtm_campaign": "lesemplois", "mtm_kwd": "rechservice-" + source}
