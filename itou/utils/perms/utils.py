32e8500f0423b112648bf5f651ea95710fae9551
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2026-03-23 17:03:14 +0100

    utils: Stop using is_caseworker in can_view|edit_personal_information
    It will only make an additional query for institutionals (calling
    is_created_by)

--- itou/utils/perms/utils.py
+++ itou/utils/perms/utils.py
@@ -14,7 +14,7 @@ def _can_view_personal_information(viewer, user, *, is_allowed):
 
     return (
         user.is_job_seeker
-        and viewer.is_caseworker
+        and viewer.is_professional
         and (is_allowed or (user.is_handled_by_proxy and user.is_created_by(viewer)))
     )
 
@@ -33,7 +33,7 @@ def _can_edit_personal_information(editor, user, *, is_allowed):
 
     return (
         user.is_job_seeker
-        and editor.is_caseworker
+        and editor.is_professional
         and user.is_handled_by_proxy
         and (is_allowed or user.is_created_by(editor))
     )
