73346102a84b28b78b8d82ba0cdd0e16552cfe43
Louis <louis@gavalda.fr>
2026-04-08 19:05:14 +0200

    fix: smaller notification box when hijacking an account using djhj

--- itou/static/js/hijack_hide.js
+++ itou/static/js/hijack_hide.js
@@ -1,6 +1,16 @@
 "use strict";
-$(document).ready(() => {
-  $(document).on("click", "#hide-btn", function(){
-    document.getElementById('djhj').style.display = 'none'
+
+function ready(fn) {
+  if (document.readyState !== 'loading') {
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+ready(() => {
+  const hideButton = document.getElementById("hide-btn")
+  hideButton.addEventListener("click", function (event) {
+    document.getElementById("djhj").style.display = "none"
   })
-})
+});
