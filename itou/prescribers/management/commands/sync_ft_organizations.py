9b74c2e10576ef14a9405dc2df02485210c9d979
Romain Sébille <romain.sebille@beta.gouv.fr>
2026-04-22 17:43:08 +0200

    prescribers.sync_ft_organizations: Switch to `diff.CollectionDiffer()`

--- itou/prescribers/management/commands/sync_ft_organizations.py
+++ itou/prescribers/management/commands/sync_ft_organizations.py
@@ -1,5 +1,6 @@
 from django.db import IntegrityError, transaction
 from django.db.models import Q
+from django.utils import timezone
 from itoutils.django.commands import dry_runnable
 
 from itou.cities.models import City
@@ -8,59 +9,13 @@ from itou.prescribers.enums import PrescriberAuthorizationStatus, PrescriberOrga
 from itou.prescribers.models import PrescriberOrganization
 from itou.utils.apis.pole_emploi import pole_emploi_partenaire_api_client
 from itou.utils.command import BaseCommand
-from itou.utils.sync import DiffItemKind, yield_sync_diff
+from itou.utils.diff import CollectionDiffer, DiffItemKind, if_not_set_converter
 
 
-def name_from_api_data(data):
+def name_from_api_data(name):
     # Sometimes the `libelle` field can start with a "*",
     # it's most likely linked to the alias that agents can use instead of the official APE's email.
-    return f"France Travail - {data['libelle'].lstrip('*')}"
-
-
-def phone_from_api_data(data):
-    return data.get("contact", {}).get("telephonePublic", "")
-
-
-def email_from_api_data(data):
-    return data.get("contact", {}).get("email", "")
-
-
-def address_from_api_data(data):
-    return data["adressePrincipale"].get("ligne4")
-
-
-def post_code_from_api_data(data):
-    return data["adressePrincipale"].get("bureauDistributeur")
-
-
-def coordinates_from_api_data(data):
-    return lat_lon_to_coords(data["adressePrincipale"].get("gpsLat"), data["adressePrincipale"].get("gpsLon"))
-
-
-def insee_city_from_api_data(data):
-    try:
-        return City.objects.get(code_insee=data["adressePrincipale"].get("communeImplantation"))
-    except City.DoesNotExist:
-        return None
-
-
-def fill_organization_from_api_data(obj, siret, data):
-    # Basic information
-    obj.siret = siret
-    obj.name = name_from_api_data(data)
-    # Contact information
-    obj.phone = phone_from_api_data(data)
-    obj.email = email_from_api_data(data)
-    # Address
-    obj.address_line_1 = address_from_api_data(data)
-    obj.post_code = post_code_from_api_data(data)
-    # Geolocation
-    obj.coords = coordinates_from_api_data(data)
-    obj.geocoding_score = BAN_API_RELIANCE_SCORE if obj.coords else None
-    # INSEE city
-    obj.insee_city = insee_city_from_api_data(data)
-    obj.city = obj.insee_city.name if obj.insee_city else ""
-    obj.department = obj.insee_city.department if obj.insee_city else ""
+    return f"France Travail - {name.lstrip('*')}"
 
 
 class Command(BaseCommand):
@@ -88,50 +43,83 @@ class Command(BaseCommand):
             except IntegrityError:
                 self.stdout.write(f"ERR: safir={organization.code_safir_pole_emploi} already used")
 
+    @staticmethod
+    def set_extra_informations(obj, data):
+        updated_fields = set()
+        if "siret" in data:
+            # Some agencies (with different location) share the same siret, this trigger the (siret, kind) unique
+            # constraint, as we don't really care about siret for France Travail organization we circumvent that by
+            # clearing the siret for the current one if it doesn't already exist or if is not the first one we see.
+            siret = data["siret"].after
+            siret_already_used = (
+                PrescriberOrganization.objects.filter(kind=PrescriberOrganizationKind.FT, siret=siret)
+                .exclude(code_safir_pole_emploi=obj.code_safir_pole_emploi)
+                .exists()
+            )
+            obj.siret = None if siret_already_used else siret
+            updated_fields.add("siret")
+        if "coords" in data:
+            obj.geocoding_score = BAN_API_RELIANCE_SCORE if data["coords"].after else None
+            updated_fields.add("geocoding_score")
+        if "insee_city" in data:
+            city = data["insee_city"].after
+            obj.city = city.name if city else ""
+            obj.department = city.department if city else ""
+            updated_fields.update(["city", "department"])
+
+        return updated_fields
+
     def update_information(self, data):
         qs = PrescriberOrganization.objects.filter(kind=PrescriberOrganizationKind.FT).exclude(
             Q(code_safir_pole_emploi="") | Q(code_safir_pole_emploi=None)
         )
-        mapping = [
-            (lambda d: d.get("siret"), "siret"),  # Don't break if the "required" siret is missing
-            (name_from_api_data, "name"),
-            (phone_from_api_data, "phone"),
-            (email_from_api_data, "email"),
-            (address_from_api_data, "address_line_1"),
-            (post_code_from_api_data, "post_code"),
-            (coordinates_from_api_data, "coords"),
-            (insee_city_from_api_data, "insee_city"),
-        ]
-        for item in yield_sync_diff(data, "codeSafir", qs, "code_safir_pole_emploi", mapping):
-            if item.kind == DiffItemKind.DELETION:  # Ignore DELETION completely
+        differ = CollectionDiffer(
+            qs,
+            data,
+            (["code_safir_pole_emploi"], ["codeSafir"]),
+            watched_data={
+                "siret": "siret",
+                "name": "libelle",
+                "phone": "contact.telephonePublic",
+                "email": "contact.email",
+                "address_line_1": "adressePrincipale.ligne4",
+                "post_code": "adressePrincipale.bureauDistributeur",
+                "coords": ("adressePrincipale.gpsLat", "adressePrincipale.gpsLon"),
+                "insee_city": "adressePrincipale.communeImplantation",
+            },
+            comparative_data_converters={
+                "siret": if_not_set_converter(None),
+                "libelle": name_from_api_data,
+                "contact.telephonePublic": if_not_set_converter(""),
+                "contact.email": if_not_set_converter(""),
+                ("adressePrincipale.gpsLat", "adressePrincipale.gpsLon"): lambda v: lat_lon_to_coords(*v),
+                "adressePrincipale.communeImplantation": lambda v: City.objects.filter(code_insee=v).first(),
+            },
+        )
+        for diff_item in differ:
+            if diff_item.kind is DiffItemKind.REMOVED:  # Ignore DELETION completely
                 continue
-            self.stdout.write(item.label)
-            if item.kind == DiffItemKind.SUMMARY:  # Nothing more to do than display a message for SUMMARY
-                continue
+            self.stdout.write(diff_item.label())
+            safir = diff_item.key[0]
 
-            safir = item.raw["codeSafir"]
-            siret = item.raw.get("siret")
-            # Some agencies (with different location) share the same siret, this trigger the (siret, kind) unique
-            # constraint, as we don't really care about siret for France Travail organization we circumvent that by
-            # clearing the siret for the current one if it doesn't already exist or if is not the first one we see.
-            if (
-                siret
-                and PrescriberOrganization.objects.filter(kind=PrescriberOrganizationKind.FT, siret=siret)
-                .exclude(code_safir_pole_emploi=safir)
-                .exists()
-            ):
-                siret = None
-            if item.kind == DiffItemKind.ADDITION:
+            if diff_item.kind is DiffItemKind.ADDED:
                 obj = PrescriberOrganization(
                     kind=PrescriberOrganizationKind.FT,
                     code_safir_pole_emploi=safir,
                     authorization_status=PrescriberAuthorizationStatus.VALIDATED,
                 )
-                fill_organization_from_api_data(obj, siret, item.raw)
+                for current_item_attr, comparative_item_value in diff_item.data.items():
+                    setattr(obj, current_item_attr, comparative_item_value.after)
+                self.set_extra_informations(obj, diff_item.data)
                 obj.save()
-            elif item.kind == DiffItemKind.EDITION:
-                fill_organization_from_api_data(item.db_obj, siret, item.raw)
-                item.db_obj.save()
+            elif diff_item.kind is DiffItemKind.UPDATED:
+                for current_item_attr, data_diff in diff_item.data.items():
+                    setattr(diff_item.current_item, current_item_attr, data_diff.after)
+                updated_fields = self.set_extra_informations(diff_item.current_item, diff_item.data)
+                diff_item.current_item.updated_at = timezone.now()
+                diff_item.current_item.save(update_fields={*diff_item.data.keys(), *updated_fields, "updated_at"})
+
+        self.stdout.write(differ.summary_label())
 
     @dry_runnable
     def handle(self, *, action, wet_run=False, **options):
