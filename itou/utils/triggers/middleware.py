203b2fb1d520e89e7d3dca0c0b6228fa5f5d7ec0
Xavier Fernandez <xavier.fernandez@inclusion.gouv.fr>
2026-05-04 13:43:36 +0200

    triggers: define connection_wrapper context manager
    connection.execute_wrapper(triggers._set_context_connection_wrapper) is
    quite long to type.

--- itou/utils/triggers/middleware.py
+++ itou/utils/triggers/middleware.py
@@ -1,11 +1,9 @@
-from django.db import connection
-
 from itou.utils import triggers
 
 
 def fields_history(get_response):
     def middleware(request):
-        with connection.execute_wrapper(triggers._set_context_connection_wrapper):
+        with triggers.connection_wrapper():
             return get_response(request)
 
     return middleware
