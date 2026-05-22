85b39075caae42d0d7a243275112d46775d141b4
Damien Baty <damien.baty@inclusion.gouv.fr>
2026-05-22 17:16:25 +0200

    otp: Make device enrollment flow more user-friendly
    There are now more (but clearer) steps to enroll a new device. No
    technical change, except that we don't create an unverified (and
    unnamed) `TOTPDevice` before it's confirmed. Now the `TOTPDevice`
    object is stored in the database only when it's confirmed.

---
+++ itou/www/otp_views/enums.py
@@ -0,0 +1,6 @@
+import enum
+
+
+class DeviceType(enum.StrEnum):
+    SMARTPHONE = "smartphone"
+    DESKTOP = "desktop"
--- tests/www/otp_views/tests.py
+++ tests/www/otp_views/tests.py
@@ -3,6 +3,8 @@ from functools import partial
 import pytest
 from django.contrib import messages
 from django.urls import reverse
+from django.utils import timezone
+from django_otp import DEVICE_ID_SESSION_KEY
 from django_otp.oath import TOTP
 from django_otp.plugins.otp_totp.models import TOTPDevice
 from freezegun import freeze_time
@@ -24,6 +26,14 @@ from tests.users.factories import (
 from tests.utils.testing import parse_response_to_soup, pretty_indented
 
 
+def attach_device_to_user_session(client, device):
+    # Mimic what is done when user enters a TOTP code.
+    device.set_last_used_timestamp(timezone.now())
+    session = client.session
+    session[DEVICE_ID_SESSION_KEY] = device.persistent_id
+    session.save()
+
+
 @pytest.mark.parametrize(
     "factory,expected_status",
     [
@@ -40,103 +50,37 @@ def test_permissions(client, factory, expected_status):
     response = client.get(reverse("otp_views:otp_devices"))
     assert response.status_code == expected_status
 
-    device = TOTPDevice.objects.create(user=user, confirmed=False)
-    response = client.get(reverse("otp_views:otp_confirm_device", args=(device.pk,)))
+    response = client.get(reverse("otp_views:enrollment_step_1_choose_device_type"))
     assert response.status_code == expected_status
 
 
 @freeze_time("2025-03-11 05:18:56")
-def test_devices(client, snapshot):
-    staff_user = ItouStaffFactory()
-    client.force_login(staff_user)
-    url = reverse("otp_views:otp_devices")
-
-    response = client.get(url)
-    assert pretty_indented(parse_response_to_soup(response, ".s-section")) == snapshot(name="no_device")
-
-    response = client.post(url, data={"action": "new"})
-    device = TOTPDevice.objects.get()
-    assertRedirects(response, reverse("otp_views:otp_confirm_device", args=(device.pk,)))
-
-    # As long as the device isn't confirmed it isn't shown, and we don't create a new one.
-    response = client.get(url)
-    assert pretty_indented(parse_response_to_soup(response, ".s-section")) == snapshot(name="no_device")
-
-    response = client.post(url, data={"action": "new"})
-    device = TOTPDevice.objects.get()  # Still only one
-    assertRedirects(response, reverse("otp_views:otp_confirm_device", args=(device.pk,)))
-
-    # When the user already confirmed a device he must first confirm the otp token
-    device.name = "Mon appareil"
-    device.confirmed = True
-    device.save()
-    post_data = {
-        "name": "Mon appareil",
-        "otp_token": TOTP(device.bin_key).token(),
-    }
-    response = client.post(reverse("login:verify_otp"), data=post_data)
-    # The devices page is different
-    response = client.get(url)
-    assert pretty_indented(
-        parse_response_to_soup(
-            response,
-            ".s-section",
-            replace_in_attr=[
-                ("value", f"{device.pk}", "[PK of device]"),
-                ("id", f"delete_{device.pk}_modal", "delete_[PK of device]_modal"),
-                ("data-bs-target", f"#delete_{device.pk}_modal", "#delete_[PK of device]_modal"),
-            ],
-        )
-    ) == snapshot(name="with_device")
-
-    response = client.post(url, data={"action": "new"})
-    device = TOTPDevice.objects.exclude(pk=device.pk).get()
-    assertRedirects(response, reverse("otp_views:otp_confirm_device", args=(device.pk,)))
-
-
-@freeze_time()
-def test_confirm(client):
-    staff_user = ItouStaffFactory()
-    client.force_login(staff_user)
-
+def test_device_list(client, snapshot):
+    user = ItouStaffFactory()
     device = TOTPDevice.objects.create(
-        user=staff_user, confirmed=False, key="8fe0a9983c7dddb4acb0146c5507553371e9f211"
+        user=user,
+        name="Mon appareil",
+        confirmed=True,
     )
-    url = reverse("otp_views:otp_confirm_device", args=(device.pk,))
-    response = client.get(url)
-    assertContains(response, "R7QKTGB4PXO3JLFQCRWFKB2VGNY6T4QR")  # the otp secret matching the hex key
 
-    totp = TOTP(device.bin_key, drift=100)
-    post_data = {
-        "name": "Mon appareil",
-        "otp_token": totp.token(),  # a token from a long time ago
-    }
-    response = client.post(url, data=post_data)
-    assert response.status_code == 200
-    assert response.context["form"].errors == {"otp_token": ["Mauvais code OTP"]}
-    device.refresh_from_db()
-    assert device.confirmed is False
+    client.force_login(user)
+    attach_device_to_user_session(client, device)
+    response = client.get(reverse("otp_views:otp_devices"))
 
-    # by default, django-otp's throttle delay is 1 second after the first failure
-    totp = TOTP(device.bin_key)
-    post_data["otp_token"] = totp.token()
-    response = client.post(url, data=post_data)
-    assert response.status_code == 200
-    assert response.context["form"].errors == {"otp_token": ["Mauvais code OTP"]}
-    device.refresh_from_db()
-    assert device.confirmed is False
-
-    # When resetting the failure count
-    device.throttling_failure_timestamp = None
-    device.throttling_failure_count = 0
-    device.save()
-    response = client.post(url, data=post_data)
-    assertMessages(
-        response, [messages.Message(messages.SUCCESS, "Votre nouvel appareil est confirmé", extra_tags="toast")]
+    assert (
+        pretty_indented(
+            parse_response_to_soup(
+                response,
+                ".s-section",
+                replace_in_attr=[
+                    ("value", f"{device.pk}", "[PK of device]"),
+                    ("id", f"delete_{device.pk}_modal", "delete_[PK of device]_modal"),
+                    ("data-bs-target", f"#delete_{device.pk}_modal", "#delete_[PK of device]_modal"),
+                ],
+            )
+        )
+        == snapshot()
     )
-    assertRedirects(response, reverse("otp_views:otp_devices"))
-    device.refresh_from_db()
-    assert device.confirmed is True
 
 
 def test_delete_devices(client, snapshot, settings):
@@ -145,20 +89,17 @@ def test_delete_devices(client, snapshot, settings):
     url = reverse("otp_views:otp_devices")
 
     with freeze_time("2025-03-11 05:18:56") as frozen_time:
+        device_1 = TOTPDevice.objects.create(
+            user=staff_user,
+            confirmed=True,
+            name="authenticator",
+        )
+        frozen_time.tick(60)
+        device_2 = TOTPDevice.objects.create(user=staff_user, confirmed=True, name="bitwarden")
+        frozen_time.tick(60)
+
         client.force_login(staff_user)
-
-        device_1 = TOTPDevice.objects.create(user=staff_user, confirmed=True, name="bitwarden")
-        frozen_time.tick(60)
-
-        device_2 = TOTPDevice.objects.create(user=staff_user, confirmed=True, name="authenticator")
-        frozen_time.tick(60)
-
-        # Verify user
-        post_data = {
-            "name": "Mon appareil",
-            "otp_token": TOTP(device_1.bin_key).token(),
-        }
-        client.post(reverse("login:verify_otp"), data=post_data)
+        attach_device_to_user_session(client, device_1)
 
         # List devices
         response = client.get(url)
@@ -197,3 +138,93 @@ def test_delete_devices(client, snapshot, settings):
         assertContains(response, device_1.name)
         assertNotContains(response, device_2.name)
         assertMessages(response, [messages.Message(messages.SUCCESS, "L’appareil a été supprimé.")])
+
+
+def test_enrollment_step_0_intro(client):
+    user = ItouStaffFactory()
+
+    client.force_login(user)
+    url = reverse("otp_views:enrollment_step_0_intro")
+    response = client.get(url)
+    assertContains(response, "Nous vous guidons étape par étape")
+
+
+def test_enrollment_step_1_choose_device_type(client):
+    user = ItouStaffFactory()
+
+    client.force_login(user)
+    url = reverse("otp_views:enrollment_step_1_choose_device_type")
+    response = client.get(url)
+    assertContains(response, "<strong>Étape 1</strong>/2 : Choisissez votre méthode")
+
+
+class TestEnrollmentStep2ConfirmDevice:
+    @pytest.mark.parametrize(
+        "device_type,should_show_qr_code",
+        (
+            ("smartphone", True),
+            ("desktop", False),
+        ),
+    )
+    def test_get_known_device_type(self, client, device_type, should_show_qr_code):
+        user = ItouStaffFactory()
+        client.force_login(user)
+        url = reverse("otp_views:enrollment_step_2_confirm_device")
+
+        response = client.get(url, query_params={"device_type": device_type})
+
+        assertContains(response, "<strong>Étape 2</strong>/2 : Associez votre compte")
+        qr_code_text = "scannez ce QR code"
+        if should_show_qr_code:
+            assertContains(response, qr_code_text)
+        else:
+            assertNotContains(response, qr_code_text)
+
+    def test_get_unknown_device_type(self, client):
+        user = ItouStaffFactory()
+        client.force_login(user)
+        url = reverse("otp_views:enrollment_step_2_confirm_device")
+
+        response = client.get(url, query_params={"device_type": "unknown"})
+        assertRedirects(response, reverse("otp_views:enrollment_step_1_choose_device_type"))
+
+    def test_post_valid_totp(self, client):
+        user = ItouStaffFactory()
+        client.force_login(user)
+        url = reverse("otp_views:enrollment_step_2_confirm_device")
+        fake_device = TOTPDevice(key="8fe0a9983c7dddb4acb0146c5507553371e9f211")
+
+        data = {
+            "name": "My Apploogle IPixel 34",
+            "device_type": "smartphone",
+            "key": "R7QKTGB4PXO3JLFQCRWFKB2VGNY6T4QR",
+            "otp_token": TOTP(fake_device.bin_key).token(),
+        }
+        response = client.post(url, data)
+
+        assertRedirects(response, reverse("otp_views:otp_devices"))
+        assertMessages(
+            response, [messages.Message(messages.SUCCESS, "Votre nouvel appareil est confirmé", extra_tags="toast")]
+        )
+        device = user.totpdevice_set.get()
+        assert device.key == fake_device.key
+        assert client.session[DEVICE_ID_SESSION_KEY] == device.persistent_id
+
+    def test_post_invalid_totp(self, client):
+        user = ItouStaffFactory()
+        client.force_login(user)
+        url = reverse("otp_views:enrollment_step_2_confirm_device")
+        fake_device = TOTPDevice(key="8fe0a9983c7dddb4acb0146c5507553371e9f211")
+
+        expired_token = TOTP(fake_device.bin_key, drift=100).token()
+        data = {
+            "name": "My Apploogle IPixel 34",
+            "device_type": "smartphone",
+            "key": "R7QKTGB4PXO3JLFQCRWFKB2VGNY6T4QR",
+            "otp_token": expired_token,
+        }
+        response = client.post(url, data)
+
+        assertContains(response, "Le code unique de validation (OTP) n’est pas correct.")
+        assertContains(response, data["key"])
+        assert user.totpdevice_set.count() == 0
