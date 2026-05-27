90514b9db1883795a3f6e0621750cb3c905f05e4
Xavier Fernandez <xavier.fernandez@inclusion.gouv.fr>
2026-05-27 17:51:18 +0200

    tests: automatically add snapshot mark on snapshot tests
    To allow to easily select snapshot tests with `-m snapshot`

--- tests/conftest.py
+++ tests/conftest.py
@@ -57,6 +57,8 @@ def pytest_collection_modifyitems(config, items):
         markers = {marker.name for marker in item.iter_markers()}
         if "no_django_db" not in markers and "django_db" not in markers:
             item.add_marker(pytest.mark.django_db)
+        if "snapshot" in item.fixturenames:
+            item.add_marker(pytest.mark.snapshot)
 
 
 @pytest.hookimpl(trylast=True)
