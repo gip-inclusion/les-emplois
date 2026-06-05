2e09ed2f99d9b3c5238eb5ecc3169385406b3176
Damien Baty <damien.baty@inclusion.gouv.fr>
2026-06-05 09:46:54 +0200

    otp: Use dashes, not underscores in URLs
    Next step: use a nice emoji instead. Maybe the cute cat with
    heart-eyes? Unless there is a poney emoji. I expect a heated debate
    among our development team...

--- itou/www/otp_views/urls.py
+++ itou/www/otp_views/urls.py
@@ -8,22 +8,22 @@ app_name = "otp_views"
 urlpatterns = [
     path("devices", views.otp_devices, name="otp_devices"),
     path(
-        "enrollment_step_0",
+        "enrollment-step-0",
         views.enrollment_step_0_intro,
         name="enrollment_step_0_intro",
     ),
     path(
-        "enrollment_step_1",
+        "enrollment-step-1",
         views.enrollment_step_1_choose_device_type,
         name="enrollment_step_1_choose_device_type",
     ),
     path(
-        "enrollment_step_2_and_3",
+        "enrollment-step-2-and-3",
         views.enrollment_step_2_and_3_confirm_device,
         name="enrollment_step_2_and_3_confirm_device",
     ),
     path(
-        "login_with_backup_code",
+        "login-with-backup-code",
         views.login_with_backup_code,
         name="login_with_backup_code",
     ),
