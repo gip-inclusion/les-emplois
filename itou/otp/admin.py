71ba4eacb8b209bd9eb4188fec22e2b48739f4b2
Damien Baty <damien.baty@inclusion.gouv.fr>
2026-06-16 12:25:56 +0200

    otp: Display `created_at` in `ItouTOTPDeviceAdmin`

--- itou/otp/admin.py
+++ itou/otp/admin.py
@@ -9,6 +9,7 @@ from itou.utils.admin import ItouModelAdmin, ReadonlyMixin
 class ItouTOTPDeviceAdmin(ReadonlyMixin, ItouModelAdmin):
     list_display = (
         "user",
+        "created_at",
         "last_used_at",
     )
     list_select_related = ("user",)
@@ -16,6 +17,7 @@ class ItouTOTPDeviceAdmin(ReadonlyMixin, ItouModelAdmin):
     def get_fields(self, request, obj=None):
         fields = super().get_fields(request, obj=obj)
         fields.remove("key")
+        fields.insert(0, "created_at")
         return fields
 
 
