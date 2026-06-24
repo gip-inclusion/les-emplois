f1e1f87d070912e3d98270adc2072e448441edb7
Victor Perron <victor@iso3103.net>
2026-06-24 10:50:08 +0200

    insertion: remove dead DORA orientation JWT from service detail
    The service card no longer renders the DORA prefill link, so the
    orientation_jwt context value (and its can_prefill_orientation_on_dora
    guard) was unused.

--- itou/www/insertion_views/views.py
+++ itou/www/insertion_views/views.py
@@ -17,10 +17,9 @@ from itoutils.django.decoupage_administratif.admin_division_parsing import get_d
 
 from itou.insertion import models as insertion_models
 from itou.insertion.opening_hours import format_osm_hours
-from itou.insertion.utils import get_missing_orientation_beneficiary_field_labels, get_orientation_jwt
+from itou.insertion.utils import get_missing_orientation_beneficiary_field_labels
 from itou.users.enums import UserKind
 from itou.users.models import User
-from itou.users.perms import can_prefill_orientation_on_dora
 from itou.utils.apis.dora import DoraAPIClient, DoraAPIException
 from itou.utils.auth import LoginNotRequiredMixin
 from itou.utils.perms.utils import can_edit_personal_information, can_view_personal_information
@@ -131,9 +130,6 @@ class ServiceDetailView(LoginNotRequiredMixin, DetailView):
         context["formatted_opening_hours"] = format_osm_hours(self.object.opening_hours)
         context["back_url"] = get_safe_url(self.request, "back_url", fallback_url=reverse("search:services_home"))
         context["matomo_custom_title"] = "Fiche de la service d'insértion"
-        context["orientation_jwt"] = (
-            get_orientation_jwt(self.request) if can_prefill_orientation_on_dora(self.request) else None
-        )
         context["geographic_perimeter"] = get_division_label(self.object.eligibility_zones) or "France entière"
         context["credential_documents"] = self.object.generate_credential_documents_info()
         context["show_mobilization_section"] = self.object.has_mobilization_modes()
