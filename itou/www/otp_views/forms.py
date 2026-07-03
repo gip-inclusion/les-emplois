5b2d97df74b6d915ab14d52ae53a08624a9973bb
Damien Baty <damien.baty@inclusion.gouv.fr>
2026-07-03 16:52:35 +0200

    otp_views: Change wording in TOTP device confirmation form

--- itou/www/otp_views/forms.py
+++ itou/www/otp_views/forms.py
@@ -1,6 +1,7 @@
 from base64 import b32encode
 
 from django import forms
+from django.utils.html import mark_safe
 
 from itou.otp.utils import get_user_devices
 from itou.www.otp_views.enums import DeviceType
@@ -11,7 +12,7 @@ class ConfirmTOTPDeviceForm(forms.Form):
         label="Choisissez un nom pour votre appareil",
         help_text="Ce nom vous aidera à retrouver cet appareil dans vos paramètres de sécurité.",
     )
-    otp_token = forms.CharField(label="Générez le code unique de validation (OTP) dans l’application et entrez-le ici")
+    otp_token = forms.CharField(label="")  # customized in `__init__()`
     key = forms.CharField(widget=forms.HiddenInput)
     device_type = forms.CharField(widget=forms.HiddenInput)
 
@@ -29,6 +30,15 @@ class ConfirmTOTPDeviceForm(forms.Form):
         self.fields["device_type"].initial = device_type
         self.fields["name"].widget.attrs.update(
             {"placeholder": "Téléphone pro" if device_type == DeviceType.SMARTPHONE else "Ordinateur pro"}
+        )
+        self.fields["otp_token"].label = mark_safe(
+            "Une fois le service ajouté, un code à 6 chiffres s'affiche automatiquement "
+            + (
+                "dans votre application."
+                if device_type == DeviceType.SMARTPHONE
+                else "dans votre gestionnaire de mots de passe."
+            )
+            + "<br>Reportez-le ici :"
         )
 
     def clean(self):
