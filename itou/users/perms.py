4eace4e85307a92c037e6652a89efae4ac7ffb4c
Ewen Corre <ewen.corre@inclusion.gouv.fr>
2026-07-09 09:46:02 +0200

    insertion_views: refactor mobilization event registration permission

--- itou/users/perms.py
+++ itou/users/perms.py
@@ -1,2 +1,6 @@
 def can_orient_towards_insertion_service(request):
     return bool(request.user.is_authenticated and (request.from_employer or request.from_prescriber))
+
+
+def can_register_mobilization_event(request):
+    return bool(not request.user.is_authenticated or request.from_employer or request.from_prescriber)
