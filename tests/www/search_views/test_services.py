a9ea033d18fdf585d1713a57ef5ddbb56de014e9
François Freitag <mail@franek.fr>
2026-07-16 17:07:25 +0200

    Revamp the landing page
    With the work on plateforme.inclusion.gouv.fr, the focus is shifting
    from pure employment towards the broader ecosystem (services,
    unauthorized prescribers of different kinds).
    
    To better greet users, the landing page should be more configurable,
    allow A/B testing, be easier to update by bizdevs… Instead of building a
    CMS inside the app, the landing page is now loaded as an iframe,
    allowing external management (by the product team) while keeping
    isolation from the site sensitive data (in particular, the `sessionid`
    cookie).
    
    Centralize the search types so that users can more easily discover
    services offered by the platform, removing the need for anonymous
    navigation, simplifying the nav bar.
    
    To preserve functionality for links in emails (or bookmarks, etc) that
    pointed to a specific search, the existing URLs now load the frame with
    the matching search type (employer, prescriber, service).
    
    Since iframes are isolated from the host, some features need
    cooperation:
    
    - adjusting the host container height to the iframe size. The iframe
      reports its height on load, plus any time it changes. The host listens
      to the iframe messages and adjusts the container height accordingly.
    
    - tracking user actions in the iframe. The iframe embeds tracking logic
      and emits events, the host listens to those events and forwards them
      to Matomo, when users have consented to tracking.
    
    To avoid full disruption if the external frame isn’t available, a
    rudimentary search fallback was implemented, that only searches for
    employers. Soon, the search forms will be moved from the iframe back
    into the project, so the search feature will remain even when the iframe
    is not available, making it much less critical.

--- tests/www/search_views/test_services.py
+++ tests/www/search_views/test_services.py
@@ -2,7 +2,7 @@ import pytest
 from data_inclusion.schema import v1 as data_inclusion_v1
 from django.test import override_settings
 from django.urls import reverse, reverse_lazy
-from pytest_django.asserts import assertContains, assertNotContains, assertRedirects
+from pytest_django.asserts import assertContains, assertNotContains
 
 from itou.www.search_views.forms import ServiceSearchForm
 from tests.cities.factories import create_city_vannes
@@ -25,15 +25,18 @@ class TestSearchServices:
     URL = reverse_lazy("search:services_results")
     FIRST_RESULT_LINK = "#services-search-results > .c-box--results:first-child a"
 
-    def test_home_anonymous(self, client):
+    def test_home_anonymous(self, client, settings):
+        settings.PLATEFORME_ACCUEIL_BASE_URL = "https://plateforme.accueil.fr"
         response = client.get(reverse("search:services_home"))
-        assertContains(response, "Rechercher un service d'insertion")
+        assertContains(
+            response,
+            'data-plateforme-accueil="https://plateforme.accueil.fr?host=localhost%3A8000&amp;type=insertion"',
+        )
 
     def test_home_connected(self, client):
         client.force_login(EmployerFactory())
-        with pytest.warns(RuntimeWarning, match="Access to 'search_services_home' while authenticated"):
-            response = client.get(reverse("search:services_home"))
-        assertRedirects(response, reverse("search:services_results"))
+        with pytest.warns(RuntimeWarning, match="Access to 'search_home' while authenticated"):
+            client.get(reverse("search:services_home"))
 
     def test_invalid_query_parameters(self, client):
         response = client.get(self.URL, {"city": "foo-44", "category": "foobar"})
