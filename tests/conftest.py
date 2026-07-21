67c76474bd75d9f194aa10b7ed7bd5f2195ce6d2
Damien Baty <damien.baty@inclusion.gouv.fr>
2026-07-21 11:35:26 +0200

    tests: Add `slow` marker and `--runslow` pytest argument
    Some tests are too slow to be run every time. They can now be tagged
    with the "slow" marker so that they are skipped by default, and
    executed only with "pytest --runslow".

--- tests/conftest.py
+++ tests/conftest.py
@@ -48,15 +48,28 @@ from tests.utils.htmx.testing import HtmxClient  # noqa: E402
 from tests.utils.testing import ItouClient  # noqa: E402
 
 
+def pytest_addoption(parser):
+    parser.addoption(
+        "--runslow",
+        action="store_true",
+        default=False,
+        help="run slow tests",
+    )
+
+
 @pytest.hookimpl(tryfirst=True)
 def pytest_collection_modifyitems(config, items):
     """Automatically add pytest db marker if needed."""
+    run_slow = config.getoption("--runslow")
+    skip_slow_marker = pytest.mark.skip(reason="Use --runslow to execute this test")
     for item in items:
         markers = {marker.name for marker in item.iter_markers()}
         if "no_django_db" not in markers and "django_db" not in markers:
             item.add_marker(pytest.mark.django_db)
         if "snapshot" in item.fixturenames:
             item.add_marker(pytest.mark.snapshot)
+        if "slow" in markers and not run_slow:
+            item.add_marker(skip_slow_marker)
 
 
 @pytest.hookimpl(trylast=True)
