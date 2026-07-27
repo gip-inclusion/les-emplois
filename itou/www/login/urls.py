c9f0789f4fb638c05ea70e1db8f30bc38f8e5a13
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2026-07-27 13:28:15 +0200

    login: Improve old url comment

--- itou/www/login/urls.py
+++ itou/www/login/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path("existing", views.ExistingUserLoginView.as_view(), name="existing_user"),
     path("demo", views.demo_login_view, name="demo"),
     # Retro compatibility url
-    path("job_seeker", views.PreLoginView.as_view(), name="job_seeker"),  # FIXME use redirect instead ?
+    # FIXME(alaurent) This url was used in new_for_job_seeker_body until 21/05. Remove in december 2026
+    path("job_seeker", views.PreLoginView.as_view(), name="job_seeker"),
 ]
