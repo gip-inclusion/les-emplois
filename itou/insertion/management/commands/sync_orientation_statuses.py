4ab9493d9e438e3039c513c2403c7a9023f9e9e7
Gérald Gounot <gerald@gounot.eu>
2026-07-27 17:57:50 +0200

    refactor(insertion): use NamedTuple in orientation status sync command

--- itou/insertion/management/commands/sync_orientation_statuses.py
+++ itou/insertion/management/commands/sync_orientation_statuses.py
@@ -1,4 +1,5 @@
 import datetime
+from typing import NamedTuple
 
 from django.conf import settings
 from django.db.models import Max
@@ -7,6 +8,29 @@ from itoutils.django.commands import dry_runnable
 from itou.insertion.models import Orientation
 from itou.utils.apis.dora import DoraAPIClient, DoraApiItemsIterator
 from itou.utils.command import BaseCommand
+
+
+class DoraStatus(NamedTuple):
+    status: str
+    processing_date: datetime.datetime | None  # date à laquelle l'orientation a été traitée côté Dora
+    dora_status_updated_at: datetime.datetime  # date de dernière modification côté DORA, borne de la synchro
+
+    @classmethod
+    def from_api_item(cls, item):
+        processing_date = item["processing_date"]
+        return cls(
+            status=item["status"],
+            processing_date=datetime.datetime.fromisoformat(processing_date) if processing_date else None,
+            dora_status_updated_at=datetime.datetime.fromisoformat(item["updated_at"]),
+        )
+
+    @classmethod
+    def from_orientation(cls, orientation):
+        return cls(
+            status=orientation.status,
+            processing_date=orientation.processing_date,
+            dora_status_updated_at=orientation.dora_status_updated_at,
+        )
 
 
 class Command(BaseCommand):
@@ -26,35 +50,25 @@ class Command(BaseCommand):
         params = {"updated_after": watermark.isoformat()} if watermark else {}
 
         with DoraAPIClient(settings.DORA_API_BASE_URL, settings.DORA_API_TOKEN) as client:
-            statuses = {}
-            for item in DoraApiItemsIterator(client.orientation_statuses, params=params):
-                processing_date = item["processing_date"]
-                statuses[item["emplois_sync_uid"]] = (
-                    item["status"],
-                    datetime.datetime.fromisoformat(processing_date) if processing_date else None,
-                    datetime.datetime.fromisoformat(item["updated_at"]),
-                )
+            statuses = {
+                item["emplois_sync_uid"]: DoraStatus.from_api_item(item)
+                for item in DoraApiItemsIterator(client.orientation_statuses, params=params)
+            }
 
         self.logger.info("Retrieved count=%d orientation statuses from DORA", len(statuses))
 
-        orientations = Orientation.objects.filter(id__in=statuses).only(
-            "id", "status", "processing_date", "dora_status_updated_at"
-        )
+        orientations = Orientation.objects.filter(id__in=statuses).only("id", *DoraStatus._fields)
         to_update = []
         for orientation in orientations:
-            status, processing_date, updated_at = statuses.pop(str(orientation.id))
-            if (
-                orientation.status == status
-                and orientation.processing_date == processing_date
-                and orientation.dora_status_updated_at == updated_at
-            ):
+            dora_status = statuses.pop(str(orientation.id))
+            if dora_status == DoraStatus.from_orientation(orientation):
                 continue
-            orientation.status = status
-            orientation.processing_date = processing_date
-            orientation.dora_status_updated_at = updated_at
+            orientation.status = dora_status.status
+            orientation.processing_date = dora_status.processing_date
+            orientation.dora_status_updated_at = dora_status.dora_status_updated_at
             to_update.append(orientation)
 
-        Orientation.objects.bulk_update(to_update, ["status", "processing_date", "dora_status_updated_at"])
+        Orientation.objects.bulk_update(to_update, DoraStatus._fields)
         self.logger.info("Updated count=%d orientation statuses", len(to_update))
 
         if statuses:
