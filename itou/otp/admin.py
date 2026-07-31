06c8db803f68f3e6e27825cc6ce96f769d257e10
Damien Baty <damien.baty@inclusion.gouv.fr>
2026-07-31 16:31:42 +0200

    admin: Add "enabled" column in ItoutTOTPDevice admin list

--- itou/otp/admin.py
+++ itou/otp/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.admin.utils import display_for_value
 from django_otp.plugins.otp_totp.models import TOTPDevice
 
 from itou.otp.models import ItouTOTPDevice
@@ -7,11 +8,7 @@ from itou.utils.admin import ItouModelAdmin, ReadonlyMixin
 
 @admin.register(ItouTOTPDevice)
 class ItouTOTPDeviceAdmin(ReadonlyMixin, ItouModelAdmin):
-    list_display = (
-        "user",
-        "created_at",
-        "last_used_at",
-    )
+    list_display = ("user", "enabled", "created_at", "last_used_at")
     list_select_related = ("user",)
 
     def get_fields(self, request, obj=None):
@@ -19,6 +16,14 @@ class ItouTOTPDeviceAdmin(ReadonlyMixin, ItouModelAdmin):
         fields.remove("key")
         fields.insert(0, "created_at")
         return fields
+
+    @admin.display(description="activé")
+    def enabled(self, obj):
+        return display_for_value(
+            obj.disabled_at is None,
+            empty_value_display="unused kwarg",
+            boolean=True,
+        )
 
 
 admin.site.unregister(TOTPDevice)
