3596fd958c1106c66715b251d09a7aefa9b90c1d
Damien Baty <damien.baty@inclusion.gouv.fr>
2026-08-06 17:00:43 +0200

    companies: Remove custom workaround in company import command
    The support issue is more than 2 years old (and is not accessible
    anymore). I'll suppose that the condition is not needed anymore.

--- itou/companies/management/commands/_import_siae/siae.py
+++ itou/companies/management/commands/_import_siae/siae.py
@@ -155,10 +155,7 @@ def create_new_siaes(siret_to_siae_row, conventions_by_siae_key):
                 continue
             creatable_siaes.append(build_siae(row, kind, is_active=True))
         else:
-            # TODO: Delete “ or row.siret == "52172441900036"” when email with subject
-            # “Demande de clarification ACI 521724419” sent on 17/04/2024 18:44 has been answered.
-            # https://plateforme-inclusion.zendesk.com/agent/tickets/11571
-            if existing_siae.source == CompanySource.ASP or row.siret == "52172441900036":
+            if existing_siae.source == CompanySource.ASP:
                 continue
             # Siae with this siret+kind already exists but with the wrong source.
             assert existing_siae.source in [CompanySource.USER_CREATED, CompanySource.STAFF_CREATED]
