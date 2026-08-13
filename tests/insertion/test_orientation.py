6a51354b82759b6c69c0c5df65683e5f0a178849
Ewen Corre <ewen.corre@inclusion.gouv.fr>
2026-08-13 15:13:16 +0200

    insertion: send emails when orientation is accepted

--- tests/insertion/test_orientation.py
+++ tests/insertion/test_orientation.py
@@ -172,11 +172,18 @@ def test_sender_can_view_personal_information(membership_factory, expected):
     )
 
 
-def test_transition_accept():
-    orientation = OrientationFactory()
+@pytest.mark.parametrize("sender_is_referent", [True, False], ids=["sender_is_referent", "sender_is_not_referent"])
+def test_transition_accept(sender_is_referent, mailoutbox, django_capture_on_commit_callbacks):
+    orientation = OrientationFactory(
+        status=OrientationStatus.PENDING, service__contact_email="service.contact@email.fake"
+    )
+    if sender_is_referent:
+        orientation.referent_email = orientation.sender.email
+        orientation.save()
     timestamp = datetime.datetime(2026, 8, 6, 12, 0, tzinfo=datetime.UTC)
     with freeze_time(timestamp):
-        orientation.accept()
+        with django_capture_on_commit_callbacks(execute=True):
+            orientation.accept()
 
     log = OrientationTransitionLog.objects.get(
         orientation=orientation,
@@ -187,6 +194,18 @@ def test_transition_accept():
     )
     assert log.orientation.status == OrientationStatus.ACCEPTED
     assert log.orientation.updated_at == timestamp
+
+    assert OrientationProcessLink.objects.filter(created_at=timestamp).exists()
+
+    # Notifications
+    if sender_is_referent:
+        [structure_email, beneficiary_email, sender_email] = mailoutbox
+    else:
+        [structure_email, referent_email, beneficiary_email, sender_email] = mailoutbox
+        assert referent_email.to == [orientation.referent_email]
+    assert structure_email.to == [orientation.service.contact_email]
+    assert beneficiary_email.to == [orientation.beneficiary.email]
+    assert sender_email.to == [orientation.sender.email]
 
 
 def test_transition_refuse():
