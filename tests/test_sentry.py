543eae8d2d547947d3e3ead6dbb2e9668c16db44
Julien Palard <julien@palard.fr>
2026-08-21 15:04:22 +0200

    FIX flacky test on Sentry.
    Problem was: breadcrumbs were stored by Sentry inside the isoluation
    scope, not only in the scope created by the context manager.
    
    So we randomly had other breadcrumbs in the call (depending on the
    order the tests are executed I think), sometimes there was a lot of
    them not just two.
    
    Cleaning the isolation scope before the test solves this, we
    consistently have a single breadcrumb at the end of the test.

--- tests/test_sentry.py
+++ tests/test_sentry.py
@@ -4,27 +4,27 @@ import sentry_sdk
 
 def test_before_send_http_breadcrumb_sanitizer(mocker, respx_mock):
     mocker.spy(sentry_sdk.client._Client, "_prepare_event")
-    with sentry_sdk.new_scope() as scope:
-        url = "https://example.com/"
-        params = {
-            "foobar": 34,
-            "nomNaissance": "Martin",
-            "prenoms[]": "Jean",
-            "jourDateNaissance": 12,
-            "moisDateNaissance": 5,
-            "anneeDateNaissance": 1980,
-        }
-        respx_mock.get(url, params=params).mock(return_value=httpx.Response(418))
-        httpx.get(url, params=params)
-        scope.capture_message("Test message")
+    sentry_sdk.get_isolation_scope().clear_breadcrumbs()
+    url = "https://example.com/"
+    params = {
+        "foobar": 34,
+        "nomNaissance": "Martin",
+        "prenoms[]": "Jean",
+        "jourDateNaissance": 12,
+        "moisDateNaissance": 5,
+        "anneeDateNaissance": 1980,
+    }
+    respx_mock.get(url, params=params).mock(return_value=httpx.Response(418))
+    httpx.get(url, params=params)
+    sentry_sdk.capture_message("Test message")
     assert sentry_sdk.client._Client._prepare_event.call_count == 1
     assert sentry_sdk.client._Client._prepare_event.spy_return["message"] == "Test message"
-    http_breacrumbs = [
+    http_breadcrumbs = [
         breadcrumb
         for breadcrumb in sentry_sdk.client._Client._prepare_event.spy_return["breadcrumbs"]["values"]
         if breadcrumb["type"] == "http"
     ]
-    [http_breadcrumb] = http_breacrumbs
+    [http_breadcrumb] = http_breadcrumbs
     assert http_breadcrumb["data"]["http.query"] == (
         "foobar=34&nomNaissance=_REDACTED_&prenoms%5B%5D=_REDACTED_&jourDateNaissance=_REDACTED_&moisDateNaissance=_REDACTED_&anneeDateNaissance=_REDACTED_"
     )
