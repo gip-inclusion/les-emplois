a598bc5c11f4e5241e3a3947dd9ea7c661da5bfe
Damien Baty <damien.baty@inclusion.gouv.fr>
2026-09-01 18:00:52 +0200

    scripts: Add script to reset 2FA

--- tests/otp/factories.py
+++ tests/otp/factories.py
@@ -2,8 +2,8 @@ import uuid
 
 import factory
 
-from itou.otp.models import ItouTOTPDevice
-from tests.users.factories import ItouStaffFactory
+from itou.otp.models import ItouStaticDevice, ItouStaticToken, ItouTOTPDevice
+from tests.users.factories import ItouStaffFactory, UserFactory
 
 
 class ItouTOTPDeviceFactory(factory.django.DjangoModelFactory):
@@ -12,3 +12,23 @@ class ItouTOTPDeviceFactory(factory.django.DjangoModelFactory):
 
     user = factory.SubFactory(ItouStaffFactory)
     name = factory.LazyFunction(uuid.uuid4)
+
+
+class ItouStaticDeviceFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = ItouStaticDevice
+
+    user = factory.SubFactory(UserFactory)
+
+
+class ItouStaticTokenFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = ItouStaticToken
+
+    class Params:
+        user = factory.SubFactory(UserFactory)
+
+    device = factory.SubFactory(
+        ItouStaticDeviceFactory,
+        user=factory.SelfAttribute("..user"),
+    )
