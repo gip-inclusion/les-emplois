8cb17c6b665dd42480f6662b0342d81a5953d806
François Freitag <mail@franek.fr>
2026-09-11 09:23:09 +0200

    Create a unique search page
    With the work on plateforme.inclusion.gouv.fr, the focus is shifting
    from pure employment towards the broader ecosystem (services,
    unauthorized prescribers of different kinds).
    
    Centralize the search types so that users can more easily discover
    services offered by the platform.
    
    To better greet users, the landing page should be more configurable,
    allow A/B testing, be easier to update by bizdevs… Instead of building a
    CMS inside the app, the landing page is now loaded as an iframe,
    allowing external management (by the product team) while keeping
    isolation from the site sensitive data (in particular, the `sessionid`
    cookie).
    
    To preserve functionality for links in emails (or bookmarks, etc) that
    pointed to a specific search, the iframe listens to a GET parameter
    (`tab`) to select a type of search. The existing URLs load the frame
    with their parameter, to avoid surprising users by changing their search
    type.
    
    Since iframes are isolated from the host, some features need
    cooperation:
    
    - adjusting the host container height to the iframe size. The iframe
      reports its height on load, plus any time it changes. The host listens
      to the iframe messages and adjusts the container height accordingly.
    
    - tracking user actions in the iframe. The iframe embeds tracking logic
      and emits events, the host listens to those events and forwards them
      to Matomo, when users have consented to tracking.
    
    To avoid full disruption if the external frame isn’t available, a
    rudimentary search fallback was implemented, that only searches for
    employers.
    
    Soon, the search forms will be moved from the iframe back into the
    project, so the search feature will remain even when the iframe is not
    available, making it much less critical.

--- tests/www/home/tests.py
+++ tests/www/home/tests.py
@@ -5,11 +5,15 @@ from itou.www.constants import REDIRECTED_FROM_OLD_DOMAIN_QUERY_PARAM
 from tests.users.factories import PrescriberFactory
 
 
-def test_home_anonymous(client):
+def test_home_anonymous(client, settings):
+    settings.PLATEFORME_ACCUEIL_BASE_URL = "https://plateforme.accueil.fr"
     url = reverse("home:hp")
     response = client.get(url, follow=True)
     assertRedirects(response, reverse("search:employers_home"))
-    assertContains(response, "Rechercher un emploi inclusif")
+    assertContains(
+        response,
+        'data-plateforme-accueil="https://plateforme.accueil.fr?host=localhost%3A8000"',
+    )
 
     query = {REDIRECTED_FROM_OLD_DOMAIN_QUERY_PARAM: "1"}
     response = client.get(url, query_params=query)
