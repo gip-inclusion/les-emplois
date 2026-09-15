87f025922287ab2f667cc6ed15cea3cc40f78705
Xavier Fernandez <xavier.fernandez@inclusion.gouv.fr>
2026-09-15 21:35:56 +0200

    employee_record: adapt sanitize_employee_records method
    To new update_employee_record_watched_data_updated_at trigger:
    now when an approval date is modified, the linked employee records's
    updated_at fields are also updated, short-circuiting
    missed_notifications() filtering.
    
    Instead use watched_data_updated_at field to check if an employee
    record's approval dates need to be sent to the ASP.

--- itou/employee_record/models.py
+++ itou/employee_record/models.py
@@ -7,8 +7,7 @@ from dateutil.relativedelta import relativedelta
 from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.db import models
-from django.db.models import Exists, F, Max, OuterRef, Q, Subquery
-from django.db.models.functions import Greatest
+from django.db.models import Exists, OuterRef, Q, Subquery
 from django.db.models.query import QuerySet
 from django.utils import timezone
 from django_xworkflows import models as xwf_models
@@ -184,20 +183,6 @@ class EmployeeRecordQuerySet(models.QuerySet):
                 created_at__lt=timezone.now() - relativedelta(months=6),  # 6-months grace period if recently created
                 updated_at__lt=timezone.now() - relativedelta(months=1),  # 1-month grace period if recently updated
             )
-        )
-
-    def missed_notifications(self):
-        return self.annotate(
-            last_employee_record_snapshot=Greatest(
-                # We take `updated_at` and not `created_at` to mimic how the trigger would have behaved if the
-                # employee record was never ARCHIVED. For exemple, if the ER was DISABLED before ARCHIVED then no
-                # notification would have been sent, the trigger ask for a PROCESSED, if a prolongation was
-                # submitted between those two events.
-                F("updated_at"),
-                Max(F("update_notifications__created_at")),
-            ),
-        ).filter(
-            last_employee_record_snapshot__lt=F("job_application__approval__updated_at"),
         )
 
     def with_siret_from_asp_source(self):
@@ -488,7 +473,7 @@ class EmployeeRecord(ASPExchangeInformation, xwf_models.WorkflowEnabled):
         ]:
             transition = getattr(self, transition_name)
             if transition.is_available():
-                if EmployeeRecord.objects.missed_notifications().filter(pk=self.pk).exists():
+                if self.has_watched_data_updated_at_set():
                     EmployeeRecordUpdateNotification.objects.update_or_create(
                         employee_record=self,
                         status=NotificationStatus.NEW,
