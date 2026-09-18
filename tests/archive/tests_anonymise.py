649e41b3553a43ba9817b823a25d20d53be24d07
emillumine <camille@yaal.coop>
2026-09-18 12:43:47 +0200

    fix: anonymize professional command reset updated_at and updated_by fields
    align with deactivate_user admin action
    updated_at is auto_now, but auto_now is only automatically updated when calling Model.save()
    The field isn’t updated when making updates to other fields in other ways such as QuerySet.update()
    https://docs.djangoproject.com/en/6.0/ref/models/fields/#django.db.models.DateField.auto_now

---
+++ tests/archive/tests_anonymise.py
@@ -0,0 +1,52 @@
+from itou.archive.anonymize import anonymize_professionals_without_deletion
+from itou.companies.models import CompanyMembership
+from itou.users.models import User
+from itou.utils import triggers
+from tests.cities.factories import create_city_saint_andre
+from tests.companies.factories import CompanyMembershipFactory
+from tests.users.factories import ItouStaffFactory
+
+
+def test_anonymize_professional_without_deletion():
+    city = create_city_saint_andre()
+    membership = CompanyMembershipFactory(
+        user__is_active=True,
+        user__first_name="Alice",
+        user__last_name="Cooper",
+        user__phone=f"06060{city.post_codes[0]}",
+        user__address_line_1="8 rue du moulin",
+        user__address_line_2="Apt 4B",
+        user__post_code=city.post_codes[0],
+        user__city="Test City",
+        user__coords=city.coords,
+        user__insee_city=city,
+        user__email=f"test{city.post_codes[0]}@mail.com",
+        user__with_verified_email=True,
+        is_active=True,
+        is_admin=True,
+        updated_by=ItouStaffFactory(),
+    )
+    employer = membership.user
+    updated_at_before_anonymization = membership.updated_at
+
+    with triggers.fake_context():
+        anonymize_professionals_without_deletion([employer])
+
+    employer = User.objects.get(pk=employer.pk)
+    assert not employer.is_active
+    assert employer.email is None
+    assert employer.phone == ""
+    assert employer.address_line_1 == ""
+    assert employer.address_line_2 == ""
+    assert employer.post_code == ""
+    assert employer.city == ""
+    assert employer.coords is None
+    assert employer.insee_city is None
+    assert employer.first_name == "Alice"
+    assert employer.last_name == "Cooper"
+
+    membership = CompanyMembership.include_inactive.get(user=employer)
+    assert not membership.is_active
+    assert not membership.is_admin
+    assert membership.updated_by is None
+    assert membership.updated_at > updated_at_before_anonymization
