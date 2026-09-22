0d568339e6f0d17dff9ad83c34ad6ac5f269c946
Mathieu Dupuy <mathieu.dupuy@inclusion.gouv.fr>
2026-09-22 18:00:16 +0200

    directory: Add person contact details

---
+++ itou/www/directory_views/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from itou.www.directory_views import views
+
+
+app_name = "directory"
+
+urlpatterns = [
+    path("personnes/<str:key>/", views.person_detail, name="person_detail"),
+    path("personnes/<str:key>/contact/<str:field>/", views.reveal_contact, name="reveal_contact"),
+]
