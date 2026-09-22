348c4ced43d98ac49ad584385ba49bb69adfafc5
Mathieu Dupuy <mathieu.dupuy@inclusion.gouv.fr>
2026-09-22 17:59:28 +0200

    directory: Add beta notice

---
+++ itou/static/js/directory.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const betaNotice = document.getElementById("directory-beta-notice");
+const betaNoticeCookie = "notice_beta_hidden=1";
+
+function hideBetaNotice() {
+  betaNotice.classList.add("d-none");
+  const expires = new Date(Date.now() + 15 * 24 * 60 * 60 * 1000).toUTCString();
+  document.cookie = `${betaNoticeCookie}; expires=${expires}; path=/; SameSite=Lax`;
+}
+
+if (betaNotice) {
+  if (!document.cookie.split("; ").includes(betaNoticeCookie)) {
+    betaNotice.classList.remove("d-none");
+  }
+  betaNotice.querySelector(".btn-close").addEventListener("click", hideBetaNotice);
+  document.getElementById("directory-beta-profile-link").addEventListener("click", hideBetaNotice);
+}
